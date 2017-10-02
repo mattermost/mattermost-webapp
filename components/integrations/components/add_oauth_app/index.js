1047b94a080ffe3b57a4e04dd9e3065b86bc4ed2
JoramWilander <jwawilander@gmail.com>
2017-10-02 10:28:16 -0400

    Hide trusted option in OAuth app UI for non-admins

--- components/integrations/components/add_oauth_app/index.js
+++ components/integrations/components/add_oauth_app/index.js
@@ -4,13 +4,15 @@
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {addOAuthApp} from 'mattermost-redux/actions/integrations';
+import {isCurrentUserSystemAdmin} from 'mattermost-redux/selectors/entities/users';
 
 import AddOAuthApp from './add_oauth_app.jsx';
 
 function mapStateToProps(state, ownProps) {
     return {
         ...ownProps,
-        addOAuthAppRequest: state.requests.integrations.addOAuthApp
+        addOAuthAppRequest: state.requests.integrations.addOAuthApp,
+        isSystemAdmin: isCurrentUserSystemAdmin(state)
     };
 }
 
