561c5a5d859f38cc457ef17e710caf60972759e7
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2017-10-04 15:10:37 +0500

    Configure enzyme to work with react 15
    After updating enzyme to 3(commit:
    https://github.com/mattermost/mattermost-webapp/commit/561b93b3eee4a13d849d0bbe9b0eea72427fd2bf)
    
    Need to add Adapter for
    enzyme(http://airbnb.io/enzyme/docs/guides/migration-from-2-to-3.html)

---
+++ tests/setup.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+import Adapter from 'enzyme-adapter-react-15';
+import {configure} from 'enzyme';
+
+configure({adapter: new Adapter()});
