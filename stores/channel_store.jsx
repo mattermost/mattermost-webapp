1a4b579635a14e490cd3e59d7f2cdb530e88130f
Saturnino Abril <saturnino.abril@gmail.com>
2017-10-06 17:17:44 +0800

    move import UserStore before the simport Selectors

--- stores/channel_store.jsx
+++ stores/channel_store.jsx
@@ -5,13 +5,14 @@ import EventEmitter from 'events';
 
 import {batchActions} from 'redux-batched-actions';
 
+import UserStore from 'stores/user_store.jsx'; // eslint-disable-line import/order
+
 import {ChannelTypes, UserTypes} from 'mattermost-redux/action_types';
 import * as Selectors from 'mattermost-redux/selectors/entities/channels';
 
 import AppDispatcher from 'dispatcher/app_dispatcher.jsx';
 import store from 'stores/redux_store.jsx';
 import TeamStore from 'stores/team_store.jsx';
-import UserStore from 'stores/user_store.jsx';
 
 import {ActionTypes, Constants} from 'utils/constants.jsx';
 import {isFromWebhook, isSystemMessage} from 'utils/post_utils.jsx';
