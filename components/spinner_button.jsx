9c4fc8c4a17ce1a7be99c630d4c95f558a2adf99
Saturnino Abril <saturnino.abril@gmail.com>
2017-10-23 18:32:40 +0800

    refactor SpinnerButton and fix component test

--- components/spinner_button.jsx
+++ components/spinner_button.jsx
@@ -2,23 +2,31 @@
 // See License.txt for license information.
 
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {PureComponent} from 'react';
 
 import loadingGif from 'images/load.gif';
 
-export default class SpinnerButton extends React.Component {
-    static get propTypes() {
-        return {
-            children: PropTypes.node,
-            spinning: PropTypes.bool.isRequired,
-            onClick: PropTypes.func
-        };
+export default class SpinnerButton extends PureComponent {
+    static defaultProps = {
+        spinning: false
     }
 
-    static get defaultProps() {
-        return {
-            spinning: false
-        };
+    static propTypes = {
+
+        /**
+         * Children to show when not spinning
+         */
+        children: PropTypes.node,
+
+        /**
+         * Set to true to spin
+         */
+        spinning: PropTypes.bool.isRequired,
+
+        /**
+         * Callback function when button is clicked
+         */
+        onClick: PropTypes.func
     }
 
     render() {
