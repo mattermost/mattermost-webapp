9a8cc71e028cfc7b8c3ccb3acd575cd013a46455
KY <infcurious@gmail.com>
2017-10-25 23:47:55 +0800

    Add component tests

---
+++ tests/components/unread_channel_indicator.test.jsx
@@ -0,0 +1,63 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+import React from 'react';
+
+import {shallow} from 'enzyme';
+
+import UnreadChannelIndicator from 'components/unread_channel_indicator.jsx';
+
+describe('components/UnreadChannelIndicator', () => {
+    test('should match snapshot', () => {
+        function emptyFunction() {} //eslint-disable-line no-empty-function
+
+        const wrapper = shallow(
+            <UnreadChannelIndicator
+                onClick={emptyFunction}
+            />
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should match snapshot when show is set', () => {
+        function emptyFunction() {} //eslint-disable-line no-empty-function
+
+        const wrapper = shallow(
+            <UnreadChannelIndicator
+                onClick={emptyFunction}
+                show={true}
+            />
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should match snapshot when content is text', () => {
+        function emptyFunction() {} //eslint-disable-line no-empty-function
+
+        const wrapper = shallow(
+            <UnreadChannelIndicator
+                onClick={emptyFunction}
+                show={true}
+                content='foo'
+            />
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should match snapshot when content is an element', () => {
+        function emptyFunction() {} //eslint-disable-line no-empty-function
+
+        const wrapper = shallow(
+            <UnreadChannelIndicator
+                onClick={emptyFunction}
+                show={true}
+                content={<div>{'foo'}</div>}
+            />
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+});
