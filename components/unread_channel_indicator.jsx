b08e8c17eb99cabe3d11c800e3f983fa38326568
KY <tkbky@users.noreply.github.com>
2017-10-26 10:30:10 +0800

    Migrate unread channel indicator to be pure (#201)
    * Migrate unread channel indicator to be pure
    
    * Rename props text to content for clarity
    
    * Use correct proptypes for content
    
    * Add component tests
    
    * Add additional test for function/event call on props

--- components/unread_channel_indicator.jsx
+++ components/unread_channel_indicator.jsx
@@ -6,38 +6,62 @@ import React from 'react';
 
 import Constants from 'utils/constants.jsx';
 
-export default function UnreadChannelIndicator(props) {
-    const unreadIcon = Constants.UNREAD_ICON_SVG;
-    let displayValue = 'none';
-    if (props.show) {
-        displayValue = 'block';
+export default class UnreadChannelIndicator extends React.PureComponent {
+    static propTypes = {
+
+      /**
+       * Function to call when the indicator is clicked
+       */
+        onClick: PropTypes.func.isRequired,
+
+      /**
+       * Set whether to show the indicator or not
+       */
+        show: PropTypes.bool,
+
+      /**
+       * The additional CSS class for the indicator
+       */
+        extraClass: PropTypes.string,
+
+      /**
+       * The content of the indicator
+       */
+        content: PropTypes.node,
+
+      /**
+       * The name of the indicator
+       */
+        name: PropTypes.string
     }
 
-    return (
-        <div
-            id={'unreadIndicator' + props.name}
-            className={'nav-pills__unread-indicator ' + props.extraClass}
-            style={{display: displayValue}}
-            onClick={props.onClick}
-        >
-            {props.text}
-            <span
-                className='icon icon__unread'
-                dangerouslySetInnerHTML={{__html: unreadIcon}}
-            />
-        </div>
-    );
-}
+    static defaultProps = {
+        show: false,
+        extraClass: '',
+        content: ''
+    }
 
-UnreadChannelIndicator.defaultProps = {
-    show: false,
-    extraClass: '',
-    text: ''
-};
-UnreadChannelIndicator.propTypes = {
-    onClick: PropTypes.func.isRequired,
-    show: PropTypes.bool,
-    extraClass: PropTypes.string,
-    text: PropTypes.object,
-    name: PropTypes.string
-};
+    render() {
+        const unreadIcon = Constants.UNREAD_ICON_SVG;
+
+        let displayValue = 'none';
+        if (this.props.show) {
+            displayValue = 'block';
+        }
+
+        return (
+            <div
+                id={'unreadIndicator' + this.props.name}
+                className={'nav-pills__unread-indicator ' + this.props.extraClass}
+                style={{display: displayValue}}
+                onClick={this.props.onClick}
+            >
+                {this.props.content}
+                <span
+                    className='icon icon__unread'
+                    dangerouslySetInnerHTML={{__html: unreadIcon}}
+                />
+            </div>
+        );
+    }
+}
