09e71db69e431be35ce1e5a54bc1f4dc37a7034d
Andre Vasconcelos <andre.onogoro@gmail.com>
2017-11-14 09:53:19 +0900

    Changing fileUrl value

--- tests/components/file_info_preview.test.jsx
+++ tests/components/file_info_preview.test.jsx
@@ -9,7 +9,7 @@ import FileInfoPreview from 'components/file_info_preview.jsx';
 
 describe('components/file_info_preview.jsx', () => {
     const requiredProps = {
-        fileUrl: 'http://www.catphotos.org/wp-content/uploads/2013/02/cat-standing.jpg',
+        fileUrl: 'https://pre-release.mattermost.com/api/v4/files/rqir81f7a7ft8m6j6ej7g1txuo',
         fileInfo: {name: 'Test Image', size: 100, extension: 'jpg'}
     };
 
