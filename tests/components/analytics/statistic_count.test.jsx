bf2c1d486b8d1c7f856ff51b23c579430908cda9
Jesús Espino <jespinog@gmail.com>
2017-11-19 00:20:15 +0100

    GH-7780: Migrating analytics/statistic_count to PureComponent

---
+++ tests/components/analytics/statistic_count.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import {shallow} from 'enzyme';
+
+import StatisticCount from 'components/analytics/statistic_count.jsx';
+
+describe('components/analytics/statistic_count.jsx', () => {
+    test('should match snapshot, on loading', () => {
+        const wrapper = shallow(
+            <StatisticCount
+                title='Test'
+                icon='test-icon'
+            />
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should match snapshot, loaded', () => {
+        const wrapper = shallow(
+            <StatisticCount
+                title='Test'
+                icon='test-icon'
+                count={4}
+            />
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+});
