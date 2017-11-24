d8e955782292ef8139852ad94ce212b30f2ea4ba
Jonathan Fritz <jonathan.fritz@mattermost.com>
2017-11-24 09:46:26 -0500

    The same issue impacted the the signup_email page when an invite link was specified

--- components/signup/components/signup_email.jsx
+++ components/signup/components/signup_email.jsx
@@ -492,8 +492,7 @@ export default class SignupEmail extends React.Component {
                             />
                             {' '}
                             <Link
-                                to={'/login'}
-                                query={Utils.isEmptyObject(this.props.location.query) ? null : this.props.location.query}
+                                to={'/login' + this.props.location.search}
                             >
                                 <FormattedMessage
                                     id='signup_user_completed.signIn'
