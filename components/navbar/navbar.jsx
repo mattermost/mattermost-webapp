a75c09a2a1e7d5dd0f22b1ead84346d62a8ef25f
JoramWilander <jwawilander@gmail.com>
2017-12-04 20:26:07 -0500

    Fix mobile view search button

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -24,7 +24,7 @@ import UserStore from 'stores/user_store.jsx';
 import WebrtcStore from 'stores/webrtc_store.jsx';
 
 import * as ChannelUtils from 'utils/channel_utils.jsx';
-import {ActionTypes, Constants, UserStatuses} from 'utils/constants.jsx';
+import {ActionTypes, Constants, UserStatuses, RHSStates} from 'utils/constants.jsx';
 import * as Utils from 'utils/utils.jsx';
 
 import ChannelInfoModal from 'components/channel_info_modal';
@@ -47,7 +47,8 @@ export default class Navbar extends React.Component {
         teamDisplayName: PropTypes.string,
         isPinnedPosts: PropTypes.bool,
         actions: PropTypes.shape({
-            closeRightHandSide: PropTypes.func
+            closeRightHandSide: PropTypes.func,
+            updateRhsState: PropTypes.func
         })
     };
 
@@ -160,9 +161,7 @@ export default class Navbar extends React.Component {
     }
 
     showSearch = () => {
-        AppDispatcher.handleServerAction({
-            type: ActionTypes.SHOW_SEARCH
-        });
+        this.props.actions.updateRhsState(RHSStates.SEARCH);
     }
 
     onChange = () => {
