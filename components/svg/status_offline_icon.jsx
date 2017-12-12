6de47c185ea2b66ee2cb77e879be43579479e772
Harrison Healey <harrisonmhealey@gmail.com>
2017-12-12 16:15:13 -0500

    Stopped using dangerouslySetInnerHtml for status icons

---
+++ components/svg/status_offline_icon.jsx
@@ -0,0 +1,29 @@
+// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+import React from 'react';
+
+export default class StatusOfflineIcon extends React.PureComponent {
+    render() {
+        return (
+            <span {...this.props}>
+                <svg
+                    className='offline--icon'
+                    width='100%'
+                    height='100%'
+                    viewBox='0 0 20 20'
+                    style={style}
+                >
+                    <path d='M10,0c5.519,0 10,4.481 10,10c0,5.519 -4.481,10 -10,10c-5.519,0 -10,-4.481 -10,-10c0,-5.519 4.481,-10 10,-10Zm0,2c4.415,0 8,3.585 8,8c0,4.415 -3.585,8 -8,8c-4.415,0 -8,-3.585 -8,-8c0,-4.415 3.585,-8 8,-8Z'/>
+                </svg>
+            </span>
+        );
+    }
+}
+
+const style = {
+    fillRule: 'evenodd',
+    clipRule: 'evenodd',
+    strokeLinejoin: 'round',
+    strokeMiterlimit: 1.41421
+};
