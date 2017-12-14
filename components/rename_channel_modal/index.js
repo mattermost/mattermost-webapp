a1a404e588ae14715c1fad6e5ae9b3c1fc9ee1a7
Saturnino Abril <saturnino.abril@gmail.com>
2017-12-15 02:30:40 +0800

    fix regression on Rename Channel URL box showing undefined for domain name

--- components/rename_channel_modal/index.js
+++ components/rename_channel_modal/index.js
@@ -6,16 +6,20 @@ import {connect} from 'react-redux';
 import {createSelector} from 'reselect';
 import {updateChannel as UpdateChannel} from 'mattermost-redux/actions/channels';
 
-import {getCurrentTeamUrl, getTeam} from 'mattermost-redux/selectors/entities/teams';
+import {getTeam} from 'mattermost-redux/selectors/entities/teams';
+
+import {getSiteURL} from 'utils/url.jsx';
 
 import RenameChannelModal from './rename_channel_modal.jsx';
 
 const mapStateToProps = createSelector(
     (state) => {
         const currentTeamId = state.entities.teams.currentTeamId;
+        const team = getTeam(state, currentTeamId);
+        const currentTeamUrl = `${getSiteURL()}/${team.name}`;
         return {
-            currentTeamUrl: getCurrentTeamUrl(state),
-            team: getTeam(state, currentTeamId)
+            currentTeamUrl,
+            team
         };
     },
     (teamInfo) => ({...teamInfo})
