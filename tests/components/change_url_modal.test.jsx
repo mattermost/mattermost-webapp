e3c4b00899489d3576d00f915d281df9fe39665a
Adrian Carolli <adrian.carolli1@gmail.com>
2017-12-15 18:04:44 -0500

    Remove .jsx from file path

--- tests/components/change_url_modal.test.jsx
+++ tests/components/change_url_modal.test.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import {Modal} from 'react-bootstrap';
 
-import {mountWithIntl} from 'tests/helpers/intl-test-helper.jsx';
+import {mountWithIntl} from 'tests/helpers/intl-test-helper';
 
 import ChangeURLModal from 'components/change_url_modal/change_url_modal';
 
