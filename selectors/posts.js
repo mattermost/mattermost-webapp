3f5f09090b731facdc597b2d5ddaf8f373c278db
Harrison Healey <harrisonmhealey@gmail.com>
2017-12-15 13:50:35 -0500

    Fixed getEditingPost selector not being memoized

--- selectors/posts.js
+++ selectors/posts.js
@@ -1,14 +1,23 @@
+// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
 import {createSelector} from 'reselect';
 
 import {getPost} from 'mattermost-redux/selectors/entities/posts';
 
 export const getEditingPost = createSelector(
-    (state) => state,
+    (state) => {
+        if (state.views.posts.editingPost && state.views.posts.editingPost.postId) {
+            return getPost(state, state.views.posts.editingPost.postId);
+        }
+
+        return null;
+    },
     (state) => state.views.posts.editingPost,
-    (state, editingPost) => {
+    (post, editingPost) => {
         return {
             ...editingPost,
-            post: getPost(state, editingPost.postId)
+            post
         };
     }
 );
