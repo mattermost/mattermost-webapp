dd5e698166c61421b00389ef4ee7a37121458827
Saturnino Abril <saturnino.abril@gmail.com>
2017-12-22 02:35:43 +0800

    add loading spinner to MFA sign in button

---
+++ tests/components/login/login_mfa.test.jsx
@@ -0,0 +1,55 @@
+// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import LoginMfa from 'components/login/components/login_mfa.jsx';
+
+describe('components/login/LoginMfa', () => {
+    const baseProps = {
+        loginId: 'logid_id',
+        password: 'password',
+        submit: jest.fn()
+    };
+
+    test('should match snapshot', () => {
+        const wrapper = shallow(
+            <LoginMfa {...baseProps}/>
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should have match state when handleChange is called', () => {
+        const wrapper = shallow(
+            <LoginMfa {...baseProps}/>
+        );
+
+        wrapper.setState({token: ''});
+        wrapper.instance().handleChange({preventDefault: jest.fn(), target: {value: '123456'}});
+
+        expect(wrapper.state('token')).toEqual('123456');
+    });
+
+    test('should have match state when handleSubmit is called', () => {
+        const submit = jest.fn();
+        const props = {...baseProps, submit};
+        const wrapper = shallow(
+            <LoginMfa {...props}/>
+        );
+
+        wrapper.setState({token: '', serverError: '', saving: false});
+        wrapper.instance().handleSubmit({preventDefault: jest.fn()});
+        expect(wrapper.state('serverError')).toEqual('Please enter an MFA token');
+        expect(wrapper.state('saving')).toEqual(false);
+        expect(submit).not.toBeCalled();
+
+        wrapper.setState({token: '123456', serverError: ''});
+        wrapper.instance().handleSubmit({preventDefault: jest.fn()});
+        expect(wrapper.state('serverError')).toEqual('');
+        expect(wrapper.state('saving')).toEqual(true);
+        expect(submit).toBeCalled();
+        expect(submit).toBeCalledWith(props.loginId, props.password, '123456');
+    });
+});
