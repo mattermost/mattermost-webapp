66f839fbfc193248da0b957c2df01bc65b16b23c
Sudheer <sudheer.105@gmail.com>
2017-12-21 18:53:07 +0530

    Change system.import to import statements (#501)

--- routes/route_emoji.jsx
+++ routes/route_emoji.jsx
@@ -6,18 +6,18 @@ import * as RouteUtils from 'routes/route_utils.jsx';
 export default {
     path: 'emoji',
     getComponents: (location, callback) => {
-        System.import('components/backstage/backstage_controller.jsx').then(RouteUtils.importComponentSuccess(callback));
+        import('components/backstage/backstage_controller.jsx').then(RouteUtils.importComponentSuccess(callback));
     },
     indexRoute: {
         getComponents: (location, callback) => {
-            System.import('components/emoji/components/emoji_list.jsx').then(RouteUtils.importComponentSuccess(callback));
+            import('components/emoji/components/emoji_list.jsx').then(RouteUtils.importComponentSuccess(callback));
         }
     },
     childRoutes: [
         {
             path: 'add',
             getComponents: (location, callback) => {
-                System.import('components/emoji/components/add_emoji.jsx').then(RouteUtils.importComponentSuccess(callback));
+                import('components/emoji/components/add_emoji.jsx').then(RouteUtils.importComponentSuccess(callback));
             }
         }
     ]
