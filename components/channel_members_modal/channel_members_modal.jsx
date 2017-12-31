fb1dbbe9fb4a706feb3ae1d4d4b5adbe11ab3b8c
Sam Kvale <skvale@widen.com>
2017-12-31 13:13:28 -0600

    Migrate channel_members_modal.jsx to redux

---
+++ components/channel_members_modal/channel_members_modal.jsx
@@ -0,0 +1,76 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import {Modal} from 'react-bootstrap';
+import {FormattedMessage} from 'react-intl';
+
+import MemberListChannel from 'components/member_list_channel';
+
+export default class ChannelMembersModal extends React.PureComponent {
+    static propTypes = {
+        canManageChannelMembers: PropTypes.bool.isRequired,
+        channel: PropTypes.shape({
+            display_name: PropTypes.string.isRequired
+        }).isRequired,
+        onModalDismissed: PropTypes.func.isRequired,
+        showInviteModal: PropTypes.bool.isRequired
+    }
+
+    state = {
+        show: true
+    };
+
+    onHide = () => {
+        this.setState({
+            show: false
+        });
+    }
+
+    onClickManageChannelsButton = () => {
+        this.props.showInviteModal();
+        this.onHide();
+    }
+
+    render() {
+        return (
+            <div>
+                <Modal
+                    dialogClassName='more-modal more-modal--action'
+                    onExited={this.props.onModalDismissed}
+                    onHide={this.onHide}
+                    show={this.state.show}
+                >
+                    <Modal.Header closeButton={true}>
+                        <Modal.Title>
+                            <span className='name'>{this.props.channel.display_name}</span>
+                            <FormattedMessage
+                                id='channel_members_modal.members'
+                                defaultMessage=' Members'
+                            />
+                        </Modal.Title>
+                        {this.props.canManageChannelMembers &&
+                            <a
+                                id='showInviteModal'
+                                className='btn btn-md btn-primary'
+                                href='#'
+                                onClick={this.onClickManageChannelsButton}
+                            >
+                                <FormattedMessage
+                                    id='channel_members_modal.addNew'
+                                    defaultMessage=' Add New Members'
+                                />
+                            </a>
+                        }
+                    </Modal.Header>
+                    <Modal.Body>
+                        <MemberListChannel
+                            channel={this.props.channel}
+                        />
+                    </Modal.Body>
+                </Modal>
+            </div>
+        );
+    }
+}
---
+++ components/channel_members_modal/index.js
@@ -0,0 +1,16 @@
+// Copyright (c) 2017 Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+import {connect} from 'react-redux';
+
+import {canManageChannelMembers} from 'mattermost-redux/selectors/entities/channels';
+
+import ChannelMembersModal from './channel_members_modal';
+
+function mapStateToProps(state) {
+    return {
+        canManageChannelMembers: canManageChannelMembers(state)
+    };
+}
+
+export default connect(mapStateToProps)(ChannelMembersModal);
