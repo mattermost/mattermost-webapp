f9fed986147987157342509c3bad67265eb0555d
Martin Kraft <mkraft@users.noreply.github.com>
2018-01-03 12:30:41 -0500

    Revert "Reverts "Wraps JS Performance API." (#520)" (#532)
    This reverts commit c79d96475e1e228438aa18503c39636704c678c9.

--- actions/diagnostics_actions.jsx
+++ actions/diagnostics_actions.jsx
@@ -22,3 +22,62 @@ export function trackEvent(category, event, props) {
         global.window.analytics.track('event', properties, options);
     }
 }
+
+/**
+ * Takes an array of string names of performance markers and invokes
+ * performance.clearMarkers on each.
+ * @param   {array} names of markers to clear
+ *
+ */
+export function clearMarks(names) {
+    if (!global.mm_config.EnableDeveloper === 'true') {
+        return;
+    }
+    names.forEach((name) => performance.clearMarks(name));
+}
+
+export function mark(name) {
+    if (!global.mm_config.EnableDeveloper === 'true') {
+        return;
+    }
+    performance.mark(name);
+}
+
+/**
+ * Takes the names of two markers and invokes performance.measure on
+ * them. The measured duration (ms) and the string name of the measure is
+ * are returned.
+ *
+ * @param   {string} name1 the first marker
+ * @param   {string} name2 the second marker
+ *
+ * @returns {[number, string]} Either the measured duration (ms) and the string name
+ * of the measure are returned or -1 and and empty string is returned if
+ * in dev. mode or one of the marker can't be found.
+ *
+ */
+export function measure(name1, name2) {
+    if (!global.mm_config.EnableDeveloper === 'true') {
+        return [-1, ''];
+    }
+
+    // Check for existence of entry name to avoid DOMException
+    const performanceEntries = performance.getEntries();
+    if (![name1, name2].every((name) => performanceEntries.find((item) => item.name === name))) {
+        return [-1, ''];
+    }
+
+    const displayPrefix = '🐐 Mattermost: ';
+    const measurementName = `${displayPrefix}${name1} - ${name2}`;
+    performance.measure(measurementName, name1, name2);
+    const lastDuration = mostRecentDurationByEntryName(measurementName);
+
+    // Clean up the measures we created
+    performance.clearMeasures(measurementName);
+    return [lastDuration, measurementName];
+}
+
+function mostRecentDurationByEntryName(entryName) {
+    const entriesWithName = performance.getEntriesByName(entryName);
+    return entriesWithName.map((item) => item.duration)[entriesWithName.length - 1];
+}
