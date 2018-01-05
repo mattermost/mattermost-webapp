ebd9176ac87fd5230f2c8137898a8c517895ffe4
Joram Wilander <jwawilander@gmail.com>
2018-01-05 08:45:24 -0500

    Minor fixes for dot and reply icon in mobile view (#551)

---
+++ actions/views/channel.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+import {isMobile} from 'utils/utils.jsx';
+import {ActionTypes} from 'utils/constants.jsx';
+
+export function checkAndSetMobileView() {
+    return (dispatch) => {
+        dispatch({
+            type: ActionTypes.UPDATE_MOBILE_VIEW,
+            data: isMobile()
+        });
+    };
+}
