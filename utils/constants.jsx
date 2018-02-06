e14695dd37eaceaa75003ec1245b3f358e8660cf
Harrison Healey <harrisonmhealey@gmail.com>
2018-02-06 16:36:48 -0500

    ICU-715 Change ExperimentalGroupUnreadChannels setting to allow for default on/off

--- utils/constants.jsx
+++ utils/constants.jsx
@@ -467,6 +467,12 @@ export const UploadStatuses = {
     DEFAULT: ''
 };
 
+export const GroupUnreadChannels = {
+    DISABLED: 'disabled',
+    DEFAULT_ON: 'default_on',
+    DEFAULT_OFF: 'default_off'
+};
+
 export const Constants = {
     PluginSettings,
     Preferences,
