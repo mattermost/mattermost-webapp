c3777192c82477640bec7ab585bbf072d449a793
Stephen Kiers <stephen@stephenkiers.com>
2018-02-06 14:17:31 -0700

    rewriting func

--- components/channel_layout/center_channel/index.js
+++ components/channel_layout/center_channel/index.js
@@ -2,13 +2,25 @@
 // See License.txt for license information.
 
 import {connect} from 'react-redux';
+import {getTeamByName} from 'mattermost-redux/selectors/entities/teams';
 
 // import {createSelector} from 'reselect';
 
+import Constants from 'utils/constants';
+import {getGlobalItem} from 'selectors/storage';
+
 import CenterChannel from './center_channel';
 
-function mapStateToProps(state) {
-    return {};
+function mapStateToProps(state, ownProps) {
+    let channelName = Constants.DEFAULT_CHANNEL;
+    const team = getTeamByName(state, ownProps.params.match.params.team);
+    if (team) {
+        const ChannelId = getGlobalItem(state, team.id, null);
+        const channel = ChannelStore.getChannelById(channelId);
+    }
+    return {
+        lastChannelPath: `${ownProps.params.match.url}/channels/${channelName}`
+    };
 }
 
 export default connect(mapStateToProps)(CenterChannel);
