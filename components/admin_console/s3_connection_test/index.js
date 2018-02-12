32ac03315bd1821c570ce36aad2b1e9caf55d297
cpanato <ctadeu@gmail.com>
2018-02-12 09:34:00 +0100

    [PLT-8186] add support for ec2 instance profile authentication

---
+++ components/admin_console/s3_connection_test/index.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2018-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {testS3Connection} from 'mattermost-redux/actions/admin';
+
+import S3ConnectionTest from './s3_connection_test.jsx';
+
+export default connect(null, mapDispatchToProps)(S3ConnectionTest);
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators({
+            testS3Connection,
+        }, dispatch),
+    };
+}
+
---
+++ tests/components/admin_console/s3_connection_test/s3_connection_test.test.jsx
@@ -0,0 +1,63 @@
+// Copyright (c) 2018-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import S3ConnectionTestButton from 'components/admin_console/s3_connection_test/s3_connection_test.jsx';
+
+describe('components/admin_console/s3_connection_test/s3_connection_test', () => {
+    test('should match snapshot, disabled', () => {
+        const baseProps = {
+            config: {},
+            getConfigFromState: jest.fn(),
+            disabled: true,
+            actions: {
+                testS3Connection: jest.fn(() => Promise.resolve({})),
+            },
+        };
+
+        const wrapper = shallow(
+            <S3ConnectionTestButton {...baseProps}/>
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should match snapshot, enable', () => {
+        const baseProps = {
+            config: {},
+            getConfigFromState: jest.fn(),
+            disabled: false,
+            actions: {
+                testS3Connection: jest.fn(() => Promise.resolve({})),
+            },
+        };
+
+        const wrapper = shallow(
+            <S3ConnectionTestButton {...baseProps}/>
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should have called handleTestConnection', () => {
+        const baseProps = {
+            config: {},
+            getConfigFromState: jest.fn(),
+            disabled: false,
+            actions: {
+                testS3Connection: jest.fn(() => Promise.resolve({})),
+            },
+        };
+
+        const wrapper = shallow(
+            <S3ConnectionTestButton {...baseProps}/>
+        );
+
+        const preventDefault = jest.fn();
+
+        wrapper.find('button').first().simulate('click', {preventDefault});
+        expect(baseProps.actions.testS3Connection).toHaveBeenCalledTimes(1);
+    });
+});
