60e7a6df398483b3d4d7d30755cf3243eb502b23
Jesse Hallam <jesse.hallam@gmail.com>
2018-02-13 11:29:11 -0500

    MM-8589: expunge global mm_config from announcement bar component

--- components/announcement_bar/index.js
+++ components/announcement_bar/index.js
@@ -7,9 +7,31 @@ import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
 import AnnouncementBar from './announcement_bar.jsx';
 
 function mapStateToProps(state, ownProps) {
+    const license = state.entities.general.license;
+    const config = state.entities.general.config;
+
+    const licenseId = license.Id;
+    const siteURL = config.SiteURL;
+    const sendEmailNotifications = config.SendEmailNotifications === 'true';
+    const bannerText = config.BannerText;
+    const allowBannerDismissal = config.AllowBannerDismissal === 'true';
+    const enableBanner = config.EnableBanner === 'true';
+    const bannerColor = config.BannerColor;
+    const bannerTextColor = config.BannerTextColor;
+    const enableSignUpWithGitLab = config.EnableSignUpWithGitLab === 'true';
+
     return {
         ...ownProps,
-        isLoggedIn: Boolean(getCurrentUserId(state))
+        isLoggedIn: Boolean(getCurrentUserId(state)),
+        licenseId,
+        siteURL,
+        sendEmailNotifications,
+        bannerText,
+        allowBannerDismissal,
+        enableBanner,
+        bannerColor,
+        bannerTextColor,
+        enableSignUpWithGitLab
     };
 }
 
