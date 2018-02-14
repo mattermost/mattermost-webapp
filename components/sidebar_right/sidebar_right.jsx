d4998b8d515f240a7977b10f68df9acc4e91d7d2
Stephen Kiers <stephen@stephenkiers.com>
2018-02-14 16:23:46 -0700

    fix rightsidebar not hiding properly (#807)
    * fix rightsidebar not hiding properly
    
    * simplified

--- components/sidebar_right/sidebar_right.jsx
+++ components/sidebar_right/sidebar_right.jsx
@@ -99,9 +99,10 @@ export default class SidebarRight extends React.Component {
         WebrtcStore.emitRhsChanged(isOpen);
 
         const wasOpen = prevProps.searchVisible || prevProps.postRightVisible;
-
-        if (isOpen && !wasOpen) {
+        if (isOpen !== wasOpen) {
             this.doStrangeThings();
+        }
+        if (isOpen && !wasOpen) {
             setTimeout(() => postListScrollChange(), 0);
         }
     }
