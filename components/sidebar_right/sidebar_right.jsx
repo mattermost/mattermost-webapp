86c46c1e820c6a261cfb9f0e71e1a4976f3c6647
Stephen Kiers <stephen@stephenkiers.com>
2018-02-14 09:40:27 -0700

    Icu 745- fix lhs popping in an out during tutorial (#795)
    * only do strange things when was closed, and is open
    
    * remove random code

--- components/sidebar_right/sidebar_right.jsx
+++ components/sidebar_right/sidebar_right.jsx
@@ -36,8 +36,6 @@ export default class SidebarRight extends React.Component {
     constructor(props) {
         super(props);
 
-        this.plScrolledToBottom = true;
-
         this.state = {
             expanded: false,
             useMilitaryTime: PreferenceStore.getBool(Constants.Preferences.CATEGORY_DISPLAY_SETTINGS, Constants.Preferences.USE_MILITARY_TIME, false)
@@ -99,11 +97,11 @@ export default class SidebarRight extends React.Component {
     componentDidUpdate(prevProps) {
         const isOpen = this.props.searchVisible || this.props.postRightVisible;
         WebrtcStore.emitRhsChanged(isOpen);
-        this.doStrangeThings();
 
         const wasOpen = prevProps.searchVisible || prevProps.postRightVisible;
 
         if (isOpen && !wasOpen) {
+            this.doStrangeThings();
             setTimeout(() => postListScrollChange(), 0);
         }
     }
