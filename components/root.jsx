489704b2981216f1bf97532db25700b0351eacf6
Jesse Hallam <jesse.hallam@gmail.com>
2018-02-14 15:12:34 -0500

    MM-8589: expunge global mm_config from get android app componen

--- components/root.jsx
+++ components/root.jsx
@@ -44,7 +44,7 @@ import loadDoVerifyEmail from 'bundle-loader?lazy!components/do_verify_email';
 import loadClaimController from 'bundle-loader?lazy!components/claim';
 import loadHelpController from 'bundle-loader?lazy!components/help/help_controller';
 import loadGetIosApp from 'bundle-loader?lazy!components/get_ios_app';
-import loadGetAndroidApp from 'bundle-loader?lazy!components/get_android_app/get_android_app';
+import loadGetAndroidApp from 'bundle-loader?lazy!components/get_android_app';
 import loadSelectTeam from 'bundle-loader?lazy!components/select_team';
 import loadAuthorize from 'bundle-loader?lazy!components/authorize';
 import loadCreateTeam from 'bundle-loader?lazy!components/create_team/create_team_controller';
