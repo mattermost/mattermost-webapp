e2e9df69480c25887851479b848b65dd5fc46b12
Jesse Hallam <jesse.hallam@gmail.com>
2018-02-16 12:56:15 -0500

    MM-8589: use the get(Config|License) selector

--- components/claim/index.js
+++ components/claim/index.js
@@ -2,11 +2,12 @@
 // See License.txt for license information.
 
 import {connect} from 'react-redux';
+import {getConfig} from 'mattermost-redux/selectors/entities/general';
 
 import ClaimController from './claim_controller.jsx';
 
 function mapStateToProps(state) {
-    const config = state.entities.general.config;
+    const config = getConfig(state);
     const siteName = config.SiteName;
     const ldapLoginFieldName = config.LdapLoginFieldName;
 
