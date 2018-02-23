c25e11a6d3369b9210580a71fa90455046f98b4c
Harrison Healey <harrisonmhealey@gmail.com>
2018-02-23 15:45:26 -0500

    MM-9586 Only update value of certain textboxes after a delay

--- components/quick_input.jsx
+++ components/quick_input.jsx
@@ -10,6 +10,12 @@ export default class QuickInput extends React.PureComponent {
     static propTypes = {
 
         /**
+         * Whether to delay updating the value of the textbox from props. Should only be used
+         * on textboxes that to properly compose CJK characters as the user types.
+         */
+        delayInputUpdate: PropTypes.bool,
+
+        /**
          * An optional React component that will be used instead of an HTML input when rendering
          */
         inputComponent: PropTypes.func,
@@ -20,11 +26,19 @@ export default class QuickInput extends React.PureComponent {
         value: PropTypes.string.isRequired,
     };
 
+    static defaultProps = {
+        delayInputUpdate: false,
+    };
+
     componentDidUpdate(prevProps) {
         if (prevProps.value !== this.props.value) {
-            requestAnimationFrame(() => {
+            if (this.props.delayInputUpdate) {
+                requestAnimationFrame(() => {
+                    this.refs.input.value = this.props.value;
+                });
+            } else {
                 this.refs.input.value = this.props.value;
-            });
+            }
         }
     }
 
@@ -50,6 +64,8 @@ export default class QuickInput extends React.PureComponent {
 
     render() {
         const {value, inputComponent, ...props} = this.props;
+
+        Reflect.deleteProperty(props, 'delayInputUpdate');
 
         return React.createElement(
             inputComponent || 'input',
