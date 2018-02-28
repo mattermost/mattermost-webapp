ffa692a3638137129bf71e2c86faf2c01e813345
Asaad Mahmood <asaad@mattermost.com>
2018-02-28 19:31:19 +0500

    Updating header size

--- components/svg/mentions_icon.jsx
+++ components/svg/mentions_icon.jsx
@@ -8,8 +8,8 @@ export default class MentionsIcon extends React.PureComponent {
         return (
             <span {...this.props}>
                 <svg
-                    width='20px'
-                    height='20px'
+                    width='15px'
+                    height='15px'
                     viewBox='0 0 20 20'
                     version='1.1'
                 >
