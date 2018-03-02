28c384f2be982f9fddb0e1ba02bbc6a60218a8e0
Varun Pai <varun@nilenso.com>
2018-03-02 17:09:43 +0530

    Added Fira Sans, fix CSS around the font changes
        - Date is now left aligned, not bold and regular weight
        - Date size is smaller
        - Removed extra hover color that would appear on posts near dates
        - Fixed spacing of channel's intro content
        - Fixed spacing of reply box to make it less big
    
        - Added new flag filled icon
        - Added new font, fira sans
    
        - Fixed more icon breaking when Flagged posts ride sidebar is open
    
        - Added lock icon
        - Date indicator is now left aligned and the hr is below it
        - Fixed new message indicator
        - Fixed alignment of group messages
    
        - Fixed alignment issues in post header
        - Fixed css for date and new message
        - Not changing the user avatar icons
    
        - Reduced background color opacity for messages from current user
        - Reduced background color opacity for posts on hover

--- components/svg/lock_icon.jsx
+++ components/svg/lock_icon.jsx
@@ -7,7 +7,7 @@ export default class LockIcon extends React.PureComponent {
     render() {
         return (
             <span {...this.props}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z"/></svg>
+                <svg version="1.1" width="36" height="36"  viewBox="0 0 36 36" preserveAspectRatio="xMidYMid meet" xlinkHref="http://www.w3.org/2000/svg" xlinkHref="http://www.w3.org/1999/xlink"><title>lock-solid</title><path class="clr-i-solid clr-i-solid-path-1" d="M26,15V10.72a8.2,8.2,0,0,0-8-8.36,8.2,8.2,0,0,0-8,8.36V15H7V32a2,2,0,0,0,2,2H27a2,2,0,0,0,2-2V15ZM19,25.23V28H17V25.14a2.4,2.4,0,1,1,2,.09ZM24,15H12V10.72a6.2,6.2,0,0,1,6-6.36,6.2,6.2,0,0,1,6,6.36Z"></path><rect x="0" y="0" width="36" height="36" fill-opacity="0"/></svg>
             </span>
         );
     }
