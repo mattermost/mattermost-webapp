af3671bb4625d3c7de074678a7e2f602905027cd
Martin Kraft <mkraft@users.noreply.github.com>
2018-03-12 14:17:30 -0400

    MM-9755: Fix for bounding new channel modal. Fix for command+enter comment. (#921)

--- components/edit_channel_header_modal/edit_channel_header_modal.jsx
+++ components/edit_channel_header_modal/edit_channel_header_modal.jsx
@@ -117,14 +117,14 @@ class EditChannelHeaderModal extends React.PureComponent {
 
     handleKeyDown = (e) => {
         const {ctrlSend} = this.props;
-        if (ctrlSend && e.keyCode === KeyCodes.ENTER && Utils.cmdOrCtrlPressed(e)) {
+        if (ctrlSend && e.keyCode === KeyCodes.ENTER && e.ctrlKey === true) {
             this.handleKeyPress(e);
         }
     }
 
     handleKeyPress = (e) => {
         const {ctrlSend} = this.props;
-        if (!UserAgent.isMobile() && ((ctrlSend && Utils.cmdOrCtrlPressed(e)) || !ctrlSend)) {
+        if (!UserAgent.isMobile() && ((ctrlSend && e.ctrlKey) || !ctrlSend)) {
             if (e.which === KeyCodes.ENTER && !e.shiftKey && !e.altKey) {
                 e.preventDefault();
                 ReactDOM.findDOMNode(this.refs.editChannelHeaderTextbox).blur();
