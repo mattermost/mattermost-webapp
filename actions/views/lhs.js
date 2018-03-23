618c738a64d215e8ad8700a1db0a267a1a5f6df1
Jesse Hallam <jesse.hallam@gmail.com>
2018-03-23 11:29:31 -0400

    remove unused initTeamChangeActions in lhs actions

--- actions/views/lhs.js
+++ actions/views/lhs.js
@@ -1,14 +1,7 @@
 // Copyright (c) 2018-present Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
 
-import {fetchMyChannelsAndMembers} from 'mattermost-redux/actions/channels';
-
-import {loadProfilesForSidebar} from 'actions/user_actions.jsx';
-import {loadStatusesForChannelAndSidebar} from 'actions/status_actions.jsx';
-import store from 'stores/redux_store.jsx';
 import {ActionTypes} from 'utils/constants.jsx';
-
-const getState = store.getState;
 
 export const toggle = () => (dispatch) => dispatch({
     type: ActionTypes.TOGGLE_LHS,
@@ -21,9 +14,3 @@ export const open = () => (dispatch) => dispatch({
 export const close = () => (dispatch) => dispatch({
     type: ActionTypes.CLOSE_LHS,
 });
-
-export async function initTeamChangeActions(teamId) {
-    await fetchMyChannelsAndMembers(teamId)(store.dispatch, getState);
-    loadStatusesForChannelAndSidebar();
-    loadProfilesForSidebar();
-}
