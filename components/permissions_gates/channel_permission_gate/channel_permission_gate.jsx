99d96fb160fe96278c3b432464062a65bb8eade8
Jesús Espino <jespinog@gmail.com>
2018-03-26 23:28:46 +0200

    Removing pure components from gates

--- components/permissions_gates/channel_permission_gate/channel_permission_gate.jsx
+++ components/permissions_gates/channel_permission_gate/channel_permission_gate.jsx
@@ -4,7 +4,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-export default class ChannelPermissionGate extends React.PureComponent {
+export default class ChannelPermissionGate extends React.Component {
     static defaultProps = {
         invert: false,
     }
