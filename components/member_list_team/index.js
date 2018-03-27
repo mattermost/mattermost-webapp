a0be77c242ab31180f9e0c24af8ce9662c33f8e7
George Goldberg <george@gberg.me>
2018-03-27 22:12:54 +0100

    Fix missing import for TeamMembers list. (#1012)
    * Fix missing import for TeamMembers list.
    
    * Use a permission that exists.

--- components/member_list_team/index.js
+++ components/member_list_team/index.js
@@ -5,11 +5,12 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {getTeamStats} from 'mattermost-redux/actions/teams';
 import {haveITeamPermission} from 'mattermost-redux/selectors/entities/roles';
+import {Permissions} from 'mattermost-redux/constants';
 
 import MemberListTeam from './member_list_team.jsx';
 
 function mapStateToProps(state, ownProps) {
-    const canManageTeamMembers = haveITeamPermission(state, {team: ownProps.teamId, permission: Permissions.MANAGE_TEAM_MEMBERS});
+    const canManageTeamMembers = haveITeamPermission(state, {team: ownProps.teamId, permission: Permissions.MANAGE_TEAM_ROLES});
     return {
         canManageTeamMembers,
         ...ownProps,
