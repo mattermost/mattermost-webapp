03516c0b4bfbf6c68b4baf47de07d670a23423f0
George Goldberg <george@gberg.me>
2018-03-27 21:55:05 +0100

    Fix missing import for TeamMembers list.

--- components/member_list_team/index.js
+++ components/member_list_team/index.js
@@ -5,6 +5,7 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {getTeamStats} from 'mattermost-redux/actions/teams';
 import {haveITeamPermission} from 'mattermost-redux/selectors/entities/roles';
+import {Permissions} from 'mattermost-redux/constants';
 
 import MemberListTeam from './member_list_team.jsx';
 
