1c9d78a7bf70637f3371fa652e900fe0a3fa74d6
Jesse Hallam <jesse.hallam@gmail.com>
2018-04-06 11:00:07 -0400

    pass the download link to the PopoverBar to "Download"
    Ironically, my earlier attempt to fix this somehow avoided fixing the
    very report of the issue, instead only fixing clicking on the image to
    download.

--- components/view_image/view_image.jsx
+++ components/view_image/view_image.jsx
@@ -4,7 +4,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import {Modal} from 'react-bootstrap';
-import {getFilePreviewUrl, getFileUrl} from 'mattermost-redux/utils/file_utils';
+import {getFilePreviewUrl, getFileUrl, getFileDownloadUrl} from 'mattermost-redux/utils/file_utils';
 
 import * as GlobalActions from 'actions/global_actions';
 import Constants, {FileTypes} from 'utils/constants';
@@ -205,6 +205,7 @@ export default class ViewImageModal extends React.PureComponent {
         const showPublicLink = !fileInfo.link;
         const fileName = fileInfo.link || fileInfo.name;
         const fileUrl = fileInfo.link || getFileUrl(fileInfo.id);
+        const fileDownloadUrl = fileInfo.link || getFileDownloadUrl(fileInfo.id);
 
         let content;
         if (this.state.loaded[this.state.imageIndex]) {
@@ -326,7 +327,7 @@ export default class ViewImageModal extends React.PureComponent {
                                 fileIndex={this.state.imageIndex}
                                 totalFiles={this.props.fileInfos.length}
                                 filename={fileName}
-                                fileURL={fileUrl}
+                                fileURL={fileDownloadUrl}
                                 enablePublicLink={this.props.enablePublicLink}
                                 canDownloadFiles={this.props.canDownloadFiles}
                                 onGetPublicLink={this.handleGetPublicLink}
