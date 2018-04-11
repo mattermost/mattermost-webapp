1bd7d66ca4a076c92fc40dbfe9ade1bdb3679e1b
Asaad Mahmood <asaad@mattermost.com>
2018-04-11 23:30:03 +0500

    MM-10106 - Extra line in user popover if no email

--- components/profile_popover/profile_popover.jsx
+++ components/profile_popover/profile_popover.jsx
@@ -283,13 +283,6 @@ class ProfilePopover extends React.Component {
             );
         }
 
-        dataContent.push(
-            <hr
-                key='user-popover-hr'
-                className='divider divider--expanded'
-            />
-        );
-
         if (this.props.user.position) {
             const position = this.props.user.position.substring(0, Constants.MAX_POSITION_LENGTH);
             dataContent.push(
@@ -311,17 +304,24 @@ class ProfilePopover extends React.Component {
         const email = this.props.user.email;
         if (email) {
             dataContent.push(
-                <div
-                    data-toggle='tooltip'
-                    title={email}
-                    key='user-popover-email'
-                >
-                    <a
-                        href={'mailto:' + email}
-                        className='text-nowrap text-lowercase user-popover__email padding-bottom half'
+                <div>
+                    <hr
+                        key='user-popover-hr'
+                        className='divider divider--expanded'
+                    />
+
+                    <div
+                        data-toggle='tooltip'
+                        title={email}
+                        key='user-popover-email'
                     >
-                        {email}
-                    </a>
+                        <a
+                            href={'mailto:' + email}
+                            className='text-nowrap text-lowercase user-popover__email padding-bottom half'
+                        >
+                            {email}
+                        </a>
+                    </div>
                 </div>
             );
         }
