a3f18a0e589a5b648e0f1665986f3a74e85fe283
Harrison Healey <harrisonmhealey@gmail.com>
2018-04-27 09:16:56 -0400

    Removed accidentally committed file

--- components/admin_console/disabled_by_env.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function() {
-    return <p style={{color: 'red'}}>{'This setting has been set through an environment variable and cannot be changed.'}</p>;
-}
