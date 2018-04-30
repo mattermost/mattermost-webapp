c0ac0b8b92d9c8e637636ed0900b00fbff3d546c
Harrison Healey <harrisonmhealey@gmail.com>
2018-04-30 11:20:37 -0400

    Updated 'set by environment variable' help text

--- components/admin_console/set_by_env.jsx
+++ components/admin_console/set_by_env.jsx
@@ -1,5 +1,16 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See License.txt for license information.
+
 import React from 'react';
+import {FormattedMessage} from 'react-intl';
 
 export default function() {
-    return <p style={{color: 'red'}}>{'This setting has been set through an environment variable and cannot be changed.'}</p>;
+    return (
+        <div className='alert alert-warning'>
+            <FormattedMessage
+                id='admin.set_by_env'
+                defaultMessage='This setting has been set through an environment variable and cannot be changed. It cannot be changed through the System Console.'
+            />
+        </div>
+    );
 }
