e76203baa0d85d772aeb47e276c249049d686347
Jesse Hallam <jesse.hallam@gmail.com>
2018-04-30 13:31:29 -0400

    add eslint-plugin-header, and --fix all (#1140)
    * add eslint-plugin-header, and --fix all
    
    While the checking feature of this plugin seems to work great, its --fix
    will sometimes removing leading comments when the license header doesn't
    match. I've hand-edited the over-zealous removals, so this won't be an
    issue going forward except for new files with missing headers but
    leading comments.
    
    * fixes from latest master changes
    
    * latest changes from master

--- utils/latinise.jsx
+++ utils/latinise.jsx
@@ -1,6 +1,5 @@
-// Copyright (c) 2017-present Mattermost, Inc. All Rights Reserved.
-// See License.txt for license information.
-// Credit to http://semplicewebsites.com/removing-accents-javascript
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
 
 var latinMap = {
     Á: 'A', // LATIN CAPITAL LETTER A WITH ACUTE
