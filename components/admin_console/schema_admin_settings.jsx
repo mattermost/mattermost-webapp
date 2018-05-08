adde61966ab47a45aa48a285ca92c22a347a97b1
Joram Wilander <jwawilander@gmail.com>
2018-05-08 09:29:18 -0400

    MM-10492 Fix saving plugin settings in the system console (#1186)
    * Fix saving plugin settings in the system console
    
    * Minor fix
    
    * Fix render plugins panels on refresh
    
    * Only update schema once when it first loads

--- components/admin_console/schema_admin_settings.jsx
+++ components/admin_console/schema_admin_settings.jsx
@@ -458,7 +458,12 @@ export default class SchemaAdminSettings extends AdminSettings {
         if (schema.settings) {
             schema.settings.forEach((setting) => {
                 if (this.buildSettingFunctions[setting.type] && !this.isHidden(setting)) {
-                    settingsList.push(this.buildSettingFunctions[setting.type](setting));
+                    // This is a hack required as plugin settings are case insensitive
+                    let s = setting;
+                    if (this.isPlugin) {
+                        s = {...setting, key: setting.key.toLowerCase()};
+                    }
+                    settingsList.push(this.buildSettingFunctions[setting.type](s));
                 }
             });
         }
@@ -495,7 +500,7 @@ export default class SchemaAdminSettings extends AdminSettings {
     render = () => {
         const schema = this.props.schema;
 
-        if (schema.component) {
+        if (schema && schema.component) {
             const CustomComponent = schema.component;
             return (<CustomComponent {...this.props}/>);
         }
