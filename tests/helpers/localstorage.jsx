0395d5b54272e49135423c3b4ed06bbd320f076b
JoramWilander <jwawilander@gmail.com>
2018-05-09 18:00:47 -0400

    Add credit for local storage mock

--- tests/helpers/localstorage.jsx
+++ tests/helpers/localstorage.jsx
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+// Based on https://stackoverflow.com/a/41434763
 class LocalStorageMock {
     constructor() {
         this.store = {};
