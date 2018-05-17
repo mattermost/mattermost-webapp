40f1e9caffc0ce59145a7839e7a409ffa1088174
Harrison Healey <harrisonmhealey@gmail.com>
2018-05-17 12:39:19 -0400

    MM-10617 Fixed checking if a link is an image (#1225)

--- components/post_view/post_body_additional_content/post_body_additional_content.jsx
+++ components/post_view/post_body_additional_content/post_body_additional_content.jsx
@@ -9,6 +9,7 @@ import * as Utils from 'utils/utils.jsx';
 import {StoragePrefixes} from 'utils/constants.jsx';
 import YoutubeVideo from 'components/youtube_video';
 import ViewImageModal from 'components/view_image';
+import Constants from 'utils/constants';
 import * as PostUtils from 'utils/post_utils.jsx';
 
 import PostAttachmentList from '../post_attachment_list.jsx';
@@ -131,10 +132,17 @@ export default class PostBodyAdditionalContent extends React.PureComponent {
     }
 
     isLinkImage(link) {
-        const regex = /.+\/(.+\.(?:jpg|gif|bmp|png|jpeg))(?:\?.*)?$/i;
-        const match = link.match(regex);
-        if (match && match[1]) {
-            return true;
+        let linkWithoutQuery = link;
+        if (link.indexOf('?') !== -1) {
+            linkWithoutQuery = linkWithoutQuery.split('?')[0];
+        }
+
+        for (let i = 0; i < Constants.IMAGE_TYPES.length; i++) {
+            const imageType = Constants.IMAGE_TYPES[i];
+
+            if (linkWithoutQuery.endsWith('.' + imageType) || linkWithoutQuery.endsWith('=' + imageType)) {
+                return true;
+            }
         }
 
         return false;
