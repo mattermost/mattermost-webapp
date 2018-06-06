23e908528fda458416799402613ec2f7b8dbc4ed
Saturnino Abril <saturnino.abril@gmail.com>
2018-06-07 02:55:53 +0800

    add e2e for account settings display

--- tests/e2e/tests/3_sidebarLeft/accountSettingsDisplay.action.js
+++ tests/e2e/tests/3_sidebarLeft/accountSettingsDisplay.action.js
@@ -67,6 +67,62 @@ module.exports = {
             click('#saveSetting').
             assert.containsText('@clockDesc', '12-hour clock (example: 4:00 PM)');
     },
+    'Test account settings display - Teammate Name Display': (client) => {
+        const accountSettingsModalPage = client.page.accountSettingsModalPage();
+        accountSettingsModalPage.expect.section('@accountSettingsModal').to.be.visible;
+
+        const accountSettingsModalSection = accountSettingsModalPage.section.accountSettingsModal;
+
+        accountSettingsModalSection.click('@displayButton');
+        accountSettingsModalSection.expect.section('@displaySettings').to.be.visible;
+
+        const displaySettingsSection = accountSettingsModalSection.section.displaySettings;
+
+        // contents and cancel check
+        displaySettingsSection.
+            waitForElementVisible('@teammateNameDisplayEdit', Constants.DEFAULT_WAIT).
+            click('@teammateNameDisplayEdit').
+            assert.containsText('.setting-list-item', 'Show username').
+            assert.containsText('.setting-list-item', 'Show nickname if one exists, otherwise show first and last name').
+            assert.containsText('.setting-list-item', 'Show first and last name').
+            assert.containsText('.setting-list-item', 'Show username').
+            waitForElementVisible('#name_formatFormatA', Constants.DEFAULT_WAIT).
+            waitForElementVisible('#name_formatFormatB', Constants.DEFAULT_WAIT).
+            waitForElementVisible('#name_formatFormatC', Constants.DEFAULT_WAIT).
+            waitForElementVisible('#saveSetting', Constants.DEFAULT_WAIT).
+            assert.containsText('#saveSetting', 'Save').
+            waitForElementVisible('#cancelSetting', Constants.DEFAULT_WAIT).
+            assert.containsText('#cancelSetting', 'Cancel').
+            click('#cancelSetting').
+            assert.containsText('@teammateNameDisplayDesc', 'Show username');
+
+        // save/change setting to "Show nickname if one exists, otherwise show first and last name"
+        displaySettingsSection.
+            waitForElementVisible('@teammateNameDisplayEdit', Constants.DEFAULT_WAIT).
+            click('@teammateNameDisplayEdit').
+            waitForElementVisible('#name_formatFormatB', Constants.DEFAULT_WAIT).
+            click('#name_formatFormatB').
+            click('#saveSetting').
+            assert.containsText('@teammateNameDisplayDesc', 'Show nickname if one exists, otherwise show first and last name');
+
+        // save/change setting to "Show first and last name"
+        displaySettingsSection.
+            waitForElementVisible('@teammateNameDisplayEdit', Constants.DEFAULT_WAIT).
+            click('@teammateNameDisplayEdit').
+            waitForElementVisible('#name_formatFormatC', Constants.DEFAULT_WAIT).
+            click('#name_formatFormatC').
+            click('#saveSetting').
+            assert.containsText('@teammateNameDisplayDesc', 'Show first and last name');
+
+        // save/change setting to "Show username"
+        displaySettingsSection.
+            waitForElementVisible('@teammateNameDisplayEdit', Constants.DEFAULT_WAIT).
+            click('@teammateNameDisplayEdit').
+            waitForElementVisible('#name_formatFormatA', Constants.DEFAULT_WAIT).
+            click('#name_formatFormatA').
+            click('#saveSetting').
+            assert.containsText('@teammateNameDisplayDesc', 'Show username');
+    },
     'Test account settings display - Website Link Previews': (client) => {
         const accountSettingsModalPage = client.page.accountSettingsModalPage();
         accountSettingsModalPage.expect.section('@accountSettingsModal').to.be.visible;
@@ -112,7 +168,7 @@ module.exports = {
             click('#saveSetting').
             assert.containsText('@linkPreviewDesc', 'On');
     },
-    'Test account settings display - Default appearance of image link previews': (client) => {
+    'Test account settings display - Default appearance of image previews': (client) => {
         const accountSettingsModalPage = client.page.accountSettingsModalPage();
         accountSettingsModalPage.expect.section('@accountSettingsModal').to.be.visible;
 
@@ -129,7 +185,7 @@ module.exports = {
             click('@collapseEdit').
             assert.containsText('.setting-list-item', 'Expanded').
             assert.containsText('.setting-list-item', 'Collapsed').
-            assert.containsText('.setting-list-item', 'Set whether previews of image links show as expanded or collapsed by default. This setting can also be controlled using the slash commands /expand and /collapse.').
+            assert.containsText('.setting-list-item', 'Set whether previews of image links and image attachment thumbnails show as expanded or collapsed by default. This setting can also be controlled using the slash commands /expand and /collapse.').
             waitForElementVisible('#collapseFormatA', Constants.DEFAULT_WAIT).
             waitForElementVisible('#collapseFormatB', Constants.DEFAULT_WAIT).
             waitForElementVisible('#saveSetting', Constants.DEFAULT_WAIT).
@@ -156,5 +212,95 @@ module.exports = {
             click('#collapseFormatA').
             click('#saveSetting').
             assert.containsText('@collapseDesc', 'Expanded');
+    },
+    'Test account settings display - Message Display': (client) => {
+        const accountSettingsModalPage = client.page.accountSettingsModalPage();
+        accountSettingsModalPage.expect.section('@accountSettingsModal').to.be.visible;
+
+        const accountSettingsModalSection = accountSettingsModalPage.section.accountSettingsModal;
+
+        accountSettingsModalSection.click('@displayButton');
+        accountSettingsModalSection.expect.section('@displaySettings').to.be.visible;
+
+        const displaySettingsSection = accountSettingsModalSection.section.displaySettings;
+
+        // contents and cancel check
+        displaySettingsSection.
+            waitForElementVisible('@messageDisplayEdit', Constants.DEFAULT_WAIT).
+            click('@messageDisplayEdit').
+            assert.containsText('.setting-list-item', 'Standard: Easy to scan and read.').
+            assert.containsText('.setting-list-item', 'Compact: Fit as many messages on the screen as we can.').
+            assert.containsText('.setting-list-item', 'Select how messages in a channel should be displayed.').
+            waitForElementVisible('#message_displayFormatA', Constants.DEFAULT_WAIT).
+            waitForElementVisible('#message_displayFormatB', Constants.DEFAULT_WAIT).
+            waitForElementVisible('#saveSetting', Constants.DEFAULT_WAIT).
+            assert.containsText('#saveSetting', 'Save').
+            waitForElementVisible('#cancelSetting', Constants.DEFAULT_WAIT).
+            assert.containsText('#cancelSetting', 'Cancel').
+            click('#cancelSetting').
+            assert.containsText('@messageDisplayDesc', 'Standard');
+
+        // save/change setting to "Compact: Fit as many messages on the screen as we can."
+        displaySettingsSection.
+            waitForElementVisible('@messageDisplayEdit', Constants.DEFAULT_WAIT).
+            click('@messageDisplayEdit').
+            waitForElementVisible('#message_displayFormatB', Constants.DEFAULT_WAIT).
+            click('#message_displayFormatB').
+            click('#saveSetting').
+            assert.containsText('@messageDisplayDesc', 'Compact');
+
+        // save/change setting to "Standard: Easy to scan and read."
+        displaySettingsSection.
+            waitForElementVisible('@messageDisplayEdit', Constants.DEFAULT_WAIT).
+            click('@messageDisplayEdit').
+            waitForElementVisible('#message_displayFormatA', Constants.DEFAULT_WAIT).
+            click('#message_displayFormatA').
+            click('#saveSetting').
+            assert.containsText('@messageDisplayDesc', 'Standard');
+    },
+    'Test account settings display - Channel Display Mode': (client) => {
+        const accountSettingsModalPage = client.page.accountSettingsModalPage();
+        accountSettingsModalPage.expect.section('@accountSettingsModal').to.be.visible;
+
+        const accountSettingsModalSection = accountSettingsModalPage.section.accountSettingsModal;
+
+        accountSettingsModalSection.click('@displayButton');
+        accountSettingsModalSection.expect.section('@displaySettings').to.be.visible;
+
+        const displaySettingsSection = accountSettingsModalSection.section.displaySettings;
+
+        // contents and cancel check
+        displaySettingsSection.
+            waitForElementVisible('@channelDisplayModeEdit', Constants.DEFAULT_WAIT).
+            click('@channelDisplayModeEdit').
+            assert.containsText('.setting-list-item', 'Full width').
+            assert.containsText('.setting-list-item', 'Fixed width, centered').
+            assert.containsText('.setting-list-item', 'Select the width of the center channel.').
+            waitForElementVisible('#channel_display_modeFormatA', Constants.DEFAULT_WAIT).
+            waitForElementVisible('#channel_display_modeFormatB', Constants.DEFAULT_WAIT).
+            waitForElementVisible('#saveSetting', Constants.DEFAULT_WAIT).
+            assert.containsText('#saveSetting', 'Save').
+            waitForElementVisible('#cancelSetting', Constants.DEFAULT_WAIT).
+            assert.containsText('#cancelSetting', 'Cancel').
+            click('#cancelSetting').
+            assert.containsText('@channelDisplayModeDesc', 'Full width');
+
+        // save/change setting to "Fixed width, centered"
+        displaySettingsSection.
+            waitForElementVisible('@channelDisplayModeEdit', Constants.DEFAULT_WAIT).
+            click('@channelDisplayModeEdit').
+            waitForElementVisible('#channel_display_modeFormatB', Constants.DEFAULT_WAIT).
+            click('#channel_display_modeFormatB').
+            click('#saveSetting').
+            assert.containsText('@channelDisplayModeDesc', 'Fixed width, centered');
+
+        // save/change setting to "Full width"
+        displaySettingsSection.
+            waitForElementVisible('@channelDisplayModeEdit', Constants.DEFAULT_WAIT).
+            click('@channelDisplayModeEdit').
+            waitForElementVisible('#channel_display_modeFormatA', Constants.DEFAULT_WAIT).
+            click('#channel_display_modeFormatA').
+            click('#saveSetting').
+            assert.containsText('@channelDisplayModeDesc', 'Full width');
     },
 };
