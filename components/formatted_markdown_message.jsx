80978dc8df4a6d8de8046ee7608207a094b4a16c
Martin Kraft <martinkraft@gmail.com>
2018-06-08 16:50:16 -0400

    Removes <br> support. Adds newline character support.

--- components/formatted_markdown_message.jsx
+++ components/formatted_markdown_message.jsx
@@ -6,10 +6,9 @@ import {injectIntl, intlShape} from 'react-intl';
 import PropTypes from 'prop-types';
 import marked from 'marked';
 
-const UNESCAPED_TAGS = ['BR'];
 const TARGET_BLANK_URL_PREFIX = '!';
 
-class MarkdownRenderer extends marked.Renderer {
+class CustomRenderer extends marked.Renderer {
     link(href, title, text) {
         if (href[0] === TARGET_BLANK_URL_PREFIX) {
             return `<a href="${href.substring(1, href.length)}" target="_blank">${text}</a>`;
@@ -28,25 +27,6 @@ class FormattedMarkdownMessage extends React.PureComponent {
         };
     }
 
-    static escapeHtml(str) {
-        var div = document.createElement('div');
-        div.appendChild(document.createTextNode(str));
-        return div.innerHTML;
-    }
-
-    static sanitizer(p1) {
-        const parser = new DOMParser();
-        const htmlDoc = parser.parseFromString(p1, 'text/html');
-        const childNodes = htmlDoc.getElementsByTagName('body')[0].childNodes;
-        if (childNodes.length > 0) {
-            const {tagName} = childNodes[0];
-            if (UNESCAPED_TAGS.includes(tagName)) {
-                return p1;
-            }
-        }
-        return FormattedMarkdownMessage.escapeHtml(p1);
-    }
-
     render() {
         const origMsg = this.props.intl.formatMessage({
             id: this.props.id,
@@ -54,9 +34,9 @@ class FormattedMarkdownMessage extends React.PureComponent {
         }, this.props.values);
 
         const markedUpMessage = marked(origMsg, {
+            breaks: true,
             sanitize: true,
-            sanitizer: FormattedMarkdownMessage.sanitizer,
-            renderer: new MarkdownRenderer(),
+            renderer: new CustomRenderer(),
         });
 
         return (<span dangerouslySetInnerHTML={{__html: markedUpMessage}}/>);
