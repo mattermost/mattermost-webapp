271e0240636c447e55792c5091e891c8cd42c47e
Jesse Hallam <jesse.hallam@gmail.com>
2018-06-13 17:11:30 -0400

    test for channel references in various system messages

--- tests/components/post_markdown.test.jsx
+++ tests/components/post_markdown.test.jsx
@@ -3,6 +3,7 @@
 
 import React from 'react';
 import {shallow} from 'enzyme';
+import {Posts} from 'mattermost-redux/constants';
 
 import PostMarkdown from 'components/post_markdown/post_markdown';
 
@@ -24,8 +25,55 @@ describe('components/PostMarkdown', () => {
     test('should render properly with a post', () => {
         const props = {
             ...baseProps,
+            message: 'See ~test',
             post: {
                 props: {
+                    channel_mentions: {
+                        test: {
+                            display_name: 'Test',
+                        },
+                    },
+                },
+            },
+        };
+        const wrapper = shallow(
+            <PostMarkdown {...props}/>
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should render header change properly', () => {
+        const props = {
+            ...baseProps,
+            post: {
+                type: Posts.POST_TYPES.HEADER_CHANGE,
+                props: {
+                    username: 'user',
+                    old_header: 'see ~test',
+                    new_header: 'now ~test',
+                    channel_mentions: {
+                        test: {
+                            display_name: 'Test',
+                        },
+                    },
+                },
+            },
+        };
+        const wrapper = shallow(
+            <PostMarkdown {...props}/>
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should render purpose change properly', () => {
+        const props = {
+            ...baseProps,
+            post: {
+                type: Posts.POST_TYPES.PURPOSE_CHANGE,
+                props: {
+                    username: 'user',
+                    old_purpose: 'see ~test',
+                    new_purpose: 'now ~test',
                     channel_mentions: {
                         test: {
                             display_name: 'Test',
