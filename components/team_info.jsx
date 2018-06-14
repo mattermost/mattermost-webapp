3dca1603f04d36fa0c534ee7a9f70745550bc13f
Jesús Espino <jespinog@gmail.com>
2018-06-14 17:11:52 +0200

    Remove number of members on teams to avoid a lot of requests

--- components/team_info.jsx
+++ components/team_info.jsx
@@ -3,29 +3,16 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import {FormattedMessage} from 'react-intl';
 
 import {imageURLForTeam} from 'utils/utils.jsx';
 
 export default class TeamInList extends React.Component {
     static propTypes = {
         team: PropTypes.object.isRequired,
-        stats: PropTypes.object,
-        actions: PropTypes.shape({
-            loadTeamStats: PropTypes.func.isRequired,
-        }).isRequired,
-    }
-
-    componentDidMount() {
-        this.props.actions.loadTeamStats(this.props.team.id);
     }
 
     render() {
-        const {team, stats} = this.props;
-        let totalMembers = 0;
-        if (stats && stats[team.id]) {
-            totalMembers = stats[team.id].total_member_count;
-        }
+        const {team} = this.props;
         const teamIconUrl = imageURLForTeam(team);
         let icon = null;
         if (teamIconUrl) {
@@ -47,13 +34,6 @@ export default class TeamInList extends React.Component {
                 <span className='icon'>{icon}</span>
                 <div className='team-data'>
                     <div className='title'>{team.display_name}</div>
-                    <div className='members'>
-                        <FormattedMessage
-                            id='admin.team_info.numMembers'
-                            defaultMessage='{count} {count, plural, one {Member} other {Members}}'
-                            values={{count: totalMembers}}
-                        />
-                    </div>
                 </div>
             </div>
         );
