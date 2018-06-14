0818662617e4ab88e2bff960bf111d48ee1954aa
Harrison Healey <harrisonmhealey@gmail.com>
2018-06-14 13:28:44 -0400

    MM-10888 Hide parts of combined system messages that don't affect the current user (#1332)
    * MM-10888 Hide parts of combined system messages that don't affect the current user
    
    * Switch redux to release-5.0 branch

--- tests/components/combined_system_message/combined_system_message.test.jsx
+++ tests/components/combined_system_message/combined_system_message.test.jsx
@@ -12,22 +12,69 @@ import CombinedSystemMessage from 'components/post_view/combined_system_message/
 describe('components/post_view/CombinedSystemMessage', () => {
     function emptyFunc() {} // eslint-disable-line no-empty-function
     const baseProps = {
-        teammateNameDisplay: General.TEAMMATE_NAME_DISPLAY.SHOW_USERNAME,
         currentUserId: 'current_user_id',
         currentUsername: 'current_username',
-        allUserIds: ['added_user_id_1', 'user_id_1'],
+        allUserIds: ['added_user_id_1', 'added_user_id_2', 'current_user_id', 'user_id_1'],
         allUsernames: [],
-        messageData: [{actorId: 'user_id_1', postType: Posts.POST_TYPES.ADD_TO_CHANNEL, userIds: ['added_user_id_1']}],
+        messageData: [{
+            actorId: 'user_id_1',
+            postType: Posts.POST_TYPES.ADD_TO_CHANNEL,
+            userIds: ['added_user_id_1'],
+        }, {
+            actorId: 'user_id_1',
+            postType: Posts.POST_TYPES.ADD_TO_CHANNEL,
+            userIds: ['current_user_id'],
+        }, {
+            actorId: 'current_user_id',
+            postType: Posts.POST_TYPES.ADD_TO_CHANNEL,
+            userIds: ['added_user_id_2'],
+        }],
+        showJoinLeave: true,
+        teammateNameDisplay: General.TEAMMATE_NAME_DISPLAY.SHOW_USERNAME,
         actions: {
             getProfilesByIds: emptyFunc,
             getProfilesByUsernames: emptyFunc,
         },
     };
 
+    const userProfiles = [{
+        id: 'added_user_id_1',
+        username: 'AddedUser1',
+    },
+    {
+        id: 'added_user_id_2',
+        username: 'AddedUser2',
+    },
+    {
+        id: 'current_user',
+        username: 'CurrentUser',
+    },
+    {
+        id: 'user_id_1',
+        username: 'User1',
+    }];
+
     test('should match snapshot', () => {
         const wrapper = shallowWithIntl(
             <CombinedSystemMessage {...baseProps}/>
         );
+
+        // Fake this state change since getProfilesByIds is called asynchronously
+        wrapper.setState({userProfiles});
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should match snapshot when join leave messages are turned off', () => {
+        const wrapper = shallowWithIntl(
+            <CombinedSystemMessage
+                {...baseProps}
+                showJoinLeave={false}
+            />
+        );
+
+        // Fake this state change since getProfilesByIds is called asynchronously
+        wrapper.setState({userProfiles});
 
         expect(wrapper).toMatchSnapshot();
     });
