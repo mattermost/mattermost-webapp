3e5f61de521b46afb9dbdb8c1606cd4dc9b41f41
sudheer <sudheer.105@gmail.com>
2018-06-22 16:05:29 +0530

    MM-9479 Add ability to join team and channel through url
     * If the url belongs to an unjoined team try to join and
       before redirecting to default team
     * Add tests to check join logic

--- components/needs_team/index.js
+++ components/needs_team/index.js
@@ -3,12 +3,17 @@
 
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {fetchMyChannelsAndMembers, markChannelAsRead, viewChannel} from 'mattermost-redux/actions/channels';
-import {getMyTeamUnreads} from 'mattermost-redux/actions/teams';
-import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
 import {withRouter} from 'react-router-dom';
-import {getLicense, getConfig} from 'mattermost-redux/selectors/entities/general';
 
+import {fetchMyChannelsAndMembers, markChannelAsRead, viewChannel} from 'mattermost-redux/actions/channels';
+import {getMyTeamUnreads, getTeams, joinTeam, selectTeam} from 'mattermost-redux/actions/teams';
+import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
+import {getLicense, getConfig} from 'mattermost-redux/selectors/entities/general';
+import {getCurrentUser} from 'mattermost-redux/selectors/entities/users';
+import {getCurrentTeamId, getTeamsList} from 'mattermost-redux/selectors/entities/teams';
+import {getCurrentChannelId} from 'mattermost-redux/selectors/entities/channels';
+
+import {setGlobalItem} from 'actions/storage';
 import {checkIfMFARequired} from 'utils/route';
 
 import NeedsTeam from './needs_team.jsx';
@@ -20,6 +25,10 @@ function mapStateToProps(state, ownProps) {
     return {
         theme: getTheme(state),
         mfaRequired: checkIfMFARequired(license, config, ownProps.match.url),
+        currentUser: getCurrentUser(state),
+        currentTeamId: getCurrentTeamId(state),
+        teamsList: getTeamsList(state),
+        currentChannelId: getCurrentChannelId(state),
     };
 }
 
@@ -30,6 +39,10 @@ function mapDispatchToProps(dispatch) {
             getMyTeamUnreads,
             viewChannel,
             markChannelAsRead,
+            getTeams,
+            joinTeam,
+            setGlobalItem,
+            selectTeam,
         }, dispatch),
     };
 }
