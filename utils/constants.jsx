57c94d16e9ec11efef58b3135088be4ce7f40952
Miguel Espinoza <espinozamiguel349@gmail.com>
2018-06-26 15:43:53 -0400

    Clean up code

--- utils/constants.jsx
+++ utils/constants.jsx
@@ -508,15 +508,6 @@ export const RHSStates = {
     PIN: 'pin',
 };
 
-export const SidebarOptions = {
-    BY_TYPE: 'by_type',
-    NONE: 'none',
-    FAVORITE: 'favorite',
-    UNREADS: 'unreads',
-    RECENT: 'recent',
-    ALPHABETICAL: 'alpha',
-};
-
 export const UploadStatuses = {
     LOADING: 'loading',
     COMPLETE: 'complete',
