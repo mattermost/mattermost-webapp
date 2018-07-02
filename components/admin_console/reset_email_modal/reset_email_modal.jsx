c1a51a92b9f53081277d0cdd9b6cefdf33c49da5
Ryan Wang <R-Wang97@users.noreply.github.com>
2018-07-02 09:49:26 -0700

    [MM-10974] Remove Character Limit in Reset Email Modal (#1359)
    * Remove character limit in email reset modal
    
    * Set limit to 128 characters

--- components/admin_console/reset_email_modal/reset_email_modal.jsx
+++ components/admin_console/reset_email_modal/reset_email_modal.jsx
@@ -121,7 +121,7 @@ export default class ResetEmailModal extends React.Component {
                                         type='email'
                                         ref='email'
                                         className='form-control'
-                                        maxLength='22'
+                                        maxLength='128'
                                         autoFocus={true}
                                         tabIndex='1'
                                     />
