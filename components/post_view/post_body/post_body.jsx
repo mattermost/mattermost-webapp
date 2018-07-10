cc0074d09c53b430b0e5a1d137fee72f2eed5cf8
Kaarlo Kekkonen <kaarlo.kekkonen@elisa.fi>
2018-07-10 10:31:36 +0300

    rethreading actions moved to separate file, fixed style

--- components/post_view/post_body/post_body.jsx
+++ components/post_view/post_body/post_body.jsx
@@ -92,6 +92,11 @@ export default class PostBody extends React.PureComponent {
          * Set not to allow edits on post
          */
         isReadOnly: PropTypes.bool,
+
+        /**
+         * Set to mark the post as selected for rethreding
+         */
+        selected: PropTypes.bool,
     }
 
     static defaultProps = {
@@ -273,9 +278,9 @@ export default class PostBody extends React.PureComponent {
         if (isEphemeral) {
             ephemeralPostClass = 'post--ephemeral';
         }
-        
+    
         let selected = '';
-        if(this.props.selected) {
+        if (this.props.selected) {
             selected = 'post-rethreadtarget';
         }
 
