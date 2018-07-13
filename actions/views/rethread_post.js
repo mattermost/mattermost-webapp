94a263e62bd77a9e5461ba2a87a20f63b3328c19
Kaarlo Kekkonen <kaarlo.kekkonen@elisa.fi>
2018-07-13 12:30:18 +0300

    webapp now uses modified mattermost-redux

--- actions/views/rethread_post.js
+++ actions/views/rethread_post.js
@@ -1,26 +1,13 @@
 // Copyright (c) 2017 Mattermost, Inc. All Rights Reserved.
 // See License.txt for license information.
-
-import {Client4} from 'mattermost-redux/client';
-import {bindClientFunc} from 'mattermost-redux/actions/helpers';
-import {PostTypes} from 'mattermost-redux/action_types';
+import {rethreadPost as rethreadPostRedux} from 'mattermost-redux/actions/posts';
 
 import AppDispatcher from 'dispatcher/app_dispatcher.jsx';
 import {ActionTypes} from 'utils/constants.jsx';
 
-function rethreadPostHelper(post) {
-    return bindClientFunc(
-        Client4.updatePost,
-        PostTypes.EDIT_POST_REQUEST,
-        [PostTypes.RECEIVED_POST, PostTypes.EDIT_POST_SUCCESS],
-        PostTypes.EDIT_POST_FAILURE,
-        post
-    );
-}
-
 export function rethreadPost(post) {
     return async (dispatch, getState) => {
-        const result = await rethreadPostHelper(post)(dispatch, getState);
+        const result = await rethreadPostRedux(post)(dispatch, getState);
 
         if (result.error) {
             AppDispatcher.handleServerAction({
