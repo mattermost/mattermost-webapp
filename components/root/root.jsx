ee59addc6a3b85cbb39b9fc445497b2edd18e0b1
George Goldberg <george@gberg.me>
2018-07-19 14:00:27 +0100

    MM-10999: Fix server ID setting race condition in diagnostics.

--- components/root/root.jsx
+++ components/root/root.jsx
@@ -86,6 +86,7 @@ const LoggedInRoute = ({component: Component, ...rest}) => (
 export default class Root extends React.Component {
     static propTypes = {
         diagnosticsEnabled: PropTypes.bool,
+        diagnosticId: PropTypes.string,
         noAccounts: PropTypes.bool,
         children: PropTypes.object,
     }
@@ -148,11 +149,26 @@ export default class Root extends React.Component {
 
     onConfigLoaded = () => {
         const segmentKey = Constants.DIAGNOSTICS_SEGMENT_KEY;
+        const diagnosticId = this.props.diagnosticId;
 
         /*eslint-disable */
         if (segmentKey != null && segmentKey !== '' && this.props.diagnosticsEnabled) {
             !function(){var analytics=global.window.analytics=global.window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.0.1";
                 analytics.load(segmentKey);
+
+                analytics.identify(diagnosticId, {}, {
+                    context: {
+                        ip: '0.0.0.0',
+                    },
+                    page: {
+                        path: '',
+                        referrer: '',
+                        search: '',
+                        title: '',
+                        url: '',
+                    },
+                    anonymousId: '00000000000000000000000000',
+                });
 
                 analytics.page('ApplicationLoaded', {
                         path: '',
