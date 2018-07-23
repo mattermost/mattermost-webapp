bff87ad80378e5016294c28a56b38f96edacc2b6
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2018-07-23 19:27:40 +0500

    MM-11377 - Making unread indicators position dynamic (#1468)
    * MM-11377 - Making unread indicators position dynamic
    
    * Updating snapshot
    
    * Reverting package-lock.json
    
    * Reverting package-lock.json
    
    * Reverted package-lock.json

--- tests/components/sidebar/sidebar.test.jsx
+++ tests/components/sidebar/sidebar.test.jsx
@@ -475,16 +475,4 @@ describe('component/sidebar/sidebar_channel/SidebarChannel', () => {
         instance.componentWillUnmount();
         expect(document.removeEventListener).toHaveBeenCalledTimes(2);
     });
-
-    test('should match snapshot, on sidebar with plugin components', () => {
-        const wrapper = shallow(
-            <Sidebar
-                {...{
-                    ...defaultProps,
-                    pluginComponents: [<div key='plugin_one'>{'plugin_one'}</div>],
-                }}
-            />
-        );
-        expect(wrapper).toMatchSnapshot();
-    });
 });
