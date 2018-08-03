ec6beca17b0721d7ba00aa4a3c163d27ae1704d4
Jesse Hallam <jesse.hallam@gmail.com>
2018-08-03 10:57:45 -0400

    rip out broken attempt to ellipsize group channel names

--- utils/channel_utils.jsx
+++ utils/channel_utils.jsx
@@ -28,8 +28,6 @@ export function sortChannelsByDisplayName(a, b) {
     return ChannelUtilsRedux.sortChannelsByTypeAndDisplayName(locale, a, b);
 }
 
-const MAX_CHANNEL_NAME_LENGTH = 64;
-
 export function getChannelDisplayName(channel) {
     if (channel.type !== Constants.GM_CHANNEL) {
         return channel.display_name;
@@ -38,17 +36,11 @@ export function getChannelDisplayName(channel) {
     const currentUser = UserStore.getCurrentUser();
 
     if (currentUser) {
-        let displayName = channel.display_name.
+        return channel.display_name.
             split(',').
             map((username) => username.trim()).
             filter((username) => username !== currentUser.username).
             join(', ');
-
-        if (displayName.length >= MAX_CHANNEL_NAME_LENGTH) {
-            displayName += '...';
-        }
-
-        return displayName;
     }
 
     return channel.display_name;
