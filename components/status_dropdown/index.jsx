d122887f97c449fbd6236122746ae6d949c657c2
Jesús Espino <jespinog@gmail.com>
2018-08-27 12:00:12 +0200

    MM-11655: Don't ask me again fix

--- components/status_dropdown/index.jsx
+++ components/status_dropdown/index.jsx
@@ -6,6 +6,8 @@ import {bindActionCreators} from 'redux';
 import {setStatus} from 'mattermost-redux/actions/users';
 import {Client4} from 'mattermost-redux/client';
 import {getCurrentUser, getStatusForUserId} from 'mattermost-redux/selectors/entities/users';
+import {Preferences} from 'mattermost-redux/constants';
+import {get} from 'mattermost-redux/selectors/entities/preferences';
 
 import {openModal} from 'actions/views/modals';
 
@@ -22,6 +24,7 @@ function mapStateToProps(state) {
     return {
         userId,
         profilePicture: Client4.getProfilePictureUrl(userId, currentUser.last_picture_update),
+        autoResetPref: get(state, Preferences.CATEGORY_AUTO_RESET_MANUAL_STATUS, userId, ''),
         status: getStatusForUserId(state, userId),
     };
 }
