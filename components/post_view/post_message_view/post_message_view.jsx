6541c8c594e9dbab26c576d4d8c9b63e5515a391
Saturnino Abril <saturnino.abril@gmail.com>
2018-09-04 20:40:06 +0800

    Update plugin based on hackathon observation - working on translate plugin (#1610)
    * add PostMessage pluggable at PostMessageView and add filter to PostDropdownMenuAction
    
    * update snapshots
    
    * rebase to latest and change pluggable name to PostMessageAttachment
    
    * update registry function to registerPostMessageComponentAttachment
    
    * rename to registerPostMessageAttachmentComponent

--- components/post_view/post_message_view/post_message_view.jsx
+++ components/post_view/post_message_view/post_message_view.jsx
@@ -12,8 +12,8 @@ import * as PostUtils from 'utils/post_utils';
 import * as Utils from 'utils/utils';
 
 import PostMarkdown from 'components/post_markdown';
-
-import ShowMore from '../show_more';
+import Pluggable from 'plugins/pluggable';
+import ShowMore from 'components/post_view/show_more';
 
 const MAX_POST_HEIGHT = 600;
 
@@ -195,6 +195,11 @@ export default class PostMessageView extends React.PureComponent {
                     />
                 </div>
                 {this.renderEditedIndicator()}
+                <Pluggable
+                    pluggableName='PostMessageAttachment'
+                    postId={post.id}
+                    onHeightChange={this.handleHeightReceived}
+                />
             </ShowMore>
         );
     }
