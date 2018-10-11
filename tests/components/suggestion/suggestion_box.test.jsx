2d94a360443e5012b4ee2d2699c2bcedba4a2da6
Jesse Hallam <jesse.hallam@gmail.com>
2018-10-11 13:34:59 -0400

    reproduce original error as unit test

--- tests/components/suggestion/suggestion_box.test.jsx
+++ tests/components/suggestion/suggestion_box.test.jsx
@@ -1,7 +1,11 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import React from 'react';
+import {mount} from 'enzyme';
+
 import SuggestionBox from 'components/suggestion/suggestion_box.jsx';
+import SuggestionList from 'components/suggestion/suggestion_list.jsx';
 
 describe('components/SuggestionBox', function() {
     test('findOverlap', () => {
@@ -12,5 +16,22 @@ describe('components/SuggestionBox', function() {
         expect(SuggestionBox.findOverlap('red', 'education')).toBe('ed');
         expect(SuggestionBox.findOverlap('red', 'reduce')).toBe('red');
         expect(SuggestionBox.findOverlap('black', 'ack')).toBe('ack');
+    });
+
+    it('should avoid ref access on unmount race', (done) => {
+        const props = {
+            listComponent: SuggestionList,
+            value: 'value',
+            containerClass: 'test',
+            openOnFocus: true,
+        };
+
+        const wrapper = mount(
+            <SuggestionBox {...props}/>
+        );
+        wrapper.instance().handleFocusIn({});
+        wrapper.unmount();
+
+        setTimeout(done, 100);
     });
 });
