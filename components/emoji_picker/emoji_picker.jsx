fda447c596dfe5e1c4cab5bbe6a070a6a3249767
JayaKrishna <namburu1995@gmail.com>
2018-10-11 12:18:29 +0530

    Fixed typos in ``emoji_picker`` and ``en.json`` files

--- components/emoji_picker/emoji_picker.jsx
+++ components/emoji_picker/emoji_picker.jsx
@@ -474,7 +474,7 @@ export default class EmojiPicker extends React.PureComponent {
                 <span className='fa fa-search emoji-picker__search-icon'/>
                 <input
                     aria-label={Utils.localizeMessage('emoji_picker.searchPlaceholder',
-                        'Enter text to search for Emojies')}
+                        'Enter text to search for emojis')}
                     ref={this.emojiSearchInput}
                     className='emoji-picker__search'
                     type='text'
