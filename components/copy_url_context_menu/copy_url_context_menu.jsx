164692091ef4e309a9dc5100379c61879ffd5264
Jesse Hallam <jesse.hallam@gmail.com>
2018-10-12 15:35:51 -0400

    MM-12513: fix copy link on desktop (#1878)
    * MM-12513: fix copy link on desktop
    
    * linting fixes after updating eslint properly

--- components/copy_url_context_menu/copy_url_context_menu.jsx
+++ components/copy_url_context_menu/copy_url_context_menu.jsx
@@ -6,33 +6,41 @@ import React from 'react';
 import {ContextMenu, ContextMenuTrigger, MenuItem} from 'react-contextmenu';
 import {FormattedMessage} from 'react-intl';
 
-import * as Utils from '../utils/utils';
-
 export default class CopyUrlContextMenu extends React.Component {
     static propTypes = {
 
-        /**
-         * The child component that will be right-clicked on to show the context menu
-         */
+        // The child component that will be right-clicked on to show the context menu
         children: PropTypes.element,
 
-        /**
-         * The link to copy to the user's clipboard when the 'Copy' option is selected from the context menu
-         */
+        // The link to copy to the user's clipboard when the 'Copy' option is selected from the context menu
         link: PropTypes.string.isRequired,
 
-        /**
-         * A unique id differentiating this instance of context menu from others on the page. Will be set to a random value if not provided.
-         */
+        // A unique id differentiating this instance of context menu from others on the page.
         menuId: PropTypes.string.isRequired,
+
+        siteURL: PropTypes.string.isRequired,
+
+        actions: PropTypes.shape({
+            copyToClipboard: PropTypes.func.isRequired,
+        }),
     };
+
+    copy = () => {
+        let link = this.props.link;
+
+        // Transform relative links to absolute ones for copy and paste.
+        if (link.indexOf('http://') === -1 && link.indexOf('https://') === -1) {
+            link = this.props.siteURL + link;
+        }
+
+        this.props.actions.copyToClipboard(link);
+    }
 
     render() {
         const contextMenu = (
             <ContextMenu id={'copy-url-context-menu' + this.props.menuId}>
                 <MenuItem
-                    data={{link: this.props.link}}
-                    onClick={Utils.copyToClipboard}
+                    onClick={this.copy}
                 >
                     <FormattedMessage
                         id='copy_url_context_menu.getChannelLink'
