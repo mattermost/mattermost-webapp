a647be15c95df1a4bf3c87c91821debd1ce0dc68
Elias Nahum <nahumhbl@gmail.com>
2018-10-13 15:26:18 -0300

    Migrate ChannelActions favorite and unfavorite channel to redux

--- tests/components/navbar/navbar.test.jsx
+++ tests/components/navbar/navbar.test.jsx
@@ -6,16 +6,6 @@ import {shallow} from 'enzyme';
 
 import Navbar from 'components/navbar/navbar.jsx';
 
-jest.mock('utils/browser_history', () => {
-    const original = require.requireActual('utils/browser_history');
-    return {
-        ...original,
-        browserHistory: {
-            push: jest.fn(),
-        },
-    };
-});
-
 describe('components/navbar/Navbar', () => {
     const baseProps = {
         teamDisplayName: 'team_display_name',
@@ -24,8 +14,8 @@ describe('components/navbar/Navbar', () => {
             closeLhs: jest.fn(),
             closeRhs: jest.fn(),
             closeRhsMenu: jest.fn(),
-            leaveChannel: jest.fn(),
             markFavorite: jest.fn(),
+            leaveChannel: jest.fn(),
             showPinnedPosts: jest.fn(),
             toggleLhs: jest.fn(),
             toggleRhsMenu: jest.fn(),
@@ -181,7 +171,6 @@ describe('components/navbar/Navbar', () => {
             preventDefault: jest.fn(),
         };
 
-        wrapper.setState(validState);
         wrapper.instance().toggleFavorite(event);
         expect(wrapper.instance().props.actions.markFavorite).toBeCalled();
 
