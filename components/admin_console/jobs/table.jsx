691fa35d2617ec64541031550df99a38b620ede8
Martin Kraft <martinkraft@gmail.com>
2018-10-16 15:08:12 -0400

    Adjusts details col width.

--- components/admin_console/jobs/table.jsx
+++ components/admin_console/jobs/table.jsx
@@ -336,10 +336,7 @@ class JobTable extends React.PureComponent {
                     <td className='whitespace--nowrap'>{this.getStatus(job)}</td>
                     <td className='whitespace--nowrap'>{this.getFinishAt(job.status, job.last_activity_at)}</td>
                     <td className='whitespace--nowrap'>{this.getRunLength(job)}</td>
-                    <td
-                        colSpan='3'
-                        style={{whiteSpace: 'pre'}}
-                    >{this.getExtraInfoText(job)}</td>
+                    <td style={{whiteSpace: 'pre'}}>{this.getExtraInfoText(job)}</td>
                 </tr>
             );
         });
