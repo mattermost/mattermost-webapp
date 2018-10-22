71e7b816c7540feecfe18bc7e319a7de3b583917
Saturnino Abril <saturnino.abril@gmail.com>
2018-10-22 18:58:58 +0800

    fix babel requirement for e2e and move/use constants to/from globals

--- nightwatch.conf.js
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
-require('babel-core/register');
-
-module.exports = require('./nightwatch.json');
