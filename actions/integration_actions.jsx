c90128842641410930675927d0199da9a2f4f221
JoramWilander <jwawilander@gmail.com>
2018-11-01 16:21:58 -0400

    Correctly load integration creators and convert integration actions

--- actions/integration_actions.jsx
+++ actions/integration_actions.jsx
@@ -3,195 +3,96 @@
 
 import request from 'superagent';
 import * as IntegrationActions from 'mattermost-redux/actions/integrations';
-import {getProfilesByIds, getUser} from 'mattermost-redux/actions/users';
-import {getCurrentTeamId} from 'mattermost-redux/selectors/entities/teams';
+import {getProfilesByIds} from 'mattermost-redux/actions/users';
+import {getUser} from 'mattermost-redux/selectors/entities/users';
 
-import store from 'stores/redux_store.jsx';
+import {Integrations} from 'utils/constants.jsx';
 
-const dispatch = store.dispatch;
-const getState = store.getState;
-
-export async function loadIncomingHooks(complete) {
-    const {data} = await IntegrationActions.getIncomingHooks('', 0, 10000)(dispatch, getState);
-    if (data) {
-        loadProfilesForIncomingHooks(data);
-    }
-
-    if (complete) {
-        complete(data);
-    }
-}
-
-export async function loadIncomingHooksForTeam(teamId, complete) {
-    const {data} = await IntegrationActions.getIncomingHooks(teamId, 0, 10000)(dispatch, getState);
-    if (data) {
-        loadProfilesForIncomingHooks(data);
-    }
-
-    if (complete) {
-        complete(data);
-    }
-}
-
-function loadProfilesForIncomingHooks(hooks) {
-    const state = getState();
-    const profilesToLoad = {};
-    for (let i = 0; i < hooks.length; i++) {
-        const hook = hooks[i];
-        if (!getUser(state, hook.user_id)) {
-            profilesToLoad[hook.user_id] = true;
+export function loadIncomingHooksAndProfilesForTeam(teamId, page = 0, perPage = Integrations.PAGE_SIZE) {
+    return async (dispatch) => {
+        const {data} = await dispatch(IntegrationActions.getIncomingHooks(teamId, page, perPage));
+        if (data) {
+            dispatch(loadProfilesForIncomingHooks(data));
         }
-    }
-
-    const list = Object.keys(profilesToLoad);
-    if (list.length === 0) {
-        return;
-    }
-
-    getProfilesByIds(list)(dispatch, getState);
+    };
 }
 
-export async function loadOutgoingHooks(complete) {
-    const {data} = await IntegrationActions.getOutgoingHooks('', '', 0, 10000)(dispatch, getState);
-    if (data) {
-        loadProfilesForOutgoingHooks(data);
-    }
-
-    if (complete) {
-        complete(data);
-    }
-}
-
-export async function loadOutgoingHooksForTeam(teamId, complete) {
-    const {data} = await IntegrationActions.getOutgoingHooks('', teamId, 0, 10000)(dispatch, getState);
-    if (data) {
-        loadProfilesForOutgoingHooks(data);
-    }
-
-    if (complete) {
-        complete(data);
-    }
-}
-
-function loadProfilesForOutgoingHooks(hooks) {
-    const state = getState();
-    const profilesToLoad = {};
-    for (let i = 0; i < hooks.length; i++) {
-        const hook = hooks[i];
-        if (!getUser(state, hook.creator_id)) {
-            profilesToLoad[hook.creator_id] = true;
+export function loadProfilesForIncomingHooks(hooks) {
+    return async (dispatch, getState) => {
+        const state = getState();
+        const profilesToLoad = {};
+        for (let i = 0; i < hooks.length; i++) {
+            const hook = hooks[i];
+            if (!getUser(state, hook.user_id)) {
+                profilesToLoad[hook.user_id] = true;
+            }
         }
-    }
 
-    const list = Object.keys(profilesToLoad);
-    if (list.length === 0) {
-        return;
-    }
-
-    getProfilesByIds(list)(dispatch, getState);
-}
-
-export async function loadTeamCommands(complete) {
-    const {data} = await dispatch(IntegrationActions.getCustomTeamCommands(getCurrentTeamId(getState())));
-    if (data) {
-        loadProfilesForCommands(data);
-    }
-
-    if (complete) {
-        complete(data);
-    }
-}
-
-function loadProfilesForCommands(commands) {
-    const state = getState();
-    const profilesToLoad = {};
-    for (let i = 0; i < commands.length; i++) {
-        const command = commands[i];
-        if (!getUser(state, command.creator_id)) {
-            profilesToLoad[command.creator_id] = true;
+        const list = Object.keys(profilesToLoad);
+        if (list.length === 0) {
+            return;
         }
-    }
 
-    const list = Object.keys(profilesToLoad);
-    if (list.length === 0) {
-        return;
-    }
-
-    getProfilesByIds(list)(dispatch, getState);
+        dispatch(getProfilesByIds(list));
+    };
 }
 
-export async function addIncomingHook(hook, success, error) {
-    const {data, error: err} = await IntegrationActions.createIncomingHook(hook)(dispatch, getState);
-    if (data && success) {
-        success(data);
-    } else if (err && error) {
-        error({id: err.server_error_id, ...err});
-    }
+export function loadOutgoingHooksAndProfilesForTeam(teamId, page = 0, perPage = Integrations.PAGE_SIZE) {
+    return async (dispatch) => {
+        const {data} = await dispatch(IntegrationActions.getOutgoingHooks('', teamId, page, perPage));
+        if (data) {
+            dispatch(loadProfilesForOutgoingHooks(data));
+        }
+    };
 }
 
-export async function updateIncomingHook(hook, success, error) {
-    const {data, error: err} = await IntegrationActions.updateIncomingHook(hook)(dispatch, getState);
-    if (data && success) {
-        success(data);
-    } else if (err && error) {
-        error({id: err.server_error_id, ...err});
-    }
+export function loadProfilesForOutgoingHooks(hooks) {
+    return async (dispatch, getState) => {
+        const state = getState();
+        const profilesToLoad = {};
+        for (let i = 0; i < hooks.length; i++) {
+            const hook = hooks[i];
+            if (!getUser(state, hook.creator_id)) {
+                profilesToLoad[hook.creator_id] = true;
+            }
+        }
+
+        const list = Object.keys(profilesToLoad);
+        if (list.length === 0) {
+            return;
+        }
+
+        dispatch(getProfilesByIds(list));
+    };
 }
 
-export async function addOutgoingHook(hook, success, error) {
-    const {data, error: err} = await IntegrationActions.createOutgoingHook(hook)(dispatch, getState);
-    if (data && success) {
-        success(data);
-    } else if (err && error) {
-        error({id: err.server_error_id, ...err});
-    }
+export function loadCommandsAndProfilesForTeam(teamId) {
+    return async (dispatch) => {
+        const {data} = await dispatch(IntegrationActions.getCustomTeamCommands(teamId));
+        if (data) {
+            dispatch(loadProfilesForCommands(data));
+        }
+    };
 }
 
-export async function updateOutgoingHook(hook, success, error) {
-    const {data, error: err} = await IntegrationActions.updateOutgoingHook(hook)(dispatch, getState);
-    if (data && success) {
-        success(data);
-    } else if (err && error) {
-        error({id: err.server_error_id, ...err});
-    }
-}
+export function loadProfilesForCommands(commands) {
+    return async (dispatch, getState) => {
+        const state = getState();
+        const profilesToLoad = {};
+        for (let i = 0; i < commands.length; i++) {
+            const command = commands[i];
+            if (!getUser(state, command.creator_id)) {
+                profilesToLoad[command.creator_id] = true;
+            }
+        }
 
-export function deleteIncomingHook(id) {
-    IntegrationActions.removeIncomingHook(id)(dispatch, getState);
-}
+        const list = Object.keys(profilesToLoad);
+        if (list.length === 0) {
+            return;
+        }
 
-export function deleteOutgoingHook(id) {
-    IntegrationActions.removeOutgoingHook(id)(dispatch, getState);
-}
-
-export function regenOutgoingHookToken(id) {
-    IntegrationActions.regenOutgoingHookToken(id)(dispatch, getState);
-}
-
-export async function addCommand(command, success, error) {
-    const {data, error: err} = await IntegrationActions.addCommand(command)(dispatch, getState);
-    if (data && success) {
-        success(data);
-    } else if (err && error) {
-        error({id: err.server_error_id, ...err});
-    }
-}
-
-export async function editCommand(command, success, error) {
-    const {data, error: err} = await IntegrationActions.editCommand(command)(dispatch, getState);
-    if (data && success) {
-        success(data);
-    } else if (err && error) {
-        error({id: err.server_error_id, ...err});
-    }
-}
-
-export function deleteCommand(id) {
-    IntegrationActions.deleteCommand(id)(dispatch, getState);
-}
-
-export function regenCommandToken(id) {
-    IntegrationActions.regenCommandToken(id)(dispatch, getState);
+        dispatch(getProfilesByIds(list));
+    };
 }
 
 export function getYoutubeVideoInfo(googleKey, videoId, success, error) {
