6a178faa5a138f8c59892a74a475963b1ff626f5
sudheer <sudheer.105@gmail.com>
2018-11-02 17:20:27 +0530

    Add a default timezone 'America/Los_Angeles' for tests

--- tests/helpers/intl-test-helper.jsx
+++ tests/helpers/intl-test-helper.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 import {IntlProvider, intlShape} from 'react-intl';
 import {mount, shallow} from 'enzyme';
 
-const intlProvider = new IntlProvider({locale: 'en'}, {});
+const intlProvider = new IntlProvider({locale: 'en', timeZone: 'America/Los_Angeles'}, {});
 const {intl} = intlProvider.getChildContext();
 
 export function shallowWithIntl(node, {context} = {}) {
