351e916ed9ccd61b9b51451dd2d16c37c3282411
Jesse Hallam <jesse.hallam@gmail.com>
2018-11-02 12:18:37 -0400

    MM-12708: unify sign-in change redirection (#1989)
    * MM-12708: unify sign-in change redirection
    
    Back out some of my earlier changes in MM-12680 and MM-12619 to just rely on the login controller detecting the sign in change and suppressing the session expiry.
    
    * fix unit tests

--- components/claim/components/email_to_oauth.jsx
+++ components/claim/components/email_to_oauth.jsx
@@ -60,8 +60,9 @@ export default class EmailToOAuth extends React.Component {
             token,
             this.props.newType,
             (data) => {
-                // Stay logged in and just redirect to the OAuth provider.
-                window.location.href = data.follow_link;
+                if (data.follow_link) {
+                    window.location.href = data.follow_link;
+                }
             },
             (err) => {
                 this.setState({error: err.message, showMfa: false});
