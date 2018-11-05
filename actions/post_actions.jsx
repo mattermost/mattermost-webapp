4481dd50df04e5dee1d00ddaeab1db363a4bf4ed
Saturnino Abril <saturnino.abril@gmail.com>
2018-11-06 00:03:58 +0800

    remove updatePost from post_actions (#1983)

--- actions/post_actions.jsx
+++ actions/post_actions.jsx
@@ -155,19 +155,6 @@ export function storeCommentDraft(rootPostId, draft) {
     };
 }
 
-export async function updatePost(post, success) {
-    const {data, error: err} = await PostActions.editPost(post)(dispatch, getState);
-    if (data && success) {
-        success();
-    } else if (err) {
-        AppDispatcher.handleServerAction({
-            type: ActionTypes.RECEIVED_ERROR,
-            err: {id: err.server_error_id, ...err},
-            method: 'editPost',
-        });
-    }
-}
-
 export function emitEmojiPosted(emoji) {
     dispatch(addRecentEmoji(emoji));
 }
