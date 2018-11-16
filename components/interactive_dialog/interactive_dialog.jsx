151d45ed98ebaad403545f5c81e4df20db5b0e59
JoramWilander <jwawilander@gmail.com>
2018-11-16 17:06:18 -0500

    Move common functions to mattermost-redux

--- components/interactive_dialog/interactive_dialog.jsx
+++ components/interactive_dialog/interactive_dialog.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Modal} from 'react-bootstrap';
 import {FormattedMessage} from 'react-intl';
+import {checkDialogElementForError, checkIfErrorsMatchElements} from 'mattermost-redux/utils/integration_utils';
 
 import SpinnerButton from 'components/spinner_button';
 
@@ -200,70 +201,4 @@ export default class InteractiveDialog extends React.Component {
             </Modal>
         );
     }
-}
-
-export function checkDialogElementForError(elem, value) {
-    if (!value && !elem.optional) {
-        return {
-            id: 'interactive_dialog.error.required',
-            defaultMessage: 'This field is required.',
-        };
-    }
-
-    const type = elem.type;
-    if (type === 'text' || type === 'textarea') {
-        if (value && value.length < elem.min_length) {
-            return {
-                id: 'interactive_dialog.error.too_short',
-                defaultMessage: 'Minimum input length is {minLength}.',
-                values: {minLength: elem.min_length},
-            };
-        }
-
-        if (elem.subtype === 'email') {
-            if (value && !value.includes('@')) {
-                return {
-                    id: 'interactive_dialog.error.bad_email',
-                    defaultMessage: 'Must be a valid email address.',
-                };
-            }
-        }
-
-        if (elem.subtype === 'number') {
-            if (value && isNaN(value)) {
-                return {
-                    id: 'interactive_dialog.error.bad_number',
-                    defaultMessage: 'Must be a number.',
-                };
-            }
-        }
-
-        if (elem.subtype === 'url') {
-            if (value && !value.includes('http://') && !value.includes('https://')) {
-                return {
-                    id: 'interactive_dialog.error.bad_url',
-                    defaultMessage: 'URL must include http:// or https://.',
-                };
-            }
-        }
-    }
-
-    return null;
-}
-
-// If we're returned errors that don't match any of the elements we have,
-// ignore them and complete the dialog
-export function checkIfErrorsMatchElements(errors = {}, elements = []) {
-    for (const name in errors) {
-        if (!errors.hasOwnProperty(name)) {
-            continue;
-        }
-        for (const elem of elements) {
-            if (elem.name === name) {
-                return true;
-            }
-        }
-    }
-
-    return false;
 }
