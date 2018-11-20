84a809e0f4c619736085df6cccc9129ae66ce12f
Hyeseong Kim <cometkim.kr@gmail.com>
2018-11-21 02:45:27 +0900

    Fix view_and_manage_members to show different name by permission

--- components/channel_header_dropdown/menu_items/view_and_manage_members.js
+++ components/channel_header_dropdown/menu_items/view_and_manage_members.js
@@ -4,9 +4,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {FormattedMessage} from 'react-intl';
+import {Permissions} from 'mattermost-redux/constants';
 
 import {Constants, ModalIdentifiers} from 'utils/constants';
 
+import ChannelPermissionGate from 'components/permissions_gates/channel_permission_gate';
 import ToggleModalButtonRedux from 'components/toggle_modal_button_redux';
 import ChannelMembersModal from 'components/channel_members_modal';
 
@@ -19,6 +21,9 @@ const ViewAndManageMembers = ({channel}) => {
         return null;
     }
 
+    const isPrivate = channel.type === Constants.PRIVATE_CHANNEL;
+    const permission = isPrivate ? Permissions.MANAGE_PRIVATE_CHANNEL_MEMBERS : Permissions.MANAGE_PUBLIC_CHANNEL_MEMBERS;
+
     return (
         <li role='presentation'>
             <ToggleModalButtonRedux
@@ -27,10 +32,27 @@ const ViewAndManageMembers = ({channel}) => {
                 dialogType={ChannelMembersModal}
                 dialogProps={{channel}}
             >
-                <FormattedMessage
-                    id='channel_header.viewMembers'
-                    defaultMessage='View Members'
-                />
+                <ChannelPermissionGate
+                    channelId={channel.id}
+                    teamId={channel.team_id}
+                    permissions={[permission]}
+                >
+                    <FormattedMessage
+                        id='channel_header.manageMembers'
+                        defaultMessage='Manage Members'
+                    />
+                </ChannelPermissionGate>
+                <ChannelPermissionGate
+                    channelId={channel.id}
+                    teamId={channel.team_id}
+                    permissions={[permission]}
+                    invert={true}
+                >
+                    <FormattedMessage
+                        id='channel_header.viewMembers'
+                        defaultMessage='View Members'
+                    />
+                </ChannelPermissionGate>
             </ToggleModalButtonRedux>
         </li>
     );
