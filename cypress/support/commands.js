0aab376d67b6da63fa435adb744eda1d2b381e5c
Saturnino Abril <saturnino.abril@gmail.com>
2018-11-23 23:34:11 +0800

    add E2E to hide/show channel switcher button in LHS (#2058)

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,11 +7,11 @@ import users from '../fixtures/users.json';
 // Read more: https://on.cypress.io/custom-commands
 // ***********************************************************
 
-Cypress.Commands.add('login', (userType, {username, password, url}) => {
-    const user = users[userType];
-    const usernameParam = user && user.username ? user.username : username;
-    const passwordParam = user && user.password ? user.password : password;
-    const urlParam = url ? `${url}/api/v4/users/login` : '/api/v4/users/login';
+Cypress.Commands.add('login', (username, {otherUsername, otherPassword, otherURL} = {}) => {
+    const user = users[username];
+    const usernameParam = user && user.username ? user.username : otherUsername;
+    const passwordParam = user && user.password ? user.password : otherPassword;
+    const urlParam = otherURL ? `${otherURL}/api/v4/users/login` : '/api/v4/users/login';
 
     cy.request({
         url: urlParam,
@@ -20,3 +20,14 @@ Cypress.Commands.add('login', (userType, {username, password, url}) => {
     });
 });
 
+Cypress.Commands.add('toAccountSettingsModal', (username, isLoggedInAlready = false, {otherUsername, otherPassword, otherURL} = {}) => {
+    if (!isLoggedInAlready) {
+        cy.login('user-1', {otherUsername, otherPassword, otherURL});
+        cy.visit('/');
+    }
+
+    cy.get('#channel_view').should('be.visible');
+    cy.get('#sidebarHeaderDropdownButton').should('be.visible').click();
+    cy.get('#accountSettings').should('be.visible').click();
+    cy.get('#accountSettingsModal').should('be.visible');
+});
