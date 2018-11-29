af39140854702570d6169f1a6f844d792bab9510
Harrison Healey <harrisonmhealey@gmail.com>
2018-11-29 12:41:57 -0500

    Fix accidentally reverted unit tests

--- components/size_aware_image.test.jsx
+++ components/size_aware_image.test.jsx
@@ -2,42 +2,91 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount, shallow} from 'enzyme';
 
 import SizeAwareImage from 'components/size_aware_image';
 
-describe('components/SizeAwareImage', () => {
-    test('should match snapshot', () => {
-        const wrapper = shallow(
-            <SizeAwareImage
-                src={'https://something.com/image.png'}
-                dimensions={{
-                    width: '100',
-                    height: '200',
-                }}
-                onHeightReceived={jest.fn()}
-            />
-        );
+jest.mock('utils/image_utils');
 
-        expect(wrapper).toMatchSnapshot();
+import {createPlaceholderImage, loadImage} from 'utils/image_utils';
+
+describe('components/SizeAwareImage', () => {
+    const baseProps = {
+        dimensions: {
+            height: 200,
+            width: 300,
+        },
+        onHeightReceived: jest.fn(),
+        src: 'https://example.com/image.png',
+    };
+
+    loadImage.mockReturnValue(() => ({}));
+
+    test('should render a placeholder when first mounted with dimensions', () => {
+        createPlaceholderImage.mockImplementation(() => 'data:image/png;base64,abc123');
+
+        const wrapper = mount(<SizeAwareImage {...baseProps}/>);
+
+        const img = wrapper.getDOMNode();
+        expect(img.src.startsWith('data:image')).toBeTruthy();
     });
 
-    test('should call onHeightReceived', () => {
-        const onHeightReceived = jest.fn();
-        const wrapper = shallow(
-            <SizeAwareImage
-                src={'https://something.com/image.png'}
-                onHeightReceived={onHeightReceived}
-            />
-        );
-        const instance = wrapper.instance();
+    test('should render the actual image after it is loaded', () => {
+        const wrapper = mount(<SizeAwareImage {...baseProps}/>);
+        wrapper.setState({loaded: true});
 
-        instance.refs = {
-            image: {
-                height: 100,
-            },
-        };
-        wrapper.find('img').prop('onLoad')();
-        expect(onHeightReceived).toHaveBeenCalledWith(100);
+        const img = wrapper.getDOMNode();
+        expect(img.src).toEqual(baseProps.src);
+    });
+
+    test('should render the actual image when first mounted without dimensions', () => {
+        const props = {...baseProps};
+        Reflect.deleteProperty(props, 'dimensions');
+
+        const wrapper = mount(<SizeAwareImage {...props}/>);
+
+        const img = wrapper.getDOMNode();
+        expect(img.src).toEqual(baseProps.src);
+    });
+
+    test('should load image when mounted and when src changes', () => {
+        const wrapper = shallow(<SizeAwareImage {...baseProps}/>);
+
+        expect(loadImage).toHaveBeenCalledTimes(1);
+        expect(loadImage.mock.calls[0][0]).toEqual(baseProps.src);
+
+        const newSrc = 'https://example.com/new_image.png';
+        wrapper.setProps({src: newSrc});
+
+        expect(loadImage).toHaveBeenCalledTimes(2);
+        expect(loadImage.mock.calls[1][0]).toEqual(newSrc);
+    });
+
+    test('should call onHeightReceived on load when dimensions are needed', () => {
+        const height = 123;
+        loadImage.mockImplementation((src, onLoad) => {
+            onLoad({height});
+
+            return {};
+        });
+
+        const props = {...baseProps};
+        Reflect.deleteProperty(props, 'dimensions');
+
+        shallow(<SizeAwareImage {...props}/>);
+
+        expect(baseProps.onHeightReceived).toHaveBeenCalledWith(height);
+    });
+
+    test('should not call onHeightReceived when dimensions are provided', () => {
+        loadImage.mockImplementation((src, onLoad) => {
+            onLoad({height: 100});
+
+            return {};
+        });
+
+        shallow(<SizeAwareImage {...baseProps}/>);
+
+        expect(baseProps.onHeightReceived).not.toHaveBeenCalled();
     });
 });
