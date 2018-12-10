a550e7be349f21649db957f604a4d5072829ea16
Jesse Hallam <jesse.hallam@gmail.com>
2018-12-10 11:49:34 -0500

    MM-13085: update prefix if context changes (#2155)
    A rendered suggestion box can have its context changed out from underneath it without any other props changing, for example when switching between channels. Trigger a pretext match when such a context change is detected to ensure we aren't comparing "state" from a previous context.

--- components/suggestion/suggestion_box.jsx
+++ components/suggestion/suggestion_box.jsx
@@ -113,6 +113,12 @@ export default class SuggestionBox extends React.Component {
          * If true, replace all input in the suggestion box with the selected option after a select, defaults to false
          */
         replaceAllInputOnSelect: PropTypes.bool,
+
+        /**
+         * An optional, opaque identifier that distinguishes the context in which the suggestion
+         * box is rendered. This allows the reused component to otherwise respond to changes.
+         */
+        contextId: PropTypes.string,
     }
 
     static defaultProps = {
@@ -156,6 +162,15 @@ export default class SuggestionBox extends React.Component {
             allowDividers: true,
             presentationType: 'text',
         };
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.contextId !== this.props.contextId) {
+            const textbox = this.getTextbox();
+            const pretext = textbox.value.substring(0, textbox.selectionEnd).toLowerCase();
+
+            this.handlePretextChanged(pretext);
+        }
     }
 
     getTextbox = () => {
