eeff695d5f4d50148bb6c4afe71a6b79963af944
George Goldberg <george@gberg.me>
2018-12-11 13:52:47 +0000

    MM-13425: Fix set header option in DM/GM dropdown. (#2175)
    Should not depend on permissions for these channel types.

--- components/channel_header_dropdown/menu_items/set_channel_header.test.js
+++ components/channel_header_dropdown/menu_items/set_channel_header.test.js
@@ -46,18 +46,100 @@ describe('components/ChannelHeaderDropdown/MenuItem.SetChannelHeader', () => {
         expect(wrapper.isEmptyRender()).toBeTruthy();
     });
 
-    it('should requires right permission level for channel type to manage header', () => {
+    it('should not be permissions gated for a DM channel', () => {
+        const props = {
+            ...baseProps,
+            channel: {
+                ...baseProps.channel,
+                type: Constants.DM_CHANNEL,
+            },
+        };
+        const wrapper = shallow(<SetChannelHeader {...props}/>);
+
+        expect(wrapper).toMatchInlineSnapshot(`
+<li
+  role="presentation"
+>
+  <Connect(ModalToggleButtonRedux)
+    dialogProps={
+      Object {
+        "channel": Object {
+          "id": "channel_id",
+          "team_id": "team_id",
+          "type": "D",
+        },
+      }
+    }
+    dialogType={[Function]}
+    id="channelEditHeader"
+    modalId="edit_channel_header"
+    role="menuitem"
+  >
+    <FormattedMessage
+      defaultMessage="Edit Channel Header"
+      id="channel_header.setHeader"
+      values={Object {}}
+    />
+  </Connect(ModalToggleButtonRedux)>
+</li>
+`);
+    });
+
+    it('should not be permissions gated for a GM channel', () => {
+        const props = {
+            ...baseProps,
+            channel: {
+                ...baseProps.channel,
+                type: Constants.GM_CHANNEL,
+            },
+        };
+        const wrapper = shallow(<SetChannelHeader {...props}/>);
+
+        expect(wrapper).toMatchInlineSnapshot(`
+<li
+  role="presentation"
+>
+  <Connect(ModalToggleButtonRedux)
+    dialogProps={
+      Object {
+        "channel": Object {
+          "id": "channel_id",
+          "team_id": "team_id",
+          "type": "G",
+        },
+      }
+    }
+    dialogType={[Function]}
+    id="channelEditHeader"
+    modalId="edit_channel_header"
+    role="menuitem"
+  >
+    <FormattedMessage
+      defaultMessage="Edit Channel Header"
+      id="channel_header.setHeader"
+      values={Object {}}
+    />
+  </Connect(ModalToggleButtonRedux)>
+</li>
+`);
+    });
+
+    it('should requires right permission level for channel type to manage header in Public and Private channels', () => {
         const props = {
             ...baseProps,
             channel: {...baseProps.channel},
         };
         const makeWrapper = () => shallow(<SetChannelHeader {...props}/>);
 
-        // Public, DM, GM (is this correct?)
+        // Public Channel
         props.channel.type = Constants.OPEN_CHANNEL;
-        expect(makeWrapper().prop('permissions')[0]).toBe(Permissions.MANAGE_PUBLIC_CHANNEL_PROPERTIES);
+        expect(makeWrapper().prop('permissions')[0]).toBe(
+            Permissions.MANAGE_PUBLIC_CHANNEL_PROPERTIES
+        );
 
         props.channel.type = Constants.PRIVATE_CHANNEL;
-        expect(makeWrapper().prop('permissions')[0]).toBe(Permissions.MANAGE_PRIVATE_CHANNEL_PROPERTIES);
+        expect(makeWrapper().prop('permissions')[0]).toBe(
+            Permissions.MANAGE_PRIVATE_CHANNEL_PROPERTIES
+        );
     });
 });
