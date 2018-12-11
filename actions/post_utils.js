1ff75be021bdb81b7f9425eda6122ac00d71af2a
sudheer <sudheer.105@gmail.com>
2018-12-11 18:09:50 +0530

    Fix calling for postThread if we dont have channel initiated

--- actions/post_utils.js
+++ actions/post_utils.js
@@ -28,7 +28,8 @@ export function completePostReceive(post, websocketMessageProps) {
         const state = getState();
 
         const rootPost = PostSelectors.getPost(state, post.root_id);
-        if (post.root_id && !rootPost) {
+        const postsInChannel = PostSelectors.getPostIdsInChannel(getState(), post.channel_id);
+        if (post.root_id && !rootPost && postsInChannel && postsInChannel.length !== 0) {
             const {data: posts} = await dispatch(PostActions.getPostThread(post.root_id));
             if (posts) {
                 dispatch(lastPostActions(post, websocketMessageProps));
