9105699777a0e35e004ce3e00df29bf75a28278f
Jesús Espino <jespinog@gmail.com>
2018-12-21 16:18:01 +0100

    Using the values stored in the 'channels' store

--- reducers/views/channel_selector_modal.js
+++ reducers/views/channel_selector_modal.js
@@ -5,12 +5,12 @@ import {combineReducers} from 'redux';
 
 import {ChannelTypes, UserTypes} from 'mattermost-redux/action_types';
 
-function channels(state = {}, action) {
+function channels(state = [], action) {
     switch (action.type) {
     case ChannelTypes.RECEIVED_ALL_CHANNELS:
-        return action.data;
+        return action.data.map((v) => v.id);
     case UserTypes.LOGOUT_SUCCESS:
-        return {};
+        return [];
     default:
         return state;
     }
