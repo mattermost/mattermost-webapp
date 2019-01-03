e214745d8f0e095dca051769a6e9612c76e08da3
Asaad Mahmood <asaad@mattermost.com>
2019-01-03 07:10:27 +0500

    Removing console

--- components/user_profile.jsx
+++ components/user_profile.jsx
@@ -58,8 +58,6 @@ export default class UserProfile extends React.Component {
         let profileImg = '';
         let placement = 'right';
 
-        console.log(this.props.isRHS);
-
         if (this.props.isRHS && !Utils.isMobile()) {
             placement = 'left';
         }
