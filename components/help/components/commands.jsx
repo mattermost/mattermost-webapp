b322c5f6cc548fabdc8a6826f6e289bf8e15dc2d
kosgrz <45372453+kosgrz@users.noreply.github.com>
2019-02-05 08:17:31 +0100

    [MM-13297] Remove usage of localizeMessage and dangerouslySetInnerHTML (#2233)
    from components in `components/help`
    
    * Remove the markdown from the messages and use proper HTML tags instead
    * Change the translation files

--- components/help/components/commands.jsx
+++ components/help/components/commands.jsx
@@ -5,26 +5,99 @@ import React from 'react';
 import {FormattedMessage} from 'react-intl';
 import {Link} from 'react-router-dom';
 
-import {formatText} from 'utils/text_formatting.jsx';
-import {localizeMessage} from 'utils/utils.jsx';
+import FormattedMarkdownMessage from 'components/formatted_markdown_message.jsx';
 
 export default function HelpCommands() {
-    const message = [];
-    message.push(localizeMessage('help.commands.title', '# Executing Commands\n___'));
-    message.push(localizeMessage('help.commands.intro', 'Slash commands perform operations in Mattermost by typing into the text input box. Enter a `/` followed by a command and some arguments to perform actions.\n\nBuilt-in slash commands come with all Mattermost installations and custom slash commands are configurable to interact with external applications. Learn about configuring custom slash commands on the [developer slash command documentation page](http://docs.mattermost.com/developer/slash-commands.html).'));
-    message.push(localizeMessage('help.commands.builtin', '## Built-in Commands\nThe following slash commands are available on all Mattermost installations:'));
-    message.push('![commands](https://s3.amazonaws.com/images.mattermost.com/slashCommandsTable1.png)');
-    message.push('![commands](https://s3.amazonaws.com/images.mattermost.com/slashCommandsTable2.png)');
-    message.push(localizeMessage('help.commands.builtin2', 'Begin by typing `/` and a list of slash command options appears above the text input box. The autocomplete suggestions help by providing a format example in black text and a short description of the slash command in grey text.'));
-    message.push('![autocomplete](https://docs.mattermost.com/_images/slashCommandsAutocomplete.PNG)');
-    message.push(localizeMessage('help.commands.custom', '## Custom Commands\nCustom slash commands integrate with external applications. For example, a team might configure a custom slash command to check internal health records with `/patient joe smith` or check the weekly weather forecast in a city with `/weather toronto week`. Check with your System Admin or open the autocomplete list by typing `/` to determine if your team configured any custom slash commands.'));
-    message.push(localizeMessage('help.commands.custom2', 'Custom slash commands are disabled by default and can be enabled by the System Admin in the **System Console** > **Integrations** > **Webhooks and Commands**. Learn about configuring custom slash commands on the [developer slash command documentation page](http://docs.mattermost.com/developer/slash-commands.html).'));
-
     return (
         <div>
-            <span
-                dangerouslySetInnerHTML={{__html: formatText(message.join('\n\n'))}}
-            />
+            <h1 className='markdown__heading'>
+                <FormattedMessage
+                    id='help.commands.title'
+                    defaultMessage='Executing Commands'
+                />
+            </h1>
+
+            <hr/>
+
+            <p>
+                <FormattedMarkdownMessage
+                    id='help.commands.intro1'
+                    defaultMessage='Slash commands perform operations in Mattermost by typing into the text input box. Enter a `/` followed by a command and some arguments to perform actions.'
+                />
+            </p>
+
+            <p>
+                <FormattedMarkdownMessage
+                    id='help.commands.intro2'
+                    defaultMessage='Built-in slash commands come with all Mattermost installations and custom slash commands are configurable to interact with external applications. Learn about configuring custom slash commands on the [developer slash command documentation page](!http://docs.mattermost.com/developer/slash-commands.html).'
+                />
+            </p>
+
+            <h2 className='markdown__heading'>
+                <FormattedMessage
+                    id='help.commands.builtin.title'
+                    defaultMessage='Built-in Commands'
+                />
+            </h2>
+
+            <p>
+                <FormattedMessage
+                    id='help.commands.builtin.description'
+                    defaultMessage='The following slash commands are available on all Mattermost installations:'
+                />
+            </p>
+
+            <p>
+                <img
+                    src='https://s3.amazonaws.com/images.mattermost.com/slashCommandsTable1.png'
+                    alt='commands'
+                    className='markdown-inline-img'
+                />
+            </p>
+            <p>
+                <img
+                    src='https://s3.amazonaws.com/images.mattermost.com/slashCommandsTable2.png'
+                    alt='commands'
+                    className='markdown-inline-img'
+                />
+            </p>
+
+            <p>
+                <FormattedMarkdownMessage
+                    id='help.commands.builtin2'
+                    defaultMessage='Begin by typing `/` and a list of slash command options appears above the text input box. The autocomplete suggestions help by providing a format example in black text and a short description of the slash command in grey text.'
+                />
+            </p>
+
+            <p>
+                <img
+                    src='https://docs.mattermost.com/_images/slashCommandsAutocomplete.PNG'
+                    alt='autocomplete'
+                    className='markdown-inline-img'
+                />
+            </p>
+
+            <h2 className='markdown__heading'>
+                <FormattedMarkdownMessage
+                    id='help.commands.custom.title'
+                    defaultMessage='Custom Commands'
+                />
+            </h2>
+
+            <p>
+                <FormattedMarkdownMessage
+                    id='help.commands.custom.description'
+                    defaultMessage='Custom slash commands integrate with external applications. For example, a team might configure a custom slash command to check internal health records with `/patient joe smith` or check the weekly weather forecast in a city with `/weather toronto week`. Check with your System Admin or open the autocomplete list by typing `/` to determine if your team configured any custom slash commands.'
+                />
+            </p>
+
+            <p>
+                <FormattedMarkdownMessage
+                    id='help.commands.custom2'
+                    defaultMessage='Custom slash commands are disabled by default and can be enabled by the System Admin in the **System Console** > **Integrations** > **Webhooks and Commands**. Learn about configuring custom slash commands on the [developer slash command documentation page](!http://docs.mattermost.com/developer/slash-commands.html).'
+                />
+            </p>
+
             <p className='links'>
                 <FormattedMessage
                     id='help.learnMore'
