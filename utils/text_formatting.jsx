89a665f800e242ca8edda9fc496181eec40b7af9
Dean Whillier <dean@whillier.com>
2019-02-05 15:31:41 -0500

    handle @channel, @here, @all before other @mention
    - general @mentions need to support periods as the last character, messing up highlighting if @channel, @here or @all are directly followed by a period (i.e. end of sentance)
    - handling @channel, @here and @all before allows for ignoring any punctuation immediately following

--- utils/text_formatting.jsx
+++ utils/text_formatting.jsx
@@ -181,6 +181,10 @@ export function autolinkAtMentions(text, tokens) {
 
     let output = text;
 
+    // handle @channel, @all, @here mentions first (purposely excludes trailing punctuation)
+    output = output.replace(/@(channel|all|here)/gi, replaceAtMentionWithToken);
+
+    // handle all other mentions (supports training punctuation)
     let match = output.match(AT_MENTION_PATTERN);
     while (match && match.length > 0) {
         output = output.replace(AT_MENTION_PATTERN, replaceAtMentionWithToken);
