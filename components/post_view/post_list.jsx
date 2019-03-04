25687661b69064561a4a304cc96542e9f803f328
Harrison Healey <harrisonmhealey@gmail.com>
2019-03-04 17:11:53 -0500

    Remove unnecessary null check

--- components/post_view/post_list.jsx
+++ components/post_view/post_list.jsx
@@ -604,7 +604,7 @@ export default class PostList extends React.PureComponent {
         const posts = this.props.posts || [];
         const channel = this.props.channel;
 
-        if ((posts.length === 0 && this.state.isDoingInitialLoad) || channel == null) {
+        if (posts.length === 0 && this.state.isDoingInitialLoad) {
             return (
                 <div id='post-list'>
                     <LoadingScreen
