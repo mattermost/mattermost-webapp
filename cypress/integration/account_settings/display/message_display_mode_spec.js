77c88c9bcaed1599c1f223905a09a86a3c8472de
Bob Lubecker <robertlubecker@gmail.com>
2019-03-04 05:50:02 -0600

    [MM-14283] Updated test based on PR feedback

--- cypress/integration/account_settings/display/message_display_mode_spec.js
+++ cypress/integration/account_settings/display/message_display_mode_spec.js
@@ -11,72 +11,48 @@
 
 describe('Account Settings > Display > Message Display', () => {
     before(() => {
-        // 1. Go to Account Settings with "user-2"
-        cy.toAccountSettingsModal('user-2');
+        // 1. Change message display setting to compact
+        cy.changeMessageDisplaySetting('COMPACT');
+    });
+
+    after(() => {
+        // Revert setting so it does not impact other tests
+        cy.changeMessageDisplaySetting('STANDARD');
     });
 
     it('M14283 Compact view: Line breaks remain intact after editing', () => {
-        // 2. Click the Display tab
-        cy.get('#displayButton').click();
+        // 2. Enter in text
+        cy.get('#post_textbox').
+            type('First line').
+            type('{shift}{enter}{enter}').
+            type('Text after{enter}');
 
-        // * Check that it changed into the Display section
-        cy.get('#displaySettingsTitle').
-            should('be.visible').
-            should('contain', 'Display Settings');
-
-        // 3. Scroll up to bring Message Display setting in viewable area.
-        cy.get('#message_displayTitle').scrollIntoView();
-
-        // 4. Click "Edit" to the right of "Message Display"
-        cy.get('#message_displayTitle').click();
-
-        // 5. Scroll a bit to show the "Save" button
-        cy.get('.section-max').scrollIntoView();
-
-        // 6. Click the compact radio
-        cy.get('#message_displayFormatB').click();
-
-        // 7. Click "Save"
-        cy.get('#saveSetting').click();
-
-        // 8. Click "x" button to close Account Settings modal
-        cy.get('#accountSettingsHeader > .close').click();
-
-        // 9. Go to channel which has any posts
-        cy.visit('/ad-1/channels/ratione-1');
-
-        // 10. Enter in text
-        cy.get('#post_textbox').type('First line');
-        cy.get('#post_textbox').type('{shift}{enter}{enter}');
-        cy.get('#post_textbox').type('Text after');
-
-        // 11. Submit post
-        cy.get('#create_post').submit();
-
-        // 12. Get last postId
+        // 3. Get last postId
         cy.getLastPostId().then((postId) => {
             const postMessageTextId = `#postMessageText_${postId}`;
 
             // * Verify HTML still includes new line
             cy.get(postMessageTextId).should('have.html', '<p>First line</p>\n<p>Text after</p>\n');
 
-            // 13. click dot menu button
+            // 4. click dot menu button
             cy.clickPostDotMenu();
 
-            // 14. click edit post
+            // 5. click edit post
             cy.get(`#edit_post_${postId}`).click();
 
-            // 15. Add ",edited" to the text
+            // 6. Add ",edited" to the text
             cy.get('#edit_textbox').type(',edited');
 
-            // 16. Save
+            // 7. Save
             cy.get('#editButton').click();
 
             // * Verify HTML includes newline and the edit
             cy.get(postMessageTextId).should('have.html', '<p>First line</p>\n<p>Text after,edited</p>\n');
 
-            // *  Post should have (edited)
-            cy.get(`#post_${postId}`).find('.post-edited__indicator');
+            // * Post should have (edited)
+            cy.get(`#postEdited_${postId}`).
+                should('be.visible').
+                should('contain', '(edited)');
         });
     });
 });
