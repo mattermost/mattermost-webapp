a336a8a7ed05d58f4044c67d6350efe5884477a7
oliverJurgen <oliver.jurgen90@gmail.com>
2019-03-12 21:51:27 +0800

    [MM-14432] use before and after hooks

--- cypress/integration/channel/autocomplete_shown_each_channel_spec.js
+++ cypress/integration/channel/autocomplete_shown_each_channel_spec.js
@@ -12,44 +12,51 @@ describe('Identical Message Drafts', () => {
         // 1. Login and go to /
         cy.login('user-1');
         cy.visit('/');
+
+        // 2. Clear channel textbox
+        cy.get('#post_textbox').clear();
     });
 
     it('M14432 shows Autocomplete in each channel', () => {
-        // 1. Go to test Channel A on sidebar
-        cy.get('#sidebarItem_town-square').scrollIntoView();
+        // 3. Go to test Channel A on sidebar
         cy.get('#sidebarItem_town-square').should('be.visible').click();
 
         // * Validate if the channel has been opened
-        cy.url().should('include', '/ad-1/channels/town-square');
+        cy.url().should('include', '/channels/town-square');
 
-        // 2. Start a draft in Channel A containing just "@"
-        cy.get('#post_textbox').scrollIntoView();
-        cy.get('#post_textbox').clear().type('@');
+        // 4. Start a draft in Channel A containing just "@"
+        cy.get('#post_textbox').type('@');
 
         // * At mention auto-complete appears in Channel A
         cy.get('#suggestionList').should('be.visible');
 
-        // 3. Go to test Channel B on sidebar
-        cy.get('#sidebarItem_ratione-1').scrollIntoView();
-        cy.get('#sidebarItem_ratione-1').should('be.visible').click();
+        // 5. Go to test Channel B on sidebar
+        cy.get('#sidebarItem_autem-2').should('be.visible').click();
 
-        //* Validate if the newly navigated channel is open
-        cy.url().should('include', '/ad-1/channels/ratione-1');
+        // * Validate if the newly navigated channel is open
+        // * autocomplete should not be visible in channel
+        // * Clear channel textbox
+        cy.url().should('include', '/channels/autem-2');
+        cy.get('#suggestionList').should('not.be.visible');
+        cy.get('#post_textbox').clear();
 
-        // 4. Start a draft in Channel B containing just "@"
-        cy.get('#post_textbox').scrollIntoView();
-        cy.get('#post_textbox').clear().type('@');
+        // 6. Start a draft in Channel B containing just "@"
+        cy.get('#post_textbox').type('@');
 
         // * At mention auto-complete appears in Channel B
         cy.get('#suggestionList').should('be.visible');
 
-        // 5. Go back to test Channel A on sidebar
-        cy.get('#sidebarItem_town-square').scrollIntoView();
+        // 7. Go back to test Channel A on sidebar
         cy.get('#sidebarItem_town-square').should('be.visible').click();
 
         // * Validate if the channel has been opened
         // * At mention auto-complete is preserved in Channel A
-        cy.url().should('include', '/ad-1/channels/town-square');
+        cy.url().should('include', '/channels/town-square');
         cy.get('#suggestionList').should('be.visible');
+    });
+
+    after(() => {
+        // 7. Clear channel textbox
+        cy.get('#post_textbox').clear();
     });
 });
