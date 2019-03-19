d80f62c4d23046f4f4184bc1c71991300d39afb9
Adzim Zul Fahmi <adzimzulfahmy@gmail.com>
2019-03-19 22:32:45 +0700

    assert final result

--- cypress/integration/emoji/recently_used_emoji_spec.js
+++ cypress/integration/emoji/recently_used_emoji_spec.js
@@ -7,7 +7,11 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-describe('Emoji', () => {
+/* eslint max-nested-callbacks: ["error", 3] */
+
+import {getRandomInt} from '../../utils';
+
+describe('MM-14014 Recently Emoji', () => {
     before(() => {
         // 1. Login and go to /
         cy.login('user-1');
@@ -15,8 +19,42 @@ describe('Emoji', () => {
     });
 
     it('M14014 Recently used emojis are shown 1st', async () => {
+        // 2. Get random emoji index
+        const emojiIndex = getRandomInt(400);
+
+        // 3. Show emoji list
         cy.get('#emojiPickerButton').should('be.visible').click();
 
-        cy.get('.emoji-picker__container').should('be.visible').children();
+        // 4. Click emoji with random index
+        cy.get('.emoji-picker__item').eq(emojiIndex).click();
+
+        // 5. Submit post
+        cy.get('#create_post').submit();
+
+        // 6. Open emoji list
+        cy.get('#emojiPickerButton').click();
+
+        // 7. Wait 500 millisecond
+        cy.wait(500); // eslint-disable-line
+
+        // 8. Get second emoji
+        const secondEmoji = getRandomInt(400);
+
+        // 9. Click chosen emoji
+        cy.get('.emoji-picker__item').eq(secondEmoji).click();
+
+        // 10. Submit post
+        cy.get('#create_post').submit();
+
+        // 11. Open emoji list
+        cy.get('#emojiPickerButton').click();
+
+        // 12. Wait 500 millisecond
+        cy.wait(500); // eslint-disable-line
+
+        // * Assert recent emoji should equal with last post emoji
+        cy.get('.emoji-picker__item').eq(secondEmoji).find('img').then(($el) => {
+            cy.get('.emoji-picker__item').eq(0).find('img').should('have.attr', 'src', $el.attr('src'));
+        });
     });
 });
