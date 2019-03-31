56613c6838e85d00e2d01912eaa42f6c6796a530
Joram Wilander <jwawilander@gmail.com>
2019-03-31 18:39:18 -0400

    Revert "MM-14489 Making SuggestionList component render at bottom of input on dialogs (#2235)" (#2559)
    This reverts commit ab31a59913253843305a987ce27666918cc0dbf2.

--- components/suggestion/suggestion_box.jsx
+++ components/suggestion/suggestion_box.jsx
@@ -3,7 +3,6 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import EventEmitter from 'mattermost-redux/utils/event_emitter';
 
@@ -171,8 +170,6 @@ export default class SuggestionBox extends React.Component {
             selection: '',
             allowDividers: true,
             presentationType: 'text',
-            dropdownPosition: 0,
-            inputWidth: 0,
         };
     }
 
@@ -269,8 +266,8 @@ export default class SuggestionBox extends React.Component {
             return;
         }
 
-        this.getDropdownPosition();
         this.setState({focused: true});
+
         if (this.props.openOnFocus || this.props.openWhenEmpty) {
             setTimeout(() => {
                 const textbox = this.getTextbox();
@@ -601,13 +598,6 @@ export default class SuggestionBox extends React.Component {
         this.container = container;
     };
 
-    getDropdownPosition = () => {
-        const input = ReactDOM.findDOMNode(this.refs.input);
-        const dropdownPosition = input.getBoundingClientRect().top;
-        const inputWidth = input.clientWidth;
-        this.setState({dropdownPosition, inputWidth});
-    }
-
     render() {
         const {
             listComponent,
@@ -671,8 +661,6 @@ export default class SuggestionBox extends React.Component {
                         terms={this.state.terms}
                         selection={this.state.selection}
                         components={this.state.components}
-                        dropdownPosition={this.state.dropdownPosition}
-                        inputWidth={this.state.inputWidth}
                     />
                 }
                 {(this.props.openWhenEmpty || this.props.value.length >= this.props.requiredCharacters) && this.state.presentationType === 'date' &&
