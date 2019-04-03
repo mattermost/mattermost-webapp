a8eefd2271b9fae4e4b5174cc75168a7a5b0a616
reflog <reflog@gmail.com>
2019-04-03 17:32:17 +0300

    test fix

--- components/integrations/bots/bots.test.jsx
+++ components/integrations/bots/bots.test.jsx
@@ -2,7 +2,6 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import {shallow} from 'enzyme';
 import {BrowserRouter} from 'react-router-dom';
 
 import TestHelper from 'tests/helpers/client-test-helper';
