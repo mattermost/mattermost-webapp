beab720075ab67e22409a217a2a665d9809c1ff1
Saturnino Abril <saturnino.abril@gmail.com>
2019-04-04 16:13:42 +0800

    MM-14873 Add API command to create and delete a team and channel (#2601)
    * add API command to create and delete a team and channel
    
    * remove unnecessary .then in commands
    
    * add return to API commands

--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {getRandomInt} from '../utils';
+
 import users from '../fixtures/users.json';
 import theme from '../fixtures/theme.json';
 
@@ -22,7 +24,7 @@ import theme from '../fixtures/theme.json';
 Cypress.Commands.add('apiLogin', (username = 'user-1') => {
     const user = users[username];
 
-    cy.request({
+    return cy.request({
         url: '/api/v4/users/login',
         method: 'POST',
         body: {login_id: user.username, password: user.password},
@@ -33,9 +35,99 @@ Cypress.Commands.add('apiLogin', (username = 'user-1') => {
  * Logout a user directly via API
  */
 Cypress.Commands.add('apiLogout', () => {
-    cy.request({
+    return cy.request({
         url: '/api/v4/users/logout',
         method: 'POST',
+    });
+});
+
+// *****************************************************************************
+// Channels
+// https://api.mattermost.com/#tag/channels
+// *****************************************************************************
+
+/**
+ * Creates a channel directly via API
+ * This API assume that the user is logged in and has cookie to access
+ * @param {String} teamId - The team ID of the team to create the channel on
+ * @param {String} name - The unique handle for the channel, will be present in the channel URL
+ * @param {String} displayName - The non-unique UI name for the channel
+ * @param {String} type - 'O' for a public channel (default), 'P' for a private channel
+ * @param {String} purpose - A short description of the purpose of the channel
+ * @param {String} header - Markdown-formatted text to display in the header of the channel
+ * All parameters required except purpose and header
+ */
+Cypress.Commands.add('apiCreateChannel', (teamId, name, displayName, type = 'O', purpose = '', header = '') => {
+    const uniqueName = `${name}-${getRandomInt(9999).toString()}`;
+
+    return cy.request({
+        headers: {'X-Requested-With': 'XMLHttpRequest'},
+        url: '/api/v4/channels',
+        method: 'POST',
+        body: {
+            team_id: teamId,
+            name: uniqueName,
+            display_name: displayName,
+            type,
+            purpose,
+            header,
+        },
+    });
+});
+
+/**
+ * Deletes a channel directly via API
+ * This API assume that the user is logged in and has cookie to access
+ * @param {String} channelId - The channel ID to be deleted
+ * All parameter required
+ */
+Cypress.Commands.add('apiDeleteChannel', (channelId) => {
+    return cy.request({
+        headers: {'X-Requested-With': 'XMLHttpRequest'},
+        url: '/api/v4/channels/' + channelId,
+        method: 'DELETE',
+    });
+});
+
+// *****************************************************************************
+// Teams
+// https://api.mattermost.com/#tag/teams
+// *****************************************************************************
+
+/**
+ * Creates a team directly via API
+ * This API assume that the user is logged in and has cookie to access
+ * @param {String} name - Unique handler for a team, will be present in the team URL
+ * @param {String} displayName - Non-unique UI name for the team
+ * @param {String} type - 'O' for open (default), 'I' for invite only
+ * All parameters required
+ */
+Cypress.Commands.add('apiCreateTeam', (name, displayName, type = 'O') => {
+    const uniqueName = `${name}-${getRandomInt(9999).toString()}`;
+
+    return cy.request({
+        headers: {'X-Requested-With': 'XMLHttpRequest'},
+        url: '/api/v4/teams',
+        method: 'POST',
+        body: {
+            name: uniqueName,
+            display_name: displayName,
+            type,
+        },
+    });
+});
+
+/**
+ * Deletes a team directly via API
+ * This API assume that the user is logged in and has cookie to access
+ * @param {String} teamId - The team ID to be deleted
+ * All parameter required
+ */
+Cypress.Commands.add('apiDeleteTeam', (teamId, permanent = false) => {
+    return cy.request({
+        headers: {'X-Requested-With': 'XMLHttpRequest'},
+        url: '/api/v4/teams/' + teamId + (permanent ? '/?permanent=true' : ''),
+        method: 'DELETE',
     });
 });
 
@@ -50,7 +142,7 @@ Cypress.Commands.add('apiLogout', () => {
  * @param {Array} preference - a list of user's preferences
  */
 Cypress.Commands.add('apiSaveUserPreference', (preferences = []) => {
-    cy.request({
+    return cy.request({
         headers: {'X-Requested-With': 'XMLHttpRequest'},
         url: '/api/v4/users/me/preferences',
         method: 'PUT',
@@ -64,7 +156,7 @@ Cypress.Commands.add('apiSaveUserPreference', (preferences = []) => {
  * @param {String} value - Either "full" (default) or "centered"
  */
 Cypress.Commands.add('apiSaveChannelDisplayModePreference', (value = 'full') => {
-    cy.getCookie('MMUSERID').then((cookie) => {
+    return cy.getCookie('MMUSERID').then((cookie) => {
         const preference = {
             user_id: cookie.value,
             category: 'display_settings',
@@ -72,7 +164,7 @@ Cypress.Commands.add('apiSaveChannelDisplayModePreference', (value = 'full') => 
             value,
         };
 
-        cy.apiSaveUserPreference([preference]);
+        return cy.apiSaveUserPreference([preference]);
     });
 });
 
@@ -82,7 +174,7 @@ Cypress.Commands.add('apiSaveChannelDisplayModePreference', (value = 'full') => 
  * @param {String} value - Either "clean" (default) or "compact"
  */
 Cypress.Commands.add('apiSaveMessageDisplayPreference', (value = 'clean') => {
-    cy.getCookie('MMUSERID').then((cookie) => {
+    return cy.getCookie('MMUSERID').then((cookie) => {
         const preference = {
             user_id: cookie.value,
             category: 'display_settings',
@@ -90,7 +182,7 @@ Cypress.Commands.add('apiSaveMessageDisplayPreference', (value = 'clean') => {
             value,
         };
 
-        cy.apiSaveUserPreference([preference]);
+        return cy.apiSaveUserPreference([preference]);
     });
 });
 
@@ -100,7 +192,7 @@ Cypress.Commands.add('apiSaveMessageDisplayPreference', (value = 'clean') => {
  * @param {Object} value - theme object.  Will pass default value if none is provided.
  */
 Cypress.Commands.add('apiSaveThemePreference', (value = JSON.stringify(theme.default)) => {
-    cy.getCookie('MMUSERID').then((cookie) => {
+    return cy.getCookie('MMUSERID').then((cookie) => {
         const preference = {
             user_id: cookie.value,
             category: 'theme',
@@ -108,6 +200,6 @@ Cypress.Commands.add('apiSaveThemePreference', (value = JSON.stringify(theme.def
             value,
         };
 
-        cy.apiSaveUserPreference([preference]);
+        return cy.apiSaveUserPreference([preference]);
     });
 });
