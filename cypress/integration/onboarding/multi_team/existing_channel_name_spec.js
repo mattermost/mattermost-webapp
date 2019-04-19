50d6119de13101b60c60e6c19e32861d8144b5ca
Prapti Shrestha <praptishrestha@gmail.com>
2019-04-19 00:01:54 -0500

    Adding new IDs and refactoring

--- cypress/integration/onboarding/multi_team/existing_channel_name_spec.js
+++ cypress/integration/onboarding/multi_team/existing_channel_name_spec.js
@@ -9,98 +9,81 @@
 
 /*eslint max-nested-callbacks: ["error", 3]*/
 
+/*
+function: verifyExistingChannelError
+arg - channelTypeID:        ID of public or private channel to create
+arg - newChannelName:       New channel name to assign
+
+This function clicks on #createPublicChannel or #createPrivateChannel.
+Types `newChannelName` provided in the #newChannelName input.
+Clicks the 'Create New Channel' button.
+Then verifies that an error message 'A channel with that name already exists on the same team' is shown.
+*/
+function verifyExistingChannelError(channelTypeID, newChannelName) {
+    // Click on '+' button for Public or Private Channel
+    cy.get(channelTypeID).click();
+
+    // Type `newChannelName` in the input field for new channel
+    cy.get('#newChannelName').type(newChannelName);
+
+    // Click 'Create New Channel' button
+    cy.get('#submitNewChannel').click();
+
+    // * User gets a message saying "A channel with that name already exists on the same team"
+    cy.get('#createChannelError').contains('A channel with that name already exists on the same team');
+}
+
+/*
+function: channelNameTest
+arg - channelTypeHeading:   PUBLIC CHANNELS or PRIVATE CHANNELS
+arg - channelName:          Existing channel name that is also being tested for error
+
+var - origChannelLength:    The original number of channels in PUBLIC CHANNELS or PRIVATE CHANNELS
+
+This function stores the number of channels in public or private channels as origChannelLength.
+Verifies that the channel name picked exists.
+Attempts creating a public channel with the existing `channelName`, which should return error as implemented in `verifyExistingChannelError`.
+Then atempts createing a private channel with the existing `channelName`, which should return error as implemented in `verifyExistingChannelError`.
+Verifies that the number of channels under PUBLIC and PRIVATE CHANNELS are still the same.
+Closes out of the New Channel Modal.
+*/
+function channelNameTest(channelTypeHeading, channelName) {
+    // 1. Find how many public channels there are and store as origChannelLength
+    cy.get('#sidebarChannelContainer').children().contains(channelTypeHeading).parent().parent().siblings().its('length').then((origChannelLength) => {
+        // * Verify channel `channelName` exists
+        cy.get('#sidebarChannelContainer').should('contain', channelName);
+
+        // * Verify new public channel cannot be created with existing public channel name; see verifyExistingChannelError function
+        verifyExistingChannelError('#createPublicChannel', channelName);
+
+        // 2. Click on Cancel button to move out of New Channel Modal
+        cy.get('#cancelNewChannel').contains('Cancel').click();
+
+        // * Verify new private channel cannot be created with existing public channel name:
+        verifyExistingChannelError('#createPrivateChannel', channelName);
+
+        // * Verify the number of channels is still the same as before (by comparing it to origChannelLenth)
+        cy.get('#sidebarChannelContainer').children().contains(channelTypeHeading).parent().parent().siblings().its('length').should('eq', origChannelLength);
+
+        // 3. Click on Cancel button to move out of New Channel Modal
+        cy.get('#cancelNewChannel').contains('Cancel').click();
+    });
+}
+
 describe('Channel', () => {
     before(() => {
-        // 1. Login and go to /
+        // Login and go to /
         cy.apiLogin('user-1');
         cy.visit('/');
     });
 
-    // * Verify new public or private channel cannot be created with existing public channel name:
     it('M14635 Should not create new channel with existing public channel name', () => {
-        // 2. Find how many public channels there are and store as origPublicChannelLenth
-        cy.get('#sidebarChannelContainer').children().contains('PUBLIC CHANNELS').parent().parent().siblings().its('length').then(($origPublicChannelLength) => {
-            // * Verify new public channel cannot be created with existing public channel name:
-            // 3. Verify channel with name 'Town Square' exists
-            cy.get('#sidebarChannelContainer').should('contain', 'Town Square');
-
-            // 4. Click on '+' button for Public Channel
-            cy.get('#createPublicChannel').click();
-
-            // 5. Type 'Town Square' in the input field for new public channel
-            cy.get('#newPublicChannelName').type('Town Square');
-
-            //6. Click 'Create New Channel' button
-            cy.get('button[type=submit]').click();
-
-            // * User gets a message saying "A channel with that name already exists on the same team"
-            cy.get('p').contains('A channel with that name already exists on the same team');
-
-            // 7. Click on Cancel button to move out of New Channel dialog
-            cy.get('button').contains('Cancel').click();
-
-            // * Verify new private channel cannot be created with existing public channel name:
-            // 8. Click on '+' button for Private Channel
-            cy.get('#createPrivateChannel').click();
-
-            // 9. Type 'Town Square' in the input field for new private channel
-            cy.get('#newPrivateChannelName').type('Town Square');
-
-            // 10. Click 'Create New Channel' button
-            cy.get('button[type=submit]').click();
-
-            // * User gets a message saying "A channel with that name already exists on the same team"
-            cy.get('p').contains('A channel with that name already exists on the same team');
-
-            // * Verify the number of Public Channels is still the same as before (by comparing it to origPublicChannelLenth)
-            cy.get('#sidebarChannelContainer').children().contains('PUBLIC CHANNELS').parent().parent().siblings().its('length').should('eq', $origPublicChannelLength);
-        });
-
-        // 11. Exit out of the New Channel dialog
-        cy.get('button').contains('Cancel').click();
+        // * Verify new public or private channel cannot be created with existing public channel name:
+        channelNameTest('PUBLIC CHANNELS', 'Town Square');
     });
 
-    // * Verify new public or private channel cannot be created with existing private channel name:
     it('Should not create new channel with existing private channel name', () => {
-        // 12. Find how many public channels there are and store as origPrivateChannelLenth
-        cy.get('#sidebarChannelContainer').children().contains('PRIVATE CHANNELS').parent().parent().siblings().its('length').then(($origPrivateChannelLength) => {
-            // * Verify new public channel cannot be created with existing private channel name:
-            // 13. Verify a private channel
-            cy.get('#sidebarItem_autem-2').should('contain', 'commodi');
-
-            // 14. Click on '+' button for Public Channel
-            cy.get('#createPublicChannel').click();
-
-            // 15. Type 'commodi' in the input field for new public channel
-            cy.get('#newPublicChannelName').type('commodi');
-
-            // 16. Click 'Create New Channel' button
-            cy.get('button[type=submit]').click();
-
-            // * User gets a message saying "A channel with that name already exists on the same team"
-            cy.get('p').contains('A channel with that name already exists on the same team');
-
-            // 17. Click on Cancel button to move out of New Channel dialog
-            cy.get('button').contains('Cancel').click();
-
-            // * Verify new private channel cannot be created with existing private channel name:
-            // 18. Click on '+' button for Private Channel
-            cy.get('#createPrivateChannel').click();
-
-            // 19. Type 'commodi' in the input field for new private channel
-            cy.get('#newPrivateChannelName').type('commodi');
-
-            // 20. Click 'Create New Channel' button
-            cy.get('button[type=submit]').click();
-
-            // * User gets a message saying "A channel with that name already exists on the same team"
-            cy.get('p').contains('A channel with that name already exists on the same team');
-
-            // * Verify the number of Private Channels is still the same as before (by comparing it to origPrivateChannelLenth)
-            cy.get('#sidebarChannelContainer').children().contains('PRIVATE CHANNELS').parent().parent().siblings().its('length').should('eq', $origPrivateChannelLength);
-
-            // 21. Exit out of the New Channel dialog
-            cy.get('button').contains('Cancel').click();
-        });
+        // * Verify new public or private channel cannot be created with existing private channel name:
+        channelNameTest('PRIVATE CHANNELS', 'commodi');
     });
 });
