4e20c702c9fd14d07dfc4375bd18082c38c1e91b
Prapti Shrestha <praptishrestha@gmail.com>
2019-04-23 21:11:04 -0500

    Using unique email and username for test reusability

--- cypress/integration/onboarding/email/existing_email_adress_spec.js
+++ cypress/integration/onboarding/email/existing_email_adress_spec.js
@@ -1,35 +1,39 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {getRandomInt} from '../../../utils';
+
 // ***************************************************************
 // - [number] indicates a test step (e.g. 1. Go to a page)
 // - [*] indicates an assertion (e.g. * Check the title)
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-function signupWithEmail(email, name, pw) {
-    // 2. Go to /login
+const uniqueUserId = getRandomInt(99999);
+
+function signupWithEmail(name, pw) {
+    // 1. Go to /login
     cy.visit('/login');
 
-    // 3. Click on sign up button
+    // 2. Click on sign up button
     cy.get('#signup').click();
 
-    // 4. Type 'unique.email@sample.mattermost.com' for email
-    cy.get('#email').type(email);
+    // 3. Type email address (by adding the uniqueUserId in the email address)
+    cy.get('#email').type('unique.' + uniqueUserId + '@sample.mattermost.com');
 
-    // 5. Type 'unique-1' for username
+    // 4. Type 'unique-1' for username
     cy.get('#name').type(name);
 
-    // 6. Type 'unique1pw' for password
+    // 5. Type 'unique1pw' for password
     cy.get('#password').type(pw);
 
-    // 7. Click on Create Account button
+    // 6. Click on Create Account button
     cy.get('#createAccountButton').click();
 }
 
 describe('Email Address', () => {
     before(() => {
-        // 1. Go to / and logout
+        // Before test, login as sysadmin, set Enable Open Server to true then logout
         cy.apiLogin('sysadmin');
         cy.apiEnableOpenServer(true);
         cy.apiLogout();
@@ -42,7 +46,8 @@ describe('Email Address', () => {
     });
 
     it('M14634 Should not create account with an existing email address', () => {
-        signupWithEmail('unique.email@sample.mattermost.com', 'unique-1', 'unique1pw');
+        // 7. Signup a new user with an email address and user generated in signupWithEmail
+        signupWithEmail('unique.' + uniqueUserId, 'unique1pw');
 
         // * Verify there is Logout Button
         cy.contains('Logout').should('be.visible');
@@ -53,11 +58,9 @@ describe('Email Address', () => {
         // * Verify the link to create a new team is available
         cy.get('#createNewTeamLink').should('have.attr', 'href', '/create_team').and('be.visible', 'contain', 'Create a new team');
 
-        // Create another user with the same email but different username and password:
-        // 8. cy.apiLogout()
+        // 8. Logout and signup another user with the same email but different username and password
         cy.apiLogout();
-
-        signupWithEmail('unique.email@sample.mattermost.com', 'unique-2', 'unique2pw');
+        signupWithEmail('unique-2', 'unique2pw');
 
         // * Error message displays below the Create Account button that says "An account with that email already exists"
         cy.get('#existingEmailErrorContainer').should('contain', 'An account with that email already exists');
