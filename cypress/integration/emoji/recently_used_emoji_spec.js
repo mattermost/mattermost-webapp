83f587fbd7c3c325a35a26fad808ba61524991de
Adzim Zul Fahmi <adzimzulfahmy@gmail.com>
2019-04-27 17:42:52 +0700

    delete recent emoji on local storage

--- cypress/integration/emoji/recently_used_emoji_spec.js
+++ cypress/integration/emoji/recently_used_emoji_spec.js
@@ -17,40 +17,43 @@ describe('Recent Emoji', () => {
     });
 
     it('M14014 Recently used emojis are shown 1st', async () => {
-        // 2. Get random emoji index
+        // 2 before test delete all recent emoji on local storage
+        cy.clearLocalStorage(/recent_emojis/);
+
+        // 3. Get random emoji index
         const firstEmoji = 200;
 
-        // 3. Show emoji list
+        // 4. Show emoji list
         cy.get('#emojiPickerButton').should('be.visible').click();
 
-        // 4. Click emoji with random index
+        // 5. Click emoji with random index
         cy.get('.emoji-picker__item').eq(firstEmoji).click();
 
-        // 5. Submit post
+        // 6. Submit post
         cy.get('#create_post').submit();
 
-        // 6. Wait 500 millisecond
+        // 7. Wait 500 millisecond
         cy.wait(500); // eslint-disable-line
 
-        // 7. Post reaction to post
+        // 8. Post reaction to post
         cy.clickPostReactionIcon();
 
-        // 8. Get second emoji
+        // 9. Get second emoji
         const secondEmoji = 100;
 
-        // 9. Click chosen emoji
+        // 10. Click chosen emoji
         cy.get('.emoji-picker__item').eq(secondEmoji).click();
 
-        // 10. Show emoji list
+        // 11. Show emoji list
         cy.get('#emojiPickerButton').click();
 
         // * Assert first emoji should equal with second recent emoji
-        cy.get('.emoji-picker__item').eq(firstEmoji).find('img').then(($el) => {
+        cy.get('.emoji-picker__item').eq(firstEmoji + 2).find('img').then(($el) => {
             cy.get('.emoji-picker__item').eq(1).find('img').should('have.attr', 'class', $el.attr('class'));
         });
 
         // * Assert second emoji should equal with first recent emoji
-        cy.get('.emoji-picker__item').eq(secondEmoji).find('img').then(($el) => {
+        cy.get('.emoji-picker__item').eq(secondEmoji + 1).find('img').then(($el) => {
             cy.get('.emoji-picker__item').eq(0).find('img').should('have.attr', 'class', $el.attr('class'));
         });
     });
