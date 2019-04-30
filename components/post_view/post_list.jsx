9f47208b15c3478b36586e1fe6f17aad2e554a02
Sudheer <sudheer.105@gmail.com>
2019-04-30 14:43:04 +0530

    MM-14949 Correct scroll when new posts are loaded (#2687)
    * MM-14949 Correct scroll when new posts are loaded
    * Check for state change in posts instead of props
    * Correct scroll when posts change at top or when header changes
    * use snapshot for taking height before updating postslist
    * Downgrade enzyme

--- components/post_view/post_list.jsx
+++ components/post_view/post_list.jsx
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import AutoSizer from 'react-virtualized-auto-sizer';
 import {DynamicSizeList} from 'react-window';
+import {debounce} from 'mattermost-redux/actions/helpers';
 
 import LoadingScreen from 'components/loading_screen.jsx';
 
@@ -24,6 +25,18 @@ const MAX_EXTRA_PAGES_LOADED = 10;
 const OVERSCAN_COUNT_BACKWARD = window.OVERSCAN_COUNT_BACKWARD || 50; // Exposing the value for PM to test will be removed soon
 const OVERSCAN_COUNT_FORWARD = window.OVERSCAN_COUNT_FORWARD || 100; // Exposing the value for PM to test will be removed soon
 const HEIGHT_TRIGGER_FOR_MORE_POSTS = window.HEIGHT_TRIGGER_FOR_MORE_POSTS || 1000; // Exposing the value for PM to test will be removed soon
+
+const postListHeightChangeForPadding = 21;
+
+const postListStyle = {
+    padding: '14px 0px 7px', //21px of height difference from autosized list compared to DynamicSizeList. If this is changed change the above variable postListHeightChangeForPadding accordingly
+};
+
+const virtListStyles = {
+    position: 'absolute',
+    bottom: '0',
+    maxHeight: '100%',
+};
 
 export default class PostList extends React.PureComponent {
     static propTypes = {
@@ -101,6 +114,7 @@ export default class PostList extends React.PureComponent {
         };
 
         this.listRef = React.createRef();
+        this.postlistRef = React.createRef();
         if (isMobile) {
             this.scrollStopAction = new DelayedAction(this.handleScrollStop);
         }
@@ -116,9 +130,40 @@ export default class PostList extends React.PureComponent {
         window.addEventListener('resize', this.handleWindowResize);
     }
 
-    componentDidUpdate(prevProps) {
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        if (this.postlistRef && this.postlistRef.current) {
+            const postsAddedAtTop = this.state.postListIds.length !== prevState.postListIds.length && this.state.postListIds[0] === prevState.postListIds[0];
+            const channelHeaderAdded = this.state.atEnd !== prevState.atEnd && this.state.postListIds.length === prevState.postListIds.length;
+            if (postsAddedAtTop || channelHeaderAdded) {
+                const previousScrollTop = this.postlistRef.current.scrollTop;
+                const previousScrollHeight = this.postlistRef.current.scrollHeight;
+
+                return {
+                    previousScrollTop,
+                    previousScrollHeight,
+                };
+            }
+        }
+        return null;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
         if (prevProps.channelLoading && !this.props.channelLoading) {
             this.loadPosts(this.props.channel.id, this.props.focusedPostId);
+        }
+
+        if (!this.postlistRef.current || !snapshot) {
+            return;
+        }
+
+        const postlistScrollHeight = this.postlistRef.current.scrollHeight;
+        const postsAddedAtTop = this.state.postListIds.length !== prevState.postListIds.length && this.state.postListIds[0] === prevState.postListIds[0];
+        const channelHeaderAdded = this.state.atEnd !== prevState.atEnd && this.state.postListIds.length === prevState.postListIds.length;
+        if (postsAddedAtTop || channelHeaderAdded) {
+            const scrollValue = snapshot.previousScrollTop + (postlistScrollHeight - snapshot.previousScrollHeight);
+            if (scrollValue !== 0 && (scrollValue - snapshot.previousScrollTop) !== 0) {
+                this.listRef.current.scrollTo(scrollValue, scrollValue - snapshot.previousScrollTop, !this.state.atEnd);
+            }
         }
     }
 
@@ -196,7 +241,7 @@ export default class PostList extends React.PureComponent {
         if (error) {
             if (this.autoRetriesCount < MAX_NUMBER_OF_AUTO_RETRIES) {
                 this.autoRetriesCount++;
-                this.loadMorePosts();
+                debounce(this.loadMorePosts());
             } else if (this.mounted) {
                 this.setState({autoRetryEnable: false});
             }
@@ -317,8 +362,8 @@ export default class PostList extends React.PureComponent {
         visibleStartIndex,
         visibleStopIndex,
     }) => {
-        this.updateFloatingTimestamp(visibleStopIndex);
-        this.checkBottom(visibleStartIndex);
+        this.updateFloatingTimestamp(visibleStartIndex);
+        this.checkBottom(visibleStopIndex);
     }
 
     initScrollToIndex = () => {
@@ -443,7 +488,7 @@ export default class PostList extends React.PureComponent {
                                 {({height, width}) => (
                                     <DynamicSizeList
                                         ref={this.listRef}
-                                        height={height}
+                                        height={height - postListHeightChangeForPadding}
                                         width={width}
                                         className='post-list__dynamic'
                                         itemCount={this.state.postListIds.length}
@@ -454,10 +499,11 @@ export default class PostList extends React.PureComponent {
                                         onScroll={this.onScroll}
                                         onItemsRendered={this.onItemsRendered}
                                         initScrollToIndex={this.initScrollToIndex}
-                                        onNewItemsMounted={this.onNewItemsMounted}
                                         canLoadMorePosts={this.canLoadMorePosts}
                                         skipResizeClass='col__reply'
-                                        style={dynamicListStyle}
+                                        innerRef={this.postlistRef}
+                                        style={{...virtListStyles, ...dynamicListStyle}}
+                                        innerListStyle={postListStyle}
                                     >
                                         {this.renderRow}
                                     </DynamicSizeList>
