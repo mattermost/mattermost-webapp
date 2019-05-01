5e0699e358fb97df737c080266a5b4a8f8b1eb23
Your Name <jesse.hallam@gmail.com>
2019-05-01 12:45:13 -0400

    update comments re: clearing cookies

--- actions/views/root.js
+++ actions/views/root.js
@@ -70,8 +70,9 @@ export function loadTranslations(locale, url) {
 
 export function clearUserCookie() {
     // We need to clear the cookie without the domain, with the domain, and with both the domain and path set because we
-    // can't tell if the server set the cookie with or without the domain. At this time, the domain will be set if
-    // ServiceSettings.EnableCookiesForSubdomains is true and path will be set if Mattermost is also served from a subpath.
+    // can't tell if the server set the cookie with or without the domain.
+    // The server will have set the domain if ServiceSettings.EnableCookiesForSubdomains is true
+    // The server will have set a non-default path if Mattermost is also served from a subpath.
     document.cookie = 'MMUSERID=;expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/';
     document.cookie = `MMUSERID=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=${window.location.hostname};path=/`;
     document.cookie = `MMUSERID=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=${window.location.hostname};path=${window.basename}`;
