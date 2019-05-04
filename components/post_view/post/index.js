60aad9f886c0e579421e719353051a18a208c244
Scott Davis <skawtus@gmail.com>
2019-05-03 21:17:13 -0700

    new post.props 'from_bot'

--- components/post_view/post/index.js
+++ components/post_view/post/index.js
@@ -8,7 +8,7 @@ import {createSelector} from 'reselect';
 import {Posts} from 'mattermost-redux/constants';
 import {getPost, makeIsPostCommentMention} from 'mattermost-redux/selectors/entities/posts';
 import {get} from 'mattermost-redux/selectors/entities/preferences';
-import {getUser, getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
+import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
 import {isPostEphemeral, isSystemMessage} from 'mattermost-redux/utils/post_utils';
 
 import {selectPost} from 'actions/views/rhs';
@@ -37,7 +37,6 @@ function makeMapStateToProps() {
 
     return (state, ownProps) => {
         const post = ownProps.post || getPost(state, ownProps.postId);
-        const user = getUser(state, post.user_id);
 
         let previousPost = null;
         if (ownProps.previousPostId) {
@@ -61,7 +60,6 @@ function makeMapStateToProps() {
 
         return {
             post,
-            isBot: (user && user.is_bot),
             currentUserId: getCurrentUserId(state),
             isFirstReply,
             consecutivePostByUser,
--- components/post_view/post/post.jsx
+++ components/post_view/post/post.jsx
@@ -68,11 +68,6 @@ export default class Post extends React.PureComponent {
          */
         replyCount: PropTypes.number,
 
-        /**
-         * If is a bot post
-         */
-        isBot: PropTypes.bool,
-
         actions: PropTypes.shape({
             selectPost: PropTypes.func.isRequired,
         }).isRequired,
@@ -122,8 +117,6 @@ export default class Post extends React.PureComponent {
 
         if (props.isFirstReply) {
             return false;
-        } else if (props.isBot) {
-            return false;
         } else if (!post.root_id && !props.previousPostIsComment && props.consecutivePostByUser) {
             return true;
         } else if (post.root_id) {
@@ -133,7 +126,7 @@ export default class Post extends React.PureComponent {
         return false;
     }
 
-    getClassName = (post, isSystemMessage, fromWebhook, fromAutoResponder) => {
+    getClassName = (post, isSystemMessage, fromWebhook, fromAutoResponder, fromBot) => {
         let className = 'post';
 
         if (post.failed || post.state === Posts.POST_DELETED) {
@@ -145,7 +138,7 @@ export default class Post extends React.PureComponent {
         }
 
         let rootUser = '';
-        if (this.state.sameRoot) {
+        if (this.state.sameRoot && !fromBot) {
             rootUser = 'same--root';
         } else {
             rootUser = 'other--root';
@@ -218,9 +211,10 @@ export default class Post extends React.PureComponent {
         const isSystemMessage = PostUtils.isSystemMessage(post);
         const fromAutoResponder = PostUtils.fromAutoResponder(post);
         const fromWebhook = post && post.props && post.props.from_webhook === 'true';
+        const fromBot = post && post.props && post.props.from_bot === 'true';
 
         let profilePic;
-        const hideProfilePicture = this.state.sameRoot && this.props.consecutivePostByUser && (!post.root_id && this.props.replyCount === 0);
+        const hideProfilePicture = this.state.sameRoot && this.props.consecutivePostByUser && (!post.root_id && this.props.replyCount === 0) && !fromBot;
         if (!hideProfilePicture) {
             profilePic = (
                 <PostProfilePicture
@@ -248,7 +242,7 @@ export default class Post extends React.PureComponent {
             <div
                 ref={this.getRef}
                 id={'post_' + post.id}
-                className={this.getClassName(post, isSystemMessage, fromWebhook, fromAutoResponder)}
+                className={this.getClassName(post, isSystemMessage, fromWebhook, fromAutoResponder, fromBot)}
                 onMouseOver={this.setHover}
                 onMouseLeave={this.unsetHover}
                 onTouchStart={this.setHover}
