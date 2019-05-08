a90c0309dbee312a1afa66e623eff1a2a9af02a4
Cooper Trowbridge <cooper.trowbridge@gmail.com>
2019-05-08 14:10:25 -0600

    make sure DMs are non unread, change to find to avoid multiple click error

--- cypress/integration/channel/channel_settings_spec.js
+++ cypress/integration/channel/channel_settings_spec.js
@@ -16,11 +16,11 @@ describe('Channel Settings', () => {
     });
 
     it('C15052 All channel types have appropriate close button', () => {
-        cy.get('#publicChannel').parent().parent().children('li:not(:first-child)').not('#moreButton').each(($el) => {
+        cy.get('#publicChannel').parent().parent().children('li:not(:first-child)').not('#morePublicButton').each(($el) => {
             cy.wrap($el).find('a').find('span.btn-close').should('have.length', 0);
         });
 
-        cy.get('#privateChannel').parent().parent().children('li:not(:first-child)').not('#moreButton').each(($el) => {
+        cy.get('#privateChannel').parent().parent().children('li:not(:first-child)').not('#moreRecentAlphaButton').each(($el) => {
             cy.wrap($el).find('a').find('span.btn-close').should('have.length', 0);
         });
 
@@ -29,14 +29,20 @@ describe('Channel Settings', () => {
         cy.get('.more-modal__row.clickable').first().click();
         cy.get('#saveItems').click();
 
-        cy.get('#directChannel').parent().parent().children('li:not(:first-child)').not('#moreButton').each(($el) => {
+        // click on all the messages to make sure there are none left unread
+        cy.get('#directChannel').parent().parent().children('li:not(:first-child)').not('#moreDMButton').each(($el) => {
+            cy.wrap($el).click();
+        });
+
+        // check for the close button
+        cy.get('#directChannel').parent().parent().children('li:not(:first-child)').not('#moreDMButton').each(($el) => {
             cy.wrap($el).find('a').find('span.btn-close').should('have.length', 1);
         });
     });
 
     after(() => {
         // cleanup one direct message that was created above
-        cy.get('#directChannel').parent().parent().children('li:not(:first-child)').not('#moreButton').
-            first().get('span.btn-close').click({force: true});
+        cy.get('#directChannel').parent().parent().children('li:not(:first-child)').not('#moreDMButton').
+            first().find('span.btn-close').click({force: true});
     });
 });
