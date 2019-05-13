3c57d59c7192260816d9eb1eb1d1f9d26b0fba25
Sudheer <sudheer.105@gmail.com>
2019-05-13 21:07:10 +0530

    Revert "Revert "MM-14975 remove virt_list for IE11 (#2747)"" (#2779)
    * Revert "Revert "MM-14975 remove virt_list for IE11 (#2747)" (#2774)"
    
    This reverts commit 496360e53c13515edb1b36d0bf56e682f34a2395.
    
    * MM-15516  Web: New messages separator is missing
    
    * Add lastViewedAt from state to preparePostIdsForPostList for New messages separator

---
+++ components/post_view/post_list_ie/index.js
@@ -0,0 +1,31 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import {connect} from 'react-redux';
+
+import {makeGetPostsAroundPost, makeGetPostsInChannel} from 'mattermost-redux/selectors/entities/posts';
+import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
+
+import PostList from './post_list_ie.jsx';
+
+function makeMapStateToProps() {
+    const getPostsInChannel = makeGetPostsInChannel();
+    const getPostsAroundPost = makeGetPostsAroundPost();
+
+    return function mapStateToProps(state, ownProps) {
+        let posts;
+        if (ownProps.focusedPostId) {
+            posts = getPostsAroundPost(state, ownProps.focusedPostId, ownProps.channelId);
+        } else {
+            posts = getPostsInChannel(state, ownProps.channelId, ownProps.postVisibility);
+        }
+
+        return {
+            posts,
+            currentUserId: getCurrentUserId(state),
+            lastViewedAt: state.views.channel.lastChannelViewTime[ownProps.channelId],
+        };
+    };
+}
+
+export default connect(makeMapStateToProps)(PostList);
--- components/sidebar_right/sidebar_right.jsx
+++ components/sidebar_right/sidebar_right.jsx
@@ -8,6 +8,7 @@ import classNames from 'classnames';
 import {trackEvent} from 'actions/diagnostics_actions.jsx';
 import Constants from 'utils/constants.jsx';
 import * as Utils from 'utils/utils.jsx';
+
 import FileUploadOverlay from 'components/file_upload_overlay.jsx';
 import RhsThread from 'components/rhs_thread';
 import SearchBar from 'components/search_bar';
@@ -28,6 +29,7 @@ export default class SidebarRight extends React.PureComponent {
         actions: PropTypes.shape({
             setRhsExpanded: PropTypes.func.isRequired,
             showPinnedPosts: PropTypes.func.isRequired,
+            scrollPostList: PropTypes.func.isRequired,
         }),
     };
 
@@ -37,6 +39,9 @@ export default class SidebarRight extends React.PureComponent {
 
         if (!wasOpen && isOpen) {
             trackEvent('ui', 'ui_rhs_opened');
+            if (Utils.disableVirtList()) {
+                setTimeout(this.props.actions.scrollPostList, 0);
+            }
         }
 
         const {actions, isPinnedPosts, channel} = this.props;
