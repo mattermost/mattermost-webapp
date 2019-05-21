ca53a07aea1f8927a9cb14585e8518ecb8acfd5f
Mariale <marialexandra@gmail.com>
2019-05-21 10:51:02 -0400

    Updated and added RHS tests for plugin case

--- reducers/views/rhs.test.js
+++ reducers/views/rhs.test.js
@@ -14,11 +14,13 @@ describe('Reducers.RHS', () => {
         rhsState: null,
         searchTerms: '',
         searchResultsTerms: '',
+        pluginId: '',
         isSearchingFlaggedPost: false,
         isSearchingPinnedPost: false,
         isMenuOpen: false,
         isSidebarOpen: false,
         isSidebarExpanded: false,
+
     };
 
     test('Initial state', () => {
@@ -44,6 +46,24 @@ describe('Reducers.RHS', () => {
             ...initialState,
             selectedChannelId: '123',
             rhsState: RHSStates.PIN,
+            isSidebarOpen: true,
+        });
+    });
+
+    test('should match RHS state to plugin id', () => {
+        const nextState = rhsReducer(
+            {},
+            {
+                type: ActionTypes.UPDATE_RHS_STATE,
+                state: RHSStates.PLUGIN,
+                pluginId: '123',
+            }
+        );
+
+        expect(nextState).toEqual({
+            ...initialState,
+            pluginId: '123',
+            rhsState: RHSStates.PLUGIN,
             isSidebarOpen: true,
         });
     });
