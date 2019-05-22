5c7cfba955aab71872b7e6ed81819f647df4da23
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2019-05-22 18:50:06 +0500

    MM-15001 - Updating system console structure (#2761)
    * MM-15001 - Updating system console structure
    
    * Updating content wrapper for system console
    
    * Fixing errors
    
    * Updating changes to sys console pages
    
    * Updating permissions page
    
    * Updating permissions
    
    * Updating tests
    
    * Updating permissions file
    
    * Updating permisisons and audit pages

--- components/admin_console/admin_settings.jsx
+++ components/admin_console/admin_settings.jsx
@@ -191,15 +191,15 @@ export default class AdminSettings extends React.Component {
 
     render() {
         return (
-            <div className='wrapper--fixed'>
-                <AdminHeader>
-                    {this.renderTitle()}
-                </AdminHeader>
-                <form
-                    className='form-horizontal'
-                    role='form'
-                    onSubmit={this.handleSubmit}
-                >
+            <form
+                className='form-horizontal'
+                role='form'
+                onSubmit={this.handleSubmit}
+            >
+                <div className='wrapper--fixed'>
+                    <AdminHeader>
+                        {this.renderTitle()}
+                    </AdminHeader>
                     {this.renderSettings()}
                     <div className='admin-console-save'>
                         <SaveButton
@@ -227,8 +227,8 @@ export default class AdminSettings extends React.Component {
                             </Tooltip>
                         </Overlay>
                     </div>
-                </form>
-            </div>
+                </div>
+            </form>
         );
     }
 }
--- components/analytics/system_analytics/system_analytics.jsx
+++ components/analytics/system_analytics/system_analytics.jsx
@@ -368,16 +368,20 @@ export default class SystemAnalytics extends React.PureComponent {
                     id='analytics.system.title'
                     defaultMessage='System Statistics'
                 />
-                {banner}
-                <div className='row'>
-                    {firstRow}
-                    {secondRow}
-                    {thirdRow}
-                    {advancedStats}
+                <div className='admin-console__wrapper'>
+                    <div className='admin-console__content'>
+                        {banner}
+                        <div className='row'>
+                            {firstRow}
+                            {secondRow}
+                            {thirdRow}
+                            {advancedStats}
+                        </div>
+                        {advancedGraphs}
+                        {postTotalGraph}
+                        {activeUserGraph}
+                    </div>
                 </div>
-                {advancedGraphs}
-                {postTotalGraph}
-                {activeUserGraph}
             </div>
         );
     }
