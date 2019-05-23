1b94147eadcb26d878ebcb4c637552f7356a2876
Saturnino Abril <saturnino.abril@gmail.com>
2019-05-23 19:13:15 +0800

    use the inbucket mailbox api

--- cypress/integration/email/mention_email_notification_spec.js
+++ cypress/integration/email/mention_email_notification_spec.js
@@ -10,61 +10,78 @@
 /* eslint max-nested-callbacks: ["error", 4] */
 
 import users from '../../fixtures/users.json';
+import * as TIMEOUTS from '../../fixtures/timeouts';
 
-const cypressConfig = require('../../../cypress.json');
+const reUrl = /(https?:\/\/[^ ]*)/;
 
 const user1 = users['user-1'];
 const user2 = users['user-2'];
 
-const text = `@${user2.username}`;
+const text = `Hello @${user2.username}`;
+const feedbackEmail = 'test@example.com';
 
 describe('Email notification', () => {
-    it('post a message that mentions a user', () => {
-        cy.apiLogin('user-1');
-        cy.visit('/');
-        cy.postMessage(`${text}{enter}`);
+    before(() => {
+        cy.apiUpdateConfig({EmailSettings: {FeedbackEmail: feedbackEmail}});
     });
 
-    it('sends email notification with correct email contents', () => {
-        cy.visit(`${cypressConfig.localEmailUrl}`);
-        cy.url().should('include', `${cypressConfig.localEmailUrl}`);
+    it('post a message that mentions a user', () => {
+        cy.apiLogin('user-1');
+        cy.visit('/ad-1/channels/town-square');
+        cy.postMessage(`${text}{enter}`);
 
-        verifyEmailNotification('eligendi', 'Town Square', user2, user1, text);
+        // Wait for a while to ensure that email notification is sent.
+        cy.wait(TIMEOUTS.SMALL);
+
+        cy.task('getRecentEmail', {username: 'user-2'}).then((response) => {
+            verifyEmailNotification(response, 'eligendi', 'Town Square', user2, user1, text, feedbackEmail);
+
+            const bodyText = response.data.body.text.split('\n');
+
+            const permalink = bodyText[9].match(reUrl)[0];
+            const permalinkPostId = permalink.split('/')[5];
+            cy.visit(permalink);
+
+            const postText = `#postMessageText_${permalinkPostId}`;
+            cy.get(postText).should('have.text', text);
+
+            cy.getLastPostId().then((postId) => {
+                // * Should match last post and permalink post IDs
+                expect(permalinkPostId).to.equal(postId);
+            });
+        });
     });
 });
 
-function verifyEmailNotification(teamName, channelDisplayName, mentionedUser, byUser, message, feedbackEmail = 'test@example.com') {
+function verifyEmailNotification(response, teamDisplayName, channelDisplayName, mentionedUser, byUser, message, fromEmail) {
     const isoDate = new Date().toISOString().substring(0, 10);
+    const {data, status} = response;
 
-    const emailName = mentionedUser.emailname || mentionedUser.username;
+    // * Should return success status
+    expect(status).to.equal(200);
 
-    // # Visit local email provider and go directly to a user
-    cy.visit(`${cypressConfig.localEmailUrl}/mailbox?name=${emailName}`);
+    // * Verify that email is addressed to user-2
+    expect(data.to[0]).to.contain(mentionedUser.email);
 
-    // # Click the most recent message
-    cy.get('#message-list').children().last().click();
+    // * Verify that email is from default feedback email
+    expect(data.from).to.contain(fromEmail);
 
-    // * Verify text contents of email header
-    cy.get('.panel-body').should('be.visible').within(() => {
-        cy.get('dl dt').first().should('have.text', 'From:');
-        cy.get('dl dd').first().should('have.text', `<${feedbackEmail}>`);
+    // * Verify that date is current
+    expect(data.date).to.contain(isoDate);
 
-        cy.get('dl dt').eq(1).should('have.text', 'To:');
-        cy.get('dl dd').eq(1).should('contain', `<${mentionedUser.email}>`);
+    // * Verify that the email subject is correct
+    expect(data.subject).to.contain(`[Mattermost] Notification in ${teamDisplayName}`);
 
-        cy.get('dl dt').eq(2).should('have.text', 'Date:');
-        cy.get('dl dd').eq(2).should('contain', `${isoDate}`);
-
-        cy.get('dl dt').last().should('have.text', 'Subject:');
-        cy.get('dl dd').last().should('contain', `[Mattermost] Notification in ${teamName}`);
-    });
-
-    // * Verify text contents of email body
-    cy.get('.message-body').
-        should('contain', 'You have a new notification.').
-        and('contain', `@${byUser.username}`).
-        and('contain', `Channel: ${channelDisplayName}`).
-        and('contain', `${message}`).
-        and('contain', 'Go To Post').
-        and('contain', 'To change your notification preferences, log in to your team site and go to Account Settings > Notifications.');
+    // * Verify that the email body is correct
+    const bodyText = data.body.text.split('\n');
+    expect(bodyText.length).to.equal(16);
+    expect(bodyText[1]).to.contain('You have a new notification.');
+    expect(bodyText[4]).to.contain(`Channel: ${channelDisplayName}`);
+    expect(bodyText[5]).to.contain(`${byUser.username}`);
+    expect(bodyText[7]).to.contain(`${message}`);
+    expect(bodyText[9]).to.contain('Go To Post');
+    expect(bodyText[11]).to.contain('Any questions at all, mail us any time: feedback@mattermost.com');
+    expect(bodyText[12]).to.contain('Best wishes,');
+    expect(bodyText[13]).to.contain('The Mattermost Team');
+    expect(bodyText[15]).to.contain('To change your notification preferences, log in to your team site and go to Account Settings > Notifications.');
 }
