dcd402c3833ce9a2c55719903f39f4412496c7bc
nitin gupta <composed.nitin@gmail.com>
2019-05-28 11:13:21 +0530

    using api for createchannel

--- cypress/integration/channel/message_draft_with_attachment_then_switch_channel_spec.js
+++ cypress/integration/channel/message_draft_with_attachment_then_switch_channel_spec.js
@@ -7,24 +7,7 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-/* eslint max-nested-callbacks: ["error", 4] */
-
-function createNewChannel(name, header, purpose, type) {
-    const createChannel = '#create' + type + 'Channel';
-    const createChannelName = '#newChannelName';
-    const createChannelPurpose = '#newChannelPurpose';
-    const createChannelHeader = '#newChannelHeader';
-
-    cy.get(createChannel).click();
-    cy.get(createChannelName).type(name);
-    cy.get(createChannelPurpose).type(purpose);
-    cy.get(createChannelHeader).type(header);
-
-    cy.contains('Create New Channel').click({force: true});
-    if (cy.get('#createChannelError')) {
-        cy.get('#cancelNewChannel').click();
-    }
-}
+/* eslint max-nested-callbacks: ["error", 5] */
 
 describe('Message Draft with attachment and Switch Channels', () => {
     before(() => {
@@ -32,36 +15,43 @@ describe('Message Draft with attachment and Switch Channels', () => {
         cy.apiLogin('user-1');
         cy.visit('/');
     });
-
+    const channelName1 = 'test-channel-1';
+    const channelName2 = 'test-channel-2';
+    let testChannel1;
+    let testChannel2;
     it('M14126 Message Draft Pencil Icon - No text, only file attachment', () => {
-        // # Click on a test channel
-        createNewChannel('test-channel-3', 'Test-Channel-3', 'channel created for testing purpose', 'Public');
-        cy.get('#sidebarItem_test-channel-3').click({force: true});
+        // # Create new test channel
+        cy.getCurrentTeamId().then((teamId) => {
+            cy.apiCreateChannel(teamId, channelName1, channelName1, 'O', 'Test channel').then((response) => {
+                testChannel1 = response.body;
+                cy.get(`#sidebarItem_${testChannel1.name}`).click({force: true});
 
-        // # Validate if the channel has been opened
-        cy.url().should('include', '/channels/test-channel-3');
+                // # Validate if the channel has been opened
+                cy.url().should('include', '/channels/' + testChannel1.name);
 
-        // # Validate if the draft icon is not visible on the sidebar before making a draft
-        cy.get('#sidebarItem_test-channel- #draftIcon', {force: true}).should('be.not.visible');
+                // # Validate if the draft icon is not visible on the sidebar before making a draft
+                cy.get(`#sidebarItem_${testChannel1.name} #draftIcon`, {force: true}).should('be.not.visible');
 
-        // # Attach image in text area
-        cy.fixture('mattermost-icon.png').then((fileContent) => {
-            cy.get('#fileUploadButton input').upload(
-                {fileContent, fileName: 'mattermost-icon.png', mimeType: 'image/png'},
-                {subjectType: 'drag-n-drop'},
-            );
+                // # Attach image in text area
+                cy.fixture('mattermost-icon.png').then((fileContent) => {
+                    cy.get('#fileUploadButton input').upload(
+                        {fileContent, fileName: 'mattermost-icon.png', mimeType: 'image/png'},
+                        {subjectType: 'drag-n-drop'},
+                    );
+                });
+            });
+            cy.apiCreateChannel(teamId, channelName2, channelName2, 'O', 'Test channel').then((response) => {
+                testChannel2 = response.body;
+
+                // # Go to test channel without submitting the draft in the previous channel
+                cy.get(`#sidebarItem_${testChannel2.name}`, {force: true}).should('be.visible').click();
+
+                // # Validate if the newly navigated channel is open
+                cy.url().should('include', '/channels/' + testChannel2.name);
+
+                // # Validate if the draft icon is visible in side bar for the previous channel
+                cy.get(`#sidebarItem_${testChannel1.name} #draftIcon`).should('be.visible');
+            });
         });
-
-        // # Create second test channel
-        createNewChannel('test-channel-4', 'Test-Channel-4', 'channel created for testing purpose', 'Public');
-
-        // # Go to test channel without submitting the draft in the previous channel
-        cy.get('#sidebarItem_test-channel-4', {force: true}).should('be.visible').click();
-
-        // # Validate if the newly navigated channel is open
-        cy.url().should('include', '/channels/test-channel-4');
-
-        // # Validate if the draft icon is visible on side bar on the previous channel with a draft
-        cy.get('#sidebarItem_test-channel-3 #draftIcon').should('be.visible');
     });
 });
