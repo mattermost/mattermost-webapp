e1f156373b3b2ae4d2541ce635f128d0a1596318
Brad Coughlin <brad@bradcoughlin.com>
2019-06-12 08:25:08 -0700

    [MM-16089] Compliance reports job table missing (#2944)
    * [MM-16089] Compliance reports job table missing
    
    * Fix layout of hybrid schema and component page so that the compliance reports and user activity sections are visible
    
    * Remove unused string

--- components/admin_console/audits/audits.jsx
+++ components/admin_console/audits/audits.jsx
@@ -9,13 +9,11 @@ import ComplianceReports from 'components/admin_console/compliance_reports';
 import AuditTable from 'components/audit_table';
 import LoadingScreen from 'components/loading_screen.jsx';
 
-import AdminHeader from 'components/widgets/admin_console/admin_header.jsx';
 import ReloadIcon from 'components/icon/reload_icon';
 
 export default class Audits extends React.PureComponent {
     static propTypes = {
         isLicensed: PropTypes.bool.isRequired,
-        customWrapperClass: PropTypes.string,
 
         /*
          * Array of audits to render
@@ -37,8 +35,6 @@ export default class Audits extends React.PureComponent {
         this.state = {
             loadingAudits: true,
         };
-
-        this.props.customWrapperClass('compliance-not-licensed');
     }
 
     componentDidMount() {
@@ -55,17 +51,33 @@ export default class Audits extends React.PureComponent {
     }
 
     activityLogHeader = () => {
-        const style = {
+        const h4Style = {
             display: 'inline-block',
-            margin: 0,
+            marginBottom: '6px',
+        };
+        const divStyle = {
+            clear: 'both',
         };
         return (
-            <h4 style={style}>
-                <FormattedMessage
-                    id='admin.complianceMonitoring.userActivityLogsTitle'
-                    defaultMessage='User Activity Logs'
-                />
-            </h4>
+            <div style={divStyle}>
+                <h4 style={h4Style}>
+                    <FormattedMessage
+                        id='admin.complianceMonitoring.userActivityLogsTitle'
+                        defaultMessage='User Activity Logs'
+                    />
+                </h4>
+                <button
+                    type='submit'
+                    className='btn btn-link pull-right'
+                    onClick={this.reload}
+                >
+                    <ReloadIcon/>
+                    <FormattedMessage
+                        id='admin.audits.reload'
+                        defaultMessage='Reload User Activity Logs'
+                    />
+                </button>
+            </div>
         );
     }
 
@@ -97,33 +109,12 @@ export default class Audits extends React.PureComponent {
         }
 
         return (
-            <div className='wrapper--admin'>
+            <div>
                 {this.renderComplianceReports()}
-                <div className='panel audit-panel'>
-                    <AdminHeader>
-                        <FormattedMessage
-                            id='admin.complianceMonitoring.title'
-                            defaultMessage='Compliance Monitoring'
-                        />
-                        {this.activityLogHeader()}
-                        <button
-                            type='submit'
-                            className='btn btn-link pull-right'
-                            onClick={this.reload}
-                        >
-                            <ReloadIcon/>
-                            <FormattedMessage
-                                id='admin.audits.reload'
-                                defaultMessage='Reload User Activity Logs'
-                            />
-                        </button>
-                    </AdminHeader>
-                    <div className='admin-console__wrapper'>
-                        <div className='admin-console__content'>
-                            <div className='audit-panel__table'>
-                                {content}
-                            </div>
-                        </div>
+                <div className='panel compliance-panel'>
+                    {this.activityLogHeader()}
+                    <div className='compliance-panel__table'>
+                        {content}
                     </div>
                 </div>
             </div>
