f9b98e1290e5a530ca45484d5f989ad7b8c9655b
Harshil Sharma <harshilsharma63@gmail.com>
2019-06-15 14:18:44 +0530

    Update utils/utils.jsx
    Co-Authored-By: Harrison Healey <harrisonmhealey@gmail.com>

--- utils/utils.jsx
+++ utils/utils.jsx
@@ -765,7 +765,7 @@ export function applyTheme(theme) {
         changeCss('.app__body .emoji-picker .nav-tabs li a', 'fill:' + theme.centerChannelColor);
         changeCss('.app__body .post .post-collapse__show-more-button', `border-color:${changeOpacity(theme.centerChannelColor, 0.1)}`);
         changeCss('.app__body .post .post-collapse__show-more-line', `background-color:${changeOpacity(theme.centerChannelColor, 0.1)}`);
-        changeCss('.dropdown-menu > li > a', `color:${theme.centerChannelColor}`);
+        changeCss('.app__body .dropdown-menu > li > a', `color:${theme.centerChannelColor}`);
         changeCss('.dropdown-menu > li > a:hover', `color:${theme.centerChannelColor}`);
 
         if (theme.centerChannelBg) {
