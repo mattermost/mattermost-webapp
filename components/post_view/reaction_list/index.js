f32612d0d975e956dfd518bf497d66df65b22e8d
Jesse Hallam <jesse.hallam@gmail.com>
2019-06-21 10:17:43 -0300

    revert unintended debugging changes

--- components/post_view/reaction_list/index.js
+++ components/post_view/reaction_list/index.js
@@ -5,7 +5,7 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
 import {getChannel} from 'mattermost-redux/selectors/entities/channels';
-import {getReactionsForPosts} from 'mattermost-redux/selectors/entities/posts';
+import {makeGetReactionsForPost} from 'mattermost-redux/selectors/entities/posts';
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
 
 import {addReaction} from 'actions/post_actions.jsx';
@@ -13,25 +13,22 @@ import {scrollPostList} from 'actions/views/channel';
 
 import ReactionList from './reaction_list.jsx';
 
-function makeMapStateToProps(state, ownProps) {
-    // const getReactionsForPost = makeGetReactionsForPost();
+function makeMapStateToProps() {
+    const getReactionsForPost = makeGetReactionsForPost();
 
-    // return function mapStateToProps(state, ownProps) {
-    const config = getConfig(state);
-    const enableEmojiPicker = config.EnableEmojiPicker === 'true' && !ownProps.isReadOnly;
+    return function mapStateToProps(state, ownProps) {
+        const config = getConfig(state);
+        const enableEmojiPicker = config.EnableEmojiPicker === 'true' && !ownProps.isReadOnly;
 
-    const channel = getChannel(state, ownProps.post.channel_id) || {};
-    const teamId = channel.team_id;
+        const channel = getChannel(state, ownProps.post.channel_id) || {};
+        const teamId = channel.team_id;
 
-    // console.log('ReactionList::mapStateToProps', ownProps.post.id,
-
-    return {
-        teamId,
-        reactions: getReactionsForPosts(state)[ownProps.post.id],
-        enableEmojiPicker,
+        return {
+            teamId,
+            reactions: getReactionsForPost(state, ownProps.post.id),
+            enableEmojiPicker,
+        };
     };
-
-    // };
 }
 
 function mapDispatchToProps(dispatch) {
