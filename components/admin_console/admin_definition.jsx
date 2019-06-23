ddaa984c2e12df6212180ad674b2a38af2b52045
Brad Coughlin <brad@bradcoughlin.com>
2019-06-23 12:02:34 -0700

    [MM-16235] System Console: Plugins section is missing for restricted system admin (#2989)
    * hide plugin setting when restricted admin enabled
    * show plugins section in LHS
    * add new test case
    * Add Oxford comma
    * Update snapshot

--- components/admin_console/admin_definition.jsx
+++ components/admin_console/admin_definition.jsx
@@ -3297,7 +3297,6 @@ export default {
             url: 'plugins/plugin_management',
             title: t('admin.plugins.pluginManagement'),
             title_default: 'Plugin Management',
-            isHidden: it.configIsTrue('ExperimentalSettings', 'RestrictSystemAdmin'),
             searchableStrings: [
                 'admin.plugin.management.title',
                 'admin.plugins.settings.enable',
