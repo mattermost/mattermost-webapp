028b0876b80446b710caa62175ec6c0825e3e48d
Jonathan Rigsby <jonathanrigsby@gmail.com>
2019-07-05 13:19:59 -0500

    MM 15240 - No Status on a System Message (#3034)
    * added new spec file for system message test
    
    * working on system message spec
    
    * Finished system message status spec
    
    * Forgot to lint
    
    * rearranged spec; added api call to update channel
    
    * corrected API call; reorganized spec
    
    * Modified updateChannel method; added patchChannel method
    
    * Updated spec describe; switched to use api for display type; switch updateChannel with patchChannel
    
    * reduced number of nested callbacks

--- e2e/cypress/support/api_commands.js
+++ e2e/cypress/support/api_commands.js
@@ -101,6 +101,56 @@ Cypress.Commands.add('apiDeleteChannel', (channelId) => {
     });
 });
 
+/**
+ * Updates a channel directly via API
+ * This API assume that the user is logged in and has cookie to access
+ * @param {String} channelId - The channel's id, not updatable
+ * @param {Object} channelData
+ *   {String} name - The unique handle for the channel, will be present in the channel URL
+ *   {String} display_name - The non-unique UI name for the channel
+ *   {String} type - 'O' for a public channel (default), 'P' for a private channel
+ *   {String} purpose - A short description of the purpose of the channel
+ *   {String} header - Markdown-formatted text to display in the header of the channel
+ * Only channelId is required
+ */
+Cypress.Commands.add('apiUpdateChannel', (channelId, channelData) => {
+    return cy.request({
+        headers: {'X-Requested-With': 'XMLHttpRequest'},
+        url: '/api/v4/channels/' + channelId,
+        method: 'PUT',
+        body: {
+            id: channelId,
+            ...channelData,
+        },
+    }).then((response) => {
+        expect(response.status).to.match(/20\d/);
+        return cy.wrap(response);
+    });
+});
+
+/**
+ * Partially update a channel directly via API
+ * This API assume that the user is logged in and has cookie to access
+ * @param {String} channelId - The channel's id, not updatable
+ * @param {Object} channelData
+ *   {String} name - The unique handle for the channel, will be present in the channel URL
+ *   {String} display_name - The non-unique UI name for the channel
+ *   {String} purpose - A short description of the purpose of the channel
+ *   {String} header - Markdown-formatted text to display in the header of the channel
+ * Only channelId is required
+ */
+Cypress.Commands.add('apiPatchChannel', (channelId, channelData) => {
+    return cy.request({
+        headers: {'X-Requested-With': 'XMLHttpRequest'},
+        method: 'PUT',
+        url: `/api/v4/channels/${channelId}/patch`,
+        body: channelData,
+    }).then((response) => {
+        expect(response.status).to.match(/20\d/);
+        return cy.wrap(response);
+    });
+});
+
 // *****************************************************************************
 // Teams
 // https://api.mattermost.com/#tag/teams
