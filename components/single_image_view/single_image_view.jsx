13f2b440303a6f70c7271defc4f8e7cd80278817
Asaad Mahmood <asaad@mattermost.com>
2019-07-06 22:41:54 +0500

    Updating minor accessibility changes

--- components/single_image_view/single_image_view.jsx
+++ components/single_image_view/single_image_view.jsx
@@ -108,9 +108,9 @@ export default class SingleImageView extends React.PureComponent {
         }
 
         const toggle = (
-            <a
+            <button
                 key='toggle'
-                className='post__embed-visibility'
+                className='style--none post__embed-visibility color--link'
                 data-expanded={this.props.isEmbedVisible}
                 aria-label='Toggle Embed Visibility'
                 onClick={this.toggleEmbedVisibility}
