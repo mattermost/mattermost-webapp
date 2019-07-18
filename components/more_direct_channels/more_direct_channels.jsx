91edcece8ae6e4de5317cca4389205c95dad4f78
Devin Binnie <devin.binnie@mattermost.com>
2019-07-18 11:21:50 -0400

    WIP

--- components/more_direct_channels/more_direct_channels.jsx
+++ components/more_direct_channels/more_direct_channels.jsx
@@ -269,6 +269,10 @@ export default class MoreDirectChannels extends React.Component {
         this.setState({values});
     }
 
+    renderAriaLabel = (option) => {
+        return option.username;
+    }
+
     renderOption = (option, isSelected, onAdd) => {
         if (option.type && option.type === 'G') {
             return (
@@ -415,6 +419,7 @@ export default class MoreDirectChannels extends React.Component {
                 optionRenderer={this.renderOption}
                 values={this.state.values}
                 valueRenderer={this.renderValue}
+                ariaLabelRenderer={this.renderAriaLabel}
                 perPage={USERS_PER_PAGE}
                 handlePageChange={this.handlePageChange}
                 handleInput={this.search}
@@ -459,7 +464,9 @@ export default class MoreDirectChannels extends React.Component {
                         />
                     </Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
+                <Modal.Body
+                    role='application'
+                >
                     {body}
                 </Modal.Body>
                 <Modal.Footer className='modal-footer--invisible'>
