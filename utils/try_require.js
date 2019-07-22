1c63daac75fd4a9d12ca2d89ff76513ca32d6f3f
marianunez <maria.nunez@mattermost.com>
2019-07-22 11:54:33 -0400

    Return reject on loadplugin promise and added error handling. Other PR feedback improvements

--- utils/try_require.js
+++ utils/try_require.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-// Performs a safe require to the given path
+// tryRequire performs a safe require to the given path
 // and returns null in case of failure
 export default function tryRequire(path) {
     try {
