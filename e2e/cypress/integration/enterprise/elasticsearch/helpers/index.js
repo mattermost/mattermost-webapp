5ee5988a2039cacd0ae6f0999b979b3d172b2148
Bob Lubecker <bob@mattermost.com>
2019-07-24 06:15:10 -0500

    Add Elastic search E2E tests (#3073)
    * Add Elastic search E2E tests
    
    * reverted removal of automatically logging in as admin for some API commands, updated affected tests
    
    * missed a place where we do actually need to manually login as sysadmin
    
    * remove accidental cy.log()
    
    * improve how we return responses and handle errors in externalRequest
    
    * fix eslint error, add comments
    
    * expect explicit error codes for api calls, fix the new elastic search tests based on recent changes

---
+++ e2e/cypress/integration/enterprise/elasticsearch/helpers/index.js
@@ -0,0 +1,54 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import * as TIMEOUTS from '../../../../fixtures/timeouts';
+
+module.exports = {
+    withTimestamp: (string, timestamp) => {
+        return string + '-' + timestamp;
+    },
+    createEmail: (name, timestamp) => {
+        return name + timestamp + '@sample.mattermost.com';
+    },
+    enableElasticSearch: () => {
+        // Enabled elastic search via the API
+        cy.apiUpdateConfig({
+            ElasticsearchSettings: {
+                EnableAutocomplete: true,
+                EnableIndexing: true,
+                EnableSearching: true,
+                Sniff: false,
+            },
+        });
+
+        cy.apiLogin('sysadmin');
+
+        // Navigate to the elastic search setting page
+        cy.visit('/admin_console/environment/elasticsearch');
+
+        // Test the connection and verify that we are successful
+        cy.contains('button', 'Test Connection').click();
+        cy.get('.alert-success').should('have.text', 'Test successful. Configuration saved.');
+
+        // Index so we are up to date
+        cy.contains('button', 'Index Now').click();
+
+        // Small wait to ensure new row is added
+        cy.wait(TIMEOUTS.TINY);
+
+        // Newest row should eventuall result in Success
+        cy.get('.job-table__table').find('tbody > tr').eq(0).as('firstRow').find('.status-icon-warning', {timeout: TIMEOUTS.LARGE}).should('be.visible');
+        cy.get('@firstRow').find('.status-icon-success', {timeout: TIMEOUTS.GIGANTIC}).should('be.visible');
+    },
+    disableElasticSearch: () => {
+        // Disable elastic search via API
+        cy.apiUpdateConfig({
+            ElasticsearchSettings: {
+                EnableAutocomplete: false,
+                EnableIndexing: false,
+                EnableSearching: false,
+                Sniff: false,
+            },
+        });
+    },
+};
