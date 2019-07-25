2b10e742b44eec5f1cb4c44f0250aa63e53036c2
Asaad Mahmood <asaad@mattermost.com>
2019-07-26 00:09:01 +0500

    MM-17304 - Updating favourites button readout

--- components/channel_header/channel_header.js
+++ components/channel_header/channel_header.js
@@ -229,6 +229,10 @@ export default class ChannelHeader extends React.PureComponent {
         }
     }
 
+    removeTooltipLink = () => {
+        this.refs.toggleFavorite.removeAttribute('aria-describedby');
+    }
+
     showEditChannelHeaderModal = () => {
         if (this.refs.headerOverlay) {
             this.refs.headerOverlay.hide();
@@ -451,9 +455,11 @@ export default class ChannelHeader extends React.PureComponent {
                     delayShow={Constants.OVERLAY_TIME_DELAY}
                     placement='bottom'
                     overlay={toggleFavoriteTooltip}
+                    onEntering={this.removeTooltipLink}
                 >
                     <button
                         id='toggleFavorite'
+                        ref='toggleFavorite'
                         onClick={this.toggleFavorite}
                         className={'style--none color--link channel-header__favorites ' + (this.props.isFavorite ? 'active' : 'inactive')}
                         aria-label={ariaLabel}
@@ -510,6 +516,8 @@ export default class ChannelHeader extends React.PureComponent {
                         aria-label={formatMessage({id: 'channel_header.menuAriaLabel', defaultMessage: 'Channel Menu'}).toLowerCase()}
                     >
                         <strong
+                            role='heading'
+                            aria-level='2'
                             id='channelHeaderTitle'
                             className='heading'
                         >
@@ -536,6 +544,8 @@ export default class ChannelHeader extends React.PureComponent {
                 >
                     {toggleFavorite}
                     <strong
+                        role='heading'
+                        aria-level='2'
                         id='channelHeaderTitle'
                         className='heading'
                     >
@@ -568,9 +578,9 @@ export default class ChannelHeader extends React.PureComponent {
                             <div
                                 className='channel-header__title dropdown'
                             >
-                                <h2>
+                                <div>
                                     {title}
-                                </h2>
+                                </div>
                                 {muteTrigger}
                             </div>
                             {headerTextContainer}
