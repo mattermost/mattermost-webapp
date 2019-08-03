5d93ee1d82b42a7f0887abe738f1274ffa59ac32
Abraham Arias <arias.abram@gmail.com>
2019-08-03 12:15:57 -0700

    Creating test structure
    Signed-off-by: Abraham Arias <arias.abram@gmail.com>

---
+++ e2e/cypress/integration/emoji/multi_emoji_shows_jumbo_spec.js
@@ -0,0 +1,21 @@
+
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+// ***************************************************************
+// - [#] indicates a test step (e.g. 1. Go to a page)
+// - [*] indicates an assertion (e.g. * Check the title)
+// - Use element ID when selecting an element. Create one if none.
+// ***************************************************************
+describe('MM-17457:Emojis show as jumbo in main thread', () => {
+    before(() => {
+        cy.apiLogin('user-1');
+        cy.visit('/');
+    });
+
+    it('should show multi emoji as jumbo in main thread', () => {
+
+    });
+});
+
+
