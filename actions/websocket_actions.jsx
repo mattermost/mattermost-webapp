b370d8fa18b0c1f1d42f5f3baca99150e1d17188
Jesús Espino <jespinog@gmail.com>
2019-08-05 16:26:19 +0200

    Fixing roles update on team member roles updates

--- actions/websocket_actions.jsx
+++ actions/websocket_actions.jsx
@@ -629,7 +629,7 @@ function handleDeleteTeamEvent(msg) {
 function handleUpdateMemberRoleEvent(msg) {
     dispatch({
         type: TeamTypes.RECEIVED_MY_TEAM_MEMBER,
-        data: msg.data.member,
+        data: JSON.parse(msg.data.member),
     });
 }
 
