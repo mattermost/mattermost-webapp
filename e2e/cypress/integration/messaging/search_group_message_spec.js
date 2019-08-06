7d437bbfeff8fe777b1f325b9e0b2b14b5148045
Abraham Arias <arias.abram@gmail.com>
2019-08-06 14:29:26 -0700

    Adding Documentation

--- e2e/cypress/integration/messaging/search_group_message_spec.js
+++ e2e/cypress/integration/messaging/search_group_message_spec.js
@@ -16,34 +16,43 @@ describe('Messaging', () => {
         let user1;
         let user2;
 
+        //# text from invoke method
         cy.get('#moreDirectMessage').click();
 
+        //# text from invoke method
         cy.getAllByTestId('username').eq(1).children('span').invoke('text').then((text) => {
-            user1 = text;
-            user1 = user1.split(' ');
-            user1 = user1[0];
+            //# getting username text
+            user1 = text.split(' ')[0];
+
             cy.get('.more-modal__name').contains(text).click();
         });
 
         cy.getAllByTestId('username').eq(1).children('span').invoke('text').then((text) => {
-            user2 = text;
-            user2 = user2.split(' ');
-            user2 = user2[0].replace(/([^a])/, '');
+            //# getting username text and removing @
+            user2 = text.split(' ')[0].substring(1);
 
+            //# select user from modal
             cy.get('.more-modal__name').contains(text).click();
 
+            //# save values
             cy.get('#saveItems').click();
 
+            //# searching 'lucho' text and hitting enter
             cy.get('#post_textbox').clear().type('lucho').type('{enter}');
 
+            //# writing "in:" text in search input
             cy.get('#searchBox').type('in:');
 
+            //# Search user1 and user2 in menu
             cy.getAllByTestId('listItem').contains(user1 + ',' + user2 + ',user-1').click();
 
+            //# hits enter
             cy.get('#searchBox').type('{enter}');
 
+            //# Search 'lucho'
             cy.get('#searchBox').clear().type('lucho{enter}');
 
+            //# Expect 'lucho' to be highlighted in the page
             cy.get('.search-highlight').first().contains('lucho').should('text', 'lucho');
         });
     });
