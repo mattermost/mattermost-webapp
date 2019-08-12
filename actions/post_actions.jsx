9f066b44787754dcd43176e418f03b83a34a4ba4
Guillermo Vaya <guivaya@gmail.com>
2019-08-13 01:13:03 +0200

    [MM-17065] unread since given post from dots menu

--- actions/post_actions.jsx
+++ actions/post_actions.jsx
@@ -225,6 +225,23 @@ export function setEditingPost(postId = '', commentCount = 0, refocusId = '', ti
     };
 }
 
+export function unreadPost(post) {
+    return async (dispatch, getState) => {
+        const state = getState();
+        const channelId = post.channel_id;
+        const timestamp = post.create_at;
+        const userId = getCurrentUserId(state);
+        const data = await dispatch(PostActions.setUnreadPost(userId, post.id));
+        if (data) {
+            dispatch({
+                type: ActionTypes.RECEIVED_POSTS_FOR_CHANNEL_AT_TIME,
+                channelId,
+                timestamp,
+            });
+        }
+    };
+}
+
 export function hideEditPostModal() {
     return {
         type: ActionTypes.HIDE_EDIT_POST_MODAL,
