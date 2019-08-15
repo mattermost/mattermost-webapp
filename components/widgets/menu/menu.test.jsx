a95c554a80c487346ad64c7e248809533b69035b
Jesús Espino <jespinog@gmail.com>
2019-08-15 18:10:13 +0200

    Moving divider hidding logic from css to javascript (#3442)
    * Moving divider hidding logic from css to javascript
    
    * Adding tests
    
    * Addressing PR comments
    
    * Adding a test case for mobile-menu-divider

--- components/widgets/menu/menu.test.jsx
+++ components/widgets/menu/menu.test.jsx
@@ -111,4 +111,200 @@ describe('components/Menu', () => {
 </ul>
 `);
     });
+
+    test('should hide the correct dividers', () => {
+        const utils = require('utils/utils'); //eslint-disable-line global-require
+        utils.isMobile.mockReturnValue(false);
+        const pseudoMenu = document.createElement('div');
+        const listOfItems = [
+            'menu-divider', 'menu-divider', 'menu-divider', 'menu-divider',
+            'other', 'other',
+            'menu-divider', 'menu-divider',
+            'other',
+            'menu-divider', 'menu-divider', 'menu-divider',
+            'other', 'other', 'other',
+            'menu-divider', 'menu-divider', 'menu-divider', 'menu-divider',
+        ];
+        for (const className of listOfItems) {
+            const element = document.createElement('div');
+            element.classList.add(className);
+            pseudoMenu.append(element);
+        }
+
+        const wrapper = shallow(<Menu ariaLabel='test-label'>{'text'}</Menu>);
+        const instance = wrapper.instance();
+        instance.node.current = pseudoMenu;
+        instance.hideUnneededDividers();
+
+        expect(instance.node.current).toMatchInlineSnapshot(`
+        <div>
+          <div
+            class="menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="menu-divider"
+          />
+          <div
+            class="menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="menu-divider"
+          />
+          <div
+            class="menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="menu-divider"
+            style="display: none;"
+          />
+        </div>
+        `);
+    });
+
+    test('should hide the correct dividers on mobile', () => {
+        const utils = require('utils/utils'); //eslint-disable-line global-require
+        utils.isMobile.mockReturnValue(false);
+        const pseudoMenu = document.createElement('div');
+        const listOfItems = [
+            'mobile-menu-divider', 'mobile-menu-divider', 'mobile-menu-divider', 'mobile-menu-divider',
+            'other', 'other',
+            'mobile-menu-divider', 'mobile-menu-divider',
+            'other',
+            'mobile-menu-divider', 'mobile-menu-divider', 'mobile-menu-divider',
+            'other', 'other', 'other',
+            'mobile-menu-divider', 'mobile-menu-divider', 'mobile-menu-divider', 'mobile-menu-divider',
+        ];
+        for (const className of listOfItems) {
+            const element = document.createElement('div');
+            element.classList.add(className);
+            pseudoMenu.append(element);
+        }
+
+        const wrapper = shallow(<Menu ariaLabel='test-label'>{'text'}</Menu>);
+        const instance = wrapper.instance();
+        instance.node.current = pseudoMenu;
+        instance.hideUnneededDividers();
+
+        expect(instance.node.current).toMatchInlineSnapshot(`
+        <div>
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="mobile-menu-divider"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="mobile-menu-divider"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+        </div>
+        `);
+    });
 });
