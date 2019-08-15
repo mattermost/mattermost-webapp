117f450c8603d5f07a503817e8b10a709d1dc5e1
Jesús Espino <jespinog@gmail.com>
2019-08-15 11:59:54 +0200

    Adding a test case for mobile-menu-divider

--- components/widgets/menu/menu.test.jsx
+++ components/widgets/menu/menu.test.jsx
@@ -209,4 +209,102 @@ describe('components/Menu', () => {
         </div>
         `);
     });
+
+    test('should hide the correct dividers on mobile', () => {
+        const utils = require('utils/utils'); //eslint-disable-line global-require
+        utils.isMobile.mockReturnValue(false);
+        const pseudoMenu = document.createElement('div');
+        const listOfItems = [
+            'mobile-menu-divider', 'mobile-menu-divider', 'mobile-menu-divider', 'mobile-menu-divider',
+            'other', 'other',
+            'mobile-menu-divider', 'mobile-menu-divider',
+            'other',
+            'mobile-menu-divider', 'mobile-menu-divider', 'mobile-menu-divider',
+            'other', 'other', 'other',
+            'mobile-menu-divider', 'mobile-menu-divider', 'mobile-menu-divider', 'mobile-menu-divider',
+        ];
+        for (const className of listOfItems) {
+            const element = document.createElement('div');
+            element.classList.add(className);
+            pseudoMenu.append(element);
+        }
+
+        const wrapper = shallow(<Menu ariaLabel='test-label'>{'text'}</Menu>);
+        const instance = wrapper.instance();
+        instance.node.current = pseudoMenu;
+        instance.hideUnneededDividers();
+
+        expect(instance.node.current).toMatchInlineSnapshot(`
+        <div>
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="mobile-menu-divider"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="mobile-menu-divider"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="other"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+          <div
+            class="mobile-menu-divider"
+            style="display: none;"
+          />
+        </div>
+        `);
+    });
 });
