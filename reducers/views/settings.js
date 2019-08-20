7d273e6293f52b1e80592d281f4811ad0d1002e1
Devin Binnie <devin.binnie@mattermost.com>
2019-08-20 09:43:30 -0400

    PR feedback

--- reducers/views/settings.js
+++ reducers/views/settings.js
@@ -1,19 +1,16 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {combineReducers} from 'redux';
-
 import {ActionTypes} from 'utils/constants.jsx';
 
-function previousActiveSection(state = null, action) {
+export default function settings(state = {}, action) {
     switch (action.type) {
     case ActionTypes.UPDATE_ACTIVE_SECTION:
-        return action.data;
+        return {
+            activeSection: action.data,
+            previousActiveSection: state.activeSection,
+        };
     default:
         return state;
     }
 }
-
-export default combineReducers({
-    previousActiveSection,
-});
