0212b4237d5a19d3d34018cd538caac17589413d
JoramWilander <jwawilander@gmail.com>
2019-08-21 14:35:00 -0400

    Fix some license fields showing as NaN and remove forced refresh when uploading/removing licenses

--- components/admin_console/license_settings/license_settings.jsx
+++ components/admin_console/license_settings/license_settings.jsx
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
 
-import {removeLicenseFile, uploadLicenseFile} from 'actions/admin_actions.jsx';
 import * as Utils from 'utils/utils.jsx';
 
 import FormattedMarkdownMessage from 'components/formatted_markdown_message.jsx';
@@ -15,6 +14,11 @@ export default class LicenseSettings extends React.Component {
     static propTypes = {
         license: PropTypes.object.isRequired,
         config: PropTypes.object,
+        actions: PropTypes.shape({
+            getLicenseConfig: PropTypes.func.isRequired,
+            uploadLicense: PropTypes.func.isRequired,
+            removeLicense: PropTypes.func.isRequired,
+        }).isRequired,
     }
 
     constructor(props) {
@@ -29,6 +33,10 @@ export default class LicenseSettings extends React.Component {
         };
     }
 
+    componentDidMount() {
+        this.props.actions.getLicenseConfig();
+    }
+
     handleChange = () => {
         const element = this.refs.fileInput;
         if (element && element.files.length > 0) {
@@ -36,7 +44,7 @@ export default class LicenseSettings extends React.Component {
         }
     }
 
-    handleSubmit = (e) => {
+    handleSubmit = async (e) => {
         e.preventDefault();
 
         const element = this.refs.fileInput;
@@ -47,37 +55,34 @@ export default class LicenseSettings extends React.Component {
 
         this.setState({uploading: true});
 
-        uploadLicenseFile(
-            file,
-            () => {
-                this.setState({fileSelected: false, fileName: null, serverError: null, uploading: false});
-                window.location.reload(true);
-            },
-            (error) => {
-                Utils.clearFileInput(element[0]);
-                this.setState({fileSelected: false, fileName: null, serverError: error.message, uploading: false});
-            }
-        );
+        const {error} = await this.props.actions.uploadLicense(file);
+        if (error) {
+            Utils.clearFileInput(element[0]);
+            this.setState({fileSelected: false, fileName: null, serverError: error.message, uploading: false});
+            return;
+        }
+
+        await this.props.actions.getLicenseConfig();
+        this.setState({fileSelected: false, fileName: null, serverError: null, uploading: false});
     }
 
-    handleRemove = (e) => {
+    handleRemove = async (e) => {
         e.preventDefault();
 
         this.setState({removing: true});
 
-        removeLicenseFile(
-            () => {
-                this.setState({fileSelected: false, fileName: null, serverError: null, removing: false});
-                window.location.reload(true);
-            },
-            (error) => {
-                this.setState({fileSelected: false, fileName: null, serverError: error.message, removing: false});
-            }
-        );
+        const {error} = await this.props.actions.removeLicense();
+        if (error) {
+            this.setState({fileSelected: false, fileName: null, serverError: error.message, removing: false});
+            return;
+        }
+
+        await this.props.actions.getLicenseConfig();
+        this.setState({fileSelected: false, fileName: null, serverError: null, removing: false});
     }
 
     render() {
-        var serverError = '';
+        let serverError = '';
         if (this.state.serverError) {
             serverError = <div className='col-sm-12'><div className='form-group has-error'><label className='control-label'>{this.state.serverError}</label></div></div>;
         }
@@ -88,6 +93,7 @@ export default class LicenseSettings extends React.Component {
         }
 
         const {license} = this.props;
+        const {uploading} = this.state;
 
         let edition;
         let licenseType;
@@ -97,7 +103,7 @@ export default class LicenseSettings extends React.Component {
         const startsAt = Utils.displayDate(parseInt(license.StartsAt, 10));
         const expiresAt = Utils.displayDate(parseInt(license.ExpiresAt, 10));
 
-        if (license.IsLicensed === 'true') {
+        if (license.IsLicensed === 'true' && !uploading) {
             // Note: DO NOT LOCALISE THESE STRINGS. Legally we can not since the license is in English.
             const sku = license.SkuShortName ? <React.Fragment>{`Edition: Mattermost Enterprise Edition ${license.SkuShortName}`}<br/></React.Fragment> : null;
             edition = 'Mattermost Enterprise Edition. Enterprise features on this server have been unlocked with a license key and a valid subscription.';
@@ -184,7 +190,7 @@ export default class LicenseSettings extends React.Component {
                     defaultMessage='Upload'
                 />
             );
-            if (this.state.uploading) {
+            if (uploading) {
                 uploadButtonText = (
                     <FormattedMessage
                         id='admin.license.uploading'
