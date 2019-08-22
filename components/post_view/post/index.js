9032f6a8518c22b45f3145f768ab47fb79459138
Guillermo Vaya <guivaya@gmail.com>
2019-08-22 15:45:42 +0200

    [MM-17066] use alt for marking as unread

--- components/post_view/post/index.js
+++ components/post_view/post/index.js
@@ -12,6 +12,7 @@ import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
 import {isPostEphemeral, isSystemMessage} from 'mattermost-redux/utils/post_utils';
 
 import {selectPost, selectPostCard} from 'actions/views/rhs';
+import {unreadPost} from 'actions/post_actions.jsx';
 import {Preferences} from 'utils/constants.jsx';
 import {makeCreateAriaLabelForPost} from 'utils/post_utils.jsx';
 
@@ -93,6 +94,7 @@ function mapDispatchToProps(dispatch) {
         actions: bindActionCreators({
             selectPost,
             selectPostCard,
+            unreadPost,
         }, dispatch),
     };
 }
