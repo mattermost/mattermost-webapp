a94be55976bafdc7bd0d7311fc5d49a2d0db5221
Jesús Espino <jespinog@gmail.com>
2019-08-23 15:30:25 +0200

    Using an observer to handle menu updates to recalculate the dividers when change while is open

--- components/widgets/menu/menu.jsx
+++ components/widgets/menu/menu.jsx
@@ -33,6 +33,7 @@ export default class Menu extends React.PureComponent {
     constructor(props) {
         super(props);
         this.node = React.createRef();
+        this.observer = new MutationObserver(this.hideUnneededDividers);
     }
 
     hideUnneededDividers = () => {
@@ -40,12 +41,14 @@ export default class Menu extends React.PureComponent {
             return;
         }
 
+        this.observer.disconnect();
         const children = Object.values(this.node.current.children).slice(0, this.node.current.children.length);
 
         // Hiding dividers at beginning and duplicated ones
         let prevWasDivider = false;
         let isAtBeginning = true;
         for (const child of children) {
+            child.style.display = 'block';
             if (child.classList.contains('menu-divider') || child.classList.contains('mobile-menu-divider')) {
                 if (isAtBeginning || prevWasDivider) {
                     child.style.display = 'none';
@@ -65,6 +68,7 @@ export default class Menu extends React.PureComponent {
                 break;
             }
         }
+        this.observer.observe(this.node.current, {attributes: true, childList: true, subtree: true});
     }
 
     componentDidMount() {
@@ -73,6 +77,10 @@ export default class Menu extends React.PureComponent {
 
     componentDidUpdate() {
         this.hideUnneededDividers();
+    }
+
+    componentWillUnmount() {
+        this.observer.disconnect();
     }
 
     // Used from DotMenu component to know in which direction show the menu
