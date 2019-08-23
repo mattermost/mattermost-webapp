e4ffd42d2cc7102ffbb818534619065282be843f
AGMETEOR <guwats10@gmail.com>
2019-08-23 10:15:47 -0400

    Migrate autocompleteChannels in channel_actions to redux

--- actions/channel_actions.jsx
+++ actions/channel_actions.jsx
@@ -13,13 +13,9 @@ import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
 
 import {trackEvent} from 'actions/diagnostics_actions.jsx';
 import {loadNewDMIfNeeded, loadNewGMIfNeeded, loadProfilesForSidebar} from 'actions/user_actions.jsx';
-import store from 'stores/redux_store.jsx';
 import {browserHistory} from 'utils/browser_history';
 import {Constants, Preferences} from 'utils/constants.jsx';
 import {getDirectChannelName} from 'utils/utils';
-
-const doDispatch = store.dispatch;
-const doGetState = store.getState;
 
 export function openDirectChannelToUserId(userId) {
     return async (dispatch, getState) => {
@@ -113,19 +109,21 @@ export function searchMoreChannels(term) {
     };
 }
 
-export async function autocompleteChannels(term, success, error) {
-    const state = doGetState();
-    const teamId = getCurrentTeamId(state);
-    if (!teamId) {
-        return;
-    }
+export function autocompleteChannels(term, success, error) {
+    return async (dispatch, getState) => {
+        const state = getState();
+        const teamId = getCurrentTeamId(state);
+        if (!teamId) {
+            return;
+        }
 
-    const {data, error: err} = await ChannelActions.autocompleteChannels(teamId, term)(doDispatch, doGetState);
-    if (data && success) {
-        success(data);
-    } else if (err && error) {
-        error({id: err.server_error_id, ...err});
-    }
+        const {data, error: err} = await dispatch(ChannelActions.autocompleteChannels(teamId, term));
+        if (data && success) {
+            success(data);
+        } else if (err && error) {
+            error({id: err.server_error_id, ...err});
+        }
+    };
 }
 
 export function autocompleteChannelsForSearch(term, success, error) {
