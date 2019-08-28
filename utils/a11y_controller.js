59e15d71d76a53ca89c06fd8b0c3454a5b7b07ab
Devin Binnie <devin.binnie@mattermost.com>
2019-08-28 16:35:28 -0400

    Spacebar now activates all <button>'s when focused

--- utils/a11y_controller.js
+++ utils/a11y_controller.js
@@ -738,6 +738,11 @@ export default class A11yController {
         case isKeyPressed(event, Constants.KeyCodes.ENTER):
             this.enterKeyIsPressed = true;
             break;
+        case isKeyPressed(event, Constants.KeyCodes.SPACE):
+            if (event.target.nodeName === 'BUTTON') {
+                event.target.click();
+            }
+            break;
         }
     }
 
