9d1548e6577bf2f1310c2c110258dece84ad014b
Valentijn Nieman <valentijnnieman@gmail.com>
2019-08-29 09:20:03 -0400

    Chain 'should' calls together

--- e2e/cypress/integration/messaging/focus_move_spec.js
+++ e2e/cypress/integration/messaging/focus_move_spec.js
@@ -121,8 +121,7 @@ describe('Messaging', () => {
     });
     it('M17455 - Focus does not move for non-character keys', () => {
         //# Make sure main input is visible and focused
-        cy.get('#post_textbox').should('be.visible');
-        cy.get('#post_textbox').should('be.focused');
+        cy.get('#post_textbox').should('be.visible').and('be.focused');
 
         //#Click anywhere in the body to move the focus out of the main input box
         cy.get('body').click();
