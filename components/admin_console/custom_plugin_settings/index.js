702c48c7aee3d8f5d284402101ec98cb1736b3d5
Jesse Hallam <jesse.hallam@gmail.com>
2019-08-30 17:19:02 -0300

    MM-18093: expose plugin enable on each page
    Largely eliminate the `CustomPluginSettings` class by instead carefully encoding the paths to the plugin settings. Prepend a boolean setting that points at the plugin being enabled, allowing the plugin to be enabled and disabled from its own configuration page (in addition to the top level plugin management page).
    
    This also fixes a minor issue with the header and footers of plugins potentially clashing with an id (admittedly incredibly unlikely).

--- components/admin_console/custom_plugin_settings/index.js
+++ components/admin_console/custom_plugin_settings/index.js
@@ -6,6 +6,10 @@ import {createSelector} from 'reselect';
 
 import {getRoles} from 'mattermost-redux/selectors/entities/roles';
 
+import {Constants} from 'utils/constants';
+import {t} from 'utils/i18n';
+import SchemaAdminSettings from '../schema_admin_settings';
+
 import CustomPluginSettings from './custom_plugin_settings.jsx';
 
 function makeGetPluginSchema() {
@@ -16,16 +20,29 @@ function makeGetPluginSchema() {
                 return null;
             }
 
+            const escapedPluginId = SchemaAdminSettings.escapePathPart(plugin.id);
+
             let settings;
             if (plugin.settings_schema && plugin.settings_schema.settings) {
                 settings = plugin.settings_schema.settings.map((setting) => {
                     return {
                         ...setting,
+                        key: 'PluginSettings.Plugins.' + escapedPluginId + '.' + setting.key.toLowerCase(),
                         help_text_markdown: true,
                         label: setting.display_name,
+                        translate: Boolean(plugin.translate),
                     };
                 });
             }
+
+            settings.unshift({
+                type: Constants.SettingsTypes.TYPE_BOOL,
+                key: 'PluginSettings.PluginStates.' + escapedPluginId + '.Enable',
+                label: t('admin.plugin.enable_plugin'),
+                label_default: 'Enable plugin: ',
+                help_text: t('admin.plugin.enable_plugin.help'),
+                help_text_default: 'When true, this plugin is enabled.',
+            });
 
             return {
                 ...plugin.settings_schema,
