4297f02076957f8c5e34848fcefb4a06e08f675e
Nikhil Ranjan <niklabh811@gmail.com>
2019-08-30 16:30:47 +0200

    removed unused import

--- components/admin_console/admin_sidebar/admin_sidebar.jsx
+++ components/admin_console/admin_sidebar/admin_sidebar.jsx
@@ -1,7 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import $ from 'jquery';
 import PropTypes from 'prop-types';
 import React from 'react';
 import {FormattedMessage, intlShape} from 'react-intl';
