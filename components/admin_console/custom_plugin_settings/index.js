e931b49547b5f14c1e2a82ec85fbba88cb066b86
Jesse Hallam <jesse.hallam@gmail.com>
2019-09-03 16:58:32 -0300

    disable plugin settings if disabled

--- components/admin_console/custom_plugin_settings/index.js
+++ components/admin_console/custom_plugin_settings/index.js
@@ -9,6 +9,7 @@ import {getRoles} from 'mattermost-redux/selectors/entities/roles';
 import {Constants} from 'utils/constants';
 import {t} from 'utils/i18n';
 import SchemaAdminSettings from '../schema_admin_settings';
+import {it} from '../admin_definition';
 
 import CustomPluginSettings from './custom_plugin_settings.jsx';
 
@@ -21,6 +22,7 @@ function makeGetPluginSchema() {
             }
 
             const escapedPluginId = SchemaAdminSettings.escapePathPart(plugin.id);
+            const pluginEnabledConfigKey = 'PluginSettings.PluginStates.' + escapedPluginId + '.Enable';
 
             let settings;
             if (plugin.settings_schema && plugin.settings_schema.settings) {
@@ -31,13 +33,14 @@ function makeGetPluginSchema() {
                         help_text_markdown: true,
                         label: setting.display_name,
                         translate: Boolean(plugin.translate),
+                        isDisabled: it.stateIsFalse(pluginEnabledConfigKey),
                     };
                 });
             }
 
             settings.unshift({
                 type: Constants.SettingsTypes.TYPE_BOOL,
-                key: 'PluginSettings.PluginStates.' + escapedPluginId + '.Enable',
+                key: pluginEnabledConfigKey,
                 label: t('admin.plugin.enable_plugin'),
                 label_default: 'Enable plugin: ',
                 help_text: t('admin.plugin.enable_plugin.help'),
