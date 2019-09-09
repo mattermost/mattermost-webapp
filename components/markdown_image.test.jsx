3523d5c54aa50f11aa38bc8c5a4c34ae58080e66
sudheer <sudheer.105@gmail.com>
2019-09-09 13:08:45 +0530

    Fix test snapshot

--- components/markdown_image.test.jsx
+++ components/markdown_image.test.jsx
@@ -4,9 +4,7 @@
 import React from 'react';
 
 import configureStore from 'redux-mock-store';
-import {Provider} from 'react-redux';
-
-import {mountWithIntl} from 'tests/helpers/intl-test-helper.jsx';
+import {shallow} from 'enzyme';
 
 import MarkdownImage from 'components/markdown_image.jsx';
 
@@ -23,14 +21,16 @@ describe('components/MarkdownImage', () => {
     };
     const store = mockStore(initialState);
     it('should match snapsnot', () => {
-        const wrapper = mountWithIntl(
-            <Provider store={store}>
-                <MarkdownImage
-                    imagesMetadata={{format: 'jpg', frame_count: 0, width: 100, height: 100}}
-                    src='path/image'
-                />
-            </Provider>
+        const imageMetadata = {format: 'jpg', frame_count: 0, width: 100, height: 100};
+        const wrapper = shallow(
+            <MarkdownImage
+                imageMetadata={imageMetadata}
+                src='path/image'
+            />
         );
-        expect(wrapper).toMatchSnapshot();
+
+        const childrenWrapper = wrapper.dive({context: {store}}).props().children('safeSrc');
+
+        expect(childrenWrapper).toMatchSnapshot();
     });
 });
