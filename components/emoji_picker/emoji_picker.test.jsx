9dfc8916de5168555c5658242a63efe0f2f881b1
sudheer <sudheer.105@gmail.com>
2019-09-10 13:18:30 +0530

    MM-18436 Improve perceived performance of emoji picker
      * Render only first two categories i.e recent and people
        on mount so we decrease the first meaninful paint time.
      * Render the rest of the categories on mount
    
      * PR also remove the title and category section of recent emojis
        when then do not exist

--- components/emoji_picker/emoji_picker.test.jsx
+++ components/emoji_picker/emoji_picker.test.jsx
@@ -1,9 +1,28 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {filterEmojiSearchInput} from 'components/emoji_picker/emoji_picker';
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import EmojiMap from 'utils/emoji_map';
+
+import EmojiPicker, {filterEmojiSearchInput} from 'components/emoji_picker/emoji_picker';
+
+import EmojiPickerCategory from './components/emoji_picker_category';
+import EmojiPickerItem from './components/emoji_picker_item';
+import EmojiPickerCategorySection from './emoji_picker_category_section';
 
 describe('components/emoji_picker/EmojiPicker', () => {
+    jest.useFakeTimers();
+
+    beforeEach(() => {
+        jest.spyOn(window, 'requestAnimationFrame').mockImplementation((cb) => setTimeout(cb, 16));
+    });
+
+    afterEach(() => {
+        window.requestAnimationFrame.mockRestore();
+    });
+
     const testCases = [
         {input: 'smile', output: 'smile'},
         {input: 'SMILE', output: 'smile'},
@@ -19,5 +38,120 @@ describe('components/emoji_picker/EmojiPicker', () => {
         test(`'${testCase.input}' should return '${testCase.output}'`, () => {
             expect(filterEmojiSearchInput(testCase.input)).toEqual(testCase.output);
         });
+    });
+
+    const actions = {
+        getCustomEmojis: jest.fn(),
+        incrementEmojiPickerPage: jest.fn(),
+        searchCustomEmojis: jest.fn(),
+    };
+
+    const customEmojis = new Map();
+    customEmojis.set('smiley', {name: 'smiley'});
+
+    const emojiMap = new Set();
+    emojiMap.add({customEmojis});
+    emojiMap.add({customEmojisArray: ['smiley']});
+
+    const smiley = new Map();
+    smiley.set('smiley', {fileName: '1f603', category: 'people', aliases: ['smiley']});
+
+    emojiMap.add(smiley);
+
+    const baseProps = {
+        customEmojiPage: 1,
+        customEmojisEnabled: true,
+        listHeight: 245,
+        onEmojiClick: jest.fn(),
+        onEmojiClose: jest.fn(),
+        emojiMap: new EmojiMap([]),
+        recentEmojis: [],
+        actions,
+    };
+
+    test('init snapshot', () => {
+        const wrapper = shallow(
+            <EmojiPicker {...baseProps}/>
+        );
+
+        wrapper.instance().emojiPickerContainer = {
+            offsetHeight: 200,
+        };
+
+        expect(wrapper).toMatchSnapshot();
+
+        // Nine categories as there is no recent caterogry
+        expect(wrapper.find(EmojiPickerCategory).length).toBe(9);
+        expect(wrapper.find(EmojiPickerCategory).find({selected: true}).length).toBe(1);
+
+        // People EmojiPickerCategory should have prop selected true
+        expect(wrapper.find(EmojiPickerCategory).find({selected: true}).find({category: 'people'}).length).toBe(1);
+
+        expect(wrapper.find(EmojiPickerItem).length).toBe(291);
+        expect(wrapper.find(EmojiPickerCategorySection).length).toBe(1);
+        expect(wrapper.find(EmojiPickerCategorySection).find({categoryName: 'people'}).length).toBe(1);
+    });
+
+    test('init snapshot with recent category', () => {
+        const props = {
+            ...baseProps,
+            recentEmojis: [
+                'smiley',
+            ],
+        };
+
+        const wrapper = shallow(
+            <EmojiPicker {...props}/>
+        );
+
+        wrapper.instance().emojiPickerContainer = {
+            offsetHeight: 200,
+        };
+
+        expect(wrapper).toMatchSnapshot();
+
+        // 10 categories as there is recent caterogry and prop selected should be true on EmojiPickerCategory for recent category
+        expect(wrapper.find(EmojiPickerCategory).length).toBe(10);
+        expect(wrapper.find(EmojiPickerCategory).find({selected: true}).length).toBe(1);
+        expect(wrapper.find(EmojiPickerCategory).find({selected: true}).find({category: 'recent'}).length).toBe(1);
+
+        expect(wrapper.find(EmojiPickerItem).length).toBe(292);
+        expect(wrapper.find(EmojiPickerCategorySection).length).toBe(2);
+        expect(wrapper.find(EmojiPickerCategorySection).find({categoryName: 'recent'}).length).toBe(1);
+    });
+
+    test('Update should have for all categories', async () => {
+        const props = {
+            ...baseProps,
+            recentEmojis: [
+                'smiley',
+            ],
+        };
+
+        const wrapper = await shallow(
+            <EmojiPicker {...props}/>
+        );
+
+        wrapper.instance().emojiPickerContainer = {
+            offsetHeight: 200,
+        };
+
+        //oveflow hidden to not show the scroll bar
+        expect(wrapper.find('.emoji-picker__items').prop('style')).toStrictEqual({overflowY: 'hidden'});
+
+        jest.runAllTimers();
+        wrapper.update();
+
+        // 10 categories as there is recent caterogry and prop selected should be true on EmojiPickerCategory for recent category
+        expect(wrapper.find(EmojiPickerCategory).length).toBe(10);
+        expect(wrapper.find(EmojiPickerCategory).find({selected: true}).length).toBe(1);
+        expect(wrapper.find(EmojiPickerCategory).find({selected: true}).find({category: 'recent'}).length).toBe(1);
+
+        expect(wrapper.state('renderAllCategories')).toEqual(true);
+        expect(wrapper.find(EmojiPickerItem).length).toBe(1449);
+        expect(wrapper.find(EmojiPickerCategorySection).length).toBe(10);
+
+        //oveflow hidden to not show the scroll bar
+        expect(wrapper.find('.emoji-picker__items').prop('style')).toStrictEqual({overflowY: 'auto'});
     });
 });
