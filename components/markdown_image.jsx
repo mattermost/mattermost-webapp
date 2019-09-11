8538809b42c6c058756fd48d9018ba285b91dd44
abdusabri <abdusabri@outlook.com>
2019-09-11 02:16:36 +0200

    Allow in-line markdown images to open preview window

--- components/markdown_image.jsx
+++ components/markdown_image.jsx
@@ -6,43 +6,100 @@ import React from 'react';
 
 import ExternalImage from 'components/external_image';
 import SizeAwareImage from 'components/size_aware_image';
+import ViewImageModal from 'components/view_image';
 
-export default function MarkdownImage({imageMetadata, src, ...props}) {
-    return (
-        <ExternalImage
-            src={src}
-            imageMetadata={imageMetadata}
-        >
-            {(safeSrc) => {
-                if (!safeSrc) {
+export default class MarkdownImage extends React.PureComponent {
+    static defaultProps = {
+        imageMetadata: {},
+    };
+
+    static propTypes = {
+        alt: PropTypes.string,
+        imageMetadata: PropTypes.object,
+        src: PropTypes.string.isRequired,
+        title: PropTypes.string,
+        className: PropTypes.string.isRequired,
+        postId: PropTypes.string.isRequired,
+        imageIsLink: PropTypes.bool.isRequired,
+        onImageLoaded: PropTypes.func,
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showModal: false,
+        };
+    }
+
+    showModal = (e) => {
+        if (!this.props.imageIsLink) {
+            e.preventDefault();
+            this.setState({showModal: true});
+        }
+    }
+
+    hideModal = () => {
+        this.setState({showModal: false});
+    }
+
+    render() {
+        const {imageMetadata, src, imageIsLink} = this.props;
+
+        return (
+            <ExternalImage
+                src={src}
+                imageMetadata={imageMetadata}
+            >
+                {(safeSrc) => {
+                    if (!safeSrc) {
+                        return (
+                            <a
+                                className='theme markdown__link'
+                                href={src}
+                                rel='noopener noreferrer'
+                                target='_blank'
+                                title={this.props.title}
+                            >
+                                {this.props.alt}
+                            </a>
+                        );
+                    }
+
+                    const className = imageIsLink ?
+                        this.props.className :
+                        `${this.props.className} markdown-inline-img--hover cursor--pointer a11y--active`;
+
+                    const getFileExtentionFromUrl = (url) => url.substring(safeSrc.lastIndexOf('.') + 1);
+
                     return (
-                        <a
-                            className='theme markdown__link'
-                            href={src}
-                            rel='noopener noreferrer'
-                            target='_blank'
-                            title={props.title}
-                        >
-                            {props.alt}
-                        </a>
+                        <>
+                            <SizeAwareImage
+                                className={className}
+                                src={safeSrc}
+                                dimensions={imageMetadata}
+                                showLoader={true}
+                                onClick={this.showModal}
+                                onImageLoaded={this.props.onImageLoaded}
+                            />
+                            {!imageIsLink &&
+                                <ViewImageModal
+                                    show={this.state.showModal}
+                                    onModalDismissed={this.hideModal}
+                                    postId={this.props.postId}
+                                    startIndex={0}
+                                    fileInfos={[{
+                                        has_preview_image: false,
+                                        link: safeSrc,
+                                        extension: imageMetadata.format || getFileExtentionFromUrl(safeSrc),
+                                        name: this.props.alt,
+                                    }]}
+                                />
+                            }
+                        </>
                     );
-                }
-
-                return (
-                    <SizeAwareImage
-                        {...props}
-                        src={safeSrc}
-                        dimensions={imageMetadata}
-                    />
-                );
-            }}
-        </ExternalImage>
-    );
+                }}
+            </ExternalImage>
+        );
+    }
 }
-
-MarkdownImage.propTypes = {
-    alt: PropTypes.string,
-    imageMetadata: PropTypes.object,
-    src: PropTypes.string.isRequired,
-    title: PropTypes.string,
-};
--- components/markdown_image.test.jsx
+++ components/markdown_image.test.jsx
@@ -5,19 +5,106 @@ import React from 'react';
 
 import {shallow} from 'enzyme';
 
-import MarkdownImage from 'components/markdown_image.jsx';
+import MarkdownImage from './markdown_image';
+import SizeAwareImage from './size_aware_image';
+import ViewImageModal from './view_image';
 
 describe('components/MarkdownImage', () => {
-    it('should match snapsnot for SizeAwareImage dimensions', () => {
-        const imageMetadata = {format: 'jpg', frame_count: 0, width: 100, height: 100};
+    const baseProps = {
+        imageMetadata: {
+            format: 'png',
+            height: 165,
+            width: 1041,
+            frame_count: 0,
+        },
+        alt: 'test image',
+        className: 'markdown-inline-img',
+        postId: 'post_id',
+        imageIsLink: false,
+    };
+
+    test('should match snapshot', () => {
+        const props = {...baseProps, src: '/images/logo.png'};
         const wrapper = shallow(
-            <MarkdownImage
-                imageMetadata={imageMetadata}
-                src='path/image'
-            />
+            <MarkdownImage {...props}/>
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should match snapshot for SizeAwareImage dimensions', () => {
+        const props = {...baseProps,
+            imageMetadata: {format: 'jpg', frame_count: 0, width: 100, height: 100},
+            src: 'path/image',
+        };
+        const wrapper = shallow(
+            <MarkdownImage {...props}/>
         );
 
         const childrenWrapper = wrapper.props().children('safeSrc');
         expect(childrenWrapper).toMatchSnapshot();
+    });
+
+    test('should render a link if the source is unsafe', () => {
+        const props = {...baseProps, src: ''};
+        const wrapper = shallow(
+            <MarkdownImage {...props}/>
+        );
+        const childrenWrapper = shallow(wrapper.props().children());
+
+        expect(childrenWrapper.find('a.markdown__link')).toHaveLength(1);
+        expect(childrenWrapper).toMatchSnapshot();
+    });
+
+    test('should render an image with preview modal if the source is safe', () => {
+        const props = {...baseProps, src: 'https://example.com/image.png'};
+        const wrapper = shallow(
+            <MarkdownImage {...props}/>
+        );
+        const childrenNode = wrapper.props().children(props.src);
+
+        // using a div as a workaround because shallow doesn't support react fragments
+        const childrenWrapper = shallow(<div>{childrenNode}</div>);
+
+        expect(childrenWrapper.find(SizeAwareImage)).toHaveLength(1);
+        expect(childrenWrapper.find(SizeAwareImage).first().prop('className')).
+            toEqual(`${props.className} markdown-inline-img--hover cursor--pointer a11y--active`);
+        expect(childrenWrapper.find(ViewImageModal)).toHaveLength(1);
+        expect(childrenWrapper).toMatchSnapshot();
+    });
+
+    test('should render an image with no preview if the source is safe and the image is a link', () => {
+        const props = {...baseProps, src: 'https://example.com/image.png', imageIsLink: true};
+        const wrapper = shallow(
+            <MarkdownImage {...props}/>
+        );
+        const childrenNode = wrapper.props().children(props.src);
+
+        // using a div as a workaround because shallow doesn't support react fragments
+        const childrenWrapper = shallow(<div>{childrenNode}</div>);
+
+        expect(childrenWrapper.find(SizeAwareImage)).toHaveLength(1);
+        expect(childrenWrapper.find(SizeAwareImage).first().prop('className')).
+            toEqual(props.className);
+        expect(childrenWrapper.find(ViewImageModal)).toHaveLength(0);
+        expect(childrenWrapper).toMatchSnapshot();
+    });
+
+    test('should handle state properly', () => {
+        const props = {...baseProps, src: 'https://example.com/image.png'};
+        const wrapper = shallow(
+            <MarkdownImage {...props}/>
+        );
+        wrapper.instance().showModal({preventDefault: () => {}});
+        expect(wrapper.state('showModal')).toEqual(true);
+    });
+
+    test('should handle state properly in case the image is a link', () => {
+        const props = {...baseProps, src: 'https://example.com/image.png', imageIsLink: true};
+        const wrapper = shallow(
+            <MarkdownImage {...props}/>
+        );
+        wrapper.instance().showModal();
+        expect(wrapper.state('showModal')).toEqual(false);
     });
 });
