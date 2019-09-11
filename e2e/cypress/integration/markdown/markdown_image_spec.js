0814af9753358ff9d5556da5b2af46a90826e85b
abdusabri <abdusabri@outlook.com>
2019-09-11 03:06:16 +0200

    Update markdown_image E2E tests

--- e2e/cypress/integration/markdown/markdown_image_spec.js
+++ e2e/cypress/integration/markdown/markdown_image_spec.js
@@ -22,8 +22,8 @@ describe('Markdown', () => {
 
     const baseUrl = Cypress.config('baseUrl');
 
-    const inlineImage1 = `<h3 class="markdown__heading">In-line Images</h3><p>Mattermost/platform build status:  <a class="theme markdown__link" href="https://travis-ci.org/mattermost/platform" rel="noreferrer" target="_blank"><div class="style--none file-preview__button"><img class="markdown-inline-img" alt="Build Status" aria-label="file thumbnail" tabindex="0" src="${baseUrl}/api/v4/image?url=https%3A%2F%2Ftravis-ci.org%2Fmattermost%2Fplatform.svg%3Fbranch%3Dmaster"></div></a></p>`;
-    const inlineImage2 = `<h3 class="markdown__heading">In-line Images</h3><p>GitHub favicon:  <div class="style--none file-preview__button"><img class="markdown-inline-img" alt="Github" aria-label="file thumbnail" tabindex="0" src="${baseUrl}/api/v4/image?url=https%3A%2F%2Fgithub.githubassets.com%2Ffavicon.ico"></div></p>`;
+    const inlineImage1 = `<h3 class="markdown__heading">In-line Images</h3><div class="style--none">Mattermost/platform build status:  <a class="theme markdown__link" href="https://travis-ci.org/mattermost/platform" rel="noreferrer" target="_blank"><div class="style--none file-preview__button" style=""><img class="markdown-inline-img" aria-label="file thumbnail" tabindex="0" src="${baseUrl}/api/v4/image?url=https%3A%2F%2Ftravis-ci.org%2Fmattermost%2Fplatform.svg%3Fbranch%3Dmaster"></div></a></div>`;
+    const inlineImage2 = `<h3 class="markdown__heading">In-line Images</h3><div class="style--none">GitHub favicon:  <div class="style--none file-preview__button" style=""><img class="markdown-inline-img markdown-inline-img--hover cursor--pointer a11y--active" aria-label="file thumbnail" tabindex="0" src="${baseUrl}/api/v4/image?url=https%3A%2F%2Fgithub.githubassets.com%2Ffavicon.ico"></div></div>`;
 
     const tests = [
         {name: 'with in-line images 1', fileKey: 'markdown_inline_images_1', expected: inlineImage1},
@@ -39,6 +39,22 @@ describe('Markdown', () => {
             // Note we use the Gigantic timeout to ensure that the large images will load
             cy.getLastPostId().then((postId) => {
                 cy.get(`#postMessageText_${postId}`, {timeout: TIMEOUTS.GIGANTIC}).should('have.html', test.expected);
+            });
+        });
+    });
+
+    it('opens preview window when image is clicked', () => {
+        // #  Post markdown message
+        cy.postMessageFromFile('markdown/markdown_inline_images_2.md');
+
+        // Note we use the Gigantic timeout to ensure that the large images will load
+        cy.getLastPostId().then((postId) => {
+            cy.get(`#postMessageText_${postId}`, {timeout: TIMEOUTS.GIGANTIC}).then((post) => {
+                // # Get the image and simulate a click.
+                cy.wrap(post).find('img.markdown-inline-img').first().click();
+
+                // * Verify that the file preview opens
+                cy.get('div.file-details__container').first().should('exist');
             });
         });
     });
