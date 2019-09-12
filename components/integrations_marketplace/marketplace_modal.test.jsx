74d17957eee65525b735f8a1cca0f57dfc211ca6
Jesse Hallam <jesse.hallam@gmail.com>
2019-09-12 13:41:29 -0400

    fix snapshots

--- components/integrations_marketplace/marketplace_modal.test.jsx
+++ components/integrations_marketplace/marketplace_modal.test.jsx
@@ -8,16 +8,16 @@ import MarketplaceModal from './marketplace_modal.jsx';
 
 describe('components/MarketplaceModal', () => {
     const marketplacePluginsSample = [{
-        HomepageURL: 'https://github.com/mattermost/mattermost-plugin-nps',
-        DownloadURL: 'https://github.com/mattermost/mattermost-plugin-nps/releases/download/v1.0.3/com.mattermost.nps-1.0.3.tar.gz',
-        Manifest: {
+        homepage_url: 'https://github.com/mattermost/mattermost-plugin-nps',
+        download_url: 'https://github.com/mattermost/mattermost-plugin-nps/releases/download/v1.0.3/com.mattermost.nps-1.0.3.tar.gz',
+        manifest: {
             id: 'com.mattermost.nps',
             name: 'User Satisfaction Surveys',
             description: 'This plugin sends quarterly user satisfaction surveys to gather feedback and help improve Mattermost',
             version: '1.0.3',
             minServerVersion: '5.14.0',
         },
-        InstalledVersion: '',
+        installed_version: '',
     }];
 
     const installedPlugins = [];
@@ -42,16 +42,16 @@ describe('components/MarketplaceModal', () => {
 
     test('should match the snapshot, with plugins installed', () => {
         const installedPlugin = {
-            HomepageURL: 'https://github.com/mattermost/mattermost-test',
-            DownloadURL: 'https://github.com/mattermost/mattermost-test/releases/download/v1.0.3/com.mattermost.nps-1.0.3.tar.gz',
-            Manifest: {
+            homepage_url: 'https://github.com/mattermost/mattermost-test',
+            download_url: 'https://github.com/mattermost/mattermost-test/releases/download/v1.0.3/com.mattermost.nps-1.0.3.tar.gz',
+            manifest: {
                 id: 'com.mattermost.test',
                 name: 'Test',
                 description: 'This plugin is to test',
                 version: '1.0.3',
                 minServerVersion: '5.14.0',
             },
-            InstalledVersion: '1.0.3',
+            installed_version: '1.0.3',
         };
 
         marketplacePluginsSample.push(installedPlugin);
