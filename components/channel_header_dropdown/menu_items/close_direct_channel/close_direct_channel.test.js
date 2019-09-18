70e470967c02fde9b653c89d7b633a5310817036
Darrell Richards <hello@darrellrichards.com>
2019-09-18 00:49:28 -0400

    Added Unit Tests for the new closeDirectChannel component

---
+++ components/channel_header_dropdown/menu_items/close_direct_channel/close_direct_channel.test.js
@@ -0,0 +1,54 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import {Constants} from 'utils/constants';
+
+import Menu from 'components/widgets/menu/menu';
+
+import CloseDirectChannel from './close_direct_channel';
+
+describe('components/ChannelHeaderDropdown/MenuItem.CloseDirectChannel', () => {
+    const baseProps = {
+        currentUser: {
+            id: 'user_id',
+        },
+        channel: {
+            id: 'channel_id',
+            type: Constants.DM_CHANNEL,
+            teammate_id: 'teammate-id',
+        },
+        redirectChannel: 'test-default-channel',
+        currentTeam: {
+            id: 'team_id',
+            name: 'test-team',
+            display_name: 'Test team display name',
+            description: 'Test team description',
+            type: 'team-type',
+        },
+        actions: {
+            savePreferences: jest.fn(() => Promise.resolve()),
+        },
+    };
+
+    it('should match snapshot', () => {
+        const wrapper = shallow(<CloseDirectChannel {...baseProps}/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('shoud be hidden if the channel is not a DM', () => {
+        const wrapper = shallow(<CloseDirectChannel {...baseProps}/>);
+        baseProps.channel.type = Constants.GM_CHANNEL;
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should run savePreferences function on click', () => {
+        const wrapper = shallow(<CloseDirectChannel {...baseProps}/>);
+        wrapper.find(Menu.ItemAction).simulate('click', {
+            preventDefault: jest.fn(),
+        });
+        expect(baseProps.actions.savePreferences).toBeCalledWith(baseProps.currentUser.id, [{user_id: baseProps.currentUser.id, category: Constants.Preferences.CATEGORY_DIRECT_CHANNEL_SHOW, name: baseProps.channel.teammate_id, value: 'false'}]);
+    });
+});
