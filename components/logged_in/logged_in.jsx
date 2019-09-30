03850440004a54e4fa51087c5695d6198a4d8327
Dean Whillier <deanwhillier@users.noreply.github.com>
2019-09-30 14:48:24 -0400

    [MM-18819] Backport desktop notification changes to 5.14 (#3773)
    * backport desktop notification changes
    
    * change to fixed version

--- components/logged_in/logged_in.jsx
+++ components/logged_in/logged_in.jsx
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {Redirect} from 'react-router';
 import {viewChannel} from 'mattermost-redux/actions/channels';
+import semver from 'semver';
 
 import * as GlobalActions from 'actions/global_actions.jsx';
 import * as WebSocketActions from 'actions/websocket_actions.jsx';
@@ -15,6 +16,8 @@ import {getBrowserTimezone} from 'utils/timezone.jsx';
 import store from 'stores/redux_store.jsx';
 import {webappConnector} from 'utils/webapp_connector';
 import WebSocketClient from 'client/web_websocket_client.jsx';
+import {browserHistory} from 'utils/browser_history';
+import {getChannelURL} from 'utils/utils';
 
 const dispatch = store.dispatch;
 const getState = store.getState;
@@ -71,10 +74,16 @@ export default class LoggedIn extends React.PureComponent {
         window.addEventListener('focus', this.onFocusListener);
         window.addEventListener('blur', this.onBlurListener);
 
-        // Listen for user activity updates from external sources via the webapp connector
-        if (webappConnector.active) {
-            webappConnector.on('user-activity-update', this.handleUserActivityUpdates);
-        }
+        // Listen for messages from the desktop app
+        window.addEventListener('message', this.onDesktopMessageListener);
+
+        // Tell the desktop app the webapp is ready
+        window.postMessage(
+            {
+                type: 'webapp-ready',
+            },
+            window.location.origin
+        );
 
         // Because current CSS requires the root tag to have specific stuff
 
@@ -173,12 +182,41 @@ export default class LoggedIn extends React.PureComponent {
         GlobalActions.emitBrowserFocus(false);
     }
 
-    handleUserActivityUpdates = ({userIsActive, manual}) => {
+    // listen for messages from the desktop app
+    onDesktopMessageListener = ({origin, data: {type, message = {}} = {}} = {}) => {
         if (!this.props.currentUser) {
             return;
         }
+        if (origin !== window.location.origin) {
+            return;
+        }
 
-        // update the server with the users current away status
-        WebSocketClient.userUpdateActiveStatus(userIsActive, manual);
+        switch (type) {
+        case 'register-desktop': {
+            const {version} = message;
+            if (!window.desktop) {
+                window.desktop = {};
+            }
+            window.desktop.version = semver.valid(semver.coerce(version));
+            break;
+        }
+        case 'user-activity-update': {
+            const {userIsActive, manual} = message;
+
+            // update the server with the users current away status
+            if (userIsActive === true || userIsActive === false) {
+                WebSocketClient.userUpdateActiveStatus(userIsActive, manual);
+            }
+            break;
+        }
+        case 'notification-clicked': {
+            const {channel, teamId} = message;
+            window.focus();
+
+            // navigate to the appropriate channel
+            browserHistory.push(getChannelURL(channel, teamId));
+            break;
+        }
+        }
     }
 }
