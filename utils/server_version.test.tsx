c37dfc0be0232f33c30902c0874da5ae93d8ae43
Micah Thompson <mjthomp95@gmail.com>
2019-10-02 01:02:17 -0400

    Migrate 'utils/server_version.jsx' and tests to TypeScript

--- utils/server_version.test.tsx
+++ utils/server_version.test.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {equalServerVersions, isServerVersionGreaterThanOrEqualTo} from 'utils/server_version.jsx';
+import {equalServerVersions, isServerVersionGreaterThanOrEqualTo} from 'utils/server_version';
 
 describe('utils/server_version/equalServerVersions', () => {
     test('should handle undefined values', () => {
