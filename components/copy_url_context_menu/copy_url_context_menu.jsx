62cf239b632ded59c1821c088315f826c40adf4b
Guillermo Vaya <guivaya@gmail.com>
2019-10-03 17:54:40 +0200

    prevent context menu to appear when holding the left click

--- components/copy_url_context_menu/copy_url_context_menu.jsx
+++ components/copy_url_context_menu/copy_url_context_menu.jsx
@@ -51,9 +51,12 @@ export default class CopyUrlContextMenu extends React.Component {
         );
 
         const contextMenuTrigger = (
-            <ContextMenuTrigger id={'copy-url-context-menu' + this.props.menuId}>
+            <ContextMenuTrigger
+                id={'copy-url-context-menu' + this.props.menuId}
+                holdToDisplay={-1}
+            >
                 {this.props.children}
-            </ContextMenuTrigger>
+            </ContextMenuTrigger >
         );
 
         return (
