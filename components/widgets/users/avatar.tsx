efc8b4aeeab81bee7284a0b7034043b02c842e14
Romain Maneschi <rmaneschi@gmail.com>
2019-10-04 23:05:54 +0200

    delete props as suggested y @jespino

--- components/widgets/users/avatar.tsx
+++ components/widgets/users/avatar.tsx
@@ -1,7 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import PropTypes from 'prop-types';
 import React from 'react';
 
 import './avatar.scss';
@@ -12,19 +11,13 @@ type Props = {
     size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
 };
 
-const Avatar: React.FunctionComponent<Props> = ({url, username, size = 'md'}) => (
+const Avatar: React.FunctionComponent<Props> = ({url, username, size = 'md'}: Props) => (
     <img
         className={`Avatar Avatar-${size}`}
         alt={`${username || 'user'} profile image`}
         src={url}
     />
 );
-
-Avatar.propTypes = {
-    url: PropTypes.string.isRequired,
-    username: PropTypes.string,
-    size: PropTypes.oneOf<'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl'>(['xs', 'sm', 'md', 'lg', 'xl', 'xxl']),
-};
 
 Avatar.defaultProps = {
     size: 'md',
