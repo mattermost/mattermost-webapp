94047f76dc49dc60d339405938dcf66c0b34a14a
anchepiece <anchepiece@gmail.com>
2019-10-08 14:37:13 -0400

    MM-18993 Migrate 'components/toggle.jsx' to TypeScript

--- components/toggle.tsx
+++ components/toggle.tsx
@@ -2,33 +2,36 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import PropTypes from 'prop-types';
 
-export default function Toggle({onToggle, toggled, onText, offText, disabled}) {
+type Props = {
+    onToggle: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+    toggled: boolean,
+    disabled?: boolean,
+    onText: React.ReactNode,
+    offText: React.ReactNode,
+}
+
+export default function Toggle(props: Props) {
+    const {onToggle, toggled, disabled, onText, offText} = props;
     return (
         <button
             type='button'
             onClick={onToggle}
             className={`btn btn-lg btn-toggle ${toggled && 'active'} ${disabled && 'disabled'}`}
             aria-pressed={toggled ? 'true' : 'false'}
-            autoComplete='off'
         >
             <div className='handle'/>
             {text(toggled, onText, offText)}
         </button>);
 }
 
-function text(toggled, onText, offText) {
+function text(
+    toggled: boolean,
+    onText: React.ReactNode,
+    offText: React.ReactNode
+): React.ReactNode | null {
     if ((toggled && !onText) || (!toggled && !offText)) {
         return null;
     }
     return (<div className={`bg-text ${toggled ? 'on' : 'off'}`}>{toggled ? onText : offText}</div>);
 }
-
-Toggle.propTypes = {
-    onToggle: PropTypes.func.isRequired,
-    toggled: PropTypes.bool,
-    disabled: PropTypes.bool,
-    onText: PropTypes.node,
-    offText: PropTypes.node,
-};
