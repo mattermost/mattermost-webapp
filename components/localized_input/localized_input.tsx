08bd1838f301898878ae9b7a733fb8c807cda785
Keith Brewster <keith.brewster22@gmail.com>
2019-10-09 10:57:05 -0400

    Add typings for localized_input.tsx

--- components/localized_input/localized_input.tsx
+++ components/localized_input/localized_input.tsx
@@ -1,52 +1,49 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import PropTypes from 'prop-types';
 import React from 'react';
 import {intlShape} from 'react-intl';
 
-export default class LocalizedInput extends React.Component {
-    static propTypes = {
-
-        placeholder: PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            defaultMessage: PropTypes.string.isRequired,
-        }).isRequired,
-        value: PropTypes.string,
+type Props = {
+    placeholder: {
+        id: string;
+        defaultMessage: string;
     };
+    value?: string;
+}
 
-    static contextTypes = {
+export default class LocalizedInput extends React.Component<Props> {
+    public static contextTypes = {
         intl: intlShape.isRequired,
     };
+    public input: React.RefObject<HTMLInputElement> = React.createRef<HTMLInputElement>();
 
-    constructor(props) {
-        super(props);
-
-        this.input = React.createRef();
+    public get value(): string {
+        return this.input.current ? this.input.current.value : '';
     }
 
-    get value() {
-        return this.input.current.value;
+    public set value(value: string) {
+        if (this.input.current) {
+            this.input.current.value = value;
+        }
     }
 
-    set value(value) {
-        this.input.current.value = value;
-    }
-
-    focus = () => {
-        this.input.current.focus();
+    public focus = (): void => {
+        if (this.input.current) {
+            this.input.current.focus();
+        }
     };
 
-    shouldComponentUpdate(nextProps) {
+    public shouldComponentUpdate(nextProps: Props): boolean {
         return nextProps.value !== this.props.value ||
             nextProps.placeholder.id !== this.props.placeholder.id ||
             nextProps.placeholder.defaultMessage !== this.props.placeholder.defaultMessage;
     }
 
-    render() {
+    public render(): JSX.Element {
         const {formatMessage} = this.context.intl;
         const {placeholder, ...otherProps} = this.props;
-        const placeholderString = formatMessage(placeholder);
+        const placeholderString: string = formatMessage(placeholder);
 
         return (
             <input
