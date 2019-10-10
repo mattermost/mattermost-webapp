97401e059417f75a3ced5e326974ea011755bcea
Patryk Pomykalski <pomyks@gmail.com>
2019-10-10 15:47:31 +0200

    [MM-18965] Migrate components/msg_typing to TypeScript (#3872)
    * [MM-18965] Migrate components/msg_typing to TypeScript
    
    * fix lint errors
    
    * rename test snapshot

--- components/msg_typing/msg_typing.tsx
+++ components/msg_typing/msg_typing.tsx
@@ -1,17 +1,16 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import PropTypes from 'prop-types';
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
 
-export default class MsgTyping extends React.Component {
-    static propTypes = {
-        typingUsers: PropTypes.array.isRequired,
-    }
+type Props = {
+    typingUsers: string[];
+}
 
-    getTypingText = () => {
-        let users = [];
+export default class MsgTyping extends React.Component<Props> {
+    private getTypingText = () => {
+        let users: string[] = [];
         let numUsers = 0;
         if (this.props.typingUsers) {
             users = [...this.props.typingUsers];
@@ -45,7 +44,7 @@ export default class MsgTyping extends React.Component {
         );
     }
 
-    render() {
+    public render() {
         return (
             <span className='msg-typing'>{this.getTypingText()}</span>
         );
