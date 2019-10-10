1f63d1d0e615dbbd7310c84e02f7a49f24cafa00
Allan Guwatudde <guwats10@gmail.com>
2019-10-10 16:30:40 +0300

    Migrate 'components/get_ios_app' module and associated tests to TypeScript (#3879)
    * Migrate 'components/get_ios_app' module and associated tests to TypeScript
    
    * Add type to event arg
    
    * Fix TypeScript linting issue
    
    * Revert accidental changes in package-lock.json

--- components/get_ios_app/get_ios_app.test.tsx
+++ components/get_ios_app/get_ios_app.test.tsx
@@ -3,9 +3,10 @@
 
 import React from 'react';
 import {shallow} from 'enzyme';
+import {createMemoryHistory, createLocation} from 'history';
 
 import {mountWithIntl} from 'tests/helpers/intl-test-helper.jsx';
-import GetIosApp from 'components/get_ios_app/get_ios_app.jsx';
+import GetIosApp from 'components/get_ios_app/get_ios_app';
 
 jest.mock('images/app-store-button.png', () => 'store-button.png');
 jest.mock('images/iphone-6-mockup.png', () => 'mockup.png');
@@ -15,6 +16,8 @@ describe('components/GetIosApp', () => {
         const wrapper = shallow(
             <GetIosApp
                 iosAppDownloadLink={'https://about.mattermost.com/mattermost-ios-app'}
+                history={createMemoryHistory()}
+                location={createLocation('/')}
             />
         );
 
@@ -25,6 +28,8 @@ describe('components/GetIosApp', () => {
         const wrapper = shallow(
             <GetIosApp
                 iosAppDownloadLink={'https://about.mattermost.com/mattermost-ios-app'}
+                history={createMemoryHistory()}
+                location={createLocation('/')}
             />
         );
 
@@ -33,36 +38,44 @@ describe('components/GetIosApp', () => {
     });
 
     test('should redirect if the user chooses to stay in the browser. Redirect url param is present', () => {
-        const push = jest.fn();
+        const history = createMemoryHistory();
+        history.push = jest.fn();
+
+        const location = createLocation('/');
+        location.search = '?redirect_to=last_page';
         const wrapper = mountWithIntl(
             <GetIosApp
                 iosAppDownloadLink={'https://about.mattermost.com/mattermost-ios-app'}
-                history={{push}}
-                location={{search: '?redirect_to=last_page'}}
+                history={history}
+                location={location}
             />
         );
 
-        expect(push).not.toHaveBeenCalled();
+        expect(history.push).not.toHaveBeenCalled();
 
         const link = wrapper.find('.get-ios-app__continue');
         link.simulate('click');
-        expect(push).toHaveBeenCalledWith('last_page');
+        expect(history.push).toHaveBeenCalledWith('last_page');
     });
 
     test('should redirect if the user chooses to stay in the browser. Redirect url param is not present', () => {
-        const push = jest.fn();
+        const history = createMemoryHistory();
+        history.push = jest.fn();
+
+        const location = createLocation('/');
+        location.search = '';
         const wrapper = mountWithIntl(
             <GetIosApp
                 iosAppDownloadLink={'https://about.mattermost.com/mattermost-ios-app'}
-                history={{push}}
-                location={{search: ''}}
+                history={history}
+                location={location}
             />
         );
 
-        expect(push).not.toHaveBeenCalled();
+        expect(history.push).not.toHaveBeenCalled();
 
         const link = wrapper.find('.get-ios-app__continue');
         link.simulate('click');
-        expect(push).toHaveBeenCalledWith('/');
+        expect(history.push).toHaveBeenCalledWith('/');
     });
 });
