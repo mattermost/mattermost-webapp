c2521bb8f9003d1c72473aec9c915c52f3c5dc1a
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-10-10 14:02:39 -0600

    Add style check changes

--- e2e/cypress/integration/messaging/permalink_message_edit_spec.js
+++ e2e/cypress/integration/messaging/permalink_message_edit_spec.js
@@ -7,55 +7,53 @@
 // Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-
 describe('Permalink message edit', () => {
-	it('M18717 - Edit a message in permalink view', () => {
-		// # Login as "user-1" and go to /
-		cy.apiLogin('user-1');
-		cy.visit('/');
+    it('M18717 - Edit a message in permalink view', () => {
+        // # Login as "user-1" and go to /
+        cy.apiLogin('user-1');
+        cy.visit('/');
 
-		const searchWord = 'searchtest';
-		
-		// # Post message
-		cy.postMessage(searchWord)
-		
-		// # Search for searchWord
-		cy.get('#searchBox').type(searchWord).type('{enter}').then(() => {
-		
-			// # Jump to permalink view	
-			cy.get('.search-item__jump').first().click()
-			
-			cy.getLastPostId().then((postId) => {
-				// # Click on ... button of last post matching the searchWord
-				cy.clickPostDotMenu(postId);
-				
-				// # Click on edit post
-				cy.get(`#edit_post_${postId}`).click();
-				
-				// # Add new text in edit box
-				cy.get('#edit_textbox').clear().type('edited searchtest');
-				
-				// # Click edit button
-				cy.get('#editButton').click();
-				
-				// # Check edited post
-				cy.get(`#postMessageText_${postId}`).should('contain', 'edited searchtest')
-				cy.get(`#postEdited_${postId}`).should('contain', '(edited)')
-				
-				// # Logout as "user-1"
-				cy.apiLogout('user-1');
-				
-				// # Login as "user-2" and go to /
-				cy.apiLogin('user-2');
-				cy.visit('/');
-				
-				// # Find searchWord and verify edited post
-				cy.get('#searchBox').type(searchWord).type('{enter}').then(() => {
-					cy.get('.search-item__jump').first().click()
-					cy.get(`#postMessageText_${postId}`).should('contain', 'edited searchtest')
-					cy.get(`#postEdited_${postId}`).should('contain', '(edited)')
-				});
-			});
-		});
-	});
+        const searchWord = 'searchtest';
+
+        // # Post message
+        cy.postMessage(searchWord);
+
+        // # Search for searchWord
+        cy.get('#searchBox').type(searchWord).type('{enter}').then(() => {
+            // # Jump to permalink view
+            cy.get('.search-item__jump').first().click();
+
+            cy.getLastPostId().then((postId) => {
+                // # Click on ... button of last post matching the searchWord
+                cy.clickPostDotMenu(postId);
+
+                // # Click on edit post
+                cy.get(`#edit_post_${postId}`).click();
+
+                // # Add new text in edit box
+                cy.get('#edit_textbox').clear().type('edited searchtest');
+
+                // # Click edit button
+                cy.get('#editButton').click();
+
+                // # Check edited post
+                cy.get(`#postMessageText_${postId}`).should('contain', 'edited searchtest');
+                cy.get(`#postEdited_${postId}`).should('contain', '(edited)');
+
+                // # Logout as "user-1"
+                cy.apiLogout('user-1');
+
+                // # Login as "user-2" and go to /
+                cy.apiLogin('user-2');
+                cy.visit('/');
+
+                // # Find searchWord and verify edited post
+                cy.get('#searchBox').type(searchWord).type('{enter}').then(() => {
+                    cy.get('.search-item__jump').first().click();
+                    cy.get(`#postMessageText_${postId}`).should('contain', 'edited searchtest');
+                    cy.get(`#postEdited_${postId}`).should('contain', '(edited)');
+                });
+            });
+        });
+    });
 });
