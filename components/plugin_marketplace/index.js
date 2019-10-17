64532f1aeea88e2b36b48bc0bde236f69731ae69
Michael Kochell <mjkochell@gmail.com>
2019-10-17 11:24:41 -0600

    Cherry pick of #3885 Fix basename in paths to System Console (#3992)
    * Clarify bot account creation flow
    
    * fix basename in paths to System Console

--- components/plugin_marketplace/index.js
+++ components/plugin_marketplace/index.js
@@ -9,6 +9,7 @@ import {getMarketplaceInstalledPlugins} from 'mattermost-redux/selectors/entitie
 
 import {isModalOpen} from 'selectors/views/modals';
 import {ModalIdentifiers} from 'utils/constants';
+import {getSiteURL} from 'utils/url.jsx';
 
 import {closeModal} from 'actions/views/modals';
 
@@ -19,6 +20,7 @@ function mapStateToProps(state) {
         show: isModalOpen(state, ModalIdentifiers.PLUGIN_MARKETPLACE),
         installedPlugins: getMarketplaceInstalledPlugins(state),
         marketplacePlugins: state.entities.plugins.marketplacePlugins,
+        siteURL: getSiteURL(state),
         pluginStatuses: state.entities.admin.pluginStatuses,
     };
 }
