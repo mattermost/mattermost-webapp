e89107c1c8287b384ab2d8c315cf3edac75b998f
Mattermost Build <build@mattermost.com>
2019-10-21 19:18:48 +0200

    Automated cherry pick of #4001 (#4013)
    * Filter out users not in team from list when adding users to a channel
    
    * Improve code cleanliness

--- components/channel_invite_modal/channel_invite_modal.jsx
+++ components/channel_invite_modal/channel_invite_modal.jsx
@@ -24,6 +24,7 @@ const MAX_SELECTABLE_VALUES = 20;
 export default class ChannelInviteModal extends React.Component {
     static propTypes = {
         profilesNotInCurrentChannel: PropTypes.array.isRequired,
+        profilesNotInCurrentTeam: PropTypes.array.isRequired,
         onHide: PropTypes.func.isRequired,
         channel: PropTypes.object.isRequired,
         actions: PropTypes.shape({
@@ -213,8 +214,9 @@ export default class ChannelInviteModal extends React.Component {
         const buttonSubmitText = localizeMessage('multiselect.add', 'Add');
         const buttonSubmitLoadingText = localizeMessage('multiselect.adding', 'Adding...');
 
-        let users = filterProfilesMatchingTerm(this.props.profilesNotInCurrentChannel, this.state.term);
-        users = users.filter((user) => user.delete_at === 0);
+        const users = filterProfilesMatchingTerm(this.props.profilesNotInCurrentChannel, this.state.term).filter((user) => {
+            return user.delete_at === 0 && !this.props.profilesNotInCurrentTeam.includes(user);
+        });
 
         const content = (
             <MultiSelect
