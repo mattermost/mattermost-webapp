6b577759e751519083273d7ce5e978467759dc7e
Scott Bishel <scott.bishel@mattermost.com>
2019-10-22 08:17:32 -0600

    MM-16861: Support Guest Authentication via AD/LDAP (#3936)
    * Add UI for LDAPSettings GuestFilter
    
    * remove hidden setting for ExperimentLDAP feature

--- components/admin_console/admin_definition.jsx
+++ components/admin_console/admin_definition.jsx
@@ -2391,6 +2391,20 @@ const AdminDefinition = {
                     },
                     {
                         type: Constants.SettingsTypes.TYPE_TEXT,
+                        key: 'LdapSettings.GuestFilter',
+                        label: t('admin.ldap.guestFilterTitle'),
+                        label_default: 'Guest Filter:',
+                        help_text: t('admin.ldap.guestFilterFilterDesc'),
+                        help_text_default: '(Optional) Enter an AD/LDAP filter to use when searching for guest objects. Only the users selected by the query will be able to access Mattermost as Guests. Guests are prevented from accessing teams or channels upon logging in until they are assigned a team and at least one channel.',
+                        placeholder: t('admin.ldap.guestFilterEx'),
+                        placeholder_default: 'E.g.: "(objectClass=guests)"',
+                        isDisabled: it.both(
+                            it.stateIsFalse('LdapSettings.Enable'),
+                            it.stateIsFalse('LdapSettings.EnableSync'),
+                        ),
+                    },
+                    {
+                        type: Constants.SettingsTypes.TYPE_TEXT,
                         key: 'LdapSettings.GroupFilter',
                         label: t('admin.ldap.groupFilterTitle'),
                         label_default: 'Group Filter:',
