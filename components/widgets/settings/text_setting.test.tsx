374c9303bb6e39c37504900ef71ffaa1c8e90be4
zubair <m-zubairahmed@protonmail.com>
2019-10-30 02:24:33 +0530

    removed reduntant test at components/widgets/settings/text_settings, since TSC will not allow any other value to be passed to type prop other than given string literals

--- components/widgets/settings/text_setting.test.tsx
+++ components/widgets/settings/text_setting.test.tsx
@@ -66,45 +66,6 @@ describe('components/widgets/settings/TextSetting', () => {
         `);
     });
 
-    // This test is disabled as TSC will not allow to pass any other value to `type`
-/*
-    test('render with bad type', () => {
-        const originalError = console.error;
-        console.error = jest.fn();
-
-        const wrapper = shallow(
-            <TextSetting
-                id='string.id'
-                label='some label'
-                value='some value'
-                type='junk'
-            />
-        );
-
-        expect(console.error).toBeCalledTimes(1);
-
-        console.error = originalError;
-
-        expect(wrapper).toMatchInlineSnapshot(`
-<Setting
-  inputClassName=""
-  inputId="string.id"
-  label="some label"
-  labelClassName=""
->
-  <input
-    className="form-control"
-    id="string.id"
-    maxLength={null}
-    onChange={[Function]}
-    type="input"
-    value="some value"
-  />
-</Setting>
-`);
-    });
-*/
-
     test('onChange', () => {
         const onChange = jest.fn();
         const wrapper = shallow(
