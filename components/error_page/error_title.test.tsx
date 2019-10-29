f2542d6c6f9681c817f22b71c6059007e612e633
Valentijn Nieman <valentijnnieman@gmail.com>
2019-10-29 10:19:13 -0400

    Remove Jest type reference fix

--- components/error_page/error_title.test.tsx
+++ components/error_page/error_title.test.tsx
@@ -1,8 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-/// <reference types="Jest" />
-
 import React from 'react';
 import {shallow} from 'enzyme';
 
