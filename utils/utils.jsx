bf8ee4218f69c11d3f63f18debefd161cc209622
Ali Farooq <ali.farooq0@pm.me>
2019-10-30 15:28:27 -0400

    Updating error text

--- utils/utils.jsx
+++ utils/utils.jsx
@@ -1205,7 +1205,7 @@ export function isValidBotUsername(name) {
     }
 
     if (name.endsWith('.')) {
-        error = "Must not end with '.' symbol.";
+        error = "Username must not end with '.' symbol.";
     }
 
     return error;
