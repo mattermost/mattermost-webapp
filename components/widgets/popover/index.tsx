537fa12b864322bd65d8d753ad9942d405fe5bd3
Eli Yukelzon <reflog@gmail.com>
2019-11-09 16:17:16 +0200

    .

--- components/widgets/popover/index.tsx
+++ components/widgets/popover/index.tsx
@@ -8,39 +8,40 @@ const SizeMap = {xs: 'xsmall', sm: 'small', md: 'medium', lg: 'large'};
 export type Sizes = 'xs' | 'sm' | 'md' | 'lg';
 
 interface Props {
-    id: string;
-    children?: JSX.Element | JSX.Element[] | string;
+    id?: string;
+    children?: React.ReactNode;
     popoverStyle?: 'info';
     popoverSize?: Sizes;
     title?: React.ReactNode;
     placement?: 'bottom' | 'top' | 'right' | 'left';
-    ref?: string;
     className?: string;
     onMouseOut?: React.MouseEventHandler<BSPopover>; // didn't find a better way to satisfy typing, so for now we have a slight 'bootstrap leakage'
     onMouseOver?: React.MouseEventHandler<BSPopover>;
 }
 
-const Popover: React.FC<Props> = ({placement, popoverSize, children, popoverStyle, title, id, onMouseOut, onMouseOver, ref, className}: Props) => (
-    <BSPopover
-        id={id}
-        className={className}
-        ref={ref}
-        bsStyle={popoverStyle}
-        placement={placement}
-        bsClass='popover'
-        title={title}
-        bsSize={popoverSize && SizeMap[popoverSize] as BSSizes} // map our sizes to bootstrap
-        onMouseOut={onMouseOut!}
-        onMouseOver={onMouseOver}
-    >
-        {children}
-    </BSPopover>
-);
-
-Popover.defaultProps = {
-    placement: 'right',
-    popoverStyle: 'info',
-    popoverSize: 'sm'
-
-};
-export default Popover;
+export default class Popover extends React.PureComponent<Props> {
+    static defaultProps = {
+        placement: 'right',
+        popoverStyle: 'info',
+        popoverSize: 'sm'
+    
+    }
+    render() {
+        const { placement, popoverSize, children, popoverStyle, title, id, onMouseOut, onMouseOver, className } = this.props;
+        return (
+            <BSPopover
+                id={id}
+                className={className}
+                bsStyle={popoverStyle}
+                placement={placement}
+                bsClass='popover'
+                title={title}
+                bsSize={popoverSize && SizeMap[popoverSize] as BSSizes} // map our sizes to bootstrap
+                onMouseOut={onMouseOut!}
+                onMouseOver={onMouseOver}
+            >
+                {children}
+            </BSPopover>
+        );
+    }
+}
