5fc0c5ff6f4b8c94ed3fc4e1228a7e7befd7af59
Daniel Espino <larkox@gmail.com>
2019-11-11 06:32:22 -0500

    Fix several issues:
    - Correct description
    - Use Town Square channel
    - Reposition cursor each time we write the tilde
    - Change logic on when should the autocomplete show
    - Add edited verification
    - Shorten types

--- e2e/cypress/integration/messaging/strikethrough_spec.js
+++ e2e/cypress/integration/messaging/strikethrough_spec.js
@@ -9,7 +9,7 @@
 
 import * as TIMEOUTS from '../../fixtures/timeouts';
 
-describe('Edit post with "strikethrough', () => {
+describe('Messaging', () => {
     before(() => {
         // # Login and go to /
         cy.apiLogin('user-1');
@@ -19,53 +19,47 @@ describe('Edit post with "strikethrough', () => {
     it('M18710-Edit post with "strikethrough" and ensure channel auto-complete closes after second tilde (~~)', () => {
         const message = 'Hello' + Date.now();
 
-        // # Create new DM channel with user's email
-        cy.apiGetUsers(['user-1', 'sysadmin']).then((userResponse) => {
-            const userEmailArray = [userResponse.body[1].id, userResponse.body[0].id];
+        cy.visit('/ad-1/channels/town-square');
 
-            cy.apiCreateDirectChannel(userEmailArray).then(() => {
-                cy.visit('/ad-1/messages/@sysadmin');
+        // # Post message to use
+        cy.postMessage(message);
 
-                // # Post message to use
-                cy.postMessage(message);
+        // # Hit up arrow to open edit modal
+        cy.get('#post_textbox', {timeout: TIMEOUTS.LARGE}).clear().type('{uparrow}');
 
-                // # Hit up arrow to open edit modal
-                cy.get('#post_textbox', {timeout: TIMEOUTS.LARGE}).clear().type('{uparrow}');
+        // # Type first tilde (a{backspace} used so cursor is in the textbox and {home} gets us to the beginning of the line)
+        cy.get('#edit_textbox').type('a{backspace}{home}~');
+        cy.wait(TIMEOUTS.TINY);
 
-                // # Type first tilde (a{backspace} used so cursor is in the textbox and {home} gets us to the beginning of the line)
-                cy.get('#edit_textbox').type('a{backspace}{home}~');
-                cy.wait(TIMEOUTS.TINY);
+        // # Channel autocomplete should show
+        cy.get('#suggestionList').should('exist');
 
-                // # Channel autocomplete should show
-                cy.get('#suggestionList').should('exist');
+        // # Write the second tilde
+        cy.get('#edit_textbox').type('{home}{rightarrow}~');
 
-                // # Write the second tilde
-                cy.get('#edit_textbox').type('~');
+        // * Channel autocomplete should have closed
+        cy.get('#suggestionList').should('not.exist');
 
-                // * Channel autocomplete should have closed
-                cy.get('#suggestionList').should('not.exist');
+        // # Go to the end of the line and type the first tilde
+        cy.get('#edit_textbox').type('{end}~');
+        cy.wait(TIMEOUTS.TINY);
 
-                // # Go to the end of the line and type the first tilde
-                cy.get('#edit_textbox').type('{end}').type('~');
-                cy.wait(TIMEOUTS.TINY);
+        // # Channel autocomplete should not show
+        cy.get('#suggestionList').should('not.exist');
 
-                // # Channel autocomplete should show
-                cy.get('#suggestionList').should('exist');
+        // # Write the second tilde
+        cy.get('#edit_textbox').type('{end}~');
 
-                // # Write the second tilde
-                cy.get('#edit_textbox').type('~');
+        // * Channel autocomplete should still not show
+        cy.get('#suggestionList').should('not.exist');
 
-                // * Channel autocomplete should have closed
-                cy.get('#suggestionList').should('not.exist');
+        // # Save the changes
+        cy.get('#editButton').click({force: true});
 
-                // # Save the changes
-                cy.get('#editButton').click({force: true});
-
-                cy.getLastPostId().then((postId) => {
-                    // * Message strikedthrough should be the same message we posted
-                    cy.get(`#postMessageText_${postId}`).find('del').should('contain', message);
-                });
-            });
+        cy.getLastPostId().then((postId) => {
+            // * Message strikedthrough should be the same message we posted
+            cy.get(`#postMessageText_${postId}`).find('del').should('contain', message);
+            cy.get(`#postEdited_${postId}`).should('be.visible').and('have.text', '(edited)');
         });
     });
 });
