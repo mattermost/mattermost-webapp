6f5615cf000fbb64c29c11340a0833a7ad65bb29
Daniel Espino <larkox@gmail.com>
2019-11-11 10:29:46 -0500

    Fix description, typo on comment and use Town Square channel

--- e2e/cypress/integration/messaging/markdown_quotation_paragraphs_spec.js
+++ e2e/cypress/integration/messaging/markdown_quotation_paragraphs_spec.js
@@ -7,7 +7,7 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-describe('Markdown quotation paragraphs', () => {
+describe('Messaging', () => {
     before(() => {
         // # Login and go to /
         cy.apiLogin('user-1');
@@ -18,36 +18,29 @@ describe('Markdown quotation paragraphs', () => {
         const messageParts = ['this is', 'really', 'three quote lines'];
         const message = `>${messageParts[0]}\n\n>${messageParts[1]}\n\n>${messageParts[2]}`;
 
-        // # Create new DM channel with user's email
-        cy.apiGetUsers(['user-1', 'sysadmin']).then((userResponse) => {
-            const userEmailArray = [userResponse.body[1].id, userResponse.body[0].id];
+        cy.visit('/ad-1/channels/town-square');
 
-            cy.apiCreateDirectChannel(userEmailArray).then(() => {
-                cy.visit('/ad-1/messages/@sysadmin');
+        // # Post message to use
+        cy.postMessage(message);
 
-                // # Post message to use
-                cy.postMessage(message);
+        var firstPartLeft;
+        cy.getLastPostId().then((postId) => {
+            // * There is only one blockquote, and therefore only one Quote icon
+            cy.get(`#postMessageText_${postId} > blockquote`).should('have.length', 1);
 
-                var firstPartLeft;
-                cy.getLastPostId().then((postId) => {
-                    // * There is only one blockquote, and therefore only one Quote icon
-                    cy.get(`#postMessageText_${postId} > blockquote`).should('have.length', 1);
+            // * There are three distinct paragraphs, and therefore the three of them are separated by a space
+            cy.get(`#postMessageText_${postId} > blockquote > p`).should('have.length', 3);
+            cy.get(`#postMessageText_${postId} > blockquote > p`).each((el, i) => {
+                // * Each paragraph contains the content we put on the message
+                expect(messageParts[i]).equals(el.html());
 
-                    // * There are three distinct paragraphs, and therefore the three of them are separated by a space
-                    cy.get(`#postMessageText_${postId} > blockquote > p`).should('have.length', 3);
-                    cy.get(`#postMessageText_${postId} > blockquote > p`).each((el, i) => {
-                        // * Each paragraph contains the content we put on the message
-                        expect(messageParts[i]).equals(el.html());
+                // # We save the alignment of the first paragraph
+                if (i === 0) {
+                    firstPartLeft = el[0].getBoundingClientRect().left;
+                }
 
-                        // # We save the alignment of the firts paragraph
-                        if (i === 0) {
-                            firstPartLeft = el[0].getBoundingClientRect().left;
-                        }
-
-                        // * All paragraph are aligned on their left border
-                        expect(firstPartLeft).equals(el[0].getBoundingClientRect().left);
-                    });
-                });
+                // * All paragraph are aligned on their left border
+                expect(firstPartLeft).equals(el[0].getBoundingClientRect().left);
             });
         });
     });
