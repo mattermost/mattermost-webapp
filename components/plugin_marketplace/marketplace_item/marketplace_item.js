490d2e648d77d48bcc158a26c6b27e0027813468
Jesse Hallam <jesse.hallam@gmail.com>
2019-11-11 13:09:16 -0400

    MM-17549: support upgrading plugins (#4108)
    * marketplace: jsx -> js
    
    * marketplace: fix indentation
    
    * marketplace: remove confirm, and always overwrite
    
    * avoid isEqual dependency
    
    * MM-17549: Plugin upgrade in marketplace
    
    Support plugin upgrades from the marketplace. This change also reworks
    the state modelling of the marketplace to move some logic out of the
    components and into the views reducers, simplifying some transitions.
    
    Fixes: https://mattermost.atlassian.net/browse/MM-17549
    
    * extend marketplace e2e tests
    
    Include coverage for upgrading plugins, as well as coverage for existing functionality. Extend the cypress utilities to support installation and uninstallation of plugins for test setup.
    
    * fix missing translations
    
    * use Client4 directly
    
    * clarify marketplace spec search & install
    
    * Remove "Installing..." checks, since flaky when fast
    
    * simplify CSS, fix hover effect
    
    * suppress update subsection when installing
    
    * Update actions/marketplace.js
    
    Co-Authored-By: Maria A Nunez <maria.nunez@mattermost.com>
    
    * Update actions/marketplace.js
    
    Co-Authored-By: Maria A Nunez <maria.nunez@mattermost.com>
    
    * Update components/plugin_marketplace/marketplace_item/marketplace_item.test.js
    
    Co-Authored-By: Maria A Nunez <maria.nunez@mattermost.com>
    
    * drop unnecessary .js on imports
    
    * handle loading error after filterPlugins
    
    * updated snapshots
    
    * actually fix snapshots
    
    * tweak CSS styling

---
+++ components/plugin_marketplace/marketplace_item/marketplace_item.js
@@ -0,0 +1,197 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import {FormattedMessage} from 'react-intl';
+
+import {Link} from 'react-router-dom';
+
+import LoadingWrapper from 'components/widgets/loading/loading_wrapper.tsx';
+import PluginIcon from 'components/widgets/icons/plugin_icon.jsx';
+
+import {trackEvent} from 'actions/diagnostics_actions.jsx';
+import {localizeMessage} from 'utils/utils';
+
+export default class MarketplaceItem extends React.Component {
+    static propTypes = {
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        version: PropTypes.string.isRequired,
+        downloadUrl: PropTypes.string,
+        homepageUrl: PropTypes.string,
+        iconData: PropTypes.string,
+        installedVersion: PropTypes.string.isRequired,
+        installing: PropTypes.bool.isRequired,
+        error: PropTypes.string,
+        actions: PropTypes.shape({
+            installPlugin: PropTypes.func.isRequired,
+            closeMarketplaceModal: PropTypes.func.isRequired,
+        }).isRequired,
+    };
+
+    onInstall = () => {
+        trackEvent('plugins', 'ui_marketplace_download');
+        this.props.actions.installPlugin(this.props.id);
+    }
+
+    onUpdate = () => {
+        trackEvent('plugins', 'ui_marketplace_download_update');
+        this.props.actions.installPlugin(this.props.id);
+    }
+
+    onConfigure = () => {
+        trackEvent('plugins', 'ui_marketplace_configure');
+        this.props.actions.closeMarketplaceModal();
+    }
+
+    getItemButton() {
+        let actionButton = (
+            <FormattedMessage
+                id='marketplace_modal.list.Install'
+                defaultMessage='Install'
+            />
+        );
+        if (this.props.error) {
+            actionButton = (
+                <FormattedMessage
+                    id='marketplace_modal.list.try_again'
+                    defaultMessage='Try Again'
+                />
+            );
+        }
+
+        let button = (
+            <button
+                onClick={this.onInstall}
+                className='btn btn-primary'
+                disabled={this.props.installing || this.props.downloadUrl === ''}
+            >
+                <LoadingWrapper
+                    loading={this.props.installing}
+                    text={localizeMessage('marketplace_modal.installing', 'Installing...')}
+                >
+                    {actionButton}
+                </LoadingWrapper>
+
+            </button>
+        );
+
+        if (this.props.installedVersion !== '' && !this.props.installing && !this.props.error) {
+            button = (
+                <Link
+                    to={'/admin_console/plugins/plugin_' + this.props.id}
+                >
+                    <button
+                        onClick={this.onConfigure}
+                        className='btn btn-outline'
+                    >
+                        <FormattedMessage
+                            id='marketplace_modal.list.configure'
+                            defaultMessage='Configure'
+                        />
+                    </button>
+                </Link>
+            );
+        }
+
+        return button;
+    }
+
+    render() {
+        const ariaLabel = `${this.props.name}, ${this.props.description}`.toLowerCase();
+        let versionLabel = `(${this.props.version})`;
+        if (this.props.installedVersion !== '') {
+            versionLabel = `(${this.props.installedVersion})`;
+        }
+
+        let pluginIcon;
+        if (this.props.iconData) {
+            pluginIcon = (
+                <div className='icon__plugin icon__plugin--background'>
+                    <img src={this.props.iconData}/>
+                </div>
+            );
+        } else {
+            pluginIcon = <PluginIcon className='icon__plugin icon__plugin--background'/>;
+        }
+
+        let pluginDetails = (
+            <>
+                {this.props.name} <span className='light subtitle'>{versionLabel}</span>
+                <p className={classNames('more-modal__description', {error_text: this.props.error})}>
+                    {this.props.error ? this.props.error : this.props.description}
+                </p>
+            </>
+        );
+
+        if (this.props.homepageUrl) {
+            pluginDetails = (
+                <a
+                    aria-label={ariaLabel}
+                    className='style--none more-modal__row--link'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href={this.props.homepageUrl}
+                >
+                    {pluginDetails}
+                </a>
+            );
+        } else {
+            pluginDetails = (
+                <span
+                    aria-label={ariaLabel}
+                    className='style--none'
+                >
+                    {pluginDetails}
+                </span>
+            );
+        }
+
+        var update;
+        if (this.props.installedVersion !== '' && this.props.installedVersion !== this.props.version && !this.props.installing) {
+            update = (
+                <div className={classNames('update')}>
+                    <FormattedMessage
+                        id='marketplace_modal.list.update_available'
+                        defaultMessage='Update available: {version}'
+                        values={{
+                            version: this.props.version,
+                        }}
+                    />
+                    {' - '}
+                    <b>
+                        <a onClick={this.onInstall}>
+                            <FormattedMessage
+                                id='marketplace_modal.list.update'
+                                defaultMessage='Update'
+                            />
+                        </a>
+                    </b>
+                </div>
+            );
+        }
+
+        return (
+            <>
+                <div
+                    className={classNames('more-modal__row', 'more-modal__row--link', {item_error: this.props.error})}
+                    key={this.props.id}
+                    id={'marketplace-plugin-' + this.props.id}
+                >
+                    {pluginIcon}
+                    <div className='more-modal__details'>
+                        {pluginDetails}
+                        {update}
+                    </div>
+                    <div className='more-modal__actions'>
+                        {this.getItemButton()}
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
