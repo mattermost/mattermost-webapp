ea597cba7906e6c06e20a9d1e8b4a1c1a32b5dfd
Daniel Espino <larkox@gmail.com>
2019-11-11 08:48:23 -0500

    Correct description, lower delay between types to 0, and user Town Square channel.

--- e2e/cypress/integration/messaging/quick_send_spec.js
+++ e2e/cypress/integration/messaging/quick_send_spec.js
@@ -9,7 +9,7 @@
 
 import * as TIMEOUTS from '../../fixtures/timeouts';
 
-describe('Posts change order when being sent quickly', () => {
+describe('Messaging', () => {
     before(() => {
         // # Login and go to /
         cy.apiLogin('user-1');
@@ -17,27 +17,20 @@ describe('Posts change order when being sent quickly', () => {
     });
 
     it('M18698-Posts change order when being sent quickly', () => {
-        // # Create new DM channel with user's email
-        cy.apiGetUsers(['user-1', 'sysadmin']).then((userResponse) => {
-            const userEmailArray = [userResponse.body[1].id, userResponse.body[0].id];
+        cy.visit('/ad-1/channels/town-square');
 
-            cy.apiCreateDirectChannel(userEmailArray).then(() => {
-                cy.visit('/ad-1/messages/@sysadmin');
+        // # Build a message and send
+        let message = '';
+        for (let i = 9; i >= 0; i--) {
+            message += i + '{enter}';
+        }
+        cy.get('#post_textbox', {timeout: TIMEOUTS.LARGE}).clear().type(message, {delay: 0});
 
-                // # Build a message and send
-                let message = '';
-                for (let i = 9; i >= 0; i--) {
-                    message += i + '{enter}';
-                }
-                cy.get('#post_textbox', {timeout: TIMEOUTS.LARGE}).clear().type(message, {delay: 1});
-
-                for (let i = 10; i > 0; i--) {
-                    cy.getNthPostId(-i).then((postId) => {
-                        // * Check if the text is the correct one
-                        cy.get(`#postMessageText_${postId} > p`).should('have.text', String(i - 1));
-                    });
-                }
+        for (let i = 10; i > 0; i--) {
+            cy.getNthPostId(-i).then((postId) => {
+                // * Check if the text is the correct one
+                cy.get(`#postMessageText_${postId} > p`).should('have.text', String(i - 1));
             });
-        });
+        }
     });
 });
