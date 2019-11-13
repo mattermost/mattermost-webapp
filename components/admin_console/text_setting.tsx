e7b6c08634d431ffe456a5b90afbc25d8d8176b0
Allen Lai <allenplai@gmail.com>
2019-11-12 17:21:35 -0800

    change file extension

--- components/admin_console/text_setting.tsx
+++ components/admin_console/text_setting.tsx
@@ -1,30 +1,27 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import PropTypes from 'prop-types';
 import React from 'react';
-
 import TextSetting from 'components/widgets/settings/text_setting';
-
 import SetByEnv from './set_by_env';
 
-const AdminTextSetting = (props) => {
-    const {setByEnv, disabled, ...sharedProps} = props;
+type Props = {
+    setByEnv: boolean
+};
+
+const AdminTextSetting: React.SFC<Props> = props => {
+    const { setByEnv, disabled, ...sharedProps } = props;
 
     return (
         <TextSetting
             {...sharedProps}
-            labelClassName='col-sm-4'
-            inputClassName='col-sm-8'
+            labelClassName="col-sm-4"
+            inputClassName="col-sm-8"
             disabled={disabled || setByEnv}
-            footer={setByEnv ? <SetByEnv/> : null}
+            footer={setByEnv ? <SetByEnv /> : null}
         />
     );
 };
 
-AdminTextSetting.propTypes = {
-    ...TextSetting.propTypes,
-    setByEnv: PropTypes.bool.isRequired,
-};
 
 export default AdminTextSetting;
