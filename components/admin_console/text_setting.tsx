75660c707da9276af5d071a17569f2d4154cccd4
Allen Lai <allenplai@gmail.com>
2019-11-12 21:31:42 -0800

    lint

--- components/admin_console/text_setting.tsx
+++ components/admin_console/text_setting.tsx
@@ -2,27 +2,27 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
+
 import TextSetting from 'components/widgets/settings/text_setting';
+
 import SetByEnv from './set_by_env';
 
 interface Props {
     setByEnv: boolean;
     disabled?: boolean;
 }
-
-const AdminTextSetting: React.SFC<Props> = props => {
+const AdminTextSetting: React.SFC<Props> = (props)=> {
     const { setByEnv, disabled, ...sharedProps } = props;
 
     return (
         <TextSetting
             {...sharedProps}
-            labelClassName="col-sm-4"
-            inputClassName="col-sm-8"
+            labelClassName='col-sm-4'
+            inputClassName='col-sm-8'
             disabled={disabled || setByEnv}
-            footer={setByEnv ? <SetByEnv /> : null}
+            footer={setByEnv ? <SetByEnv/> : null}
         />
     );
 };
-
 
 export default AdminTextSetting;
