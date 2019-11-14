d7a392aa38279e51aae50c6621586c9eec5df125
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2019-11-14 10:50:51 -0500

    Address PR comments and add cyrpress test

--- components/admin_console/admin_definition.jsx
+++ components/admin_console/admin_definition.jsx
@@ -1689,7 +1689,7 @@ const AdminDefinition = {
                         label: t('admin.lockTeammateNameDisplay'),
                         label_default: 'Lock Teammate Name Display for all users: ',
                         help_text: t('admin.lockTeammateNameDisplayHelpText'),
-                        help_text_default: 'When true, disables users ability to change settings under Main Menu > Accounts Settings > Display > Teammate Name Display.',
+                        help_text_default: 'When true, disables users\' ability to change settings under Main Menu > Account Settings > Display > Teammate Name Display.',
                     },
                     {
                         type: Constants.SettingsTypes.TYPE_PERMISSION,
