a253c06d724cc2a524ff6cbbb263f1455efe33c3
Allen Lai <allenplai@gmail.com>
2019-11-14 18:27:34 -0800

    add unit test

---
+++ components/admin_console/text_setting.test.tsx
@@ -0,0 +1,34 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import AdminTextSetting from './text_setting';
+
+describe('components/admin_console/TextSetting', () => {
+    test('render component with required props', () => {
+        const onChange = jest.fn();
+        const wrapper = shallow(
+            <AdminTextSetting
+                id='string.id'
+                label='some label'
+                value='some value'
+                setByEnv={false}
+            />
+        );
+        expect(wrapper).toMatchInlineSnapshot(`
+<TextSetting
+  disabled={false}
+  footer={null}
+  id="string.id"
+  inputClassName="col-sm-8"
+  label="some label"
+  labelClassName="col-sm-4"
+  maxLength={-1}
+  resizable={true}
+  type="input"
+  value="some value"
+/>`
+        );
+    });
+});
