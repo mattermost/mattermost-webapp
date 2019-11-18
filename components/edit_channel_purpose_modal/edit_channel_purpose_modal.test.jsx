7f4ad5d4805de5bfdd03e1e72c009af9d7de5d83
sudheer <sudheer.105@gmail.com>
2019-11-19 00:26:21 +0700

    Fix lint

--- components/edit_channel_purpose_modal/edit_channel_purpose_modal.test.jsx
+++ components/edit_channel_purpose_modal/edit_channel_purpose_modal.test.jsx
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 import React from 'react';
+
 import {shallowWithIntl} from 'tests/helpers/intl-test-helper';
 import EditChannelPurposeModal from 'components/edit_channel_purpose_modal/edit_channel_purpose_modal.jsx';
 import Constants from 'utils/constants';
