d778a8657e7baf880ba509e29eef1e7eb54e45cb
Allen Lai <allenplai@gmail.com>
2019-11-18 17:50:23 -0800

    fix searching within channel

--- components/suggestion/search_channel_provider.jsx
+++ components/suggestion/search_channel_provider.jsx
@@ -92,7 +92,7 @@ export default class SearchChannelProvider extends Provider {
                     // MM-12677 When this is migrated this needs to be fixed to pull the user's locale
                     //
                     const channels = data.sort(sortChannelsByTypeAndDisplayName.bind(null, 'en'));
-                    const channelNames = channels.map(itemToName);
+                    const channelNames = channels.map((channel) => channel.name);
 
                     resultsCallback({
                         matchedPretext: channelPrefix,
