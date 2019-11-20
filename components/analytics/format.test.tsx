a01f095e2e7cf1130ba3288ee8b9ecffafb6ac27
Eli Yukelzon <reflog@gmail.com>
2019-11-20 10:55:38 +0200

    lint

--- components/analytics/format.test.tsx
+++ components/analytics/format.test.tsx
@@ -7,21 +7,20 @@ describe('components/analytics/format.tsx', () => {
     test('should syncronize chart date ranges', () => {
         const data1 = {
             datasets: [
-                {data: [1,2]}
+                {data: [1, 2]}
             ],
-            labels: ["date1", "date2"],
+            labels: ['date1', 'date2'],
         };
         const data2 = {
             datasets: [],
             labels: [],
         };
         const data3 = {
-            datasets: [{data:[3]}],
-            labels: ["date1"],
+            datasets: [{data: [3]}],
+            labels: ['date1'],
         };
         synchronizeChartData(data1, data2, data3);
         expect(data2.labels.length).toBe(0);
         expect(data3.labels).toEqual(data1.labels);
     });
-
 });
