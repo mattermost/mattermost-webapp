0ea7d7ca03399d618a3196cc207ef46c937510ef
Jesús Espino <jespinog@gmail.com>
2019-11-21 22:01:43 +0100

    Fixing cypress tests

--- e2e/cypress/integration/enterprise/guest_accounts/member_invitation_ui_spec.js
+++ e2e/cypress/integration/enterprise/guest_accounts/member_invitation_ui_spec.js
@@ -17,13 +17,15 @@ let testTeam;
 const user1 = users['user-1'];
 const sysadmin = users.sysadmin;
 
-function invitePeople(typeText, resultsCount, verifyText) {
+function invitePeople(typeText, resultsCount, verifyText, skipInitialStep = false) {
     // # Open Invite People
     cy.get('#sidebarHeaderDropdownButton').should('be.visible').click();
     cy.get('#invitePeople').should('be.visible').click();
 
-    // #Click on the next icon to invite new member
-    cy.findByTestId('inviteMembersLink').find('.arrow').click();
+    if (!skipInitialStep) {
+        // #Click on the next icon to invite new member
+        cy.findByTestId('inviteMembersLink').find('.arrow').click();
+    }
 
     // # Search and add a member
     cy.findByTestId('inputPlaceholder').should('be.visible').within(($el) => {
@@ -131,13 +133,13 @@ describe('Guest Account - Member Invitation Flow', () => {
 
         // * Verify the header has changed in the modal
         cy.findByTestId('invitationModal').within(($el) => {
-            cy.wrap($el).find('h1').should('have.text', 'Invite Members');
+            cy.wrap($el).find('h1').should('have.text', 'Invite Members to Test Team');
         });
 
         // * Verify Share Link Header and helper text
         cy.findByTestId('shareLink').should('be.visible').within(($el) => {
             cy.wrap($el).find('h2 > span').should('have.text', 'Share This Link');
-            cy.wrap($el).find('.help-text > span').should('have.text', 'Share this link to grant member access to this team.');
+            cy.wrap($el).find('.help-text > span').should('have.text', 'Share this link to invite people to this team.');
         });
 
         // * Verify Share Link Input field
@@ -154,8 +156,8 @@ describe('Guest Account - Member Invitation Flow', () => {
 
         // * Verify Invite People field
         cy.findByTestId('searchAdd').should('be.visible').within(($el) => {
-            cy.wrap($el).find('h2 > span').should('have.text', 'Invite People');
-            cy.wrap($el).find('.help-text > span').should('have.text', 'Search and add members from other teams or email invite new users.');
+            cy.wrap($el).find('h2 > span').should('have.text', 'Add or Invite People');
+            cy.wrap($el).find('.help-text > span').should('have.text', 'Add existing members or send email invites to new members.');
         });
         cy.get('#inviteMembersButton').scrollIntoView().should('be.visible').and('be.disabled');
         cy.findByTestId('inputPlaceholder').should('be.visible').within(($el) => {
@@ -197,20 +199,20 @@ describe('Guest Account - Member Invitation Flow', () => {
         });
 
         // # Search and add an existing member by username who is part of the team
-        invitePeople(sysadmin.username, 1, sysadmin.username);
+        invitePeople(sysadmin.username, 1, sysadmin.username, true);
 
         // * Verify the content and message in next screen
         verifyInvitationError(sysadmin.username, 'This person is already a team member.');
 
         // # Search and add an existing member by email who is not part of the team
-        invitePeople(user1.email, 1, user1.username);
+        invitePeople(user1.email, 1, user1.username, true);
 
         // * Verify the content and message in next screen
         verifyInvitationSuccess(user1.username, 'This member has been added to the team.');
 
         // # Search and add a new member by email who is not part of the team
         const email = `temp-${getRandomInt(9999).toString()}@mattermost.com`;
-        invitePeople(email, 1, email);
+        invitePeople(email, 1, email, true);
 
         // * Verify the content and message in next screen
         verifyInvitationSuccess(email, 'An invitation email has been sent.');
