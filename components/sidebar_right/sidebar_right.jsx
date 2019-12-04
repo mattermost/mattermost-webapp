4c997c31c647001820a199bcc4d06940326b9532
Allen Lai <allenplai@gmail.com>
2019-12-04 05:18:36 -0800

    fix spacing and lint

--- components/sidebar_right/sidebar_right.jsx
+++ components/sidebar_right/sidebar_right.jsx
@@ -120,7 +120,12 @@ export default class SidebarRight extends React.PureComponent {
 
         var searchForm = null;
         if (currentUserId) {
-            searchForm = <SearchBar isFocus={searchVisible && !isFlaggedPosts && !isPinnedPosts} isRHS={true} />;
+            searchForm = (
+                <SearchBar 
+                    isFocus={searchVisible && !isFlaggedPosts && !isPinnedPosts}
+                    isRHS={true}
+                />
+            );
         }
 
         let channelDisplayName = '';
