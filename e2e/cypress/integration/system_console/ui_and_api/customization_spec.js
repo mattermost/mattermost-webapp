bae353e675fb6744c2c063f2171836616893db2f
lebedev.vlaimir <lebedev.vladimir3@gmail.com>
2019-12-05 04:12:38 +0300

    move test

--- e2e/cypress/integration/system_console/ui_and_api/customization_spec.js
+++ e2e/cypress/integration/system_console/ui_and_api/customization_spec.js
@@ -41,6 +41,36 @@ describe('Customization', () => {
         cy.apiUpdateConfig(origConfig);
     });
 
+    it('SC20336 - Can change Custom Brand Image setting', () => {
+        // # Make sure necessary field is false
+        cy.apiUpdateConfigBasic({TeamSettings: {EnableCustomBrand: false}});
+        cy.reload();
+
+        // # Set Enable Custom Branding to true to be able to upload custom brand image
+        cy.get('[id="TeamSettings.EnableCustomBrandtrue"]').check();
+
+        cy.findByTestId('CustomBrandImage').should('be.visible').within(() => {
+            // * Verify that setting is visible and matches text content
+            cy.get('label').should('be.visible').and('have.text', 'Custom Brand Image:');
+
+            // * Verify that help setting is visible and matches text content
+            const contents = 'Customize your user experience by adding a custom image to your login screen. Recommended maximum image size is less than 2 MB.';
+            cy.get('.help-text').should('be.visible').and('have.text', contents);
+
+            // # upload the image
+            cy.fileUpload('input');
+        });
+
+        // # save image
+        cy.get('#saveSetting').click();
+
+        // # Verify that after page reload image exist
+        cy.reload();
+        cy.findByTestId('CustomBrandImage').should('be.visible').within(() => {
+            cy.get('img').should('have.attr', 'src').and('include', '/api/v4/brand/image?t=');
+        });
+    });
+
     it('SC20335 - Can change Site Name setting', () => {
         // * Verify site name's setting name for is visible and matches the text
         cy.findByTestId('TeamSettings.SiteNamelabel').scrollIntoView().should('be.visible').and('have.text', 'Site Name:');
@@ -217,32 +247,6 @@ describe('Customization', () => {
 
         cy.apiGetConfig().then((response) => {
             expect(response.body.SupportSettings.AboutLink).to.equal(newAboutLink);
-        });
-    });
-
-    it('SC20336 - Can change Custom Brand Image setting', () => {
-        // # Set Enable Custom Branding to true to be able to upload custom brand image
-        cy.get('[id="TeamSettings.EnableCustomBrandtrue"]').check();
-
-        cy.findByTestId('CustomBrandImage').should('be.visible').within(() => {
-            // * Verify that setting is visible and matches text content
-            cy.get('label').should('be.visible').and('have.text', 'Custom Brand Image:');
-
-            // * Verify that help setting is visible and matches text content
-            const contents = 'Customize your user experience by adding a custom image to your login screen. Recommended maximum image size is less than 2 MB.';
-            cy.get('.help-text').should('be.visible').and('have.text', contents);
-
-            // # upload the image
-            cy.fileUpload('input');
-        });
-
-        // # save image
-        cy.get('#saveSetting').click();
-
-        // # Verify that after page reload image exist
-        cy.reload();
-        cy.findByTestId('CustomBrandImage').should('be.visible').within(() => {
-            cy.get('img').should('have.attr', 'src').and('include', '/api/v4/brand/image?t=');
         });
     });
 });
