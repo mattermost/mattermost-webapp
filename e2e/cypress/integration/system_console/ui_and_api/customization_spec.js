8f778e1f79f827ac8bfd960cfa013834329176dc
lebedev.vlaimir <lebedev.vladimir3@gmail.com>
2019-12-05 20:13:20 +0300

    move test

--- e2e/cypress/integration/system_console/ui_and_api/customization_spec.js
+++ e2e/cypress/integration/system_console/ui_and_api/customization_spec.js
@@ -68,6 +68,29 @@ describe('Customization', () => {
         });
     });
 
+    it('SC20330 - Can change Privacy Policy Link setting', () => {
+        // * Verify that setting is visible and matches text content
+        cy.findByTestId('SupportSettings.PrivacyPolicyLinklabel').scrollIntoView().should('be.visible').and('have.text', 'Privacy Policy Link:');
+
+        // * Verify that help setting is visible and matches text content
+        const content = 'The URL for the Privacy link on the login and sign-up pages. If this field is empty, the Privacy link is hidden from users.';
+        cy.findByTestId('SupportSettings.PrivacyPolicyLinkhelp-text').scrollIntoView().find('span').should('be.visible').and('have.text', content);
+
+        // * Verify the input box visible and has default value
+        cy.findByTestId('SupportSettings.PrivacyPolicyLinkinput').scrollIntoView().should('have.value', origConfig.SupportSettings.PrivacyPolicyLink).and('be.visible');
+
+        // # Fill input field with value
+        const stringToSave = 'https://some.com';
+        cy.findByTestId('SupportSettings.PrivacyPolicyLinkinput').clear().type(stringToSave);
+
+        cy.get('#saveSetting').click();
+
+        // * Verify that the value is save, directly via REST API
+        cy.apiGetConfig().then((response) => {
+            expect(response.body.SupportSettings.PrivacyPolicyLink).to.equal(stringToSave);
+        });
+    });
+
     it('SC20337 Can change Support Email setting', () => {
         // # Scroll Support Email section into view and verify that it's visible
         cy.findByTestId('SupportSettings.SupportEmail').scrollIntoView().should('be.visible');
@@ -218,29 +241,6 @@ describe('Customization', () => {
 
         cy.apiGetConfig().then((response) => {
             expect(response.body.SupportSettings.AboutLink).to.equal(newAboutLink);
-        });
-    });
-
-    it('SC20330 - Can change Privacy Policy Link setting', () => {
-        // * Verify that setting is visible and matches text content
-        cy.findByTestId('SupportSettings.PrivacyPolicyLinklabel').scrollIntoView().should('be.visible').and('have.text', 'Privacy Policy Link:');
-
-        // * Verify that help setting is visible and matches text content
-        const content = 'The URL for the Privacy link on the login and sign-up pages. If this field is empty, the Privacy link is hidden from users.';
-        cy.findByTestId('SupportSettings.PrivacyPolicyLinkhelp-text').scrollIntoView().find('span').should('be.visible').and('have.text', content);
-
-        // * Verify the input box visible and has default value
-        cy.findByTestId('SupportSettings.PrivacyPolicyLinkinput').scrollIntoView().should('have.value', origConfig.SupportSettings.PrivacyPolicyLink).and('be.visible');
-
-        // # Fill input field with value
-        const stringToSave = 'https://some.com';
-        cy.findByTestId('SupportSettings.PrivacyPolicyLinkinput').clear().type(stringToSave);
-
-        cy.get('#saveSetting').click();
-
-        // * Verify that the value is save, directly via REST API
-        cy.apiGetConfig().then((response) => {
-            expect(response.body.SupportSettings.PrivacyPolicyLink).to.equal(stringToSave);
         });
     });
 });
