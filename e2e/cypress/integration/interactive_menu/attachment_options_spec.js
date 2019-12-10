5e51f5a1c29e0c4f0c96dc0fd30c333a76dad0ae
zubair <m-zubairahmed@protonmail.com>
2019-12-10 16:43:00 +0530

    added the test

--- e2e/cypress/integration/interactive_menu/attachment_options_spec.js
+++ e2e/cypress/integration/interactive_menu/attachment_options_spec.js
@@ -11,6 +11,8 @@
 * Note: This test requires webhook server running. Initiate `npm run start:webhook` to start.
 */
 
+import {getMessageMenusPayload} from '../../utils';
+
 let channelId;
 let incomingWebhook;
 
@@ -50,6 +52,42 @@ describe('Interactive Menu', () => {
     });
 
     it('IM21037 - Clicking in / Tapping on the message attachment menu box opens list of selections', () => {
-        
+        const menuOptions = [
+            {text: 'Option 1', value: 'option1'},
+            {text: 'Option 2', value: 'option2'},
+            {text: 'Option 3', value: 'option3'},
+        ];
+
+        const menuOptionsPayload = getMessageMenusPayload({menuOptions});
+
+        // # Create a message attachment with menu
+        cy.postIncomingWebhook({url: incomingWebhook.url, data: menuOptionsPayload});
+
+        // # Get the last posted message id
+        cy.getLastPostId().then((lastPostId) => {
+            // # Get the last messages attachment container and alias it to 'messageAttachmentList' for later use
+            cy.get(`#messageAttachmentList_${lastPostId}`).as('messageAttachmentList');
+        });
+
+        // # Get the last message attachment menu container
+        cy.get('@messageAttachmentList').within(() => {
+            // * Message attachment menu dropdown should not closed
+            cy.get('#suggestionList').should('not.be.visible');
+
+            // # Open the message attachment menu dropdown
+            cy.get('.select-suggestion-container > input').click();
+
+            // * Message attachment menu dropdown should open
+            cy.get('#suggestionList').should('be.visible');
+
+            // Loop through options
+            cy.get('#suggestionList').children().each(($elem, index) => {
+                // * Each dropdown should contain the options text in same order
+                cy.wrap($elem).should('have.text', menuOptions[index].text);
+            });
+        });
+
+        // # Close message attachment menu dropdown
+        cy.get('body').click();
     });
 });
