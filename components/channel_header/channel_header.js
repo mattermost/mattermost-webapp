2b5c06d20d5ed4e7f6a0afb34dba26a9f94780cc
Guillermo Vaya <guivaya@gmail.com>
2019-12-11 13:54:07 +0100

    [MM-20986] close modal on focus change due to shortcut

--- components/channel_header/channel_header.js
+++ components/channel_header/channel_header.js
@@ -201,7 +201,12 @@ export default class ChannelHeader extends React.PureComponent {
         if (Utils.cmdOrCtrlPressed(e) && e.shiftKey) {
             if (Utils.isKeyPressed(e, Constants.KeyCodes.M)) {
                 e.preventDefault();
+                this.closeQuickSwitchModalIfOpen();
                 this.searchMentions(e);
+            }
+            if (Utils.isKeyPressed(e, Constants.KeyCodes.L)) {
+                // just close the modal if it's open, but let someone else handle the shortcut
+                this.closeQuickSwitchModalIfOpen();
             }
         }
     };
@@ -224,6 +229,13 @@ export default class ChannelHeader extends React.PureComponent {
                 e.preventDefault();
                 this.toggleQuickSwitchModal();
             }
+        }
+    }
+
+    closeQuickSwitchModalIfOpen = () => {
+        const {isQuickSwitcherOpen} = this.props;
+        if (isQuickSwitcherOpen) {
+            this.props.actions.closeModal(ModalIdentifiers.QUICK_SWITCH);
         }
     }
 
