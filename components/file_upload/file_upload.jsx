56377b0f6b3601d962a3317306671f98a52dac46
Mattermost Build <build@mattermost.com>
2019-12-12 19:42:30 +0100

    Automated cherry pick of #4499 (#4510)
    * MM-20939 - File picker opens twice when trying to attach a file through the paperclip button in mobile view
    
    * feedback

--- components/file_upload/file_upload.jsx
+++ components/file_upload/file_upload.jsx
@@ -571,9 +571,10 @@ export default class FileUpload extends PureComponent {
     }
 
     render() {
+        const isMobile = isMobileApp();
         const {formatMessage} = this.context.intl;
         let multiple = true;
-        if (isMobileApp()) {
+        if (isMobile) {
             // iOS WebViews don't upload videos properly in multiple mode
             multiple = false;
         }
@@ -597,8 +598,8 @@ export default class FileUpload extends PureComponent {
                         id='fileUploadButton'
                         aria-label={ariaLabel}
                         className='style--none post-action icon icon--attachment'
-                        onClick={this.simulateInputClick}
-                        onTouchEnd={this.simulateInputClick}
+                        onClick={!isMobile && this.simulateInputClick}
+                        onTouchEnd={isMobile && this.simulateInputClick}
                     >
                         <AttachmentIcon/>
                     </button>
@@ -669,8 +670,8 @@ export default class FileUpload extends PureComponent {
                             <li>
                                 <a
                                     href='#'
-                                    onClick={this.simulateInputClick}
-                                    onTouchEnd={this.simulateInputClick}
+                                    onClick={!isMobile && this.simulateInputClick}
+                                    onTouchEnd={isMobile && this.simulateInputClick}
                                 >
                                     <span className='margin-right'><i className='fa fa-laptop'/></span>
                                     <FormattedMessage
