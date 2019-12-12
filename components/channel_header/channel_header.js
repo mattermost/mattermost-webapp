ffb0d5770a4be661eac958dfd23245889877fb02
Guillermo Vaya <guivaya@gmail.com>
2019-12-12 10:51:20 +0100

    address CR comments

--- components/channel_header/channel_header.js
+++ components/channel_header/channel_header.js
@@ -201,12 +201,12 @@ export default class ChannelHeader extends React.PureComponent {
         if (Utils.cmdOrCtrlPressed(e) && e.shiftKey) {
             if (Utils.isKeyPressed(e, Constants.KeyCodes.M)) {
                 e.preventDefault();
-                this.closeQuickSwitchModalIfOpen();
+                this.props.actions.closeModal(ModalIdentifiers.QUICK_SWITCH);
                 this.searchMentions(e);
             }
             if (Utils.isKeyPressed(e, Constants.KeyCodes.L)) {
                 // just close the modal if it's open, but let someone else handle the shortcut
-                this.closeQuickSwitchModalIfOpen();
+                this.props.actions.closeModal(ModalIdentifiers.QUICK_SWITCH);
             }
         }
     };
@@ -229,13 +229,6 @@ export default class ChannelHeader extends React.PureComponent {
                 e.preventDefault();
                 this.toggleQuickSwitchModal();
             }
-        }
-    }
-
-    closeQuickSwitchModalIfOpen = () => {
-        const {isQuickSwitcherOpen} = this.props;
-        if (isQuickSwitcherOpen) {
-            this.props.actions.closeModal(ModalIdentifiers.QUICK_SWITCH);
         }
     }
 
