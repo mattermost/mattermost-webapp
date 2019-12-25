eeff8ffab0810e1e8c087de6dc3e61674acfcbba
zubair <m-zubairahmed@protonmail.com>
2019-12-25 15:06:08 +0530

    removed mistakenly added eslint rule

--- components/tutorial/tutorial_tip/tutorial_tip.tsx
+++ components/tutorial/tutorial_tip/tutorial_tip.tsx
@@ -1,6 +1,5 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-/* eslint-disable react/require-optimization */
 
 import React from 'react';
 import {Overlay} from 'react-bootstrap';
