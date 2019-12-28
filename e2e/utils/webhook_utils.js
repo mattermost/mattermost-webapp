765d2734f21f767bae6df17c8753775953bd6f60
Daniel Espino <larkox@gmail.com>
2019-12-28 09:11:12 -0500

    Write an automated test using Cypress for "Individual "User" and "Channel" screens are scrollable

--- e2e/utils/webhook_utils.js
+++ e2e/utils/webhook_utils.js
@@ -189,7 +189,54 @@ function getSimpleDialog(triggerId, webhookBaseUrl) {
     };
 }
 
+function getUserAndChannelDialog(triggerId, webhookBaseUrl) {
+    return {
+        trigger_id: triggerId,
+        url: `${webhookBaseUrl}/dialog_submit`,
+        dialog: {
+            callback_id: 'somecallbackid',
+            title: 'Title for Dialog Test with user and channel element',
+            icon_url:
+                'http://www.mattermost.org/wp-content/uploads/2016/04/icon.png',
+            submit_label: 'Submit Test',
+            notify_on_cancel: true,
+            state: 'somestate',
+            elements: [
+                {
+                    display_name: 'User Selector',
+                    name: 'someuserselector',
+                    type: 'select',
+                    subtype: '',
+                    default: '',
+                    placeholder: 'Select a user...',
+                    help_text: '',
+                    optional: false,
+                    min_length: 0,
+                    max_length: 0,
+                    data_source: 'users',
+                    options: null,
+                },
+                {
+                    display_name: 'Channel Selector',
+                    name: 'somechannelselector',
+                    type: 'select',
+                    subtype: '',
+                    default: '',
+                    placeholder: 'Select a channel...',
+                    help_text: 'Choose a channel from the list.',
+                    optional: true,
+                    min_length: 0,
+                    max_length: 0,
+                    data_source: 'channels',
+                    options: null,
+                },
+            ],
+        }
+    };
+}
+
 module.exports = {
     getFullDialog,
     getSimpleDialog,
+    getUserAndChannelDialog,
 };
