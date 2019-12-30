60c407f57c1c0813dce0acbe31b5cf5f269b509b
ctmusicnz <ctmusicnz@gmail.com>
2019-12-31 12:56:29 +1300

    replaced any types with approiate types and passed tests, type checks and linting

--- components/admin_console/blockable_link/blockable_link.tsx
+++ components/admin_console/blockable_link/blockable_link.tsx
@@ -1,43 +1,50 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import {NavLink} from 'react-router-dom';
 
-import { browserHistory } from "utils/browser_history";
+import {browserHistory} from 'utils/browser_history';
 
 type Props = {
-  /*
+
+    /*
    * Bool whether navigation is blocked
    */
-  blocked: boolean;
-  /*
+    blocked: boolean;
+
+    /*
    * String Link destination
    */
-  to: string;
-  actions: {
-    /*
+    to: string;
+    actions: {
+
+        /*
      * Function for deferring navigation while blocked
      */
-    deferNavigation: (func: () => void) => void;
-  };
+        deferNavigation: (func: () => void) => void;
+    };
 };
 export default class BlockableLink extends React.PureComponent<Props> {
-  handleClick = (e: any) => {
-    if (this.props.blocked) {
-      e.preventDefault();
+    handleClick = (e: React.MouseEvent) => {
+        if (this.props.blocked) {
+            e.preventDefault();
 
-      this.props.actions.deferNavigation(() => {
-        browserHistory.push(this.props.to);
-      });
+            this.props.actions.deferNavigation(() => {
+                browserHistory.push(this.props.to);
+            });
+        }
+    };
+
+    public render() {
+        const props = {...this.props};
+        Reflect.deleteProperty(props, 'blocked');
+        Reflect.deleteProperty(props, 'actions');
+
+        return (
+            <NavLink
+                {...props}
+                onClick={this.handleClick}
+            />);
     }
-  };
-
-  public render() {
-    const props = { ...this.props };
-    Reflect.deleteProperty(props, "blocked");
-    Reflect.deleteProperty(props, "actions");
-
-    return <NavLink {...props} onClick={this.handleClick} />;
-  }
 }
--- components/admin_console/blockable_link/index.ts
+++ components/admin_console/blockable_link/index.ts
@@ -1,32 +1,33 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import {connect} from 'react-redux';
+import {bindActionCreators, Dispatch} from 'redux';
+import {GenericAction} from 'mattermost-redux/types/actions';
 
-import { deferNavigation } from "actions/admin_actions";
-import { getNavigationBlocked } from "selectors/views/admin";
+import {deferNavigation} from 'actions/admin_actions';
+import {getNavigationBlocked} from 'selectors/views/admin';
 
-import BlockableLink from "./blockable_link";
+import BlockableLink from './blockable_link';
 
 function mapStateToProps(state: object) {
-  return {
-    blocked: getNavigationBlocked(state)
-  };
+    return {
+        blocked: getNavigationBlocked(state)
+    };
 }
 
-function mapDispatchToProps(dispatch: any) {
-  return {
-    actions: bindActionCreators(
-      {
-        deferNavigation
-      },
-      dispatch
-    )
-  };
+function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
+    return {
+        actions: bindActionCreators(
+            {
+                deferNavigation
+            },
+            dispatch
+        )
+    };
 }
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(BlockableLink);
