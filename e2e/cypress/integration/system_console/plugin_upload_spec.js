9cf915e919da074f9ea0efec4c205eb06e8ec24a
nirosha.v <nirosha.v@ezetap.com>
2020-01-03 15:28:11 +0530

    Added few more validations to DrawPlugin upload test via UI

--- e2e/cypress/integration/system_console/plugin_upload_spec.js
+++ e2e/cypress/integration/system_console/plugin_upload_spec.js
@@ -9,44 +9,6 @@
 
 import * as TIMEOUTS from '../../fixtures/timeouts';
 
-/**
- * Method to enable/Disable/Remove draw plugin
- * @param {String} status - Action to be performed on draw plugin help text [Remove/Enable/Disable]
- * @param {String} fileName - Filename to upload from the fixture
- * @param {String} fileType - formation of the file - 'application/gzip'
- */
-function drawpluginConfiguration(status, fileName, fileType) {
-    //Navigate to system console - Plugin Management - Enable Draw plugin
-    cy.wait(TIMEOUTS.TINY);
-    switch (status) {
-    case 'Remove':
-        cy.findByTestId('com.mattermost.draw-plugin').should('be.visible').within(() => {
-            cy.findByText('Remove').click();
-        });
-        cy.findByText('Are you sure you would like to remove the plugin?').should('be.visible');
-        cy.get('#confirmModalButton').should('be.visible').click();
-        cy.findByTestId('com.mattermost.draw-plugin').should('not.exist');
-        break;
-    case 'Enable':
-        cy.get('input[type=file]').uploadFile(fileName, fileType).wait(TIMEOUTS.TINY);
-        cy.get('#uploadPlugin').should('be.visible').click().wait(TIMEOUTS.TINY);
-        cy.wait(TIMEOUTS.TINY);
-        cy.findByTestId('com.mattermost.draw-plugin').should('be.visible').within(() => {
-            cy.findByText('Enable').click();
-            cy.findByText('This plugin is running.').should('be.visible');
-        });
-        break;
-    case 'Disable':
-        cy.findByTestId('com.mattermost.draw-plugin').should('be.visible').within(() => {
-            cy.findByText('Disable').click().wait(TIMEOUTS.TINY);
-
-            //Timeout issue is happening , will fix
-            //cy.findByText('This plugin is not enabled.').should('be.visible');
-        });
-        break;
-    }
-}
-
 describe('Draw Plugin - Upload', () => {
     const fileName = 'com.mattermost.draw-plugin.tar.gz';
     const fileType = 'application/gzip';
@@ -72,9 +34,37 @@ describe('Draw Plugin - Upload', () => {
      * Draw Plugin configuration UI test - For Admin Access
     */
     it('M11759-Draw plugin Configuration - should upload draw plugin', () => {
-        // * check Upload/enable/Disable/Remove functionality of drawl plugin
-        drawpluginConfiguration('Enable', fileName, fileType);
-        drawpluginConfiguration('Disable', fileName, fileType);
-        drawpluginConfiguration('Remove', fileName, fileType);
+        // * upload Draw plugin from the browser
+        cy.get('input[type=file]').uploadFile(fileName, fileType).wait(TIMEOUTS.TINY);
+        cy.get('#uploadPlugin').should('be.visible').click().wait(TIMEOUTS.TINY);
+        cy.wait(TIMEOUTS.TINY);
+
+        // # Draw plugin ID should be visible
+        cy.findByTestId('com.mattermost.draw-plugin').should('be.visible').within(() => {
+            // #Enable draw plugin and check plugin is running
+            cy.findByText('Enable').click();
+            cy.findByText('This plugin is running.').should('be.visible');
+
+            // #Disable draw plugin and check plugin is not enabled
+            cy.findByText('Disable').click();
+            cy.findByText('This plugin is not enabled.').should('be.visible');
+
+            // * Click on remove
+            cy.findByText('Remove').click();
+        });
+
+        // #Remove plugin Id should exist upon clicking Cancel in confirmation popup
+        cy.get('#cancelModalButton').should('be.visible').click();
+        cy.findByTestId('com.mattermost.draw-plugin').should('exist');
+
+        cy.findByTestId('com.mattermost.draw-plugin').should('be.visible').within(() => {
+            // * Click on remove
+            cy.findByText('Remove').click();
+        });
+
+        // #Remove plugin Id should not exist upon clicking remove in confirmation popup
+        cy.findByText('Are you sure you would like to remove the plugin?').should('be.visible');
+        cy.get('#confirmModalButton').should('be.visible').click();
+        cy.findByTestId('com.mattermost.draw-plugin').should('not.exist');
     });
 });
