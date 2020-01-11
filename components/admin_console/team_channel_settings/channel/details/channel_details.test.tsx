e201ff36ca70243acaeca106df1c852d60c3ed4c
Allen Lai <allenplai@gmail.com>
2020-01-11 15:42:51 -0800

    chanel_detail to TS

---
+++ components/admin_console/team_channel_settings/channel/details/channel_details.test.tsx
@@ -0,0 +1,72 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import {Group} from 'mattermost-redux/types/groups';
+import {Channel} from 'mattermost-redux/types/channels';
+
+import ChannelDetails from './channel_details';
+
+describe('admin_console/team_channel_settings/channel/ChannelDetails', () => {
+    test('should match snapshot', () => {
+        const groups: Partial<Group>[] = [{
+            id: '123',
+            display_name: 'DN',
+            member_count: 3,
+        }];
+        const allGroups = {
+            123: groups[0],
+        };
+        const testChannel: Partial<Channel> = {
+            id: '123',
+            team_name: 'team',
+            type: 'O',
+            group_constrained: false,
+            name: 'DN',
+        };
+        const team = {
+            display_name: 'test',
+        };
+
+        const actions = {
+            getChannel: jest.fn().mockResolvedValue([]),
+            getTeam: jest.fn().mockResolvedValue([]),
+            linkGroupSyncable: jest.fn(),
+            conver: jest.fn(),
+            patchChannel: jest.fn(),
+            setNavigationBlocked: jest.fn(),
+            unlinkGroupSyncable: jest.fn(),
+            getGroups: jest.fn().mockResolvedValue([]),
+            membersMinusGroupMembers: jest.fn(),
+            updateChannelPrivacy: jest.fn(),
+        };
+
+        let wrapper = shallow(
+            <ChannelDetails
+                groups={groups}
+                team={team}
+                totalGroups={groups.length}
+                actions={actions}
+                channel={testChannel}
+                channelID={testChannel.id}
+                allGroups={allGroups}
+            />
+        );
+        expect(wrapper).toMatchSnapshot();
+
+        wrapper = shallow(
+            <ChannelDetails
+                groups={groups}
+                team={{}}
+                totalGroups={groups.length}
+                actions={actions}
+                channel={testChannel}
+                channelID={testChannel.id}
+                allGroups={allGroups}
+            />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+});
