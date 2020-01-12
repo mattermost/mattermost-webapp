743a44a17a321917be85a4b3799b9e38be5c51d2
Allen Lai <allenplai@gmail.com>
2020-01-11 18:59:17 -0800

    to TS

--- components/setting_item_min/index.ts
+++ components/setting_item_min/index.ts
@@ -2,20 +2,23 @@
 // See LICENSE.txt for license information.
 
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import {bindActionCreators, Dispatch} from 'redux';
+
+import {GenericAction} from 'mattermost-redux/types/actions';
+import {GlobalState} from 'mattermost-redux/types/store';
 
 import {getPreviousActiveSection} from 'selectors/views/settings';
 import {updateActiveSection} from 'actions/views/settings';
 
 import SettingItemMin from './setting_item_min.jsx';
 
-function mapStateToProps(state) {
+function mapStateToProps(state: GlobalState) {
     return {
         previousActiveSection: getPreviousActiveSection(state),
     };
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
     return {
         actions: bindActionCreators({
             updateActiveSection,
