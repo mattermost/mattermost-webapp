253a298eff3483e13250bea4292d1d9f0db2d748
Catalin Tomai <catalin.tomai@mattermost.com>
2020-01-13 18:08:35 -0800

    MM-20653: fix lint issues

--- components/channel_groups_manage_modal/index.js
+++ components/channel_groups_manage_modal/index.js
@@ -5,6 +5,7 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {getGroupsAssociatedToChannel, unlinkGroupSyncable, patchGroupSyncable} from 'mattermost-redux/actions/groups';
 import {getMyChannelMember} from 'mattermost-redux/actions/channels';
+
 import {closeModal, openModal} from 'actions/views/modals';
 
 import ChannelGroupsManageModal from './channel_groups_manage_modal';
--- components/team_groups_manage_modal/index.js
+++ components/team_groups_manage_modal/index.js
@@ -5,6 +5,7 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {getGroupsAssociatedToTeam, unlinkGroupSyncable, patchGroupSyncable} from 'mattermost-redux/actions/groups';
 import {getMyTeamMembers} from 'mattermost-redux/actions/teams';
+
 import {closeModal, openModal} from 'actions/views/modals';
 
 import TeamGroupsManageModal from './team_groups_manage_modal';
