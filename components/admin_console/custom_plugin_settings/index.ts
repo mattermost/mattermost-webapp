8708e36522259d0dd0a1f5b952e68758c56d9905
arka204 <kmichalik204@gmail.com>
2020-01-14 17:59:36 -0800

    Deleting extension.

--- components/admin_console/custom_plugin_settings/index.ts
+++ components/admin_console/custom_plugin_settings/index.ts
@@ -13,7 +13,7 @@ import {getAdminConsoleCustomComponents} from 'selectors/admin_console.jsx';
 import SchemaAdminSettings from '../schema_admin_settings';
 import {it} from '../admin_definition';
 
-import CustomPluginSettings from './custom_plugin_settings.jsx';
+import CustomPluginSettings from './custom_plugin_settings';
 import getEnablePluginSetting from './enable_plugin_setting';
 
 function makeGetPluginSchema() {
