de1219ee2ca457a50aff576d366af9b226b6ec2b
Allen Lai <allenplai@gmail.com>
2020-01-18 11:44:37 -0800

    compatiable for proptypes in JSX

--- utils/react_intl.ts
+++ utils/react_intl.ts
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import PropTypes from 'prop-types';
+
 export interface IntlConfigPropTypes {
     locale: string;
     timeZone: string;
@@ -28,3 +30,33 @@ export interface IntlShape extends IntlConfigPropTypes, IntlFormatPropTypes {
     formatters: object;
     now: (...args: any[]) => any;
 }
+
+// This is still for the components that have not yet migrated to Typescript
+export const intlConfigPropTypes = {
+    locale: PropTypes.string,
+    timeZone: PropTypes.string,
+    formats: PropTypes.object,
+    messages: PropTypes.object,
+    textComponent: PropTypes.any,
+
+    defaultLocale: PropTypes.string,
+    defaultFormats: PropTypes.object,
+
+    onError: PropTypes.func,
+};
+
+export const intlFormatPropTypes = {
+    formatDate: PropTypes.func.isRequired,
+    formatTime: PropTypes.func.isRequired,
+    formatRelativeTime: PropTypes.func.isRequired,
+    formatNumber: PropTypes.func.isRequired,
+    formatPlural: PropTypes.func.isRequired,
+    formatMessage: PropTypes.func.isRequired,
+    formatHTMLMessage: PropTypes.func.isRequired,
+};
+
+export const intlShape = PropTypes.shape({
+    ...intlConfigPropTypes,
+    ...intlFormatPropTypes,
+    formatters: PropTypes.object,
+});
