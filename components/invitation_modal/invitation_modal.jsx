0b379ae3810528d9b1024f71cee3f98b29d74f2c
Mario de Frutos <mario@defrutos.org>
2020-01-28 19:44:07 +0100

    Not needed getWrappedInstance in this case

--- components/invitation_modal/invitation_modal.jsx
+++ components/invitation_modal/invitation_modal.jsx
@@ -92,21 +92,21 @@ export default class InvitationModal extends React.Component {
             this.setState({step: STEPS_INITIAL, hasChanges: false, lastInviteChannels: [], lastInviteMesssage: '', prevStep: this.state.step});
         }
         if (this.modal && this.modal.current) {
-            this.modal.current.getWrappedInstance().resetFocus();
+            this.modal.current.resetFocus();
         }
     }
 
     goToMembers = () => {
         this.setState({step: STEPS_INVITE_MEMBERS, prevStep: this.state.step, hasChanges: false, invitesSent: [], invitesNotSent: [], invitesType: InviteTypes.INVITE_MEMBER});
         if (this.modal && this.modal.current) {
-            this.modal.current.getWrappedInstance().resetFocus();
+            this.modal.current.resetFocus();
         }
     }
 
     goToGuests = () => {
         this.setState({step: STEPS_INVITE_GUESTS, prevStep: this.state.step, hasChanges: false, invitesSent: [], invitesNotSent: [], invitesType: InviteTypes.INVITE_GUEST});
         if (this.modal && this.modal.current) {
-            this.modal.current.getWrappedInstance().resetFocus();
+            this.modal.current.resetFocus();
         }
     }
 
@@ -117,7 +117,7 @@ export default class InvitationModal extends React.Component {
             this.setState({step: STEPS_INVITE_MEMBERS, prevStep: this.state.step, hasChanges: false, invitesSent: [], invitesNotSent: [], invitesType: InviteTypes.INVITE_MEMBER});
         }
         if (this.modal && this.modal.current) {
-            this.modal.current.getWrappedInstance().resetFocus();
+            this.modal.current.resetFocus();
         }
     }
 
