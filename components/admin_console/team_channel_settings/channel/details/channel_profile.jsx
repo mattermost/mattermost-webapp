9c1589428a49619d934f7dd9c13dec070bb79a47
Scott Bishel <scott.bishel@mattermost.com>
2020-01-28 17:38:58 -0700

    MM-21890: fix channel list and details (#4731)
    * fix channel list and details
    
    * update for lastest changes
    
    * address PR comments
    
    * update redux hash so checks can pass
    
    * update types
    
    * update redux hash to latest master
    
    Co-authored-by: Hossein Ahmadian-Yazdi <hahmadia@users.noreply.github.com>
    (cherry picked from commit 7cdcbadaaa9122d0a720c5bdd615724638576a12)

--- components/admin_console/team_channel_settings/channel/details/channel_profile.jsx
+++ components/admin_console/team_channel_settings/channel/details/channel_profile.jsx
@@ -26,7 +26,7 @@ export const ChannelProfile = ({team, channel}) => (
                     defaultMessage='**Name**'
                 />
                 <br/>
-                {channel.name}
+                {channel.display_name}
                 <br/>
                 <FormattedMarkdownMessage
                     id='admin.channel_settings.channel_detail.channelTeam'
