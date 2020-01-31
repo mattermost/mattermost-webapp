81f76831479eedf4a5765f3dd80de209b97207b6
lawikip <Github1324>
2020-01-31 16:33:53 -0500

    M16735: reply to bot message automation

--- e2e/cypress/integration/channel/message_reply_bot_post_spec.js
+++ e2e/cypress/integration/channel/message_reply_bot_post_spec.js
@@ -10,10 +10,12 @@
 import users from '../../fixtures/users.json';
 
 const sysadmin = users.sysadmin;
-
-describe('Message Reply with attachment pretext', () => {
+describe('Messaging', () => {
     let newChannel;
     let botsUserId;
+    let botName;
+    let botToken;
+    let yesterdaysDate;
     before(() => {
         // # Set ServiceSettings to expected values
         const newSettings = {
@@ -23,7 +25,7 @@ describe('Message Reply with attachment pretext', () => {
         };
         cy.apiUpdateConfig(newSettings);
 
-        // # Login and go to /
+        // # Login as sysadmin
         cy.apiLogin('sysadmin');
 
         // # Create and visit new channel
@@ -32,10 +34,10 @@ describe('Message Reply with attachment pretext', () => {
         });
     });
 
-    it('MM-16734 Reply to an older bot post that has some attachment pretext', () => {
+    it('M16735 - Replying to an older bot post that has no post content and no attachment pretext', () => {
         // # Get yesterdays date in UTC
-        const yesterdaysDate = Cypress.moment().subtract(1, 'days').valueOf();
-        const botName = 'bot-' + Date.now();
+        yesterdaysDate = Cypress.moment().subtract(1, 'days').valueOf();
+        botName = 'bot-' + Date.now();
 
         // # Create a bot and get userID
         cy.apiCreateBot(botName, 'Test Bot', 'test bot for E2E test replying to older bot post').then((response) => {
@@ -44,21 +46,69 @@ describe('Message Reply with attachment pretext', () => {
 
             // # Get token from bots id
             cy.apiAccessToken(botsUserId, 'Create token').then((token) => {
+                botToken = token;
+
                 //# Add bot to team
                 cy.apiAddUserToTeam(newChannel.team_id, botsUserId);
 
-                //# Add bot to channel
-                cy.apiAddUserToChannel(newChannel.id, botsUserId);
-
                 // # Post message with auth token
-                const message = 'Hello message from ' + botName;
-                const props = {attachments: [{pretext: 'Some Pretext', text: 'Some Text'}]};
-                cy.postBotMessage({token, message, props, channelId: newChannel.id, createAt: yesterdaysDate}).
+                const props = {attachments: [{text: 'Some Text posted by bot that has no content and no attachment pretext'}]};
+                cy.postBotMessage({token: botToken, props, channelId: newChannel.id, createAt: yesterdaysDate}).
                     its('id').
                     should('exist').
-                    as('yesterdaysPost');
+                    as('yesterdaysBotPost');
             });
         });
+
+        // # Add two subsequent posts
+        cy.postMessage('First posting');
+        cy.postMessage('Another one Posted');
+
+        cy.get('@yesterdaysBotPost').then((postId) => {
+            // # Open RHS comment menu
+            cy.clickPostCommentIcon(postId);
+
+            // # Reply to message
+            cy.postMessageReplyInRHS('A reply to an older post bot post');
+
+            // # Get the latest reply post
+            cy.getLastPostId().then((replyId) => {
+                // * Verify that the reply is in the RHS with matching text
+                cy.get(`#rhsPost_${replyId}`).within(() => {
+                    cy.queryByTestId('post-link').should('not.be.visible');
+                    cy.get(`#rhsPostMessageText_${replyId}`).should('be.visible').and('have.text', 'A reply to an older post bot post');
+                });
+
+                cy.get(`#CENTER_time_${postId}`).find('time').invoke('attr', 'title').then((originalTimeStamp) => {
+                    // * Verify the first post timestamp equals the RHS timestamp
+                    cy.get(`#RHS_ROOT_time_${postId}`).find('time').invoke('attr', 'title').should('be', originalTimeStamp);
+
+                    // * Verify the first post timestamp was not modified by the reply
+                    cy.get(`#CENTER_time_${replyId}`).find('time').should('have.attr', 'title').and('not.equal', originalTimeStamp);
+                });
+
+                //# Close RHS
+                cy.closeRHS();
+
+                // * Verify that the reply is in the channel view with matching text
+                cy.get(`#post_${replyId}`).within(() => {
+                    cy.queryByTestId('post-link').should('be.visible').and('have.text', 'Commented on ' + botName + '\'s message: Some Text posted by bot that has no content and no attachment pretext');
+                    cy.get(`#postMessageText_${replyId}`).should('be.visible').and('have.text', 'A reply to an older post bot post');
+                });
+            });
+        });
+
+        // # Verify RHS is closed
+        cy.get('#rhsContainer').should('not.be.visible');
+    });
+    it('MM-16734 Reply to an older bot post that has some attachment pretext', () => {
+        // # Post message with auth token
+        const message = 'Hello message from ' + botName;
+        const props = {attachments: [{pretext: 'Some Pretext', text: 'Some Text'}]};
+        cy.postBotMessage({token: botToken, message, props, channelId: newChannel.id, createAt: yesterdaysDate}).
+            its('id').
+            should('exist').
+            as('yesterdaysPost');
 
         // # Add two subsequent posts
         cy.postMessage('First post');
