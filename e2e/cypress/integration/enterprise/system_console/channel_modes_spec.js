7c3facfb7137b6b3817e8e84711807af59c7b5b0
Rohitesh Gupta <1429138+srkgupta@users.noreply.github.com>
2020-02-12 12:01:55 +0530

    Fixed some tests based on daily runs (#4880)
    * Fixed some tests based on daily runs
    
    * Fixed a strange issue where toast notifications were not getting dismissed on Cypress invoked browser.
    
    * Adding a static wait after scrolling up instead of dynamically waiting since the loader screen is actually always visible at the top.

--- e2e/cypress/integration/enterprise/system_console/channel_modes_spec.js
+++ e2e/cypress/integration/enterprise/system_console/channel_modes_spec.js
@@ -24,7 +24,7 @@ describe('Test channel public/private toggle', () => {
             const channel = res.body;
             assert(channel.type === 'O');
             cy.visit(`/admin_console/user_management/channels/${channel.id}`);
-            cy.get('#channel_profile').contains(channel.name);
+            cy.get('#channel_profile').contains(channel.display_name);
             cy.get('#channel_manage .group-teams-and-channels--body').find('button').eq(1).click();
             cy.get('#saveSetting').click();
             cy.get('#confirmModalButton').click();
@@ -33,7 +33,7 @@ describe('Test channel public/private toggle', () => {
             const channel = res.body;
             assert(channel.type === 'P');
             cy.visit(`/admin_console/user_management/channels/${channel.id}`);
-            cy.get('#channel_profile').contains(channel.name);
+            cy.get('#channel_profile').contains(channel.display_name);
             cy.get('#channel_manage .group-teams-and-channels--body').find('button').eq(1).click();
             cy.get('#saveSetting').click();
             cy.get('#confirmModalButton').click();
@@ -53,7 +53,7 @@ describe('Test channel public/private toggle', () => {
             const channel = res.body;
             assert(channel.type === 'O');
             cy.visit(`/admin_console/user_management/channels/${channel.id}`);
-            cy.get('#channel_profile').contains(channel.name);
+            cy.get('#channel_profile').contains(channel.display_name);
             cy.get('#channel_manage .group-teams-and-channels--body').find('button').eq(0).click();
             cy.get('#channel_manage .group-teams-and-channels--body').find('button').eq(0).click();
             cy.get('#channel_manage .group-teams-and-channels--body').find('button').eq(1).contains('Public');
@@ -70,7 +70,7 @@ describe('Test channel public/private toggle', () => {
         cy.get('#sidebarItem_town-square').scrollIntoView().click({force: true});
         cy.getCurrentChannelId().then((id) => {
             cy.visit(`/admin_console/user_management/channels/${id}`);
-            cy.get('#channel_profile').contains('town-square');
+            cy.get('#channel_profile').contains('Town Square');
             cy.get('#channel_manage .group-teams-and-channels--body').find('button').eq(0).should('have.class', 'false');
             cy.get('#channel_manage .group-teams-and-channels--body').find('button').eq(1).contains('Public');
             cy.get('#channel_manage .group-teams-and-channels--body').find('button').eq(0).should('have.class', 'disabled');
