5a0d7d981936352a872c4c3865edfb8f07002586
Vladimir Lebedev <lebedev.vladimir3@gmail.com>
2020-02-28 12:10:16 +0300

    MM-19755 Add landmark roles (#4522)
    * MM-19755 Add landmark roles
    
    * fix eslint
    
    * restore snapshot
    
    * fix current issues
    
    * fix axe's "Ensures landmarks are unique" eror
    
    * fix linter
    
    * update snapshot
    
    Co-authored-by: mattermod <mattermod@users.noreply.github.com>

--- components/post_view/post_view.jsx
+++ components/post_view/post_view.jsx
@@ -67,7 +67,10 @@ export default class PostView extends React.PureComponent {
         }
 
         return (
-            <div id='post-list'>
+            <div
+                id='post-list'
+                role='main'
+            >
                 <PostList
                     unreadChunkTimeStamp={this.state.unreadChunkTimeStamp}
                     channelId={this.props.channelId}
