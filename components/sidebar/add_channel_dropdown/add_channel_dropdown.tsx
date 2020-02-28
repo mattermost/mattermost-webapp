a66d88d486fb4c454396a5f952676d41fd49cd59
Devin Binnie <52460000+devinbinnie@users.noreply.github.com>
2020-02-28 12:37:52 -0500

    [MM-21910] Modal Handling for new Sidebar (#4847)
    * WIP
    
    * Barely working sidebar
    
    * Temp fix so you can use the sidebar better
    
    * Lint and type fixes
    
    * Unread badge and bolding class added
    
    * Split up sidebar channel types into different components
    
    * Lint/type fixes
    
    * WIP
    
    * PR feedback
    
    * Lint fix
    
    * Unread indicators and scrolling!
    
    * Auto scrolling when changing teams/when channel is in view
    
    * Lint and type fixes
    
    * Another type fix
    
    * Removing unnecessary styling
    
    * PR feedback
    
    * package fix
    
    * Added a11y classes to new sidebar
    
    * Keyboard shortcuts
    
    * Reverted to regular scrollbars
    
    * Whoops, left my dumb test styling in :P
    
    * Moved modals over to sidebar
    
    * PR feedback
    
    * Removed isSwitchingChannel
    
    * aaa
    
    * Modals functional
    
    * PR feedback
    
    * Merge'd

---
+++ components/sidebar/add_channel_dropdown/add_channel_dropdown.tsx
@@ -0,0 +1,45 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {FormattedMessage} from 'react-intl';
+
+type Props = {
+    showMoreChannelsModal: () => void;
+    showNewChannelModal: () => void;
+};
+
+type State = {
+
+};
+
+export default class AddChannelDropdown extends React.PureComponent<Props, State> {
+    render() {
+        return (
+            <div>
+                <div>
+                    <a
+                        href='#'
+                        onClick={this.props.showMoreChannelsModal}
+                    >
+                        <FormattedMessage
+                            id={'sidebar_left.add_channel_dropdown.browseChannels'}
+                            defaultMessage={'Browse Channels'}
+                        />
+                    </a>
+                </div>
+                <div>
+                    <a
+                        href='#'
+                        onClick={this.props.showNewChannelModal}
+                    >
+                        <FormattedMessage
+                            id={'sidebar_left.add_channel_dropdown.createNewChannel'}
+                            defaultMessage={'Create New Channel'}
+                        />
+                    </a>
+                </div>
+            </div>
+        );
+    }
+}
--- components/sidebar/index.ts
+++ components/sidebar/index.ts
@@ -1,16 +1,32 @@
+
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
 import {connect} from 'react-redux';
 import {bindActionCreators, Dispatch} from 'redux';
 
+import Permissions from 'mattermost-redux/constants/permissions';
+import {haveITeamPermission} from 'mattermost-redux/selectors/entities/roles';
+import {getCurrentTeam} from 'mattermost-redux/selectors/entities/teams';
 import {GlobalState} from 'mattermost-redux/types/store';
 import {GenericAction} from 'mattermost-redux/types/actions';
 
 import Sidebar from './sidebar';
 
 function mapStateToProps(state: GlobalState) {
+    const currentTeam = getCurrentTeam(state);
+
+    let canCreatePublicChannel = false;
+    let canCreatePrivateChannel = false;
+
+    if (currentTeam) {
+        canCreatePublicChannel = haveITeamPermission(state, {team: currentTeam.id, permission: Permissions.CREATE_PUBLIC_CHANNEL});
+        canCreatePrivateChannel = haveITeamPermission(state, {team: currentTeam.id, permission: Permissions.CREATE_PRIVATE_CHANNEL});
+    }
+
     return {
+        canCreatePrivateChannel,
+        canCreatePublicChannel,
     };
 }
 
