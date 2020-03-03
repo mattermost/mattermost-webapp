15b51e12090335440ced31cc013f4a6b644f5020
Anindita Basu <anindita.basu@live.com>
2020-03-03 18:10:53 +0530

    lint error
    replacing the doublequotes with singlequotes

--- components/integrations/abstract_oauth_app.jsx
+++ components/integrations/abstract_oauth_app.jsx
@@ -272,7 +272,7 @@ export default class AbstractOAuthApp extends React.PureComponent {
                         <div className='form__help'>
                             <FormattedMessage
                                 id='add_oauth_app.trusted.help'
-                                defaultMessage="If true, the OAuth 2.0 application is considered trusted by the Mattermost server and does not require the user to accept authorization. If false, a window opens to ask the user to accept or deny the authorization."
+                                defaultMessage='If true, the OAuth 2.0 application is considered trusted by the Mattermost server and does not require the user to accept authorization. If false, a window opens to ask the user to accept or deny the authorization.'
                             />
                         </div>
                     </div>
