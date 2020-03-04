ca85d3823be947bfd4facc61b5475dd2bd31d667
hiendinhngoc <hiendinhngoc@yahoo.com.vn>
2020-03-04 23:16:11 +0700

    [MM-20546]Migrate 'components/user_settings/security/mfa_section' module and associated tests to TypeScript #13695

--- components/user_settings/security/mfa_section/index.ts
+++ components/user_settings/security/mfa_section/index.ts
@@ -1,48 +1,41 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-
-import {getConfig, getLicense} from 'mattermost-redux/selectors/entities/general';
-import {getCurrentUser} from 'mattermost-redux/selectors/entities/users';
-
-import {deactivateMfa} from 'actions/views/mfa';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+import { getConfig, getLicense } from 'mattermost-redux/selectors/entities/general';
+import { getCurrentUser } from 'mattermost-redux/selectors/entities/users';
+import { deactivateMfa } from 'actions/views/mfa';
 import Constants from 'utils/constants';
-
 import MfaSection from './mfa_section';
+import {GlobalState} from 'mattermost-redux/types/store';
+import {GenericAction} from 'mattermost-redux/types/actions';
+import {UserProfile} from 'mattermost-redux/types/users';
 
-function mapStateToProps(state) {
+function mapStateToProps(state: GlobalState) {
     const license = getLicense(state);
     const config = getConfig(state);
-
     const mfaLicensed = license && license.IsLicensed === 'true' && license.MFA === 'true';
-
     const mfaEnabled = config.EnableMultifactorAuthentication === 'true';
     const mfaEnforced = mfaLicensed && config.EnforceMultifactorAuthentication === 'true';
-
-    const user = getCurrentUser(state);
-
+    const user: UserProfile & {mfa_active: boolean} = Object.assign(getCurrentUser(state));
     let mfaActive = false;
     let mfaAvailable = false;
     if (user) {
         mfaActive = user.mfa_active;
         mfaAvailable = mfaEnabled && (user.auth_service === '' || user.auth_service === Constants.LDAP_SERVICE);
     }
-
     return {
         mfaActive,
         mfaAvailable,
         mfaEnforced,
     };
 }
-
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
     return {
         actions: bindActionCreators({
             deactivateMfa,
         }, dispatch),
     };
 }
-
 export default connect(mapStateToProps, mapDispatchToProps)(MfaSection);
