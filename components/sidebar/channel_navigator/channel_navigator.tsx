d6001403ca2384fb57c00ed6e1f6845f02b2789a
Devin Binnie <52460000+devinbinnie@users.noreply.github.com>
2020-03-10 11:29:45 -0400

    Channel Switcher + History Button Style (#5023)
    * [MM-22291] Additional functionality from old sidebar
    
    * PR feedback
    
    * PR feedback
    
    * Lank bline
    
    * Lint again
    
    * PR feedback
    
    * Merge'd
    
    * Actual functioning sidebar!
    
    * Using react-custom-scrollbars again and fixed a few styling issues
    
    * PR feedback
    
    * Lint fixes
    
    * Added channel switcher modal button and back/forward buttons
    
    * Lint fixes and formatting
    
    * Styled arrows and buttons, and mock back/forward
    
    * Unmocking for Phase 1
    
    * Removed unnecessary ms-filter
    
    Co-authored-by: mattermod <mattermod@users.noreply.github.com>

--- components/sidebar/channel_navigator/channel_navigator.tsx
+++ components/sidebar/channel_navigator/channel_navigator.tsx
@@ -2,9 +2,23 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
+import {FormattedMessage} from 'react-intl';
+import classNames from 'classnames';
+
+import {ModalIdentifiers} from 'utils/constants';
+import QuickSwitchModal from 'components/quick_switch_modal';
+
+import * as Utils from 'utils/utils';
+import {isDesktopApp} from 'utils/user_agent';
 
 type Props = {
-
+    canGoForward: boolean;
+    canGoBack: boolean;
+    actions: {
+        openModal: (modalData: any) => Promise<{data: boolean}>;
+        goBack: () => void;
+        goForward: () => void;
+    };
 };
 
 type State = {
@@ -12,10 +26,65 @@ type State = {
 };
 
 export default class ChannelNavigator extends React.PureComponent<Props, State> {
+    openQuickSwitcher = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+        this.props.actions.openModal({
+            modalId: ModalIdentifiers.QUICK_SWITCH,
+            dialogType: QuickSwitchModal,
+        });
+    }
+
+    goBack = () => {
+        this.props.actions.goBack();
+    }
+
+    goForward = () => {
+        this.props.actions.goForward();
+    }
+
     render() {
+        let channelSwitchTextShortcutDefault = 'CTRL+K';
+        if (Utils.isMac()) {
+            channelSwitchTextShortcutDefault = '⌘K';
+        }
+
+        let historyArrows;
+        if (isDesktopApp()) {
+            historyArrows = (
+                <React.Fragment>
+                    <button
+                        className={classNames('SidebarChannelNavigator_backButton', {disabled: !this.props.canGoBack})}
+                        disabled={!this.props.canGoBack}
+                        onClick={this.goBack}
+                    >
+                        <i className='icon icon-arrow-left'/>
+                    </button>
+                    <button
+                        className={classNames('SidebarChannelNavigator_forwardButton', {disabled: !this.props.canGoForward})}
+                        disabled={!this.props.canGoForward}
+                        onClick={this.goForward}
+                    >
+                        <i className='icon icon-arrow-left icon-flip'/>
+                    </button>
+                </React.Fragment>
+            );
+        }
+
         return (
-            <div>
-                {'Channel Navigator'}
+            <div className={'SidebarChannelNavigator'}>
+                <button
+                    className={'SidebarChannelNavigator_jumpToButton'}
+                    onClick={this.openQuickSwitcher}
+                >
+                    <FormattedMessage
+                        id='sidebar_left.channel_navigator.jumpTo'
+                        defaultMessage='Jump to...'
+                    />
+                    <div className={'SidebarChannelNavigator_shortcutText'}>
+                        {channelSwitchTextShortcutDefault}
+                    </div>
+                </button>
+                {historyArrows}
             </div>
         );
     }
