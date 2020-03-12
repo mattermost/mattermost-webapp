194534a01a1d382423d7cb47869663e9af967404
Jesús Espino <jespinog@gmail.com>
2020-03-12 17:32:20 +0100

    Removed unnused import

--- components/post_view/post/index.js
+++ components/post_view/post/index.js
@@ -3,7 +3,6 @@
 
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {createSelector} from 'reselect';
 
 import {Posts} from 'mattermost-redux/constants';
 import {getChannel} from 'mattermost-redux/selectors/entities/channels';
