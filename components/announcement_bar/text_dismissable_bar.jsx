5b51174f7a9e3991f752346a16d1b542d13c9bb5
Eli Yukelzon <reflog@gmail.com>
2020-03-12 11:02:56 +0200

    .

--- components/announcement_bar/text_dismissable_bar.jsx
+++ components/announcement_bar/text_dismissable_bar.jsx
@@ -4,7 +4,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import * as TextFormatting from 'utils/text_formatting';
 import {trackEvent} from 'actions/diagnostics_actions.jsx';
 import Markdown from 'components/markdown';
 
