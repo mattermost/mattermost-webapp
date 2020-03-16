297c9997d43952c1967f2a9d2855247f8f2a5f8d
Saturnino Abril <saturnino.abril@gmail.com>
2020-03-16 09:25:37 +0800

    fix header and image attachment specs

--- e2e/cypress/support/ui_commands.js
+++ e2e/cypress/support/ui_commands.js
@@ -137,8 +137,11 @@ function isMac() {
 
 Cypress.Commands.add('postMessage', (message) => {
     cy.get('#post_textbox', {timeout: TIMEOUTS.LARGE}).clear().type(message).type('{enter}');
-    cy.wait(TIMEOUTS.TINY);
-    cy.get('#post_textbox').should('have.value', '');
+    cy.waitUntil(() => {
+        return cy.get('#post_textbox').then((el) => {
+            return el[0].textContent === '';
+        });
+    });
 });
 
 Cypress.Commands.add('postMessageReplyInRHS', (message) => {
