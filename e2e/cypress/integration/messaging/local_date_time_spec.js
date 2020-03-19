0cfa1b4e19d027aa794f206a870dbd5341430088
Jesse Hallam <jesse.hallam@gmail.com>
2020-03-18 21:53:27 -0300

    restructure tests to break out individual posts

--- e2e/cypress/integration/messaging/local_date_time_spec.js
+++ e2e/cypress/integration/messaging/local_date_time_spec.js
@@ -26,7 +26,7 @@ describe('Messaging', () => {
 
         // # Create and visit new channel
         cy.createAndVisitNewChannel().then((channel) => {
-            // # Post messages from the past and now
+            // # Post messages from the past
             [
                 Date.UTC(2020, 0, 5, 4, 30), // Jan 5, 2020 04:30
                 Date.UTC(2020, 0, 5, 12, 30), // Jan 5, 2020 12:30
@@ -35,6 +35,8 @@ describe('Messaging', () => {
             ].forEach((createAt, index) => {
                 cy.postMessageAs({sender: sysadmin, message: `Hello from ${index}`, channelId: channel.id, createAt});
             });
+
+            // # Post messages from now
             cy.postMessage('Hello from now');
 
             // # Reload to re-arrange posts
@@ -50,52 +52,82 @@ describe('Messaging', () => {
     });
 
     describe('MM-21342 Post time should render correct format and locale', () => {
-        const re12Hour = /(?=.*\d{2}:\d{2} [AMP]{2})(^[0-9:\sAMP]{8}$)/; // matches 12-hour format with exact length of 8
-        const re24Hour = /(?=.*\d{2}:\d{2})(^[0-9:]{5}$)/; // matches 24-hour format with exact length of 5
-
-        [
+        const testCases = [
             {
                 name: 'in English',
                 channelIntro: 'Beginning of Channel Test',
                 locale: 'en',
                 manualTimezone: 'UTC',
                 localTimes: [
-                    {postIndex: 0, match12hour: '4:30 AM', match24hour: '04:30'},
-                    {postIndex: 1, match12hour: '12:30 PM', match24hour: '12:30'},
-                    {postIndex: 2, match12hour: '8:30 PM', match24hour: '20:30'},
-                    {postIndex: 3, match12hour: '12:30 AM', match24hour: '00:30'},
+                    {postIndex: 0, standard: '4:30 AM', military: '04:30'},
+                    {postIndex: 1, standard: '12:30 PM', military: '12:30'},
+                    {postIndex: 2, standard: '8:30 PM', military: '20:30'},
+                    {postIndex: 3, standard: '12:30 AM', military: '00:30'},
                 ]
             },
             {
-                name: 'in other locale like Spanish',
+                name: 'in Spanish',
                 channelIntro: 'Inicio de Channel Test',
                 locale: 'es',
                 manualTimezone: 'UTC',
                 localTimes: [
-                    {postIndex: 0, match12hour: /4:30 a\. m\./, match24hour: '4:30'},
-                    {postIndex: 1, match12hour: /12:30 p\. m\./, match24hour: '12:30'},
-                    {postIndex: 2, match12hour: /8:30 p\. m\./, match24hour: '20:30'},
-                    {postIndex: 3, match12hour: /12:30 a\. m\./, match24hour: '00:30'},
+                    {postIndex: 0, standard: '4:30 a. m.', military: '4:30'},
+                    {postIndex: 1, standard: '12:30 p. m.', military: '12:30'},
+                    {postIndex: 2, standard: '8:30 p. m.', military: '20:30'},
+                    {postIndex: 3, standard: '12:30 a. m.', military: '0:30'},
                 ]
             },
             {
-                name: 'for react-intl unsupported timezone',
+                name: 'in react-intl unsupported timezone',
                 channelIntro: 'Inicio de Channel Test',
                 locale: 'es',
                 manualTimezone: 'NZ-CHAT',
                 localTimes: [
-                    {postIndex: 0, match12hour: re12Hour, match24hour: re24Hour},
-                    {postIndex: 1, match12hour: re12Hour, match24hour: re24Hour},
-                    {postIndex: 2, match12hour: re12Hour, match24hour: re24Hour},
-                    {postIndex: 3, match12hour: re12Hour, match24hour: re24Hour},
+                    {postIndex: 0, standard: '06:15 PM', military: '18:15'},
+                    {postIndex: 1, standard: '02:15 AM', military: '02:15'},
+                    {postIndex: 2, standard: '10:15 AM', military: '10:15'},
+                    {postIndex: 3, standard: '02:15 PM', military: '14:15'},
                 ]
             },
-        ].forEach((testCase) => {
-            it(testCase.name + ' standard time', () => {
-                verifyLocalizeTime(testCase);
-            });
-            it(testCase.name + ' military time', () => {
-                verifyLocalizeTime(testCase);
+        ];
+
+        testCases.forEach((testCase) => {
+            describe(testCase.name, () => {
+                describe('standard time', () => {
+                    testCase.localTimes.forEach((localTime, index) => {
+                        it('post ' + index + ' should match', () => {
+                            // # Change user preference to 12-hour format
+                            setTo24HourTimeFormat(false);
+
+                            // # Set user locale and timezone
+                            setLocaleAndTimezone(testCase.locale, testCase.manualTimezone);
+
+                            // * Verify that the channel is loaded correctly based on locale
+                            cy.findByText(testCase.channelIntro).should('be.visible');
+
+                            // * Verify that the local time of each post is rendered in 12-hour format based on locale
+                            cy.findAllByTestId('postView').eq(index).find('.post__time', {timeout: 500}).should('have.text', localTime.standard);
+                        });
+                    });
+                });
+
+                describe('military time', () => {
+                    testCase.localTimes.forEach((localTime, index) => {
+                        it('post ' + index + ' should match', () => {
+                            // # Change user preference to 24-hour format
+                            setTo24HourTimeFormat(true);
+
+                            // # Set user locale and timezone
+                            setLocaleAndTimezone(testCase.locale, testCase.manualTimezone);
+
+                            // * Verify that the channel is loaded correctly based on locale
+                            cy.findByText(testCase.channelIntro).should('be.visible');
+
+                            // * Verify that the local time of each post is rendered in 24-hour format based on locale
+                            cy.findAllByTestId('postView').eq(index).find('.post__time', {timeout: 500}).should('have.text', localTime.military);
+                        });
+                    });
+                });
             });
         });
     });
@@ -122,43 +154,5 @@ function setTo24HourTimeFormat(is24Hour) {
         };
 
         cy.apiSaveUserPreference([preference]);
-    });
-}
-
-function verifyPostTime(postPosition, match) {
-    cy.findAllByTestId('postView').eq(postPosition).find('.post__time').within(() => {
-        cy.findByText(match).should('be.visible');
-    });
-}
-
-function verifyLocalizeTime12Hour({locale, manualTimezone, channelIntro, localTimes}) {
-    // # Set user preference to 12-hour format
-    setTo24HourTimeFormat(false);
-
-    // # Set user locale and timezone
-    setLocaleAndTimezone(locale, manualTimezone);
-
-    // * Verify that the channel is loaded correctly based on locale
-    cy.findByText(channelIntro).should('be.visible');
-
-    // * Verify that the local time of each post is rendered in 12-hour format based on locale
-    localTimes.forEach((localTime, index) => {
-        verifyPostTime(index, localTime.match12hour);
-    });
-}
-
-function verifyLocalizeTime24Hour({locale, manualTimezone, channelIntro, localTimes}) {
-    // # Change user preference to 24-hour format
-    setTo24HourTimeFormat(true);
-
-    // # Set user locale and timezone
-    setLocaleAndTimezone(locale, manualTimezone);
-
-    // * Verify that the channel is loaded correctly based on locale
-    cy.findByText(channelIntro).should('be.visible');
-
-    // * Verify that the local time of each post is rendered in 24-hour format based on locale
-    localTimes.forEach((localTime, index) => {
-        verifyPostTime(index, localTime.match24hour);
     });
 }
