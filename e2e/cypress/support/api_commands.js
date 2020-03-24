994c9b8e21489ffc67fd47b8225e53d3686501e4
Mattermost Build <build@mattermost.com>
2020-03-24 20:26:21 +0100

    Automated cherry pick of #4978 (#5131)
    * MM-23018 Add use_channel_mentions to system scheme
    
    * Fix channel_mentions permission not applying correctly for team admins
    
    * Add tests for system scheme testing the use_channel_mentions permission
    
    * Add tests for team scheme
    
    * Dont use exports
    
    * Rename files to be more apt
    
    Co-authored-by: fm2munsh <farhan.munshi@mattermost.com>

--- e2e/cypress/support/api_commands.js
+++ e2e/cypress/support/api_commands.js
@@ -1253,3 +1253,32 @@ Cypress.Commands.add('patchRole', (roleID, patch) => {
         return cy.wrap(response);
     });
 });
+
+/**
+ * Get all schemes in the system - must have PERMISSION_MANAGE_SYSTEM
+ *
+ * @param {String} scope - either "team" or "channel"
+ */
+Cypress.Commands.add('apiGetSchemes', (scope) => {
+    return cy.request({
+        headers: {'X-Requested-With': 'XMLHttpRequest'},
+        url: `/api/v4/schemes?scope=${scope}`,
+        method: 'GET',
+    });
+});
+
+/**
+ * Delete a scheme directly via API
+ *
+ * @param {String} schemeId - the id of the scheme to delete
+ */
+Cypress.Commands.add('apiDeleteScheme', (schemeId) => {
+    return cy.request({
+        headers: {'X-Requested-With': 'XMLHttpRequest'},
+        url: '/api/v4/schemes/' + schemeId,
+        method: 'DELETE',
+    }).then((response) => {
+        expect(response.status).to.equal(200);
+        return cy.wrap(response);
+    });
+});
