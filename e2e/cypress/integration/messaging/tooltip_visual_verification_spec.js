8d9f678a36585d65b59ce9386e7ee6cbab87e2ec
Saturnino Abril <saturnino.abril@gmail.com>
2020-04-01 07:54:22 +0800

    MM-23367 Add test metadata to selectively run Cypress tests (#5107)
    * draft for grouping test cases
    
    * draft
    
    * add test metadata to selectively run Cypress tests
    
    * merge into master and fix conflicts
    
    * add script to get list of unique test metadata
    
    * add notes on how to run the command
    
    * fix minor typo

--- e2e/cypress/integration/messaging/tooltip_visual_verification_spec.js
+++ e2e/cypress/integration/messaging/tooltip_visual_verification_spec.js
@@ -6,6 +6,10 @@
 // - [*] indicates an assertion (e.g. * Check the title)
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
+
+// Stage: @prod
+// Group: @messaging
+
 import * as TIMEOUTS from '../../fixtures/timeouts';
 
 describe('Messaging', () => {
