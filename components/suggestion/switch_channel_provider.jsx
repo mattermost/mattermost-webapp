5ee9492423fc02961f4e7ccd75c69001695b7e80
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2020-04-04 22:39:00 +0500

    Removing icons

--- components/suggestion/switch_channel_provider.jsx
+++ components/suggestion/switch_channel_provider.jsx
@@ -38,10 +38,6 @@ import BotBadge from 'components/widgets/badges/bot_badge';
 import GuestBadge from 'components/widgets/badges/guest_badge';
 import Avatar from 'components/widgets/users/avatar';
 
-import DraftIcon from 'components/widgets/icons/draft_icon';
-import GlobeIcon from 'components/widgets/icons/globe_icon';
-import LockIcon from 'components/widgets/icons/lock_icon';
-import ArchiveIcon from 'components/widgets/icons/archive_icon';
 import {getPostDraft} from 'selectors/rhs';
 import store from 'stores/redux_store.jsx';
 import {Constants, StoragePrefixes} from 'utils/constants';
