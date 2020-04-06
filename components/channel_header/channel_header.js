c4f90617f8524d9fc7082e6d29178e18bca6046b
Nevyana Angelova <nevyangelova@Nevyanas-MBP.fritz.box>
2020-04-06 16:11:44 +0200

    Update snaps

--- components/channel_header/channel_header.js
+++ components/channel_header/channel_header.js
@@ -430,7 +430,7 @@ class ChannelHeader extends React.PureComponent {
         }
 
         let headerTextContainer;
-        const headerText = ((isDirect && dmUser.is_bot) ? dmUser.bot_description : channel.header).trim();
+        const headerText = (isDirect && dmUser.is_bot) ? dmUser.bot_description : channel.header;
         if (headerText) {
             const popoverContent = (
                 <Popover
