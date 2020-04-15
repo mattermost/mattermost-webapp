460e6f775a2a2eb04e36ef2bdaf2a8bdeb5a22f5
clarmso <clare.m.so+github@gmail.com>
2020-04-15 00:23:17 -0400

    Address code review comments to adhere to convention and description.

--- e2e/cypress/integration/messaging/emoji_insert_position_spec.js
+++ e2e/cypress/integration/messaging/emoji_insert_position_spec.js
@@ -7,25 +7,31 @@
 // Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-// Stage: @prod
 // Group: @messaging
 
 describe('Messaging', () => {
     before(() => {
-    // # Login as "user-1" and go to /
+        // # Login as "user-1" and go to /
         cy.apiLogin('user-1');
         cy.visit('/ad-1/channels/town-square');
         cy.get('#post_textbox').clear();
     });
     it('M23348 - Selecting an emoji from emoji picker should insert it at the cursor position', () => {
+        // # Write some text in the send box.
         cy.get('#post_textbox').type('HelloWorld!');
+
+        // # Move the cursor to the middle of the text.
         cy.get('#post_textbox').type('{leftarrow}{leftarrow}{leftarrow}{leftarrow}{leftarrow}{leftarrow}');
+
+        // # Select the grinning emoji from the emoji picker.
         cy.get('#emojiPickerButton').click();
         cy.get('img[data-testid="grinning"]').click();
+
+        // * The emoji should be inserted where the cursor is at the time of selection.
         cy.get('#post_textbox').should('have.value', 'Hello :grinning: World!');
         cy.get('#post_textbox').type('{enter}');
-        cy.getLastPost().find('p').contains('Hello World!');
-        cy.getLastPost().find('p').children();
-        cy.getLastPost().find('p').find('span[data-emoticon="grinning"]').should('be.visible');
+
+        // * The emoji should be displayed in the post at the position inserted.
+        cy.getLastPost().find('p').should('have.html', `Hello <span data-emoticon="grinning"><span alt=":grinning:" class="emoticon" title=":grinning:" style="background-image: url(&quot;${Cypress.config('baseUrl')}/static/emoji/1f600.png&quot;);"></span></span> World!`);
     });
 });
