a3d5fcede38c1ddd4a91b09c9748846106c24d05
abdusabri <abdusabri@outlook.com>
2020-04-19 00:36:50 +0200

    Remove extra / from globalstate import

--- components/rhs_card/index.ts
+++ components/rhs_card/index.ts
@@ -5,7 +5,7 @@ import {connect} from 'react-redux';
 import {getCurrentRelativeTeamUrl} from 'mattermost-redux/selectors/entities/teams';
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
 
-import {GlobalState} from '../..//types/store';
+import {GlobalState} from '../../types/store';
 
 import {getSelectedPostCard} from 'selectors/rhs';
 
