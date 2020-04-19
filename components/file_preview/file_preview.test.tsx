d2d0a4de393cfc18522a26721076ab2d3966a09d
hzeroo <edusvh@gmail.com>
2020-04-19 02:45:30 +0200

    Migrate 'components/file_attachment' module and associated tests to TypeScript

--- components/file_preview/file_preview.test.tsx
+++ components/file_preview/file_preview.test.tsx
@@ -12,20 +12,39 @@ describe('FilePreview', () => {
     const onRemove = jest.fn();
     const fileInfos = [
         {
-            id: 'file_id_1',
-            create_at: '1',
             width: 100,
             height: 100,
-            extension: 'jpg',
+            name: 'test_filename',
+            id: 'file_id_1',
+            type: 'image/png',
+            extension: 'png',
+            has_preview_image: true,
+            user_id: '',
+            create_at: 0,
+            update_at: 0,
+            delete_at: 0,
+            size: 100,
+            mime_type: '',
+            clientId: ''
         },
     ];
     const uploadsInProgress = ['clientID_1'];
     const uploadsProgressPercent = {
         clientID_1: {
+            width: 100,
+            height: 100,
             name: 'file',
             percent: 50,
             extension: 'image/png',
             id: 'file_id_1',
+            has_preview_image: true,
+            user_id: '',
+            create_at: 0,
+            update_at: 0,
+            delete_at: 0,
+            size: 100,
+            mime_type: '',
+            clientId: ''
         },
     };
 
@@ -79,7 +98,24 @@ describe('FilePreview', () => {
         const fileId = 'file_id_1';
         const props = {
             ...baseProps,
-            fileInfos: [{id: fileId, extension: 'svg'}],
+            fileInfos: [
+                {
+                    width: 100,
+                    height: 100,
+                    name: 'test_filename',
+                    id: 'file_id_1',
+                    type: 'image/svg',
+                    extension: 'svg',
+                    has_preview_image: true,
+                    user_id: '',
+                    create_at: 0,
+                    update_at: 0,
+                    delete_at: 0,
+                    size: 100,
+                    mime_type: '',
+                    clientId: ''
+                }
+            ],
         };
 
         const wrapper = shallow(
@@ -95,7 +131,22 @@ describe('FilePreview', () => {
         const props = {
             ...baseProps,
             enableSVGs: true,
-            fileInfos: [{id: fileId, extension: 'svg'}],
+            fileInfos: [{
+                width: 100,
+                height: 100,
+                name: 'test_filename',
+                id: 'file_id_1',
+                type: 'image/svg',
+                extension: 'svg',
+                has_preview_image: true,
+                user_id: '',
+                create_at: 0,
+                update_at: 0,
+                delete_at: 0,
+                size: 100,
+                mime_type: '',
+                clientId: ''
+            }],
         };
 
         const wrapper = shallow(
