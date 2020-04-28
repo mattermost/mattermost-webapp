10987919cb982ccd8033da92632d4100830886d0
Devin Binnie <devin.binnie@mattermost.com>
2020-04-28 14:32:54 -0400

    DnD E2E test fixes

--- e2e/cypress/integration/channel_sidebar/drag_and_drop_spec.js
+++ e2e/cypress/integration/channel_sidebar/drag_and_drop_spec.js
@@ -10,13 +10,12 @@
 // Stage: @prod @smoke
 // Group: @channel_sidebar
 
-import users from '../../fixtures/users';
-
 import {testWithConfig} from '../../support/hooks';
 
 import {getRandomInt} from '../../utils';
 
-const sysadmin = users.sysadmin;
+const SpaceKeyCode = 32;
+const DownArrowKeyCode = 40;
 
 describe('Channel sidebar', () => {
     testWithConfig({
@@ -44,11 +43,7 @@ describe('Channel sidebar', () => {
         cy.get('.SidebarChannel > .SidebarLink').should('be.visible').eq(1).should('contain', 'Town Square');
 
         // # Perform drag using keyboard
-        cy.get('.SidebarChannel:contains(Off-Topic) > .SidebarLink')
-            .trigger('keydown', { keyCode: 32 })
-            .trigger('keydown', { keyCode: 40, force: true })
-            .wait(3000)
-            .trigger('keydown', { keyCode: 32, force: true });
+        cy.get('.SidebarChannel:contains(Off-Topic) > .SidebarLink').trigger('keydown', {keyCode: SpaceKeyCode}).trigger('keydown', {keyCode: DownArrowKeyCode, force: true}).wait(3000).trigger('keydown', {keyCode: SpaceKeyCode, force: true});
 
         // * Verify that the elements have been re-ordered
         cy.get('.SidebarChannel > .SidebarLink').should('be.visible').eq(1).should('contain', 'Off-Topic');
@@ -64,19 +59,16 @@ describe('Channel sidebar', () => {
         cy.get('#headerTeamName').should('be.visible').should('contain', teamName);
 
         // * Verify the order is correct to begin with
+        // Wait for Public Channels to be visible since for some reason it shows up later...
+        cy.get('.SidebarChannelGroupHeader_groupButton > div[data-rbd-drag-handle-draggable-id]').should('contain', 'PUBLIC CHANNELS').should('be.visible');
         cy.get('.SidebarChannelGroupHeader_groupButton > div[data-rbd-drag-handle-draggable-id]').should('be.visible').eq(0).should('contain', 'PUBLIC CHANNELS');
-        cy.get('.SidebarChannelGroupHeader_groupButton > div[data-rbd-drag-handle-draggable-id]').should('be.visible').eq(1).should('contain', 'DIRECT MESSAGES');
+        cy.get('.SidebarChannelGroupHeader_groupButton > div[data-rbd-drag-handle-draggable-id]').should('be.visible').eq(1).should('contain', 'Direct Messages');
 
         // # Perform drag using keyboard
-        cy.get('.SidebarChannelGroupHeader_groupButton > div[data-rbd-drag-handle-draggable-id]').should('be.visible').eq(0).should('contain', 'PUBLIC CHANNELS')
-            .trigger('keydown', { keyCode: 32 })
-            .trigger('keydown', { keyCode: 40, force: true })
-            .wait(3000)
-            .trigger('keydown', { keyCode: 32, force: true });
+        cy.get('.SidebarChannelGroupHeader_groupButton > div[data-rbd-drag-handle-draggable-id]').should('be.visible').eq(0).should('contain', 'PUBLIC CHANNELS').trigger('keydown', {keyCode: SpaceKeyCode}).trigger('keydown', {keyCode: DownArrowKeyCode, force: true}).wait(3000).trigger('keydown', {keyCode: SpaceKeyCode, force: true}).wait(3000);
 
         // * Verify that the elements have been re-ordered
         cy.get('.SidebarChannelGroupHeader_groupButton > div[data-rbd-drag-handle-draggable-id]').should('be.visible').eq(1).should('contain', 'PUBLIC CHANNELS');
-        cy.get('.SidebarChannelGroupHeader_groupButton > div[data-rbd-drag-handle-draggable-id]').should('be.visible').eq(0).should('contain', 'DIRECT MESSAGES');
-
+        cy.get('.SidebarChannelGroupHeader_groupButton > div[data-rbd-drag-handle-draggable-id]').should('be.visible').eq(0).should('contain', 'Direct Messages');
     });
 });
