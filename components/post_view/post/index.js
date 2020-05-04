52e66f1a5bac8f37faec5fc7c0d3b7c7313b0389
Jesús Espino <jespinog@gmail.com>
2020-05-04 13:43:51 +0200

    Removing no longer needed null check

--- components/post_view/post/index.js
+++ components/post_view/post/index.js
@@ -68,7 +68,7 @@ function makeMapStateToProps() {
             isFirstReply: isFirstReply(post, previousPost),
             consecutivePostByUser,
             previousPostIsComment,
-            replyCount: getPostRepliesCount(state, post.root_id || post.id) || 0,
+            replyCount: getPostRepliesCount(state, post.root_id || post.id),
             isCommentMention: isPostCommentMention(state, post.id),
             center: get(state, Preferences.CATEGORY_DISPLAY_SETTINGS, Preferences.CHANNEL_DISPLAY_MODE, Preferences.CHANNEL_DISPLAY_MODE_DEFAULT) === Preferences.CHANNEL_DISPLAY_MODE_CENTERED,
             compactDisplay: get(state, Preferences.CATEGORY_DISPLAY_SETTINGS, Preferences.MESSAGE_DISPLAY, Preferences.MESSAGE_DISPLAY_DEFAULT) === Preferences.MESSAGE_DISPLAY_COMPACT,
