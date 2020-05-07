7eed02829e0b2cc3efd15b6c615d7c2c105c444d
Brad Coughlin <brad@bradcoughlin.com>
2020-05-07 10:05:27 -0700

    flaky test

--- utils/admin_console_index.test.jsx
+++ utils/admin_console_index.test.jsx
@@ -13,17 +13,17 @@ const enMessages = require('../i18n/en');
 const esMessages = require('../i18n/es');
 
 describe('AdminConsoleIndex.generateIndex', () => {
-    it('should generate a index where I can search', () => {
+    it('should generate an index where I can search', () => {
         const intl = createIntl({locale: 'en', messages: enMessages, defaultLocale: 'en'}, {});
 
         const idx = generateIndex(AdminDefinition, {}, intl);
         expect(idx.search('ldap')).toEqual([
+            'environment/session_lengths',
             'authentication/mfa',
             'authentication/ldap',
             'authentication/saml',
             'experimental/features',
             'authentication/email',
-            'environment/session_lengths',
             'authentication/guest_access',
         ]);
         expect(idx.search('saml')).toEqual([
