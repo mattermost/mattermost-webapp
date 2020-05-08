d982a37c7d19776d6038ae7ff859c6eb5113bf20
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2020-05-08 16:38:43 +0200

    Check mentions and flagged posts disappear in RHS
    Add e2e tests checking that, when a user is kicked out of a channel, the
    recent mentions and flagged posts in that channel disappear from the RHS.

--- e2e/cypress/integration/websocket/handle_removed_user_spec.js
+++ e2e/cypress/integration/websocket/handle_removed_user_spec.js
@@ -11,6 +11,119 @@ import {testWithConfig} from '../../support/hooks';
 
 import {getRandomInt} from '../../utils';
 
+function shouldRemoveMentionsInRHS(sidebarItemClass) {
+    // # Start with a new team
+    const teamName = `team-${getRandomInt(999999)}`;
+    cy.createNewTeam(teamName, teamName);
+
+    // * Verify that we've switched to the new team
+    cy.get('#headerTeamName').should('be.visible').should('be.visible').should('contain', teamName);
+
+    // # Click on Off Topic
+    cy.get(`${sidebarItemClass}:contains(Off-Topic)`).should('be.visible').click();
+
+    // * Verify that the channel changed
+    cy.url().should('include', `/${teamName}/channels/off-topic`);
+    cy.get('#channelHeaderTitle').should('be.visible').should('contain', 'Off-Topic');
+
+    let channelId;
+    let userId;
+    let postId;
+    cy.getCurrentChannelId().then((res) => {
+        channelId = res;
+        return cy.apiGetMe();
+    }).then((res) => {
+        userId = res.body.id;
+
+        // # Post a unique message with a mention and retrieve its ID
+        var messageText = `${Date.now()} - mention to @${res.body.username} `;
+        cy.postMessage(messageText);
+
+        return cy.getLastPostId();
+    }).then((lastPostId) => {
+        postId = lastPostId;
+
+        // # Click on the Recent Mentions button to open the RHS
+        cy.get('#channelHeaderMentionButton').click();
+
+        // * Verify that the recently posted message is shown in the RHS
+        cy.get(`#rhsPostMessageText_${postId}`).should('exist');
+
+        return cy.removeUserFromChannel(channelId, userId);
+    }).then(() => {
+        // # Dismiss the modal informing the user they were kicked out
+        cy.get('#removedChannelBtn').click();
+
+        // * Verify that the channel changed back to Town Square
+        cy.url().should('include', `/${teamName}/channels/town-square`);
+        cy.get('#channelHeaderTitle').should('be.visible').should('contain', 'Town Square');
+
+        // * Verify that Off-Topic has been removed
+        cy.get(`${sidebarItemClass}:contains(Off-Topic)`).should('not.exist');
+
+        // * Verify that the recently posted message is no longer in the RHS
+        cy.get(`#rhsPostMessageText_${postId}`).should('not.exist');
+    });
+}
+
+function shouldRemoveFlaggedPostsInRHS(sidebarItemClass) {
+    // # Start with a new team
+    const teamName = `team-${getRandomInt(999999)}`;
+    cy.createNewTeam(teamName, teamName);
+
+    // * Verify that we've switched to the new team
+    cy.get('#headerTeamName').should('be.visible').should('be.visible').should('contain', teamName);
+
+    // # Click on Off Topic
+    cy.get(`${sidebarItemClass}:contains(Off-Topic)`).should('be.visible').click();
+
+    // * Verify that the channel changed
+    cy.url().should('include', `/${teamName}/channels/off-topic`);
+    cy.get('#channelHeaderTitle').should('be.visible').should('contain', 'Off-Topic');
+
+    let channelId;
+    let userId;
+    let postId;
+    cy.getCurrentChannelId().then((res) => {
+        channelId = res;
+        return cy.apiGetMe();
+    }).then((res) => {
+        userId = res.body.id;
+
+        // # Post a unique message and retrieve its ID
+        var messageText = `${Date.now()} - post to flag`;
+        cy.postMessage(messageText);
+
+        return cy.getLastPostId();
+    }).then((lastPostId) => {
+        postId = lastPostId;
+
+        // # Flag the last post
+        cy.clickPostFlagIcon(postId);
+
+        // # Click on the Flagged Posts button to open the RHS
+        cy.get('#channelHeaderFlagButton').click();
+
+        // * Verify that the recently posted message is shown in the RHS
+        cy.get(`#rhsPostMessageText_${postId}`).should('exist');
+
+        return cy.removeUserFromChannel(channelId, userId);
+    }).then(() => {
+        // # Dismiss the modal informing the user they were kicked out
+        cy.get('#removedChannelBtn').click();
+
+        // * Verify that the channel changed back to Town Square
+        cy.url().should('include', `/${teamName}/channels/town-square`);
+        cy.get('#channelHeaderTitle').should('be.visible').should('contain', 'Town Square');
+
+        // * Verify that Off-Topic has been removed
+        cy.get(`${sidebarItemClass}:contains(Off-Topic)`).should('not.exist');
+
+        // * Verify that the recently posted message is no longer in the RHS
+        cy.get(`#rhsPostMessageText_${postId}`).should('not.exist');
+    });
+}
+
 describe('Handle removed user - old sidebar', () => {
     before(() => {
         cy.apiLogin('user-1');
@@ -47,6 +160,14 @@ describe('Handle removed user - old sidebar', () => {
             cy.get('#channelHeaderTitle').should('be.visible').should('contain', 'Town Square');
             cy.get('.sidebar-item:contains(Off-Topic)').should('not.exist');
         });
+    });
+
+    it('should remove mentions from RHS', () => {
+        shouldRemoveMentionsInRHS('.sidebar-item');
+    });
+
+    it('should remove flagged posts from RHS', () => {
+        shouldRemoveFlaggedPostsInRHS('.sidebar-item');
     });
 });
 
@@ -91,5 +212,13 @@ describe('Handle removed user - new sidebar', () => {
             cy.url().should('include', `/${teamName}/channels/town-square`);
             cy.get('#channelHeaderTitle').should('be.visible').should('contain', 'Town Square');
         });
+    });
+
+    it('should remove mentions from RHS', () => {
+        shouldRemoveMentionsInRHS('.SidebarChannel');
+    });
+
+    it('should remove flagged posts from RHS', () => {
+        shouldRemoveFlaggedPostsInRHS('.SidebarChannel');
     });
 });
