46bf78dbe27b020362f6faa994d6b7f0750f522b
Jesse Hallam <jesse.hallam@gmail.com>
2020-05-08 08:54:31 -0300

    loadNewDMIfNeeded: fix getUserIdFromChannelName call

--- actions/user_actions.jsx
+++ actions/user_actions.jsx
@@ -143,8 +143,8 @@ export function loadNewDMIfNeeded(channelId) {
         const state = doGetState();
         const currentUserId = Selectors.getCurrentUserId(state);
 
-        function checkPreference() {
-            const userId = Utils.getUserIdFromChannelName(currentUserId);
+        function checkPreference(channel) {
+            const userId = Utils.getUserIdFromChannelName(channel);
 
             if (!userId) {
                 return;
