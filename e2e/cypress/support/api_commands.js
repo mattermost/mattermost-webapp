86fa2de65a83257f194bf077ff2d5895fa37234a
Farhan Munshi <3207297+fmunshi@users.noreply.github.com>
2020-05-21 12:57:40 -0400

    [MM-23937] Add e2e tests for group mentions (#5530)
    * Add e2e tests for group mentions
    
    Modify IDs and use snake_case instead of PascalCase for ids
    
    * Test from recieving user perspective too
    
    * Update last snapshot
    
    * Fixes from review
    
    * Last review changes
    
    * camelCase instead of snake
    
    * Reset permissions before and after group mentions test
    
    * Fix linting

--- e2e/cypress/support/api_commands.js
+++ e2e/cypress/support/api_commands.js
@@ -1238,6 +1238,49 @@ Cypress.Commands.add('apiActivateUser', (userId, active = true) => {
     cy.externalRequest({user: users.sysadmin, method: 'put', baseUrl, path: `users/${userId}/active`, data: {active}});
 });
 
+/**
+ * Get all groups via the API
+ *
+ * @param {Integer} page - The desired page of the paginated list
+ * @param {Integer} perPage - The number of groups per page
+ *
+ */
+Cypress.Commands.add('apiGetGroups', (page = 0, perPage = 100) => {
+    return cy.request({
+        headers: {'X-Requested-With': 'XMLHttpRequest'},
+        url: `/api/v4/groups?page=${page}&per_page=${perPage}`,
+        method: 'GET',
+        timeout: 60000,
+    }).then((response) => {
+        expect(response.status).to.equal(200);
+        return cy.wrap(response);
+    });
+});
+
+/**
+ * Patch a group directly via API
+ *
+ * @param {String} name - The new name for the group
+ * @param {Object} patch
+ *   {Boolean} allow_reference - Whether to allow reference (group mention) or not  - true/false
+ *   {String} name - Name for the group, used for group mentions
+ *   {String} display_name - Display name for the group
+ *   {String} description - Description for the group
+ *
+ */
+Cypress.Commands.add('apiPatchGroup', (groupID, patch) => {
+    return cy.request({
+        headers: {'X-Requested-With': 'XMLHttpRequest'},
+        url: `/api/v4/groups/${groupID}/patch`,
+        method: 'PUT',
+        timeout: 60000,
+        body: patch,
+    }).then((response) => {
+        expect(response.status).to.equal(200);
+        return cy.wrap(response);
+    });
+});
+
 // *****************************************************************************
 // SAML
 // https://api.mattermost.com/#tag/SAML
