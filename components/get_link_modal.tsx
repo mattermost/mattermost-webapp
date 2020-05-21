1cc4777b06862935e64675fe0947d6db7df38995
Jason Frerich <jason.frerich@mattermost.com>
2020-05-21 15:56:02 -0500

    modification so spinner is centered in the button. use opacity to show/hide spinner and text text sets the width of the button spinner hidden when link is available

--- components/get_link_modal.tsx
+++ components/get_link_modal.tsx
@@ -6,6 +6,7 @@ import {Modal} from 'react-bootstrap';
 import {FormattedMessage} from 'react-intl';
 
 import LoadingWrapper from 'components/widgets/loading/loading_wrapper';
+import LoadingSpinner from 'components/widgets/loading/loading_spinner';
 import SuccessIcon from 'components/widgets/icons/fa_success_icon';
 
 type Props = {
@@ -18,6 +19,7 @@ type Props = {
 
 type State = {
     copiedLink: boolean;
+    buttonOpacity: number;
 }
 
 export default class GetLinkModal extends React.PureComponent<Props, State> {
@@ -67,6 +69,9 @@ export default class GetLinkModal extends React.PureComponent<Props, State> {
 
         let copyLink = null;
         const linkAvailable = Boolean(this.props.link);
+        const buttonTextOpacity = linkAvailable ? 1 : 0;
+        const buttonSpinOpacity = linkAvailable ? 0 : 1;
+        const spinnerVisible = linkAvailable ? 'hidden' : 'visible';
 
         if (document.queryCommandSupported('copy')) {
             copyLink = (
@@ -78,15 +83,27 @@ export default class GetLinkModal extends React.PureComponent<Props, State> {
                     disabled={!linkAvailable}
                     onClick={this.copyLink}
                 >
-                    <LoadingWrapper
-                        loading={!linkAvailable}
-                        text={''}
-                    >
-                        <FormattedMessage
-                            id='get_link.copy'
-                            defaultMessage='Copy Link'
-                        />
-                    </LoadingWrapper>
+                    <span style={{position: 'relative'}} >
+                        <span style={{opacity: buttonTextOpacity}} >
+                            <FormattedMessage
+                                id='get_link.copy'
+                                defaultMessage='Copy Link'
+                            />
+                        </span>
+                        <span
+                            style={{
+                                visibility: spinnerVisible,
+                                left: 0,
+                                top: 0,
+                                marginLeft: '50%',
+                                opacity: buttonSpinOpacity,
+                                position: 'absolute',
+                                transform: 'translate3d(-50%, 0, 0)'
+                            }}
+                        >
+                            <LoadingSpinner/>
+                        </span>
+                    </span>
                 </button>
             );
         }
