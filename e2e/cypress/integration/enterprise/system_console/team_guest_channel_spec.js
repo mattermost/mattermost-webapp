2f9d8aba29876ea1d8a2f3a76202ac1fe34f5f6f
Saturnino Abril <saturnino.abril@gmail.com>
2020-05-27 08:33:13 +0800

    filter e2e production tests (#5580)
    Co-authored-by: Saturnino Abril <saturninoabril@saturninoabril.local>

--- e2e/cypress/integration/enterprise/system_console/team_guest_channel_spec.js
+++ e2e/cypress/integration/enterprise/system_console/team_guest_channel_spec.js
@@ -7,6 +7,9 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
+// Stage: @prod
+// Group: @enterprise @system_console
+
 import {getRandomId} from '../../../utils';
 import * as TIMEOUTS from '../../../fixtures/timeouts';
 
