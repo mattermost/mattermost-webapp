0cdf2db191b2054401038fe086b6b60b002e9e04
Joseph Baylon <joseph.baylon@mattermost.com>
2020-05-27 04:54:49 -0700

    Fix misc prod tests (#5596)
    * Fix misc prod tests
    
    * Fix lint

--- e2e/cypress/integration/channel_sidebar/history_channel_switcher_spec.js
+++ e2e/cypress/integration/channel_sidebar/history_channel_switcher_spec.js
@@ -52,7 +52,7 @@ describe('Channel sidebar', () => {
         cy.get('.SidebarChannelNavigator_jumpToButton').should('be.visible').click();
 
         // # Search for Off-Topic and press Enter
-        cy.get('.channel-switcher__suggestion-box #quickSwitchInput').type('Off-Topic');
+        cy.get('.channel-switcher__suggestion-box #quickSwitchInput').click().type('Off-Topic');
         cy.get('.channel-switcher__suggestion-box #suggestionList').should('be.visible');
         cy.get('.channel-switcher__suggestion-box #quickSwitchInput').type('{enter}');
 
