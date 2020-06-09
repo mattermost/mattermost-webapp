bdf67146aec81a5ebdb68591737d8f942ca962ce
Mattermost Build <build@mattermost.com>
2020-06-09 15:57:47 +0200

    Ensure calls to load page are only made once to prevent bombbarding the server with requests if a user is deleted in a different tab (#5692)
    Co-authored-by: fm2munsh <farhan.munshi@mattermost.com>

--- components/admin_console/user_grid/user_grid.tsx
+++ components/admin_console/user_grid/user_grid.tsx
@@ -45,6 +45,8 @@ const USERS_PER_PAGE = 10;
 const ROW_HEIGHT = 80;
 
 export default class UserGrid extends React.PureComponent<Props, State> {
+    private pageLoaded = 0;
+
     public constructor(props: Props) {
         super(props);
 
@@ -164,7 +166,10 @@ export default class UserGrid extends React.PureComponent<Props, State> {
             const pageToLoad = page + pagesOfUsersRemoved + 1;
 
             // Directly call action to load more users from parent component to load more users into the state
-            this.props.loadPage(pageToLoad);
+            if (pageToLoad > this.pageLoaded) {
+                this.props.loadPage(pageToLoad);
+                this.pageLoaded = pageToLoad;
+            }
         }
 
         return usersToDisplay.map((user) => {
