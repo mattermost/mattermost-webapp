4569f623ad04f47a03e24ad740bf1097be724bde
Nevyana Angelova <nevyangelova@Nevyanas-MacBook-Pro-2.local>
2020-06-16 15:57:32 +0300

    [MM-25835] - Add Icon Button common component

---
+++ components/main_menu/elements/icon_button_common.test.tsx
@@ -0,0 +1,27 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import IconButtonCommon from './icon_button_common';
+
+describe('components/main_menu/IconButtonCommon', () => {
+    test('should match snapshot', () => {
+        const Props = {
+            iconGlyph: 'emoticon-happy-outline',
+            size: 'small',
+            label: 'icon button',
+        };
+
+        const wrapper = shallow(
+            <IconButtonCommon
+                {...Props}
+            />,
+        );
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('label').text()).toBe('icon button');
+        expect(wrapper.find('button').props().className).toBe('IconButton IconButton___small');
+    });
+});
---
+++ components/main_menu/elements/icon_button_common.tsx
@@ -0,0 +1,32 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+
+interface Props {
+    iconGlyph: string;
+    size: string;
+    label: string;
+    disabled?: boolean;
+    additionalClass?: string;
+}
+
+const IconButtonCommon: React.FC<Props> = ({iconGlyph, size, label, disabled, additionalClass}: Props) => {
+    return (
+        <div
+            className='IconButton__wrapper'
+        >
+            <button
+                className={`IconButton IconButton___${size} ${additionalClass}`}
+                disabled={disabled}
+                tabIndex={-1}
+                aria-label={label}
+            >
+                {/* TODO: replace with IconCommon component */}
+                <i className={`Icon Icon___standard icon-${iconGlyph} Icon___standard IconButton_icon`}/>
+            </button>
+        </div>
+    );
+};
+
+export default IconButtonCommon;
