13558b61cb011c07b9884bf1003c7950c279a053
Saturnino Abril <saturninoabril@saturninoabril.local>
2020-06-24 15:25:52 +0800

    fix issue with batchName

--- e2e/cypress/integration/visual_regression/helpers.js
+++ e2e/cypress/integration/visual_regression/helpers.js
@@ -1,6 +1,9 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+const applitoolsConfig = require('../../../applitools.config');
+
 export function getBatchName(suffix) {
-    return `${Cypress.env('batchName')} - ${suffix}`;
+    const batchName = Cypress.env('batchName') || applitoolsConfig.batchName;
+    return `${batchName} - ${suffix}`;
 }
