c495d2da01f5c01c518898ed1dff9fabc38efc56
Artur Zubilewicz <artur.zubilewicz@gmail.com>
2020-06-28 13:17:04 +0200

    e2e: Improve code based on PR review suggestions

--- e2e/cypress/integration/messaging/direct_messages_spec.js
+++ e2e/cypress/integration/messaging/direct_messages_spec.js
@@ -18,33 +18,32 @@ const userActive = users['user-1'];
 describe('Messaging', () => {
     beforeEach(() => {
         // # Create new user that will eventually be inactive
-        cy.apiLogin('sysadmin');
-        cy.apiCreateNewUser().then((user) => {
-            cy.wrap(user).as('userInactive');
+        cy.apiAdminLogin();
+        cy.apiCreateUser().then((res) => {
+            cy.wrap(res.user).as('userInactive');
         });
     });
 
     it('M23349 - Deactivated users are not shown in Direct Messages modal', () => {
-        // # Deactivate "user-2"
+        // # Deactivate inactive user
         cy.get('@userInactive').then((userInactive) => {
-            cy.apiLogin('sysadmin');
-            cy.apiGetUserByEmail(userInactive.email).then((res) => {
-                const user = res.body;
-                cy.apiActivateUser(user.id, false);
-            });
+            // # Login as active user
+            cy.apiLogin(userActive);
 
-            // # Login as "user-1"
-            cy.apiLogin(userActive.username);
+            // # Deactivate inactive user
+            cy.apiActivateUser(userInactive.id, false);
+
+            // # Visit main channel
             cy.visit('/ad-1/channels/town-square');
 
             // # click on '+' sign next to 'Direct Messages'
-            cy.get('#addDirectChannel').click().wait(TIMEOUTS.TINY);
+            cy.get('#addDirectChannel').click().wait(TIMEOUTS.HALF_SEC);
 
             // * Assert that 'Direct Messages' modal appears
             cy.get('#moreDmModal').should('be.visible');
 
-            // # Search for a user
-            cy.get('#selectItems input').focus().type(userInactive.email, {force: true}).wait(TIMEOUTS.TINY);
+            // # Search for inactive user
+            cy.get('#selectItems input').focus().type(userInactive.email, {force: true}).wait(TIMEOUTS.HALF_SEC);
 
             // * Assert that the input box contains searched email
             cy.get('#selectItems input').should('have.value', userInactive.email);
@@ -56,34 +55,29 @@ describe('Messaging', () => {
 
     it('M23349 - Deactivated users are shown in Direct Messages modal after previous conversations', () => {
         cy.get('@userInactive').then((userInactive) => {
-            // # Login as 'user-1'
-            cy.apiLogin(userActive.username);
+            // # Login as active user
+            cy.apiLogin(userActive);
 
-            // # Create a 'Direct Messages' channel with 'user-2'
+            // # Create a 'Direct Messages' channel with inactive user
             cy.apiGetUsers([userActive.username, userInactive.username]).then((res) => {
                 const usersInfo = res.body;
                 cy.apiCreateDirectChannel(usersInfo.map((user) => user.id));
             });
 
-            // # Deactivate 'user-2'
-            cy.apiLogin('sysadmin');
-            cy.apiGetUserByEmail(userInactive.email).then((res) => {
-                const user = res.body;
-                cy.apiActivateUser(user.id, false);
-            });
+            // # Deactivate inactive user
+            cy.apiActivateUser(userInactive.id, false);
 
-            // # Login again as 'user-1'
-            cy.apiLogin('user-1');
+            // # Visit main channel
             cy.visit('/ad-1/channels/town-square');
 
             // # click on '+' sign next to 'Direct Messages'
-            cy.get('#addDirectChannel').click().wait(TIMEOUTS.TINY);
+            cy.get('#addDirectChannel').click().wait(TIMEOUTS.HALF_SEC);
 
             // * Assert that 'Direct Messages' modal appears
             cy.get('#moreDmModal').should('be.visible');
 
-            // # Search for a user
-            cy.get('#selectItems input').focus().type(userInactive.email, {force: true}).wait(TIMEOUTS.TINY);
+            // # Search for inactive user
+            cy.get('#selectItems input').focus().type(userInactive.email, {force: true}).wait(TIMEOUTS.HALF_SEC);
 
             // * Assert that the input box contains searched email
             cy.get('#selectItems input').should('have.value', userInactive.email);
