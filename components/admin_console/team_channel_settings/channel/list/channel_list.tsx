5b4b53fb183be368bc45df72274d46c356b58faf
Hossein Ahmadian-Yazdi <hahmadia@users.noreply.github.com>
2020-06-30 09:30:15 -0400

    [MM-25658] Replace Data Grid on Channel and Teams page in System console with Common Component (#5707)
    * phase 1 work
    
    * replace channel grid with common component
    
    * re-add stuff farhan mentioned
    
    * refactor code
    
    * fix typo
    
    * lint fixes
    
    * lint fix
    
    * remove mouse event
    
    * update snapshots
    
    * fix check types
    
    * fix linting
    
    * update snapshot
    
    * address PR comments
    
    * only debounce on new search
    
    * fix linting
    
    * address stylistic stuff
    
    * update snapshots
    
    * add so no shifting
    
    * Address styling problems
    
    * fix margin right as per ux
    
    * handle failure and add text overflow
    
    * fix lint
    
    * update snapshot
    
    * Fix group class names
    
    * fix snapshot
    
    * Display error message when search fails
    
    * fix linting
    
    * Update text as per Michael
    
    * Fix linting
    
    * Regenerate snapshot
    
    * add padding
    
    * add 80% opacity
    
    * Make row clickable

--- components/admin_console/team_channel_settings/channel/list/channel_list.tsx
+++ components/admin_console/team_channel_settings/channel/list/channel_list.tsx
@@ -2,182 +2,260 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-
-import {ChannelWithTeamData} from 'mattermost-redux/types/channels';
-import {ActionFunc, ActionResult} from 'mattermost-redux/types/actions';
 import {FormattedMessage} from 'react-intl';
+import {Link} from 'react-router-dom';
 
-import * as Utils from 'utils/utils.jsx';
-import {Constants} from 'utils/constants';
+import {ActionFunc, ActionResult} from 'mattermost-redux/types/actions';
+import {ChannelWithTeamData} from 'mattermost-redux/types/channels';
+import {debounce} from 'mattermost-redux/actions/helpers';
 
-import AbstractList, {PAGE_SIZE} from 'components/admin_console/team_channel_settings/abstract_list.jsx';
 import {browserHistory} from 'utils/browser_history';
 
-import SearchIcon from 'components/widgets/icons/search_icon';
+import {Constants} from 'utils/constants';
+import DataGrid, {Row, Column} from 'components/admin_console/data_grid/data_grid';
+import {PAGE_SIZE} from 'components/admin_console/team_channel_settings/abstract_list.jsx';
+import GlobeIcon from 'components/widgets/icons/globe_icon';
+import LockIcon from 'components/widgets/icons/lock_icon';
 
-import ChannelRow from './channel_row';
-
+import './channel_list.scss';
 interface ChannelListProps {
     actions: {
-        searchAllChannels: (term: string, notAssociatedToGroup?: string, excludeDefaultChannels?: boolean, page?: number, perPage?: number, includeDeleted?: boolean) => ActionFunc | ActionResult;
-        getData: (page: number, perPage: number, notAssociatedToGroup? : string, excludeDefaultChannels?: boolean, includeDeleted?: boolean) => ActionFunc | ActionResult | Promise<ChannelWithTeamData[]>;
+        searchAllChannels: (term: string, notAssociatedToGroup?: string, excludeDefaultChannels?: boolean, page?: number, perPage?: number) => Promise<{ data: any }>;
+        getData: (page: number, perPage: number, notAssociatedToGroup? : string, excludeDefaultChannels?: boolean) => ActionFunc | ActionResult | Promise<ChannelWithTeamData[]>;
     };
-    data?: {id: string; display_name: string}[];
-    total?: number;
-    removeGroup?: () => void;
-    onPageChangedCallback?: () => void;
-    emptyListTextId?: string;
-    emptyListTextDefaultMessage?: string;
+    data: ChannelWithTeamData[];
+    total: number;
 }
 
 interface ChannelListState {
-    searchString: string;
+    term: string;
     channels: ChannelWithTeamData[];
-    searchTotalCount: number;
-    pageResetKey: number;
-    searchMode: boolean;
+    loading: boolean;
+    page: number;
+    total: number;
+    searchErrored: boolean;
 }
+
+const ROW_HEIGHT = 40;
 
 export default class ChannelList extends React.PureComponent<ChannelListProps, ChannelListState> {
     constructor(props: ChannelListProps) {
         super(props);
         this.state = {
-            searchString: '',
+            loading: false,
+            term: '',
             channels: [],
-            searchTotalCount: 0,
-            pageResetKey: 0,
-            searchMode: false,
+            page: 0,
+            total: 0,
+            searchErrored: false,
         };
     }
 
-    searchBar = () => {
-        return (
-            <div className='groups-list--global-actions'>
-                <div className='group-list-search'>
-                    <input
-                        type='text'
-                        placeholder={Utils.localizeMessage('search_bar.search', 'Search')}
-                        onKeyUp={this.handleChannelSearchKeyUp}
-                        onChange={this.searchBarChangeHandler}
-                        value={this.state.searchString}
-                        data-testid='search-input'
-                    />
-                    <SearchIcon
-                        id='searchIcon'
-                        className='search__icon'
-                        aria-hidden='true'
-                    />
-                    <i
-                        className={'fa fa-times-circle group-filter-action ' + (this.state.searchString.length ? '' : 'hidden')}
-                        onClick={this.resetSearch}
-                        data-testid='clear-search'
-                    />
-                </div>
-            </div>
-        );
-    };
-
-    private searchBarChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {searchString} = this.state;
-        if (searchString.length !== 0 && e.target.value.length === 0) {
-            this.resetSearch();
-            return;
-        }
-        this.setState({searchString: e.target.value});
-    };
-
-    private handleChannelSearchKeyUp = async (e: React.KeyboardEvent) => {
-        const {key} = e;
-        const {searchString} = this.state;
-        if (key === Constants.KeyCodes.ENTER[0]) {
-            if (searchString.length > 1) {
-                const response = await this.props.actions.searchAllChannels(searchString, '', false, 0, PAGE_SIZE, true);
-                if ('data' in response) {
-                    this.setState({searchMode: true, channels: response.data.channels, searchTotalCount: response.data.total_count, pageResetKey: Date.now()});
-                }
-            }
-        }
-    };
-    private getDataBySearch = async (page: number, perPage: number): Promise<ChannelWithTeamData[]> => {
-        const response = await this.props.actions.searchAllChannels(this.state.searchString, '', false, page, perPage, true);
-        const channels = new Array(page * perPage); // Pad the array with empty entries because AbstractList expects to slice the results based on the pagination offset.
-        if ('data' in response) {
-            return channels.concat(response.data.channels);
-        }
-        return [];
-    };
-
-    private resetSearch = () => {
-        this.setState({searchString: '', channels: [], searchMode: false, searchTotalCount: 0, pageResetKey: Date.now()});
-    };
-
-    header() {
-        return (
-            <div className='groups-list--header'>
-                <div className='group-name adjusted'>
-                    <FormattedMessage
-                        id='admin.channel_settings.channel_list.nameHeader'
-                        defaultMessage='Name'
-                    />
-                </div>
-                <div className='group-content'>
-                    <div className='group-description'>
-                        <FormattedMessage
-                            id='admin.channel_settings.channel_list.teamHeader'
-                            defaultMessage='Team'
-                        />
-                    </div>
-                    <div className='group-description adjusted'>
-                        <FormattedMessage
-                            id='admin.channel_settings.channel_list.managementHeader'
-                            defaultMessage='Management'
-                        />
-                    </div>
-                    <div className='group-actions'/>
-                </div>
-            </div>
-        );
+    componentDidMount() {
+        this.loadPage();
     }
 
-    onPageChangedCallback = (pagination: {startCount: number; endCount: number; total: number}, channels: ChannelWithTeamData[]) => {
-        if (this.state.searchMode) {
-            this.setState({channels});
-        }
-    };
+    getPaginationProps = () => {
+        const {page, term} = this.state;
+        const total = term === '' ? this.props.total : this.state.total;
+        const startCount = (page * PAGE_SIZE) + 1;
+        let endCount = (page + 1) * PAGE_SIZE;
+        endCount = endCount > total ? total : endCount;
+        return {startCount, endCount, total};
+    }
 
-    render(): JSX.Element {
-        const absProps = {...this.props};
-        if (this.state.searchMode) {
-            absProps.actions.getData = this.getDataBySearch;
+    loadPage = async (page = 0, term = '') => {
+        this.setState({loading: true, term});
+
+        if (term.length > 0) {
+            if (page > 0) {
+                this.searchChannels(page, term);
+            } else {
+                this.searchChannelsDebounced(page, term);
+            }
+            return;
         }
+
+        await this.props.actions.getData(page, PAGE_SIZE);
+        this.setState({page, loading: false});
+    }
+
+    searchChannels = async (page = 0, term = '') => {
+        let channels = [];
+        let total = 0;
+        let searchErrored = true;
+        const response = await this.props.actions.searchAllChannels(term, '', false, page, PAGE_SIZE);
+        if (response?.data) {
+            channels = page > 0 ? this.state.channels.concat(response.data.channels) : response.data.channels;
+            total = response.data.total_count;
+            searchErrored = false;
+        }
+        this.setState({page, loading: false, channels, total, searchErrored});
+    }
+
+    searchChannelsDebounced = debounce((page, term) => this.searchChannels(page, term), 300, false, () => {});
+
+    nextPage = () => {
+        this.loadPage(this.state.page + 1, this.state.term);
+    }
+
+    previousPage = () => {
+        this.setState({page: this.state.page - 1});
+    }
+
+    search = async (term = '') => {
+        this.loadPage(0, term);
+    }
+
+    getColumns = (): Column[] => {
+        const name: JSX.Element = (
+            <FormattedMessage
+                id='admin.channel_settings.channel_list.nameHeader'
+                defaultMessage='Name'
+            />
+        );
+        const team: JSX.Element = (
+            <FormattedMessage
+                id='admin.channel_settings.channel_list.teamHeader'
+                defaultMessage='Team'
+            />
+        );
+        const management: JSX.Element = (
+            <FormattedMessage
+                id='admin.channel_settings.channel_list.managementHeader'
+                defaultMessage='Management'
+            />
+        );
+
+        return [
+            {
+                name,
+                field: 'name',
+                width: 4,
+                fixed: true,
+            },
+            {
+                name: team,
+                field: 'team',
+                width: 1.5,
+                fixed: true,
+            },
+            {
+                name: management,
+                field: 'management',
+                fixed: true,
+            },
+            {
+                name: '',
+                field: 'edit',
+                textAlign: 'right',
+                fixed: true,
+            },
+        ];
+    }
+
+    getRows = (): Row[] => {
+        const {data} = this.props;
+        const {term, channels} = this.state;
+        const {startCount, endCount} = this.getPaginationProps();
+        let channelsToDisplay = term.length > 0 ? channels : data;
+        channelsToDisplay = channelsToDisplay.slice(startCount - 1, endCount);
+
+        return channelsToDisplay.map((channel) => {
+            return {
+                cells: {
+                    id: channel.id,
+                    name: (
+                        <span
+                            className='group-name overflow--ellipsis row-content'
+                            data-testid='channel-display-name'
+                        >
+                            {channel.type === Constants.PRIVATE_CHANNEL ? (
+                                <LockIcon className='channel-icon channel-icon__lock channel-icon___lowerOpacity'/>
+                            ) : (
+                                <GlobeIcon className='channel-icon channel-icon__globe channel-icon___lowerOpacity'/>
+                            )}
+                            <span className='TeamList_channelDisplayName'>
+                                {channel.display_name}
+                            </span>
+                        </span>
+                    ),
+                    team: (
+                        <span className='group-description row-content'>
+                            {channel.team_display_name}
+                        </span>
+                    ),
+                    management: (
+                        <span className='group-description adjusted row-content'>
+                            <FormattedMessage
+                                id={`admin.channel_settings.channel_row.managementMethod.${channel.group_constrained ? 'group' : 'manual'}`}
+                                defaultMessage={channel.group_constrained ? 'Group Sync' : 'Manual Invites'}
+                            />
+                        </span>
+                    ),
+                    edit: (
+                        <span
+                            className='group-actions TeamList_editRow'
+                            data-testid={`${channel.display_name}edit`}
+                        >
+                            <Link to={`/admin_console/user_management/channels/${channel.id}`} >
+                                <FormattedMessage
+                                    id='admin.channel_settings.channel_row.configure'
+                                    defaultMessage='Edit'
+                                />
+                            </Link>
+                        </span>
+                    ),
+                },
+                onClick: () => browserHistory.push(`/admin_console/user_management/channels/${channel.id}`),
+            };
+        });
+    }
+
+    render = (): JSX.Element => {
+        const {term, searchErrored} = this.state;
+        const rows: Row[] = this.getRows();
+        const columns: Column[] = this.getColumns();
+        const {startCount, endCount, total} = this.getPaginationProps();
+
+        let placeholderEmpty: JSX.Element = (
+            <FormattedMessage
+                id='admin.channel_settings.channel_list.no_channels_found'
+                defaultMessage='No channels found'
+            />
+        );
+
+        if (searchErrored) {
+            placeholderEmpty = (
+                <FormattedMessage
+                    id='admin.channel_settings.channel_list.search_channels_errored'
+                    defaultMessage='Something went wrong. Try again'
+                />
+            );
+        }
+
+        const rowsContainerStyles = {
+            minHeight: `${rows.length * ROW_HEIGHT}px`,
+        };
+
         return (
-            <div className='groups-list groups-list-no-padding'>
-                {this.searchBar()}
-                <AbstractList
-                    header={this.header()}
-                    renderRow={this.renderRow}
-                    noPadding={true}
-                    {...absProps}
-                    key={this.state.pageResetKey}
-                    onPageChangedCallback={this.onPageChangedCallback}
-                    data={this.state.searchMode ? this.state.channels : this.props.data}
-                    total={this.state.searchMode ? this.state.searchTotalCount : this.props.total}
+            <div className='ChannelsList'>
+                <DataGrid
+                    columns={columns}
+                    rows={rows}
+                    loading={this.state.loading}
+                    page={this.state.page}
+                    nextPage={this.nextPage}
+                    previousPage={this.previousPage}
+                    startCount={startCount}
+                    endCount={endCount}
+                    total={total}
+                    search={this.search}
+                    term={term}
+                    placeholderEmpty={placeholderEmpty}
+                    rowsContainerStyles={rowsContainerStyles}
                 />
             </div>
         );
     }
-
-    private renderRow = (item: ChannelWithTeamData) => {
-        return (
-            <ChannelRow
-                key={item.id}
-                channel={item}
-                onRowClick={this.onChannelClick}
-            />
-        );
-    };
-
-    private onChannelClick = (id: string) => {
-        browserHistory.push(`/admin_console/user_management/channels/${id}`);
-    };
 }
