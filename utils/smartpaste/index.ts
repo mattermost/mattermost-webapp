10243cdb52cd387c3630f17406f9d8a01c208797
Jesús Espino <jespinog@gmail.com>
2020-07-02 20:14:13 +0200

    Fix linter problems

--- utils/smartpaste/index.ts
+++ utils/smartpaste/index.ts
@@ -1,3 +1,6 @@
-import smartPaste from "./smartpaste"
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import smartPaste from './smartpaste';
 
 export default smartPaste;
--- utils/smartpaste/tables.tsx
+++ utils/smartpaste/tables.tsx
@@ -1,5 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
+
 import {splitMessageBasedOnCaretPosition} from 'utils/post_utils.jsx';
 
 export function parseTable(html: string): HTMLTableElement | null {
