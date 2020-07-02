9fddd255cdeac47cb62b9c1542949fda8e52b22b
Jesús Espino <jespinog@gmail.com>
2020-07-02 22:48:40 +0200

    Removing unneded import

--- utils/smartpaste/tables.tsx
+++ utils/smartpaste/tables.tsx
@@ -1,8 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {splitMessageBasedOnCaretPosition} from 'utils/post_utils.jsx';
-
 export function parseTable(html: string): HTMLTableElement | null {
     const el = document.createElement('div');
     el.innerHTML = html;
