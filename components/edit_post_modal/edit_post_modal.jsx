65ad21c92cc988e3e37c56f106a10fe1457acdd6
Harrison Healey <harrisonmhealey@gmail.com>
2020-07-02 10:58:21 -0400

    MM-26597/MM-26672/MM-26616 Properly forward ref to Textbox

--- components/edit_post_modal/edit_post_modal.jsx
+++ components/edit_post_modal/edit_post_modal.jsx
@@ -337,9 +337,7 @@ class EditPostModal extends React.PureComponent {
     }
 
     setEditboxRef = (ref) => {
-        if (ref && ref.getWrappedInstance) {
-            this.editbox = ref;
-        }
+        this.editbox = ref;
 
         if (this.editbox) {
             this.editbox.focus();
