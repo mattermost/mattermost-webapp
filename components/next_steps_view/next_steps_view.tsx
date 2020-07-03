7f57467c4fd2298aa81dab794c00e35d1e0cbe2d
Devin Binnie <devin.binnie@mattermost.com>
2020-07-03 09:19:39 -0400

    Replaced dumb comment with useful one

--- components/next_steps_view/next_steps_view.tsx
+++ components/next_steps_view/next_steps_view.tsx
@@ -9,12 +9,12 @@ import professionalLogo from 'images/cloud-logos/professional.svg';
 import './next_steps_view.scss';
 
 type Props = {
-    skuName: string;
+    skuName: string; 
 };
 
 export default class NextStepsView extends React.PureComponent<Props> {
     getBottomText = () => {
-        // lmao
+        // TODO: will be stored in user prefs at a later date
         const {isFinished} = {isFinished: false};
 
         if (isFinished) {
