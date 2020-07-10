b2dc9c383207cd331882e28e2e3979923f24feca
Devin Binnie <devin.binnie@mattermost.com>
2020-07-10 12:08:56 -0400

    Translations and wizard navigation button styling

--- components/next_steps_view/steps/complete_profile_step.tsx
+++ components/next_steps_view/steps/complete_profile_step.tsx
@@ -2,6 +2,7 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
+import {FormattedMessage} from 'react-intl';
 
 import {StepComponentProps} from '../steps';
 
@@ -12,12 +13,38 @@ type State = {
 };
 
 export default class CompleteProfileStep extends React.PureComponent<Props, State> {
+    onSkip = () => {
+        this.props.onSkip(this.props.id);
+    }
+
+    onFinish = () => {
+        this.props.onFinish(this.props.id);
+    }
+
     render() {
         return (
             <div>
                 {'AAAAAAAA'}
-                <button onClick={() => this.props.onFinish(this.props.id)}>{'Finish'}</button>
-                <button onClick={() => this.props.onSkip(this.props.id)}>{'Skip'}</button>
+                <div className='NextStepsView__wizardButtons'>
+                    <button
+                        className='NextStepsView__button cancel'
+                        onClick={this.onSkip}
+                    >
+                        <FormattedMessage
+                            id='next_steps_view.skipForNow'
+                            defaultMessage='Skip for now'
+                        />
+                    </button>
+                    <button
+                        className='NextStepsView__button confirm'
+                        onClick={this.onFinish}
+                    >
+                        <FormattedMessage
+                            id='next_steps_view.complete_profile_step.saveProfile'
+                            defaultMessage='Save profile'
+                        />
+                    </button>
+                </div>
             </div>
         );
     }
