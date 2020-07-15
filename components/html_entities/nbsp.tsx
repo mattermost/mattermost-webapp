c1a64aefd467597e2bca0d96437ca67883449aa2
Caleb Roseland <caleb@calebroseland.com>
2020-07-15 17:19:42 -0500

    eslint config; nbsp

--- components/html_entities/nbsp.tsx
+++ components/html_entities/nbsp.tsx
@@ -1,4 +1,5 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+// https://stackoverflow.com/a/54485712
 export default () => '\u00A0';
