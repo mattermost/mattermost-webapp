2f49ce7f772b86e599dcf6c14c3acf481a6b714c
Sudheer <sudheer.105@gmail.com>
2020-07-15 20:27:09 +0530

    MM-26620 Add system console setting to disable ask the community link (#5877)
    * MM-26609 Create a new single entry point to get help, with a new "Ask Mattermost Community" menu item
    
     * Add new icon for user guide help
     * Add e2e tests for click and checking hrefs
    
    * Update snapshots
    
    * Address feedback
    
    * Update snapshots
    
    * Update svg placement
    
    * MM-26620 Add system console setting to disable ask the community button
    
    * Update redux hash
    
    * update en.json
    
    * Change case of text
    
    * Update snapshots
    
    * Update snapshots

--- e2e/cypress/integration/menus/user_guide_help_spec.js
+++ e2e/cypress/integration/menus/user_guide_help_spec.js
@@ -10,18 +10,27 @@
 // Stage: @prod
 // Group: @menu
 
+import {getAdminAccount} from '../../support/env';
+
 describe('Main menu', () => {
     let testTeam;
     let testUser;
     let testConfig;
-
+    const sysadmin = getAdminAccount();
     before(() => {
-        cy.apiGetConfig().then((response) => {
-            testConfig = response.body;
-        });
         cy.apiInitSetup().then(({team, user}) => {
             testTeam = team;
             testUser = user;
+        });
+
+        cy.apiGetConfig().then((response) => {
+            testConfig = response.body;
+        });
+
+        cy.apiUpdateConfig({
+            SupportSettings: {
+                EnableAskCommunityLink: true,
+            },
         });
     });
 
@@ -45,6 +54,39 @@ describe('Main menu', () => {
                     cy.get('#keyboardShortcuts button').click();
                     cy.get('#shortcutsModalLabel').should('be.visible');
                 });
+            });
+        });
+
+        it('Should not have askTheCommunityLink button when system console setting is false', () => {
+            cy.apiLogin(sysadmin);
+
+            // # Update config to turn EnableAskCommunityLink config setting
+            cy.apiUpdateConfig({
+                SupportSettings: {
+                    EnableAskCommunityLink: false,
+                },
+            });
+            cy.visit(`/${testTeam.name}/channels/town-square`);
+
+            cy.get('#channel-header').should('be.visible').then(() => {
+                // # Click user help button
+                cy.get('#channelHeaderUserGuideButton').click();
+                cy.get('.dropdown-menu').should('be.visible').then(() => {
+                    // * Check that Ask the community button is not visible
+                    cy.get('#askTheCommunityLink').should('be.not.visible');
+                    cy.get('#helpResourcesLink').should('be.visible');
+                });
+            });
+        });
+
+        it('Should have askTheCommunityLink system console setting', () => {
+            cy.apiLogin(sysadmin);
+            cy.visit('/admin_console/site_config/customization');
+
+            // # Click user help button
+            cy.get('#adminConsoleWrapper').should('be.visible').then(() => {
+                // * Check that enable ask community div exists
+                cy.get("div[data-testid='SupportSettings.EnableAskCommunityLink']").scrollIntoView().should('be.visible');
             });
         });
     });
