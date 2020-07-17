7197e41fbf15775fb110611eca6d5ef081a6f185
fm2munsh <farhan.munshi@mattermost.com>
2020-07-17 14:55:16 -0400

    Add filters to team list

--- components/admin_console/team_channel_settings/team/list/team_list.jsx
+++ components/admin_console/team_channel_settings/team/list/team_list.jsx
@@ -16,6 +16,7 @@ import {PAGE_SIZE} from 'components/admin_console/team_channel_settings/abstract
 import TeamIcon from 'components/widgets/team_icon/team_icon';
 
 import './team_list.scss';
+import { group } from 'yargs';
 
 const ROW_HEIGHT = 80;
 export default class TeamList extends React.PureComponent {
@@ -37,6 +38,7 @@ export default class TeamList extends React.PureComponent {
             page: 0,
             total: 0,
             searchErrored: false,
+            filters: {},
         };
     }
 
@@ -44,23 +46,27 @@ export default class TeamList extends React.PureComponent {
         this.loadPage();
     }
 
+    isSearching = (term, filters) => {
+        return (term.length + Object.keys(filters).length) > 0;
+    }
+
     getPaginationProps = () => {
-        const {page, term} = this.state;
-        const total = term === '' ? this.props.total : this.state.total;
+        const {page, term, filters} = this.state;
+        const total = this.isSearching(term, filters) ? this.state.total : this.props.total;
         const startCount = (page * PAGE_SIZE) + 1;
         let endCount = (page + 1) * PAGE_SIZE;
         endCount = endCount > total ? total : endCount;
         return {startCount, endCount, total};
     }
 
-    loadPage = async (page = 0, term = '') => {
-        this.setState({loading: true, term});
+    loadPage = async (page = 0, term = '', filters = {}) => {
+        this.setState({loading: true, term, filters});
 
-        if (term.length > 0) {
+        if (this.isSearching(term, filters)) {
             if (page > 0) {
-                this.searchTeams(page, term);
+                this.searchTeams(page, term, filters);
             } else {
-                this.searchTeamsDebounced(page, term);
+                this.searchTeamsDebounced(page, term, filters);
             }
             return;
         }
@@ -69,11 +75,11 @@ export default class TeamList extends React.PureComponent {
         this.setState({page, loading: false});
     }
 
-    searchTeams = async (page = 0, term = '') => {
+    searchTeams = async (page = 0, term = '', filters = {}) => {
         let teams = [];
         let total = 0;
         let searchErrored = true;
-        const response = await this.props.actions.searchTeams(term, page, PAGE_SIZE);
+        const response = await this.props.actions.searchTeams(term, {page, per_page: PAGE_SIZE, ...filters});
         if (response?.data) {
             teams = page > 0 ? this.state.teams.concat(response.data.teams) : response.data.teams;
             total = response.data.total_count;
@@ -82,10 +88,10 @@ export default class TeamList extends React.PureComponent {
         this.setState({page, loading: false, teams, total, searchErrored});
     }
 
-    searchTeamsDebounced = debounce((page, term) => this.searchTeams(page, term), 300);
+    searchTeamsDebounced = debounce((page, term, filters = {}) => this.searchTeams(page, term, filters), 300);
 
     nextPage = () => {
-        this.loadPage(this.state.page + 1, this.state.term);
+        this.loadPage(this.state.page + 1, this.state.term, this.state.filters);
     }
 
     previousPage = () => {
@@ -95,6 +101,31 @@ export default class TeamList extends React.PureComponent {
     search = (term) => {
         this.loadPage(0, term);
     };
+
+    onFilter = (filterOptions) => {
+        const filters = {};
+        const {group_constrained, allow_open_invite, invite_only} = filterOptions.management.values;
+        const allowOpenInvite = allow_open_invite.value;
+        const inviteOnly = invite_only.value;
+        const groupConstrained =  group_constrained.value;
+        const filtersList = [allowOpenInvite, inviteOnly, groupConstrained]
+
+        // If all filters or no filters do nothing
+        if (filtersList.includes(false) && filtersList.includes(true)) {
+            if (inviteOnly) {
+                filters.exclude_allow_open_invite = !allowOpenInvite;
+                filters.exclude_group_constrained = !groupConstrained;
+                filters.include_group_constrained = groupConstrained;
+            } else if (allowOpenInvite) {
+                filters.allow_open_invite = true;
+                filters.include_group_constrained = groupConstrained
+            } else {
+                filters.group_constrained = true;
+            }
+        }
+
+        this.loadPage(0, this.state.term, filters);
+    }
 
     getColumns = () => {
         const name = (
@@ -157,9 +188,9 @@ export default class TeamList extends React.PureComponent {
 
     getRows = () => {
         const {data} = this.props;
-        const {term, teams} = this.state;
+        const {term, teams, filters} = this.state;
         const {startCount, endCount} = this.getPaginationProps();
-        let teamsToDisplay = term.length > 0 ? teams : data;
+        let teamsToDisplay = this.isSearching(term, filters) ? teams : data;
         teamsToDisplay = teamsToDisplay.slice(startCount - 1, endCount);
 
         return teamsToDisplay.map((team) => {
@@ -233,6 +264,53 @@ export default class TeamList extends React.PureComponent {
             );
         }
 
+        const filterOptions = {
+            management: {
+                name: (
+                    <FormattedMessage
+                        id='admin.team_settings.team_list.mappingHeader'
+                        defaultMessage='Management'
+                    />
+                ),
+                values: {
+                    allow_open_invite: {
+                        name: (
+                            <FormattedMessage
+                                id='admin.team_settings.team_row.managementMethod.anyoneCanJoin'
+                                defaultMessage='Anyone Can Join'
+                            />
+                        ),
+                        value: false,
+                    },
+                    invite_only: {
+                        name: (
+                            <FormattedMessage
+                                id='admin.team_settings.team_row.managementMethod.inviteOnly'
+                                defaultMessage='Invite Only'
+                            />
+                        ),
+                        value: false,
+                    },
+                    group_constrained: {
+                        name: (
+                            <FormattedMessage
+                            id='admin.team_settings.team_row.managementMethod.groupSync'
+                                defaultMessage='Group Sync'
+                            />
+                        ),
+                        value: false,
+                    },
+                },
+                keys: ['allow_open_invite', 'invite_only', 'group_constrained'],
+            },
+        };
+
+        const filterProps = {
+            options: filterOptions,
+            keys: ['management'],
+            onFilter: this.onFilter,
+        };
+
         const rowsContainerStyles = {
             minHeight: `${rows.length * ROW_HEIGHT}px`,
         };
@@ -253,6 +331,7 @@ export default class TeamList extends React.PureComponent {
                     term={term}
                     placeholderEmpty={placeholderEmpty}
                     rowsContainerStyles={rowsContainerStyles}
+                    filterProps={filterProps}
                 />
             </div>
         );
