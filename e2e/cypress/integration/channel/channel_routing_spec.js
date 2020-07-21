155ce9e51dd154cfd9f859bb6c2f5e3ec2f5e8ec
Joseph Baylon <joseph.baylon@mattermost.com>
2020-07-21 08:22:40 -0700

    Fix other user prefix

--- e2e/cypress/integration/channel/channel_routing_spec.js
+++ e2e/cypress/integration/channel/channel_routing_spec.js
@@ -21,13 +21,13 @@ describe('Channel routing', () => {
             testTeam = team;
             testUser = user;
 
-            cy.apiCreateUser().then(({user: newUser}) => {
+            cy.apiCreateUser({prefix: 'other1'}).then(({user: newUser}) => {
                 otherUser1 = newUser;
 
                 cy.apiAddUserToTeam(team.id, newUser.id);
             });
 
-            cy.apiCreateUser().then(({user: newUser}) => {
+            cy.apiCreateUser({prefix: 'other2'}).then(({user: newUser}) => {
                 otherUser2 = newUser;
 
                 cy.apiAddUserToTeam(team.id, newUser.id);
