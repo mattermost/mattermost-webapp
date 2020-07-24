b75bda7dc4accd66755c432783edfa86e872d3d1
Devin Binnie <devin.binnie@mattermost.com>
2020-07-24 15:44:43 -0400

    Styling and help arrow for modal

--- components/sidebar/sidebar_next_steps/close_next_steps_modal.tsx
+++ components/sidebar/sidebar_next_steps/close_next_steps_modal.tsx
@@ -2,9 +2,11 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 import {FormattedMessage} from 'react-intl';
 
 import GenericModal from 'components/generic_modal';
+import closeNextStepsArrow from 'images/close_next_steps_arrow.svg';
 
 import './close_next_steps_modal.scss';
 
@@ -17,30 +19,45 @@ export default function CloseNextStepsModal(props: Props) {
     const {onConfirm, onCancel} = props;
 
     return (
-        <GenericModal
-            show={true}
-            onHide={onCancel}
-            handleConfirm={onConfirm}
-            handleCancel={onCancel}
-            modalHeaderText={(
-                <FormattedMessage
-                    id={'close_next_steps_modal.header'}
-                    defaultMessage={'Remove Tip & Next Steps?'}
-                />
+        <>
+            {ReactDOM.createPortal(
+                <div className='CloseNextStepsModal__helpBox'>
+                    <img
+                        className='CloseNextStepsModal__arrow'
+                        src={closeNextStepsArrow}
+                    />
+                    <span className='CloseNextStepsModal__helpText'>
+                        <FormattedMessage
+                            id='close_next_steps_modal.helpText'
+                            defaultMessage='Access Tips & Next Steps any time through the Help section in the Main Menu'
+                        />
+                    </span>
+                </div>,
+                document.body as HTMLElement
             )}
-            confirmButtonText={(
-                <FormattedMessage
-                    id={'close_next_steps_modal.confirm'}
-                    defaultMessage='Remove'
-                />
-            )}
-        >
-            <span className='CloseNextStepsModal__mainText'>
+            <GenericModal
+                show={true}
+                onHide={onCancel}
+                handleConfirm={onConfirm}
+                handleCancel={onCancel}
+                modalHeaderText={(
+                    <FormattedMessage
+                        id={'close_next_steps_modal.header'}
+                        defaultMessage={'Remove Tip & Next Steps?'}
+                    />
+                )}
+                confirmButtonText={(
+                    <FormattedMessage
+                        id={'close_next_steps_modal.confirm'}
+                        defaultMessage='Remove'
+                    />
+                )}
+            >
                 <FormattedMessage
                     id={'close_next_steps_modal.mainText'}
                     defaultMessage='This will remove this section from your sidebar, but you can access it later in the Help section of the Main Menu.'
                 />
-            </span>
-        </GenericModal>
+            </GenericModal>
+        </>
     );
 }
