68ec900160f9fce3b0fb61e12a6af40d8fc1fbad
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2020-07-28 16:19:32 -0400

    Address PR comments

--- components/admin_console/team_channel_settings/channel/list/channel_list.tsx
+++ components/admin_console/team_channel_settings/channel/list/channel_list.tsx
@@ -175,14 +175,14 @@ export default class ChannelList extends React.PureComponent<ChannelListProps, C
         channelsToDisplay = channelsToDisplay.slice(startCount - 1, endCount);
 
         return channelsToDisplay.map((channel) => {
-            let iconToDisplay = <GlobeIcon className='channel-icon channel-icon__globe channel-icon___lowerOpacity channel-icon___left2Pixel channel-icon___increaseMargin'/>;
+            let iconToDisplay = <GlobeIcon className='channel-icon'/>;
 
             if (channel.type === Constants.PRIVATE_CHANNEL) {
-                iconToDisplay = <LockIcon className='channel-icon channel-icon__lock channel-icon___lowerOpacity channel-icon___left2Pixel channel-icon___increaseMargin'/>;
+                iconToDisplay = <LockIcon className='channel-icon'/>;
             }
 
             if (isArchivedChannel(channel)) {
-                iconToDisplay = <ArchiveIcon className='channel-icon channel-icon__archive channel-icon___lowerOpacity channel-icon___removeLeft2Pixel channel-icon___increaseMargin'/>;
+                iconToDisplay = <ArchiveIcon className='channel-icon'/>;
             }
 
             return {
