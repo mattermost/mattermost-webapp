6ffd7858ed72b9a16c44363fd2393a90fe1c6614
sridhar02 <kattasridhar02@gmail.com>
2020-07-30 12:02:54 +0530

    short cuts for tool tips

--- components/channel_header/components/header_icon_wrapper.js
+++ components/channel_header/components/header_icon_wrapper.js
@@ -8,9 +8,38 @@ import {Tooltip} from 'react-bootstrap';
 
 import OverlayTrigger from 'components/overlay_trigger';
 
+import * as Utils from 'utils/utils';
 import {localizeMessage} from 'utils/utils.jsx';
 import {Constants} from 'utils/constants';
 import {t} from 'utils/i18n';
+
+function renderShortcut(text) {
+    if (!text) {
+        return null;
+    }
+
+    const shortcut = text.split('\t');
+    const description = <span>{shortcut[0]}</span>;
+
+    let keys = null;
+    if (shortcut.length > 1) {
+        keys = shortcut[1].split('|').map((key) => (
+            <span
+                className='shortcut-key'
+                key={key}
+            >
+                {key}
+            </span>
+        ));
+    }
+
+    return (
+        <div className='shortcut-line'>
+            {description}
+            {keys}
+        </div>
+    );
+}
 
 export default function HeaderIconWrapper({
     iconComponent,
@@ -28,24 +57,41 @@ export default function HeaderIconWrapper({
             id: 'flaggedTooltip',
             messageID: t('channel_header.flagged'),
             message: 'Saved posts',
+            default: {
+                defaultMessage: 'Recent mentions:\tCtrl|Shift|M',
+            },
         },
         pinnedPosts: {
             class: '',
             id: 'pinnedPostTooltip',
             messageID: t('channel_header.pinnedPosts'),
             message: 'Pinned posts',
+            default: {
+                defaultMessage: 'Recent mentions:\tCtrl|Shift|M',
+            },
         },
         recentMentions: {
             class: '',
             id: 'recentMentionsTooltip',
             messageID: t('channel_header.recentMentions'),
             message: 'Recent mentions',
+            default: {
+                id: t('shortcuts.nav.recent_mentions'),
+                defaultMessage: 'Ctrl|Shift|M',
+            },
+            mac: {
+                id: t('shortcuts.nav.recent_mentions.mac'),
+                defaultMessage: 'Recent mentions:\t⌘|Shift|M',
+            },
         },
         search: {
             class: '',
             id: 'searchTooltip',
             messageID: t('channel_header.search'),
             message: 'Search',
+            default: {
+                defaultMessage: 'Recent mentions:\tCtrl|Shift|M',
+            },
         },
     };
 
@@ -54,6 +100,7 @@ export default function HeaderIconWrapper({
             return null;
         }
 
+        // const isMac = Utils.ismac();
         return (
             <Tooltip
                 id={toolTips[key].id}
@@ -63,6 +110,7 @@ export default function HeaderIconWrapper({
                     id={toolTips[key].messageID}
                     defaultMessage={toolTips[key].message}
                 />
+                <p>{toolTips[key].default.defaultMessage}</p>
             </Tooltip>
         );
     }
