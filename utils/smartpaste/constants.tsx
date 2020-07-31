34d0afaba3a5bd55a1d75aa8bc516336a7e23a3c
Jesús Espino <jespinog@gmail.com>
2020-07-31 20:35:27 +0200

    Fixing tests

--- utils/smartpaste/constants.tsx
+++ utils/smartpaste/constants.tsx
@@ -1,4 +1,4 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export const NEW_LINE_REPLACEMENT = '#*#*NEW_LINE_REPLACEMENT*#*#';
+export const NEW_LINE_REPLACEMENT = '####NEW_LINE_REPLACEMENT####';
