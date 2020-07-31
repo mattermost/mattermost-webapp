57f5fcdc2c9ba42ed0c41ab647bba6cc960faa1d
Jesús Espino <jespinog@gmail.com>
2020-07-31 12:34:05 +0200

    Imprived types definitions of the new imported modules

--- types/external/lang-detector.d.ts
+++ types/external/lang-detector.d.ts
@@ -1,4 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-declare module 'lang-detector';
+declare module 'lang-detector' {
+    export default function detectLang(snippet: string, options: {statistics: true; heuristic?: bool}): {detected: string; statistics: {[key: string]: number}};
+    export default function detectLang(snippet: string, options?: {statistics?: false; heuristic?: bool}): string | {detected: string; statistics: {[key: string]: number}};
+}
--- types/external/turndown-plugin-gfm.d.ts
+++ types/external/turndown-plugin-gfm.d.ts
@@ -1,4 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-declare module 'turndown-plugin-gfm';
+declare module 'turndown-plugin-gfm' {
+    export function strikethrough(service: TurndownService): void;
+    export function taskListItems(service: TurndownService): void;
+}
