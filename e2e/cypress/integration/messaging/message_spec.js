c25c052b00357bac65673d0b59793ff6839cc2d5
clarmso <clare.m.so+github@gmail.com>
2020-08-03 10:19:14 -0400

    Address code review comments

--- e2e/cypress/integration/messaging/message_spec.js
+++ e2e/cypress/integration/messaging/message_spec.js
@@ -186,17 +186,20 @@ describe('Message', () => {
     it('MM-T1797 Compact view: Show single image thumbnail', () => {
         const filename = 'image-small-height.png';
 
-        // Make a post with some text and a single image
-        cy.get('div[id="centerChannelFooter"]').find('input[id="fileUploadInput"]').attachFile(filename);
+        // # Set message display setting to compact
+        cy.apiSaveMessageDisplayPreference('compact');
+
+        // # Make a post with some text and a single image
+        cy.get('#centerChannelFooter').find('#fileUploadInput').attachFile(filename);
         cy.postMessage(MESSAGES.MEDIUM);
 
-        cy.get('div[class="file__image"]').last().within(() => {
-            // The name of the image appears on a new line
-            cy.contains('div', filename).should('be.visible');
+        cy.get('div.file__image').last().within(() => {
+            // *  The name of the image appears on a new line and is not bolded
+            cy.contains('div', filename).should('be.visible').and('have.css', 'font-weight', '400');
 
-            // There are arrows to collapse the preview
+            // * There are arrows to collapse the preview
             cy.get('img[src*="preview"]').should('be.visible');
-            cy.get('button').click();
+            cy.findByLabelText('Toggle Embed Visibility').should('exist').and('have.attr', 'data-expanded', 'true').click();
             cy.get('img[src*="preview"]').should('not.be.visible');
         });
     });
