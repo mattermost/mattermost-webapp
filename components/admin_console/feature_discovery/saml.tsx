0328bfff9613be657b012dceff1ebceaac4b8151
Jason Blais <13119842+jasonblais@users.noreply.github.com>
2020-08-06 16:24:32 -0400

    Update saml.tsx

--- components/admin_console/feature_discovery/saml.tsx
+++ components/admin_console/feature_discovery/saml.tsx
@@ -13,7 +13,7 @@ const SAMLFeatureDiscovery: React.FC = () => {
         <FeatureDiscovery
             featureName='saml'
             titleID='admin.saml_feature_discovery.title'
-            titleDefault='Integrate SAML 2.0 with Mattermost Enterprise Edition'
+            titleDefault='Integrate SAML 2.0 with Mattermost Enterprise E20'
             copyID='admin.saml_feature_discovery.copy'
             copyDefault={'When you connect Mattermost with your organization\'s single sign-on provider, users can access Mattermost without having to re-enter their credentials.'}
             learnMoreURL='https://www.mattermost.com/docs-saml/?utm_medium=product&utm_source=product-feature-discovery&utm_content=saml'
