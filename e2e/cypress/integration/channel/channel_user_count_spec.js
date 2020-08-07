7b0e728736357e211b98b03a0ab03ae63000cd46
Simon Beaulieu <s.beaulie2@gmail.com>
2020-08-07 01:28:45 -0400

    Fix lint issues

--- e2e/cypress/integration/channel/channel_user_count_spec.js
+++ e2e/cypress/integration/channel/channel_user_count_spec.js
@@ -12,31 +12,32 @@
 describe('Channel user count', () => {
     let testTeam;
     let secondUser;
-    before(() => {
+    before(() =>
+
         // [#] Create a new user and assign it to a team
-        cy.apiInitSetup().then(({team, user}) => {
+        cy.apiInitSetup().then(({team}) => {
             testTeam = team;
 
             // [#] Visit 'off-topic' channel for this team, as sysadmin
             cy.visit(`/${testTeam.name}/channels/off-topic`);
-        });
-    });
+        }),
+    );
 
-    it('MM-T481 User count is updated if user automatically joins channel', () => {            
+    it('MM-T481 User count is updated if user automatically joins channel', () => {
         const initialUserCount = 2;
 
         // [*] Assert channel user count displays '2' (system admin + main user)
-        cy.get('#channelMemberCountText').should('have.text', `${initialUserCount}`)
-        cy.get('#channelMemberCountText').invoke('text').as('initialUserCountText')
-    
-        // [#] Create another user
-        cy.apiCreateUser().then(({user}) => {          
-            secondUser = user;  
+        cy.get('#channelMemberCountText').should('have.text', `${initialUserCount}`);
+        cy.get('#channelMemberCountText').invoke('text').as('initialUserCountText');
 
-            // [#] Add new user (secondUser) to current team            
+        // [#] Create another user
+        cy.apiCreateUser().then(({user}) => {
+            secondUser = user;
+
+            // [#] Add new user (secondUser) to current team
             cy.apiAddUserToTeam(testTeam.id, secondUser.id);
         });
-        
+
         cy.apiGetChannelByName(testTeam.name, 'off-topic').then((response) => {
             const channel = response.body;
 
@@ -45,7 +46,6 @@ describe('Channel user count', () => {
         });
 
         // [*] Assert channel user count now displays '3'  (system admin + main user + second user)
-        cy.get('#channelMemberCountText').should('have.text', `${initialUserCount + 1}`)
+        cy.get('#channelMemberCountText').should('have.text', `${initialUserCount + 1}`);
     });
-
 });
