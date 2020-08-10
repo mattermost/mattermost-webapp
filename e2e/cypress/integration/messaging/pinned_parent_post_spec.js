dc73af5d170d59f7d62ae8d1f147ddf732f61809
Soo Hwan Kim <kimsoohwan10@gmail.com>
2020-08-10 22:54:03 +0900

    Implement suggested changes for issue MM-23346

--- e2e/cypress/integration/messaging/pinned_parent_post_spec.js
+++ e2e/cypress/integration/messaging/pinned_parent_post_spec.js
@@ -9,14 +9,11 @@
 
 // Group: @messaging
 
-import * as TIMEOUTS from '../../fixtures/timeouts';
-
 describe('Messaging', () => {
     let testTeam;
     let testChannel;
     let receiver;
     let sender;
-    let lastPostId;
 
     before(() => {
         // # Login as test user
@@ -37,11 +34,6 @@ describe('Messaging', () => {
             // # Visit a test channel and post a message
             cy.visit(`/${testTeam.name}/channels/${testChannel.name}`);
             cy.postMessage('Hello');
-
-            // # Assign lastPostId variable to the id of the last post
-            cy.getLastPostId().then((postId) => {
-                lastPostId = postId;
-            });
         });
     });
 
@@ -49,27 +41,27 @@ describe('Messaging', () => {
         // # Login as the other user
         cy.apiLogin(sender);
 
-        // # Wait for three seconds (sometimes the element won't render if we don't wait)
-        cy.wait(TIMEOUTS.THREE_SEC);
+        // # Reload the page
+        cy.reload();
 
-        // # Click the reply button, and post a reply four times and close the thread rhs tab
-        cy.get(`#post_${lastPostId}`).trigger('mouseover');
-        cy.get(`#CENTER_commentIcon_${lastPostId}`).click({force: true});
-        for (let i = 0; i < 5; i++) {
-            cy.get('#reply_textbox').type(`Hello to you too ${i}`);
-            cy.get('#addCommentButton').click();
-        }
-        cy.get('#rhsCloseButton').click();
+        cy.getLastPostId().then((postId) => {
+            // # Click the reply button, and post a reply four times and close the thread rhs tab
+            cy.get(`#post_${postId}`).trigger('mouseover');
+            cy.get(`#CENTER_commentIcon_${postId}`).click({force: true});
+            for (let i = 0; i < 5; i++) {
+                cy.get('#reply_textbox').should('be.visible').type(`Hello to you too ${i}`);
+                cy.get('#addCommentButton').should('be.enabled').click();
+            }
+            cy.get('#rhsCloseButton').click();
 
-        // # Wait for three seconds (sometimes the element won't render if we don't wait)
-        cy.wait(TIMEOUTS.THREE_SEC);
+            // # Pin the post to the channel
+            cy.getPostMenu(postId, 'Pin to Channel').click();
 
-        // # Pin the post to the channel
-        cy.get(`#post_${lastPostId}`).trigger('mouseover');
-        cy.get(`#CENTER_button_${lastPostId}`).click({force: true});
-        cy.get(`#pin_post_${lastPostId}`).click({force: true});
+            // # Find the 'Pinned' span in the post pre-header to verify that the post was actually pinned
+            cy.findByText('Pinned').should('exist');
 
-        // * Assert that the reply count exists and is correct
-        cy.get(`#CENTER_commentIcon_${lastPostId}`).find('span').eq(0).find('span').eq(1).should('have.text', '5');
+            // * Assert that the reply count exists and is correct
+            cy.get(`#CENTER_commentIcon_${postId}`).find('span').eq(0).find('span').eq(1).should('have.text', '5');
+        });
     });
 });
