aa0987c2f5d518029953ec1a147a5dceae05b670
David Janda <djanda97@gmail.com>
2020-08-11 21:14:22 -0700

    Updated to check initial render state and image preview visibility

--- e2e/cypress/integration/files_and_attachments/image_link_preview_spec.js
+++ e2e/cypress/integration/files_and_attachments/image_link_preview_spec.js
@@ -11,32 +11,28 @@ describe('Image Link Preview', () => {
     let testTeam;
 
     before(() => {
+        // # Enable Link Previews
+        cy.apiUpdateConfig({
+            ServiceSettings: {
+                EnableLinkPreviews: true,
+            },
+        });
+
         // # Create new team and new user and visit Town Square channel
         cy.apiInitSetup({loginAfter: true}).then(({team}) => {
             testTeam = team;
+
+            // # For test user, enable link previews and expand image previews
+            cy.apiSaveLinkPreviewsPreference('true');
+
+            cy.apiSaveCollapsePreviewsPreference('false');
+
             cy.visit(`/${testTeam.name}/channels/town-square`);
         });
     });
 
     it('MM-T331 Image link preview - Collapse and expand', () => {
         const link = 'http://www.traveller.com.au/content/dam/images/g/u/n/q/h/0/image.related.articleLeadwide.620x349.gunpvd.png/1488330286332.png';
-
-        const expanded = 'button[data-expanded="true"]';
-        const collapsed = 'button[data-expanded="false"]';
-
-        // # Ensure Account Settings -> Display -> Default appearance of image previews is set to Expanded
-
-        cy.get('#sidebarHeaderDropdownButton').should('be.visible').click();
-
-        cy.get('.dropdown-menu').should('be.visible').findByText('Account Settings').click();
-
-        cy.get('.settings-links').should('be.visible').findByText('Display').click();
-
-        cy.get('#collapseDesc').within(() => {
-            cy.get('span').contains('Expanded');
-        });
-
-        cy.get('#accountSettingsHeader > .close').click();
 
         // # Post a link to an externally hosted image
         cy.postMessage(link);
@@ -47,31 +43,50 @@ describe('Image Link Preview', () => {
         cy.postMessageReplyInRHS(link);
 
         cy.getLastPostId().then((postId) => {
-            cy.get(`#post_${postId}`).within(() => {
-                cy.get(expanded).click().then(($element) => {
-                    expect($element).to.have.attr('data-expanded', 'false');
-                });
+            // * Verify it renders correctly both on center and RHS view
+            cy.get(`#post_${postId}`).should('be.visible').within(() => {
+                cy.findByLabelText('Toggle Embed Visibility').
+                    should('be.visible').and('have.attr', 'data-expanded', 'true');
+                cy.findByLabelText('file thumbnail').should('be.visible');
             });
 
-            cy.get(`#rhsPost_${postId}`).within(() => {
-                cy.get(collapsed).click().then(($element) => {
-                    expect($element).to.have.attr('data-expanded', 'true');
-                });
+            cy.get(`#rhsPost_${postId}`).should('be.visible').within(() => {
+                cy.findByLabelText('Toggle Embed Visibility').
+                    should('be.visible').and('have.attr', 'data-expanded', 'true');
+                cy.findByLabelText('file thumbnail').should('be.visible');
             });
+
+            // # In center, click the collapse arrows to collapse the image preview
+            cy.get(`#post_${postId}`).findByLabelText('Toggle Embed Visibility').
+                click().
+                should('have.attr', 'data-expanded', 'false');
+
+            // * Observe it collapses in both center and RHS view
+            cy.get(`#post_${postId}`).findByLabelText('file thumbnail').should('not.exist');
+
+            cy.get(`#rhsPost_${postId}`).findByLabelText('file thumbnail').should('not.exist');
+
+            // # In RHS, click the expand arrows to expand the image preview again
+            cy.get(`#rhsPost_${postId}`).findByLabelText('Toggle Embed Visibility').
+                click().
+                should('have.attr', 'data-expanded', 'true');
+
+            // * Observe it expand in center and RHS
+            cy.get(`#post_${postId}`).findByLabelText('file thumbnail').should('be.visible');
+
+            cy.get(`#rhsPost_${postId}`).findByLabelText('file thumbnail').should('be.visible');
         });
 
         // # In center message box, post slash command /collapse
         cy.postMessage('/collapse');
 
         // # Observe all image previews collapse
-        cy.get(collapsed).should('exist');
-        cy.get(expanded).should('not.exist');
+        cy.findByLabelText('file thumbnail').should('not.exist');
 
         // # In RHS reply box, post slash command /expand
         cy.postMessageReplyInRHS('/expand');
 
         // # All image previews expand back open
-        cy.get(expanded).should('exist');
-        cy.get(collapsed).should('not.exist');
+        cy.findAllByLabelText('file thumbnail').should('be.visible').and('have.length', 4);
     });
 });
