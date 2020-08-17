194c5e3c05859190b806ffd04afdb782f32c0b47
Md_ZubairAhmed <m-zubairahmed@protonmail.com>
2020-08-17 19:01:10 +0530

    MM-T702 Edit to invalid URL (#6160)

--- e2e/cypress/integration/integrations/integrations_spec.js
+++ e2e/cypress/integration/integrations/integrations_spec.js
@@ -278,6 +278,101 @@ describe('Integrations page', () => {
             });
         });
     });
+
+    it('MM-T702 Edit to invalid URL', () => {
+        // # Visit home channel
+        cy.visit(`/${testTeam.name}/channels/town-square`);
+
+        // # Open main menu
+        cy.findByLabelText('main menu').should('be.visible').click();
+
+        // # Scan the area of main menu section
+        cy.get('#sidebarDropdownMenu').within(() => {
+            // # Open integrations menu
+            cy.findByText('Integrations').should('exist').and('be.visible').click();
+        });
+
+        // * Verify we are at integrations page URL
+        cy.url().should('include', '/integrations');
+
+        // # Scan the area of integrations list
+        cy.get('.integrations-list').should('exist').within(() => {
+            // # Open Slash commands directory
+            cy.findByText('Slash Commands').should('exist').and('be.visible').click({force: true});
+        });
+
+        // * Verify we are at slash commands URL
+        cy.url().should('include', '/integrations/commands');
+
+        // # Hit create slash command button
+        cy.findByText('Add Slash Command').should('exist').and('be.visible').click();
+
+        // * Verify we are at slash commands add URL
+        cy.url().should('include', '/integrations/commands/add');
+
+        const customSlashName = `customSlash-${Date.now()}`;
+
+        // # Enter a title for custom slash command
+        cy.findByLabelText('Title').should('exist').scrollIntoView().type(customSlashName);
+
+        // # Enter a trigger word for custom slash command
+        cy.findByLabelText('Command Trigger Word').should('exist').scrollIntoView().type(customSlashName);
+
+        // # Enter a request url for custom slash command
+        cy.findByLabelText('Request URL').should('exist').scrollIntoView().type('https://example.com');
+
+        // # Hit save to save the custom slash command
+        cy.findByText('Save').should('exist').scrollIntoView().click();
+
+        // * Verify we are at setup successfull URL
+        cy.url().should('include', '/integrations/commands/confirm');
+
+        // * Verify slash was successfully created
+        cy.findByText('Setup Successful').should('exist').and('be.visible');
+
+        // * Verify token was created
+        cy.findByText('Token').should('exist').and('be.visible');
+
+        // # Hit done to move from confirm screen
+        cy.findByText('Done').should('exist').and('be.visible').click();
+
+        // * Verify we are back to installed slash commands screen
+        cy.url().should('include', '/integrations/commands/installed');
+
+        // * Verify our created command is in the list
+        cy.findByText(customSlashName).should('exist').and('be.visible').scrollIntoView().
+            parents('.backstage-list__item').within(() => {
+                // # Click on the edit of slash command
+                cy.findByText('Edit').should('exist').and('be.visible').click();
+            });
+
+        // * Verify that we are on edit slash command page
+        cy.url().should('include', '/integrations/commands/edit');
+
+        // # Edit the request url field
+        cy.findByLabelText('Request URL').should('exist').and('be.visible').scrollIntoView().
+            clear().type('mattermost.com');
+
+        // # Hit save to save edited custom slash command
+        cy.findByText('Update').should('exist').scrollIntoView().click();
+
+        // * Verify that confirm modal is displayed to save the changes
+        cy.get('#confirmModal').should('exist').and('be.visible').within(() => {
+            // * Confirn that caution text is visible
+            cy.findByText('Your changes may break the existing slash command. Are you sure you would like to update it?').
+                should('exist').and('be.visible');
+
+            // # Press update button to confirm
+            cy.findByText('Update').should('exist').and('be.visible').click();
+        });
+
+        // * Verify that we get the error message
+        cy.findByText('Invalid URL. Must be a valid URL and start with http:// or https://.').
+            should('exist').and('be.visible').scrollIntoView();
+
+        // # Go back to home channel
+        cy.visit(`/${testTeam.name}/channels/town-square`);
+    });
 });
 
 function integrationPageTitleIsBold(title) {
