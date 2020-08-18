597c05b65939adc6e723fd0b60a0b224be2faca6
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2020-08-18 09:39:02 -0400

    Write MM-T2618 (1.0)

--- e2e/cypress/integration/enterprise/ldap/ldap_group_sync_spec.js
+++ e2e/cypress/integration/enterprise/ldap/ldap_group_sync_spec.js
@@ -60,7 +60,7 @@ context('ldap', () => {
                     }
                 });
     
-                // # Link board group
+                // # Link developers group
                 cy.visit('/admin_console/user_management/groups');
                 cy.get('#developers_group').then((el) => {
                     if (!el.text().includes('Edit')) {
@@ -78,18 +78,51 @@ context('ldap', () => {
 
         });
 
-        it('MM-T1537 - Sync Group Removal from Channel Configuration Page', () => {
+        // it('MM-T1537 - Sync Group Removal from Channel Configuration Page', () => {
+        //     // # Login as sysadmin and add board-one to test team
+        //     cy.apiAdminLogin();
+        //     cy.visit(`/admin_console/user_management/channels/${testChannel.id}`);
+        //     cy.wait(2000);
+
+        //     cy.findByTestId('addGroupsToChannelToggle').click();
+        //     cy.get('#multiSelectList').should('be.visible');
+        //     cy.get('#multiSelectList>div').children().eq(0).click();
+        //     cy.get('#saveItems').click();
+
+        //     cy.findByTestId('addGroupsToChannelToggle').click();
+        //     cy.get('#multiSelectList').should('be.visible');
+        //     cy.get('#multiSelectList>div').children().eq(0).click();
+        //     cy.get('#saveItems').click();
+
+        //     cy.get('#saveSetting').should('be.enabled').click({force: true});
+        //     cy.wait(1000);
+
+        //     cy.visit(`/admin_console/user_management/channels/${testChannel.id}`);
+        //     cy.wait(1000);
+
+        //     cy.get('.group-row').eq(0).scrollIntoView().should('be.visible').within(() => {
+        //         cy.get('.group-name').should('have.text', 'board');
+        //         cy.get('.group-actions > a').should('have.text', 'Remove').click({force: true});
+        //     });
+
+        //     cy.get('#saveSetting').should('be.enabled').click({force: true});
+        //     cy.wait(1000);
+
+        //     cy.visit(`/admin_console/user_management/channels/${testChannel.id}`);
+        //     cy.wait(1000);
+
+        //     cy.get('.group-row').should('have.length', 1);
+        // });
+
+        it('MM-T2618 - Team Configuration Page: Group removal User removed from sync\'ed team', () => {
             // # Login as sysadmin and add board-one to test team
             cy.apiAdminLogin();
-            cy.visit(`/admin_console/user_management/channels/${testChannel.id}`);
+            cy.visit(`/admin_console/user_management/teams/${testTeam.id}`);
             cy.wait(2000);
 
-            cy.findByTestId('addGroupsToChannelToggle').click();
-            cy.get('#multiSelectList').should('be.visible');
-            cy.get('#multiSelectList>div').children().eq(0).click();
-            cy.get('#saveItems').click();
+            cy.findByTestId('syncGroupSwitch').scrollIntoView().click();
 
-            cy.findByTestId('addGroupsToChannelToggle').click();
+            cy.findByTestId('addGroupsToTeamToggle').scrollIntoView().click();
             cy.get('#multiSelectList').should('be.visible');
             cy.get('#multiSelectList>div').children().eq(0).click();
             cy.get('#saveItems').click();
@@ -97,133 +130,17 @@ context('ldap', () => {
             cy.get('#saveSetting').should('be.enabled').click({force: true});
             cy.wait(1000);
 
-            cy.visit(`/admin_console/user_management/channels/${testChannel.id}`);
-            cy.wait(1000);
+            cy.get('#confirmModalButton').should('be.visible').click();
 
-            cy.get('.group-row').eq(0).scrollIntoView().should('be.visible').within(() => {
-                cy.get('.group-name').should('have.text', 'board');
-                cy.get('.group-actions > a').should('have.text', 'Remove').click({force: true});
-            });
+            cy.visit('/admin_console/user_management/groups');
+            cy.get('#board_edit').click();
 
-            cy.get('#saveSetting').should('be.enabled').click({force: true});
-            cy.wait(1000);
-
-            cy.visit(`/admin_console/user_management/channels/${testChannel.id}`);
-            cy.wait(1000);
-
-            cy.get('.group-row').should('have.length', 1);
+            cy.findByTestId(`${testTeam.display_name}_groupsyncable_remove`).click();
+            cy.get('#confirmModalBody').should('be.visible').and('have.text', `Removing this membership will prevent future users in this group from being added to the ${testTeam.display_name} team.`);
+            cy.get('#confirmModalButton').should('be.visible').click();
+            cy.get('#saveSetting').click();
         });
 
-        // it('LDAP login existing MM admin', () => {
-        //     // existing user, verify and logout
-        //     cy.doLDAPLogin(testSettings).then(() => {
-        //         cy.doLDAPLogout(testSettings);
-        //     });
-        // });
+
     });
-
-    // describe('LDAP Login flow - Member Login)', () => {
-    //     it('Invalid login with user filter', () => {
-    //         testSettings.user = user1;
-    //         const ldapSetting = {
-    //             LdapSettings: {
-    //                 UserFilter: '(cn=no_users)',
-    //             },
-    //         };
-    //         cy.apiAdminLogin().then(() => {
-    //             cy.apiUpdateConfig(ldapSetting).then(() => {
-    //                 cy.doLDAPLogin(testSettings).then(() => {
-    //                     cy.checkLoginFailed(testSettings);
-    //                 });
-    //             });
-    //         });
-    //     });
-
-    //     it('LDAP login, new MM user, no channels', () => {
-    //         testSettings.user = user1;
-    //         const ldapSetting = {
-    //             LdapSettings: {
-    //                 UserFilter: '(cn=test*)',
-    //             },
-    //         };
-    //         cy.apiAdminLogin().then(() => {
-    //             cy.apiUpdateConfig(ldapSetting).then(() => {
-    //                 cy.doLDAPLogin(testSettings).then(() => {
-    //                     cy.doMemberLogout(testSettings);
-    //                 });
-    //             });
-    //         });
-    //     });
-    // });
-
-    // describe('LDAP Login flow - Guest Login', () => {
-    //     it('Invalid login with guest filter', () => {
-    //         testSettings.user = guest1;
-    //         const ldapSetting = {
-    //             LdapSettings: {
-    //                 GuestFilter: '(cn=no_guests)',
-    //             },
-    //         };
-    //         cy.apiAdminLogin().then(() => {
-    //             cy.apiUpdateConfig(ldapSetting).then(() => {
-    //                 cy.doLDAPLogin(testSettings).then(() => {
-    //                     cy.get('#createPublicChannel').should('be.visible');
-    //                     cy.doMemberLogout(testSettings);
-    //                 });
-    //             });
-    //         });
-    //     });
-
-    //     it('LDAP login, new guest, no channels', () => {
-    //         testSettings.user = guest1;
-    //         const ldapSetting = {
-    //             LdapSettings: {
-    //                 GuestFilter: '(cn=board*)',
-    //             },
-    //         };
-    //         cy.apiAdminLogin().then(() => {
-    //             cy.apiUpdateConfig(ldapSetting).then(() => {
-    //                 cy.doLDAPLogin(testSettings).then(() => {
-    //                     cy.doGuestLogout(testSettings);
-    //                 });
-    //             });
-    //         });
-    //     });
-    // });
-
-    // describe('LDAP Add Member and Guest to teams and test logins', () => {
-    //     before(() => {
-    //         cy.apiAdminLogin();
-
-    //         cy.apiGetTeamByName(testSettings.teamName).then(({team}) => {
-    //             cy.apiGetChannelByName(testSettings.teamName, 'town-square').then((r2) => {
-    //                 const channelId = r2.body.id;
-    //                 cy.apiGetUserByEmail(guest1.email).then(({user}) => {
-    //                     cy.apiAddUserToTeam(team.id, user.id).then(() => {
-    //                         cy.apiAddUserToChannel(channelId, user.id);
-    //                     });
-    //                 });
-
-    //                 // add member user to team
-    //                 cy.apiGetUserByEmail(user1.email).then(({user}) => {
-    //                     cy.apiAddUserToTeam(team.id, user.id);
-    //                 });
-    //             });
-    //         });
-    //     });
-
-    //     it('LDAP Member login with team invite', () => {
-    //         testSettings.user = user1;
-    //         cy.doLDAPLogin(testSettings).then(() => {
-    //             cy.doLDAPLogout(testSettings);
-    //         });
-    //     });
-
-    //     it('LDAP Guest login with team invite', () => {
-    //         testSettings.user = guest1;
-    //         cy.doLDAPLogin(testSettings).then(() => {
-    //             cy.doGuestLogout(testSettings);
-    //         });
-    //     });
-    // });
 });
