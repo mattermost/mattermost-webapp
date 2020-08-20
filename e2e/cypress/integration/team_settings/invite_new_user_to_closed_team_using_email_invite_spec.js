c162aff02688604300c7fa73c7686ff2970ab3b5
Soo Hwan Kim <kimsoohwan10@gmail.com>
2020-08-21 00:25:49 +0900

    Add e2e test for MM-T388

--- e2e/cypress/integration/team_settings/invite_new_user_to_closed_team_using_email_invite_spec.js
+++ e2e/cypress/integration/team_settings/invite_new_user_to_closed_team_using_email_invite_spec.js
@@ -7,19 +7,18 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-// Stage: @prod
 // Group: @team_settings
 
 import * as TIMEOUTS from '../../fixtures/timeouts';
-
-import {getEmailUrl, getEmailMessageSeparator, reUrl} from '../../utils';
-
-let config;
+import {getEmailUrl, getEmailMessageSeparator, reUrl, getRandomId} from '../../utils';
 
 describe('Team Settings', () => {
-    let testUser;
-    let mentionedUser;
     let testTeam;
+    const randomId = getRandomId();
+    const username = `user${randomId}`;
+    const email = `user${randomId}@sample.mattermost.com`;
+    const password = 'passwd';
+
     const baseUrl = Cypress.config('baseUrl');
     const mailUrl = getEmailUrl(baseUrl);
 
@@ -27,28 +26,14 @@ describe('Team Settings', () => {
         // # Do email test if setup properly
         cy.apiEmailTest();
 
-        // # Get config
-        cy.apiGetConfig().then((data) => {
-            ({config} = data);
-        });
-
-        cy.apiInitSetup().then(({team, user}) => {
-            testUser = user;
+        cy.apiInitSetup().then(({team}) => {
             testTeam = team;
-
-            cy.apiCreateUser().then(({user: newUser}) => {
-                mentionedUser = newUser;
-                console.log(mentionedUser);
-            });
-
-            // # Login as test user and go to town square
-            // cy.apiLogin(testUser);
             cy.visit(`/${team.name}/channels/town-square`);
         });
     });
 
     it('MM-T385 Invite new user to closed team using email invite', () => {
-        // Open 'Team Settings' modal
+        // # Open 'Team Settings' modal
         cy.get('.sidebar-header-dropdown__icon').click();
         cy.findByText('Team Settings').should('be.visible').click();
 
@@ -59,81 +44,82 @@ describe('Team Settings', () => {
             // # Click on the 'Allow only users with a specific email domain to join this team' edit button
             cy.get('#allowed_domainsEdit').should('be.visible').click();
 
-            // # Set 'sample.mattermost.com' as the only allowed email domain and save
+            // * Verify that the '#allowedDomains' input field is empty
             cy.get('#allowedDomains').should('be.empty');
 
             // # Close the modal
             cy.get('#teamSettingsModalLabel').find('button').should('be.visible').click();
         });
 
-        		         // # Open the 'Invite People' full screen modal
+        // # Open the 'Invite People' full screen modal
         cy.get('.sidebar-header-dropdown__icon').click();
         cy.get('#invitePeople').find('button').eq(0).click();
 
+        // # Wait half a second to ensure that the modal has been fully loaded and invite a new user (with the email declared in the parent scope)
         cy.wait(TIMEOUTS.HALF_SEC);
-        cy.findByRole('textbox', {name: 'Add or Invite People'}).type(mentionedUser.email, {force: true}).wait(TIMEOUTS.HALF_SEC).type('{enter}');
+        cy.findByRole('textbox', {name: 'Add or Invite People'}).type(email, {force: true}).wait(TIMEOUTS.HALF_SEC).type('{enter}');
         cy.get('#inviteMembersButton').click();
 
-        // Wait for a while to ensure that email notification is sent.
+        // # Wait for a while to ensure that email notification is sent and logout from sysadmin account
         cy.wait(TIMEOUTS.FIVE_SEC);
+        cy.apiLogout();
 
-        cy.task('getRecentEmail', {username: mentionedUser.username, mailUrl}).then((response) => {
+        cy.task('getRecentEmail', {username, mailUrl}).then((response) => {
             const messageSeparator = getEmailMessageSeparator(baseUrl);
-
-            // verifyEmailNotification(response, config.TeamSettings.SiteName, testTeam.display_name, 'Town Square', mentionedUser, testUser, text, config.EmailSettings.FeedbackEmail, config.SupportSettings.SupportEmail, messageSeparator);
+            verifyEmailInvite(response, testTeam.name, testTeam.display_name, email, messageSeparator);
 
             const bodyText = response.data.body.text.split('\n');
-            console.log(bodyText);
+            const permalink = bodyText[6].match(reUrl)[0];
 
-            const permalink = bodyText[9].match(reUrl)[0];
-            const permalinkPostId = permalink.split('/')[6];
-
-            // # Visit permalink (e.g. click on email link), view in browser to proceed
+            // # Visit permalink (e.g. click on email link)
             cy.visit(permalink);
-            cy.findByText('View in Browser').click();
-
-            const postText = `#postMessageText_${permalinkPostId}`;
-
-            // cy.get(postText).should('have.text', text);
-
-            cy.getLastPostId().then((postId) => {
-                // * Should match last post and permalink post IDs
-                expect(permalinkPostId).to.equal(postId);
-            });
         });
+
+        // # Type username and password
+        cy.wait(TIMEOUTS.HALF_SEC);
+        cy.get('#name').type(username);
+        cy.wait(TIMEOUTS.HALF_SEC);
+        cy.get('#password').type(password);
+
+        // # Attempt to create an account by clicking on the 'Create Account' buton
+        cy.get('#createAccountButton').click();
+
+        // * Check that the display name of the team the user was invited to is being correctly displayed
+        cy.get('#headerTeamName').should('contain.text', testTeam.display_name);
+
+        // * Check that 'Town Square' is currently being selected
+        cy.get('.active').within(() => {
+            cy.get('#sidebarItem_town-square').should('exist');
+        });
+
+        // * Check that the 'Welcome to: Mattermost' message is visible
+        cy.get('#tutorialIntroOne').findByText('Welcome to:').should('be.visible');
+        cy.get('#tutorialIntroOne').findByText('Mattermost').should('be.visible');
     });
+
+    function verifyEmailInvite(response, teamName, teamDisplayName, invitedUserEmail, messageSeparator) {
+        const isoDate = new Date().toISOString().substring(0, 10);
+        const {data, status} = response;
+
+        // * Should return success status
+        expect(status).to.equal(200);
+
+        // * Verify that email is addressed to the correct user
+        expect(data.to.length).to.equal(1);
+        expect(data.to[0]).to.contain(invitedUserEmail);
+
+        // * Verify that date is current
+        expect(data.date).to.contain(isoDate);
+
+        // * Verify that the email subject is correct
+        expect(data.subject).to.contain(`[Mattermost] sysadmin invited you to join ${teamDisplayName} Team`);
+
+        // * Verify that the email body is correct
+        const bodyText = data.body.text.split(messageSeparator);
+        expect(bodyText.length).to.equal(17);
+        expect(bodyText[1]).to.equal('You\'ve been invited');
+        expect(bodyText[4]).to.equal(`*sysadmin* , has invited you to join *${teamDisplayName}*.`);
+        expect(bodyText[10]).to.contain(`${baseUrl}/${teamName}`);
+    }
 });
 
-function verifyEmailNotification(response, siteName, teamDisplayName, channelDisplayName, mentionedUser, byUser, message, feedbackEmail, supportEmail, messageSeparator) {
-    const isoDate = new Date().toISOString().substring(0, 10);
-    const {data, status} = response;
-
-    // * Should return success status
-    expect(status).to.equal(200);
-
-    // * Verify that email is addressed to mentionedUser
-    expect(data.to.length).to.equal(1);
-    expect(data.to[0]).to.contain(mentionedUser.email);
-
-    // * Verify that email is from default feedback email
-    expect(data.from).to.contain(feedbackEmail);
-
-    // * Verify that date is current
-    expect(data.date).to.contain(isoDate);
-
-    // * Verify that the email subject is correct
-    expect(data.subject).to.contain(`[${siteName}] Notification in ${teamDisplayName}`);
-
-    // * Verify that the email body is correct
-    const bodyText = data.body.text.split(messageSeparator);
-    expect(bodyText.length).to.equal(16);
-    expect(bodyText[1]).to.equal('You have a new notification.');
-    expect(bodyText[4]).to.equal(`Channel: ${channelDisplayName}`);
-    expect(bodyText[5]).to.contain(`@${byUser.username}`);
-    expect(bodyText[7]).to.equal(`${message}`);
-    expect(bodyText[9]).to.contain('Go To Post');
-    expect(bodyText[11]).to.equal(`Any questions at all, mail us any time: ${supportEmail}`);
-    expect(bodyText[12]).to.equal('Best wishes,');
-    expect(bodyText[13]).to.equal(`The ${siteName} Team`);
-    expect(bodyText[15]).to.equal('To change your notification preferences, log in to your team site and go to Account Settings > Notifications.');
-}
