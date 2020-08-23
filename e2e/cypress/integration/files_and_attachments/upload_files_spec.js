56effdb2f38cb799252ea4699007cbe7a08c187d
za <m-zubairahmed@protonmail.com>
2020-08-24 03:33:24 +0530

    test

--- e2e/cypress/integration/files_and_attachments/upload_files_spec.js
+++ e2e/cypress/integration/files_and_attachments/upload_files_spec.js
@@ -10,6 +10,8 @@
 // Stage: @prod
 // Group: @file_and_attachments
 
+import * as MESSAGES from '../../fixtures/messages';
+
 describe('Upload Files', () => {
     let testTeam;
 
@@ -52,4 +54,89 @@ describe('Upload Files', () => {
             });
         });
     });
+
+    it('MM-T340 Download - File name link on thumbnail', () => {
+        const attachmentFilesList = [
+            {
+                filename: 'word-file.doc',
+                extensions: 'DOC',
+                type: 'document',
+            },
+            {
+                filename: 'wordx-file.docx',
+                extensions: 'DOCX',
+                type: 'document',
+            },
+            {
+                filename: 'powerpoint-file.ppt',
+                extensions: 'PPT',
+                type: 'document',
+            },
+            {
+                filename: 'powerpointx-file.pptx',
+                extensions: 'PPTX',
+                type: 'document',
+            },
+            {
+                filename: 'image-file.jpg',
+                extensions: 'JPG',
+                type: 'image',
+            },
+        ];
+
+        attachmentFilesList.forEach((file) => {
+            // # Attach the file as attachment and post a message
+            cy.get('#fileUploadInput').attachFile(file.filename);
+            cy.postMessage(MESSAGES.SMALL);
+
+            // # Get the post id of the last message
+            cy.getLastPostId().then((lastPostId) => {
+                // # Scan inside of the last post message
+                cy.get(`#${lastPostId}_message`).should('exist').and('be.visible').within(() => {
+                    // # If file type is document then file container will be rendered
+                    if (file.type === 'document') {
+                        // * Check if the download icon is exists
+                        cy.findByLabelText('download').should('exist').and((fileAttachment) => {
+                            // * Verify that download link has correct name
+                            verifyLinkHasDownloadProperties(fileAttachment, file.filename);
+                        });
+
+                        // * Check if the file name is shown in the attachment
+                        cy.findByText(file.filename).should('be.visible');
+
+                        // * Check if correct extension is shown in the attachment and click to open preview
+                        cy.findByText(file.extensions).should('be.visible').click();
+                    } else if (file.type === 'image') {
+                        // * Check that image is in the post message with valid source link
+                        cy.findByLabelText(`file thumbnail ${file.filename}`).should('be.visible').
+                            and('have.attr', 'src');
+
+                        // # Click on the image to open the preview
+                        cy.findByLabelText(`file thumbnail ${file.filename}`).click();
+                    }
+                });
+            });
+
+            // * Verify preview modal is opened
+            cy.get('.a11y__modal').should('exist').and('be.visible').
+                within(() => {
+                    // * Check for download property of the download button
+                    cy.findByText('Download').should('be.visible').parent().and((fileAttachment) => {
+                        // * Verify that download link has correct name
+                        verifyLinkHasDownloadProperties(fileAttachment, file.filename);
+                    });
+                });
+
+            // # Close the image preview modal
+            cy.get('body').type('{esc}');
+        });
+    });
 });
+
+function verifyLinkHasDownloadProperties(fileAttachment, filename) {
+    // * Verify if download attribute exists which allows to download instead of navigation
+    expect(fileAttachment.attr('download')).to.equal(filename);
+
+    // * Verify it has a not empty download link
+    expect(fileAttachment.attr('href')).is.not.null;
+}
