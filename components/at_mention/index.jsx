32a6f770cffb9288cb6e06559a734684485fcb4b
fm2munsh <farhan.munshi@mattermost.com>
2020-08-24 11:47:16 -0400

    Fix outsider valid groups not showing as blue

--- components/at_mention/index.jsx
+++ components/at_mention/index.jsx
@@ -4,20 +4,16 @@
 import {connect} from 'react-redux';
 import {getTeammateNameDisplaySetting} from 'mattermost-redux/selectors/entities/preferences';
 import {getCurrentUserId, getUsersByUsername} from 'mattermost-redux/selectors/entities/users';
-import {getAssociatedGroupsByName} from 'mattermost-redux/selectors/entities/groups';
-import {getCurrentTeamId} from 'mattermost-redux/selectors/entities/teams';
-import {getCurrentChannelId} from 'mattermost-redux/selectors/entities/channels';
-
-import {getSelectedChannelId} from 'selectors/rhs';
+import {getAllGroupsForReferenceByName} from 'mattermost-redux/selectors/entities/groups';
 
 import AtMention from './at_mention.jsx';
 
-function mapStateToProps(state, props) {
+function mapStateToProps(state) {
     return {
         currentUserId: getCurrentUserId(state),
         teammateNameDisplay: getTeammateNameDisplaySetting(state),
         usersByUsername: getUsersByUsername(state),
-        groupsByName: getAssociatedGroupsByName(state, getCurrentTeamId(state), props.isRHS ? getSelectedChannelId(state) : getCurrentChannelId(state)),
+        groupsByName: getAllGroupsForReferenceByName(state),
     };
 }
 
