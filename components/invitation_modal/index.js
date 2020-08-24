9d7f3bb78883fea1047ba42be7c93af22cac8e6b
Nick Misasi <nick.misasi@mattermost.com>
2020-08-24 10:26:21 -0400

    undo change to index.js

--- components/invitation_modal/index.js
+++ components/invitation_modal/index.js
@@ -4,9 +4,7 @@
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
-import {
-    getCurrentTeam,
-} from 'mattermost-redux/selectors/entities/teams';
+import {getCurrentTeam} from 'mattermost-redux/selectors/entities/teams';
 import {getChannelsInCurrentTeam} from 'mattermost-redux/selectors/entities/channels';
 import {haveIChannelPermission, haveITeamPermission} from 'mattermost-redux/selectors/entities/roles';
 import {getConfig, getLicense} from 'mattermost-redux/selectors/entities/general';
