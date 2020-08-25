c006629b3796ec71c261004bb0ac77dfef1b7009
Caleb Roseland <caleb@calebroseland.com>
2020-08-25 13:06:06 -0500

    [MM-27261] fix: scrollbar click/drag in edit post modal (#6188)
    * add scroll class on overflow
    
    * update snapshots
    
    * update test
    
    * fix cursor issues; use detected scrollbar width

--- components/create_comment/create_comment.jsx
+++ components/create_comment/create_comment.jsx
@@ -262,6 +262,7 @@ class CreateComment extends React.PureComponent {
             channelTimezoneCount: 0,
             uploadsProgressPercent: {},
             renderScrollbar: false,
+            scrollbarWidth: 0,
             suggestionListStyle: 'top',
             mentions: [],
             memberNotifyCount: 0,
@@ -939,6 +940,11 @@ class CreateComment extends React.PureComponent {
 
     handleHeightChange = (height, maxHeight) => {
         this.setState({renderScrollbar: height > maxHeight});
+        window.requestAnimationFrame(() => {
+            if (this.refs.textbox) {
+                this.setState({scrollbarWidth: Utils.scrollbarWidth(this.refs.textbox.getInputBox())});
+            }
+        });
     }
 
     render() {
@@ -1180,6 +1186,7 @@ class CreateComment extends React.PureComponent {
                     aria-label={ariaLabelReplyInput}
                     tabIndex='-1'
                     className={`post-create a11y__region${scrollbarClass}`}
+                    style={this.state.renderScrollbar && this.state.scrollbarWidth ? {'--detected-scrollbar-width': `${this.state.scrollbarWidth}px`} : undefined}
                     data-a11y-sort-order='4'
                 >
                     <div
