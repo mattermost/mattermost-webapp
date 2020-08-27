5558ab8665e47e79d58c6ea1b4787f098de3d130
Guillermo Vaya <guivaya@gmail.com>
2020-08-27 12:11:39 +0200

    [MM-27207] first test

--- e2e/cypress/integration/channel/archived_channels_spec.js
+++ e2e/cypress/integration/channel/archived_channels_spec.js
@@ -55,5 +55,24 @@ describe('Leave an archived channel', () => {
         // * Verify sure that we have switched channels
         cy.get('#channelHeaderTitle').should('not.contain', testChannel.display_name);
     });
+    it('MM-T1670 Can view channel info for an archived channel', () => {
+        // # Visit archived channel
+        cy.visit(`/${testTeam.name}/channels/${testChannel.name}`);
+
+        // # Click the channel header
+        cy.get('#channelHeaderDropdownButton button').click();
+
+        // # Select View Info
+        cy.get('#channelViewInfo button').click();
+
+        // * Channel title is shown with archived icon
+        cy.get('#channelInfoModalLabel span.icon__archive').should('be.visible');
+        cy.contains('#channelInfoModalLabel strong', `${testChannel.display_name}`).should('be.visible');
+
+        // * Channel URL is listed (non-linked text)
+        cy.url().then((loc) => { 
+            cy.contains('div.info__value', loc).should('be.visible');
+        });
+    });
 });
 
