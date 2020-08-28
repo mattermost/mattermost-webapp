c497497c1490e46a9d39bf8b8e029c18fa3c1353
Mohan Prasath <openmohan@gmail.com>
2020-08-28 21:46:39 +0530

    MM-T12 Loading indicator when posting images (#6271)
    * MM-T12 Loading indicator when posting images
    
    * addressed comments on image loading component visibility
    
    * simplified and made post multiple times

--- e2e/cypress/integration/files_and_attachments/upload_files_spec.js
+++ e2e/cypress/integration/files_and_attachments/upload_files_spec.js
@@ -10,14 +10,28 @@
 // Stage: @prod
 // Group: @file_and_attachments
 
+import * as TIMEOUTS from '../../fixtures/timeouts';
+
 describe('Upload Files', () => {
     let testTeam;
+    let testChannel;
+    let otherUser;
 
-    before(() => {
+    beforeEach(() => {
         // # Create new team and new user and visit Town Square channel
-        cy.apiInitSetup({loginAfter: true}).then(({team}) => {
+        cy.apiInitSetup().then(({team, channel}) => {
             testTeam = team;
-            cy.visit(`/${testTeam.name}/channels/town-square`);
+            testChannel = channel;
+
+            cy.apiCreateUser().then(({user: user2}) => {
+                otherUser = user2;
+
+                cy.apiAddUserToTeam(testTeam.id, otherUser.id).then(() => {
+                    cy.apiAddUserToChannel(testChannel.id, otherUser.id);
+                });
+            });
+
+            cy.visit(`/${testTeam.name}/channels/${channel.name}`);
         });
     });
 
@@ -50,6 +64,38 @@ describe('Upload Files', () => {
             cy.get('img[src*="/preview"]').should('be.visible').and((img) => {
                 expect(img.width() / img.height()).to.be.closeTo(aspectRatio, 0.01);
             });
+        });
+    });
+
+    it('MM-T12 Loading indicator when posting images', () => {
+        const filename = 'huge-image.jpg';
+
+        // # Post an image in center channel
+        cy.get('#centerChannelFooter').find('#fileUploadInput').attachFile(filename);
+        cy.get('.post-image').should('be.visible');
+        cy.postMessage('{enter}');
+
+        // # Login as otherUser
+        cy.apiLogin(otherUser);
+
+        // # Reload the page
+        cy.reload();
+
+        // * Verify the image loading component is visible
+        cy.get('.image-container').should('be.visible').find('.image-loading__container').should('be.visible');
+
+        Cypress._.times(5, () => {
+            // # OtherUser creates posts in the channel
+            cy.postMessageAs({
+                sender: otherUser,
+                message: 'message',
+                channelId: testChannel.id,
+            });
+
+            // * Verify image is not loading for each posts
+            cy.get('.image-container').should('be.visible').find('.image-loading__container').should('not.exist');
+
+            cy.wait(TIMEOUTS.HALF_SEC);
         });
     });
 });
