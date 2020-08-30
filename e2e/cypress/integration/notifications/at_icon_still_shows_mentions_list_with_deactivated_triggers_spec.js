ffd72887bd6666df793177808df4ea7dca21281b
Soo Hwan Kim <kimsoohwan10@gmail.com>
2020-08-30 16:43:48 +0900

    Initial commit for MM-T550

---
+++ e2e/cypress/integration/notifications/at_icon_still_shows_mentions_list_with_deactivated_triggers_spec.js
@@ -0,0 +1,87 @@
+
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+// ***************************************************************
+// - [#] indicates a test step (e.g. # Go to a page)
+// - [*] indicates an assertion (e.g. * Check the title)
+// - Use element ID when selecting an element. Create one if none.
+// ***************************************************************
+
+// Group: @notifications
+
+import * as TIMEOUTS from '../../fixtures/timeouts';
+
+describe('Notifications', () => {
+    let testTeam;
+    let otherUser;
+
+    before(() => {
+        cy.apiInitSetup().then(({team}) => {
+            testTeam = team;
+            cy.apiCreateUser().then(({user}) => {
+                otherUser = user;
+                cy.apiAddUserToTeam(testTeam.id, otherUser.id);
+                cy.apiLogin(otherUser);
+            });
+
+            cy.visit(`/${testTeam.name}`);
+
+            // # Open 'Account Settings' modal
+            cy.findByLabelText('main menu').should('be.visible').click();
+            cy.findByText('Account Settings').should('be.visible').click();
+
+            // * Check that the 'Account Settings' modal was opened
+            cy.get('#accountSettingsModal').should('exist').within(() => {
+                cy.get('#notificationsButton').should('be.visible').click();
+                cy.get('#keysEdit').should('be.visible').click();
+
+                // * Uncheck all the 'Words That Trigger Mentions'
+                cy.get('#notificationTriggerFirst').should('not.be.checked');
+                cy.get('#notificationTriggerUsername').should('not.be.checked');
+                cy.get('#notificationTriggerShouts').should('be.checked').click().then(($shouts) => {
+                    cy.get($shouts).should('not.be.checked');
+                });
+                cy.get('#notificationTriggerCustomText').should('not.be.checked');
+                cy.get('#saveSetting').should('be.visible').click();
+
+                // # Close the modal
+                cy.get('#accountSettingsHeader').find('button').should('be.visible').click();
+            });
+            cy.apiLogout();
+
+            // # Login as sysadmin
+            cy.apiAdminLogin();
+            cy.visit(`/${testTeam.name}`);
+        });
+    });
+
+    it('MM-T546 Words that trigger mentions - Deselect username, still get mention when added to private channel', () => {
+        const text = `${otherUser.username} test message!`;
+
+        // # Type @ in the input box
+        cy.focused().type('@');
+
+        // * Make sure that the suggestion list is visible and that otherUser's username is visible
+        cy.get('#suggestionList').should('be.visible').within(() => {
+            cy.findByText(`@${otherUser.username}`).should('be.visible');
+        });
+
+        // # Post the text that was declared earlier and logout from sysadmin account
+        cy.focused().type(`${text}{enter}{enter}`);
+        cy.apiLogout();
+
+        // # Login as otherUser and visit the team
+        cy.apiLogin(otherUser);
+        cy.visit(`${testTeam.name}`);
+
+        // # Click on the 'Recent Mentions' button
+        cy.get('#channelHeaderMentionButton', {timeout: TIMEOUTS.HALF_MIN}).should('be.visible').click();
+        cy.get('#search-items-container').should('be.visible').within(() => {
+            // * Ensure that the message typed by sysadmin is visible
+            cy.findByText(`@${otherUser.username}`).should('be.visible');
+            cy.findByText('test message!').should('be.visible');
+        });
+    });
+});
+
