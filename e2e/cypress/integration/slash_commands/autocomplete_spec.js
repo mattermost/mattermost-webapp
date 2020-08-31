52dd4b32f624df359e0cafb5849dbf1089c52fce
Jason Frerich <jason.frerich@mattermost.com>
2020-08-31 10:18:15 -0500

    wip

--- e2e/cypress/integration/slash_commands/autocomplete_spec.js
+++ e2e/cypress/integration/slash_commands/autocomplete_spec.js
@@ -17,7 +17,7 @@
  */
 
 describe('Integrations', () => {
-    const pluginIdDemo = 'com.mattermost.demo-plugin';
+    // const pluginIdDemo = 'com.mattermost.demo-plugin';
 
     before(() => {
         // # Initialize setup and visit town-square
@@ -25,25 +25,56 @@ describe('Integrations', () => {
             cy.visit(`/${team.name}/channels/town-square`);
 
             // # If Demo plugin is already enabled, uninstall it
-            cy.apiRemovePluginById(pluginIdDemo);
+            // cy.apiRemovePluginById(pluginIdDemo);
         });
 
-        const demoURL = 'https://github.com/mattermost/mattermost-plugin-demo/releases/download/v0.8.0/com.mattermost.demo-plugin-0.8.0.tar.gz';
+        // const demoURL = 'https://github.com/mattermost/mattermost-plugin-demo/releases/download/v0.8.0/com.mattermost.demo-plugin-0.8.0.tar.gz';
+        // const jiraURL = 'https://github.com/mattermost/mattermost-plugin-jira/releases/download/v3.0.0-beta2.1/jira-3.0.0-beta2.1.tar.gz';
 
         // # Install plugins from URL
-        cy.apiInstallPluginFromUrl(demoURL);
+        // cy.apiInstallPluginFromUrl(demoURL);
+        // cy.apiInstallPluginFromUrl(jiraURL, true);
     });
 
     after(() => {
-        cy.apiRemovePluginById(pluginIdDemo);
+        // cy.apiRemovePluginById(pluginIdDemo);
     });
 
-    it('T2834 Slash command help stays visible for system slash command', () => {
+    // it('T2834 Slash command help stays visible for system slash command', () => {
+    //     // # Post a slash command without trailing space
+    //     cy.get('#post_textbox').type('/rename');
+    //
+    //     // * Verify suggestion list is visible
+    //     cy.get('#suggestionList').should('be.visible');
+    //
+    //     // * Verify command text is visible before space is added
+    //     cy.get('.slash-command__desc').contains('Rename the channel').should('be.visible');
+    //
+    //     // # Add trailing space to '/rename' command
+    //     cy.get('#post_textbox').type(' ');
+    //
+    //     // * Verify command text is no longer visible after space is added
+    //     cy.findByText('Rename the channel').should('not.be.visible');
+    //
+    //     // * Verify execute current command text shows
+    //     cy.get('#suggestionList').findByText('Execute Current Command').should('be.visible');
+    //
+    //     // * After typing the space character the relevant tip is still displayed
+    //     cy.get('.slash-command__desc').contains('[text]').should('be.visible');
+    // });
+
+    it('T2829 Test an example of plugin that uses sub commands', () => {
         // # Post a slash command without trailing space
-        cy.get('#post_textbox').type('/rename');
+        cy.get('#post_textbox').type('/jira instance ');
 
         // * Verify suggestion list is visible
-        cy.get('#suggestionList').should('be.visible');
+        cy.get('#suggestionList').should('be.visible').children().should('have.length', 7);
+
+        // # Add trailing space to '/rename' command
+        cy.get('#post_textbox').type('c');
+
+        // * Verify suggestion list is visible
+        cy.get('#suggestionList').should('be.visible').children().should('have.length', 1);
 
         // * Verify command text is visible before space is added
         cy.get('.slash-command__desc').contains('Rename the channel').should('be.visible');
