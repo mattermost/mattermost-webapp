6500cc3332971e0cab7768cbe4ba7bec2d1e5cff
Soo Hwan Kim <kimsoohwan10@gmail.com>
2020-09-02 22:29:13 +0900

    Refactored team_settings/helpers.js and MM-T389, MM-T386

--- e2e/cypress/integration/team_settings/closed_team_invite_with_non_mattermost_domain_spec.js
+++ e2e/cypress/integration/team_settings/closed_team_invite_with_non_mattermost_domain_spec.js
@@ -10,19 +10,16 @@
 
 // Group: @team_settings
 
-import * as TIMEOUTS from '../../fixtures/timeouts';
-import {getEmailUrl, getEmailMessageSeparator, reUrl, getRandomId} from '../../utils';
+import {generateRandomUser} from '../../support/api/user';
+
+import {allowOnlyUserFromSpecificDomain, inviteUserByEmail, verifyEmailInviteAndVisitLink, signupAndVerifyTutorial} from './helpers';
 
 describe('Team Settings', () => {
     let testTeam;
-    const randomId = getRandomId();
-    const username = `user${randomId}`;
-    const email = `user${randomId}@gmail.com`;
-    const password = 'passwd';
+    const {username, password} = generateRandomUser();
+    const email = `${username}@gmail.com`;
     const emailDomain = 'gmail.com';
 
-    const baseUrl = Cypress.config('baseUrl');
-    const mailUrl = getEmailUrl(baseUrl);
     let isLicensed;
 
     before(() => {
@@ -42,100 +39,19 @@ describe('Team Settings', () => {
     });
 
     it('MM-T389 Invite new user to closed team with \'Allow only users with a specific email domain to join this team\' set to an email that is NOT \'sample.mattermost.com\'', () => {
-        // # Open 'Team Settings' modal
-        cy.get('.sidebar-header-dropdown__icon').click();
-        cy.findByText('Team Settings').should('be.visible').click();
-
-        // * Check that the 'Team Settings' modal was opened
-        cy.get('#teamSettingsModal').should('exist').within(() => {
-            // * Ensure that 'Allow any user with an account on this server' is set to 'No'
-            cy.get('#open_inviteDesc').should('have.text', 'No');
-
-            // # Click on the 'Allow only users with a specific email domain to join this team' edit button
-            cy.get('#allowed_domainsEdit').should('be.visible').click();
-
-            // # Set 'gmail.com' as the only allowed email domain and save
-            cy.wait(TIMEOUTS.HALF_SEC);
-            cy.focused().type(emailDomain);
-            cy.findByText('Save').should('be.visible').click();
-
-            // # Close the modal
-            cy.get('#teamSettingsModalLabel').find('button').should('be.visible').click();
-        });
-
-        // # Open the 'Invite People' full screen modal
-        cy.get('.sidebar-header-dropdown__icon').click();
-        cy.get('#invitePeople').find('button').eq(0).click();
-
-        // # Wait half a second to ensure that the modal has been fully loaded
-        cy.wait(TIMEOUTS.HALF_SEC);
-
-        if (isLicensed) {
-            // # Click "Invite members"
-            cy.findByTestId('inviteMembersLink').should('be.visible').click();
-        }
-
-        cy.findByRole('textbox', {name: 'Add or Invite People'}).type(email, {force: true}).wait(TIMEOUTS.HALF_SEC).type('{enter}', {force: true});
-        cy.get('#inviteMembersButton').click();
-
-        // # Wait for a while to ensure that email notification is sent and logout from sysadmin account
-        cy.wait(TIMEOUTS.FIVE_SEC);
-        cy.apiLogout();
+        // # Allow only users from 'gmail.com' domain
+        allowOnlyUserFromSpecificDomain(emailDomain);
 
         // # Invite a new user (with the email declared in the parent scope)
-        cy.task('getRecentEmail', {username, mailUrl}).then((response) => {
-            const messageSeparator = getEmailMessageSeparator(baseUrl);
-            verifyEmailInvite(response, testTeam.name, testTeam.display_name, email, messageSeparator);
+        inviteUserByEmail(email, isLicensed);
 
-            const bodyText = response.data.body.text.split('\n');
-            const permalink = bodyText[6].match(reUrl)[0];
+        // # Logout from sysadmin account
+        cy.apiLogout();
 
-            // # Visit permalink (e.g. click on email link)
-            cy.visit(permalink);
-        });
+        // # Verify that the invite email is correct, and visit the link provided in the email
+        verifyEmailInviteAndVisitLink(username, email, testTeam.name, testTeam.display_name);
 
-        // # Type username and password
-        cy.get('#name').should('be.visible').type(username);
-        cy.get('#password').should('be.visible').type(password);
-
-        // # Attempt to create an account by clicking on the 'Create Account' button
-        cy.get('#createAccountButton').click();
-
-        // * Check that the display name of the team the user was invited to is being correctly displayed
-        cy.get('#headerTeamName').should('contain.text', testTeam.display_name);
-
-        // * Check that 'Town Square' is currently being selected
-        cy.get('.active').within(() => {
-            cy.get('#sidebarItem_town-square').should('exist');
-        });
-
-        // * Check that the 'Welcome to: Mattermost' message is visible
-        cy.get('#tutorialIntroOne').findByText('Welcome to:').should('be.visible');
-        cy.get('#tutorialIntroOne').findByText('Mattermost').should('be.visible');
+        // # Signup and verify the initial tutorial
+        signupAndVerifyTutorial(username, password, testTeam.display_name);
     });
-
-    function verifyEmailInvite(response, teamName, teamDisplayName, invitedUserEmail, messageSeparator) {
-        const isoDate = new Date().toISOString().substring(0, 10);
-        const {data, status} = response;
-
-        // * Should return success status
-        expect(status).to.equal(200);
-
-        // * Verify that email is addressed to the correct user
-        expect(data.to.length).to.equal(1);
-        expect(data.to[0]).to.contain(invitedUserEmail);
-
-        // * Verify that date is current
-        expect(data.date).to.contain(isoDate);
-
-        // * Verify that the email subject is correct
-        expect(data.subject).to.contain(`[Mattermost] sysadmin invited you to join ${teamDisplayName} Team`);
-
-        // * Verify that the email body is correct
-        const bodyText = data.body.text.split(messageSeparator);
-        expect(bodyText.length).to.equal(17);
-        expect(bodyText[1]).to.equal('You\'ve been invited');
-        expect(bodyText[4]).to.equal(`*sysadmin* , has invited you to join *${teamDisplayName}*.`);
-        expect(bodyText[10]).to.contain(`${baseUrl}/${teamName}`);
-    }
 });
