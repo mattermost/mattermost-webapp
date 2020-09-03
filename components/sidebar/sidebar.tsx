c12000598b3f6a59c955be8cdfaac0cfd3591b96
Nick Misasi <nick.misasi@mattermost.com>
2020-09-03 13:24:57 -0400

    Don't show old tutorial or what's new modal for cloud instances

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -37,6 +37,7 @@ type Props = {
             data: boolean;
         }>;
     };
+    isCloud: boolean;
 };
 
 type State = {
@@ -58,11 +59,11 @@ export default class Sidebar extends React.PureComponent<Props, State> {
             this.props.actions.fetchMyCategories(this.props.teamId);
         }
 
-        if (!this.props.hasSeenModal) {
-            this.props.actions.openModal({
-                modalId: ModalIdentifiers.SIDEBAR_WHATS_NEW_MODAL,
-                dialogType: SidebarWhatsNewModal,
-            });
+        if (!this.props.hasSeenModal && !this.props.isCloud) {
+          this.props.actions.openModal({
+            modalId: ModalIdentifiers.SIDEBAR_WHATS_NEW_MODAL,
+            dialogType: SidebarWhatsNewModal,
+          });
         }
     }
 
