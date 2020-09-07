b9f52babc29b2c1bcace40d2917562570939aac7
Agniva De Sarker <agnivade@yahoo.co.in>
2020-09-07 20:03:23 +0530

    Remove square brackets

--- e2e/cypress/integration/add_user_to_channel_from_profile_popover/profile_popover_spec.js
+++ e2e/cypress/integration/add_user_to_channel_from_profile_popover/profile_popover_spec.js
@@ -28,7 +28,7 @@ describe('Profile popover', () => {
     });
 
     it('MM-T2 Add user — Error if already in channel', () => {
-        // [#] Do the system console setup
+        // # Do the system console setup
         cy.apiAdminLogin();
         cy.visit('/admin_console/user_management/permissions/system_scheme');
         cy.findByTestId('all_users-public_channel-checkbox').scrollIntoView().should('be.visible').click();
@@ -36,7 +36,7 @@ describe('Profile popover', () => {
         // Expand the section
         cy.get('#all_users-public_channel > .fa').should('be.visible').click();
 
-        // [*] Verify that all the sub-checkboxes are enabled
+        // * Verify that all the sub-checkboxes are enabled
         cy.findByTestId('all_users-public_channel-create_public_channel-checkbox').should('have.class', 'checked');
         cy.findByTestId('all_users-public_channel-manage_public_channel_properties-checkbox').should('have.class', 'checked');
         cy.findByTestId('all_users-public_channel-manage_public_channel_members_and_read_groups-checkbox').should('have.class', 'checked');
@@ -47,7 +47,7 @@ describe('Profile popover', () => {
         // Expand the section
         cy.get('#all_users-private_channel > .fa').should('be.visible').click();
 
-        // [*] Verify that all the sub-checkboxes are enabled
+        // * Verify that all the sub-checkboxes are enabled
         cy.findByTestId('all_users-private_channel-create_private_channel-checkbox').should('have.class', 'checked');
         cy.findByTestId('all_users-private_channel-manage_private_channel_properties-checkbox').should('have.class', 'checked');
         cy.findByTestId('all_users-private_channel-manage_private_channel_members_and_read_groups-checkbox').should('have.class', 'checked');
@@ -57,34 +57,34 @@ describe('Profile popover', () => {
 
         cy.apiLogout();
 
-        // [#] Login as test user and go to town square
+        // # Login as test user and go to town square
         cy.apiLogin(testUser);
         cy.visit(`/${testTeam.name}/channels/town-square`);
 
-        // [#] Send multiple messages so that the profile popover appears completely.
+        // # Send multiple messages so that the profile popover appears completely.
         cy.postMessage('Hi there\nsending\na\nmessage');
         cy.apiLogout();
 
-        // [#] Login as the second user now
+        // # Login as the second user now
         cy.apiLogin(otherUser);
         cy.visit(`/${testTeam.name}/channels/town-square`);
 
-        // [#] Open profile popover
+        // # Open profile popover
         cy.get('#postListContent').within(() => {
             cy.findAllByText(testUser.username).first().should('have.text', testUser.username).click();
         });
 
-        // [#] Add to a Channel
+        // # Add to a Channel
         cy.findByText('Add to a Channel').should('be.visible').click();
 
         cy.get('div[aria-labelledby="addChannelModalLabel"]').within(() => {
             // Type "Town" and press enter.
             cy.get('input').should('be.visible').type('Town').wait(TIMEOUTS.HALF_SEC).type('{enter}');
 
-            // [*] Verify error message
+            // * Verify error message
             cy.get('#add-user-to-channel-modal__user-is-member').should('have.text', `${testUser.first_name} ${testUser.last_name} is already a member of that channel`);
 
-            // [*] And verify that button is disabled
+            // * And verify that button is disabled
             cy.get('#add-user-to-channel-modal__add-button').should('be.disabled');
         });
     });
