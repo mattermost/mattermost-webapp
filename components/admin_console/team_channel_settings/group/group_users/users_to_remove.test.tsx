fba1f4c6f218e8c7a34149214b63836851553927
Farhan Munshi <3207297+fmunshi@users.noreply.github.com>
2020-09-07 16:09:35 -0400

    [MM-26970] Add filters to users to remove modal (#6081)
    * Convert group users to be removed to Datagrid
    
    * Remove old group_users component
    
    * Add filters to users to be removed list
    
    * Fix tests and style
    
    * Keep modalFilters separate
    
    * Update i18n
    
    * Add tests
    
    * Remove unneeded
    
    * Tests for group_users_groups
    
    * Add tests for group users role
    
    * Fix tests
    
    * Remove separate translations for team member and channel member and just consolidate to member
    
    * Fix incorrect indent
    
    * Update e2e tests
    
    * Address review comments
    
    Convert group users groups into a functional component
    Convert group users role into a functional component
    Use memoizeResult instead of create selector
    
    * Dont use redux-thunk
    
    * Fix types
    
    * No more need for this timeout
    
    * Create filter options in function
    
    * Rename to users_to_remove
    
    * Update components/admin_console/team_channel_settings/channel/details/channel_details.tsx
    
    Co-authored-by: Hossein Ahmadian-Yazdi <hahmadia@users.noreply.github.com>
    
    * Fix MM-28272 & MM-28273
    
    Co-authored-by: Hossein Ahmadian-Yazdi <hahmadia@users.noreply.github.com>
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

---
+++ components/admin_console/team_channel_settings/group/group_users/users_to_remove.test.tsx
@@ -0,0 +1,99 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import {UserProfile} from 'mattermost-redux/types/users';
+import {TeamMembership} from 'mattermost-redux/types/teams';
+
+import {TestHelper} from 'utils/test_helper';
+
+import UsersToRemove from './users_to_remove';
+
+describe('components/admin_console/team_channel_settings/group/UsersToRemove', () => {
+    function createUser(id: string, username: string, bot: boolean): UserProfile {
+        return TestHelper.getUserMock({
+            id,
+            username,
+            is_bot: bot,
+        });
+    }
+
+    function createMembership(userId: string, admin: boolean): TeamMembership {
+        return {
+            mention_count: 0,
+            msg_count: 0,
+            team_id: 'team',
+            user_id: userId,
+            roles: admin ? 'team_user team_admin' : 'team_user',
+            delete_at: 0,
+            scheme_user: true,
+            scheme_admin: admin,
+        };
+    }
+
+    const user1 = createUser('userid1', 'user-1', false);
+    const membership1 = createMembership('userId1', false);
+    const user2 = createUser('userid2', 'user-2', false);
+    const membership2 = createMembership('userId2', true);
+    const scope: 'team' | 'channel' = 'team';
+    const baseProps = {
+        members: [user1, user2],
+        memberships: {[user1.id]: membership1, [user2.id]: membership2},
+        total: 2,
+        searchTerm: '',
+        scope,
+        scopeId: 'team',
+        enableGuestAccounts: true,
+        filters: {},
+
+        actions: {
+            loadTeamMembersForProfilesList: jest.fn(),
+            loadChannelMembersForProfilesList: jest.fn(),
+            setModalSearchTerm: jest.fn(),
+            setModalFilters: jest.fn(),
+        },
+    };
+
+    test('should match snapshot with 2 users', () => {
+        const wrapper = shallow(
+            <UsersToRemove
+                {...baseProps}
+            />,
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should match snapshot with guests disabled', () => {
+        const wrapper = shallow(
+            <UsersToRemove
+                {...baseProps}
+                enableGuestAccounts={false}
+            />,
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should match snapshot searching with filters', () => {
+        const wrapper = shallow(
+            <UsersToRemove
+                {...baseProps}
+                searchTerm={'foo'}
+                filters={{roles: ['system_user']}}
+            />,
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should match snapshot loading', () => {
+        const wrapper = shallow(
+            <UsersToRemove
+                {...baseProps}
+            />,
+        );
+
+        wrapper.setState({loading: true});
+        expect(wrapper).toMatchSnapshot();
+    });
+});
