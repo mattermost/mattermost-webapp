625ff46463e18665f5f4e0650bb381f58961a9bc
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2020-09-07 14:52:00 -0400

    Use system console to toggle config values

--- e2e/cypress/integration/plugins/marketplace/marketplace_spec.js
+++ e2e/cypress/integration/plugins/marketplace/marketplace_spec.js
@@ -13,12 +13,14 @@ import * as TIMEOUTS from '../../../fixtures/timeouts';
 
 describe('Plugin Marketplace', () => {
     let townsquareLink;
+    let pluginManagementPage;
     let regularUser;
 
     before(() => {
         cy.apiInitSetup().then(({team, user}) => {
             regularUser = user;
             townsquareLink = `/${team.name}/channels/town-square`;
+            pluginManagementPage = '/admin_console/plugins/plugin_management';
         });
     });
 
@@ -41,7 +43,7 @@ describe('Plugin Marketplace', () => {
             cy.visit(townsquareLink);
 
             // * Verify Plugin Marketplace does not exist
-            verifyPluginMarketplaceDoesNotExist();
+            verifyPluginMarketplaceVisibility(false);
         });
 
         it('MM-T1957 Marketplace is not available when "Enable Marketplace" is set to false', () => {
@@ -62,7 +64,7 @@ describe('Plugin Marketplace', () => {
             cy.visit(townsquareLink);
 
             // * Verify Plugin Marketplace does not exist
-            verifyPluginMarketplaceDoesNotExist();
+            verifyPluginMarketplaceVisibility(false);
         });
 
         it('MM-T1959 Marketplace is not available when "Enable Plugins" is false', () => {
@@ -83,54 +85,57 @@ describe('Plugin Marketplace', () => {
             cy.visit(townsquareLink);
 
             // * Verify Plugin Marketplace does not exist
-            verifyPluginMarketplaceDoesNotExist();
+            verifyPluginMarketplaceVisibility(false);
         });
     });
 
     describe('should render in main menu', () => {
-        afterEach(() => {
-            cy.get('#lhsHeader').should('be.visible').within(() => {
-                // # Click hamburger main menu
-                cy.get('#sidebarHeaderDropdownButton').click();
-
-                // * Dropdown menu should be visible
-                cy.get('.dropdown-menu').should('be.visible').within(() => {
-                    // * Plugin Marketplace button should be visible
-                    cy.findByText('Plugin Marketplace').should('be.visible');
-                });
-            });
-        });
-
         it('MM-T1960 Marketplace is available when "Enable Plugins" is true', () => {
             // # Login as sysadmin
             cy.apiAdminLogin();
 
-            // # Enable Plugins
+            // # Disable Plugins
             // # Enable Plugin Marketplace
             cy.apiUpdateConfig({
                 PluginSettings: {
-                    Enable: true,
+                    Enable: false,
                     EnableMarketplace: true,
                     MarketplaceUrl: 'https://api.integrations.mattermost.com',
                 },
             });
+            cy.visit(pluginManagementPage);
+
+            cy.wait(TIMEOUTS.HALF_SEC).get('input[data-testid="enablefalse"]').should('be.checked')
+            cy.get('input[data-testid="enabletrue"]').check();
+            cy.get('#saveSetting').click();
+
+            // Verify that the Plugin Marketplace is available
             cy.visit(townsquareLink);
+            verifyPluginMarketplaceVisibility(true);
         });
 
-        it('MM-T1960 Marketplace is available when "Enable Marketplace" is set to true', () => {
+        it('MM-T1958 Marketplace is available when "Enable Marketplace" is set to true', () => {
             // # Login as sysadmin
             cy.apiAdminLogin();
 
             // # Enable Plugins
-            // # Enable Plugin Marketplace
+            // # Disable Plugin Marketplace
             cy.apiUpdateConfig({
                 PluginSettings: {
                     Enable: true,
-                    EnableMarketplace: true,
+                    EnableMarketplace: false,
                     MarketplaceUrl: 'https://api.integrations.mattermost.com',
                 },
             });
+            cy.visit(pluginManagementPage);
+
+            cy.wait(TIMEOUTS.HALF_SEC).get('input[data-testid="enableMarketplacefalse"]').should('be.checked')
+            cy.get('input[data-testid="enableMarketplacetrue"]').check();
+            cy.get('#saveSetting').click();
+
+            // Verify that the Plugin Marketplace is available
             cy.visit(townsquareLink);
+            verifyPluginMarketplaceVisibility(true);
         });
     });
 
@@ -490,15 +495,20 @@ function uninstallAllPlugins() {
     });
 }
 
-function verifyPluginMarketplaceDoesNotExist() {
+function verifyPluginMarketplaceVisibility(shouldBeVisible) {
     cy.wait(TIMEOUTS.HALF_SEC).get('#lhsHeader').should('be.visible').within(() => {
         // # Click hamburger main menu
         cy.get('#sidebarHeaderDropdownButton').click();
 
         // * Verify dropdown menu should be visible
         cy.get('.dropdown-menu').should('be.visible').within(() => {
-            // * Verify Plugin Marketplace button should not exist
-            cy.findByText('Plugin Marketplace').should('not.exist');
+            if (shouldBeVisible) {
+                // * Verify Plugin Marketplace button should exist
+                cy.findByText('Plugin Marketplace').should('exist');
+            } else {
+                // * Verify Plugin Marketplace button should not exist
+                cy.findByText('Plugin Marketplace').should('not.exist');
+            }
         });
     });
 }
