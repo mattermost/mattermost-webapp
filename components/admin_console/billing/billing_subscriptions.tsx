1d08c0602f36b72ea6d8b72b6738be0b029afd58
Devin Binnie <52460000+devinbinnie@users.noreply.github.com>
2020-09-08 09:14:12 -0400

    [MM-28202] Common Component - Alert Banner (#6352)
    * [MM-28202] Common Component - Alert Banner
    
    * Add demo closing
    
    * PR feedback
    
    * Quick css update
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- components/admin_console/billing/billing_subscriptions.tsx
+++ components/admin_console/billing/billing_subscriptions.tsx
@@ -1,17 +1,22 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, {useState} from 'react';
 
 import FormattedAdminHeader from 'components/widgets/admin_console/formatted_admin_header';
 
 import './billing_subscriptions.scss';
+import AlertBanner from 'components/alert_banner';
 
 type Props = {
 
 };
 
 const BillingSubscriptions: React.FC<Props> = () => {
+    const [showDanger, setShowDanger] = useState(true);
+    const [showWarning, setShowWarning] = useState(true);
+    const [showInfo, setShowInfo] = useState(true);
+
     return (
         <div className='wrapper--fixed BillingSubscriptions'>
             <FormattedAdminHeader
@@ -20,10 +25,34 @@ const BillingSubscriptions: React.FC<Props> = () => {
             />
             <div className='admin-console__wrapper'>
                 <div className='admin-console__content'>
-                    <div style={{border: '1px solid #000', width: '100%', height: '81px', marginBottom: '20px'}}>
-                        {'Alert Banner (credit card expired/recent payment failed)'}
-                    </div>
-                    <div className='BillingSubscriptions__topWrapper'>
+                    {showDanger &&
+                        <AlertBanner
+                            mode='danger'
+                            title='Test Danger Title'
+                            message='This is a test danger message'
+                            onDismiss={() => setShowDanger(false)}
+                        />
+                    }
+                    {showWarning &&
+                        <AlertBanner
+                            mode='warning'
+                            title='Test Warning Title'
+                            message='This is a test warning message'
+                            onDismiss={() => setShowWarning(false)}
+                        />
+                    }
+                    {showInfo &&
+                        <AlertBanner
+                            mode='info'
+                            title='Test Info Title'
+                            message='This is a test info message'
+                            onDismiss={() => setShowInfo(false)}
+                        />
+                    }
+                    <div
+                        className='BillingSubscriptions__topWrapper'
+                        style={{marginTop: '20px'}}
+                    >
                         <div style={{border: '1px solid #000', width: '568px', height: '438px'}}>
                             {'Plan Details Card'}
                         </div>
