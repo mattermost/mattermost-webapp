421df269d340c941aeb7fee6ef4af5feb8c71e41
Miguel de la Cruz <miguel@mcrx.me>
2020-09-10 09:03:31 +0200

    Fix test ID, location and wrapped response

--- e2e/cypress/integration/system_console/site_configuration/customization_spec.js
+++ e2e/cypress/integration/system_console/site_configuration/customization_spec.js
@@ -12,7 +12,11 @@
 import * as TIMEOUTS from '../../../fixtures/timeouts';
 
 describe('Customization', () => {
-    before(() => {
+    beforeEach(() => {
+        // # as many of the tests logout the user, ensure it's logged
+        // in as an admin before each test
+        cy.apiAdminLogin();
+
         // # Visit customization system console page
         cy.visit('/admin_console/site_config/customization');
         cy.get('.admin-console__header').should('be.visible').and('have.text', 'Customization');
@@ -44,6 +48,47 @@ describe('Customization', () => {
         // * Ensure Site Name and Description are shown the updated values in the login screen
         cy.get('#site_name').should('have.text', siteName);
         cy.get('#site_description').should('have.text', siteDescription);
+    });
+
+    it('MM-T1028 - Custom brand image and text - true, and uploaded / updated', () => {
+        // # Make sure necessary field is false
+        cy.apiUpdateConfig({TeamSettings: {EnableCustomBrand: false}});
+
+        // # Ensure that the brand image is deleted
+        cy.apiDeleteBrandImage();
+        cy.reload();
+
+        // # Enable custom branding
+        cy.findByTestId('TeamSettings.EnableCustomBrandtrue').check();
+
+        // # Upload the image
+        cy.findByTestId('CustomBrandImage').should('be.visible').within(() => {
+            cy.get('input').attachFile('mattermost-icon.png');
+        });
+
+        // * Verify that custom brand image setting is visible and matches text content
+        cy.findByTestId('TeamSettings.CustomBrandTextlabel').scrollIntoView().should('be.visible').and('have.text', 'Custom Brand Text:');
+
+        // # Update custom brand text
+        const customBrandText = 'This is a custom brand text';
+        cy.findByTestId('TeamSettings.CustomBrandTextinput').clear().type(customBrandText);
+
+        // # Save setting
+        saveSetting();
+
+        // # Logout from the current user
+        cy.apiLogout();
+
+        // * Ensure that the user was redirected to the login page after the logout
+        cy.url().should('include', '/login');
+
+        cy.get('.signup__markdown').within(() => {
+            // * Ensure that the signup is loaded and the img is visible
+            cy.get('img').should('be.visible');
+
+            // * Ensure that the custom brand text has been updated
+            cy.get('p').should('have.text', customBrandText);
+        });
     });
 });
 
