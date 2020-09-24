49c4a7264519e233c3e712a4e3e98daf2da1376a
Nick Misasi <nick.misasi@mattermost.com>
2020-09-24 09:51:35 -0400

    Working, PR commit

--- components/announcement_bar/cloud_announcement_bar/cloud_announcement_bar.tsx
+++ components/announcement_bar/cloud_announcement_bar/cloud_announcement_bar.tsx
@@ -6,33 +6,38 @@ import React from 'react';
 import {PreferenceType} from 'mattermost-redux/types/preferences';
 import './cloud_announcement_bar.scss';
 import {UserProfile} from 'mattermost-redux/types/users';
+import {Dictionary} from 'mattermost-redux/types/utilities';
+import {AnalyticsRow} from 'mattermost-redux/types/admin';
+import {FormattedMessage} from 'react-intl';
 
 import classNames from 'classnames';
 import {isEmpty} from 'lodash';
 
-import {Preferences} from 'utils/constants';
+import {Preferences, CloudBanners} from 'utils/constants';
 
 type Props = {
-    userLimit: string;
+    userLimit: number;
     userIsAdmin: boolean;
     currentUser: UserProfile;
     preferences: PreferenceType[];
     isCloud: boolean;
-    analytics: {
-        TOTAL_USERS: number,
-    };
+    analytics?: Dictionary<number | AnalyticsRow[]>;
     actions: {
-        closeModal: () => void;
         savePreferences: (userId: string, preferences: PreferenceType[]) => void;
         getStandardAnalytics: () => void;
     };
 };
 
 export default class CloudAnnouncementBar extends React.PureComponent<Props> {
-    componentDidMount() {
+    async componentDidMount() {
         if (isEmpty(this.props.analytics)) {
-            this.props.actions.getStandardAnalytics();
+            await this.props.actions.getStandardAnalytics();
         }
+        document.body.classList.add('cloud-banner--fixed');
+    }
+
+    componentWillUnmount() {
+        document.body.classList.remove('cloud-banner--fixed');
     }
 
     handleButtonClick = () => {
@@ -43,7 +48,7 @@ export default class CloudAnnouncementBar extends React.PureComponent<Props> {
         await this.props.actions.savePreferences(this.props.currentUser.id, [{
             category: Preferences.CLOUD_UPGRADE_BANNER,
             user_id: this.props.currentUser.id,
-            name: 'hide',
+            name: CloudBanners.HIDE,
             value: 'true',
         }]);
     }
@@ -58,29 +63,41 @@ export default class CloudAnnouncementBar extends React.PureComponent<Props> {
             return false;
         }
 
-        if (parseInt(userLimit, 10) > analytics.TOTAL_USERS) {
+        if (userLimit > analytics!.TOTAL_USERS) {
             return false;
         }
 
         return true;
     }
 
+    removeFixedBannerClass = () => {
+        document.body.classList.remove('cloud-banner--fixed');
+    }
+
     render() {
-        if (!this.shouldShowBanner() || isEmpty(this.props.analytics)) {
-            return null;
-        }
         const {userLimit, analytics, preferences} = this.props;
 
+        if (isEmpty(this.props.analytics)) {
+            // If the analytics aren't yet loaded, return null to avoid a flash of the banner
+            return null;
+        }
+
+        if (!this.shouldShowBanner()) {
+            this.removeFixedBannerClass();
+            return null;
+        }
+
         // If AT user limit, and banner hidden, don't render anything
-        if (parseInt(userLimit, 10) === analytics.TOTAL_USERS &&
-            preferences.some((pref) => pref.name === 'hide' && pref.value === 'true')) {
+        if (userLimit === analytics!.TOTAL_USERS &&
+            preferences.some((pref) => pref.name === CloudBanners.HIDE && pref.value === 'true')) {
+            this.removeFixedBannerClass();
             return null;
         }
 
         let dismissable = true;
 
         // If the user limit is less than the current number of users, the banner is not dismissable
-        if (parseInt(userLimit, 10) < analytics.TOTAL_USERS) {
+        if (userLimit < analytics!.TOTAL_USERS) {
             dismissable = false;
         }
 
@@ -112,13 +129,27 @@ export default class CloudAnnouncementBar extends React.PureComponent<Props> {
                         <div className={'content__icon'}>{''}</div>
                     )}
                     <div className={'content__description'}>
-                        {"You've reached the user limit of the free tier"}
+                        {dismissable ? (
+                            <FormattedMessage
+                                id={'upgrade.cloud_banner_reached'}
+                                defaultMessage={'You have reached the user limit of the free tier'}
+                            />
+                        ) : (
+                            <FormattedMessage
+                                id={'upgrade.cloud_banner_over'}
+                                defaultMessage={'You are currently over the user limit for the free tier'}
+                            />
+                        )
+                        }
                     </div>
                     <button
                         onClick={this.handleButtonClick}
                         className='upgrade-button'
                     >
-                        {'Upgrade Mattermost Cloud'}
+                        <FormattedMessage
+                            id={'admin.billing.subscription.upgradeMattermostCloud.upgradeButton'}
+                            defaultMessage={'Upgrade Mattermost Cloud'}
+                        />
                     </button>
                     {closeButton}
                 </div>
--- components/announcement_bar/cloud_announcement_bar/index.ts
+++ components/announcement_bar/cloud_announcement_bar/index.ts
@@ -4,32 +4,29 @@
 import {connect} from 'react-redux';
 import {bindActionCreators, Dispatch} from 'redux';
 
-import { savePreferences } from "mattermost-redux/actions/preferences";
+import {savePreferences} from 'mattermost-redux/actions/preferences';
 import {getConfig, getLicense} from 'mattermost-redux/selectors/entities/general';
 import {GenericAction} from 'mattermost-redux/types/actions';
-import { getMyTeamMember } from 'mattermost-redux/selectors/entities/teams';
-import { getStandardAnalytics } from "mattermost-redux/actions/admin";
-import { makeGetCategory } from "mattermost-redux/selectors/entities/preferences";
+import {getStandardAnalytics} from 'mattermost-redux/actions/admin';
+import {makeGetCategory} from 'mattermost-redux/selectors/entities/preferences';
+
+import {getCurrentUser, isCurrentUserSystemAdmin} from 'mattermost-redux/selectors/entities/users';
 
 import {GlobalState} from 'types/store';
 
+import {Preferences} from 'utils/constants';
+
 import CloudAnnouncementBar from './cloud_announcement_bar';
-import { getCurrentUser, isCurrentUserSystemAdmin } from 'mattermost-redux/selectors/entities/users';
-import { Preferences } from "utils/constants";
 
-type OwnProps = {
-    currentTeamId: string;
-};
-
-function mapStateToProps(state: GlobalState, ownProps: OwnProps) {
+function mapStateToProps(state: GlobalState) {
     const getCategory = makeGetCategory();
     return {
-      userLimit: getConfig(state).ExperimentalCloudUserLimit,
-      analytics: state.entities.admin.analytics,
-      userIsAdmin: isCurrentUserSystemAdmin(state),
-      currentUser: getCurrentUser(state),
-      isCloud: getLicense(state).Cloud === "true",
-      preferences: getCategory(state, Preferences.CLOUD_UPGRADE_BANNER),
+        userLimit: parseInt(getConfig(state).ExperimentalCloudUserLimit!, 10),
+        analytics: state.entities.admin.analytics,
+        userIsAdmin: isCurrentUserSystemAdmin(state),
+        currentUser: getCurrentUser(state),
+        isCloud: getLicense(state).Cloud === 'true',
+        preferences: getCategory(state, Preferences.CLOUD_UPGRADE_BANNER),
     };
 }
 
