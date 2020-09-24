0e9ac889a818479ff8760091468825096b96adb4
Sudheer <sudheer.105@gmail.com>
2020-09-24 23:47:56 +0530

    MM-27530 Fix for Multiselect items not scrolling into view on arrow keys (#6286)
    * MM-27530 Fix for Multiselect items not scrolling into view on arrow keys
    
       * The way it used to work is based on list item which is lost in
         recent refactors.
       * Passing in on an item ref if renderOption is overriding defaultOptionRenderer
    
    * Add arrow func
    
    * Update to use arrow functions
    
    * Add ref to GM component
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- components/more_direct_channels/more_direct_channels.tsx
+++ components/more_direct_channels/more_direct_channels.tsx
@@ -104,12 +104,13 @@ export default class MoreDirectChannels extends React.PureComponent<Props, State
     searchTimeoutId: any;
     exitToChannel?: string;
     multiselect: React.RefObject<MultiSelect<OptionType>>;
-
+    selectedItemRef: React.RefObject<HTMLDivElement>;
     constructor(props: Props) {
         super(props);
 
         this.searchTimeoutId = 0;
         this.multiselect = React.createRef();
+        this.selectedItemRef = React.createRef();
 
         const values: (OptionType | UserProfile)[] = [];
 
@@ -327,6 +328,7 @@ export default class MoreDirectChannels extends React.PureComponent<Props, State
         if ((option as GroupChannelValue).type && (option as GroupChannelValue).type === 'G') {
             return (
                 <GroupMessageOption
+                    selectedItemRef={this.selectedItemRef}
                     key={option.id}
                     channel={(option as GroupChannelValue)}
                     isSelected={isSelected}
@@ -372,7 +374,7 @@ export default class MoreDirectChannels extends React.PureComponent<Props, State
         return (
             <div
                 key={option.id}
-                ref={isSelected ? 'selected' : option.id}
+                ref={isSelected ? this.selectedItemRef : option.id}
                 className={'more-modal__row clickable ' + rowSelected}
                 onClick={() => onAdd(option)}
                 onMouseMove={() => onMouseMove(option)}
@@ -509,6 +511,7 @@ export default class MoreDirectChannels extends React.PureComponent<Props, State
                 ref={this.multiselect}
                 options={options.slice(0, 20)}
                 optionRenderer={this.renderOption}
+                selectedItemRef={this.selectedItemRef}
                 values={this.state.values}
                 valueRenderer={this.renderValue}
                 ariaLabelRenderer={this.renderAriaLabel}
