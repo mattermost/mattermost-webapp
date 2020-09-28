75d87027f84551e53c4f3b218a8f9d64168afeac
Mike Leonard <mike@worx4you.com>
2020-09-28 16:45:24 -0400

    minor format change

--- e2e/cypress/integration/messaging/MM-T134-visual-verification-of-tooltips_spec.js
+++ e2e/cypress/integration/messaging/MM-T134-visual-verification-of-tooltips_spec.js
@@ -29,7 +29,7 @@ function verifyChannel(res, verifyExistence = true) {
 
 describe('Messaging', () => {
     let loggedUser;
-    //let longUser;
+    
     let testTeam;
 
     before(() => {
@@ -41,15 +41,12 @@ describe('Messaging', () => {
 
             cy.apiAdminLogin;
 
-            //cy.apiCreateUser({prefix: `thisIsALongUsername${timestamp}`}).then(({user: user1}) => {
-            //    longUser = user1;
             cy.apiAddUserToTeam(testTeam.id, loggedUser.id);
-            // });
             
             cy.visit(`/${testTeam.name}/channels/town-square`);
         });
     });
-    it('MM-T134-visual-verification-of-tooltips', () => {
+        it('MM-T134-visual-verification-of-tooltips', () => {
         // * Hover effect wraps around Date and New Messages lines
         // supposed to validate shadow over effect wraping around date but not sure if this is possible
         cy.get('#postListContent').find('.top').should('be.visible');
@@ -57,47 +54,48 @@ describe('Messaging', () => {
         // * Members tool-tip is present
         cy.findAllByLabelText('members').should('be.visible').trigger('mouseover');
         cy.get('div.tooltip-inner').should('be.visible').and('contain', 'Members');
-        cy.findAllByLabelText('members').should('be','visible').trigger('mouseout');
+        cy.findAllByLabelText('members').should('be', 'visible').trigger('mouseout');
         
         // * Pinned post tool-tip is present
         cy.findAllByLabelText('Pinned posts').should('be.visible').trigger('mouseover');
         cy.get('div.tooltip-inner').should('be.visible').and('contain', 'Pinned posts');
-        cy.findAllByLabelText('Pinned posts').should('be','visible').trigger('mouseout');
+        cy.findAllByLabelText('Pinned posts').should('be', 'visible').trigger('mouseout');
 
         // * Saved posts tool-tip is present
         cy.findAllByLabelText('Saved posts').should('be.visible').trigger('mouseover');
         cy.get('div.tooltip-inner').should('be.visible').and('contain', 'Saved posts');
-        cy.findAllByLabelText('Saved posts').should('be','visible').trigger('mouseout');
+        cy.findAllByLabelText('Saved posts').should('be', 'visible').trigger('mouseout');
 
         // * Add to favourites posts tool-tip is present - un checked
         cy.findAllByLabelText('add to favorites').should('be.visible').trigger('mouseover');
-        cy.get('div.tooltip-inner').should('be.visible').and('contain','Add to Favorites');
-        cy.findAllByLabelText('add to favorites').should('be','visible').trigger('mouseout');
+        cy.get('div.tooltip-inner').should('be.visible').and('contain', 'Add to Favorites');
+        cy.findAllByLabelText('add to favorites').should('be', 'visible').trigger('mouseout');
 
         // * Add to favourites posts tool-tip is present - checked
         cy.findAllByLabelText('add to favorites').click();
         cy.findAllByLabelText('remove from favorites').should('be.visible').trigger('mouseover');
-        cy.get('div.tooltip-inner').should('be.visible').and('contain','Remove from Favorites');
-        cy.findAllByLabelText('remove from favorites').should('be','visible').trigger('mouseout');
+        cy.get('div.tooltip-inner').should('be.visible').and('contain', 'Remove from Favorites');
+        cy.findAllByLabelText('remove from favorites').should('be', 'visible').trigger('mouseout');
 
         // * Unmute a channel tool-tip is present
         cy.findAllByLabelText('dropdown icon').click();
         cy.findAllByText('Mute Channel').click();
         cy.findAllByLabelText('Muted Icon').should('be','visible').trigger('mouseover');
-        cy.get('div.tooltip-inner').should('be.visible').and('contain','Unmute');
-        cy.findAllByLabelText('Muted Icon').should('be','visible').trigger('mouseout');
+        cy.get('div.tooltip-inner').should('be.visible').and('contain', 'Unmute');
+        cy.findAllByLabelText('Muted Icon').should('be', 'visible').trigger('mouseout');
 
         // * Long channel name (shown truncated on the LHS)
-        cy.apiCreateChannel(testTeam.id,'channel-test',`Public channel with a long name-${timestamp}`,).then((res) => {
-            verifyChannel(res);});
+        cy.apiCreateChannel(testTeam.id,'channel-test', `Public channel with a long name-${timestamp}`,).then((res) => {
+            verifyChannel(res);
+        });
 
         // # Set up the Demo plugin
         cy.apiInstallPluginFromUrl('https://github.com/mattermost/mattermost-plugin-demo/releases/download/v0.8.0/com.mattermost.demo-plugin-0.8.0.tar.gz', true);
         cy.visit('/admin_console/plugins/plugin_com.mattermost.demo-plugin');
-        cy.get('[data-testid="PluginSettings.PluginStates.com+mattermost+demo-plugin.Enabletrue"]').check().should('be','checked');
-        cy.get('#saveSetting').click({force:true});
+        cy.get('[data-testid="PluginSettings.PluginStates.com+mattermost+demo-plugin.Enabletrue"]').check().should('be', 'checked');
+        cy.get('#saveSetting').click({force: true});
         cy.visit(`/${testTeam.name}/channels/town-square`);
-        
+
         // * Check that the Demo plugin tool-tip is present
         cy.get('#channel-header').find('.fa-plug').should('be.visible').trigger('mouseover');
         cy.get('#pluginTooltip').should('be.visible').and('have.text', 'Demo Plugin');
