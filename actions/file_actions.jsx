66be69a2462d4eb7eb7c0da4d5ee48ca827a47b9
Claudio Costa <cstcld91@gmail.com>
2020-09-28 08:58:37 +0200

    Enable stream mode for file uploads

--- actions/file_actions.jsx
+++ actions/file_actions.jsx
@@ -17,9 +17,9 @@ export function uploadFile(file, name, channelId, rootId, clientId) {
         return request.
             post(Client4.getFilesRoute()).
             set(Client4.getOptions({method: 'post'}).headers).
-            attach('files', file, name).
             field('channel_id', channelId).
             field('client_ids', clientId).
+            attach('files', file, name).
             accept('application/json');
     };
 }
