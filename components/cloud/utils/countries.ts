d64151f6cc05dcb26f79a3321333e77d448cd1e2
Nick Misasi <nick.misasi@mattermost.com>
2020-09-29 13:40:31 -0400

    Fix country sort definitions

--- components/cloud/utils/countries.ts
+++ components/cloud/utils/countries.ts
@@ -1,6 +1,11 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {getData} from 'country-list';
+import { getData } from 'country-list';
 
-export const COUNTRIES = getData().sort((a, b) => (a.name > b.name ? 1 : -1));
+type Country = {
+    name: String;
+    code: String;
+}
+
+export const COUNTRIES = getData().sort((a: Country, b: Country) => (a.name > b.name ? 1 : -1));
