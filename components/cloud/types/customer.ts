13a27800ed83933686c82f600fec8a4082f6d4af
Nick Misasi <nick.misasi@mattermost.com>
2020-09-30 15:25:54 -0400

    Theme changes

--- components/cloud/types/customer.ts
+++ components/cloud/types/customer.ts
@@ -1,8 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-//import {LogoutAction} from './user';
-
 export type Customer = {
     id: string;
     name: string;
