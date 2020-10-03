4bd8245fbd2e9af218e7c2dec1420d30ce8a1c93
ayanrocks <ayanb1999@gmail.com>
2020-10-03 20:29:49 +0530

    converting emoji_list.tsx

--- components/emoji/emoji_list/index.ts
+++ components/emoji/emoji_list/index.ts
@@ -2,21 +2,23 @@
 // See LICENSE.txt for license information.
 
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import {bindActionCreators, Dispatch} from 'redux';
 
 import {getCustomEmojiIdsSortedByName} from 'mattermost-redux/selectors/entities/emojis';
-
+import {GenericAction} from 'mattermost-redux/types/actions';
 import {getCustomEmojis, searchCustomEmojis} from 'mattermost-redux/actions/emojis';
+
+import {GlobalState} from 'mattermost-redux/types/store';
 
 import EmojiList from './emoji_list.jsx';
 
-function mapStateToProps(state) {
+function mapStateToProps(state: GlobalState) {
     return {
         emojiIds: getCustomEmojiIdsSortedByName(state) || [],
     };
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
     return {
         actions: bindActionCreators({
             getCustomEmojis,
