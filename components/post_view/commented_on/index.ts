4e79e38213d94a0d3ea6d2332f6bf2e7a37adf0c
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2020-10-06 22:21:23 +0700

    Remove actions props

--- components/post_view/commented_on/index.ts
+++ components/post_view/commented_on/index.ts
@@ -2,29 +2,20 @@
 // See LICENSE.txt for license information.
 
 import {connect} from 'react-redux';
-import {ActionCreatorsMapObject, bindActionCreators} from 'redux';
 
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
 import {getUser} from 'mattermost-redux/selectors/entities/users';
 
-import {showSearchResults, updateSearchTerms} from 'actions/views/rhs';
+import {GlobalState} from 'mattermost-redux/types/store';
+
+import {Post} from 'mattermost-redux/types/posts';
 
 import {getDisplayNameByUser} from 'utils/utils.jsx';
 
 import CommentedOn from './commented_on';
-import { GlobalState } from 'mattermost-redux/types/store';
-import { Post } from 'mattermost-redux/types/posts';
-import { Dispatch } from 'react';
-import { ActionFunc, GenericAction } from 'mattermost-redux/types/actions';
-import { ActionTypes } from 'react-select';
 
 type Props = {
     post: Post;
-}
-
-type Actions = {
-    showSearchResults: (isMentionSearch: boolean) => ActionFunc;
-    updateSearchTerms: (terms: any) => { type: ActionTypes, terms: any };
 }
 
 function mapStateToProps(state: GlobalState, ownProps: Props) {
@@ -43,13 +34,4 @@ function mapStateToProps(state: GlobalState, ownProps: Props) {
     };
 }
 
-function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
-    return {
-        actions: bindActionCreators<ActionCreatorsMapObject<ActionFunc>, Actions>({
-            showSearchResults,
-            updateSearchTerms,
-        }, dispatch),
-    };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CommentedOn);
+export default connect(mapStateToProps)(CommentedOn);
