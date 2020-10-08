e7bce870053fb8d32c9238923b7f2e6d545e493a
Filip Ghorbani <filip.ghorbani@hotmail.com>
2020-10-08 23:30:19 +0200

    Remove comment

--- components/admin_console/reset_email_modal/reset_email_modal.jsx
+++ components/admin_console/reset_email_modal/reset_email_modal.jsx
@@ -1,6 +1,5 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-/* eslint-disable react/no-string-refs */
 
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -171,4 +170,3 @@ export default class ResetEmailModal extends React.PureComponent {
         );
     }
 }
-/* eslint-enable react/no-string-refs */
