2732cf47d0b070e3596137758f05f3b6ca20a8bb
Joseph Baylon <joseph.baylon@mattermost.com>
2020-10-08 14:12:08 -0700

    Revert "Cypress/E2E: Fix main menu spec"
    This reverts commit 776fb06c4a065c106089550ab4af081349ed97b2.

--- e2e/cypress/integration/system_console/main_menu_spec.js
+++ e2e/cypress/integration/system_console/main_menu_spec.js
@@ -60,6 +60,9 @@ describe('Main menu', () => {
         // # click to open about modal
         cy.findByText('About Mattermost').click();
 
+        // * Verify about.mattermost.com link text has correct link destination and opens in a new tab
+        verifyLink('about.mattermost.com', 'http://about.mattermost.com/');
+
         // * Verify server link text has correct link destination and opens in a new tab
         verifyLink('server', 'https://about.mattermost.com/platform-notice-txt/');
 
@@ -72,8 +75,11 @@ describe('Main menu', () => {
         // * Verify version exists in modal
         cy.findByText('Mattermost Version:').should('be.visible');
 
-        // * Verify licensed to exists in modal
-        cy.findByText('Licensed to:').should('be.visible');
+        // * Verify build hash exists in modal
+        cy.findByText('Build Hash:').should('be.visible');
+
+        // * Verify build date exists in modal
+        cy.findByText('Build Date:').should('be.visible');
     });
 });
 
