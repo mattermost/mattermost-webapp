484e309f47ac80c8e4c52b53d50f0c88b13679a8
Abdulrahman (Abdu) Assabri <abdusabri@outlook.com>
2020-10-14 10:05:00 +0200

    [MM-20489] Migrate failed_post_options and its tests to typescript (#6717)
    * Migrate failed_post_options and its tests to typescript
    
    * Move mocked post props to test_helper
    
    * Use getPostMock in rhs_thread tests
    
    * Use getChannelMock in rhs_thread tests
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- components/post_view/failed_post_options/index.ts
+++ components/post_view/failed_post_options/index.ts
@@ -2,14 +2,16 @@
 // See LICENSE.txt for license information.
 
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import {bindActionCreators, Dispatch} from 'redux';
+
 import {removePost} from 'mattermost-redux/actions/posts';
+import {GenericAction} from 'mattermost-redux/types/actions';
 
-import {createPost} from 'actions/post_actions.jsx';
+import {createPost} from 'actions/post_actions';
 
-import FailedPostOptions from './failed_post_options.jsx';
+import FailedPostOptions from './failed_post_options';
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
     return {
         actions: bindActionCreators({
             createPost,
