da5fe8a496574e6a12067aa8385cb3c7e100f3ea
Rohitesh Gupta <rohitesh.gupta@mattermost.com>
2020-10-14 19:02:49 +0530

    Fixed comments from PR 6773

--- e2e/cypress/integration/enterprise/group_mentions/group_mentions_posts_spec.js
+++ e2e/cypress/integration/enterprise/group_mentions/group_mentions_posts_spec.js
@@ -37,9 +37,6 @@ describe('Group Mentions', () => {
             testTeam = team;
         });
 
-        // # Login as admin
-        cy.apiAdminLogin();
-
         // # Test LDAP configuration and server connection
         // # Synchronize user attributes
         cy.apiLDAPTest();
@@ -84,13 +81,7 @@ describe('Group Mentions', () => {
                 });
             });
 
-            const preferences = [{
-                user_id: user.id,
-                category: 'tutorial_step',
-                name: user.id,
-                value: '999',
-            }];
-            cy.apiSaveUserPreference(preferences, user.id);
+            cy.apiSaveTutorialStep(user.id, '999');
         });
     });
 
