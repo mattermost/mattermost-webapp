22c6c10feeea8db35baaa9191758d51e8555cbc8
Mike Leonard <mike@worx4you.com>
2020-10-14 09:58:35 -0400

    almost finished - working

--- e2e/cypress/integration/integrations/integrations_page_spec.js
+++ e2e/cypress/integration/integrations/integrations_page_spec.js
@@ -10,20 +10,79 @@
 // Group: @integrations
 
 describe('Integrations', () => {
-    let team;
+    let teamA;
 
     before(() => {
         // # Login, create incoming webhook for Team A
-        cy.apiInitSetup().then(({team, channel}) => {
-            team = team.name;
+        cy.apiInitSetup().then(({team}) => {
+            teamA = team.name;
         });
     });
 
     it('MM-T569 Integrations Page', () => {
-        // # Visit Test Team B Incoming Webhooks page
-        cy.visit(`/${team}/integrations`);
+        // # Visit the integrations page
+        cy.visit(`/${teamA}/integrations`);
 
         // # Shrink the page
-        cy.viewport('iphone-x');
-    });
+        cy.viewport(500, 500);
+
+        // * Assert left side bar integrations link is visible and works
+        cy.get('.backstage-sidebar__category > .category-title').should('be.visible').and('have.attr', 'href', `/${teamA}/integrations`).click()
+        cy.url().should('include','/integrations');
+
+        // * Assert left side bar incoming webhooks link is visible and works
+        cy.get('#incomingWebhooks > .section-title').should('be.visible').and('have.attr', 'href', `/${teamA}/integrations/incoming_webhooks`).click();
+        cy.url().should('include','/incoming_webhooks');
+
+        // * Assert left side bar outgoing webhooks link is visible and works
+        cy.get('#outgoingWebhooks > .section-title').should('be.visible').and('have.attr', 'href', `/${teamA}/integrations/outgoing_webhooks`).click();
+        cy.url().should('include','/outgoing_webhooks');
+
+        // * Assert left side bar slash commands link is visible and works
+        cy.get('#slashCommands > .section-title').should('be.visible').and('have.attr', 'href', `/${teamA}/integrations/commands`).click();
+        cy.url().should('include','commands');
+
+        // * Assert left side bar bot accounts link is visible and works
+        cy.get('#botAccounts > .section-title').should('be.visible').and('have.attr', 'href', `/${teamA}/integrations/bots`).click();
+        cy.url().should('include','/bots');
+
+        // # Return to integrations home
+        cy.visit(`/${teamA}/integrations`);
+
+        // # Isolate the icon links
+        cy.get('.integrations-list.d-flex.flex-wrap').within(() => {
+
+            // * Assert Incoming Webhooks link is visible and works
+            cy.findByText('Incoming Webhooks').scrollIntoView().should('be.visible').click();
+            cy.url().should('include','/incoming_webhooks');
+        });
+
+        // # Return to integrations home
+        cy.visit(`/${teamA}/integrations`);
+
+        // * Assert Outgoing Webhooks link is visible and works
+        cy.get('.integrations-list.d-flex.flex-wrap').within(() => {
+            cy.findByText('Outgoing Webhooks').scrollIntoView().should('be.visible').click();
+            cy.url().should('include','/outgoing_webhooks');
+        });
+
+         // # Return to integrations home
+         cy.visit(`/${teamA}/integrations`);     
+
+        // * Assert Slash Commands link is visible and works
+        cy.get('.integrations-list.d-flex.flex-wrap').within(() => {
+            cy.findByText('Slash Commands').scrollIntoView().should('be.visible').click();
+            cy.url().should('include','/commands');
+        });
+
+        // # Return to integrations home
+        cy.visit(`/${teamA}/integrations`);
+
+        // * Assert Outgoing Webhooks link is visible and works
+        cy.get('.integrations-list.d-flex.flex-wrap').within(() => {
+            cy.findByText('Bot Accounts').scrollIntoView().should('be.visible').click();
+            cy.url().should('include','/bots');
+        });
+    })
 });
+
