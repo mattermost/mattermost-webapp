ee8e6504bc47c84c2bb42ecd4ec61b87e325926c
Sudipto Ghosh <sudipto@ghosh.pro>
2020-10-15 19:03:30 +0530

    [GH-15891] Migrate changeCSS() to CSS variable in utils/utils.jsx, ln. 629 (#6734)
    * migrated call to changeCss for .app__body .popover.bottom>.arrow:after
    
    * fix: replace v with var for .app__body .popover.bottom>.arrow:after
    
    Co-authored-by: Hossein Ahmadian-Yazdi <hahmadia@users.noreply.github.com>
    
    * fix: addressed review comments for #6734
    
    Co-authored-by: Hossein Ahmadian-Yazdi <hahmadia@users.noreply.github.com>

--- utils/utils.jsx
+++ utils/utils.jsx
@@ -602,7 +602,6 @@ export function applyTheme(theme) {
         changeCss('#post-create, .app__body .emoji-picker__preview', 'background:' + theme.centerChannelBg);
         changeCss('.app__body .date-separator .separator__text, .app__body .new-separator .separator__text', 'background:' + theme.centerChannelBg);
         changeCss('.app__body .dropdown-menu, .app__body .popover, .app__body .tip-overlay', 'background:' + theme.centerChannelBg);
-        changeCss('.app__body .popover.bottom>.arrow:after', 'border-bottom-color:' + theme.centerChannelBg);
         changeCss('.app__body .popover.right>.arrow:after, .app__body .tip-overlay.tip-overlay--sidebar .arrow, .app__body .tip-overlay.tip-overlay--header .arrow', 'border-right-color:' + theme.centerChannelBg);
         changeCss('.app__body .popover.top>.arrow:after, .app__body .tip-overlay.tip-overlay--chat .arrow', 'border-top-color:' + theme.centerChannelBg);
         changeCss('.app__body .attachment__content, .app__body .attachment-actions button', 'background:' + theme.centerChannelBg);
