4d389ea5b489a0121347a71603a3f7ce20dba616
Devin Binnie <52460000+devinbinnie@users.noreply.github.com>
2020-10-21 15:58:22 -0400

    [MM-28218] Fix for when both billing address and company address are missing and you can't add company info (#6875)
    * [MM-28218] Fix for when both billing address and company address are missing and you can't add company info
    
    * Type fix

--- components/admin_console/billing/company_info_edit.tsx
+++ components/admin_console/billing/company_info_edit.tsx
@@ -42,7 +42,7 @@ const CompanyInfoEdit: React.FC<Props> = () => {
     const [country, setCountry] = useState(getName(companyInfo?.company_address?.country || 'US'));
     const [state, setState] = useState(companyInfo?.company_address?.state);
 
-    const [sameAsBillingAddress, setSameAsBillingAddress] = useState(!companyInfo?.company_address?.line1);
+    const [sameAsBillingAddress, setSameAsBillingAddress] = useState(Boolean(!companyInfo?.company_address?.line1 && companyInfo?.billing_address?.line1));
     const [isValid, setIsValid] = useState<boolean | undefined>(undefined);
     const [isSaving, setIsSaving] = useState(false);
 
