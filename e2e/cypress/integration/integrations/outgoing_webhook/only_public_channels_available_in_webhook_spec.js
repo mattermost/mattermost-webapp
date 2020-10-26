0416f14e77b6f650db2ab148a7021682060f31a3
Mike Leonard <mike@worx4you.com>
2020-10-26 10:41:38 -0400

    wip

--- e2e/cypress/integration/integrations/outgoing_webhook/only_public_channels_available_in_webhook_spec.js
+++ e2e/cypress/integration/integrations/outgoing_webhook/only_public_channels_available_in_webhook_spec.js
@@ -7,14 +7,18 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-// Group: @messaging
+// Group: @integrations
+
+import * as TIMEOUTS from '../../../fixtures/timeouts';
+
 
 describe('Messaging', () => {
     let testTeam;
-    let testChannel
+    let testChannel;
+    let testUser;
     let channel;
-    let archiveChannel
-    let privateChannel
+    let archiveChannel;
+    let privateChannel;
 
     before(() => {
         // # Login as test user and visit the newly created test channel
@@ -22,52 +26,20 @@ describe('Messaging', () => {
             testTeam = team;
             testChannel = channel
             testUser = user
-
-            // # Login as regular user
-            cy.apiLogin(user);
-
-            // # Set up test channel with a long name
-            //cy.apiCreateChannel(testTeam.id, 'channel-test', 'Archive Test').then(({channel}) => {
-            //    const archivedChannel = channel.name
-            //    cy.visit(`/${testTeam.name}/channels/${channel.name}`);
-
-                //cy.uiArchiveChannel();
             });
-            
         });
-    
 
     it('MM-T134 Visual verification of tooltips on top nav, channel icons, posts', () => {
-
-
         cy.apiCreateChannel(testTeam.id, 'channel-test', 'Archive Test', 'O').then(({channel}) => {
             archiveChannel = channel;
             cy.visit(`/${testTeam.name}/channels/${channel.name}`);
             cy.uiArchiveChannel();
-            //cy.visit(`/${testTeam.name}/integrations/outgoing_webhooks/add`);
-            //cy.get('#channelSelect').contains(channel.display_name);
+            cy.visit(`/${testTeam.name}/integrations/outgoing_webhooks/add`);
+            cy.get('#channelSelect').children().should('not.contain', (channel.display_name));
         });
-        cy.apiCreateChannel(testTeam.id, 'channel-test', 'Private Test', 'P').then(({channel}) => {
-            privateChannel = channel;
-            //cy.visit(`/${testTeam.name}/channels/${channel.name}`);
-            //cy.uiArchiveChannel();
-            //cy.visit(`/${testTeam.name}/integrations/outgoing_webhooks/add`);
-            //cy.get('#channelSelect').should('not.contain', (channel.display_name);
-        });
-        cy.visit(`/${testTeam.name}/integrations/outgoing_webhooks/add`);
-        cy.get('#channelSelect').should('not.contain', (channel.display_name);
-        cy.get('#channelSelect').contains(channel.display_name);
-
-        cy.apiLogin(testUser);
-
-        
-            
-
-
-
-
-        //cy.visit(`/${testTeam.name}/integrations/outgoing_webhooks/add`);
-        //cy.get('#channelSelect').contains(channel.name);
-        //cy.get('#postListContent').find('.top').should('be.visible');
+        //cy.apiCreateChannel(testTeam.id, 'channel-test', 'Private Test', 'P').then(({channel}) => {
+        //    privateChannel = channel;
+        //    cy.visit(`/${testTeam.name}/integrations/outgoing_webhooks/add`);
+        //    cy.get('#channelSelect').should('not.contain', (channel.display_name));
     });
 });
