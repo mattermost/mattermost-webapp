7635139e5e97a441b9d8422898301d38907f941e
Sven Huester <sven.svelle@gmail.com>
2020-10-27 13:38:10 +0100

    add linting fixes and clean up code

--- utils/utils.jsx
+++ utils/utils.jsx
@@ -1399,28 +1399,25 @@ export function imageURLForTeam(team) {
 // Converts a file size in bytes into a human-readable string of the form '123MB'.
 export function fileSizeToString(bytes) {
     // it's unlikely that we'll have files bigger than this
-    if (bytes > 1024 * 1024 * 1024 * 1024) {
-        if (bytes > 1024 * 1024 * 1024 * 1024 * 10) {
-            return Math.round(bytes / (1024 * 1024 * 1024 * 1024)) + 'TB';
-        } else {
-            return Math.round((bytes / (1024 * 1024 * 1024 * 1024)) * 10) / 10 + 'TB';
+    if (bytes > 1024 ** 4) {
+        // check if file is smaller than 10 to display fractions
+        if (bytes < (1024 ** 4) * 10) {
+            return Math.round(((bytes / (1024 ** 4)) * 10) / 10) + 'TB';
         }
-    } else if (bytes > 1024 * 1024 * 1024) {
-        if (bytes > 1024 * 1024 * 1024 * 10) {
-            return Math.round(bytes / (1024 * 1024 * 1024)) + 'GB';
-        } else {
-            return Math.round((bytes / (1024 * 1024 * 1024)) * 10) / 10 + 'GB';
+        return Math.round(bytes / (1024 ** 4)) + 'TB';
+    } else if (bytes > 1024 ** 3) {
+        if (bytes < (1024 ** 3) * 10) {
+            return Math.round(((bytes / (1024 ** 3)) * 10) / 10) + 'GB';
         }
-    } else if (bytes > 1024 * 1024) {
-        if (bytes > 1024 * 1024 * 10) {
-            return Math.round(bytes / (1024 * 1024)) + 'MB';
-        } else {
-            return Math.round((bytes / (1024 * 1024)) * 10) / 10 + 'MB';
+        return Math.round(bytes / (1024 ** 3)) + 'GB';
+    } else if (bytes > 1024 ** 2) {
+        if (bytes < (1024 ** 2) * 10) {
+            return Math.round(((bytes / (1024 ** 2)) * 10) / 10) + 'MB';
         }
+        return Math.round(bytes / (1024 ** 2)) + 'MB';
     } else if (bytes > 1024) {
         return Math.round(bytes / 1024) + 'KB';
     }
-
     return bytes + 'B';
 }
 
