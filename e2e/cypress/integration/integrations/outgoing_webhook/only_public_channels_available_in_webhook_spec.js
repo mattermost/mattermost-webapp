b24bd74c24aee71f8982c0509f7d9e7a42e04488
Mike Leonard <mike@worx4you.com>
2020-10-26 20:25:34 -0400

    wip

--- e2e/cypress/integration/integrations/outgoing_webhook/only_public_channels_available_in_webhook_spec.js
+++ e2e/cypress/integration/integrations/outgoing_webhook/only_public_channels_available_in_webhook_spec.js
@@ -9,37 +9,50 @@
 
 // Group: @integrations
 
-import * as TIMEOUTS from '../../../fixtures/timeouts';
-
-
 describe('Messaging', () => {
     let testTeam;
-    let testChannel;
     let testUser;
-    let channel;
+    let otherUser;
     let archiveChannel;
     let privateChannel;
+    let publicChannel;
+    let dmChannel;
 
     before(() => {
-        // # Login as test user and visit the newly created test channel
+        // # Setup of testuser, team, channel
         cy.apiInitSetup().then(({team, user, channel}) => {
             testTeam = team;
-            testChannel = channel
-            testUser = user
+            testUser = user;
+            publicChannel = channel.display_name;
+            cy.apiCreateUser().then(({user: newUser}) => {
+                otherUser = newUser;
+            });
+
+            // # Create then archive (delete) a public channel
+            cy.apiCreateChannel(team.id, 'channel-test', 'Archive Test', 'O').then(() => {
+                archiveChannel = channel.display_name;
+                cy.visit(`/${testTeam.name}/channels/${channel.name}`);
+                cy.apiDeleteChannel(channel.id);
+            });
+
+            // # Create a private channel
+            cy.apiCreateChannel(testTeam.id, 'channel-test', 'Private Test', 'P').then(() => {
+                privateChannel = channel.display_name;
+            });
+
+            // # Create a direct message channel
+            cy.apiLogin(testUser);
+            cy.apiCreateDirectChannel([testUser.id, otherUser.id]).then(() => {
+                dmChannel = channel.display_name;
             });
         });
+    });
 
     it('MM-T134 Visual verification of tooltips on top nav, channel icons, posts', () => {
-        cy.apiCreateChannel(testTeam.id, 'channel-test', 'Archive Test', 'O').then(({channel}) => {
-            archiveChannel = channel;
-            cy.visit(`/${testTeam.name}/channels/${channel.name}`);
-            cy.uiArchiveChannel();
-            cy.visit(`/${testTeam.name}/integrations/outgoing_webhooks/add`);
-            cy.get('#channelSelect').children().should('not.contain', (channel.display_name));
-        });
-        //cy.apiCreateChannel(testTeam.id, 'channel-test', 'Private Test', 'P').then(({channel}) => {
-        //    privateChannel = channel;
-        //    cy.visit(`/${testTeam.name}/integrations/outgoing_webhooks/add`);
-        //    cy.get('#channelSelect').should('not.contain', (channel.display_name));
+        cy.visit(`/${testTeam.name}/integrations/outgoing_webhooks/add`);
+        cy.get('#channelSelect').children().should('not.contain', (privateChannel));
+        cy.get('#channelSelect').children().should('not.contain', (archiveChannel)); // currently this fails due to a bug
+        cy.get('#channelSelect').children().should('contain', publicChannel);
+        cy.get('#channelSelect').children().should('not.contain', dmChannel);
     });
 });
