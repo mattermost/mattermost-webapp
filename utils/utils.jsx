78f4bc4615cd55f8563939ea1e5b9a834690b5fe
Sven Huester <sven.svelle@gmail.com>
2020-10-27 14:01:57 +0100

    fix incorrect brackets

--- utils/utils.jsx
+++ utils/utils.jsx
@@ -1402,17 +1402,17 @@ export function fileSizeToString(bytes) {
     if (bytes > 1024 ** 4) {
         // check if file is smaller than 10 to display fractions
         if (bytes < (1024 ** 4) * 10) {
-            return Math.round(((bytes / (1024 ** 4)) * 10) / 10) + 'TB';
+            return (Math.round((bytes / (1024 ** 4)) * 10) / 10) + 'TB';
         }
         return Math.round(bytes / (1024 ** 4)) + 'TB';
     } else if (bytes > 1024 ** 3) {
         if (bytes < (1024 ** 3) * 10) {
-            return Math.round(((bytes / (1024 ** 3)) * 10) / 10) + 'GB';
+            return (Math.round((bytes / (1024 ** 3)) * 10) / 10) + 'GB';
         }
         return Math.round(bytes / (1024 ** 3)) + 'GB';
     } else if (bytes > 1024 ** 2) {
         if (bytes < (1024 ** 2) * 10) {
-            return Math.round(((bytes / (1024 ** 2)) * 10) / 10) + 'MB';
+            return (Math.round((bytes / (1024 ** 2)) * 10) / 10) + 'MB';
         }
         return Math.round(bytes / (1024 ** 2)) + 'MB';
     } else if (bytes > 1024) {
