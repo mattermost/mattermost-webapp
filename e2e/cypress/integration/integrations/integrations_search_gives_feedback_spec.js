e4d04d4d4da10ab50bf72c4330099e6a3c99dc98
Mike Leonard <mike@worx4you.com>
2020-10-27 13:18:15 -0400

    refactor

--- e2e/cypress/integration/integrations/integrations_search_gives_feedback_spec.js
+++ e2e/cypress/integration/integrations/integrations_search_gives_feedback_spec.js
@@ -13,8 +13,6 @@ import {getRandomId} from '../../utils';
 
 describe('Integrations', () => {
     let teamName;
-    const results = 'Test';
-    const noResults = `${getRandomId(10)}`;
 
     before(() => {
         // # Setup with the new team and channel
@@ -22,76 +20,55 @@ describe('Integrations', () => {
             teamName = team.name;
 
             // # Setup 2 incoming webhooks
-            const newIncomingHook1 = {
-                channel_id: channel.id,
-                description: 'Incoming webhook Test Description One',
-                display_name: 'Test 1',
-            };
-            cy.apiCreateWebhook(newIncomingHook1);
-
-            const newIncomingHook2 = {
-                channel_id: channel.id,
-                description: 'Incoming webhook Test Description Two',
-                display_name: 'Test 2',
-            };
-            cy.apiCreateWebhook(newIncomingHook2);
+            Cypress._.times(2, (i) => {
+                const newIncomingHook = {
+                    channel_id: channel.id,
+                    description: `Incoming webhook Test Description ${i}`,
+                    display_name: `Test ${i}`,
+                };
+                cy.apiCreateWebhook(newIncomingHook);
+            });
 
             // # Setup 2 outgoing webhooks
-            const newOutgoingHook1 = {
-                team_id: team.id,
-                display_name: 'Test 1',
-                trigger_words: ['banana'],
-                callback_urls: ['https://mattermost.com'],
-            };
-            cy.apiCreateWebhook(newOutgoingHook1, false);
-            const newOutgoingHook2 = {
-                team_id: team.id,
-                display_name: 'Test 2',
-                trigger_words: ['apple'],
-                callback_urls: ['https://mattermost.com'],
-            };
-            cy.apiCreateWebhook(newOutgoingHook2, false);
+            Cypress._.times(2, (i) => {
+                const newOutgoingHook = {
+                    team_id: team.id,
+                    display_name: `Test ${i} `,
+                    trigger_words: [`test-trigger-${i}`],
+                    callback_urls: ['https://mattermost.com'],
+                };
+                cy.apiCreateWebhook(newOutgoingHook, false);
+            });
 
             // # Setup 2 Slash Commands
-            const slashCommand1 = {
-                description: 'Test Slash Command 1',
-                display_name: 'Test 1',
-                method: 'P',
-                team_id: team.id,
-                trigger: 'search-test',
-                url: 'https://google.com',
-            };
-            cy.apiCreateCommand(slashCommand1);
-            const slashCommand2 = {
-                description: 'Test Slash Command 2',
-                display_name: 'Test 2',
-                method: 'P',
-                team_id: team.id,
-                trigger: 'search-testsfds',
-                url: 'https://google.com',
-            };
-            cy.apiCreateCommand(slashCommand2);
+            Cypress._.times(2, (i) => {
+                const slashCommand1 = {
+                    description: `Test Slash Command ${i}`,
+                    display_name: `Test ${i}`,
+                    method: 'P',
+                    team_id: team.id,
+                    trigger: `trigger${i}`,
+                    url: 'https://google.com',
+                };
+                cy.apiCreateCommand(slashCommand1);
+            });
 
             // # Setup 2 bot accounts
-            const botName1 = `${getRandomId(6)}`;
-            const botName2 = `${getRandomId(6)}`;
-            cy.visit(`/${teamName}/integrations/bots/add`);
-            cy.get('#username').type(`Test1-${botName1}`);
-            cy.get('#saveBot').click();
-            cy.get('#doneButton').click();
-            cy.visit(`/${teamName}/integrations/bots/add`);
-            cy.get('#username').type(`Test2-${botName2}`);
-            cy.get('#saveBot').click();
-            cy.get('#doneButton').click();
+            Cypress._.times(2, () => {
+                const randomBotName = `${getRandomId(2)}`;
+                cy.apiCreateBot(`bot-${randomBotName}`);
+            });
+
+            // # Visit the integrations page
+            cy.visit(`/${teamName}/integrations`);
         });
     });
 
     it('MM-T571 Integration search gives feed back when there are no results', () => {
-        // # Visit the integrations page
-        cy.visit(`/${teamName}/integrations`);
-
-        // # Shrink the page
-        cy.viewport(300, 500);
+        // # Shrink the page, set up constants
+        cy.viewport('ipad-2');
+        const results = 'Test';
+        const noResults = `${getRandomId(6)}`;
 
         // * Check incoming webhooks for no match message
         cy.get('#incomingWebhooks').click();
