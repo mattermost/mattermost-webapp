9736a782e7e6dca5f49226ddc3a32b435d549901
Mike Leonard <mike@worx4you.com>
2020-10-28 07:24:42 -0400

    wip

--- e2e/cypress/integration/integrations/outgoing_webhook/only_public_channels_available_in_webhook_spec.js
+++ e2e/cypress/integration/integrations/outgoing_webhook/only_public_channels_available_in_webhook_spec.js
@@ -12,11 +12,12 @@
 describe('Integrations', () => {
     let testTeam;
     let testUser;
-    let otherUser;
+    let user2;
     let archiveChannel;
     let privateChannel;
     let publicChannel;
     let dmChannel;
+    let nonMemberPublicChannel;
 
     before(() => {
         // # Setup of testuser, team, channel
@@ -24,33 +25,47 @@ describe('Integrations', () => {
             testTeam = team;
             testUser = user;
             publicChannel = channel.display_name;
-        });
+            
+            // # Create a second user
+            // Saturn advised to have the user create a new team and that will enable
+            // the integrations page for the non sysadmin user
+            cy.apiCreateUser().then(({user: user2}) => {
+                dmChannel = user2.username 
+                console.log(user2.username)
+            })
+            //dmChannel = `@${user.username}`
+            //    console.log(dmChannel)
 
-        // # Create a direct message channel with a new user
-        cy.apiCreateUser().then(({user: newUser}) => {
-            otherUser = newUser;
-            cy.apiCreateDirectChannel([testUser.id, otherUser.id]).then(({channel}) => {
-                dmChannel = channel.display_name;
+            // # Create a public channel, add a user, then archive the channel
+            cy.apiCreateChannel(testTeam.id, 'channel-test', 'Archive Test', 'O').then(({channel}) => {
+                cy.apiAddUserToChannel(channel.id, user2.id);
+                archiveChannel = channel.display_name;
+                console.log(archiveChannel)
+                cy.apiDeleteChannel(channel.id);
             });
-        });
 
-        // # Create then archive (delete) a public channel
-        cy.apiCreateChannel(testTeam.id, 'channel-test', 'Archive Test', 'O').then(({channel}) => {
-            archiveChannel = channel.display_name;
-            cy.apiDeleteChannel(channel.id);
-        });
+            // # Create a private channel and add user to the channel
+            cy.apiCreateChannel(testTeam.id, 'channel-test', 'Private Test', 'P').then(({channel}) => {
+                cy.apiAddUserToChannel(channel.id, user2.id);
+                privateChannel = channel.display_name;
+             });
 
-        // # Create a private channel
-        cy.apiCreateChannel(testTeam.id, 'channel-test', 'Private Test', 'P').then(({channel}) => {
-            privateChannel = channel.display_name;
+            // # Create a public channel that the user does not belong to
+            cy.apiCreateChannel(testTeam.id, 'channel-test', 'Not Member Test', 'O').then(({channel}) => {
+                nonMemberPublicChannel = channel.display_name;
+                console.log(nonMemberPublicChannel)
+            });
+
+             // # Visit the Add Outgoing Webhooks page
+             cy.apiLogin(user2)
+             cy.visit(`/${testTeam.name}/integrations/outgoing_webhooks/add`);
         });
     });
 
     it('MM-T615 Only public channels available in outgoing webhooks setup', () => {
-        cy.visit(`/${testTeam.name}/integrations/outgoing_webhooks/add`);
-        cy.get('#channelSelect').children().should('not.contain', (privateChannel));
-
-        //cy.get('#channelSelect').children().should('contain', archiveChannel); // currently this fails due to a bug
+        
+        cy.get('#channelSelect').children().should('not.contain', privateChannel);
+        cy.get('#channelSelect').children().should('contain', archiveChannel); // currently this fails due to a bug
         cy.get('#channelSelect').children().should('contain', publicChannel);
         cy.get('#channelSelect').children().should('not.contain', dmChannel);
     });
