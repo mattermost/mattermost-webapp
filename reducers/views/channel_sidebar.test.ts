0b617017bca0ba7cb36981919e2074d9164554cc
Devin Binnie <devin.binnie@mattermost.com>
2020-10-30 18:19:09 -0400

    Lint fix

--- reducers/views/channel_sidebar.test.ts
+++ reducers/views/channel_sidebar.test.ts
@@ -1,8 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {ChannelTypes} from 'mattermost-redux/action_types';
-
 import {ActionTypes} from 'utils/constants';
 
 import * as Reducers from './channel_sidebar';
