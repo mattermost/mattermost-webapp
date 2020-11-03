5cc27bc7470f2b38c2dd2bc536e49d8c2caf7597
Pijus Kamandulis <pkpjuklas@gmail.com>
2020-11-03 09:11:44 +0200

    MM-20578 Remove 'any' cast from index.ts

--- components/admin_console/system_user_detail/index.ts
+++ components/admin_console/system_user_detail/index.ts
@@ -2,16 +2,20 @@
 // See LICENSE.txt for license information.
 
 import {connect} from 'react-redux';
-import {bindActionCreators, Dispatch} from 'redux';
+import {ActionCreatorsMapObject, bindActionCreators, Dispatch} from 'redux';
 
 import {getUser} from 'mattermost-redux/selectors/entities/users';
 import {updateUserActive} from 'mattermost-redux/actions/users';
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
 import {addUserToTeam} from 'mattermost-redux/actions/teams';
 
-import {GenericAction} from 'mattermost-redux/types/actions';
+import {ActionFunc, GenericAction} from 'mattermost-redux/types/actions';
 
 import {GlobalState} from 'mattermost-redux/types/store';
+
+import {ServerError} from 'mattermost-redux/types/errors';
+
+import {TeamMembership} from 'mattermost-redux/types/teams';
 
 import {setNavigationBlocked} from 'actions/admin_actions.jsx';
 
@@ -31,14 +35,26 @@ function mapStateToProps(state: GlobalState, ownProps: OwnProps) {
     };
 }
 
+type Actions = {
+    updateUserActive: (userId: string, active: boolean) => Promise<{error: ServerError}>;
+    setNavigationBlocked: (blocked: boolean) => void;
+    addUserToTeam: (teamId: string, userId?: string) => Promise<{data: TeamMembership; error?: any}>;
+}
+
 function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
-    return {
-        actions: bindActionCreators({
-            updateUserActive,
-            setNavigationBlocked,
-            addUserToTeam,
-        }, dispatch),
-    } as any;
+    const apiActions = bindActionCreators<ActionCreatorsMapObject<ActionFunc>, Actions>({
+        updateUserActive,
+        addUserToTeam,
+    }, dispatch);
+    const uiActions = bindActionCreators({
+        setNavigationBlocked,
+    }, dispatch);
+
+    const props = {
+        actions: Object.assign(apiActions, uiActions),
+    };
+
+    return props;
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SystemUserDetail);
