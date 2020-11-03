e69ffa18438d3e9df2ce8825063d55c5bf6e4eb0
Morgan Connolly <morganrconnolly@berkeley.edu>
2020-11-03 12:22:18 -0800

    fix linter errors for index.ts

--- components/password_reset_form/index.ts
+++ components/password_reset_form/index.ts
@@ -6,9 +6,10 @@ import {connect} from 'react-redux';
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
 import {resetUserPassword} from 'mattermost-redux/actions/users';
 
-import {ServerError} from 'mattermost-redux/types/errors';
 import {GenericAction} from 'mattermost-redux/types/actions';
+
 import {GlobalState} from 'types/store';
+
 import PasswordResetForm from './password_reset_form';
 
 type Actions = {
@@ -17,14 +18,14 @@ type Actions = {
 function mapStateToProps(state: GlobalState) {
     const {SiteName: siteName} = getConfig(state);
     return {siteName};
-};
+}
 
 function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
-  return {
-    actions: bindActionCreators({
-        resetUserPassword,
-    }, dispatch),
-  };
-};
+    return {
+        actions: bindActionCreators({
+            resetUserPassword,
+        }, dispatch),
+    };
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(PasswordResetForm);
