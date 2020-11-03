4dac5b6222452040a79ba35deaedb2b59bfa151a
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2020-11-02 20:19:23 -0500

    tabs to spaces for types

--- actions/applet_types.ts
+++ actions/applet_types.ts
@@ -1,25 +1,25 @@
 export type AppletBinding = {
-	// For use by Mattermost only, not for apps
+    // For use by Mattermost only, not for apps
     app_id: string;
 
     location_id?: string;
 
-	// For PostMenu, ChannelHeader locations specifies the icon.
+    // For PostMenu, ChannelHeader locations specifies the icon.
     icon?: string;
 
-	// Name is the (usually short) primary text to display at the location.
-	// - For LocationPostMenu is the menu item text.
-	// - For LocationChannelHeader is the dropdown text.
-	// - For LocationCommand is the name of the command
+    // Name is the (usually short) primary text to display at the location.
+    // - For LocationPostMenu is the menu item text.
+    // - For LocationChannelHeader is the dropdown text.
+    // - For LocationCommand is the name of the command
     name: string;
 
-	// Hint is the secondary text to display
-	// - LocationPostMenu: not used
-	// - LocationChannelHeader: tooltip
-	// - LocationCommand: the "Hint" line
+    // Hint is the secondary text to display
+    // - LocationPostMenu: not used
+    // - LocationChannelHeader: tooltip
+    // - LocationCommand: the "Hint" line
     hint?: string;
 
-	// Description is the (optional) extended help text, used in modals and autocomplete
+    // Description is the (optional) extended help text, used in modals and autocomplete
     description?: string;
 
     role_id?: string;
@@ -28,9 +28,9 @@ export type AppletBinding = {
     depends_on_user?: boolean;
     depends_on_post?: boolean;
 
-	// A Binding is either to a Call, or is a "container" for other locations -
-	// i.e. menu sub-items or subcommands.
-	call?: AppletCall;
+    // A Binding is either to a Call, or is a "container" for other locations -
+    // i.e. menu sub-items or subcommands.
+    call?: AppletCall;
     bindings?: AppletBinding[];
 
     func?: AppletFunction;
@@ -38,65 +38,65 @@ export type AppletBinding = {
 
 export type AppletCall = {
     url: string;
-	context: AppletContext;
-	values?: {[name: string]: string};
-	as_modal?: boolean;
-	raw_command?: string;
+    context: AppletContext;
+    values?: {[name: string]: string};
+    as_modal?: boolean;
+    raw_command?: string;
 }
 
 export type AppletCallResponseType = string;
 
 export const AppletCallResponseTypes: {[name: string]: AppletCallResponseType} = {
     CALL: 'call',
-	MODAL: 'modal',
+    MODAL: 'modal',
     OK: 'ok',
-	NAVIGATE: 'navigate',
+    NAVIGATE: 'navigate',
     ERROR: 'error',
 };
 
-export type AppletCallResponse<Res={}> = {
-	type: AppletCallResponseType;
+export type AppletCallResponse<Res = {}> = {
+    type: AppletCallResponseType;
 
-	markdown?: string;
-	data?: Res;
+    markdown?: string;
+    data?: Res;
 
-	error?: string;
+    error?: string;
 
-	url?: string;
-	use_external_browser?: boolean;
+    url?: string;
+    use_external_browser?: boolean;
 
-	call?: AppletCall;
+    call?: AppletCall;
 }
 
 export type AppletContext = {
-	app_id: string;
-	acting_user_id: string;
-	user_id?: string;
-	team_id?: string;
-	channel_id: string;
-	post_id?: string;
-	root_post_id?: string;
+    app_id: string;
+    acting_user_id: string;
+    user_id?: string;
+    team_id?: string;
+    channel_id: string;
+    post_id?: string;
+    root_post_id?: string;
     props?: {[name: string]: string};
 };
 
 export type AppletExpandLevel = string;
 
 const AppletExpandLevels: {[name: string]: AppletExpandLevel} = {
-	EXPAND_ALL: 'All',
-	EXPAND_SUMMARY: 'Summary',
+    EXPAND_ALL: 'All',
+    EXPAND_SUMMARY: 'Summary',
 };
 
 export type AppletExpand = {
-	app?: AppletExpandLevel;
-	acting_user?: AppletExpandLevel;
-	channel?: AppletExpandLevel;
-	config?: AppletExpandLevel;
-	mentioned?: AppletExpandLevel;
-	parent_post?: AppletExpandLevel;
-	post?: AppletExpandLevel;
-	root_post?: AppletExpandLevel;
-	team?: AppletExpandLevel;
-	user?: AppletExpandLevel;
+    app?: AppletExpandLevel;
+    acting_user?: AppletExpandLevel;
+    channel?: AppletExpandLevel;
+    config?: AppletExpandLevel;
+    mentioned?: AppletExpandLevel;
+    parent_post?: AppletExpandLevel;
+    post?: AppletExpandLevel;
+    root_post?: AppletExpandLevel;
+    team?: AppletExpandLevel;
+    user?: AppletExpandLevel;
 }
 
 export type AppletForm = {
@@ -109,7 +109,7 @@ export type AppletForm = {
 };
 
 export type AppletSelectOption = {
-	label: string;
+    label: string;
     value: string;
     icon_data?: string;
 };
@@ -118,43 +118,43 @@ export type AppletFieldType = string;
 
 export const AppletFieldTypes: {[name: string]: AppletFieldType} = {
     TEXT: 'text',
-	STATIC_SELECT: 'static_select',
-	DYNAMIC_SELECT: 'dynamic_select',
-	BOOL: 'bool',
-	USER: 'user',
-	CHANNEL: 'channel',
+    STATIC_SELECT: 'static_select',
+    DYNAMIC_SELECT: 'dynamic_select',
+    BOOL: 'bool',
+    USER: 'user',
+    CHANNEL: 'channel',
 }
 
 // This should go in mattermost-redux
 export type AppletField = {
-	// Name is the name of the JSON field to use.
+    // Name is the name of the JSON field to use.
     name: string;
-	type: AppletFieldType;
+    type: AppletFieldType;
     is_required?: boolean;
 
-	// Present (default) value of the field
+    // Present (default) value of the field
     value?: string;
 
     description: string;
 
-	// Autocomplete name should be set to either the name of the --flag for
-	// named fields, or to $X for positional arguments, where X is 1-based
-	// number.
+    // Autocomplete name should be set to either the name of the --flag for
+    // named fields, or to $X for positional arguments, where X is 1-based
+    // number.
     autocomplete_label?: string;
     hint?: string;
     positional?: boolean;
 
     modal_label?: string;
 
-	// Select props
-	refresh_on_change_to?: string[];
+    // Select props
+    refresh_on_change_to?: string[];
     source_url?: string;
-	options?: AppletSelectOption[];
+    options?: AppletSelectOption[];
 
-	// Text props
+    // Text props
     subtype?: string;
-	min_length?: number;
-	max_length?: number;
+    min_length?: number;
+    max_length?: number;
 }
 
 export type AppletFunction = {
