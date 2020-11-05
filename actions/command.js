1f0df298ac5eb91b7d659c1b75e4633d8197073d
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2020-11-04 21:08:36 -0500

    fix whitespace

--- actions/command.js
+++ actions/command.js
@@ -37,62 +37,62 @@ export function executeCommand(message, args) {
         msg = cmd + ' ' + msg.substring(cmdLength, msg.length).trim();
 
         switch (cmd) {
-            case '/search':
-                dispatch(PostActions.searchForTerm(msg.substring(cmdLength + 1, msg.length)));
-                return {data: true};
-            case '/shortcuts':
-                if (UserAgent.isMobile()) {
-                    const error = {message: localizeMessage('create_post.shortcutsNotSupported', 'Keyboard shortcuts are not supported on your device')};
-                    return {error};
-                }
-
-                GlobalActions.toggleShortcutsModal();
-                return {data: true};
-            case '/leave': {
-                // /leave command not supported in reply threads.
-                if (args.channel_id && (args.root_id || args.parent_id)) {
-                    GlobalActions.sendEphemeralPost('/leave is not supported in reply threads. Use it in the center channel instead.', args.channel_id, args.parent_id);
-                    return {data: true};
-                }
-                const channel = getCurrentChannel(state);
-                if (channel.type === Constants.PRIVATE_CHANNEL) {
-                    GlobalActions.showLeavePrivateChannelModal(channel);
-                    return {data: true};
-                }
-                if (
-                    channel.type === Constants.DM_CHANNEL ||
-                    channel.type === Constants.GM_CHANNEL
-                ) {
-                    const currentUserId = getCurrentUserId(state);
-                    let name;
-                    let category;
-                    if (channel.type === Constants.DM_CHANNEL) {
-                        name = getUserIdFromChannelName(channel);
-                        category = Constants.Preferences.CATEGORY_DIRECT_CHANNEL_SHOW;
-                    } else {
-                        name = channel.id;
-                        category = Constants.Preferences.CATEGORY_GROUP_CHANNEL_SHOW;
-                    }
-                    const currentTeamId = getCurrentTeamId(state);
-                    const redirectChannel = getRedirectChannelNameForTeam(state, currentTeamId);
-                    const teamUrl = getCurrentRelativeTeamUrl(state);
-                    browserHistory.push(`${teamUrl}/channels/${redirectChannel}`);
-
-                    dispatch(savePreferences(currentUserId, [{category, name, user_id: currentUserId, value: 'false'}]));
-                    if (isFavoriteChannel(state, channel.id)) {
-                        dispatch(unfavoriteChannel(channel.id));
-                    }
-
-                    return {data: true};
-                }
-                break;
+        case '/search':
+            dispatch(PostActions.searchForTerm(msg.substring(cmdLength + 1, msg.length)));
+            return {data: true};
+        case '/shortcuts':
+            if (UserAgent.isMobile()) {
+                const error = {message: localizeMessage('create_post.shortcutsNotSupported', 'Keyboard shortcuts are not supported on your device')};
+                return {error};
             }
-            case '/settings':
-                dispatch(openModal({modalId: ModalIdentifiers.USER_SETTINGS, dialogType: UserSettingsModal}));
+
+            GlobalActions.toggleShortcutsModal();
+            return {data: true};
+        case '/leave': {
+            // /leave command not supported in reply threads.
+            if (args.channel_id && (args.root_id || args.parent_id)) {
+                GlobalActions.sendEphemeralPost('/leave is not supported in reply threads. Use it in the center channel instead.', args.channel_id, args.parent_id);
                 return {data: true};
-            case '/collapse':
-            case '/expand':
-                dispatch(PostActions.resetEmbedVisibility());
+            }
+            const channel = getCurrentChannel(state);
+            if (channel.type === Constants.PRIVATE_CHANNEL) {
+                GlobalActions.showLeavePrivateChannelModal(channel);
+                return {data: true};
+            }
+            if (
+                channel.type === Constants.DM_CHANNEL ||
+                channel.type === Constants.GM_CHANNEL
+            ) {
+                const currentUserId = getCurrentUserId(state);
+                let name;
+                let category;
+                if (channel.type === Constants.DM_CHANNEL) {
+                    name = getUserIdFromChannelName(channel);
+                    category = Constants.Preferences.CATEGORY_DIRECT_CHANNEL_SHOW;
+                } else {
+                    name = channel.id;
+                    category = Constants.Preferences.CATEGORY_GROUP_CHANNEL_SHOW;
+                }
+                const currentTeamId = getCurrentTeamId(state);
+                const redirectChannel = getRedirectChannelNameForTeam(state, currentTeamId);
+                const teamUrl = getCurrentRelativeTeamUrl(state);
+                browserHistory.push(`${teamUrl}/channels/${redirectChannel}`);
+
+                dispatch(savePreferences(currentUserId, [{category, name, user_id: currentUserId, value: 'false'}]));
+                if (isFavoriteChannel(state, channel.id)) {
+                    dispatch(unfavoriteChannel(channel.id));
+                }
+
+                return {data: true};
+            }
+            break;
+        }
+        case '/settings':
+            dispatch(openModal({modalId: ModalIdentifiers.USER_SETTINGS, dialogType: UserSettingsModal}));
+            return {data: true};
+        case '/collapse':
+        case '/expand':
+            dispatch(PostActions.resetEmbedVisibility());
         }
 
         const bindings = getAppCommandLocations(state);
