e75102c7789f50c31a6d3d5aa877fad56c7b93bd
Mike Leonard <mike@worx4you.com>
2020-11-05 12:27:48 -0500

    wip

--- e2e/cypress/integration/integrations/incoming_webhook/webhook_posts_when_creator_not_in_channel_spec.js
+++ e2e/cypress/integration/integrations/incoming_webhook/webhook_posts_when_creator_not_in_channel_spec.js
@@ -16,49 +16,53 @@ import * as TIMEOUTS from '../../../fixtures/timeouts';
 describe('Incoming webhook', () => {
     let testTeam;
     let incomingWebhook;
-    //let user;
+    let user2;
+    let channel;
 
     before(() => {
         // # Create and visit new channel and create incoming webhook
-        cy.apiInitSetup().then(({team, channel, user}) => {
+        cy.apiInitSetup().then(({user}) => {
+            cy.apiCreateUser(user2);
             cy.apiLogin(user).then(() => {
-                // # Create a new team
-                cy.apiCreateTeam('team', 'Users Team').then(({team}) => {
+                cy.apiCreateTeam('team', 'Webhook Team').then(({team}) => {
                     testTeam = team;
-                    cy.apiCreateUser().then(({user: user2}) => {
-                        cy.apiAddUserToTeam(testTeam.id, user2.id);
-                        cy.apiCreateChannel(team.id, 'channel-test', 'Webhook Test').then(({channel}) => {
-                            webhookChannel = channel.display_name;
-                            cy.apiLogin(user).then(() => {
-                                const newIncomingHook = {
-                                    channel_id: channel.id,
-                                    channel_locked: false,
-                                    description: 'Incoming webhook - setting',
-                                    display_name: 'webhook-setting',
-                                };
-                                cy.apiCreateWebhook(newIncomingHook).then((hook) => {
-                                    incomingWebhook = hook;
-                                });
-                                cy.apiAddUserToChannel(user2).then(() => {
-                                    cy.apiAdminLogin().then(() => {
-                                        cy.apiDeleteUserFromTeam(user);
-                                    });
-                                });
-                            });
+                    console.log(testTeam)
+                    console.log(team)
+
+                    cy.apiCreateChannel(team.id, 'channel-test', 'Webhook Channel').then(({channel}) => {
+                        channel.id = channel.id;
+                        console.log(channel)
+                        const newIncomingHook = {
+                            channel_id: channel.id,
+                            channel_locked: false,
+                            description: 'Incoming webhook - setting',
+                            display_name: 'webhook-setting',
+                        };
+                        cy.apiCreateWebhook(newIncomingHook).then((hook) => {
+                            incomingWebhook = hook;
+                            const message = 'TestMessage'
+                            console.log(incomingWebhook)
+                            cy.postIncomingWebhook({url: incomingWebhook.url, data: message});
                         });
+                        console.log(incomingWebhook)
+                        cy.removeUserFromChannel(channel.id, user.id).then((res) => {
+                            expect(res.status).to.equal(200);
+                    });
+                    cy.apiAddUserToTeam(user2).then(() => {
+                        cy.apiAddUserToChannel(user2)
+                    });
+                        cy.apiLogin(user2);
+                        cy.visit(`/${team.name}/channels/${channel.name}`);
                     });
                 });
             });
         });
     });
 
+
     it('MM-T638 Webhook posts when webhook creator is not a member of the channel', () => {
             // # Post the first incoming webhook
-            const message = "Test Message"
-            cy.postIncomingWebhook({url: incomingWebhook.url, data: message});
-
-            cy.visit(`/${team.name}/channels/${channel.name}`);
-
+            //const message = "Test Message"
+           // cy.postIncomingWebhook({url: incomingWebhook.url, data: message});
     });
 });
-
