dd2dd68c8ba943e3559eaddf518f86696162d576
Harrison Healey <harrisonmhealey@gmail.com>
2020-11-06 16:31:39 -0500

    Add E2E test for moving channels into muted categories

--- e2e/cypress/integration/channel_sidebar/category_muting_spec.js
+++ e2e/cypress/integration/channel_sidebar/category_muting_spec.js
@@ -30,11 +30,8 @@ describe('Category muting', () => {
     });
 
     it('category headers should be muted and unmuted correctly', () => {
-        // # Wait for the sidebar to be loaded
-        cy.get('.SidebarChannelGroupHeader:contains(CHANNELS)').should('be.visible');
-
         // * Verify that the Channels category and its channels start unmuted
-        cy.get('.SidebarChannelGroupHeader:contains(CHANNELS)').should('not.have.class', 'muted');
+        cy.get('.SidebarChannelGroupHeader:contains(CHANNELS)').should('be.visible').should('not.have.class', 'muted');
         cy.get('#sidebarItem_town-square').should('not.have.class', 'muted');
         cy.get('#sidebarItem_off-topic').should('not.have.class', 'muted');
 
@@ -53,5 +50,31 @@ describe('Category muting', () => {
         cy.get('.SidebarChannelGroupHeader:contains(CHANNELS)').should('have.class', 'muted');
         cy.get('#sidebarItem_town-square').should('not.have.class', 'muted');
         cy.get('#sidebarItem_off-topic').should('not.have.class', 'muted');
+    });
+
+    it('moving a channel into a muted category should mute it', () => {
+        // # Create a new category
+        cy.uiCreateSidebarCategory().then((category) => {
+            // # Mute the new category
+            clickCategoryMenuItem(category.displayName, 'Mute Category');
+
+            // * Verify that Town Square starts unmuted
+            cy.get('#sidebarItem_town-square').should('not.have.class', 'muted');
+
+            // * Verify that the new category is muted
+            cy.get(`.SidebarChannelGroupHeader:contains(${category.displayName})`).should('have.class', 'muted');
+
+            // # Move Town Square into the custom category
+            cy.uiMoveChannelToCategory('town-square', category.displayName);
+
+            // * Verify that Town Square is now muted
+            cy.get('#sidebarItem_town-square').should('have.class', 'muted');
+
+            // # Move Town Square back to Channels
+            cy.uiMoveChannelToCategory('town-square', 'CHANNELS');
+
+            // * Verify that Town Square is now unmuted
+            cy.get('#sidebarItem_town-square').should('not.have.class', 'muted');
+        });
     });
 });
