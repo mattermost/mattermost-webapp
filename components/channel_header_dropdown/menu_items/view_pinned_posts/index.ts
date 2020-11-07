8352e3d80ca192de6053d52360c7c31cc5c13a37
Pablo Velez <pvelez@bp-3.com>
2020-11-07 12:03:34 +0100

    MM-20433 - convert view_pinned_posts to typescript

--- components/channel_header_dropdown/menu_items/view_pinned_posts/index.ts
+++ components/channel_header_dropdown/menu_items/view_pinned_posts/index.ts
@@ -1,20 +1,24 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {bindActionCreators} from 'redux';
+import {bindActionCreators, Dispatch} from 'redux';
 import {connect} from 'react-redux';
 
 import {closeRightHandSide, showPinnedPosts} from 'actions/views/rhs';
 import {getRhsState} from 'selectors/rhs';
 import {RHSStates} from 'utils/constants';
 
+import {GlobalState} from 'types/store';
+import {GenericAction} from 'mattermost-redux/types/actions';
+
 import ViewPinnedPosts from './view_pinned_posts';
 
-const mapStateToProps = (state) => ({
+
+const mapStateToProps = (state: GlobalState) => ({
     hasPinnedPosts: getRhsState(state) === RHSStates.PIN,
 });
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = (dispatch: Dispatch<GenericAction>) => ({
     actions: bindActionCreators({
         closeRightHandSide,
         showPinnedPosts,
