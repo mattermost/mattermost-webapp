3fe78fe962f835d661cd03928ed3a6bbb1f76d35
sridhar02 <kattasridhar02@gmail.com>
2020-11-12 22:41:18 +0530

    removed eslint no string refs comment

--- components/admin_console/reset_password_modal/reset_password_modal.tsx
+++ components/admin_console/reset_password_modal/reset_password_modal.tsx
@@ -1,6 +1,5 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-/* eslint-disable react/no-string-refs */
 
 import React from 'react';
 import {Modal} from 'react-bootstrap';
@@ -247,4 +246,3 @@ export default class ResetPasswordModal extends React.PureComponent<Props, State
         );
     }
 }
-/* eslint-enable react/no-string-refs */
