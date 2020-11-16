81cd02d27498863bf4b07cb06b94b483cffefafa
Jason Frerich <jason.frerich@mattermost.com>
2020-11-16 15:06:35 -0600

    Add more tests

--- e2e/cypress/integration/menus/status_dropdown_spec.js
+++ e2e/cypress/integration/menus/status_dropdown_spec.js
@@ -10,11 +10,23 @@
 // Stage: @prod
 // Group: @menu
 
+import * as TIMEOUTS from '../../fixtures/timeouts';
+
 describe('Status dropdown menu', () => {
+    let user1;
+    let user2;
+    let testChannelUrl;
     before(() => {
         // # Login as test user and visit town-square
-        cy.apiInitSetup().then(({team}) => {
-            cy.visit(`/${team.name}/channels/town-square`);
+        cy.apiInitSetup().then(({user, team}) => {
+            user1 = user;
+            testChannelUrl = `/${team.name}/channels/town-square`;
+            cy.visit(testChannelUrl);
+
+            cy.apiCreateUser().then(({user: otherUser}) => {
+                user2 = otherUser;
+                cy.apiAddUserToTeam(team.id, user2.id);
+            });
         });
     });
 
@@ -34,6 +46,106 @@ describe('Status dropdown menu', () => {
 
         // # Wait for status menu to transition in
         cy.get('.MenuWrapper.status-dropdown-menu .Menu__content.dropdown-menu').should('be.visible');
+    });
+
+    it('MM-T671 Prompting to set status to online from Away', () => {
+        cy.apiLogin(user1);
+        cy.visit(testChannelUrl);
+
+        // # Use the status drop-down on your profile pic to go Away
+        cy.get('img.Avatar').click();
+        cy.findByText('Away').click();
+
+        // * Your status stays offline in your view
+        cy.get('.away--icon').should('be.visible');
+
+        // # Log out
+        cy.apiLogout();
+
+        // # Log back in
+        cy.apiLogin(user1);
+        cy.visit(testChannelUrl);
+
+        // # On modal that asks if you want to be set Online, select Yes.
+        cy.get('.modal-content').within(() => {
+            cy.findByText('Your Status is Set to "Away"').should('be.visible');
+            cy.findByText('Yes, set my status to "Online"').click();
+        });
+
+        // * Your status is online in your view
+        cy.get('.online--icon').should('be.visible');
+
+        // # login in as user2
+        cy.apiLogin(user2);
+        cy.visit(testChannelUrl);
+        openDM(user1.username);
+
+        // * Your status is online in other users' views.
+        cy.apiGetUserStatus(`${user1.id}`).then((result) => {
+            cy.wrap(result.status.status).should('be.equal', 'online');
+        });
+    });
+
+    it('MM-T675 offline in RHS', () => {
+        cy.apiLogin(user1);
+
+        cy.visit(testChannelUrl);
+
+        // # Reset user status to online to prevent status modal
+        cy.apiUpdateUserStatus('online');
+
+        // # make a post so we can open RHS reply thread
+        cy.uiPostMessageQuickly('hello');
+
+        // # Open RHS
+        cy.clickPostCommentIcon();
+
+        // # post /offline to change status to offline
+        cy.postMessageReplyInRHS('/offline');
+
+        // * Your status is offine in your view
+        cy.get('.offline--icon').should('be.visible');
+
+        // * System message displays "You are now offline"
+        cy.getLastPostId().then((postId) => {
+            cy.get(`#post_${postId}`).should('contain', 'You are now offline');
+        });
+    });
+
+    it('MM-T676 online in RHS', () => {
+        cy.visit(testChannelUrl);
+
+        // # make a post so we can open RHS reply thread
+        cy.uiPostMessageQuickly('hello');
+
+        // # Open RHS
+        cy.clickPostCommentIcon();
+
+        // # post /online to change status to online
+        cy.postMessageReplyInRHS('/online');
+
+        // * Your status is online in your view
+        cy.get('.online--icon').should('be.visible');
+
+        // * System message displays "You are now online"
+        cy.getLastPostId().then((postId) => {
+            cy.get(`#post_${postId}`).should('contain', 'You are now online');
+        });
+    });
+
+    it('MM-T677 text after /<status>', () => {
+        cy.visit(testChannelUrl);
+
+        // # post /online with some extra text
+        cy.get('#post_textbox').type('/online here\'s some extra text {enter}');
+
+        // * Your status is online in your view
+        cy.get('.online--icon').should('be.visible');
+
+        // * System message displays "You are now online"
+        cy.getLastPostId().then((postId) => {
+            cy.get(`#post_${postId}`).should('contain', 'You are now online').should('not.contain', 'some extra text');
+        });
     });
 
     it('Changes status icon to online when "Online" menu item is selected', () => {
@@ -101,3 +213,12 @@ describe('Status dropdown menu', () => {
         cy.get('.MenuWrapper.status-dropdown-menu > .status-wrapper > button.status > span > svg.offline--icon').should('exist');
     });
 });
+
+const openDM = (username) => {
+    cy.get('#addDirectChannel').click();
+    cy.get('#selectItems').type(`${username}`);
+    cy.wait(TIMEOUTS.ONE_SEC);
+    cy.get('#multiSelectList').findByText(`@${username}`).click();
+    cy.get('#selectItems').findByText(`${username}`).should('be.visible');
+    cy.findByText('Go').click();
+};
