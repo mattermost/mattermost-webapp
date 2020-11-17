b4805a612fc27b4977d7a6223b47ef984de18bfb
Saturnino Abril <saturnino.abril@gmail.com>
2020-11-17 19:19:29 +0800

    promote and demote tests to/from prod (#7080)

--- e2e/cypress/integration/enterprise/system_console/cluster_spec.js
+++ e2e/cypress/integration/enterprise/system_console/cluster_spec.js
@@ -6,7 +6,6 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-// Stage: @prod
 // Group: @enterprise @system_console
 
 describe('Cluster', () => {
