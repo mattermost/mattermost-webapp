a7f5aecdf235013f1906fbbb87997a9d7b88a094
Mike Leonard <mike@worx4you.com>
2020-11-17 19:06:57 -0500

    wip

--- e2e/cypress/integration/integrations/incoming_webhook/webhook_posts_when_creator_not_in_channel_spec.js
+++ e2e/cypress/integration/integrations/incoming_webhook/webhook_posts_when_creator_not_in_channel_spec.js
@@ -7,62 +7,56 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-// Stage: @prod
-// Group: @incoming_webhook
+// Group: @integrations
 
 import {getRandomId} from '../../../utils';
-import * as TIMEOUTS from '../../../fixtures/timeouts';
 
-describe('Incoming webhook', () => {
-    let testTeam;
+describe('Integrations', () => {
+    let newIncomingHook;
     let incomingWebhook;
-    let user2;
-    let channel;
+    let testChannel;
+    let testTeam;
+    let otherUser;
 
     before(() => {
-        // # Create and visit new channel and create incoming webhook
-        cy.apiInitSetup().then(({user}) => {
-            cy.apiCreateUser(user2);
-            cy.apiLogin(user).then(() => {
-                cy.apiCreateTeam('team', 'Webhook Team').then(({team}) => {
-                    testTeam = team;
-                    console.log(testTeam)
-                    console.log(team)
-
-                    cy.apiCreateChannel(team.id, 'channel-test', 'Webhook Channel').then(({channel}) => {
-                        channel.id = channel.id;
-                        console.log(channel)
-                        const newIncomingHook = {
-                            channel_id: channel.id,
-                            channel_locked: false,
-                            description: 'Incoming webhook - setting',
-                            display_name: 'webhook-setting',
-                        };
-                        cy.apiCreateWebhook(newIncomingHook).then((hook) => {
-                            incomingWebhook = hook;
-                            const message = 'TestMessage'
-                            console.log(incomingWebhook)
-                            cy.postIncomingWebhook({url: incomingWebhook.url, data: message});
-                        });
-                        console.log(incomingWebhook)
-                        cy.removeUserFromChannel(channel.id, user.id).then((res) => {
-                            expect(res.status).to.equal(200);
-                    });
-                    cy.apiAddUserToTeam(user2).then(() => {
-                        cy.apiAddUserToChannel(user2)
-                    });
-                        cy.apiLogin(user2);
-                        cy.visit(`/${team.name}/channels/${channel.name}`);
-                    });
+        // # Create test team, channel, and webhook
+        cy.apiInitSetup().then(({team, channel, user}) => {
+            testTeam = team;
+            testChannel = channel;
+            newIncomingHook = {
+                channel_id: channel.id,
+                channel_locked: true,
+                description: 'Test Webhook Description',
+                display_name: 'Test Webhook Name',
+            };
+            cy.log('create a second user');
+            cy.apiCreateUser().then(({user: user2}) => {
+                otherUser = user2;
+                cy.log('add this user to the team');
+                cy.apiAddUserToTeam(testTeam.id, otherUser.id).then(() => {
+                    cy.log('add this user to the channel');
+                    cy.apiAddUserToChannel(testChannel.id, otherUser.id);
                 });
             });
+
+            //# Create a new webhook
+            cy.apiCreateWebhook(newIncomingHook).then((hook) => {
+                incomingWebhook = hook;
+            });
+            cy.apiLogin(user);
+            cy.visit(`/${testTeam.name}/channels/${testChannel.name}`);
         });
     });
 
-
-    it('MM-T638 Webhook posts when webhook creator is not a member of the channel', () => {
-            // # Post the first incoming webhook
-            //const message = "Test Message"
-           // cy.postIncomingWebhook({url: incomingWebhook.url, data: message});
+    it('MM-T640 Cancel out of edit', () => {
+        const payload = getPayload(testChannel);
+        cy.postIncomingWebhook({url: incomingWebhook.url, data: payload});
     });
 });
+
+function getPayload(testChannel) {
+    return {
+        channel: testChannel.name,
+        text: `${getRandomId()} - this is from incoming webhook`,
+    };
+}
