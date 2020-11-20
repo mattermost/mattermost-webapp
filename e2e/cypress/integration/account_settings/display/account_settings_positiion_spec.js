37bd4772fe86cdace77bb1ef1d85b843290e6fec
Nevyana Angelova <nevyangelova@Nevyanas-MacBook-Pro-2.local>
2020-11-20 20:52:02 +0200

    [MM-30358] - Add account position spec

---
+++ e2e/cypress/integration/account_settings/display/account_settings_positiion_spec.js
@@ -0,0 +1,99 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+// ***************************************************************
+// - [#] indicates a test step (e.g. # Go to a page)
+// - [*] indicates an assertion (e.g. * Check the title)
+// - Use element ID when selecting an element. Create one if none.
+// ***************************************************************
+
+// Stage: @prod
+// Group: @account_setting
+import * as TIMEOUTS from '../../../fixtures/timeouts';
+
+describe('Account Settings > General > Position', () => {
+    let testTeam;
+    let testUser;
+
+    beforeEach(() => {
+        cy.apiAdminLogin();
+        cy.apiInitSetup().then(({team, user}) => {
+            testTeam = team;
+            testUser = user;
+        });
+    });
+
+    it('MM-T2063 Position / Can enter position', () => {
+        cy.apiLogin(testUser);
+
+        // # Visit town square
+        cy.visit(`/${testTeam.name}/channels/town-square`);
+
+        // # Open the hamburger menu
+        cy.findByLabelText('main menu').should('be.visible').click();
+
+        // # Click on Account settings menu item
+        cy.findByText('Account Settings').should('be.visible').click();
+
+        // # Fill-in the position field
+        cy.findByText('Position').should('be.visible').click();
+        cy.get('#position').type('Master hacker');
+        cy.get('#saveSetting').click();
+
+        // # Exit the modal
+        cy.get('body').type('{esc}', {force: true});
+
+        // # Post message in the main channel
+        cy.postMessage('hello from master hacker');
+
+        // # Click on the profile image
+        cy.get('.profile-icon > img').as('profileIconForPopover').click();
+
+        // # Verify that the popover is visible
+        cy.get('#user-profile-popover').should('be.visible').within(() => {
+            // * and contains the entered position
+            cy.findByText('Master hacker').should('be.visible');
+        });
+    });
+
+    it('MM-T2064 Position / 128 characters', () => {
+        const position = 'masterhackermasterhackermasterhackermasterhackermasterhackermasterhackermasterhackermasterhackermasterhackermasterhackermasterha';
+
+        cy.apiLogin(testUser);
+
+        // # Visit town square
+        cy.visit(`/${testTeam.name}/channels/town-square`);
+
+        // # Open the hamburger menu
+        cy.findByLabelText('main menu').should('be.visible').click();
+
+        // # Click on Account settings menu item
+        cy.findByText('Account Settings').should('be.visible').click();
+
+        // # Fill-in the position field with a value of 128 characters
+        cy.findByText('Position').should('be.visible').click();
+        cy.get('#position').type(position);
+        cy.get('#saveSetting').click();
+
+        cy.get('#position').invoke('val').then((val) => {
+            // # Verify that the input value is 128 characters
+            expect(val.length).to.equal(128);
+        });
+
+        // # Try to edit the field with maximum characters
+        cy.findByText('Position').click();
+        cy.wait(TIMEOUTS.ONE_SEC);
+
+        cy.get('#position').focus().type('random');
+        cy.get('#position').invoke('val').then((val) => {
+            // # Verify that the position hasn't changed
+            expect(val).to.equal(position);
+        });
+
+        // # Save position
+        cy.get('#saveSetting').click();
+
+        // # Exit the modal
+        cy.get('body').type('{esc}', {force: true});
+    });
+});
