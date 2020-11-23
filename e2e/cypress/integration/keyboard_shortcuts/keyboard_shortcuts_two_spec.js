dfce9a0b8bd2f6e9ceebde41f1cadf07fbc150f1
Catalin Tomai <catalin.tomai@mattermost.com>
2020-11-22 23:33:58 -0800

    further iteration

--- e2e/cypress/integration/keyboard_shortcuts/keyboard_shortcuts_two_spec.js
+++ e2e/cypress/integration/keyboard_shortcuts/keyboard_shortcuts_two_spec.js
@@ -9,8 +9,6 @@
 
 // Group: @keyboard_shortcuts
 
-/* eslint no-constant-condition: 2 */
-
 import * as TIMEOUTS from '../../fixtures/timeouts';
 
 describe('Keyboard Shortcuts', () => {
@@ -18,10 +16,8 @@ describe('Keyboard Shortcuts', () => {
     let testChannel;
     let testUser;
     let otherUser;
-    let thirdUser;
 
     before(() => {
-        // # Login as admin and visit town-square
         cy.apiInitSetup().then(({team, channel, user}) => {
             testTeam = team;
             testChannel = channel;
@@ -34,28 +30,23 @@ describe('Keyboard Shortcuts', () => {
                     cy.apiAddUserToChannel(testChannel.id, otherUser.id);
                 });
             });
-
-            cy.apiCreateUser({prefix: 'third'}).then(({user: user1}) => {
-                thirdUser = user1;
-
-                cy.apiAddUserToTeam(testTeam.id, thirdUser.id).then(() => {
-                    cy.apiAddUserToChannel(testChannel.id, thirdUser.id);
-                });
-            });
-
-            cy.visit(`/${team.name}/channels/town-square`);
         });
     });
 
     beforeEach(() => {
+        // # Login as admin and visit town-square
         cy.apiAdminLogin();
+        cy.visit(`/${testTeam.name}/channels/town-square`);
     });
 
     it('MM-T1239 - CTRL+/ and CMD+/ and /shortcuts', () => {
         // # Type CTRL/CMD+/
         cy.get('#post_textbox').cmdOrCtrlShortcut('/');
+
+        // # Verify that the 'Keyboard Shortcuts' modal is open
         cy.get('#shortcutsModalLabel').should('be.visible');
 
+        // # Verify that the 'Keyboard Shortcuts' modal displays ths CTRL+U shortcut
         cy.get('.section').eq(2).within(() => {
             cy.findByText('Files').should('be.visible');
             cy.get('.shortcut-line').within(() => {
@@ -68,154 +59,165 @@ describe('Keyboard Shortcuts', () => {
             });
         });
 
+        // # Type CTRL/CMD+/ to close the 'Keyboard Shortcuts' modal
         cy.get('body').cmdOrCtrlShortcut('/');
         cy.get('#shortcutsModalLabel').should('not.be.visible');
 
         // # Type /shortcuts
-        cy.get('#post_textbox').type('/shortcuts{enter}');
+        cy.get('#post_textbox').clear().type('/shortcuts{enter}');
         cy.get('#shortcutsModalLabel').should('be.visible');
 
-        // # Close the modal using the x
+        // # Close the 'Keyboard Shortcuts' modal using the x
         cy.get('.modal-header button.close').should('have.attr', 'aria-label', 'Close').click();
         cy.get('#shortcutsModalLabel').should('not.be.visible');
 
         // # Type /shortcuts
-        cy.get('#post_textbox').type('/shortcuts{enter}');
+        cy.get('#post_textbox').clear().type('/shortcuts{enter}');
 
-        // # Close the modal by pressing ESC
+        // # Close the 'Keyboard Shortcuts' modal by pressing ESC
         cy.get('body').type('{esc}');
         cy.get('#shortcutsModalLabel').should('not.be.visible');
     });
 
     it('MM-T1254 - CTRL/CMD+UP; CTRL/CMD+DOWN', () => {
         let message;
+        const count = 5;
 
-        // post 5 messages
-        for (let index = 0; index < 5; index++) {
-            // # Post Message as Current user
+        // # Post 5 messages
+        for (let index = 0; index < count; index++) {
             message = 'hello from current user: ' + index;
             cy.postMessage(message);
         }
 
-        for (let index = 0; index < 5; index++) {
-            // # Type CTRL/CMD+UP
+        for (let index = 0; index < count; index++) {
+            // # Type CTRL/CMD + UP
             cy.get('#post_textbox').cmdOrCtrlShortcut('{uparrow}');
             message = 'hello from current user: ' + (4 - index);
             cy.get('#post_textbox').contains(message);
         }
 
-        // # One exta CTRL/CMD+UP does not change the displayed message
+        // # One exta CTRL/CMD + UP does not change the displayed message
         cy.get('#post_textbox').cmdOrCtrlShortcut('{uparrow}');
         message = 'hello from current user: 0';
         cy.get('#post_textbox').contains(message);
 
-        for (let index = 0; index < 4; index++) {
-            // # Type CTRL/CMD+UP
+        for (let index = 1; index < count; index++) {
+            // # Type CTRL/CMD + DOWN
             cy.get('#post_textbox').cmdOrCtrlShortcut('{downarrow}');
-            message = 'hello from current user: ' + (index + 1);
+            message = 'hello from current user: ' + index;
             cy.get('#post_textbox').contains(message);
         }
     });
 
-    //CANNOT BE DONE(?)
-    it('MM-T1258 - CTRL/CMD+U', () => {
-        // # Type CTRL/CMD+U
-        cy.get('body').cmdOrCtrlShortcut('U');
-        cy.get('input').attachFile('mattermost-icon.png');
+    // // CANNOT BE DONE(?)
+    // it('MM-T1258 - CTRL/CMD+U', () => {
+    //     // # Type CTRL/CMD+U
+    //     cy.get('body').cmdOrCtrlShortcut('U');
+    //     cy.get('input').attachFile('mattermost-icon.png');
 
-        cy.getLastPostId().then((postId) => {
-            cy.get(`#${postId}_message`).findByTestId('fileAttachmentList').children().should('have.length', '1');
-        });
-    });
+    //     cy.getLastPostId().then((postId) => {
+    //         cy.get(`#${postId}_message`).findByTestId('fileAttachmentList').children().should('have.length', '1');
+    //     });
+    // });
 
-    it('MM-T1259 - BROWSER BACK: ALT+LEFT or RIGHT / CMD+[ or ]', () => {
-        // # Visit initially the 'Off Topic' channel
-        cy.visit(`/${testTeam.name}/channels/off-topic`);
+    // // CANNOT BE DONE(?)
+    // it('MM-T1259 - BROWSER BACK: ALT+LEFT or RIGHT / CMD+[ or ]', () => {
+    //     // # Visit initially the 'Off Topic' channel
+    //     cy.visit(`/${testTeam.name}/channels/off-topic`);
 
-        // # Navigate to the 'Town Square' channel
-        cy.visit(`/${testTeam.name}/channels/town-square`);
+    //     // # Navigate to the 'Town Square' channel
+    //     cy.visit(`/${testTeam.name}/channels/town-square`);
 
-        //cy.get('#sidebarItem_town-square').scrollIntoView().click();
-        cy.get('#channelHeaderTitle').should('be.visible').should('contain', 'Town Square');
-        cy.get('#headerInfo button').focus();
+    //     //cy.get('#sidebarItem_town-square').scrollIntoView().click();
+    //     cy.get('#channelHeaderTitle').should('be.visible').should('contain', 'Town Square');
+    //     cy.get('#headerInfo button').focus();
 
-        // # Type CTRL/CMD+[ (Mac) or ALT+LEFT (Windows)
-        if (cy.isMac()) {
-            cy.get('#post_textbox').type('{cmd}', {release: false, delay: 1000}).type('[', {delay: 1000}).type('{cmd}', {release: true}).wait(TIMEOUTS.HALF_SEC);
-        } else {
-            cy.get('#post_textbox').type('{alt}', {release: false, delay: 1000}).type('{leftarrow}').type('{alt}', {release: true}).wait(TIMEOUTS.HALF_SEC);
-        }
+    //     // # Type CTRL/CMD+[ (Mac) or ALT+LEFT (Windows)
+    //     if (cy.isMac()) {
+    //         cy.get('#post_textbox').type('{cmd}', {release: false, delay: 1000}).type('[', {delay: 1000}).type('{cmd}', {release: true}).wait(TIMEOUTS.HALF_SEC);
+    //     } else {
+    //         cy.get('#post_textbox').type('{alt}', {release: false, delay: 1000}).type('{leftarrow}').type('{alt}', {release: true}).wait(TIMEOUTS.HALF_SEC);
+    //     }
 
-        // # Verify that we are back to 'Off Topic' channel
-        cy.get('#channelHeaderTitle').should('be.visible').should('contain', 'Off-Topic');
-        cy.get('#headerInfo button').focus();
+    //     // # Verify that we are back to 'Off Topic' channel
+    //     cy.get('#channelHeaderTitle').should('be.visible').should('contain', 'Off-Topic');
+    //     cy.get('#headerInfo button').focus();
 
-        // # Type CTRL/CMD+] (Mac) or ALT+RIGHT (Windows)
-        if (cy.isMac()) {
-            cy.get('#post_textbox').type('{cmd}', {release: false}).type(']').type('{cmd}', {release: true}).wait(TIMEOUTS.HALF_SEC);
-        } else {
-            cy.get('#post_textbox').type('{alt}', {release: false}).type('{rightarrow}').type('{alt}', {release: true}).wait(TIMEOUTS.HALF_SEC);
-        }
+    //     // # Type CTRL/CMD+] (Mac) or ALT+RIGHT (Windows)
+    //     if (cy.isMac()) {
+    //         cy.get('#post_textbox').type('{cmd}', {release: false}).type(']').type('{cmd}', {release: true}).wait(TIMEOUTS.HALF_SEC);
+    //     } else {
+    //         cy.get('#post_textbox').type('{alt}', {release: false}).type('{rightarrow}').type('{alt}', {release: true}).wait(TIMEOUTS.HALF_SEC);
+    //     }
 
-        // # Verify that we are back to 'Town Square' channel
-        cy.get('#channelHeaderTitle').should('be.visible').should('contain', 'Town Square');
-    });
+    //     // # Verify that we are back to 'Town Square' channel
+    //     cy.get('#channelHeaderTitle').should('be.visible').should('contain', 'Town Square');
+    // });
 
     it('MM-T1260 - UP arrow', () => {
         const message = 'Test';
         const message1 = 'Reply';
 
-        // # Post message
-        cy.get('#post_textbox').type(message).type('{enter}').wait(TIMEOUTS.HALF_SEC);
+        // # Post message text
+        cy.get('#post_textbox').clear().type(message).type('{enter}').wait(TIMEOUTS.HALF_SEC);
 
-        // # [3] Edit previous post
+        // # Edit previous post
         cy.getLastPostId().then(() => {
             cy.get('#post_textbox').type('{uparrow}');
 
             // * Edit post modal should appear
             cy.get('#editPostModal').should('be.visible');
 
-            // * Update the post message and type ENTER
+            // * Reply to the post message and type ENTER
             cy.get('#edit_textbox').invoke('val', '').type(message1).type('{enter}').wait(TIMEOUTS.HALF_SEC);
         });
     });
 
     it('MM-T1273 - @[character]+TAB', () => {
         const userName = `@${testUser.username}`;
-        cy.get('#post_textbox').
-            as('input').
-            should('be.visible').
-            clear().
-            type(userName.substring(0, 5));
 
+        // # Enter the first characters of a user name
+        cy.get('#post_textbox').should('be.visible').clear().type(userName.substring(0, 5)).wait(TIMEOUTS.HALF_SEC);
+
+        // # Select the selected user from the list using TAB
         cy.get('#suggestionList').should('be.visible').within(() => {
             cy.focused().tab();
         });
 
+        // # Verify that the correct selection has been made
         cy.get('#post_textbox').should('be.visible').should('contain', `${testUser.username}`);
+
+        // # Clear the message box
+        cy.get('#post_textbox').clear();
     });
 
     it('MM-T1274 - :[character]+TAB', () => {
         const emojiName = ':tomato';
 
-        cy.get('#post_textbox').
-            as('input').
-            should('be.visible').
-            clear().
-            type(emojiName.substring(0, 3));
+        // # Enter the first characters of an emoji name
+        cy.get('#post_textbox').should('be.visible').clear().type(emojiName.substring(0, 3)).wait(TIMEOUTS.HALF_SEC);
 
-        cy.get('body').type('{downarrow}{downarrow}{downarrow}{downarrow}');
+        // # Go down the list of emojis
+        cy.get('body').type('{downarrow}').wait(TIMEOUTS.HALF_SEC);
+        cy.get('body').type('{downarrow}').wait(TIMEOUTS.HALF_SEC);
+        cy.get('body').type('{downarrow}').wait(TIMEOUTS.HALF_SEC);
+        cy.get('body').type('{downarrow}').wait(TIMEOUTS.HALF_SEC);
 
+        // # Select the fourth emoji from the top using TAB
         cy.get('#suggestionList').should('be.visible').within(() => {
             cy.focused().tab();
         });
 
+        // # Verify that the correct selection has been made
         cy.get('#post_textbox').should('be.visible').should('contain', emojiName);
     });
 
     it('MM-T1275 - SHIFT+UP', () => {
         const message = `hello${Date.now()}`;
+
+        // # Post message
         cy.postMessage(message);
+
         cy.get('#post_textbox').type('{shift}{uparrow}');
         cy.get('#reply_textbox').type('{shift}{uparrow}').should('be.focused');
 
@@ -266,13 +268,13 @@ describe('Keyboard Shortcuts', () => {
         }
 
         for (let index = 0; index < count; index++) {
-            // * Verify that we've switched to the right team
+            // # Verify that we've switched to the correct team
             cy.get('#headerTeamName').should('contain', teamDisplayNames[count - index - 1]);
 
-            // * Verify that we've switched to the right channel
+            // # Verify that we've switched to the correct channel
             cy.get('#channelHeaderTitle').should('be.visible').should('contain', channelDisplayNames[count - index - 1]);
 
-            // # Press alt + shift + up
+            // # Press ctrl + shift + up
             if (cy.isMac()) {
                 cy.get('body').type('{cmd}{option}', {release: false}).type('{uparrow}').type('{cmd}{option}', {release: true});
             } else {
