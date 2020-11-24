e58011d7b4b33a0cbabdb5ac5f85e5272a2f678f
dizkek <41262168+dizkek@users.noreply.github.com>
2020-11-25 00:55:03 +0900

    [MM_20423]'external_image'module to ts (#16151) (#7025)
    - migrate ts index.js file
    - migrate ts for external_image.tsx
    - migrate ts for external_images.test.jsx
    
    Co-authored-by: Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>

--- components/external_image/external_image.tsx
+++ components/external_image/external_image.tsx
@@ -1,20 +1,21 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import PropTypes from 'prop-types';
 import React from 'react';
+
+import {PostImage} from 'mattermost-redux/types/posts';
 
 import {getImageSrc} from 'utils/post_utils.jsx';
 
-export default class ExternalImage extends React.PureComponent {
-    static propTypes = {
-        children: PropTypes.func.isRequired,
-        enableSVGs: PropTypes.bool.isRequired,
-        hasImageProxy: PropTypes.bool.isRequired,
-        imageMetadata: PropTypes.object,
-        src: PropTypes.string.isRequired,
-    };
+interface Props {
+    children: (src : string) => React.ReactNode;
+    enableSVGs: boolean;
+    hasImageProxy: boolean;
+    imageMetadata?: PostImage,
+    src: string;
+}
 
+export default class ExternalImage extends React.PureComponent<Props> {
     isSVGImage = () => {
         if (!this.props.imageMetadata) {
             // Just check if the string contains an svg extension instead of if it ends with one because it avoids
