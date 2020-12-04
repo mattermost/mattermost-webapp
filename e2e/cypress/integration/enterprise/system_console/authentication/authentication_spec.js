49d794aaa35f778044a75f8d0884667c292478b6
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2020-12-04 10:59:16 -0500

    authentication file in enterprise

---
+++ e2e/cypress/integration/enterprise/system_console/authentication/authentication_spec.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+// ***************************************************************
+// - [#] indicates a test step (e.g. # Go to a page)
+// - [*] indicates an assertion (e.g. * Check the title)
+// - Use element ID when selecting an element. Create one if none.
+// ***************************************************************
+
+// Group: @enterprise @system_console @authentication
