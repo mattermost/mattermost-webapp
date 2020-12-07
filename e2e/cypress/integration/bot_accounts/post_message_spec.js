ef1ab0c5e1504ae9496e1ea42b276a6a754812bf
Caleb Roseland <caleb@calebroseland.com>
2020-12-07 02:07:20 -0600

    add MM-T1812 and MM-T1813

---
+++ e2e/cypress/integration/bot_accounts/post_message_spec.js
@@ -0,0 +1,119 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+// ***************************************************************
+// - [#] indicates a test step (e.g. # Go to a page)
+// - [*] indicates an assertion (e.g. * Check the title)
+// - Use element ID when selecting an element. Create one if none.
+// ***************************************************************
+
+// Group: @bot_accounts
+
+import {getAdminAccount} from '../../support/env';
+
+describe('Bot Post Message', () => {
+    let townsquareChannel;
+
+    before(() => {
+        // # Set ServiceSettings to expected values
+        const newSettings = {
+            ServiceSettings: {
+                EnableBotAccountCreation: true,
+                EnableUserAccessTokens: false,
+            },
+        };
+        cy.apiUpdateConfig(newSettings);
+
+        cy.apiInitSetup().then(({team}) => {
+            cy.apiGetChannelByName(team.name, 'town-square').then(({channel}) => {
+                townsquareChannel = channel;
+            });
+            cy.visit(`/${team.name}/channels/town-square`);
+        });
+    });
+
+    it('MM-T1812 Post as a bot when personal access tokens are false', () => {
+        // # Create a bot and get bot user id
+        cy.apiCreateBot().then(({bot}) => {
+            const botUserId = bot.user_id;
+            const message = 'This is a message from a bot.';
+
+            // # Get token from bot's id
+            cy.apiAccessToken(botUserId, 'Create token').then(({token}) => {
+                //# Add bot to team
+                cy.apiAddUserToTeam(townsquareChannel.team_id, botUserId);
+
+                // # Post message as bot through api with auth token
+                const props = {attachments: [{pretext: 'Some Pretext', text: 'Some Text'}]};
+                cy.postBotMessage({token, message, props, channelId: townsquareChannel.id}).
+                    its('id').
+                    should('exist').
+                    as('botPost');
+
+                // # Go to the channel
+                cy.get('#sidebarItem_town-square').click({force: true});
+
+                // * Verify bot message
+                cy.get('@botPost').then((postId) => {
+                    cy.get(`#postMessageText_${postId}`).
+                        should('be.visible').
+                        and('have.text', message);
+                });
+            });
+        });
+    });
+
+    it('MM-T1813 Display name for bots stays current', () => {
+        cy.makeClient({user: getAdminAccount()}).then((client) => {
+        // # Create a bot and get bot user id
+            cy.apiCreateBot().then(({bot}) => {
+                const botUserId = bot.user_id;
+                const firstMessage = 'This is the first message from a bot that will change its name';
+                const secondMessage = 'This is the second message from a bot that has changed its name';
+
+                // # Get token from bot's id
+                cy.apiAccessToken(botUserId, 'Create token').then(({token}) => {
+                    //# Add bot to team
+                    cy.apiAddUserToTeam(townsquareChannel.team_id, botUserId);
+
+                    // # Post message as bot through api with auth token
+                    const props = {attachments: [{pretext: 'Some Pretext', text: 'Some Text'}]};
+                    cy.postBotMessage({token, message: firstMessage, props, channelId: townsquareChannel.id}).
+                        its('id').
+                        should('exist').
+                        as('botPost');
+
+                    // # Go to the channel
+                    cy.get('#sidebarItem_town-square').click({force: true});
+
+                    // * Verify bot displayname
+                    cy.get('@botPost').then((postIdA) => {
+                        cy.get(`#post_${postIdA} button.user-popover`).click();
+
+                        cy.get('#user-profile-popover').
+                            should('be.visible');
+
+                        cy.findByTestId(`popover-fullname-${bot.username}`).
+                            should('have.text', bot.display_name);
+                    }).then(() => {
+                        cy.wrap(client.patchBot(bot.user_id, {display_name: `NEW ${bot.display_name}`})).then((newBot) => {
+                            cy.postBotMessage({token, message: secondMessage, props, channelId: townsquareChannel.id}).
+                                its('id').
+                                should('exist').
+                                as('newBotPost');
+
+                            // * Verify changed bot displayname
+                            cy.get('@newBotPost').then(() => {
+                                cy.get('#user-profile-popover').
+                                    should('be.visible');
+
+                                cy.findByTestId(`popover-fullname-${bot.username}`).
+                                    should('have.text', newBot.display_name);
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
