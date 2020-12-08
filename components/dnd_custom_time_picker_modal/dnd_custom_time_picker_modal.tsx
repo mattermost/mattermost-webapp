20707cd021d8d41726fbd9fe8b4629f18257d3c1
Umang Parmar <umangjparmar@gmail.com>
2020-12-08 15:23:17 +0530

    simplify generating time dropdown

--- components/dnd_custom_time_picker_modal/dnd_custom_time_picker_modal.tsx
+++ components/dnd_custom_time_picker_modal/dnd_custom_time_picker_modal.tsx
@@ -77,10 +77,6 @@ export default class DndCustomTimePicker extends React.PureComponent<Props, Stat
         };
     }
 
-    handleDayClick = (day: Date) => {
-        const dayString = day.toISOString().split('T')[0];
-    }
-
     render() {
         const {
             modalHeaderText,
@@ -94,19 +90,20 @@ export default class DndCustomTimePicker extends React.PureComponent<Props, Stat
             };
         }
 
-        const times = ['00:00', '00:30', '01:00', '01:30', '02:00', '02:30', '03:00', '03:30', '04:00', '04:30', '05:00',
-            '05:30', '06:00', '06:30', '07:00', '07:30', '08:00', '08:30', '09:00', '09:30', '10:00', '10:30',
-            '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00',
-            '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30',
-            '22:00', '22:30', '23:00', '23:30',
-        ];
+        let dayString;
+        const handleDayClick = (day: Date) => {
+            dayString = day.toISOString().split('T')[0];
+        };
 
         const timeMenuItems = [];
 
-        for (let i = 0; i < times.length; i++) {
-            timeMenuItems.push(
-                <MenuItem eventKey={i}> {times[i]} </MenuItem>,
-            );
+        for (let i = 0; i < 24; i++) {
+            for (let j = 0; j < 2; j++) {
+                const t = i.toString().padStart(2, '0') + ':' + (j * 30).toString().padStart(2, '0');
+                timeMenuItems.push(
+                    <MenuItem eventKey={i}> {t} </MenuItem>,
+                );
+            }
         }
 
         return (
@@ -128,7 +125,7 @@ export default class DndCustomTimePicker extends React.PureComponent<Props, Stat
                             className={'dnd-pickers-dropdown-menu'}
                         >
                             <DayPicker
-                                onDayClick={this.handleDayClick}
+                                onDayClick={handleDayClick}
                                 showOutsideDays={true}
                                 modifiers={modifiers}
                             />
