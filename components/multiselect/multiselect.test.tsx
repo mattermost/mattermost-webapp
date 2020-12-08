c00b96d638a42a94e1560284139d1643b980b510
Asaad Mahmood <asaadmahmood@users.noreply.github.com>
2020-12-08 16:25:09 +0500

    Updating test

--- components/multiselect/multiselect.test.tsx
+++ components/multiselect/multiselect.test.tsx
@@ -55,7 +55,6 @@ describe('components/multiselect/multiselect', () => {
         expect(wrapper).toMatchSnapshot();
     });
 
-
     test('MultiSelectList should match state on next page', () => {
         const renderOption: MultiSelectProps<Value>['optionRenderer'] = (option, isSelected, onAdd, onMouseMove) => {
             return (
