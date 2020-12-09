94666d54bb2c0ebeab48d268ae4b0eb53718658d
Agniva De Sarker <agnivade@yahoo.co.in>
2020-12-09 11:00:27 +0530

    incorporate review comments

--- e2e/cypress/integration/enterprise/system_console/authentication_method_spec.js
+++ e2e/cypress/integration/enterprise/system_console/authentication_method_spec.js
@@ -7,11 +7,10 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-// Stage: @prod
-// Group: @system_console
+// Group: @enterpise @system_console @mfa
 
-import ldapUsers from '../../fixtures/ldap_users.json';
-import * as TIMEOUTS from '../../fixtures/timeouts';
+import ldapUsers from '../../../fixtures/ldap_users.json';
+import * as TIMEOUTS from '../../../fixtures/timeouts';
 
 const authenticator = require('authenticator');
 
@@ -116,105 +115,6 @@ describe('Settings', () => {
                 should('be.visible').
                 and('contain.text', 'MFA: Yes');
         });
-    });
-
-    it('MM-T1149: Hide mobile-specific settings', () => {
-        // * Ensure license
-        cy.apiRequireLicense();
-
-        // # Visit license page
-        cy.visit('/admin_console/about/license');
-
-        // # Remove license
-        cy.get('#remove-button').click();
-
-        cy.visit('/admin_console/site_config/file_sharing_downloads');
-
-        // * Check buttons
-        cy.get('#adminConsoleWrapper .wrapper--fixed > .admin-console__wrapper').
-            should('be.visible').
-            and('contain.text', 'Allow File Sharing');
-
-        // Bring back license
-        cy.apiRequireLicense();
-    });
-
-    it('MM-T1161: Data retention - Settings are saved', () => {
-        cy.visit('/admin_console/compliance/data_retention');
-
-        // # Change dropdown
-        cy.findByTestId('enableMessageDeletiondropdown').select('Keep messages for a set amount of time');
-
-        // * Verify that button is enabled
-        cy.get('#adminConsoleWrapper .wrapper--fixed > .admin-console__wrapper').
-            within(() => {
-                cy.get('.job-table__panel button').should('be.enabled');
-            });
-
-        // # Save setting
-        cy.findByTestId('saveSetting').should('be.enabled').click().wait(TIMEOUTS.HALF_SEC);
-
-        // * Confirm that modal shows up
-        cy.get('#confirmModalLabel').should('be.visible').should('have.text', 'Confirm data retention policy');
-        cy.get('#confirmModalButton').should('be.enabled').click();
-
-        // # Change dropdown
-        cy.findByTestId('enableMessageDeletiondropdown').select('Keep all messages indefinitely');
-
-        // * Verify that button is disabled
-        cy.get('#adminConsoleWrapper .wrapper--fixed > .admin-console__wrapper').
-            within(() => {
-                cy.get('.job-table__panel button').should('be.disabled');
-            });
-
-        cy.findByTestId('saveSetting').should('be.enabled').click().wait(TIMEOUTS.HALF_SEC);
-
-        // * Confirm that modal shows up
-        cy.get('#confirmModalLabel').should('be.visible').should('have.text', 'Confirm data retention policy');
-        cy.get('#confirmModalButton').should('be.enabled').click();
-    });
-
-    it('MM-T1181: Compliance and Auditing: Run a report, it appears in the job table', () => {
-        cy.visit('/admin_console/compliance/monitoring');
-
-        // # Enable compliance reporting
-        cy.findByTestId('ComplianceSettings.Enabletrue').click();
-
-        cy.findByTestId('saveSetting').should('be.enabled').click().wait(TIMEOUTS.HALF_SEC);
-
-        // # Fill up the boxes
-        cy.get('#desc').clear().type('sample report');
-        const now = new Date();
-        cy.get('#to').clear().type(now.toLocaleDateString());
-        now.setDate(now.getDate() - 1);
-        cy.get('#from').clear().type(now.toLocaleDateString());
-
-        // # Run compliance reports
-        cy.get('#run-button').click().wait(TIMEOUTS.HALF_SEC);
-
-        cy.findByText('Reload Completed Compliance Reports').click().wait(TIMEOUTS.HALF_SEC);
-
-        // * Ensure that reports appear
-        cy.get('.compliance-panel__table tbody').children().should('have.length.greaterThan', 0);
-
-        // * Ensure that the report is correct
-        cy.get('.compliance-panel__table tbody tr').first().should('contain.text', 'Download');
-        cy.get('.compliance-panel__table tbody tr').first().should('contain.text', 'sample report');
-    });
-
-    it('MM-T1635: Channel listing is displayed correctly with proper team name', () => {
-        cy.visit('/admin_console/user_management/channels').wait(TIMEOUTS.FIVE_SEC);
-
-        // # Get the team name
-        cy.get('#channels .DataGrid .DataGrid_rows > :nth-child(1) > :nth-child(2)').
-            invoke('text').
-            then((name) => {
-                // # Click on the channel
-                cy.get('#channels .DataGrid .DataGrid_rows .DataGrid_row').first().click();
-
-                // * Confirm that the team name is same
-                cy.get('#channel_profile .channel-team').should('have.text', 'Team' + name);
-            });
     });
 });
 
