13069b29b64c04399cf9e21dc3a499ba828a2aa5
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2020-12-14 12:11:12 -0500

    fix linting

--- e2e/cypress/integration/enterprise/system_console/authentication/authentication3_spec.js
+++ e2e/cypress/integration/enterprise/system_console/authentication/authentication3_spec.js
@@ -11,22 +11,15 @@
 
 import * as TIMEOUTS from '../../../../fixtures/timeouts';
 
-import {getEmailUrl, getRandomId} from '../../../../utils';
+import {getRandomId} from '../../../../utils';
 
 describe('Authentication Part 3', () => {
     let testUser;
-    let testUserAlreadyInTeam;
-    let testTeam;
 
     before(() => {
         cy.apiRequireLicense();
 
-        // # Do email test if setup properly
-        cy.apiEmailTest();
-
-        cy.apiInitSetup().then(({user, team}) => {
-            testUserAlreadyInTeam = user;
-            testTeam = team;
+        cy.apiInitSetup().then(() => {
             cy.apiCreateUser().then(({user: newUser}) => {
                 testUser = newUser;
             });
@@ -39,6 +32,9 @@ describe('Authentication Part 3', () => {
     });
 
     after(() => {
+        // # Logout as any current user
+        cy.apiLogout();
+
         // # Log in as a admin.
         cy.apiAdminLogin({failOnStatusCode: false});
 
@@ -50,88 +46,120 @@ describe('Authentication Part 3', () => {
         });
     });
 
-    // it('MM-T1767 - Email signin false Username signin true', () => {
-    //     cy.apiUpdateConfig({
-    //         EmailSettings: {
-    //             EnableSignInWithEmail: false,
-    //             EnableSignInWithUsername: true,
-    //         },
-    //         LdapSettings: {
-    //             Enable: false
-    //         },
-    //     });
+    it('MM-T1767 - Email signin false Username signin true', () => {
+        cy.apiUpdateConfig({
+            EmailSettings: {
+                EnableSignInWithEmail: false,
+                EnableSignInWithUsername: true,
+            },
+            LdapSettings: {
+                Enable: false,
+            },
+        });
 
-    //     cy.apiLogout();
+        cy.apiLogout();
 
-    //     // # Go to front page
-    //     cy.visit('/login');
+        // # Go to front page
+        cy.visit('/login');
 
-    //     // * Make sure input section for username contains username and not email
-    //     cy.findByTestId('loginId').invoke('attr', 'placeholder').should('contain', 'Username').and('not.contain', 'Email');
-    // });
+        // * Make sure input section for username contains username and not email
+        cy.findByTestId('loginId').invoke('attr', 'placeholder').should('contain', 'Username').and('not.contain', 'Email');
+    });
 
-    // it('MM-T1768 - Email signin true Username signin true', () => {
-    //     cy.apiUpdateConfig({
-    //         EmailSettings: {
-    //             EnableSignInWithEmail: true,
-    //             EnableSignInWithUsername: true,
-    //         },
-    //         LdapSettings: {
-    //             Enable: false
-    //         },
-    //     });
+    it('MM-T1768 - Email signin true Username signin true', () => {
+        cy.apiUpdateConfig({
+            EmailSettings: {
+                EnableSignInWithEmail: true,
+                EnableSignInWithUsername: true,
+            },
+            LdapSettings: {
+                Enable: false,
+            },
+        });
 
-    //     cy.apiLogout();
+        cy.apiLogout();
 
-    //     // # Go to front page
-    //     cy.visit('/login');
+        // # Go to front page
+        cy.visit('/login');
 
-    //     // * Make sure input section for username contains username and not email
-    //     cy.findByTestId('loginId').invoke('attr', 'placeholder').should('contain', 'Username').and('contain', 'Email');
-    // });
+        // * Make sure input section for username contains username and not email
+        cy.findByTestId('loginId').invoke('attr', 'placeholder').should('contain', 'Username').and('contain', 'Email');
+    });
 
-    // it('MM-T1769 - Email signin true Username signin false', () => {
-    //     cy.apiUpdateConfig({
-    //         EmailSettings: {
-    //             EnableSignInWithEmail: true,
-    //             EnableSignInWithUsername: false,
-    //         },
-    //         LdapSettings: {
-    //             Enable: false
-    //         },
-    //     });
+    it('MM-T1769 - Email signin true Username signin false', () => {
+        cy.apiUpdateConfig({
+            EmailSettings: {
+                EnableSignInWithEmail: true,
+                EnableSignInWithUsername: false,
+            },
+            LdapSettings: {
+                Enable: false,
+            },
+        });
 
-    //     cy.apiLogout();
+        cy.apiLogout();
 
-    //     // # Go to front page
-    //     cy.visit('/login');
+        // # Go to front page
+        cy.visit('/login');
 
-    //     // * Make sure input section for username contains username and not email
-    //     cy.findByTestId('loginId').invoke('attr', 'placeholder').should('contain', 'Email').and('not.contain', 'Username');
-    // });
+        // * Make sure input section for username contains username and not email
+        cy.findByTestId('loginId').invoke('attr', 'placeholder').should('contain', 'Email').and('not.contain', 'Username');
+    });
 
-    // it('MM-T1771 - Minimum password length error field shows below 5 and above 64', () => {
-    //     cy.visit('/admin_console/authentication/password');
+    it('MM-T1771 - Minimum password length error field shows below 5 and above 64', () => {
+        cy.visit('/admin_console/authentication/password');
 
-    //     cy.findByPlaceholderText('E.g.: "5"').clear().type('88');
+        cy.findByPlaceholderText('E.g.: "5"').clear().type('88');
 
-    //     cy.findByText('Save').click();
+        cy.findByText('Save').click();
 
-    //     // * Ensure error appears when saving a password outside of the limits
-    //     cy.findByText('Minimum password length must be a whole number greater than or equal to 5 and less than or equal to 64.', {timeout: TIMEOUTS.ONE_MIN})
-    //     .should('exist')
-    //     .and('be.visible');
+        // * Ensure error appears when saving a password outside of the limits
+        cy.findByText('Minimum password length must be a whole number greater than or equal to 5 and less than or equal to 64.', {timeout: TIMEOUTS.ONE_MIN}).
+            should('exist').
+            and('be.visible');
 
-    //     cy.findByPlaceholderText('E.g.: "5"').clear().type('3');
+        cy.findByPlaceholderText('E.g.: "5"').clear().type('3');
 
-    //     cy.findByText('Save').click();
+        cy.findByText('Save').click();
 
-    //     // * Ensure error appears when saving a password outside of the limits
-    //     cy.findByText('Minimum password length must be a whole number greater than or equal to 5 and less than or equal to 64.', {timeout: TIMEOUTS.ONE_MIN})
-    //     .should('exist')
-    //     .and('be.visible');
-    // });
+        // * Ensure error appears when saving a password outside of the limits
+        cy.findByText('Minimum password length must be a whole number greater than or equal to 5 and less than or equal to 64.', {timeout: TIMEOUTS.ONE_MIN}).
+            should('exist').
+            and('be.visible');
+    });
 
+    it('MM-T1772', () => {
+        cy.visit('/admin_console/authentication/password');
+
+        cy.findByPlaceholderText('E.g.: "5"').clear().type('7');
+
+        cy.findByRole('button', {name: 'Save'}).click();
+
+        cy.findByText('Your password must contain between 7 and 64 characters.').should('be.visible').and('exist');
+
+        cy.apiLogout();
+
+        // # Go to sign up with email page
+        cy.visit('/signup_email');
+
+        cy.get('#email', {timeout: TIMEOUTS.ONE_MIN}).type(`Hossein_Is_The_Best_PROGRAMMER${getRandomId()}@BestInTheWorld.com`);
+
+        cy.get('#name').clear().type(`BestUsernameInTheWorld${getRandomId()}`);
+
+        cy.get('#password').clear().type('less');
+
+        cy.findByText('Create Account').click();
+
+        // * Assert the error is what is expected;
+        cy.findByText('Your password must contain between 7 and 64 characters.').should('be.visible');
+
+        cy.get('#password').clear().type('greaterthan7');
+
+        cy.findByText('Create Account').click();
+
+        // * Assert that we are not shown a MFA scren and instead a Teams You Can join page
+        cy.findByText('Teams you can join:', {timeout: TIMEOUTS.ONE_MIN}).should('be.visible');
+    });
 
     it('MM-T1773 - Minimum password length field resets to default after saving invalid value', () => {
         cy.visit('/admin_console/authentication/password');
@@ -152,4 +180,106 @@ describe('Authentication Part 3', () => {
         cy.findByPlaceholderText('E.g.: "5"').invoke('val').should('equal', '5');
     });
 
+    it('MM-T1774 - Select all Password Requirements, verify help text and error on bad password', () => {
+        cy.apiUpdateConfig({
+            PasswordSettings: {
+                Lowercase: true,
+                Number: true,
+                Uppercase: true,
+                Symbol: true,
+            },
+        });
+
+        cy.apiLogout();
+
+        // # Go to sign up with email page
+        cy.visit('/signup_email');
+
+        cy.get('#email', {timeout: TIMEOUTS.ONE_MIN}).type(`Hossein_Is_The_Best_PROGRAMMER${getRandomId()}@BestInTheWorld.com`);
+
+        cy.get('#name').clear().type(`BestUsernameInTheWorld${getRandomId()}`);
+
+        ['NOLOWERCASE123!', 'noupppercase123!', 'NoNumber!', 'NoSymbol123'].forEach((option) => {
+            cy.get('#password').clear().type(option);
+            cy.findByText('Create Account').click();
+
+            // * Assert the error is what is expected;
+            cy.findByText('Your password must contain between 5 and 64 characters made up of at least one lowercase letter, at least one uppercase letter, at least one number, and at least one symbol (e.g. "~!@#$%^&*()").').should('be.visible');
+        });
+    });
+
+    it('MM-T1775 - Maximum Login Attempts field resets to default after saving invalid value', () => {
+        cy.visit('/admin_console/authentication/password');
+
+        cy.findByPlaceholderText('E.g.: "10"').clear().type('ten');
+
+        cy.findByRole('button', {name: 'Save'}).click();
+
+        // * Ensure error appears when saving a password outside of the limits
+        cy.findByPlaceholderText('E.g.: "10"').invoke('val').should('equal', '10');
+    });
+
+    it('MM-T1776 - Maximum Login Attempts field successfully saves valid change', () => {
+        cy.visit('/admin_console/authentication/password');
+
+        cy.findByPlaceholderText('E.g.: "10"').clear().type('2');
+
+        cy.findByRole('button', {name: 'Save'}).click();
+
+        // * Ensure error appears when saving a password outside of the limits
+        cy.findByPlaceholderText('E.g.: "10"').invoke('val').should('equal', '2');
+    });
+
+    it('MM-T1777 - Multi-factor Authentication option hidden in Account Settings when disabled', () => {
+        cy.apiUpdateConfig({
+            ServiceSettings: {
+                EnableMultifactorAuthentication: false,
+            },
+        });
+
+        cy.visit('/');
+
+        cy.toAccountSettingsModal();
+
+        cy.findByLabelText('security').click();
+
+        // * Assert that Multifactor Authentication text does not exist
+        cy.findByText('Multi-factor Authentication').should('not.exist').and('not.be.visible');
+    });
+
+    it('MM-T1779 - Multi-factor Authentication option appears in Account Settings when enabled', () => {
+        cy.apiUpdateConfig({
+            ServiceSettings: {
+                EnableMultifactorAuthentication: true,
+            },
+        });
+
+        cy.visit('/');
+
+        cy.toAccountSettingsModal();
+
+        cy.findByLabelText('security').click();
+
+        // * Assert that Multifactor Authentication text does exist
+        cy.findByText('Multi-factor Authentication').should('exist').and('be.visible');
+    });
+
+    it('MM-T1780 - Multi-factor Authentication false: User can log in without being prompted for MFA', () => {
+        cy.apiUpdateConfig({
+            ServiceSettings: {
+                EnableMultifactorAuthentication: true,
+                EnforceMultifactorAuthentication: false,
+
+            },
+        });
+
+        cy.apiLogout();
+
+        cy.apiLogin(testUser);
+
+        cy.visit('/');
+
+        // * Assert that we are not shown a MFA scren and instead a Teams You Can join page
+        cy.findByText('Teams you can join:', {timeout: TIMEOUTS.ONE_MIN}).should('be.visible');
+    });
 });
