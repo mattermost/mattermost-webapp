6da4e487a4fcf82f5cafb965ba08359f25d631b7
Vitaliy Bratyshov <vitaliy.bratyshov@gmail.com>
2020-12-16 23:55:14 +0300

    removing spaces

--- components/legacy_sidebar/sidebar_channel_button_or_link/sidebar_channel_button_or_link_icon.jsx
+++ components/legacy_sidebar/sidebar_channel_button_or_link/sidebar_channel_button_or_link_icon.jsx
@@ -52,19 +52,19 @@ export default class SidebarChannelButtonOrLinkIcon extends React.PureComponent 
         let icon = null;
         if (this.props.channelIsArchived) {
             icon = (
-                <ArchiveIcon className='icon icon__archive' />
+                <ArchiveIcon className='icon icon__archive'/>
             );
         } else if (this.props.hasDraft) {
             icon = (
-                <DraftIcon className='icon icon__draft' />
+                <DraftIcon className='icon icon__draft'/>
             );
         } else if (this.props.channelType === Constants.OPEN_CHANNEL) {
             icon = (
-                <GlobeIcon className='icon icon__globe' />
+                <GlobeIcon className='icon icon__globe'/>
             );
         } else if (this.props.channelType === Constants.PRIVATE_CHANNEL) {
             icon = (
-                <LockIcon className='icon icon__lock' />
+                <LockIcon className='icon icon__lock'/>
             );
         } else if (this.props.channelType === Constants.GM_CHANNEL) {
             icon = <div className='status status--group'>{this.props.membersCount}</div>;
