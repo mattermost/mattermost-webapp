07e840458c5330923dd4e51c372bca9f6a9c46be
Nevyana Angelova <nevyangelova@Nevyanas-MacBook-Pro-2.local>
2020-12-17 14:58:14 +0200

    Add back comment

--- e2e/cypress/integration/notifications/desktop_notifications_spec.js
+++ e2e/cypress/integration/notifications/desktop_notifications_spec.js
@@ -1,5 +1,14 @@
+
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
+
+// ***************************************************************
+// - [#] indicates a test step (e.g. # Go to a page)
+// - [*] indicates an assertion (e.g. * Check the title)
+// - Use element ID when selecting an element. Create one if none.
+// ***************************************************************
+
+// Group: @notifications
 
 import * as MESSAGES from '../../fixtures/messages';
 import * as TIMEOUTS from '../../fixtures/timeouts';
