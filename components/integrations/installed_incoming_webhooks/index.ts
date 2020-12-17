10a95590e58a802f626df8e88f0b168d5b183e0e
niharikanagar <n.nagar12@gmail.com>
2020-12-16 20:04:13 -0500

    Revert mapStateToProps to original

--- components/integrations/installed_incoming_webhooks/index.ts
+++ components/integrations/installed_incoming_webhooks/index.ts
@@ -7,14 +7,13 @@ import {removeIncomingHook} from 'mattermost-redux/actions/integrations';
 
 import {getAllChannels} from 'mattermost-redux/selectors/entities/channels';
 import {getIncomingHooks} from 'mattermost-redux/selectors/entities/integrations';
-import {getCurrentTeam} from 'mattermost-redux/selectors/entities/teams';
-import {getUsers, getCurrentUser} from 'mattermost-redux/selectors/entities/users';
+import {getCurrentTeamId} from 'mattermost-redux/selectors/entities/teams';
+import {getUsers} from 'mattermost-redux/selectors/entities/users';
 import {GlobalState} from 'mattermost-redux/types/store';
 import {haveITeamPermission} from 'mattermost-redux/selectors/entities/roles';
 import {Permissions} from 'mattermost-redux/constants';
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
 import {ActionResult, GenericAction} from 'mattermost-redux/types/actions';
-import {IncomingWebhook} from 'mattermost-redux/types/integrations';
 
 import {loadIncomingHooksAndProfilesForTeam} from 'actions/integration_actions.jsx';
 
@@ -27,18 +26,16 @@ type Actions = {
 
 function mapStateToProps(state: GlobalState) {
     const config = getConfig(state);
-    const team = getCurrentTeam(state);
-    const user = getCurrentUser(state);
-    const canManageOthersWebhooks: boolean = haveITeamPermission(state, {team: team.id, permission: Permissions.MANAGE_OTHERS_INCOMING_WEBHOOKS});
+    const teamId = getCurrentTeamId(state);
+    const canManageOthersWebhooks = haveITeamPermission(state, {team: teamId, permission: Permissions.MANAGE_OTHERS_INCOMING_WEBHOOKS});
     const incomingHooks = getIncomingHooks(state);
-    const incomingWebhooks: IncomingWebhook[] = Object.keys(incomingHooks).
+    const incomingWebhooks = Object.keys(incomingHooks).
         map((key) => incomingHooks[key]).
-        filter((incomingWebhook) => incomingWebhook.team_id === team.id);
+        filter((incomingWebhook) => incomingWebhook.team_id === teamId);
     const enableIncomingWebhooks = config.EnableIncomingWebhooks === 'true';
 
     return {
-        team,
-        user,
+        teamId,
         incomingWebhooks,
         channels: getAllChannels(state),
         users: getUsers(state),
