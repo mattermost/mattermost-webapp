c5326573350424a641ea1daf46302006da3b47b8
lucievr <lucy.vrsovska@gmail.com>
2020-12-19 00:00:41 +0100

    Migrate to typescript installed_commands component wrapper

--- components/integrations/installed_commands/index.ts
+++ components/integrations/installed_commands/index.ts
@@ -2,14 +2,27 @@
 // See LICENSE.txt for license information.
 
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import {bindActionCreators, Dispatch, ActionCreatorsMapObject} from 'redux';
 import {deleteCommand, regenCommandToken} from 'mattermost-redux/actions/integrations';
 import {haveITeamPermission} from 'mattermost-redux/selectors/entities/roles';
 import {Permissions} from 'mattermost-redux/constants';
+import {GenericAction, ActionResult, ActionFunc} from 'mattermost-redux/types/actions';
+import {GlobalState} from 'mattermost-redux/types/store';
 
 import InstalledCommands from './installed_commands.jsx';
 
-function mapStateToProps(state, ownProps) {
+type Props = {
+    team: {
+        id: string;
+    };
+}
+
+type Actions = {
+    regenCommandToken: (id: string) => Promise<ActionResult>;
+    deleteCommand: (id: string) => Promise<ActionResult>;
+}
+
+function mapStateToProps(state: GlobalState, ownProps: Props) {
     const canManageOthersSlashCommands = haveITeamPermission(state, {team: ownProps.team.id, permission: Permissions.MANAGE_OTHERS_SLASH_COMMANDS});
 
     return {
@@ -17,9 +30,9 @@ function mapStateToProps(state, ownProps) {
     };
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
     return {
-        actions: bindActionCreators({
+        actions: bindActionCreators<ActionCreatorsMapObject<ActionFunc>, Actions>({
             regenCommandToken,
             deleteCommand,
         }, dispatch),
