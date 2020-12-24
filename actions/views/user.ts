12eed8dffbab36e94c7fe80980a23c7cb8deabbc
Jatin Sharma <jatin.sharma@brightscout.com>
2020-12-24 19:33:18 +0530

    add logic to unset custom status

--- actions/views/user.ts
+++ actions/views/user.ts
@@ -14,17 +14,23 @@ interface CustomStatus{
 export function updateUserCustomStatus(newCustomStatus: CustomStatus) {
     return async (dispatch: DispatchFunc, getState: GetStateFunc) => {
         const userProps = Object.assign({}, getCurrentUser(getState()).props);
-        const oldCustomStatus = userProps.customStatus ? JSON.parse(userProps.customStatus) : {};
         const recentCustomStatuses = userProps.recentCustomStatuses ? JSON.parse(userProps.recentCustomStatuses) : [];
-        if (oldCustomStatus) {
-            recentCustomStatuses.unshift(oldCustomStatus);
-        }
         const updatedRecentCustomStatuses = recentCustomStatuses.filter((status: CustomStatus) => status.text !== newCustomStatus.text);
+        updatedRecentCustomStatuses.unshift(newCustomStatus);
         if (updatedRecentCustomStatuses.length > 5) {
             updatedRecentCustomStatuses.pop();
         }
         userProps.customStatus = JSON.stringify(newCustomStatus);
         userProps.recentCustomStatuses = JSON.stringify(updatedRecentCustomStatuses);
+        const user = Object.assign({}, getCurrentUser(getState()), {props: userProps});
+        await dispatch(updateMe(user));
+    };
+}
+
+export function unsetUserCustomStatus() {
+    return async (dispatch: DispatchFunc, getState: GetStateFunc) => {
+        const userProps = Object.assign({}, getCurrentUser(getState()).props);
+        delete userProps.customStatus;
         const user = Object.assign({}, getCurrentUser(getState()), {props: userProps});
         await dispatch(updateMe(user));
     };
