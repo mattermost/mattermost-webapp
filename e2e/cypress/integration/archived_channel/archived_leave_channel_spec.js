38f0e08e4289c40933a80b580e554cd1cc3269f0
Guillermo Vayá <guillermo.vaya@mattermost.com>
2021-01-04 06:44:16 -0800

    [MM-27210] archived channels 4 II (#7205)
    * [MM-27210] search archive channels
    
    * move helper function

--- e2e/cypress/integration/archived_channel/archived_leave_channel_spec.js
+++ e2e/cypress/integration/archived_channel/archived_leave_channel_spec.js
@@ -13,6 +13,8 @@
 import {getRandomId} from '../../utils';
 import * as MESSAGES from '../../fixtures/messages';
 
+import {createArchivedChannel} from './helpers';
+
 describe('Leave an archived channel', () => {
     let testTeam;
     let testChannel;
@@ -328,26 +330,3 @@ describe('Leave an archived channel', () => {
     });
 });
 
-async function createArchivedChannel(channelOptions, messages, memberUsernames) {
-    const channelName = await new Promise((resolve) => {
-        cy.uiCreateChannel(channelOptions).then((newChannel) => {
-            if (memberUsernames) {
-                cy.uiAddUsersToCurrentChannel(memberUsernames);
-            }
-            if (messages) {
-                let messageList = messages;
-                if (!Array.isArray(messages)) {
-                    messageList = [messages];
-                }
-                messageList.forEach((message) => {
-                    cy.postMessage(message);
-                });
-            }
-            cy.uiArchiveChannel();
-            resolve(newChannel.name);
-        });
-    });
-
-    cy.get('#channelArchivedMessage').should('be.visible');
-    return cy.wrap({channelName});
-}
