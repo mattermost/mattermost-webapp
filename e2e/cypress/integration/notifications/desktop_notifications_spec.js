f146d5415e143fcc39b71d25aa299a3eb78394f4
Nick Misasi <nick.misasi@mattermost.com>
2021-01-11 13:43:32 -0500

    Reset 2 files to master

--- e2e/cypress/integration/notifications/desktop_notifications_spec.js
+++ e2e/cypress/integration/notifications/desktop_notifications_spec.js
@@ -8,7 +8,6 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-// Stage: @prod
 // Group: @notifications
 
 import * as MESSAGES from '../../fixtures/messages';
