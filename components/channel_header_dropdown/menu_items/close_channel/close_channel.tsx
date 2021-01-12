176653b8f8ee4d937e794a24703e383794c664e7
Pablo Velez Vidal <pablo.velez@mattermost.com>
2021-01-12 15:23:55 +0100

    MM-20437 - migrate close channels to typescript

--- components/channel_header_dropdown/menu_items/close_channel/close_channel.tsx
+++ components/channel_header_dropdown/menu_items/close_channel/close_channel.tsx
@@ -2,21 +2,20 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import {localizeMessage} from 'utils/utils';
 
 import Menu from 'components/widgets/menu/menu';
 
-export default class CloseChannel extends React.PureComponent {
-    static propTypes = {
-        isArchived: PropTypes.bool.isRequired,
-        actions: PropTypes.shape({
-            goToLastViewedChannel: PropTypes.func.isRequired,
-        }).isRequired,
-    }
+type Props = {
+    isArchived: boolean,
+    actions: {
+        goToLastViewedChannel: () => () => void,
+    },
+}
 
-    handleClose = () => {
+export default class CloseChannel extends React.PureComponent<Props> {
+    private handleClose = () => {
         this.props.actions.goToLastViewedChannel();
     }
 
--- components/channel_header_dropdown/menu_items/close_channel/index.ts
+++ components/channel_header_dropdown/menu_items/close_channel/index.ts
@@ -1,14 +1,17 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {bindActionCreators} from 'redux';
+import {bindActionCreators, Dispatch} from 'redux';
 import {connect} from 'react-redux';
 
 import {goToLastViewedChannel} from 'actions/views/channel';
 
+import {GenericAction} from 'mattermost-redux/types/actions';
+
+
 import CloseChannel from './close_channel';
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = (dispatch: Dispatch<GenericAction>) => ({
     actions: bindActionCreators({
         goToLastViewedChannel,
     }, dispatch),
