3beab88d5a28bc72274ae47f0c0bab157dcb2d89
Saturnino Abril <saturnino.abril@gmail.com>
2021-01-13 09:09:45 +0800

    fix Cypress tests (#7309)

--- e2e/cypress/support/api/user.js
+++ e2e/cypress/support/api/user.js
@@ -300,7 +300,7 @@ Cypress.Commands.add('apiActivateUser', (userId) => {
         },
     };
 
-    // # Deactivate a user account
+    // # Activate a user account
     return cy.request(options).then((response) => {
         expect(response.status).to.equal(200);
         return cy.wrap(response);
