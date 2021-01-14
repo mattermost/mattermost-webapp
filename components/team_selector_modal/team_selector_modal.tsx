f5fe256093f5de8c40eb2d751b5706f7c395eeff
Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
2021-01-14 11:18:58 -0500

    filtering out group constrained using prop

--- components/team_selector_modal/team_selector_modal.tsx
+++ components/team_selector_modal/team_selector_modal.tsx
@@ -26,6 +26,7 @@ type TeamValue = (Team & Value);
 export type Props = {
     currentSchemeId?: string;
     alreadySelected?: string[];
+    excludeGroupConstrained?: boolean;
     searchTerm: string;
     teams: Team[];
     onModalDismissed?: () => void;
@@ -246,6 +247,9 @@ export default class TeamSelectorModal extends React.PureComponent<Props, State>
         if (this.props.teams) {
             teams = this.props.teams.filter((team) => team.delete_at === 0);
             teams = teams.filter((team) => team.scheme_id !== this.currentSchemeId);
+            if (this.props.excludeGroupConstrained === true) {
+                teams = teams.filter((team) => team.group_constrained !== true);
+            }
             teams = teams.filter((team) => this.props.alreadySelected?.indexOf(team.id) === -1);
             teams.sort((a, b) => {
                 const aName = a.display_name.toUpperCase();
