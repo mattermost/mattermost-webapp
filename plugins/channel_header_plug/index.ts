313812c625745a46d2624e7fb12e9916a6a1fd5f
Daniel Espino García <larkox@gmail.com>
2021-01-14 14:28:49 +0100

    Fix missing channel header buttons

--- plugins/channel_header_plug/index.ts
+++ plugins/channel_header_plug/index.ts
@@ -2,8 +2,14 @@
 // See LICENSE.txt for license information.
 
 import {connect} from 'react-redux';
-import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
+import {bindActionCreators, Dispatch} from 'redux';
 
+import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
+import {getAppsBindings} from 'mattermost-redux/selectors/entities/apps';
+import {AppsBindings} from 'mattermost-redux/constants/apps';
+import {GenericAction} from 'mattermost-redux/types/actions';
+
+import {doAppCall} from 'actions/apps';
 import {GlobalState} from 'types/store';
 
 import ChannelHeaderPlug from './channel_header_plug';
@@ -11,8 +17,17 @@ import ChannelHeaderPlug from './channel_header_plug';
 function mapStateToProps(state: GlobalState) {
     return {
         components: state.plugins.components.ChannelHeaderButton || [],
+        appBindings: getAppsBindings(state, AppsBindings.CHANNEL_HEADER_ICON),
         theme: getTheme(state),
     };
 }
 
-export default connect(mapStateToProps)(ChannelHeaderPlug);
+function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
+    return {
+        actions: bindActionCreators({
+            doAppCall,
+        }, dispatch),
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChannelHeaderPlug);
