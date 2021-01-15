45fe7fd1212a87a1e613c7eed12f88fde3fce09b
Ben Schumacher <ben.schumacher@mattermost.com>
2021-01-15 09:15:30 +0100

    Specify props in index.ts

--- components/plugin_marketplace/marketplace_item/index.ts
+++ components/plugin_marketplace/marketplace_item/index.ts
@@ -2,7 +2,7 @@
 // See LICENSE.txt for license information.
 
 import {connect} from 'react-redux';
-import {bindActionCreators, Dispatch} from 'redux';
+import {ActionCreatorsMapObject, bindActionCreators, Dispatch} from 'redux';
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
 import {GlobalState} from 'mattermost-redux/types/store';
 import {GenericAction} from 'mattermost-redux/types/actions';
@@ -15,7 +15,11 @@ import {trackEvent} from 'actions/telemetry_actions.jsx';
 
 import MarketplaceItem, {MarketplaceItemProps} from './marketplace_item';
 
-function mapStateToProps(state: GlobalState, props: MarketplaceItemProps) {
+type Props = {
+    id: string;
+}
+
+function mapStateToProps(state: GlobalState, props: Props) {
     const installing = getInstalling(state, props.id);
     const error = getError(state, props.id);
     const isDefaultMarketplace = getConfig(state).IsDefaultMarketplace === 'true';
@@ -30,7 +34,7 @@ function mapStateToProps(state: GlobalState, props: MarketplaceItemProps) {
 
 function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
     return {
-        actions: bindActionCreators({
+        actions: bindActionCreators<ActionCreatorsMapObject, MarketplaceItemProps['actions']>({
             installPlugin,
             closeMarketplaceModal: () => closeModal(ModalIdentifiers.PLUGIN_MARKETPLACE),
         }, dispatch),
