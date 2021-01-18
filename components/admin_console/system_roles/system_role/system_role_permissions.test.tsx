e294b690161d23df59f301f0a30db50f5969478d
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2021-01-18 11:46:21 -0500

    add more test

--- components/admin_console/system_roles/system_role/system_role_permissions.test.tsx
+++ components/admin_console/system_roles/system_role/system_role_permissions.test.tsx
@@ -8,6 +8,7 @@ import {TestHelper} from 'utils/test_helper';
 import {readAccess, writeAccess} from './types';
 
 import SystemRolePermissions from './system_role_permissions';
+import SystemRolePermission from './system_role_permission';
 
 describe('admin_console/system_role_permissions', () => {
     const props = {
@@ -38,5 +39,22 @@ describe('admin_console/system_role_permissions', () => {
             />);
 
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test('ensure that when you change a prop and component is re-rendered, SystemRolePermission is not being deleted due to isLicensedForCloud being false (test for bug MM-31403)', () => {
+        const wrapper = shallow(
+            <SystemRolePermissions
+                {...props}
+            />);
+        
+        const expectedLength = 10;
+        let systemRolePermissionLength = wrapper.find(SystemRolePermission).length
+        expect(systemRolePermissionLength).toEqual(expectedLength);
+        wrapper.setProps({ permissionToUpdate: {
+            environment: writeAccess,
+            plugins: readAccess,
+        }});    
+        systemRolePermissionLength = wrapper.find(SystemRolePermission).length
+        expect(systemRolePermissionLength).toEqual(expectedLength);
     });
 });
