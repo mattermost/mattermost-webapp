59abdb21144e6ffb47da43222892f801badf585b
Joseph Baylon <joseph.baylon@mattermost.com>
2021-01-19 08:34:33 -0800

    Update e2e/cypress/integration/enterprise/guest_accounts/system_console_manage_guest_spec.js

--- e2e/cypress/integration/enterprise/guest_accounts/system_console_manage_guest_spec.js
+++ e2e/cypress/integration/enterprise/guest_accounts/system_console_manage_guest_spec.js
@@ -129,7 +129,7 @@ describe('Guest Account - Verify Manage Guest Users', () => {
         verifyGuest();
     });
 
-    it('MM-T1394 Change Email of a Guest User and Verify', () => {
+    it('MM-18048 Change Email of a Guest User and Verify', () => {
         // # Click on the Update Email option
         cy.wait(TIMEOUTS.HALF_SEC).findByTestId('userListRow').find('.MenuWrapper a').should('be.visible').click();
         cy.wait(TIMEOUTS.HALF_SEC).findByText('Update Email').click();
