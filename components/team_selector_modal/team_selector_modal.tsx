e2ee9916b672032602323f718fed19bf5e134a2c
Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
2021-01-20 10:05:39 -0500

    remove comment

--- components/team_selector_modal/team_selector_modal.tsx
+++ components/team_selector_modal/team_selector_modal.tsx
@@ -247,7 +247,6 @@ export default class TeamSelectorModal extends React.PureComponent<Props, State>
         if (this.props.teams) {
             teams = this.props.teams.filter((team) => team.delete_at === 0);
             teams = teams.filter((team) => team.scheme_id !== this.currentSchemeId);
-            // We use !== true here because
             teams = this.props.excludeGroupConstrained ? teams.filter((team) => !team.group_constrained) : teams;
 
             teams = teams.filter((team) => this.props.alreadySelected?.indexOf(team.id) === -1);
