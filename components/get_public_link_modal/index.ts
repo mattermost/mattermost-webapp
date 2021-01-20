27842186241c78ff5e1e23fcca89d31b4f2f41f9
Kashif Soofi <kashif.soofi++git@gmail.com>
2021-01-20 12:07:09 +0000

    MM-20374 Migrated get_public_link_modal to typescript
    Signed-off-by: Kashif Soofi <kashif.soofi++git@gmail.com>

--- components/get_public_link_modal/index.ts
+++ components/get_public_link_modal/index.ts
@@ -1,19 +1,21 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import {bindActionCreators, Dispatch} from 'redux';
 import {getFilePublicLink} from 'mattermost-redux/actions/files';
 import * as Selectors from 'mattermost-redux/selectors/entities/files';
 
-import GetPublicLinkModal from './get_public_link_modal.jsx';
+import GetPublicLinkModal from './get_public_link_modal';
+import { GlobalState } from 'mattermost-redux/types/store';
+import {GenericAction} from 'mattermost-redux/types/actions';
 
-function mapStateToProps(state) {
+function mapStateToProps(state: GlobalState) {
     return {
-        link: Selectors.getFilePublicLink(state).link,
+        link: Selectors.getFilePublicLink(state)?.link || '',
     };
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
     return {
         actions: bindActionCreators({
             getFilePublicLink,
