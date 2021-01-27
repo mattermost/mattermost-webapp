775b57a21bf38727220578ca42df29857e4d29f5
Aleksandr Brenchev <abrenchev@griddynamics.com>
2021-01-27 11:11:22 +0100

    MM-12290. Auto-formatting

--- components/markdown_image_expand/markdown_image_expand.tsx
+++ components/markdown_image_expand/markdown_image_expand.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React, { useState } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import React, {useState} from 'react';
+import {CSSTransition} from 'react-transition-group';
 import './markdown_image_expand.scss';
 
 type Props = {
@@ -14,41 +14,53 @@ type State = {
     isExpanded: boolean;
 };
 
-const MarkdownImageExpand: React.FC<Props> = ({ children, alt, collapseDisplay }: Props) => {
+const MarkdownImageExpand: React.FC<Props> = ({children, alt, collapseDisplay}: Props) => {
     const [state, setState] = useState<State>({
         isExpanded: !collapseDisplay,
     });
-    const { isExpanded } = state;
+    const {isExpanded} = state;
 
     const handleToggleButtonClick = () => {
-        setState((oldState) => ({ ...oldState, isExpanded: !oldState.isExpanded }));
+        setState((oldState) => ({...oldState, isExpanded: !oldState.isExpanded}));
     };
 
     const wrapperClassName = `markdown-image-expand ${isExpanded ? 'markdown-image-expand--expanded' : ''}`;
 
     return (
         <div className={wrapperClassName}>
-            <CSSTransition in={isExpanded} timeout={200} mountOnEnter unmountOnExit classNames="markdown-image-expand__expanded-view">
+            <CSSTransition
+                in={isExpanded}
+                timeout={200}
+                mountOnEnter={true}
+                unmountOnExit={true}
+                classNames='markdown-image-expand__expanded-view'
+            >
                 <div>
                     <button
                         className='markdown-image-expand__collapse-button'
                         type='button'
                         onClick={handleToggleButtonClick}
                     >
-                        <span className='icon icon-menu-down' />
+                        <span className='icon icon-menu-down'/>
                     </button>
                     {children}
                 </div>
             </CSSTransition>
 
-            <CSSTransition in={!isExpanded} timeout={200} mountOnEnter unmountOnExit classNames="markdown-image-expand__collapsed-view">
+            <CSSTransition
+                in={!isExpanded}
+                timeout={200}
+                mountOnEnter={true}
+                unmountOnExit={true}
+                classNames='markdown-image-expand__collapsed-view'
+            >
                 <div>
                     <button
                         className='markdown-image-expand__expand-button'
                         type='button'
                         onClick={handleToggleButtonClick}
                     >
-                        <span className='icon icon-menu-right markdown-image-expand__expand-icon' />
+                        <span className='icon icon-menu-right markdown-image-expand__expand-icon'/>
 
                         <span className='markdown-image-expand__alt-text'>
                             {alt}
