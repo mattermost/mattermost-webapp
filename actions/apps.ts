c1cd297e94a9dcaea00508ad11c696d512365fdd
Daniel Espino García <larkox@gmail.com>
2021-01-27 11:58:32 +0100

    Add Navigate to AppCallResonses handling (#7377)

--- actions/apps.ts
+++ actions/apps.ts
@@ -12,6 +12,8 @@ import {openModal} from 'actions/views/modals';
 import AppsForm from 'components/apps_form';
 
 import {ModalIdentifiers} from 'utils/constants';
+import {getSiteURL, shouldOpenInNewTab} from 'utils/url';
+import {browserHistory} from 'utils/browser_history';
 
 const ephemeral = (text: string, call?: AppCall) => sendEphemeralPost(text, (call && call.context.channel_id) || '', (call && call.context.root_id) || '');
 
@@ -44,6 +46,20 @@ export function doAppCall<Res=unknown>(call: AppCall): ActionFunc {
                 dispatch(openAppsModal(res.form, call));
             }
 
+            return {data: res};
+        case AppCallResponseTypes.NAVIGATE:
+            if (!res.url) {
+                const errMsg = 'An error has occurred. Please contact the App developer. Details: Response type is `navigate`, but no url was included in response.';
+                ephemeral(errMsg, call);
+                return {data: res};
+            }
+
+            if (shouldOpenInNewTab(res.url, getSiteURL())) {
+                window.open(res.url);
+                return {data: res};
+            }
+
+            browserHistory.push(res.url);
             return {data: res};
         }
 
