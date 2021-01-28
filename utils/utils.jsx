7eb132ffe0b02e8e19a2c8ab46f40a868a15890d
Haardik Dharma <dharmahaardik08@gmail.com>
2021-01-28 22:35:58 +0530

    MM-29694: Migrate changeCSS() to CSS variable in utils/utils.jsx, ln. 689  (#7406)
    * Switch center-channel-color-## with center-channel-color-rgb variables
    
    * Update utils.jsx
    
    * Update _post.scss
    
    * Update utils.jsx
    
    * Update _post.scss
    
    * Update _post.scss
    
    * Update _buttons.scss
    
    * Update _post.scss
    
    * Update utils.jsx
    
    * Update _post.scss

--- utils/utils.jsx
+++ utils/utils.jsx
@@ -383,7 +383,6 @@ export function applyTheme(theme) {
         changeCss('.app__body .help-text, .app__body .post .post-waiting, .app__body .post.post--system .post__body', 'color:' + changeOpacity(theme.centerChannelColor, 0.6));
         changeCss('.app__body .nav-tabs, .app__body .nav-tabs > li.active > a, pp__body .input-group-addon, .app__body .app__content, .app__body .post-create__container .post-create-body .btn-file, .app__body .post-create__container .post-create-footer .msg-typing, .app__body .dropdown-menu, .app__body .popover, .app__body .mentions__name, .app__body .tip-overlay, .app__body .form-control[disabled], .app__body .form-control[readonly], .app__body fieldset[disabled] .form-control', 'color:' + theme.centerChannelColor);
         changeCss('.app__body #archive-link-home, .video-div .video-thumbnail__error', 'background:' + changeOpacity(theme.centerChannelColor, 0.15));
-        changeCss('.app__body #post-create', 'color:' + theme.centerChannelColor);
         changeCss('.app__body .shortcut-key, .app__body .post__body hr, .app__body .loading-screen .loading__content .round, .app__body .tutorial__circles .circle', 'background:' + theme.centerChannelColor);
         changeCss('.app__body .markdown__table tbody tr:nth-child(2n)', 'background:' + changeOpacity(theme.centerChannelColor, 0.07));
         changeCss('.app__body .channel-header .pinned-posts-button svg', 'fill:' + changeOpacity(theme.centerChannelColor, 0.6));
