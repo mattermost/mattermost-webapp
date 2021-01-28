af40fa0fc7c2636fde95efa0b4c863f86ee0713f
Agniva De Sarker <agnivade@yahoo.co.in>
2021-01-28 11:41:16 +0530

    MM-31993: Add flag to disable gossip compression (#7329)
    * MM-31993: Add flag to disable gossip compression
    
    Full context in the server PR
    
    https://mattermost.atlassian.net/browse/MM-31993
    
    ```release-notes
    Add gossip compression flag in the admin console
    ```
    
    * Update text
    
    * Update e2e/cypress/integration/enterprise/system_console/cluster_spec.js
    
    Co-authored-by: Caleb Roseland <caleb.roseland@mattermost.com>
    
    * fix lint
    
    * make other things consistent too
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    Co-authored-by: Caleb Roseland <caleb.roseland@mattermost.com>

--- components/admin_console/admin_definition.jsx
+++ components/admin_console/admin_definition.jsx
@@ -1337,6 +1337,8 @@ const AdminDefinition = {
                 'admin.cluster.UseExperimentalGossipDesc',
                 'admin.cluster.EnableExperimentalGossipEncryption',
                 'admin.cluster.EnableExperimentalGossipEncryptionDesc',
+                'admin.cluster.EnableGossipCompression',
+                'admin.cluster.EnableGossipCompressionDesc',
                 'admin.cluster.GossipPort',
                 'admin.cluster.GossipPortDesc',
                 'admin.cluster.StreamingPort',
