ebdd7dac77a7faa0e0817fa5efe368eee04f3296
Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
2021-02-08 18:54:15 -0500

    moving card component stuff to the card header

--- components/card/card.test.tsx
+++ components/card/card.test.tsx
@@ -2,13 +2,31 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import {mount} from 'enzyme';
+import {FormattedMessage} from 'react-intl';
+import {mount, shallow} from 'enzyme';
 
 import Card from './card';
 
 describe('components/card/card', () => {
     const baseProps = {
         expanded: false,
+    };
+
+    const headerProps = {
+        title:
+            <FormattedMessage
+                id='admin.data_retention.customPolicies.title'
+                defaultMessage='Custom retention policies'
+            />,
+        subtitle:
+            <FormattedMessage
+                id='admin.data_retention.customPolicies.subTitle'
+                defaultMessage='Customize how long specific teams and channels will keep messages.'
+            />,
+        body:
+            <div>
+                {'Hello!'}
+            </div>,
     };
 
     test('should match snapshot', () => {
@@ -31,6 +49,51 @@ describe('components/card/card', () => {
         const wrapper = mount(
             <Card {...props}>
                 <Card.Header>{'Header Test'}</Card.Header>
+                <Card.Body>{'Body Test'}</Card.Body>
+            </Card>,
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should match snapshot when using alternate header and no button', () => {
+        const props = {
+            ...baseProps,
+            expanded: true,
+            className: 'console',
+        };
+
+        const wrapper = shallow(
+            <Card {...props}>
+                <Card.Header {...headerProps}>{''}</Card.Header>
+                <Card.Body>{'Body Test'}</Card.Body>
+            </Card>,
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should match snapshot when using alternate header and a button', () => {
+        const props = {
+            ...baseProps,
+            expanded: true,
+            className: 'console',
+        };
+
+        const buttonProps = {
+            buttonText:
+                <FormattedMessage
+                    id='admin.data_retention.customPolicies.addPolicy'
+                    defaultMessage='Add policy'
+                />,
+            onClick:
+                () => {}
+            ,
+        }
+
+        const wrapper = shallow(
+            <Card {...props}>
+                <Card.Header {...headerProps} {...buttonProps}>{''}</Card.Header>
                 <Card.Body>{'Body Test'}</Card.Body>
             </Card>,
         );
--- components/card/card_header.tsx
+++ components/card/card_header.tsx
@@ -7,13 +7,44 @@ import classNames from 'classnames';
 type Props = {
     children: React.ReactNode;
     expanded?: boolean;
+    title?: React.ReactNode;
+    subtitle?: React.ReactNode;
+    buttonText?: React.ReactNode;
+    onClick?: () => void;
 };
 
 const CardHeader: React.FC<Props> = (props: Props) => {
     return (
         <div className={classNames('Card__header', {expanded: props.expanded})}>
-            {props.children}
-            {props.expanded && <hr className='Card__hr'/>}
+            {
+                props.title && props.subtitle ?
+                    <>
+                        <div>
+                            <div className='text-top'>
+                                {props.title}
+                            </div>
+                            <div className='text-bottom'>
+                                {props.subtitle}
+                            </div>
+                        </div>
+                        {
+                            props.buttonText && props.onClick ?
+                                <button
+                                    className='button primary'
+                                    onClick={props.onClick}
+                                >
+                                    {props.buttonText}
+                                </button> :
+                                ''
+                        }
+                    
+                    </> : 
+                    <>
+                        {props.children}
+                        {props.expanded && <hr className='Card__hr'/>}
+                    </>
+            }
+            
         </div>
     );
 };
