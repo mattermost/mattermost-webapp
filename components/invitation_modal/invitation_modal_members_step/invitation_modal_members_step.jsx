562563dc6421e8796163df6b456be78ab41d9fd9
Pablo Velez Vidal <pablo.velez@mattermost.com>
2021-02-11 22:58:35 +0100

    use the getRemainingUsers function

--- components/invitation_modal/invitation_modal_members_step/invitation_modal_members_step.jsx
+++ components/invitation_modal/invitation_modal_members_step/invitation_modal_members_step.jsx
@@ -220,8 +220,7 @@ class InvitationModalMembersStep extends React.PureComponent {
             noMatchMessageDefault = 'No one found matching **{text}**';
         }
 
-        const {subscriptionStats} = this.props;
-        const remainingUsers = subscriptionStats && subscriptionStats.remaining_seats;
+        const remainingUsers = this.getRemainingUsers();
         const inviteMembersButtonDisabled = this.state.usersAndEmails.length > Constants.MAX_ADD_MEMBERS_BATCH || this.state.usersAndEmails.length === 0;
 
         const errorProperties = {
