e22e8b73d83654922d1f6666c1db859e6fdfce08
Malik <furqan.malik@mattermost.com>
2021-02-18 18:01:54 -0500

    Update e2e/cypress/support/ui/compliance_export.d.ts
    Co-authored-by: Saturnino Abril <saturnino.abril@gmail.com>

--- e2e/cypress/support/ui/compliance_export.d.ts
+++ e2e/cypress/support/ui/compliance_export.d.ts
@@ -14,7 +14,7 @@
 // ***************************************************************
 
 declare namespace Cypress {
-    interface Chainable<Subject = any> {
+    interface Chainable {
 
         /**
          * Select compliance export format
