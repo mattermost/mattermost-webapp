a6b0750436c84f189a4c260be3dc817ea7847fe7
AGMETEOR <guwats10@gmail.com>
2021-02-19 14:52:43 +0300

    [MM-32691] - Send email when the users are not able to join a workspace

--- components/error_page/workspace_user_limit_reached.tsx
+++ components/error_page/workspace_user_limit_reached.tsx
@@ -1,8 +1,9 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {FormattedMessage} from 'react-intl';
+import {Client4} from 'mattermost-redux/client';
 
 import computerWithAlert from 'images/cloud/computer-with-alert.svg';
 import mattermostCloud from 'images/cloud/mattermost-cloud.svg';
@@ -10,6 +11,16 @@ import wavesBackground from 'images/cloud/waves.svg';
 import blueDots from 'images/cloud/blue.svg';
 
 const WorkspaceUserLimitReached: React.FC = () => {
+    const [notifyMessageId, setMessageId] = useState('empty.id');
+
+    useEffect(() => {
+        async function notifyFunc() {
+            await Client4.sendAdminUpgradeRequestEmailOnJoin();
+            setMessageId('error.maxFreeUsersReached.notified');
+        }
+        notifyFunc();
+    }, []);
+
     return (
         <div className='ErrorPage__container_max_free_users'>
             <header className='ErrorPage__mattermostCloudImg'>
@@ -31,7 +42,11 @@ const WorkspaceUserLimitReached: React.FC = () => {
                         <div className='ErrorPage__maxFreeUsersReachedDescription'>
                             <FormattedMessage
                                 id='error.maxFreeUsersReached.description'
-                                defaultMessage='New users cannot be added to this workspace because it has reached the user limit for the free version of Mattermost Cloud. The system administrator has been notified'
+                                defaultMessage='New users cannot be added to this workspace because it has reached the user limit for the free version of Mattermost Cloud.'
+                            />
+                            <FormattedMessage
+                                id={notifyMessageId}
+                                defaultMessage=' '
                             />
                         </div>
                     </article>
