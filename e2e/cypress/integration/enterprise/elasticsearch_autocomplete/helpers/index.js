3c44d7c798591ac1dff5ddfc68742b1342f43896
Saturnino Abril <saturnino.abril@gmail.com>
2021-02-22 22:08:33 +0800

    fix e2e due to recent change in at-mention suggestion list

--- e2e/cypress/integration/enterprise/elasticsearch_autocomplete/helpers/index.js
+++ e2e/cypress/integration/enterprise/elasticsearch_autocomplete/helpers/index.js
@@ -238,9 +238,6 @@ module.exports = {
         cy.get('#suggestionList', {timeout: TIMEOUTS.FIVE_SEC}).should('be.visible');
 
         // # Verify user appears in results post-change
-        return cy.findByTestId(`mentionSuggestion_${user.username}`, {exact: false}).within((name) => {
-            cy.wrap(name).find('.mention--align').should('have.text', `@${user.username}`);
-            cy.wrap(name).find('.ml-2').should('have.text', `${user.first_name} ${user.last_name} (${user.nickname})`);
-        });
+        return cy.uiVerifyAtMentionSuggestion(user);
     },
 };
