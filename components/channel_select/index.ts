962021f6eab31d05fc9edf9a9e20b4b138854521
David Janda <djanda97@gmail.com>
2021-02-28 15:40:19 -0800

    index.js -> index.ts

--- components/channel_select/index.ts
+++ components/channel_select/index.ts
@@ -8,6 +8,8 @@ import {getMyChannels} from 'mattermost-redux/selectors/entities/channels';
 import {getCurrentUserLocale} from 'mattermost-redux/selectors/entities/i18n';
 import {sortChannelsByTypeAndDisplayName} from 'mattermost-redux/utils/channel_utils';
 
+import {GlobalState} from 'mattermost-redux/types/store';
+
 import ChannelSelect from './channel_select';
 
 const getMyChannelsSorted = createSelector(
@@ -18,7 +20,7 @@ const getMyChannelsSorted = createSelector(
     },
 );
 
-function mapStateToProps(state) {
+function mapStateToProps(state: GlobalState) {
     return {
         channels: getMyChannelsSorted(state),
     };
