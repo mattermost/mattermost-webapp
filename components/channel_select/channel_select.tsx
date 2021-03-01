868c2919fc85572bf8aebfb0b220f8eabe42ef99
David Janda <djanda97@gmail.com>
2021-02-28 16:08:41 -0800

    Use Channel type from mattermost-redux

--- components/channel_select/channel_select.tsx
+++ components/channel_select/channel_select.tsx
@@ -1,98 +1,73 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useEffect, useState} from 'react';
+import {Channel} from 'mattermost-redux/types/channels';
+import React from 'react';
 
 import Constants from 'utils/constants';
 import * as Utils from 'utils/utils.jsx';
 
 type Props = {
-    channels: Array<{
-        id: string;
-        display_name?: string;
-        name: string;
-        type: string;
-    }>;
-    onChange: () => void;
-    value: string;
+    channels: Channel[];
+    onChange?: () => void;
+    value?: string;
     selectOpen: boolean;
     selectPrivate: boolean;
     selectDm: boolean;
 };
 
-type OptionsState = Array<{
-    key: string;
-    value: string;
-    text: string;
-}>;
+export default class ChannelSelect extends React.PureComponent<Props> {
+    render(): JSX.Element {
+        const options = [
+            <option
+                key=''
+                value=''
+            >
+                {Utils.localizeMessage('channel_select.placeholder', '--- Select a channel ---')}
+            </option>,
+        ];
 
-function ChannelSelect({
-    channels,
-    onChange,
-    value,
-    selectOpen = false,
-    selectPrivate = false,
-    selectDm = false,
-}: Props): JSX.Element {
-    const [options, setOptions] = useState<OptionsState>([
-        {
-            key: '',
-            value: '',
-            text: Utils.localizeMessage('channel_select.placeholder', '--- Select a channel ---'),
-        },
-    ]);
-
-    useEffect(() => {
-        channels.forEach((channel) => {
+        this.props.channels.forEach((channel: Channel) => {
             const channelName = channel.display_name || channel.name;
-            if (channel.type === Constants.OPEN_CHANNEL && selectOpen) {
-                setOptions([
-                    ...options,
-                    {
-                        key: channel.id,
-                        value: channel.id,
-                        text: channelName,
-                    },
-                ]);
-            } else if (channel.type === Constants.PRIVATE_CHANNEL && selectPrivate) {
-                setOptions([
-                    ...options,
-                    {
-                        key: channel.id,
-                        value: channel.id,
-                        text: channelName,
-                    },
-                ]);
-            } else if (channel.type === Constants.DM_CHANNEL && selectDm) {
-                setOptions([
-                    ...options,
-                    {
-                        key: channel.id,
-                        value: channel.id,
-                        text: channelName,
-                    },
-                ]);
+            if (channel.type === Constants.OPEN_CHANNEL && this.props.selectOpen) {
+                options.push(
+                    <option
+                        key={channel.id}
+                        value={channel.id}
+                    >
+                        {channelName}
+                    </option>,
+                );
+            } else if (channel.type === Constants.PRIVATE_CHANNEL && this.props.selectPrivate) {
+                options.push(
+                    <option
+                        key={channel.id}
+                        value={channel.id}
+                    >
+                        {channelName}
+                    </option>,
+                );
+            } else if (channel.type === Constants.DM_CHANNEL && this.props.selectDm) {
+                options.push(
+                    <option
+                        key={channel.id}
+                        value={channel.id}
+                    >
+                        {channelName}
+                    </option>,
+                );
             }
         });
-    }, []);
 
-    return (
-        <select
-            className='form-control'
-            value={value}
-            onChange={onChange}
-            id='channelSelect'
-        >
-            {options.map((option) => (
-                <option
-                    key={option.key}
-                    value={option.value}
-                >
-                    {option.text}
-                </option>),
-            )}
-        </select>
-    );
+        return (
+            <select
+                className='form-control'
+                value={this.props.value}
+                onChange={this.props.onChange}
+                id='channelSelect'
+            >
+                {options}
+            </select>
+        );
+    }
 }
-
-export default ChannelSelect;
