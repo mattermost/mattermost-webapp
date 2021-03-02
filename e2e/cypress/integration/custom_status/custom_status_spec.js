2c6c8e8b881f70e60b95589db66445a58b76b432
Chetanya Kandhari <chetanya.kandhari@brightscout.com>
2021-03-03 00:54:40 +0530

    Update e2e/cypress/integration/custom_status/custom_status_spec.js
    Co-authored-by: Joseph Baylon <joseph.baylon@mattermost.com>

--- e2e/cypress/integration/custom_status/custom_status_spec.js
+++ e2e/cypress/integration/custom_status/custom_status_spec.js
@@ -7,7 +7,6 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-// Stage: @prod
 // Group: @custom_status
 
 import {openCustomStatusModal} from './helper';
