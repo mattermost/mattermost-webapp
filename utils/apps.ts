013efbeac0f98b537e0511c4d1249339c3534a69
Daniel Espino García <larkox@gmail.com>
2021-03-08 12:18:04 +0100

    Simplify return

--- utils/apps.ts
+++ utils/apps.ts
@@ -8,10 +8,7 @@ import {GlobalState} from 'mattermost-redux/types/store';
 
 export function appsEnabled(state: GlobalState): boolean {
     const enabled = getConfig(state)?.['FeatureFlagAppsEnabled' as keyof Partial<ClientConfig>];
-    if (enabled === 'true') {
-        return true;
-    }
-    return false;
+    return enabled === 'true';
 }
 
 export function fillBindingsInformation(binding?: AppBinding) {
