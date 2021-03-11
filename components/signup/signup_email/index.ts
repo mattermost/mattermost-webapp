a023c04a6b9867e14d3438c7c1a13a61a4751a65
Eric Montague <eric.g.montague@gmail.com>
2021-03-11 14:46:31 -0500

    Migrated index.js to Typescript

--- components/signup/signup_email/index.ts
+++ components/signup/signup_email/index.ts
@@ -2,7 +2,12 @@
 // See LICENSE.txt for license information.
 
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import {ActionCreatorsMapObject, bindActionCreators, Dispatch} from 'redux';
+
+import {GenericAction, ActionFunc} from 'mattermost-redux/types/actions';
+import {UserProfile} from 'mattermost-redux/types/users';
+import {ClientConfig} from 'mattermost-redux/types/config';
+import {ServerError} from 'mattermost-redux/types/errors';
 
 import {createUser} from 'mattermost-redux/actions/users';
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
@@ -14,8 +19,75 @@ import {getPasswordConfig} from 'utils/utils.jsx';
 
 import SignupEmail from './signup_email.jsx';
 
-function mapStateToProps(state) {
-    const config = getConfig(state);
+import {GlobalState} from '../../../types/store';
+import { CLIEngine } from 'eslint';
+
+
+export type PasswordConfig = {
+    minimumLength: number;
+    requireLowercase: boolean;
+    requireNumber: boolean;
+    requireSymbol: boolean;
+    requireUppercase: boolean;
+}
+
+
+export type Props = {
+    location: {search: string};
+    enableSignUpWithEmail: boolean;
+    siteName: string;
+    termsOfServiceLink?: string;
+    privacyPolicyLink?: string;
+    customDescriptionText?: string;
+    passwordConfig?: PasswordConfig;
+    hasAccounts: boolean;
+    actions: Actions;
+};
+
+
+export type State = {
+    loading: boolean;
+    inviteId?: string;
+    token?: string;
+    email?: string;
+    teamName?: string;
+    noOpenServerError?: boolean;
+    isSubmitting?: boolean;
+    nameError?: React.ReactNode;
+    emailError?: React.ReactNode;
+    passwordError?: React.ReactNode;
+    serverError?: React.ReactNode;
+};
+
+
+type TeamInviteInfo = {
+    display_name: string;
+    description: string;
+    name: string;
+    id: string;
+};
+
+
+export type Actions = {
+    createUser: (user: UserProfile, token: string, inviteId: string, redirect: string) => Promise<{data: UserProfile} | {error: ServerError}>;
+    loginById: (id: string, password: string, mfaToken?: string) => Promise<{data: boolean} | {error: ServerError}>;
+    setGlobalItem: (name: string, value: string) => {data: boolean};
+    getTeamInviteInfo: (inviteId: string) => Promise<{data: TeamInviteInfo} | {error: ServerError}>;
+};
+
+
+type ClientConfigPatched = {
+    NoAccounts: "true" | "false";
+    SiteName: string;
+}
+
+
+type ClientConfigWithNoAccounts = Partial<ClientConfig> & ClientConfigPatched;
+
+
+
+function mapStateToProps(state: GlobalState) {
+    const config = getConfig(state) as ClientConfigWithNoAccounts;
 
     const enableSignUpWithEmail = config.EnableSignUpWithEmail === 'true';
     const siteName = config.SiteName;
@@ -35,9 +107,9 @@ function mapStateToProps(state) {
     };
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
     return {
-        actions: bindActionCreators({
+        actions: bindActionCreators<ActionCreatorsMapObject<ActionFunc | GenericAction>, Actions>({
             createUser,
             loginById,
             setGlobalItem,
@@ -46,4 +118,5 @@ function mapDispatchToProps(dispatch) {
     };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(SignupEmail);
+/* This is a workaround to handle the issue of Typescript not being able to correctly infer the types of the component's Props */
+export default connect(mapStateToProps, mapDispatchToProps)(SignupEmail as React.ComponentClass<Props, State>);
