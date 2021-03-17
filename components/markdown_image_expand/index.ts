97e0ee84ce6855224f2ed19fb9c72b80b7740245
Aleksandr Brenchev <abrenchev@griddynamics.com>
2021-03-17 09:39:50 +0100

    [MM-12290] Removed redundant license information from MarkdownImageExpand

--- components/markdown_image_expand/index.ts
+++ components/markdown_image_expand/index.ts
@@ -1,10 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-
 import {bindActionCreators, Dispatch} from 'redux';
-
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
 import {GlobalState} from 'mattermost-redux/types/store';
 import {connect} from 'react-redux';
 
