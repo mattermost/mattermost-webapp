d2df0ffe29aac4b98152f05b8baa95e37dfab0a3
Catalin Tomai <catalin.tomai@mattermost.com>
2021-03-22 17:53:03 -0700

    fix make check-types error

--- packages/mattermost-redux/src/client/client4.ts
+++ packages/mattermost-redux/src/client/client4.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {SystemSetting} from 'types/general';
+import {SystemSetting} from 'mattermost-redux/types/general';
 
 import {General} from '../constants';
 
