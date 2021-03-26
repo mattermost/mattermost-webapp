9a29aac12852a9117f3c6d21cfa2164c04ce5595
Devin Binnie <devin.binnie@mattermost.com>
2021-03-26 17:37:23 -0400

    Migrate components/channel_header_dropdown/menu_items/close_message to TypeScript

--- components/channel_header_dropdown/menu_items/close_message/close_message.tsx
+++ components/channel_header_dropdown/menu_items/close_message/close_message.tsx
@@ -2,59 +2,63 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import PropTypes from 'prop-types';
+
+import {UserProfile} from 'mattermost-redux/types/users';
+import {Team} from 'mattermost-redux/types/teams';
+import {Channel} from 'mattermost-redux/types/channels';
+import {PreferenceType} from 'mattermost-redux/types/preferences';
 
 import {localizeMessage} from 'utils/utils';
 import {Constants} from 'utils/constants';
 import {browserHistory} from 'utils/browser_history';
 import Menu from 'components/widgets/menu/menu';
 
-export default class CloseMessage extends React.PureComponent {
-    static propTypes = {
+type Props = {
+
+    /**
+     * Object with info about currentUser
+     */
+    currentUser: UserProfile;
+
+    /**
+     * Object with info about currentTeam
+     */
+    currentTeam: Team;
+
+    /**
+     * String with info about redirect
+     */
+    redirectChannel: string;
+
+    /**
+     * Object with info about channel
+     */
+    channel: Channel;
+
+    /**
+     * Use for test selector
+     */
+    id?: string;
+
+    /**
+     * Object with action creators
+     */
+    actions: {
 
         /**
-         * Object with info about currentUser
+         * Action creator to update user preferences
          */
-        currentUser: PropTypes.object.isRequired,
+        savePreferences: (userId: string, preferences: PreferenceType[]) => void;
 
         /**
-         * Object with info about currentTeam
+         * Action creator to leave DM/GM
          */
-        currentTeam: PropTypes.object.isRequired,
-
-        /**
-         * String with info about redirect
-         */
-        redirectChannel: PropTypes.string.isRequired,
-
-        /**
-         * Object with info about channel
-         */
-        channel: PropTypes.object.isRequired,
-
-        /**
-         * Use for test selector
-         */
-        id: PropTypes.string,
-
-        /**
-         * Object with action creators
-         */
-        actions: PropTypes.shape({
-
-            /**
-             * Action creator to update user preferences
-             */
-            savePreferences: PropTypes.func.isRequired,
-
-            /**
-             * Action creator to leave DM/GM
-             */
-            leaveDirectChannel: PropTypes.func.isRequired,
-        }).isRequired,
+        leaveDirectChannel: (channelName: string) => void;
     };
+};
 
-    handleClose = (e) => {
+export default class CloseMessage extends React.PureComponent<Props> {
+    handleClose = (e: React.MouseEvent): void => {
         e.preventDefault();
 
         const {
@@ -68,11 +72,11 @@ export default class CloseMessage extends React.PureComponent {
             },
         } = this.props;
 
-        let name;
+        let name: string;
         let category;
         if (channel.type === Constants.DM_CHANNEL) {
             category = Constants.Preferences.CATEGORY_DIRECT_CHANNEL_SHOW;
-            name = channel.teammate_id;
+            name = channel.teammate_id!;
         } else {
             category = Constants.Preferences.CATEGORY_GROUP_CHANNEL_SHOW;
             name = channel.id;
@@ -84,7 +88,7 @@ export default class CloseMessage extends React.PureComponent {
         browserHistory.push(`/${currentTeam.name}/channels/${redirectChannel}`);
     }
 
-    render() {
+    render(): React.ReactNode {
         const {id, channel} = this.props;
 
         let text;
