86cd923ae54f81c8597664b4da26682ce9f50b62
Dominic Scanlan <dominicscanlan@hotmail.com>
2021-03-30 10:13:20 +0100

    fix eslint errors

--- components/emoji_picker/components/emoji_picker_category.tsx
+++ components/emoji_picker/components/emoji_picker_category.tsx
@@ -1,46 +1,48 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React from "react";
 
 interface EmojiPickerCategoryProps {
-    selected: boolean,
-    enable: boolean,
-    onCategoryClick: (category: string) => void,
-    category: string,
-    icon: React.ReactNode
+  selected: boolean;
+  enable: boolean;
+  onCategoryClick: (category: string) => void;
+  category: string;
+  icon: React.ReactNode;
 }
 
 export default class EmojiPickerCategory extends React.Component<EmojiPickerCategoryProps> {
-    shouldComponentUpdate(nextProps: EmojiPickerCategoryProps) {
-        return nextProps.selected !== this.props.selected ||
-            nextProps.enable !== this.props.enable;
+  shouldComponentUpdate(nextProps: EmojiPickerCategoryProps) {
+    return (
+      nextProps.selected !== this.props.selected ||
+      nextProps.enable !== this.props.enable
+    );
+  }
+
+  handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    this.props.onCategoryClick(this.props.category);
+  };
+
+  render() {
+    let className = "emoji-picker__category";
+    if (this.props.selected) {
+      className += " emoji-picker__category--selected";
     }
 
-    handleClick = (e: React.MouseEvent) => {
-        e.preventDefault();
-        this.props.onCategoryClick(this.props.category);
+    if (!this.props.enable) {
+      className += " disable";
     }
 
-    render() {
-        let className = 'emoji-picker__category';
-        if (this.props.selected) {
-            className += ' emoji-picker__category--selected';
-        }
-
-        if (!this.props.enable) {
-            className += ' disable';
-        }
-
-        return (
-            <a
-                className={className}
-                href='#'
-                onClick={this.handleClick}
-                aria-label={this.props.category}
-            >
-                {this.props.icon}
-            </a>
-        );
-    }
+    return (
+      <a
+        className={className}
+        href="#"
+        onClick={this.handleClick}
+        aria-label={this.props.category}
+      >
+        {this.props.icon}
+      </a>
+    );
+  }
 }
--- components/emoji_picker/components/emoji_picker_header.test.tsx
+++ components/emoji_picker/components/emoji_picker_header.test.tsx
@@ -1,34 +1,32 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
-import {shallow} from 'enzyme';
-import EmojiPickerHeader from 'components/emoji_picker/components/emoji_picker_header';
+import React from "react";
 
-describe('components/emoji_picker/components/EmojiPickerHeader', () => {
-    test('should match snapshot, ', () => {
-        const props = {
-            handleEmojiPickerClose: jest.fn(),
-        };
+import { shallow } from "enzyme";
 
-        const wrapper = shallow(
-            <EmojiPickerHeader {...props}/>,
-        );
+import EmojiPickerHeader from "components/emoji_picker/components/emoji_picker_header";
 
-        expect(wrapper).toMatchSnapshot();
-    });
-    test('handleEmojiPickerClose, should have called props.handleEmojiPickerClose', () => {
-        const props = {
-            handleEmojiPickerClose: jest.fn(),
-        };
+describe("components/emoji_picker/components/EmojiPickerHeader", () => {
+  test("should match snapshot, ", () => {
+    const props = {
+      handleEmojiPickerClose: jest.fn(),
+    };
 
-        const wrapper = shallow(
-            <EmojiPickerHeader {...props}/>,
-        );
+    const wrapper = shallow(<EmojiPickerHeader {...props} />);
 
-        expect(wrapper).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
+  });
+  test("handleEmojiPickerClose, should have called props.handleEmojiPickerClose", () => {
+    const props = {
+      handleEmojiPickerClose: jest.fn(),
+    };
 
-        wrapper.find('button').first().simulate('click');
-        expect(props.handleEmojiPickerClose).toHaveBeenCalledTimes(1);
-    });
+    const wrapper = shallow(<EmojiPickerHeader {...props} />);
+
+    expect(wrapper).toMatchSnapshot();
+
+    wrapper.find("button").first().simulate("click");
+    expect(props.handleEmojiPickerClose).toHaveBeenCalledTimes(1);
+  });
 });
