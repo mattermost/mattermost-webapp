35dc9f46446d326f7724852e0a01936158e9c61d
Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
2021-03-29 23:29:01 -0400

    fixing lint again

--- components/widgets/admin_console/admin_header.test.tsx
+++ components/widgets/admin_console/admin_header.test.tsx
@@ -3,20 +3,20 @@
 
 /* eslint no-console: 0 */
 
-import React from "react";
-import { shallow } from "enzyme";
+import React from 'react';
+import {shallow} from 'enzyme';
 
-import AdminHeader from "./admin_header";
+import AdminHeader from './admin_header';
 
-describe("components/widgets/admin_console/AdminHeader", () => {
-  test("render component with child", () => {
-    const wrapper = shallow(<AdminHeader>{"Test"}</AdminHeader>);
-    expect(wrapper).toMatchInlineSnapshot(`
+describe('components/widgets/admin_console/AdminHeader', () => {
+    test('render component with child', () => {
+        const wrapper = shallow(<AdminHeader>{'Test'}</AdminHeader>);
+        expect(wrapper).toMatchInlineSnapshot(`
       <div
         className="admin-console__header with-back"
       >
         Test
       </div>
     `);
-  });
+    });
 });
