4b758fafe3b2db2e7a8d3f88aaa891f4e86de56a
Aleksandr Brenchev <abrenchev@griddynamics.com>
2021-04-07 08:40:08 +0200

    [MM-12290] Fixed linting errors

--- components/markdown_image_expand/index.ts
+++ components/markdown_image_expand/index.ts
@@ -1,8 +1,10 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 import {bindActionCreators, Dispatch} from 'redux';
-import {GlobalState} from 'mattermost-redux/types/store';
+
 import {connect} from 'react-redux';
+
+import {GlobalState} from 'mattermost-redux/types/store';
 
 import {GenericAction} from 'mattermost-redux/types/actions';
 
