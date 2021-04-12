3fc6a15f7e0f42a2227240dbbca8f3d6548d95f0
Daniel Espino García <larkox@gmail.com>
2021-04-12 15:20:53 +0200

    Sync parser with latest mobile improvements (#7783)
    * Sync parser with latest mobile improvements
    
    * Sync with latest mobile changes, use modal label and use error as a constant
    
    * Update i18en
    
    * Fix i18n
    
    Co-authored-by: Michael Kochell <6913320+mickmister@users.noreply.github.com>
    (cherry picked from commit 717caaefd4279447be9d089996836d51eb6b6300)

--- components/suggestion/command_provider/app_command_parser/app_command_parser_dependencies.ts
+++ components/suggestion/command_provider/app_command_parser/app_command_parser_dependencies.ts
@@ -60,7 +60,9 @@ import {isMac, localizeAndFormatMessage} from 'utils/utils';
 import Store from 'stores/redux_store';
 export const getStore = () => Store;
 
-export const EXECUTE_CURRENT_COMMAND_ITEM_ID = '_execute_current_command';
+import {Constants} from 'utils/constants';
+export const EXECUTE_CURRENT_COMMAND_ITEM_ID = Constants.Integrations.EXECUTE_CURRENT_COMMAND_ITEM_ID;
+export const COMMAND_SUGGESTION_ERROR = Constants.Integrations.COMMAND_SUGGESTION_ERROR;
 
 import type {ParsedCommand} from './app_command_parser';
 
@@ -88,4 +90,13 @@ export const intlShim = {
     formatMessage: (config: {id: string; defaultMessage: string}, values?: {[name: string]: any}) => {
         return localizeAndFormatMessage(config.id, config.defaultMessage, values);
     },
+};
+
+export const errorMessage = (intl: typeof intlShim, error: string, _command: string, _position: number): string => { // eslint-disable-line @typescript-eslint/no-unused-vars
+    return intl.formatMessage({
+        id: 'apps.error.parser',
+        defaultMessage: 'Parsing error: {error}',
+    }, {
+        error,
+    });
 };
