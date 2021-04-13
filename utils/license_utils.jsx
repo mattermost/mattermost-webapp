85e5fc112a8f5f5704b34d63ca9c59e0d579fe46
Hossein <hahmadia@users.noreply.github.com>
2021-04-13 16:51:15 -0400

    [MM-34535][MM-34536][MM-34558] Update Trial License Banner Text (#7837)
    * Update banner
    
    * fix linting
    
    * en.json update
    
    * dismiss modal
    
    * don't show non last day banner if already dismissed last day banner
    
    * address some comments
    
    * address comments
    
    * address comments
    
    * return proper type
    
    * Update i18n/en.json
    
    Co-authored-by: Harshil Sharma <harshilsharma63@gmail.com>
    
    * Update components/announcement_bar/configuration_bar/configuration_bar.tsx
    
    Co-authored-by: Harshil Sharma <harshilsharma63@gmail.com>
    
    * address comments
    
    * fix types
    
    * fix incorrect number of days
    
    Co-authored-by: Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
    Co-authored-by: Harshil Sharma <harshilsharma63@gmail.com>
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    (cherry picked from commit 4fe566135b7869990b1c6f1fd06e9b647f4259cd)

--- utils/license_utils.jsx
+++ utils/license_utils.jsx
@@ -1,5 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
+import moment from 'moment';
 
 const LICENSE_EXPIRY_NOTIFICATION = 1000 * 60 * 60 * 24 * 60; // 60 days
 const LICENSE_GRACE_PERIOD = 1000 * 60 * 60 * 24 * 10; // 10 days
@@ -11,6 +12,15 @@ export function isLicenseExpiring(license) {
 
     const timeDiff = parseInt(license.ExpiresAt, 10) - Date.now();
     return timeDiff <= LICENSE_EXPIRY_NOTIFICATION;
+}
+
+export function daysToLicenseExpire(license) {
+    if (license.IsLicensed !== 'true') {
+        return undefined;
+    }
+
+    const endDate = new Date(parseInt(license?.ExpiresAt, 10));
+    return moment(endDate).startOf('day').diff(moment().startOf('day'), 'days');
 }
 
 export function isLicenseExpired(license) {
@@ -29,4 +39,19 @@ export function isLicensePastGracePeriod(license) {
 
     const timeDiff = Date.now() - parseInt(license.ExpiresAt, 10);
     return timeDiff > LICENSE_GRACE_PERIOD;
+}
+
+export function isTrialLicense(license) {
+    if (license.IsLicensed !== 'true') {
+        return false;
+    }
+
+    // Currently all trial licenses are issued with a 30 day, 8 hours duration.
+    // We're using this logic to detect a trial license until we add the right field in the license itself.
+    const timeDiff = parseInt(license.ExpiresAt, 10) - parseInt(license.StartsAt, 10);
+
+    // 30 days + 8 hours
+    const trialLicenseDuration = (1000 * 60 * 60 * 24 * 30) + (1000 * 60 * 60 * 8);
+
+    return timeDiff <= trialLicenseDuration;
 }
