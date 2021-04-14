8414465e3630b749a1240e48a58b3267dbe04f65
Pablo Andrés Vélez Vidal <pablovv2012@gmail.com>
2021-04-14 22:43:46 +0200

    add subscription object check

--- components/main_menu/index.jsx
+++ components/main_menu/index.jsx
@@ -73,8 +73,8 @@ function mapStateToProps(state) {
     const rhsState = getRhsState(state);
     const isCloud = getLicense(state).Cloud === 'true';
     const subscription = state.entities.cloud.subscription;
-    const isFreeTrial = subscription.is_free_trial === 'true';
-    const daysLeftOnTrial = getRemainingDaysFromFutureTimestamp(subscription.trial_end_at);
+    const isFreeTrial = subscription?.is_free_trial === 'true';
+    const daysLeftOnTrial = getRemainingDaysFromFutureTimestamp(subscription?.trial_end_at);
 
     return {
         appDownloadLink,
