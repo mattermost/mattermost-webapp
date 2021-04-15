f1a9a29c157a52ea58e733597edb424d4fbbf432
Caleb Roseland <caleb@calebroseland.com>
2021-04-14 22:42:04 -0500

    remove unused

--- packages/mattermost-redux/src/actions/threads.ts
+++ packages/mattermost-redux/src/actions/threads.ts
@@ -3,7 +3,7 @@
 
 import {uniq} from 'lodash';
 
-import {ThreadTypes, PostTypes, UserTypes, ChannelTypes} from 'mattermost-redux/action_types';
+import {ThreadTypes, PostTypes, UserTypes} from 'mattermost-redux/action_types';
 import {Client4} from 'mattermost-redux/client';
 
 import ThreadConstants from 'mattermost-redux/constants/threads';
