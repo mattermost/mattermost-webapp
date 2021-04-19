e0b3b88dc7e788834decce32d7204312eeb0316e
Pablo Andrés Vélez Vidal <pablovv2012@gmail.com>
2021-04-19 22:07:47 +0200

    MM-34975 - hide payment information to free trial customers (#7950)
    Co-authored-by: Pablo Velez Vidal <pablo.velez@mattermost.com>

--- components/admin_console/admin_definition.jsx
+++ components/admin_console/admin_definition.jsx
@@ -182,11 +182,8 @@ export const it = {
     enterpriseReady: (config, state, license, enterpriseReady) => enterpriseReady,
     licensed: (config, state, license) => license.IsLicensed === 'true',
     licensedForFeature: (feature) => (config, state, license) => license.IsLicensed && license[feature] === 'true',
-    isPaidTier: (config, state, license, enterpriseReady, consoleAccess, cloud) => {
-        if (!cloud?.subscription) {
-            return false;
-        }
-        return cloud.subscription.is_paid_tier === 'true';
+    hidePaymentInfo: (config, state, license, enterpriseReady, consoleAccess, cloud) => {
+        return cloud?.subscription?.is_paid_tier !== 'true' || cloud?.subscription?.is_free_trial === 'true';
     },
     userHasReadPermissionOnResource: (key) => (config, state, license, enterpriseReady, consoleAccess) => consoleAccess?.read?.[key],
     userHasReadPermissionOnSomeResources: (key) => Object.values(key).some((resource) => it.userHasReadPermissionOnResource(resource)),
@@ -315,7 +312,7 @@ const AdminDefinition = {
             url: 'billing/payment_info',
             title: t('admin.sidebar.payment_info'),
             title_default: 'Payment Information',
-            isHidden: it.not(it.isPaidTier),
+            isHidden: it.hidePaymentInfo,
             searchableStrings: [
                 'admin.billing.payment_info.title',
             ],
