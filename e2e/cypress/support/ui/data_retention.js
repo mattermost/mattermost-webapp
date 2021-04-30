8741b410ec29b29e12d8471474cb9762729bb669
Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
2021-04-30 14:02:58 -0400

    fixing test

--- e2e/cypress/support/ui/data_retention.js
+++ e2e/cypress/support/ui/data_retention.js
@@ -5,7 +5,7 @@ import * as TIMEOUTS from '../../fixtures/timeouts';
 
 Cypress.Commands.add('uiGoToDataRetentionPage', () => {
     cy.visit('/admin_console/compliance/data_retention_settings');
-    cy.get('.DataRetentionSettings .admin-console__header', {timeout: TIMEOUTS.TWO_MIN}).should('be.visible').invoke('text').should('include', 'Data Retention Policy');
+    cy.get('.DataRetentionSettings .admin-console__header', {timeout: TIMEOUTS.TWO_MIN}).should('be.visible').invoke('text').should('include', 'Data Retention Policies');
 });
 
 Cypress.Commands.add('uiClickCreatePolicy', () => {
