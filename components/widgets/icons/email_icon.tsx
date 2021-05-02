3529faf95ccbf9bbe197066d0ce50d85b5903ff1
Yi Wei Pang <pangyiwei@gmail.com>
2021-05-02 23:31:49 +0800

    Convert email_icon to typescript

--- components/widgets/icons/email_icon.tsx
+++ components/widgets/icons/email_icon.tsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 
-export default class EmailIcon extends React.PureComponent {
+export default class EmailIcon extends React.PureComponent<React.HTMLAttributes<HTMLSpanElement>> {
     render() {
         return (
             <span {...this.props}>
