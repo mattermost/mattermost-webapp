1e3e00d82332608e1f27b4f70344511e18178682
Yi Wei Pang <pangyiwei@gmail.com>
2021-05-03 01:02:59 +0800

    Convert link_icon to typescript

--- components/widgets/icons/link_icon.tsx
+++ components/widgets/icons/link_icon.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import {useIntl} from 'react-intl';
 
-export default function LinkIcon(props) {
+export default function LinkIcon(props: React.HTMLAttributes<HTMLSpanElement>) {
     const {formatMessage} = useIntl();
     return (
         <span {...props}>
