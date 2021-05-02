5fb0d420cd3e8e20a9edf88f0fb05afbfae3a0c0
Yi Wei Pang <pangyiwei@gmail.com>
2021-05-03 00:51:11 +0800

    Convert info_icon to typescript

--- components/widgets/icons/info_icon.tsx
+++ components/widgets/icons/info_icon.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import {useIntl} from 'react-intl';
 
-export default function InfoIcon(props) {
+export default function InfoIcon(props: React.HTMLAttributes<HTMLSpanElement>) {
     const {formatMessage} = useIntl();
     return (
         <span {...props}>
