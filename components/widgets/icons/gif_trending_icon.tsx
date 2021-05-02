0d77674b1ea4cc9322ede3bc993603c5352150e9
Yi Wei Pang <pangyiwei@gmail.com>
2021-05-03 00:48:45 +0800

    Convert gif_trending_icon to typescript

--- components/widgets/icons/gif_trending_icon.tsx
+++ components/widgets/icons/gif_trending_icon.tsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 
-export default class GifTrendingIcon extends React.PureComponent {
+export default class GifTrendingIcon extends React.PureComponent<React.HTMLAttributes<HTMLSpanElement>> {
     render() {
         return (
             <span {...this.props}>
