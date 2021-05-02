29c6b25d79840cc9958a84a1f5bb328d160224db
Yi Wei Pang <pangyiwei@gmail.com>
2021-05-02 22:43:44 +0800

    Convert close_circle_solid_icon to typescript

--- components/widgets/icons/close_circle_solid_icon.tsx
+++ components/widgets/icons/close_circle_solid_icon.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import {useIntl} from 'react-intl';
 
-export default function CloseCircleSolidIcon(props) {
+export default function CloseCircleSolidIcon(props: React.HTMLAttributes<HTMLSpanElement>) {
     const {formatMessage} = useIntl();
     return (
         <span {...props}>
