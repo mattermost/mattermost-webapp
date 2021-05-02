60304517bbec4aa2c076f15e34cd1a0449015352
Yi Wei Pang <pangyiwei@gmail.com>
2021-05-02 22:31:35 +0800

    Convert checkbox_partial_icon to typescript

--- components/admin_console/permission_schemes_settings/permission_checkbox.jsx
+++ components/admin_console/permission_schemes_settings/permission_checkbox.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import CheckboxCheckedIcon from 'components/widgets/icons/checkbox_checked_icon';
-import CheckboxPartialIcon from 'components/widgets/icons/checkbox_partial_icon.jsx';
+import CheckboxPartialIcon from 'components/widgets/icons/checkbox_partial_icon';
 
 export default class PermissionCheckbox extends React.PureComponent {
     static propTypes = {
