9f4cfe5a77f62b1bdf5fe0a3ecd997adf26a9e40
Yi Wei Pang <pangyiwei@gmail.com>
2021-05-02 23:07:54 +0800

    Convert draft_icon to typescript

--- components/widgets/icons/draft_icon.tsx
+++ components/widgets/icons/draft_icon.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import {useIntl} from 'react-intl';
 
-export default function DraftIcon(props) {
+export default function DraftIcon(props: React.HTMLAttributes<HTMLSpanElement>) {
     const {formatMessage} = useIntl();
     return (
         <span
