db8a5a799ccb86ad5c332c4f948aebec2289e089
Yi Wei Pang <pangyiwei@gmail.com>
2021-05-03 01:37:31 +0800

    Convert plugin_icon to typescript

--- components/widgets/icons/plugin_icon.tsx
+++ components/widgets/icons/plugin_icon.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import {useIntl} from 'react-intl';
 
-export default function PluginIcon(props) {
+export default function PluginIcon(props: React.HTMLAttributes<HTMLSpanElement>) {
     const {formatMessage} = useIntl();
     return (
         <span {...props}>
