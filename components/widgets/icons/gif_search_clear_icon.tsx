9647db3cd47d60a69dd280179212943f50e57d70
Yi Wei Pang <pangyiwei@gmail.com>
2021-05-03 00:43:14 +0800

    Convert gif_search_clear_icon to typescript

--- components/widgets/icons/gif_search_clear_icon.tsx
+++ components/widgets/icons/gif_search_clear_icon.tsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 
-export default class GifSearchClearIcon extends React.PureComponent {
+export default class GifSearchClearIcon extends React.PureComponent<React.HTMLAttributes<HTMLSpanElement>> {
     render() {
         return (
             <span {...this.props}>
