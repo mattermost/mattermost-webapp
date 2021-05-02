b33ca007ee322607fa4b925673802b476d25a028
Yi Wei Pang <pangyiwei@gmail.com>
2021-05-03 00:55:34 +0800

    Convert invite_members_icon to typescript

--- components/widgets/icons/invite_members_icon.tsx
+++ components/widgets/icons/invite_members_icon.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import {useIntl} from 'react-intl';
 
-export default function InviteMembersIcon(props) {
+export default function InviteMembersIcon(props: React.HTMLAttributes<HTMLSpanElement>) {
     const {formatMessage} = useIntl();
     return (
         <span {...props}>
