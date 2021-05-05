a012b49bf2eff4de9556e1b4984439b241bf3f99
Saturnino Abril <saturnino.abril@gmail.com>
2021-05-05 16:52:05 +0800

    fix failed tests from Cloud May 5

--- e2e/cypress/integration/messaging/message_draft_then_switch_channel_spec.js
+++ e2e/cypress/integration/messaging/message_draft_then_switch_channel_spec.js
@@ -12,33 +12,35 @@
 
 describe('Message Draft and Switch Channels', () => {
     let testTeam;
+    let testChannel;
 
     before(() => {
         // # Create new team and new user and visit Town Square channel
-        cy.apiInitSetup({loginAfter: true}).then(({team}) => {
+        cy.apiInitSetup({loginAfter: true}).then(({team, channel}) => {
             testTeam = team;
+            testChannel = channel;
             cy.visit(`/${testTeam.name}/channels/town-square`);
         });
     });
 
     it('MM-T131 Message Draft Pencil Icon - CTRL/CMD+K & "Jump to"', () => {
-        const testChannel = 'Off-Topic';
+        const {name, display_name: displayName} = testChannel;
         const message = 'message draft test';
 
         // * Validate if the draft icon is not visible at LHS before making a draft
-        verifyDraftIcon(testChannel, false);
+        verifyDraftIcon(name, false);
 
         // # Go to test channel and check if it opened correctly
-        openChannelFromLhs(testTeam.name, testChannel);
+        openChannelFromLhs(testTeam.name, displayName, name);
 
         // # Type a message in the input box but do not send
-        cy.findByRole('textbox', `write to ${testChannel.toLowerCase()}`).should('be.visible').type(message);
+        cy.findByRole('textbox', `write to ${displayName.toLowerCase()}`).should('be.visible').type(message);
 
         // # Switch to another channel and check if it opened correctly
         openChannelFromLhs(testTeam.name, 'Town Square', 'town-square');
 
         // * Validate if the draft icon is visible at LHS
-        verifyDraftIcon(testChannel, true);
+        verifyDraftIcon(name, true);
 
         // # Press CTRL/CMD+K shortcut to open Quick Channel Switch modal
         cy.typeCmdOrCtrl().type('K', {release: true});
@@ -47,32 +49,31 @@ describe('Message Draft and Switch Channels', () => {
         cy.findAllByRole('dialog').first().findByText('Switch Channels').should('be.visible');
 
         // # Type the first few letters of the channel name you typed the message draft in
-        cy.findByRole('textbox', {name: 'quick switch input'}).type(testChannel.substring(0, 3));
+        cy.findByRole('textbox', {name: 'quick switch input'}).type(displayName.substring(0, 3));
 
         // * Suggestion list is visible
         cy.get('#suggestionList').should('be.visible').within(() => {
             // * A pencil icon before the channel name in the filtered list is visible
-            cy.findByLabelText(testChannel).find('.icon-pencil-outline').should('be.visible');
+            cy.get(`#switchChannel_${name}`).find('.icon-pencil-outline').should('be.visible');
 
             // # Click to switch back to the test channel
-            cy.findByLabelText(testChannel).click();
+            cy.get(`#switchChannel_${name}`).click({force: true});
         });
 
         // * Draft is saved in the text input box of the test channel
-        cy.findByRole('textbox', `write to ${testChannel.toLowerCase()}`).should('be.visible').and('have.text', message);
+        cy.findByRole('textbox', `write to ${displayName.toLowerCase()}`).should('be.visible').and('have.text', message);
     });
 });
 
-function verifyDraftIcon(channelName, isVisible) {
-    cy.uiGetLhsSection('CHANNELS').findByLabelText(`${channelName.toLowerCase()} public channel`).
+function verifyDraftIcon(name, isVisible) {
+    cy.uiGetLhsSection('CHANNELS').find(`#sidebarItem_${name}`).
         should('be.visible').
         findByTestId('draftIcon').
         should(isVisible ? 'be.visible' : 'not.exist');
 }
 
-function openChannelFromLhs(teamName, displayName, name) {
+function openChannelFromLhs(teamName, channelName, name) {
     // # Go to test channel and check if it opened correctly
-    cy.uiGetLhsSection('CHANNELS').findByText(displayName).click();
-    cy.url().should('include', `/${teamName}/channels/${name || displayName.toLowerCase()}`);
+    cy.uiGetLhsSection('CHANNELS').findByText(channelName).click();
+    cy.url().should('include', `/${teamName}/channels/${name}`);
 }
-
