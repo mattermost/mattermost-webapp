74dbfdb8ffb31b23c9668a39984069e085ade13d
Caleb Roseland <caleb@calebroseland.com>
2021-05-05 16:57:07 -0500

    typo

--- packages/mattermost-redux/src/actions/posts.ts
+++ packages/mattermost-redux/src/actions/posts.ts
@@ -1,8 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {resetToChannel} from 'app/actions/navigation';
-
 import {Client4, DEFAULT_LIMIT_AFTER, DEFAULT_LIMIT_BEFORE} from 'mattermost-redux/client';
 import {General, Preferences, Posts} from '../constants';
 import {PostTypes, ChannelTypes, FileTypes, IntegrationTypes} from 'mattermost-redux/action_types';
