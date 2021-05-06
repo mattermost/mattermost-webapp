8aba8d1ad9a0705044607d273db030507714b2de
Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
2021-05-06 16:03:20 -0400

    adding test comments

--- e2e/cypress/support/ui/data_retention.js
+++ e2e/cypress/support/ui/data_retention.js
@@ -14,7 +14,9 @@ Cypress.Commands.add('uiClickCreatePolicy', () => {
 });
 
 Cypress.Commands.add('uiFillOutCustomPolicyFields', (name, durationDropdown, durationText = '') => {
+    // # Type policy name
     cy.uiGetTextbox('Policy name').clear().type(name);
+    // # Add message retention values
     cy.get('.CustomPolicy__fields #DropdownInput_message_retention').should('be.visible').click();
     cy.get(`.message_retention__menu .message_retention__option span.option_${durationDropdown}`).should('be.visible').click();
     if (durationText) {
@@ -37,8 +39,6 @@ Cypress.Commands.add('uiAddChannelsToCustomPolicy', (channelNames) => {
     cy.uiGetButton('Add channels').click();
     channelNames.forEach((channelName) => {
         cy.findByRole('textbox', {name: 'Search and add channels'}).clear().type(channelName);
-
-        // Wait for channel to load
         cy.wait(TIMEOUTS.ONE_SEC);
         cy.get('.channel-info-block').then((el) => {
             el.click();
@@ -68,8 +68,11 @@ Cypress.Commands.add('uiAddRandomChannelToCustomPolicy', (numberOfChannels = 1) 
 });
 
 Cypress.Commands.add('uiVerifyCustomPolicyRow', (policyId, description, duration, appliedTo) => {
+    // * Assert row has correct description
     cy.get(`#customDescription-${policyId}`).should('include.text', description);
+    // * Assert row has correct duration
     cy.get(`#customDuration-${policyId}`).should('include.text', duration);
+    // * Assert row has correct team/channel counts
     cy.get(`#customAppliedTo-${policyId}`).should('include.text', appliedTo);
 });
 
@@ -79,10 +82,21 @@ Cypress.Commands.add('uiClickEditCustomPolicyRow', (policyId) => {
 });
 
 Cypress.Commands.add('uiVerifyPolicyResponse', (body, teamCount, channelCount, duration, displayName) => {
+    // * Assert response body exists
     assert.isNotNull(body);
+
+    // * Assert response body contains an ID
     assert.isNotNull(body.id);
+
+    // * Assert response body team_count matches supplied value
     expect(body.team_count).to.equal(teamCount);
+
+    // * Assert response body channel_count matches supplied value
     expect(body.channel_count).to.equal(channelCount);
+
+    // * Assert response body duration matches supplied value
     expect(body.post_duration).to.equal(duration);
+
+    // * Assert response body display_name matches supplied value
     expect(body.display_name).to.equal(displayName);
 });
