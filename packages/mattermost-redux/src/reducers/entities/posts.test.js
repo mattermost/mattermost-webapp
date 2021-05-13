5878bd68a5078bce54585770060156c64b35001b
Caleb Roseland <caleb.roseland@mattermost.com>
2021-05-13 08:57:04 -0500

    MM-32785: In CRT, Prevent incoming replies via WS and handle feature toggling without refresh. (#8027)
    * in CRT short-circuit replies and reset on toggle
    
    * fix: refactor action, feature flag
    
    * only reset when removing replies
    
    * typo
    
    * Revert "only reset when removing replies"
    
    This reverts commit 653a358380cd91b836c9b33bdfd4141182ac0cb6.
    
    * comment
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- packages/mattermost-redux/src/reducers/entities/posts.test.js
+++ packages/mattermost-redux/src/reducers/entities/posts.test.js
@@ -560,6 +560,36 @@ describe('postsInChannel', () => {
             expect(nextState).toEqual({});
         });
 
+        it('should do nothing when a reply-post comes and CRT is ON', () => {
+            const state = deepFreeze({
+                channel1: [],
+            });
+
+            const nextState = reducers.postsInChannel(state, {
+                type: PostTypes.RECEIVED_NEW_POST,
+                data: {id: 'post1', channel_id: 'channel1', root_id: 'parent1'},
+                features: {crtEnabled: true},
+            }, {}, {});
+
+            expect(nextState).toBe(state);
+            expect(nextState).toEqual({
+                channel1: [],
+            });
+        });
+
+        it('should reset when called for (e.g. when CRT is TOGGLED)', () => {
+            const state = deepFreeze({
+                channel1: [],
+            });
+
+            const nextState = reducers.postsInChannel(state, {
+                type: PostTypes.RESET_POSTS_IN_CHANNEL,
+            }, {}, {});
+
+            expect(nextState).not.toBe(state);
+            expect(nextState).toEqual({});
+        });
+
         it('should store the new post when the channel is empty', () => {
             const state = deepFreeze({
                 channel1: [],
