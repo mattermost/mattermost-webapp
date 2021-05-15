438c4fd1f18d0fdeaa64940ae16193feb2c43634
majidsajadi <majidsajadi93@gmail.com>
2021-05-15 22:12:16 +0430

    migrate tutorial to typescript

--- components/tutorial/tutorial_tip/index.ts
+++ components/tutorial/tutorial_tip/index.ts
@@ -1,18 +1,20 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import {bindActionCreators, Dispatch} from 'redux';
 
 import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
 import {getInt} from 'mattermost-redux/selectors/entities/preferences';
 import {savePreferences} from 'mattermost-redux/actions/preferences';
+import {GenericAction} from 'mattermost-redux/types/actions';
 
 import {closeMenu as closeRhsMenu} from 'actions/views/rhs';
 import {Preferences} from 'utils/constants';
+import {GlobalState} from 'types/store';
 
 import TutorialTip from './tutorial_tip';
 
-function mapStateToProps(state) {
+function mapStateToProps(state: GlobalState) {
     const currentUserId = getCurrentUserId(state);
     return {
         currentUserId,
@@ -20,7 +22,7 @@ function mapStateToProps(state) {
     };
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
     return {
         actions: bindActionCreators({
             closeRhsMenu,
