3ea8267f0526e8a4dec095de0396551efa2f0e01
Chetanya Kandhari <chetanya.kandhari@brightscout.com>
2021-05-21 11:15:25 +0530

    Code refactoring

--- components/file_attachment/file_attachment.tsx
+++ components/file_attachment/file_attachment.tsx
@@ -196,11 +196,13 @@ export default class FileAttachment extends React.PureComponent<Props, State> {
             );
         });
 
-        if (!defaultItems?.length && !pluginItems?.length) {
+        const isMenuVisible = defaultItems?.length || pluginItems?.length;
+        if (!isMenuVisible) {
             return null;
         }
 
-        if (defaultItems?.length && pluginItems?.length) {
+        const isDividerVisible = defaultItems?.length && pluginItems?.length
+        if (isDividerVisible) {
             divider = (
                 <li
                     id={`divider_file_${fileInfo.id}_plugins`}
