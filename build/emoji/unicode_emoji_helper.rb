5cd7791248a67900df5e57389f626438ace4abe6
Guillermo Vaya <guivaya@gmail.com>
2021-05-21 17:29:52 +0200

    change how we load sequences

--- build/emoji/unicode_emoji_helper.rb
+++ build/emoji/unicode_emoji_helper.rb
@@ -1,57 +1,42 @@
+require 'set'
 require_relative './constants.rb'
 
 module Mattermost
   class UnicodeEmojiHelper
-    include EmojiConstants
+
+    MATCH_SEQUENCE = '; RGI_Emoji_Modifier_Sequence  ;'
+    MATCH_FLAG = '; RGI_Emoji_Flag_Sequence      ;'
+    MATCH_ZWJ = '; RGI_Emoji_ZWJ_Sequence  ;'
 
     def initialize
-      path = File.join(File.dirname(__FILE__), './emoji-sequences.txt')
-      @emoji_sequences = File.readlines(path)
-    end
+      path = File.join(File.dirname(__FILE__), '/emoji-sequences.txt')
+      path2 = File.join(File.dirname(__FILE__), '/emoji-zwj-sequences.txt')
 
-    def all_emoji_modifier_bases
-      match_sequence = '; RGI_Emoji_Modifier_Sequence  ;'
-      match_flag = '; RGI_Emoji_Flag_Sequence      ;'
-      lines = @emoji_sequences.select { |l| l.include?(match_sequence) || l.include?(match_flag)}
-      hex_strings = lines.map { |l| /^[0-9A-F]{4,5}/.match(l)[0] }
-      integers = hex_strings.map { |s| s.to_i(16) }
-      integers.map { |i| [i].pack('U') }
-    end
+      lines = File.readlines(path).select { |l| l.include?(MATCH_SEQUENCE) || l.include?(MATCH_FLAG) }
+      lines = lines + File.readlines(path2).select { |l| l.include?(MATCH_ZWJ) }
+      # @emoji_sequences = @emoji_sequences + File.readlines(path)
+      hex_strings = lines.map { |l| /^([\dA-F]{4,5}(\s[\dA-F]{4,5})+)/.match(l)[0] }
+      @hex_sequences = hex_strings.reduce( Hash.new ) do |dict, hex|
+        sequence = hex.split(' ').map { |s| [s.to_i(16)].pack("U")}
+        key = sequence[0]
+        value = dict.fetch(key, Set.new)
+        value.add(sequence.join "")
+        dict[key] = value
 
-    def emoji_modifier_base?(code_point)
-      all_emoji_modifier_bases.include?(code_point)
+        dict
+      end
+
+      class << self
+        attr_accessor :hex_sequences
+      end
     end
 
     def emoji_modifier_sequences(emoji_modifier_chars)
-      return nil unless emoji_modifier_base?(emoji_modifier_chars[0])
-
-      if emoji_modifier_chars.include?(GENDER_MALE)
-        sequences = SKIN_TONE_MAP.each_key.map do |skin_tone_modifier|
-          [emoji_modifier_chars[0],
-           skin_tone_modifier,
-           ZWJ,
-           GENDER_MALE,
-           VS16].join('')
-        end
-      else
-        sequences = SKIN_TONE_MAP.each_key.map do |skin_tone_modifier|
-          [emoji_modifier_chars[0], skin_tone_modifier].join('')
-        end
-
-        # Add female-specific sequences and see if they exist in the system font
-        # by returning nil from the AppleEmojiExtractor
-        sequences << SKIN_TONE_MAP.each_key.map do |skin_tone_modifier|
-          [emoji_modifier_chars[0],
-           skin_tone_modifier,
-           ZWJ,
-           GENDER_FEMALE,
-           VS16].join('')
-        end
-
-        sequences.flatten!
+      result = @hex_sequences[emoji_modifier_chars[0]]
+      if (result == nil || result.size == 0)
+        return nil
       end
-
-      sequences
+      result.to_a
     end
   end
 end
