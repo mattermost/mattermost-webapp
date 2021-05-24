60a55e20f3781136255ff77130ff59a87ccf8f0a
Chetanya Kandhari <chetanya.kandhari@brightscout.com>
2021-05-24 12:18:06 +0530

    Update tests props

--- components/file_attachment/file_attachment.test.tsx
+++ components/file_attachment/file_attachment.test.tsx
@@ -39,6 +39,9 @@ describe('FileAttachment', () => {
         index: 3,
         canDownloadFiles: true,
         enableSVGs: false,
+        enablePublicLink: false,
+        pluginMenuItems: [],
+        handleFileDropdownOpened: jest.fn(),
     };
 
     test('should match snapshot, regular file', () => {
