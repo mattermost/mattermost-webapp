14b264c7ac4395efad8253b72be2db5ffacbb3aa
Kyriakos Ziakoulis <cooxlee@gmail.com>
2021-05-26 17:15:02 +0300

    MM-35932: changes strings due to re-packaging

--- components/admin_console/license_settings/trial_license_card/trial_license_card.tsx
+++ components/admin_console/license_settings/trial_license_card/trial_license_card.tsx
@@ -36,7 +36,7 @@ const TrialLicenseCard: React.FC<Props> = ({license}: Props) => {
             return (
                 <FormattedMarkdownMessage
                     id='admin.license.trialCard.description.expiringToday'
-                    defaultMessage='Your free trial expires **Today at {time}**. Visit our customer portal to purchase a license now to continue using E10 & E20 features after trial ends'
+                    defaultMessage='Your free trial expires **Today at {time}**. Visit our customer portal to purchase a license now to continue using Mattermost Professional and Enterprise features after trial ends'
                     values={{
                         time: moment(endDate).endOf('day').format('h:mm a ') + moment().tz(getBrowserTimezone()).format('z'),
                     }}
@@ -47,7 +47,7 @@ const TrialLicenseCard: React.FC<Props> = ({license}: Props) => {
         return (
             <FormattedMarkdownMessage
                 id='admin.license.trialCard.description'
-                defaultMessage='Your free trial will expire in **{daysCount} {daysCount, plural, one {day} other {days}}**. Visit our customer portal to purchase a license now to continue using E10 & E20 features after trial ends.'
+                defaultMessage='Your free trial will expire in **{daysCount} {daysCount, plural, one {day} other {days}}**. Visit our customer portal to purchase a license now to continue using Mattermost Professional and Enterprise features after trial ends.'
                 values={{
                     daysCount: daysToEndLicense,
                 }}
@@ -61,7 +61,7 @@ const TrialLicenseCard: React.FC<Props> = ({license}: Props) => {
                 <div className='TrialLicenseCard__text-title'>
                     <FormattedMessage
                         id='admin.license.trialCard.licenseExpiring'
-                        defaultMessage='You’re currently on a free trial of our E20 license.'
+                        defaultMessage='You’re currently on a free trial of our Mattermost Enterprise license.'
                     />
                 </div>
                 <div className='RenewLicenseCard__text-description'>
