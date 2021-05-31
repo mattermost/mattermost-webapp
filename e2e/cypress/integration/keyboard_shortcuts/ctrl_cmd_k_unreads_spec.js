2e77b048abbbeccfb73c9f317ca02411396176da
Mohammed Zubair Ahmed <m-zubairahmed@protonmail.com>
2021-05-31 20:33:25 +0530

    few improvments

--- e2e/cypress/integration/keyboard_shortcuts/ctrl_cmd_k_unreads_spec.js
+++ e2e/cypress/integration/keyboard_shortcuts/ctrl_cmd_k_unreads_spec.js
@@ -116,30 +116,38 @@ describe('Keyboard Shortcuts', () => {
         cy.visit(`/${team1.name}/channels/town-square`);
 
         // # Post message in other channels by otherUser
-        teamAndChannels[0].channels.forEach((channel) => {
+        cy.postMessageAs({
+            sender: otherUser,
+            message: `Message on the ${teamAndChannels[0].channels[0].display_name}`,
+            channelId: teamAndChannels[0].channels[0].id,
+        }).then(() => {
             cy.postMessageAs({
                 sender: otherUser,
-                message: `Message on the ${channel.display_name}`,
-                channelId: channel.id,
-            });
-        });
+                message: `Message on the ${teamAndChannels[0].channels[1].display_name}`,
+                channelId: teamAndChannels[0].channels[1].id,
+            }).then(() => {
+                cy.postMessageAs({
+                    sender: otherUser,
+                    message: `Message on the ${teamAndChannels[0].channels[2].display_name}`,
+                    channelId: teamAndChannels[0].channels[2].id,
+                }).then(() => {
+                    // # Press keyboard shortcut for channel switcher
+                    cy.get('#post_textbox').cmdOrCtrlShortcut('k');
 
-        // # Wait a little for unread channel indicators to show up
-        cy.wait(TIMEOUTS.FIVE_SEC);
+                    // * Verify channel switcher shows up
+                    cy.get('.a11y__modal.channel-switcher').should('exist').and('be.visible').as('channelSwitcherDialog');
 
-        // # Press keyboard shortcut for channel switcher
-        cy.get('#post_textbox').cmdOrCtrlShortcut('k');
+                    // * Verify the focus is on switchers input field
+                    cy.focused().should('have.id', 'quickSwitchInput');
 
-        // * Verify channel switcher shows up
-        cy.get('.a11y__modal.channel-switcher').should('exist').and('be.visible').as('channelSwitcherDialog');
-
-        // * Verify the focus is on switchers input field
-        cy.focused().should('have.id', 'quickSwitchInput');
-
-        cy.get('@channelSwitcherDialog').within(() => {
-            // * Verify all unread channels names are showing up in the dialogs list
-            teamAndChannels[0].channels.forEach((channel) => {
-                cy.findByText(channel.display_name).should('be.visible');
+                    // * Verify all unread channels names are showing up in the dialogs list
+                    cy.get('@channelSwitcherDialog').within(() => {
+                        // * Verify all unread channels names are showing up in the dialogs list
+                        teamAndChannels[0].channels.forEach((channel) => {
+                            cy.findByText(channel.display_name).should('be.visible');
+                        });
+                    });
+                });
             });
         });
     });
