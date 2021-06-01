4383a4eb57e6623fa8c739aed0c2d5f9930c1516
Mohammed Zubair Ahmed <m-zubairahmed@protonmail.com>
2021-06-01 17:05:45 +0530

    test

--- e2e/cypress/integration/channel_settings/channel_header_spec.js
+++ e2e/cypress/integration/channel_settings/channel_header_spec.js
@@ -4,62 +4,79 @@
 import {getRandomId} from '../../utils';
 import * as TIMEOUTS from '../../fixtures/timeouts';
 
-// Hover effect exists to add a channel description / header (when not already present)
-// –––––––––––––––––––––––––
-
-//     Create a new public channel
-//     Hover over "Add a channel description" just below the channel name in the center channel  - ("Edit") should appear to the right when you hover
-//     Click on "Add a channel description" after "(Edit)" appears
-//     Add a description in the edit text input box and hit Enter
-//     Hover your mouse over the channel description/header just below the channel name in the center channel
-
-// Note: Follow the steps again for a private channel, DM and GM — expected behavior is the same across all channels
-// Expected
-
-//     After you enter a channel description you should no longer see "(Edit)" appear to the right of the channel description/header when you hove and clicking does nothing.
-//     The header text you entered is displayed
 describe('Channel Settings', () => {
-    let testUser;
     let testTeam;
+    let user1;
+    let admin;
 
     before(() => {
-        cy.apiInitSetup({loginAfter: true}).then(({team, user}) => {
-            testTeam = team;
-            testUser = user;
+        cy.apiGetMe().then(({user: adminUser}) => {
+            admin = adminUser;
 
-            cy.visit(`/${testTeam.name}/channels/town-square`);
+            cy.apiInitSetup({loginAfter: false}).then(({team, user}) => {
+                testTeam = team;
+                user1 = user;
+
+                cy.visit(`/${testTeam.name}/channels/town-square`);
+            });
         });
     });
 
     beforeEach(() => {
-        cy.apiLogin(testUser);
+        cy.apiAdminLogin();
     });
 
     it('Hover effect exists to add a channel description / header (when not already present)', () => {
-        // # Create a new public channel
-        cy.apiCreateChannel(testTeam.id, `pub${getRandomId()}`, 'public', 'O').then(({channel: publicChannel}) => {
-            // # Go to new public channel
-            cy.visit(`/${testTeam.name}/channels/${publicChannel.name}`);
+        // # Create a new public channel and then private channel
+        ['O', 'P'].forEach((channelType) => {
+            cy.apiCreateChannel(testTeam.id, `chan${getRandomId()}`, 'chan', channelType).then(({channel}) => {
+                // # Go to new channel
+                cy.visit(`/${testTeam.name}/channels/${channel.name}`);
 
-            // # wait a little for channel to load
-            cy.wait(TIMEOUTS.FIVE_SEC);
+                // # Do the header test with public and private channel respectively
+                hoverOnChannelDescriptionAndVerifyBehavior(channel.display_name);
+            });
+        });
 
-            hoverOnChannelDescriptionAndVerifyBehavior();
+        // Create Dm with admin and user 1
+        cy.apiCreateDirectChannel([user1.id, user1.id]).then(() => {
+            // # Go to DM
+            cy.visit(`/${testTeam.name}/messages/@${user1.username}`);
+
+            // # Do the header test with DM
+            hoverOnChannelDescriptionAndVerifyBehavior('', true);
+        });
+
+        // # Create another user and add to the team
+        cy.apiCreateUser().then(({user: user2}) => {
+            cy.apiAddUserToTeam(testTeam.id, user2.id).then(() => {
+                // # Create a Gm with admin, user1 and user 2
+                cy.apiCreateGroupChannel([user2.id, user1.id, admin.id]).then(({channel}) => {
+                    // # Visit the channel using the name using the channels route
+                    cy.visit(`/${testTeam.name}/channels/${channel.name}`);
+
+                    // # Do the header test with GM
+                    hoverOnChannelDescriptionAndVerifyBehavior();
+                });
+            });
         });
     });
 });
 
 function hoverOnChannelDescriptionAndVerifyBehavior() {
-    const channelDescriptionText = `channel description ${getRandomId()}`;
+    const channelDescriptionText = `test description ${getRandomId()}`;
+
+    // # wait a little for channel to load
+    cy.wait(TIMEOUTS.FIVE_SEC);
 
     // # Scan within channel header description area
-    cy.get('#channelHeaderDescription').should('be.visible').as('channelHeaderDescription').within(() => {
+    cy.get('#channelHeaderDescription').should('be.visible').within(() => {
         // * Verify that empty header text is visible and click it
         cy.findByText('Add a channel description').should('be.visible').click();
     });
 
     // # Scan inside the channel header modal
-    cy.get('.a11y__modal').should('be.visible').within(() => {
+    cy.get('.a11y__modal.modal-dialog').should('be.visible').within(() => {
         // # Enter a channel description
         cy.findByTestId('edit_textbox').should('exist').clear().type(channelDescriptionText);
 
@@ -67,8 +84,11 @@ function hoverOnChannelDescriptionAndVerifyBehavior() {
         cy.findByText('Save').should('be.visible').click();
     });
 
-    // * Verify that new channel header is set
-    cy.get('#channel-header').should('be.visible').within(() => {
-        cy.findByText(channelDescriptionText).should('be.visible');
+    cy.get('#channelHeaderDescription').should('be.visible').within(() => {
+        // * Verify that new channel header is set and click it
+        cy.findAllByText(channelDescriptionText).should('be.visible').click({multiple: true, force: true});
     });
+
+    // * Also check clicking on it doesnt open the edit modal once again
+    cy.get('.a11y__modal.modal-dialog').should('not.exist');
 }
