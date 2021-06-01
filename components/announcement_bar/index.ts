66dd434e9b4a215a8ff0db55a23c06fe9786a423
defectivepixel <majidsajadi93@gmail.com>
2021-06-01 14:07:56 +0430

    ref: migrate announcement_bar to ts (#8133)
    Co-authored-by: Maria A Nunez <maria.nunez@mattermost.com>

--- components/announcement_bar/index.ts
+++ components/announcement_bar/index.ts
@@ -2,29 +2,28 @@
 // See LICENSE.txt for license information.
 
 import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import {bindActionCreators, Dispatch} from 'redux';
 
 import {Permissions} from 'mattermost-redux/constants';
-import {getCurrentUser} from 'mattermost-redux/selectors/entities/users';
 import {haveISystemPermission} from 'mattermost-redux/selectors/entities/roles';
 import {getConfig, getLicense, warnMetricsStatus as getWarnMetricsStatus} from 'mattermost-redux/selectors/entities/general';
 import {getDisplayableErrors} from 'mattermost-redux/selectors/errors';
 import {dismissError} from 'mattermost-redux/actions/errors';
 import {getStandardAnalytics} from 'mattermost-redux/actions/admin';
+import {GenericAction} from 'mattermost-redux/types/actions';
 
 import {dismissNotice} from 'actions/views/notice';
+import {GlobalState} from 'types/store';
 
-import AnnouncementBarController from './announcement_bar_controller.jsx';
+import AnnouncementBarController from './announcement_bar_controller';
 
-function mapStateToProps(state) {
+function mapStateToProps(state: GlobalState) {
     const canViewSystemErrors = haveISystemPermission(state, {permission: Permissions.MANAGE_SYSTEM});
     const license = getLicense(state);
     const config = getConfig(state);
-    const user = getCurrentUser(state);
     const errors = getDisplayableErrors(state);
     const warnMetricsStatus = getWarnMetricsStatus(state);
 
-    const totalUsers = state.entities.admin.analytics.TOTAL_USERS;
     let latestError = null;
     if (errors && errors.length >= 1) {
         latestError = errors[0];
@@ -33,15 +32,13 @@ function mapStateToProps(state) {
     return {
         license,
         config,
-        user,
         canViewSystemErrors,
         latestError,
-        totalUsers,
         warnMetricsStatus,
     };
 }
 
-function mapDispatchToProps(dispatch) {
+function mapDispatchToProps(dispatch: Dispatch<GenericAction>) {
     const dismissFirstError = dismissError.bind(null, 0);
     return {
         actions: bindActionCreators({
