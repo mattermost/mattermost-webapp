9a9dc598361a727c227b15b29b23342c1e18ac3d
Mohammed Zubair Ahmed <m-zubairahmed@protonmail.com>
2021-06-05 02:37:26 +0530

    rev3

--- e2e/cypress/integration/channel_settings/channel_header_spec.js
+++ e2e/cypress/integration/channel_settings/channel_header_spec.js
@@ -1,6 +1,12 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+// ***************************************************************
+// - [#] indicates a test step (e.g. # Go to a page)
+// - [*] indicates an assertion (e.g. * Check the title)
+// - Use element ID when selecting an element. Create one if none.
+// ***************************************************************
+
 import {getRandomId} from '../../utils';
 import * as TIMEOUTS from '../../fixtures/timeouts';
 
@@ -32,7 +38,7 @@ describe('Channel Settings', () => {
                 cy.visit(`/${testTeam.name}/channels/${channel.name}`);
 
                 // * Test hovering over the header with public and private channel
-                hoverOnChannelDescriptionAndVerifyBehavior(channel.display_name);
+                hoverOnChannelDescriptionAndVerifyBehavior();
             });
         });
 
