ed6d6bc338681e5de93e1935a210307d1a4fd312
Kyriakos Ziakoulis <cooxlee@gmail.com>
2021-06-09 13:52:44 +0300

    MM-35919: CRT makes threads nav menu sticky
    Threads nav menu, both in legacy and regular sidebar, is moved outside
    of the scrollable area, so it always 'sticky' at the same position.

--- components/legacy_sidebar/sidebar.jsx
+++ components/legacy_sidebar/sidebar.jsx
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 /* eslint-disable react/no-string-refs */
+/* eslint-disable max-lines */
 
 import $ from 'jquery';
 import React from 'react';
@@ -145,6 +146,11 @@ class LegacySidebar extends React.PureComponent {
          * Setting that enables user to view archived channels
          */
         viewArchivedChannels: PropTypes.bool,
+
+        /**
+         * Setting that enables user to view threads nav menu
+         */
+        isCollapsedThreadsEnabled: PropTypes.bool,
 
         actions: PropTypes.shape({
             close: PropTypes.func.isRequired,
@@ -533,6 +539,7 @@ class LegacySidebar extends React.PureComponent {
         const {orderedChannelIds} = this.state;
 
         const sectionsToHide = [SidebarChannelGroups.UNREADS, SidebarChannelGroups.FAVORITE];
+        let shownIndex = 0;
 
         return (
             <Scrollbars
@@ -550,7 +557,6 @@ class LegacySidebar extends React.PureComponent {
                     id='sidebarChannelContainer'
                     className='nav-pills__container'
                 >
-                    <GlobalThreadsLink/>
                     {orderedChannelIds.map((sec) => {
                         const section = {
                             type: sec.type,
@@ -565,6 +571,12 @@ class LegacySidebar extends React.PureComponent {
                         const sectionId = `${section.type}Channel`;
                         const ariaLabel = section.name.toLowerCase();
 
+                        let sectionHeaderClassName = 'sidebar-section__header';
+                        if (shownIndex === 0 && this.props.isCollapsedThreadsEnabled) {
+                            sectionHeaderClassName += ' margin-top--none';
+                        }
+                        shownIndex += 1;
+
                         return (
                             <ul
                                 key={section.type}
@@ -573,7 +585,7 @@ class LegacySidebar extends React.PureComponent {
                                 id={sectionId + 'List'}
                                 tabIndex='-1'
                             >
-                                <li className='sidebar-section__header'>
+                                <li className={sectionHeaderClassName}>
                                     <h4
                                         role='presentation'
                                         id={sectionId}
@@ -723,6 +735,9 @@ class LegacySidebar extends React.PureComponent {
                 <div className='sidebar--left__icons'>
                     <Pluggable pluggableName='LeftSidebarHeader'/>
                 </div>
+
+                <GlobalThreadsLink/>
+
                 <div
                     id='lhsList'
                     role='application'
