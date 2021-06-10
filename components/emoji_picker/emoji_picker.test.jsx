087ca20fa4407071ed9a57b73ca08e2e231187af
Guillermo Vaya <guivaya@gmail.com>
2021-06-10 10:23:39 +0200

    fix categories for emoji-picker

--- components/emoji_picker/emoji_picker.test.jsx
+++ components/emoji_picker/emoji_picker.test.jsx
@@ -80,11 +80,11 @@ describe('components/emoji_picker/EmojiPicker', () => {
         };
 
         // Nine categories as there is no recent caterogry
-        expect(wrapper.find(EmojiPickerCategory).length).toBe(9);
+        expect(wrapper.find(EmojiPickerCategory).length).toBe(10);
         expect(wrapper.find(EmojiPickerCategory).find({selected: true}).length).toBe(1);
 
         expect(wrapper.find(EmojiPickerCategorySection).length).toBe(1);
-        expect(wrapper.find(EmojiPickerCategorySection).find({categoryName: 'people'}).length).toBe(1);
+        expect(wrapper.find(EmojiPickerCategorySection).find({categoryName: 'smileys-emotion'}).length).toBe(1);
     });
 
     test('Recent category should exist if there are recent emojis', () => {
