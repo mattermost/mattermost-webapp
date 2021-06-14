8f93486ae01a900d2ee4f38bd5c84d36cb1cef79
Guillermo Vaya <guivaya@gmail.com>
2021-06-14 18:38:06 +0200

    make linter happy

--- components/post_view/post_reaction/post_reaction.tsx
+++ components/post_view/post_reaction/post_reaction.tsx
@@ -10,7 +10,7 @@ import {Dispatch} from 'redux';
 
 import Permissions from 'mattermost-redux/constants/permissions';
 
-import {CustomEmoji, Emoji, SystemEmoji} from 'mattermost-redux/types/emojis';
+import {Emoji} from 'mattermost-redux/types/emojis';
 
 import {Locations} from 'utils/constants';
 import {localizeMessage} from 'utils/utils.jsx';
