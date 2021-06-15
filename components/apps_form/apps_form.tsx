6470083a36d6b02554a7de7563117b97d5131c61
Daniel Espino García <larkox@gmail.com>
2021-06-15 12:10:11 +0200

    Handle missing field in error response

--- components/apps_form/apps_form.tsx
+++ components/apps_form/apps_form.tsx
@@ -95,12 +95,20 @@ export class AppsForm extends React.PureComponent<Props, State> {
             state.formError = formError;
         }
 
-        if (fieldErrors &&
-            Object.keys(fieldErrors).length >= 0 &&
-            checkIfErrorsMatchElements(fieldErrors as any, elements)
-        ) {
+        if (fieldErrors && Object.keys(fieldErrors).length >= 0) {
             hasErrors = true;
-            state.fieldErrors = fieldErrors;
+            if (checkIfErrorsMatchElements(fieldErrors as any, elements)) {
+                state.fieldErrors = fieldErrors;
+            } else if (!state.formError) {
+                const field = Object.keys(fieldErrors)[0];
+                state.formError = this.props.intl.formatMessage({
+                    id: 'apps.error.responses.unknown_field_error',
+                    defaultMessage: 'Received an error for an unkown field. Field name: `{field}`. Error: `{error}`.',
+                }, {
+                    field,
+                    error: fieldErrors[field],
+                });
+            }
         }
 
         if (hasErrors) {
@@ -175,7 +183,7 @@ export class AppsForm extends React.PureComponent<Props, State> {
             break;
         default:
             hasErrors = true;
-            this.updateErrors([], undefined, this.context.intl.formatMessage({
+            this.updateErrors([], undefined, this.props.intl.formatMessage({
                 id: 'apps.error.responses.unknown_type',
                 defaultMessage: 'App response type not supported. Response type: {type}.',
             }, {
@@ -192,7 +200,7 @@ export class AppsForm extends React.PureComponent<Props, State> {
     };
 
     performLookup = async (name: string, userInput: string): Promise<AppSelectOption[]> => {
-        const intl = this.context.intl;
+        const intl = this.props.intl;
         const field = this.props.form.fields.find((f) => f.name === name);
         if (!field) {
             return [];
@@ -300,7 +308,7 @@ export class AppsForm extends React.PureComponent<Props, State> {
                     return;
                 case AppCallResponseTypes.OK:
                 case AppCallResponseTypes.NAVIGATE:
-                    this.updateErrors([], undefined, this.context.intl.formatMessage({
+                    this.updateErrors([], undefined, this.props.intl.formatMessage({
                         id: 'apps.error.responses.unexpected_type',
                         defaultMessage: 'App response type was not expected. Response type: {type}.',
                     }, {
@@ -308,7 +316,7 @@ export class AppsForm extends React.PureComponent<Props, State> {
                     }));
                     return;
                 default:
-                    this.updateErrors([], undefined, this.context.intl.formatMessage({
+                    this.updateErrors([], undefined, this.props.intl.formatMessage({
                         id: 'apps.error.responses.unknown_type',
                         defaultMessage: 'App response type not supported. Response type: {type}.',
                     }, {
