4c9f4c9e3833b289e77a559a000e9f58c05c9325
arvinDarmawan <arvin.darmawan@gmail.com>
2021-06-22 17:42:23 +0700

    refactor: refactor code

--- components/suggestion/channel_mention_provider.jsx
+++ components/suggestion/channel_mention_provider.jsx
@@ -20,13 +20,11 @@ class ChannelMentionSuggestion extends Suggestion {
         const item = this.props.item;
 
         const channelName = item.channel.display_name;
-
-        let channelIcon;
-        if (item.channel.type === Constants.OPEN_CHANNEL) {
-            channelIcon = (<span className='suggestion-list__icon suggestion-list__icon--large'><i className='icon icon--no-spacing icon-globe'/></span>);
-        } else if (item.channel.type === Constants.PRIVATE_CHANNEL) {
-            channelIcon = (<span className='suggestion-list__icon suggestion-list__icon--large'><i className='icon icon--no-spacing icon-lock'/></span>);
-        }
+        const channelIcon = (
+            <span className='suggestion-list__icon suggestion-list__icon--large'>
+                <i className={`icon icon--no-spacing icon-${item.channel.type === Constants.OPEN_CHANNEL ? 'globe' : 'lock'}`}/>
+            </span>
+        );
 
         let className = 'mentions__name no-flex';
         if (isSelection) {
