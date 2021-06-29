674d580641d3aa39865694819d57fcf3a1cb8867
Daniel Espino García <larkox@gmail.com>
2021-06-29 10:11:55 +0200

    Add style linter (#8273)
    * Add style linter
    
    * Add order and scss specific plugin
    
    * Add recommended config
    
    * Fix lint
    
    * Fix border and system console tables

--- components/accordion.tsx
+++ components/accordion.tsx
@@ -3,8 +3,6 @@
 
 import React from 'react';
 
-import './accordion.scss';
-
 type Props = {
     defaultExpandedKey: string;
     children: (setExpanded: (expandedKey: string) => void, expandedKey: string) => React.ReactNode;
