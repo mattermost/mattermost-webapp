0e8b1291d0d970f7b0a066d6a3ec7caeefb69d3c
lapaz <lapaz17@gmail.com>
2021-06-30 02:39:50 +0300

    fixed small whitespace

--- components/screenshot_upload_modal/screenshot_upload_modal.tsx
+++ components/screenshot_upload_modal/screenshot_upload_modal.tsx
@@ -1,11 +1,10 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import PropTypes from 'prop-types';
 import React from 'react';
 import {Modal} from 'react-bootstrap';
 import Cropper from 'react-easy-crop';
-import { Point } from 'react-easy-crop/types';
+import {Point} from 'react-easy-crop/types';
 type Props = {
 
     /**
