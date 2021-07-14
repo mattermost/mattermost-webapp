5b59cd2ef4384c8b1a60d464201461597d9b6c61
Kyriakos Ziakoulis <cooxlee@gmail.com>
2021-07-14 11:52:42 +0300

    Uses react-window for virtualized thread list
    This commit replaces DynamicSizeList with react-window's FixedSizeList.
    The reason is mostly dynamic-virtualized-list doesn't support adding lists at the top.
    
    The most notable change is that all thread items now have the same height.

--- components/threading/global_threads/thread_list/virtualized_thread_list.tsx
+++ components/threading/global_threads/thread_list/virtualized_thread_list.tsx
@@ -1,91 +1,52 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {memo, useRef, useCallback, useMemo} from 'react';
+import React, {memo, useCallback, useMemo} from 'react';
 import AutoSizer from 'react-virtualized-auto-sizer';
-import {DynamicSizeList} from 'dynamic-virtualized-list';
+import {FixedSizeList} from 'react-window';
 
 import {$ID} from 'mattermost-redux/types/utilities';
 import {UserThread} from 'mattermost-redux/types/threads';
 
-import ThreadItem from '../thread_item';
+import Row from './virtualized_thread_list_row';
 
 type Props = {
     ids: Array<$ID<UserThread>>;
     selectedThreadId?: $ID<UserThread>;
 };
 
-const ITEMS_ON_SCREEN = 8;
-const OVERSCAN_COUNT_BACKWARD = 4;
-const OVERSCAN_COUNT_FORWARD = 4;
-
-const virtListStyles = {
-    maxHeight: '100%',
-    position: 'absolute',
-    top: '0',
-};
-
 function VirtualizedThreadList({
     ids,
     selectedThreadId,
 }: Props) {
-    const innerRef = useRef();
+    const itemKey = useCallback((index) => ids[index], [ids]);
 
-    const renderRow = ({itemId}: {itemId: $ID<UserThread>}) => {
-        return (
-            <ThreadItem
-                key={itemId}
-                threadId={itemId}
-                isSelected={selectedThreadId === itemId}
-            />
-        );
-    };
-
-    const canLoadMorePosts = useCallback(() => {}, []);
-
-    // dynamic-virtualized-list renders items in reverse order
-    // so we need to reverse the ids before feeding the list
-    // so that we get the correct order afterwards.
-    //
-    // TODO: have dynamic-virtualized-list to maybe accept a direction prop
-    const revIds = useMemo(() => [...ids].reverse(), [ids]);
-
-    // the last index is the top item
-    const initScrollToIndex = useCallback(() => ({
-        index: ids.length - 1,
-        position: 'start',
-    }), [ids]);
-
-    // the last indexes are the items at top.
-    const initRangeToRender = useMemo(() => {
-        const lastIndex = ids.length - 1;
-
-        return [
-            lastIndex - ITEMS_ON_SCREEN,
-            lastIndex,
-        ];
-    }, [ids]);
+    const data = useMemo(() => ({ids, selectedThreadId}), [ids, selectedThreadId]);
 
     return (
         <AutoSizer>
             {({height, width}) => (
-                <DynamicSizeList
-                    canLoadMorePosts={canLoadMorePosts}
+                <FixedSizeList
                     height={height}
-                    initRangeToRender={initRangeToRender}
-                    initScrollToIndex={initScrollToIndex}
-                    innerRef={innerRef}
-                    itemData={revIds}
-                    overscanCountBackward={OVERSCAN_COUNT_BACKWARD}
-                    overscanCountForward={OVERSCAN_COUNT_FORWARD}
-                    style={virtListStyles}
+                    itemCount={ids.length}
+                    itemData={data}
+                    itemKey={itemKey}
+                    itemSize={133}
+                    style={{willChange: 'auto'}}
                     width={width}
                 >
-                    {renderRow}
-                </DynamicSizeList>
+                    {Row}
+                </FixedSizeList>
             )}
         </AutoSizer>
     );
 }
 
-export default memo(VirtualizedThreadList);
+function areEqual(prevProps: Props, nextProps: Props) {
+    return (
+        prevProps.selectedThreadId === nextProps.selectedThreadId &&
+        prevProps.ids.join() === nextProps.ids.join()
+    );
+}
+
+export default memo(VirtualizedThreadList, areEqual);
