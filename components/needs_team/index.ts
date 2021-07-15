292c5b0669987c55e57698e4f8694ad0ad5be1c8
Joram Wilander <jwawilander@gmail.com>
2021-07-15 17:18:22 -0400

    On initial app load include threads in unreads for teams

--- components/needs_team/index.ts
+++ components/needs_team/index.ts
@@ -9,7 +9,7 @@ import {loadProfilesForDirect} from 'mattermost-redux/actions/users';
 import {fetchMyChannelsAndMembers, viewChannel} from 'mattermost-redux/actions/channels';
 import {getMyTeamUnreads, getTeamByName, selectTeam} from 'mattermost-redux/actions/teams';
 import {getGroups, getAllGroupsAssociatedToChannelsInTeam, getAllGroupsAssociatedToTeam, getGroupsByUserId} from 'mattermost-redux/actions/groups';
-import {getTheme} from 'mattermost-redux/selectors/entities/preferences';
+import {getTheme, isCollapsedThreadsEnabled} from 'mattermost-redux/selectors/entities/preferences';
 import {getLicense, getConfig} from 'mattermost-redux/selectors/entities/general';
 import {getCurrentUser} from 'mattermost-redux/selectors/entities/users';
 import {getCurrentTeamId, getMyTeams} from 'mattermost-redux/selectors/entities/teams';
@@ -43,6 +43,7 @@ function mapStateToProps(state: GlobalState, ownProps: OwnProps) {
     return {
         license,
         theme: getTheme(state),
+        collapsedThreads: isCollapsedThreadsEnabled(state),
         mfaRequired: checkIfMFARequired(currentUser, license, config, ownProps.match.url),
         currentUser,
         currentTeamId: getCurrentTeamId(state),
