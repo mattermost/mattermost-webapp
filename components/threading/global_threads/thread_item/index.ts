a39d1ac73c4c12407a81d491a54349ce5a55f53f
Mattermost Build <build@mattermost.com>
2021-07-20 15:59:51 +0200

    MM-36696: global threads list now virtualized (#8350) (#8427)
    * MM-36696: global threads virtualized list
    
    Global threads list now is virtualized to avoid rendering all thread
    items at once.
    This reduces the time needed to enter the threads route.
    
    * Fixes tests
    
    * Minor fixes
    
    * Fixes performance issues in global threads
    
    Mostly refactors the use of selectors to avoid re-rendering much.
    
    - Selectors that are not memoized (via let's say createSelector)
      should pase shallowEqual to useSelector hook
    - Selectors which take an object as an argument should memo that object
      so that they don't re-render on every re-render, since the custom
      object would be created each time regardless if the property at hand
      hadn't changed
    
    Other changes unrelated to performance:
    
    - spans should not contain divs ideally
    - ThreadItem now accepts a style prop to be feeded by Virtualized list
    - ThreadViewer had shouldComponentUpdate so it can't extend PureComponent
      in the same time
    
    PS: react-dev-tools' profiler showed that a lot of re-renders are
    happening due to "hooks changed" so I tried to tackle them,
    but it's not specific on which hooks changed,
    so it could be useContext which redux uses or the react ones or our custon ones.
    
    * Uses react-window for virtualized thread list
    
    This commit replaces DynamicSizeList with react-window's FixedSizeList.
    The reason is mostly dynamic-virtualized-list doesn't support adding lists at the top.
    
    The most notable change is that all thread items now have the same height.
    
    * Revert needless "optimization"
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    (cherry picked from commit cafeacf0c3ef393c60a03ce0c4faddf2ab29bafc)
    
    Co-authored-by: Kyriakos Z <3829551+koox00@users.noreply.github.com>

--- components/threading/global_threads/thread_item/index.ts
+++ components/threading/global_threads/thread_item/index.ts
@@ -1,4 +1,42 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export {default} from './thread_item';
+import {memo} from 'react';
+import {compose} from 'redux';
+import {connect} from 'react-redux';
+
+import {getPost, makeGetPostsForThread} from 'mattermost-redux/selectors/entities/posts';
+import {makeGetChannel} from 'mattermost-redux/selectors/entities/channels';
+import {makeGetDisplayName} from 'mattermost-redux/selectors/entities/users';
+import {getThread} from 'mattermost-redux/selectors/entities/threads';
+import {getCurrentRelativeTeamUrl} from 'mattermost-redux/selectors/entities/teams';
+
+import {GlobalState} from 'types/store';
+
+import ThreadItem, {OwnProps} from './thread_item';
+
+function makeMapStateToProps() {
+    const getPostsForThread = makeGetPostsForThread();
+    const getChannel = makeGetChannel();
+    const getDisplayName = makeGetDisplayName();
+
+    return (state: GlobalState, ownProps: OwnProps) => {
+        const {threadId} = ownProps;
+
+        const post = getPost(state, threadId);
+
+        return {
+            channel: getChannel(state, {id: post.channel_id}),
+            currentRelativeTeamUrl: getCurrentRelativeTeamUrl(state),
+            displayName: getDisplayName(state, post.user_id, true),
+            post,
+            postsInThread: getPostsForThread(state, {rootId: post.id}),
+            thread: getThread(state, threadId),
+        };
+    };
+}
+
+export default compose(
+    connect(makeMapStateToProps),
+    memo,
+)(ThreadItem) as React.FunctionComponent<OwnProps>;
