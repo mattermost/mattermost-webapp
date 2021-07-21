dab523365f1572dbaebf02c3c3de0448d4e0ea97
Carrie Warner (Mattermost) <74422101+cwarnermm@users.noreply.github.com>
2021-07-21 15:16:44 -0400

    Update components/help/components/formatting.tsx
    Co-authored-by: Caleb Roseland <caleb.roseland@mattermost.com>

--- components/help/components/formatting.tsx
+++ components/help/components/formatting.tsx
@@ -260,7 +260,7 @@ export default function HelpFormatting(): JSX.Element {
                     id='help.formatting.images.description'
                     defaultMessage='Create in-line images using an `!` followed by the alt text in square brackets and the link in normal brackets. Add hover text by placing it in quotes after the link. See the [product documentation](!https://docs.mattermost.com/messaging/formatting-text.html#in-line-images) for details on working with in-line images.'
                 />
-            </FormattedMessage>
+            </p>
             <h2 className='markdown__heading'>
                 <FormattedMessage
                     id='help.formatting.emojis.title'
