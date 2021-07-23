aa54338bed12a00b1a0bf8adfc06ba5c814fe7f0
Daniel Espino García <larkox@gmail.com>
2021-07-23 11:40:30 +0200

    Fix Lint

--- packages/mattermost-redux/src/reducers/entities/apps.test.ts
+++ packages/mattermost-redux/src/reducers/entities/apps.test.ts
@@ -11,52 +11,52 @@ describe('bindings', () => {
 
     test('No element get filtered', () => {
         const data = [
-                {
-                    app_id: '1',
-                    location: '/post_menu',
-                    bindings: [
-                        {
-                            location: 'locA',
-                            label: 'a',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '2',
-                    location: '/post_menu',
-                    bindings: [
-                        {
-                            location: 'locA',
-                            label: 'a',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '1',
-                    location: '/channel_header',
-                    bindings: [
-                        {
-                            location: 'locB',
-                            label: 'b',
-                            icon: 'icon',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '3',
-                    location: '/command',
-                    bindings: [
-                        {
-                            location: 'locC',
-                            label: 'c',
-                            call: {},
-                        },
-                    ],
-                },
-            ];
+            {
+                app_id: '1',
+                location: '/post_menu',
+                bindings: [
+                    {
+                        location: 'locA',
+                        label: 'a',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '2',
+                location: '/post_menu',
+                bindings: [
+                    {
+                        location: 'locA',
+                        label: 'a',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '1',
+                location: '/channel_header',
+                bindings: [
+                    {
+                        location: 'locB',
+                        label: 'b',
+                        icon: 'icon',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '3',
+                location: '/command',
+                bindings: [
+                    {
+                        location: 'locC',
+                        label: 'c',
+                        call: {},
+                    },
+                ],
+            },
+        ];
 
         const state = Reducers.bindings(
             initialState,
@@ -71,89 +71,89 @@ describe('bindings', () => {
 
     test('Invalid channel header get filtered', () => {
         const data = [
-                {
-                    app_id: '1',
-                    location: '/post_menu',
-                    bindings: [
-                        {
-                            location: 'locA',
-                            label: 'a',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '2',
-                    location: '/post_menu',
-                    bindings: [
-                        {
-                            location: 'locA',
-                            label: 'a',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '1',
-                    location: '/channel_header',
-                    bindings: [
-                        {
-                            location: 'locB',
-                            label: 'b',
-                            icon: 'icon',
-                            call: {},
-                        },
-                        {
-                            location: 'locC',
-                            label: 'c',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '2',
-                    location: '/channel_header',
-                    bindings: [
-                        {
-                            location: 'locB',
-                            icon: 'icon',
-                            call: {},
-                        },
-                        {
-                            location: 'locC',
-                            label: 'c',
-                            icon: 'icon',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '3',
-                    location: '/channel_header',
-                    bindings: [
-                        {
-                            location: 'locB',
-                            call: {},
-                        },
-                        {
-                            location: 'locC',
-                            label: 'c',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '3',
-                    location: '/command',
-                    bindings: [
-                        {
-                            location: 'locC',
-                            label: 'c',
-                            call: {},
-                        },
-                    ],
-                },
-            ]
+            {
+                app_id: '1',
+                location: '/post_menu',
+                bindings: [
+                    {
+                        location: 'locA',
+                        label: 'a',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '2',
+                location: '/post_menu',
+                bindings: [
+                    {
+                        location: 'locA',
+                        label: 'a',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '1',
+                location: '/channel_header',
+                bindings: [
+                    {
+                        location: 'locB',
+                        label: 'b',
+                        icon: 'icon',
+                        call: {},
+                    },
+                    {
+                        location: 'locC',
+                        label: 'c',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '2',
+                location: '/channel_header',
+                bindings: [
+                    {
+                        location: 'locB',
+                        icon: 'icon',
+                        call: {},
+                    },
+                    {
+                        location: 'locC',
+                        label: 'c',
+                        icon: 'icon',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '3',
+                location: '/channel_header',
+                bindings: [
+                    {
+                        location: 'locB',
+                        call: {},
+                    },
+                    {
+                        location: 'locC',
+                        label: 'c',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '3',
+                location: '/command',
+                bindings: [
+                    {
+                        location: 'locC',
+                        label: 'c',
+                        call: {},
+                    },
+                ],
+            },
+        ];
 
         const state = Reducers.bindings(
             initialState,
@@ -168,71 +168,71 @@ describe('bindings', () => {
 
     test('Invalid post menu get filtered', () => {
         const data = [
-                {
-                    app_id: '1',
-                    location: '/post_menu',
-                    bindings: [
-                        {
-                            location: 'locA',
-                            call: {},
-                        },
-                        {
-                            location: 'locB',
-                            label: 'a',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '2',
-                    location: '/post_menu',
-                    bindings: [
-                        {
-                            location: 'locA',
-                            label: 'a',
-                            call: {},
-                        },
-                        {
-                            location: 'locB',
-                            label: 'b',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '3',
-                    location: '/post_menu',
-                    bindings: [
-                        {
-                            location: 'locA',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '1',
-                    location: '/channel_header',
-                    bindings: [
-                        {
-                            location: 'locB',
-                            label: 'b',
-                            icon: 'icon',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '3',
-                    location: '/command',
-                    bindings: [
-                        {
-                            location: 'locC',
-                            label: 'c',
-                            call: {},
-                        },
-                    ],
-                },
-            ];
+            {
+                app_id: '1',
+                location: '/post_menu',
+                bindings: [
+                    {
+                        location: 'locA',
+                        call: {},
+                    },
+                    {
+                        location: 'locB',
+                        label: 'a',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '2',
+                location: '/post_menu',
+                bindings: [
+                    {
+                        location: 'locA',
+                        label: 'a',
+                        call: {},
+                    },
+                    {
+                        location: 'locB',
+                        label: 'b',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '3',
+                location: '/post_menu',
+                bindings: [
+                    {
+                        location: 'locA',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '1',
+                location: '/channel_header',
+                bindings: [
+                    {
+                        location: 'locB',
+                        label: 'b',
+                        icon: 'icon',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '3',
+                location: '/command',
+                bindings: [
+                    {
+                        location: 'locC',
+                        label: 'c',
+                        call: {},
+                    },
+                ],
+            },
+        ];
 
         const state = Reducers.bindings(
             initialState,
@@ -247,99 +247,99 @@ describe('bindings', () => {
 
     test('Invalid commands get filtered', () => {
         const data = [
-                {
-                    app_id: '1',
-                    location: '/post_menu',
-                    bindings: [
-                        {
-                            location: 'locA',
-                            label: 'a',
-                            call: {},
-                        },
-                        {
-                            location: 'locB',
-                            label: 'a',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '1',
-                    location: '/channel_header',
-                    bindings: [
-                        {
-                            location: 'locB',
-                            label: 'b',
-                            icon: 'icon',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '3',
-                    location: '/command',
-                    bindings: [
-                        {
-                            location: 'locC',
-                            label: 'c',
-                            bindings: [
-                                {
-                                    location: 'subC1',
-                                    call: {},
-                                },
-                                {
-                                    location: 'subC2',
-                                    label: 'c2',
-                                    call: {},
-                                },
-                            ],
-                        },
-                        {
-                            location: 'locD',
-                            label: 'd',
-                            bindings: [
-                                {
-                                    location: 'subC1',
-                                    call: {},
-                                },
-                            ],
-                        },
-                    ],
-                },
-                {
-                    app_id: '1',
-                    location: '/command',
-                    bindings: [
-                        {
-                            location: 'locC',
-                            call: {},
-                        },
-                    ],
-                },
-                {
-                    app_id: '2',
-                    location: '/command',
-                    bindings: [
-                        {
-                            location: 'locC',
-                            label: 'c',
-                            call: {},
-                            bindings: [
-                                {
-                                    location: 'subC1',
-                                    label: 'c1',
-                                    call: {},
-                                },
-                                {
-                                    location: 'subC2',
-                                    label: 'c2',
-                                    call: {},
-                                },
-                            ],
-                        },
-                    ],
-                },
-            ];
+            {
+                app_id: '1',
+                location: '/post_menu',
+                bindings: [
+                    {
+                        location: 'locA',
+                        label: 'a',
+                        call: {},
+                    },
+                    {
+                        location: 'locB',
+                        label: 'a',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '1',
+                location: '/channel_header',
+                bindings: [
+                    {
+                        location: 'locB',
+                        label: 'b',
+                        icon: 'icon',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '3',
+                location: '/command',
+                bindings: [
+                    {
+                        location: 'locC',
+                        label: 'c',
+                        bindings: [
+                            {
+                                location: 'subC1',
+                                call: {},
+                            },
+                            {
+                                location: 'subC2',
+                                label: 'c2',
+                                call: {},
+                            },
+                        ],
+                    },
+                    {
+                        location: 'locD',
+                        label: 'd',
+                        bindings: [
+                            {
+                                location: 'subC1',
+                                call: {},
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                app_id: '1',
+                location: '/command',
+                bindings: [
+                    {
+                        location: 'locC',
+                        call: {},
+                    },
+                ],
+            },
+            {
+                app_id: '2',
+                location: '/command',
+                bindings: [
+                    {
+                        location: 'locC',
+                        label: 'c',
+                        call: {},
+                        bindings: [
+                            {
+                                location: 'subC1',
+                                label: 'c1',
+                                call: {},
+                            },
+                            {
+                                location: 'subC2',
+                                label: 'c2',
+                                call: {},
+                            },
+                        ],
+                    },
+                ],
+            },
+        ];
 
         const state = Reducers.bindings(
             initialState,
