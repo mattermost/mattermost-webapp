4bb5e6456167b1da43eaba6767092c7d9ff332e3
Kyriakos Ziakoulis <cooxlee@gmail.com>
2021-07-26 17:19:21 +0300

    Removes react-window
    Uses dynamic-virtualized-list instead of react-window for global threads
    list. This way we avoid having fixed height thread items and have
    dynamic ones instead.
    
    There is a hack needed to update the rendered list while adding new
    items at the top, we have to scroll by 1px and the list will update.
    It can be removed but the list won't update unless the user scrolls.

--- components/threading/global_threads/thread_list/virtualized_thread_list.tsx
+++ components/threading/global_threads/thread_list/virtualized_thread_list.tsx
@@ -1,42 +1,99 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {memo, useCallback, useMemo} from 'react';
+import React, {memo, useCallback, useEffect, useRef, useMemo} from 'react';
 import AutoSizer from 'react-virtualized-auto-sizer';
-import {FixedSizeList} from 'react-window';
+import {DynamicSizeList} from 'dynamic-virtualized-list';
 
 import {$ID} from 'mattermost-redux/types/utilities';
 import {UserThread} from 'mattermost-redux/types/threads';
 
-import Row from './virtualized_thread_list_row';
+import ThreadItem from '../thread_item';
 
 type Props = {
     ids: Array<$ID<UserThread>>;
     selectedThreadId?: $ID<UserThread>;
 };
 
+const ITEMS_ON_SCREEN = 8;
+const OVERSCAN_COUNT_BACKWARD = 4;
+const OVERSCAN_COUNT_FORWARD = 4;
+
+const virtListStyles = {
+    maxHeight: '100%',
+    position: 'absolute',
+    top: '0',
+    willChange: 'auto',
+};
+
 function VirtualizedThreadList({
     ids,
     selectedThreadId,
 }: Props) {
-    const itemKey = useCallback((index) => ids[index], [ids]);
+    const innerRef = useRef();
+    const listRef = useRef<typeof DynamicSizeList>();
 
-    const data = useMemo(() => ({ids, selectedThreadId}), [ids, selectedThreadId]);
+    useEffect(() => {
+        // if ids length have changed hack to update the view
+        if (listRef.current) {
+            listRef.current.scrollTo(0, 1, true);
+        }
+    }, [ids.length]);
+
+    const renderRow = ({itemId}: {itemId: $ID<UserThread>}) => {
+        return (
+            <ThreadItem
+                key={itemId}
+                threadId={itemId}
+                isSelected={selectedThreadId === itemId}
+            />
+        );
+    };
+
+    const canLoadMorePosts = useCallback(() => {}, []);
+
+    const onItemsRendered = useCallback(() => {}, []);
+
+    // dynamic-virtualized-list renders items in reverse order
+    // so we need to reverse the ids before feeding the list
+    // so that we get the correct order afterwards.
+    const revIds = useMemo(() => [...ids].reverse(), [ids]);
+
+    // the last index is the top item
+    const initScrollToIndex = useCallback(() => ({
+        index: ids.length - 1,
+        position: 'start',
+    }), [ids]);
+
+    // the last indexes are the items at top.
+    const initRangeToRender = useMemo(() => {
+        const lastIndex = ids.length - 1;
+
+        return [
+            lastIndex - ITEMS_ON_SCREEN,
+            lastIndex,
+        ];
+    }, [ids]);
 
     return (
         <AutoSizer>
             {({height, width}) => (
-                <FixedSizeList
+                <DynamicSizeList
+                    ref={listRef}
+                    canLoadMorePosts={canLoadMorePosts}
                     height={height}
-                    itemCount={ids.length}
-                    itemData={data}
-                    itemKey={itemKey}
-                    itemSize={133}
-                    style={{willChange: 'auto'}}
+                    initRangeToRender={initRangeToRender}
+                    initScrollToIndex={initScrollToIndex}
+                    innerRef={innerRef}
+                    itemData={revIds}
+                    onItemsRendered={onItemsRendered}
+                    overscanCountBackward={OVERSCAN_COUNT_BACKWARD}
+                    overscanCountForward={OVERSCAN_COUNT_FORWARD}
+                    style={virtListStyles}
                     width={width}
                 >
-                    {Row}
-                </FixedSizeList>
+                    {renderRow}
+                </DynamicSizeList>
             )}
         </AutoSizer>
     );
