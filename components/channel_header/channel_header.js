43305988475748169f2315e7c3e920c4ad2b2653
Harrison Healey <harrisonmhealey@gmail.com>
2021-07-27 18:35:33 -0400

    MM-32342 Remove references to old sidebar from ChannelHeader

--- components/channel_header/channel_header.js
+++ components/channel_header/channel_header.js
@@ -83,7 +83,6 @@ class ChannelHeader extends React.PureComponent {
         }).isRequired,
         teammateNameDisplaySetting: PropTypes.string.isRequired,
         currentRelativeTeamUrl: PropTypes.string.isRequired,
-        isLegacySidebar: PropTypes.bool,
         announcementBarCount: PropTypes.number,
         customStatus: PropTypes.object,
         isCustomStatusEnabled: PropTypes.bool.isRequired,
@@ -259,7 +258,6 @@ class ChannelHeader extends React.PureComponent {
             rhsState,
             hasGuests,
             teammateNameDisplaySetting,
-            isLegacySidebar,
         } = this.props;
         const {formatMessage} = this.props.intl;
         const ariaLabelChannelHeader = Utils.localizeMessage('accessibility.sections.channelHeader', 'channel header region');
@@ -447,11 +445,7 @@ class ChannelHeader extends React.PureComponent {
                     popoverSize='lg'
                     style={{maxWidth: `${this.state.popoverOverlayWidth}px`, transform: `translate(${this.state.leftOffset}px, ${this.state.topOffset}px)`}}
                     placement='bottom'
-                    className={classNames(['channel-header__popover',
-                        {
-                            'chanel-header__popover--lhs_offset': this.props.hasMoreThanOneTeam,
-                            'chanel-header__popover--new_sidebar': !isLegacySidebar,
-                        }])}
+                    className={classNames('channel-header__popover', {'chanel-header__popover--lhs_offset': this.props.hasMoreThanOneTeam})}
                 >
                     <span
                         onClick={this.handleFormattedTextClick}
