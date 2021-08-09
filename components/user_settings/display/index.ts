5bf5879171e0b08ca9dc0d2516445fab2b9f0de1
Nevyana Angelova <nevyangelova@Nevyanas-MBP.fritz.box>
2021-08-09 22:43:32 +0200

    import selector

--- components/user_settings/display/index.ts
+++ components/user_settings/display/index.ts
@@ -10,13 +10,12 @@ import {GenericAction} from 'mattermost-redux/types/actions';
 import {savePreferences} from 'mattermost-redux/actions/preferences';
 import {getSupportedTimezones} from 'mattermost-redux/actions/general';
 import {autoUpdateTimezone} from 'mattermost-redux/actions/timezone';
-import {getConfig, getSupportedTimezones as getTimezones, getLicense} from 'mattermost-redux/selectors/entities/general';
+import {getConfig, getSupportedTimezones as getTimezones, getLicense, getFeatureFlagValue} from 'mattermost-redux/selectors/entities/general';
 import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
 import {get, isCollapsedThreadsAllowed, getCollapsedThreadsPreference} from 'mattermost-redux/selectors/entities/preferences';
 import {getUserTimezone} from 'mattermost-redux/selectors/entities/timezone';
 import {getUserCurrentTimezone} from 'mattermost-redux/utils/timezone_utils';
 
-import {getGlobalHeaderEnabled} from 'selectors/global_header';
 import {GlobalState} from 'types/store';
 import {Preferences} from 'utils/constants';
 
