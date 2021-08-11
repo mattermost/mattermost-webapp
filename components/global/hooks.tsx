ed9e6d84e5703bbd6502569d75bb9b2a33836bd3
Dean Whillier <dean@whillier.com>
2021-08-11 19:47:20 -0400

    new `useIsModalOpen` hook that returns a ref
    ... for use in callbacks

--- components/global/hooks.tsx
+++ components/global/hooks.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {MutableRefObject, useEffect} from 'react';
+import {MutableRefObject, useEffect, useRef} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {useLocation} from 'react-router';
 
@@ -13,6 +13,8 @@ import {Preferences, TutorialSteps, TopLevelProducts} from 'utils/constants';
 import {savePreferences} from 'mattermost-redux/actions/preferences';
 import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
 import {getInt} from 'mattermost-redux/selectors/entities/preferences';
+
+import {isModalOpen} from 'selectors/views/modals';
 
 import store from 'stores/redux_store';
 
@@ -98,4 +100,19 @@ export const useShowTutorialStep = (stepToShow: number, products?: ProductCompon
     }
 
     return showTutorialStep;
+};
+
+/**
+ * Hook that returns the current open state of the specified modal
+ * - returns both the direct boolean for regular use and a ref that contains the boolean for usage in a callback
+ */
+export const useIsModalOpen = (modalIdentifier: string): [boolean, React.RefObject<boolean>] => {
+    const modalOpenState = useSelector((state: GlobalState) => isModalOpen(state, modalIdentifier));
+    const modalOpenStateRef = useRef(modalOpenState);
+
+    useEffect(() => {
+        modalOpenStateRef.current = modalOpenState;
+    }, [modalOpenState]);
+
+    return [modalOpenState, modalOpenStateRef];
 };
