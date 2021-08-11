e1eee2a0a8ae55bfe90b247c0a4a734927aae155
Dean Whillier <dean@whillier.com>
2021-08-11 16:10:12 -0400

    New channel sidebar header component for GH

--- components/sidebar/sidebar_header/sidebar_header.tsx
+++ components/sidebar/sidebar_header/sidebar_header.tsx
@@ -1,61 +1,99 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, {useState} from 'react';
+import {useSelector} from 'react-redux';
+import styled from 'styled-components';
+import {Tooltip} from 'react-bootstrap';
 
+import Flex from '@mattermost/compass-components/utilities/layout/Flex';
+import Heading from '@mattermost/compass-components/components/heading';
+import IconButton from '@mattermost/compass-components/components/icon-button';
+
+import {getCurrentTeam} from 'mattermost-redux/selectors/entities/teams';
+import {getCurrentUser} from 'mattermost-redux/selectors/entities/users';
+import {getInt} from 'mattermost-redux/selectors/entities/preferences';
+
+import {GlobalState} from 'types/store';
+import Constants, {Preferences, TutorialSteps} from 'utils/constants';
 import * as Utils from 'utils/utils.jsx';
-import StatusDropdown from 'components/status_dropdown';
 
-import SidebarHeaderDropdown from './dropdown';
+import OverlayTrigger from 'components/overlay_trigger';
+import MenuWrapper from 'components/widgets/menu/menu_wrapper';
+import MainMenu from 'components/main_menu';
+import MenuTutorialTip from 'components/tutorial/menu_tutorial_tip';
 
-type Props = {
-    globalHeaderEnabled: boolean;
-}
+const SidebarHeaderContainer = styled(Flex).attrs(() => ({
+    element: 'header',
+    row: true,
+    justify: 'space-between',
+    alignment: 'center',
+}))`
+    height: 52px;
+    padding: 0 16px;
 
-type State = {
-    isMobile: boolean;
-}
+    h1 {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        color: var(--sidebar-header-text-color);
 
-export default class SidebarHeader extends React.PureComponent<Props, State> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            isMobile: Utils.isMobile(),
-        };
+        #SidebarContainer & {
+            font-family: Metropolis, sans-serif;
+        }
     }
 
-    componentDidMount() {
-        window.addEventListener('resize', this.handleResize);
+    .dropdown-menu {
+        position: absolute;
+        transform: translate(-100%, 4px);
+        margin-left: 100%;
+        min-width: 210px;
+        max-width: 210px;
     }
+`;
 
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.handleResize);
-    }
+const SidebarHeader: React.FC = (): JSX.Element => {
+    const currentTeam = useSelector((state: GlobalState) => getCurrentTeam(state));
+    const currentUser = useSelector((state: GlobalState) => getCurrentUser(state));
+    const showTutorialTip = useSelector((state: GlobalState) => getInt(state, Preferences.TUTORIAL_STEP, currentUser.id)) === TutorialSteps.MENU_POPOVER && !Utils.isMobile();
 
-    handleResize = () => {
-        const isMobile = Utils.isMobile();
-        this.setState({isMobile});
-    }
+    const [menuToggled, setMenuToggled] = useState(false);
 
-    render() {
-        const ariaLabel = Utils.localizeMessage('accessibility.sections.lhsHeader', 'team menu region');
+    const handleMenuToggle = () => {
+        setMenuToggled(!menuToggled);
+    };
 
-        return (
-            <div
-                id='lhsHeader'
-                aria-label={ariaLabel}
-                tabIndex={-1}
-                role='application'
-                className='SidebarHeader team__header theme a11y__region'
-                data-a11y-sort-order='5'
-            >
-                <div
-                    className='d-flex'
+    return (
+        <>
+            {showTutorialTip && <MenuTutorialTip onBottom={false}/>}
+            <SidebarHeaderContainer>
+                <OverlayTrigger
+                    delayShow={Constants.OVERLAY_TIME_DELAY}
+                    placement='bottom'
+                    overlay={<Tooltip id='team-name__tooltip'>{currentTeam.description}</Tooltip>}
                 >
-                    {!this.state.isMobile && !this.props.globalHeaderEnabled && <StatusDropdown/>}
-                    <SidebarHeaderDropdown/>
-                </div>
-            </div>
-        );
-    }
-}
+                    <Heading
+                        element='h1'
+                        size={200}
+                        margin='none'
+                    >
+                        {currentTeam.display_name}
+                    </Heading>
+                </OverlayTrigger>
+                <MenuWrapper onToggle={handleMenuToggle}>
+                    <IconButton
+                        icon='dots-vertical'
+                        size='sm'
+                        compact={true}
+                        inverted={true}
+                        toggled={menuToggled}
+                        onClick={() => {}}
+                    />
+                    <MainMenu id='sidebarDropdownMenu'/>
+                </MenuWrapper>
+            </SidebarHeaderContainer>
+        </>
+    );
+};
+
+export default SidebarHeader;
