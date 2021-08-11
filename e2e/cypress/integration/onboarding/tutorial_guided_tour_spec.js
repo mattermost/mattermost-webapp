69fd114075d31a043faa6b2cf5a4bcdd0d5565b0
neallred <neallred@protonmail.com>
2021-08-11 13:03:21 -0500

    Add e2e test update

--- e2e/cypress/integration/onboarding/tutorial_guided_tour_spec.js
+++ e2e/cypress/integration/onboarding/tutorial_guided_tour_spec.js
@@ -9,66 +9,48 @@
 
 // Stage: @prod
 // Group: @onboarding @smoke
+const sel = {
+    tip: '#tipButton',
+    tipNext: '#tipNextButton',
+    tipText: '.tip-overlay',
+}
 
 describe('Onboarding', () => {
-    let testUser;
-    let otherUser;
-    let testTeam;
-
-    // let config;
-
     before(() => {
-        // cy.apiGetConfig().then((_data) => {
-        cy.apiGetConfig().then(() => {
-            // ({config} = data);
-        });
-
-        // # Set Support Email setting
-        // const newSettings = {
-        //     SupportSettings: {
-        //         SupportEmail: 'feedback@mattermost.com',
-        //     },
-        // };
-        // cy.apiUpdateConfig(newSettings);
-
         // # Create new team and new user and visit Town Square channel
         cy.apiInitSetup().then(({team}) => {
-            testTeam = team;
+            cy.apiCreateUser({bypassTutorial: false}).then(({user}) => {
+                cy.apiAddUserToTeam(team.id, user.id);
 
-            cy.apiCreateUser({bypassTutorial: false}).then(({user: user2}) => {
-                otherUser = user2;
-                cy.apiAddUserToTeam(testTeam.id, otherUser.id);
-            });
-
-            cy.apiCreateUser({bypassTutorial: false}).then(({user: user1}) => {
-                testUser = user1;
-                cy.apiAddUserToTeam(testTeam.id, testUser.id);
-
-                cy.apiLogin(testUser);
-                cy.visit(`/${testTeam.name}/channels/town-square`);
+                cy.apiLogin(user);
+                cy.visit(`/${team.name}/channels/town-square`);
             });
         });
     });
 
     it('Takes the user through the steps of using the app', () => {
         // tip 1: post message
-        cy.get('#create_post #tipButton').click();
-
-        cy.get('.tip-overlay').should('be.visible').
-            and('contain', 'Type your first message and select Enter to send it.');
-
-        cy.get('#tipNextButton').click();
+        cy.get(`#create_post ${sel.tip}`).click();
+        cy.get(sel.tipText).should('be.visible').
+            and('contain', 'Send a message');
+        cy.get(sel.tipNext).click();
 
         // tip 2: channels
-        // cy.get('#create_post #tipButton').click();
-        //
-        // cy.get('.tip-overlay').should('be.visible').
-        //     and('contain', 'Type your first message and select Enter to send it.');
-        //
-        // cy.get('#tipNextButton').click();
+        cy.get(`#sidebarItem_town-square ${sel.tip}`).click();
+        cy.get(sel.tipText).should('be.visible').
+            and('contain', 'Organize conversations in channels');
+        cy.get(sel.tipNext).click();
 
         // tip 3: add channels
-        // tip 4: admin
+        cy.get(`#lhsNavigator ${sel.tip}`).click();
+        cy.get(sel.tipText).should('be.visible').
+            and('contain', 'Create and join channels');
+        cy.get(sel.tipNext).click();
+
+        // tip 4: invite people/admin
+        cy.get(`#sidebarHeaderDropdownButton ${sel.tip}`).click();
+        cy.get(sel.tipText).should('be.visible').
+            and('contain', 'Invite people');
+        cy.get(sel.tipNext).click();
     });
 });
-
