e50bcf5f52f693f31e9fc0df4982baaa9ecf1f36
Michel Engelen <jsnerdic@gmail.com>
2021-08-13 10:33:30 +0200

    replaced calls to `UserAgent.isMobile()` with the new `UserAgent.isMobileWeb()`

--- selectors/global_header.ts
+++ selectors/global_header.ts
@@ -5,15 +5,13 @@ import Constants from 'utils/constants';
 import {get} from 'mattermost-redux/selectors/entities/preferences';
 import {GlobalState} from 'types/store';
 import {getFeatureFlagValue} from 'mattermost-redux/selectors/entities/general';
-import * as UserAgent from '../utils/user_agent';
+import {isMobileWeb} from '../utils/user_agent';
 
 const Preferences = Constants.Preferences;
-
-const isMobile = UserAgent.isMobile();
 
 export function getGlobalHeaderEnabled(state: GlobalState): boolean {
     const featureFlagEnabled = getFeatureFlagValue(state, 'GlobalHeader') === 'true';
     const userPreferenceEnabled = get(state, Preferences.CATEGORY_DISPLAY_SETTINGS, Preferences.GLOBAL_HEADER_DISPLAY, Preferences.GLOBAL_HEADER_DISPLAY_DEFAULT) === Preferences.GLOBAL_HEADER_DISPLAY_ON;
 
-    return !isMobile && featureFlagEnabled && userPreferenceEnabled;
+    return !isMobileWeb() && featureFlagEnabled && userPreferenceEnabled;
 }
