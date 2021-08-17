c04f2f3204a79ef33678da2e493f7cff1bcd28c3
Michel Engelen <32863416+michelengelen@users.noreply.github.com>
2021-08-17 20:54:17 +0200

    bugfix: Hide HistoryButtons in browser (#8571)
    * added conditional rendering of HistoryButtons subcomponent only for Desktop App
    
    * removed commented out condition in HistoryButton component
    
    (cherry picked from commit 5b731d49281b1e9bec3751553747a03781650949)

--- components/global/global_header.tsx
+++ components/global/global_header.tsx
@@ -10,6 +10,7 @@ import StatusDropdown from 'components/status_dropdown';
 import {getGlobalHeaderEnabled} from 'selectors/global_header';
 import {TutorialSteps} from 'utils/constants';
 import Pluggable from 'plugins/pluggable';
+import {isDesktopApp} from 'utils/user_agent';
 
 import GlobalSearchNav from './global_search_nav/global_search_nav';
 import ProductSwitcher from './product_switcher';
@@ -18,7 +19,7 @@ import HistoryButtons from './history_buttons';
 import UserGuideDropdown from './user_guide_dropdown';
 import AtMentionsButton from './at_mentions_button/at_mentions_button';
 import SavedPostsButton from './saved_posts_button/saved_posts_button';
-import SettingsButton from './settings_button//';
+import SettingsButton from './settings_button';
 import SettingsTip from './settings_tip';
 
 import {useCurrentProductId, useProducts, useShowTutorialStep} from './hooks';
@@ -103,7 +104,7 @@ const GlobalHeader = (): JSX.Element | null => {
             <LeftControls>
                 <ProductSwitcher/>
                 <ProductBranding/>
-                <HistoryButtons/>
+                {isDesktopApp() && <HistoryButtons/>}
             </LeftControls>
             <CenterControls>
                 {currentProductID !== null &&
