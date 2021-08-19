871687b2f012a16a7e9a86f5b0e24632d0108e63
Michel Engelen <jsnerdic@gmail.com>
2021-08-19 11:35:05 +0200

    fixed a missing replacement

--- selectors/global_header.ts
+++ selectors/global_header.ts
@@ -5,14 +5,15 @@ import Constants from 'utils/constants';
 import {get} from 'mattermost-redux/selectors/entities/preferences';
 import {GlobalState} from 'types/store';
 import {getFeatureFlagValue} from 'mattermost-redux/selectors/entities/general';
-import {isMobileViewPortSize, isMobile} from '../utils/user_agent';
+import * as UserAgent from '../utils/user_agent';
+import * as Utils from '../utils/utils';
 
 const Preferences = Constants.Preferences;
 
 export function getGlobalHeaderEnabled(state: GlobalState): boolean {
     const featureFlagEnabled = getFeatureFlagValue(state, 'GlobalHeader') === 'true';
     const userPreferenceEnabled = get(state, Preferences.CATEGORY_DISPLAY_SETTINGS, Preferences.GLOBAL_HEADER_DISPLAY, Preferences.GLOBAL_HEADER_DISPLAY_DEFAULT) === Preferences.GLOBAL_HEADER_DISPLAY_ON;
-    const isMobileView = isMobileViewPortSize() || isMobile();
+    const isMobileView = UserAgent.isMobile() || Utils.isMobile();
 
     return !isMobileView && featureFlagEnabled && userPreferenceEnabled;
 }
