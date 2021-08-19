8ef009763792965268a3bd375902215c1ababd20
Jesús Espino <jespinog@gmail.com>
2021-08-19 15:45:03 +0200

    Moving ModalController aside the GlobalHeader (#8621)
    Automatic Merge

--- components/root/root.jsx
+++ components/root/root.jsx
@@ -20,6 +20,7 @@ import {trackLoadTime} from 'actions/telemetry_actions.jsx';
 import {makeAsyncComponent} from 'components/async_load';
 import CompassThemeProvider from 'components/compass_theme_provider/compass_theme_provider';
 import GlobalHeader from 'components/global/global_header';
+import ModalController from 'components/modal_controller';
 import {HFTRoute, LoggedInHFTRoute} from 'components/header_footer_template_route';
 import IntlProvider from 'components/intl_provider';
 import NeedsTeam from 'components/needs_team';
@@ -364,6 +365,7 @@ export default class Root extends React.PureComponent {
                         to={`/${this.props.permalinkRedirectTeamName}/pl/:postid`}
                     />
                     <CompassThemeProvider theme={this.props.theme}>
+                        <ModalController/>
                         <GlobalHeader/>
                         <Switch>
                             {this.props.products?.map((product) => (
