9e987fa37578ad4652bf12163f22cf851201f27c
Dean Whillier <dean@whillier.com>
2021-08-24 12:19:08 -0400

    lint updates

--- components/widgets/menu/menu_items/menu_item_external_link.test.tsx
+++ components/widgets/menu/menu_items/menu_item_external_link.test.tsx
@@ -1,18 +1,21 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from "react";
-import { shallow } from "enzyme";
+import React from 'react';
+import {shallow} from 'enzyme';
 
-import { MenuItemExternalLinkImpl } from "./menu_item_external_link";
+import {MenuItemExternalLinkImpl} from './menu_item_external_link';
 
-describe("components/MenuItemExternalLink", () => {
-  test("should match snapshot", () => {
-    const wrapper = shallow(
-      <MenuItemExternalLinkImpl url="http://test.com" text="Whatever" />
-    );
+describe('components/MenuItemExternalLink', () => {
+    test('should match snapshot', () => {
+        const wrapper = shallow(
+            <MenuItemExternalLinkImpl
+                url='http://test.com'
+                text='Whatever'
+            />,
+        );
 
-    expect(wrapper).toMatchInlineSnapshot(`
+        expect(wrapper).toMatchInlineSnapshot(`
       <a
         href="http://test.com"
         rel="noopener noreferrer"
@@ -25,5 +28,5 @@ describe("components/MenuItemExternalLink", () => {
         </span>
       </a>
     `);
-  });
+    });
 });
