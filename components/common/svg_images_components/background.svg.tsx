d219348fd638eecad003394cdbdb5c361fdf520c
Dean Whillier <dean@whillier.com>
2021-08-27 00:26:53 -0400

    update cloud bg pill arrangment to match designs

--- components/common/svg_images_components/background.svg.tsx
+++ components/common/svg_images_components/background.svg.tsx
@@ -8,40 +8,61 @@ import BackgroundPillSymbol from 'components/widgets/background_pill_symbol';
 
 const BackgroundSvgContainer = styled.div`
     position: relative;
-    background: rgba(var(--center-channel-color-rgb), 0.08);
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(0deg, rgba(var(--center-channel-color-rgb), 0.08) 35%, rgba(var(--center-channel-bg-rgb), 1) 100%);
     overflow: hidden;
 
     > svg {
-        max-width: 3350px;
-        min-width: 1675px;
-        width: 158%;
+        position: relative;
+        width: calc(1950 / 1470 * 100%);
+        max-width: 3400px;
+        min-width: 1950px;
     }
 `;
 
-const pillID = 'backgroundPill';
+const pillID = 'onboardingBackgroundPill';
 
 const BackgroundSvg = (): JSX.Element => (
     <BackgroundSvgContainer>
         <svg
+            viewBox='0 0 1950 1470'
             preserveAspectRatio='xMinYMin meet'
-            width='3350'
-            height='2287'
-            viewBox='0 0 3350 2287'
             version='1.1'
             xmlns='http://www.w3.org/2000/svg'
             xmlnsXlink='http://www.w3.org/1999/xlink'
         >
-            <g style={{transformOrigin: 'top left', transform: 'translate(730px, 2346px) rotate(-135deg)'}}>
-                <use xlinkHref={`#${pillID}`}/>
+            <g style={{transform: 'translate(940px, -550px) rotate(45deg)'}}>
+                <use
+                    xlinkHref={`#${pillID}`}
+                    width='1260'
+                    height='749'
+                    style={{transform: 'translate(0, -56px)'}}
+                />
             </g>
-            <g style={{transformOrigin: 'top left', transform: 'translate(1302px, -134px) rotate(45deg)'}}>
-                <use xlinkHref={`#${pillID}`}/>
+            <g style={{transform: 'translate(960px, 1330px) rotate(-135deg)'}}>
+                <use
+                    xlinkHref={`#${pillID}`}
+                    width='1260'
+                    height='749'
+                    style={{transform: 'translate(0, -56px)'}}
+                />
             </g>
-            <g style={{transformOrigin: 'top left', transform: 'translate(2646px, 1556px) rotate(-135deg)'}}>
-                <use xlinkHref={`#${pillID}`}/>
+            <g style={{transform: 'translate(1500px, 1470px) rotate(-135deg)'}}>
+                <use
+                    xlinkHref={`#${pillID}`}
+                    width='1260'
+                    height='749'
+                    style={{transform: 'translate(0, -56px)'}}
+                />
             </g>
-            <g style={{transformOrigin: 'top left', transform: 'translate(184px, -772px) rotate(45deg)'}}>
-                <use xlinkHref={`#${pillID}`}/>
+            <g style={{transform: 'translate(-80px, 60px) rotate(45deg)'}}>
+                <use
+                    xlinkHref={`#${pillID}`}
+                    width='1260'
+                    height='749'
+                    style={{transform: 'translate(0, -56px)'}}
+                />
             </g>
             <BackgroundPillSymbol id={pillID}/>
         </svg>
