a0590392854f5856b6628a3976348aa953eb13c0
Mattermost Build <build@mattermost.com>
2021-08-27 15:54:02 +0200

    MM-36756 Add new component for positioning SuggestionList in RHS (#8408) (#8718)
    * MM-36756 Add new component for positioning SuggestionList in RHS
    
    * Remove accidental comic
    
    * Address feedback
    
    * Fix type issues
    
    (cherry picked from commit 3e89a93fe4bb784f0f15d1406457499da4693ef2)
    
    Co-authored-by: Harrison Healey <harrisonmhealey@gmail.com>

---
+++ components/suggestion/rhs_suggestion_list.tsx
@@ -0,0 +1,41 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React, {useEffect, useState} from 'react';
+
+import AutosizeTextarea from 'components/autosize_textarea';
+import QuickInput from 'components/quick_input';
+
+import Constants from 'utils/constants';
+
+import SuggestionList from './suggestion_list';
+
+type Props = React.ComponentProps<typeof SuggestionList> & {
+    inputRef: React.RefObject<QuickInput>;
+}
+
+export default function RhsSuggestionList(props: Props): JSX.Element {
+    const [position, setPosition] = useState<Props['location']>('top');
+
+    useEffect(() => {
+        const input = props.inputRef.current;
+
+        if (props.open) {
+            const inputTop = (input?.getInput() as AutosizeTextarea).getDOMNode()?.getBoundingClientRect().top || 0;
+            const newPosition = (inputTop < Constants.SUGGESTION_LIST_SPACE_RHS) ? 'bottom' : 'top';
+
+            if (newPosition !== position) {
+                // This potentially causes a second render when the list position changes, but that's better
+                // than checking the bounding rectangle while rendering.
+                setPosition(newPosition);
+            }
+        }
+    }, [props.inputRef, props.open]);
+
+    return (
+        <SuggestionList
+            {...props}
+            location={position}
+        />
+    );
+}
