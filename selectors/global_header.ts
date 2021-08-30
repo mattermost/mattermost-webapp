18369227d0eda3207b65cf453120c164e42a60d0
Dean Whillier <deanwhillier@users.noreply.github.com>
2021-08-30 12:37:25 -0400

    [MM-37365] Reposition the RHS when Global Header is enabled (#8671)
    * don't remove GH using `isMobile`
    
    `isMobile` removes GH for tablets as well as mobile phones, we only want to remove it for devices below 769px, so for now media queries need to be used
    
    * fix search bar not rendering in mobile
    
    if the screen width changes from desktop to mobile without triggering a React render, mobile view is broken because it requires the search bar to be present
    
    * resize/reposition RHS when GH is active
    
    * lint fixes
    
    * added a guard to check for the currentUser in `status_dropdown.tsx`
    
    Co-authored-by: Michel Engelen <jsnerdic@gmail.com>

--- selectors/global_header.ts
+++ selectors/global_header.ts
@@ -3,12 +3,9 @@
 
 import {GlobalState} from 'types/store';
 import {getFeatureFlagValue} from 'mattermost-redux/selectors/entities/general';
-import * as UserAgent from '../utils/user_agent';
-import * as Utils from '../utils/utils';
 
 export function getGlobalHeaderEnabled(state: GlobalState): boolean {
     const featureFlagEnabled = getFeatureFlagValue(state, 'GlobalHeader') === 'true';
-    const isMobileView = UserAgent.isMobile() || Utils.isMobile();
 
-    return !isMobileView && featureFlagEnabled;
+    return featureFlagEnabled;
 }
