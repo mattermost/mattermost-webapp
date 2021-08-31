ca53e0c7eeac881be66a94d5b121adbda7509536
Dean Whillier <deanwhillier@users.noreply.github.com>
2021-08-31 17:13:12 -0400

    [MM-38138] Fix safari "bug" with SVG sizing (#8737)
    * fix safari bug with SVG sizing
    
    * update cloud bg pill arrangment to match designs
    
    * remove gradient background
    
    * tweak cloud background
    
    * fix gradient for Safari
    
    * center, top align and scale background

--- components/common/svg_images_components/background.svg.tsx
+++ components/common/svg_images_components/background.svg.tsx
@@ -8,42 +8,77 @@ import BackgroundPillSymbol from 'components/widgets/background_pill_symbol';
 
 const BackgroundSvgContainer = styled.div`
     position: relative;
-    background: rgba(var(--center-channel-color-rgb), 0.08);
+    width: 100%;
+    height: 100%;
     overflow: hidden;
+    background: rgba(var(--center-channel-color-rgb), 0.08);
+    z-index: 0;
 
     > svg {
-        max-width: 3350px;
-        min-width: 1675px;
-        width: 158%;
+        position: relative;
+        object-position: top left;
+        object-fit: cover;
     }
 `;
 
-const pillID = 'backgroundPill';
+const pillID = 'onboardingBackgroundPill';
 
 const BackgroundSvg = (): JSX.Element => (
     <BackgroundSvgContainer>
         <svg
+            viewBox='0 0 1367 1470'
             preserveAspectRatio='xMinYMin meet'
-            width='3350'
-            height='2287'
-            viewBox='0 0 3350 2287'
             version='1.1'
             xmlns='http://www.w3.org/2000/svg'
             xmlnsXlink='http://www.w3.org/1999/xlink'
         >
-            <g style={{transformOrigin: 'top left', transform: 'translate(730px, 2346px) rotate(-135deg)'}}>
-                <use xlinkHref={`#${pillID}`}/>
+            <g style={{transform: 'translate(960px, 1330px) rotate(-135deg)'}}>
+                <use
+                    xlinkHref={`#${pillID}`}
+                    width='1260'
+                    height='749'
+                    style={{transform: 'translate(0, -56px)'}}
+                />
             </g>
-            <g style={{transformOrigin: 'top left', transform: 'translate(1302px, -134px) rotate(45deg)'}}>
-                <use xlinkHref={`#${pillID}`}/>
+            <g style={{transform: 'translate(1500px, 1470px) rotate(-135deg)'}}>
+                <use
+                    xlinkHref={`#${pillID}`}
+                    width='1260'
+                    height='749'
+                    style={{transform: 'translate(0, -56px)'}}
+                />
             </g>
-            <g style={{transformOrigin: 'top left', transform: 'translate(2646px, 1556px) rotate(-135deg)'}}>
-                <use xlinkHref={`#${pillID}`}/>
+            <g style={{transform: 'translate(-80px, 60px) rotate(45deg)'}}>
+                <use
+                    xlinkHref={`#${pillID}`}
+                    width='1260'
+                    height='749'
+                    style={{transform: 'translate(0, -56px)'}}
+                />
             </g>
-            <g style={{transformOrigin: 'top left', transform: 'translate(184px, -772px) rotate(45deg)'}}>
-                <use xlinkHref={`#${pillID}`}/>
-            </g>
+            <rect
+                x='0'
+                y='0'
+                width='1367'
+                height='523'
+                fill='url(#OverlayGradient)'
+            />
             <BackgroundPillSymbol id={pillID}/>
+            <defs>
+                <linearGradient
+                    id='OverlayGradient'
+                    gradientTransform='rotate(90)'
+                >
+                    <stop
+                        offset='45%'
+                        style={{stopColor: 'rgb(var(--center-channel-bg-rgb))', stopOpacity: 1}}
+                    />
+                    <stop
+                        offset='88%'
+                        style={{stopColor: 'rgb(var(--center-channel-bg-rgb))', stopOpacity: 0}}
+                    />
+                </linearGradient>
+            </defs>
         </svg>
     </BackgroundSvgContainer>
 );
