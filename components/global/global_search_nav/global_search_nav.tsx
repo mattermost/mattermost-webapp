a496781c158a8ea85f32a5a946a017d20f7dd578
Nev Angelova <nevy.angelova@gmail.com>
2021-09-02 18:48:20 +0300

    [MM-37814] - Handle quick switch shortcut in channel navigator (#8749)
    * [MM-37814] - Handle quick switch shortcut in sidebar
    
    * move functionality to channel navigator
    
    Co-authored-by: Nevyana Angelova <nevyangelova@Nevyanas-MacBook-Pro.local>
    (cherry picked from commit e2dc9052b33468e12134c1f5d2068bd7fd980f29)

--- components/global/global_search_nav/global_search_nav.tsx
+++ components/global/global_search_nav/global_search_nav.tsx
@@ -8,31 +8,23 @@ import Flex from '@mattermost/compass-components/utilities/layout/Flex';
 
 import {closeRightHandSide, showMentions} from 'actions/views/rhs';
 import Search from 'components/search';
-import QuickSwitchModal from 'components/quick_switch_modal';
 import {getIsRhsOpen, getRhsState} from 'selectors/rhs';
 import {GlobalState} from 'types/store';
 import {
     Constants,
-    ModalIdentifiers,
     RHSStates,
 } from 'utils/constants';
 import * as Utils from 'utils/utils';
-import {closeModal, openModal} from 'actions/views/modals';
-
-import {useIsModalOpen} from '../hooks';
 
 const GlobalSearchNav = (): JSX.Element => {
     const dispatch = useDispatch();
     const rhsState = useSelector((state: GlobalState) => getRhsState(state));
     const isRhsOpen = useSelector((state: GlobalState) => getIsRhsOpen(state));
-    const [, isQuickSwitcherOpenRef] = useIsModalOpen(ModalIdentifiers.QUICK_SWITCH);
 
     useEffect(() => {
         document.addEventListener('keydown', handleShortcut);
-        document.addEventListener('keydown', handleQuickSwitchKeyPress);
         return () => {
             document.removeEventListener('keydown', handleShortcut);
-            document.removeEventListener('keydown', handleQuickSwitchKeyPress);
         };
     }, []);
 
@@ -48,32 +40,7 @@ const GlobalSearchNav = (): JSX.Element => {
         if (Utils.cmdOrCtrlPressed(e) && e.shiftKey) {
             if (Utils.isKeyPressed(e, Constants.KeyCodes.M)) {
                 e.preventDefault();
-                dispatch(closeModal(ModalIdentifiers.QUICK_SWITCH));
                 searchMentions();
-            }
-            if (Utils.isKeyPressed(e, Constants.KeyCodes.L)) {
-                // just close the modal if it's open, but let someone else handle the shortcut
-                dispatch(closeModal(ModalIdentifiers.QUICK_SWITCH));
-            }
-        }
-    };
-
-    const toggleQuickSwitchModal = () => {
-        if (isQuickSwitcherOpenRef.current) {
-            dispatch(closeModal(ModalIdentifiers.QUICK_SWITCH));
-        } else {
-            dispatch(openModal({
-                modalId: ModalIdentifiers.QUICK_SWITCH,
-                dialogType: QuickSwitchModal,
-            }));
-        }
-    };
-
-    const handleQuickSwitchKeyPress = (e: KeyboardEvent) => {
-        if (Utils.cmdOrCtrlPressed(e) && !e.shiftKey && Utils.isKeyPressed(e, Constants.KeyCodes.K)) {
-            if (!e.altKey) {
-                e.preventDefault();
-                toggleQuickSwitchModal();
             }
         }
     };
