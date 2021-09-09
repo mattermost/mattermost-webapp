10ebbc541413febccc707751f811f0392724eeff
Harrison Healey <harrisonmhealey@gmail.com>
2021-09-09 09:37:14 -0400

    Add some memoization to makeGetOptions

--- components/more_direct_channels/list/index.ts
+++ components/more_direct_channels/list/index.ts
@@ -2,6 +2,7 @@
 // See LICENSE.txt for license information.
 
 import {connect} from 'react-redux';
+import {createSelector} from 'reselect';
 
 import {getAllChannels, getChannelsWithUserProfiles} from 'mattermost-redux/selectors/entities/channels';
 import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
@@ -25,77 +26,99 @@ type OwnProps = {
 }
 
 function makeGetOptions() {
-    return (state: GlobalState, users: UserProfile[], values: OptionValue[]) => {
-        const currentUserId = getCurrentUserId(state);
-        const searchTerm = state.views.search.modalSearch;
+    // Gets all loaded DMs (as UserProfiles)
+    const getUsersWithDMs = createSelector(
+        'getUsersWithDMs',
+        getCurrentUserId,
+        getAllChannels,
+        (state: GlobalState, users: UserProfile[]) => users,
+        (currentUserId, allChannels, users) => {
+            const directChannels = Object.values(allChannels).filter((channel) => channel.type === Constants.DM_CHANNEL);
 
-        const allChannels = getAllChannels(state);
-        const directChannels = Object.values(allChannels).filter((channel) => channel.type === Constants.DM_CHANNEL);
+            // Gets all loaded DMs (as UserProfiles)
+            const usersWithDMs: Array<UserProfile & {last_post_at: number}> = [];
+            for (const channel of directChannels) {
+                const otherUserId = getUserIdFromChannelName(currentUserId, channel.name);
+                const otherUser = users.find((user) => user.id === otherUserId);
 
-        // Gets all loaded DMs (as UserProfiles)
-        const usersWithDMs: Array<UserProfile & {last_post_at: number}> = [];
-        for (const channel of directChannels) {
-            const otherUserId = getUserIdFromChannelName(currentUserId, channel.name);
-            const otherUser = users.find((user) => user.id === otherUserId);
+                if (!otherUser) {
+                    // The user doesn't match the search filter
+                    continue;
+                }
 
-            if (!otherUser) {
-                // The user doesn't match the search filter
-                continue;
+                if (channel.last_post_at === 0) {
+                    // The DM channel exists but has no messages in it
+                    continue;
+                }
+
+                usersWithDMs.push({
+                    ...otherUser,
+                    last_post_at: channel.last_post_at,
+                });
             }
 
-            if (channel.last_post_at === 0) {
-                // The DM channel exists but has no messages in it
-                continue;
-            }
+            return usersWithDMs;
+        },
+    );
 
-            usersWithDMs.push({
-                ...otherUser,
-                last_post_at: channel.last_post_at,
+    // Gets GM channels matching the search term and selected values
+    const getFilteredGroupChannels = createSelector(
+        'getFilteredGroupChannels',
+        getChannelsWithUserProfiles,
+        (state: GlobalState) => state.views.search.modalSearch,
+        (state: GlobalState, values: OptionValue[]) => values,
+        (channelsWithProfiles, searchTerm, values) => {
+            return channelsWithProfiles.filter((channel) => {
+                if (searchTerm) {
+                    // Check that at least one of the users in the channel matches the search term
+                    const matches = filterProfilesStartingWithTerm(channel.profiles, searchTerm);
+                    if (matches.length === 0) {
+                        return false;
+                    }
+                }
+
+                if (values) {
+                    // Check that all of the selected users are in the channel
+                    const valuesInProfiles = values.every((value) => channel.profiles.find((user) => user.id === value.id));
+                    if (!valuesInProfiles) {
+                        return false;
+                    }
+                }
+
+                // Only include GM channels with messages in them
+                return channel.last_post_at > 0;
             });
-        }
+        },
+    );
 
-        const filteredGroupChannels = getChannelsWithUserProfiles(state).filter((channel) => {
-            if (searchTerm) {
-                // Check that at least one of the users in the channel matches the search term
-                const matches = filterProfilesStartingWithTerm(channel.profiles, searchTerm);
-                if (matches.length === 0) {
-                    return false;
-                }
-            }
+    return createSelector(
+        'makeGetOptions',
+        getUsersWithDMs,
+        (state: GlobalState, users: UserProfile[], values: OptionValue[]) => getFilteredGroupChannels(state, values),
+        (state: GlobalState, users: UserProfile[]) => users,
+        (usersWithDMs, filteredGroupChannels, users) => {
+            // Recent DMs (as UserProfiles) and GMs sorted by recent activity
+            const recents = [...usersWithDMs, ...filteredGroupChannels].
+                sort((a, b) => b.last_post_at - a.last_post_at);
 
-            if (values) {
-                // Check that all of the selected users are in the channel
-                const valuesInProfiles = values.every((value) => channel.profiles.find((user) => user.id === value.id));
-                if (!valuesInProfiles) {
-                    return false;
-                }
-            }
+            // Other users sorted by whether or not they've been deactivated followed by alphabetically
+            const usersWithoutDMs = users.
+                filter((user) => user.delete_at === 0 && !usersWithDMs.some((other) => other.id === user.id)).
+                map((user) => ({...user, last_post_at: 0}));
+            usersWithoutDMs.sort((a, b) => {
+                return a.username.localeCompare(b.username);
+            });
 
-            // Only include GM channels with messages in them
-            return channel.last_post_at > 0;
-        });
-
-        // Recent DMs (as UserProfiles) and GMs sorted by recent activity
-        const recents = [...usersWithDMs, ...filteredGroupChannels].
-            sort((a, b) => b.last_post_at - a.last_post_at);
-
-        // Other users sorted by whether or not they've been deactivated followed by alphabetically
-        const usersWithoutDMs = users.
-            filter((user) => user.delete_at === 0 && !usersWithDMs.some((other) => other.id === user.id)).
-            map((user) => ({...user, last_post_at: 0}));
-        usersWithoutDMs.sort((a, b) => {
-            return a.username.localeCompare(b.username);
-        });
-
-        // Returns an array containing:
-        //  1. All recent DMs (represented by UserProfiles) and GMs matching the filter
-        //      - GMs are also filtered to only show ones containing each selected user
-        //  2. Other non-deactivated users sorted by username
-        return [
-            ...recents,
-            ...usersWithoutDMs,
-        ];
-    };
+            // Returns an array containing:
+            //  1. All recent DMs (represented by UserProfiles) and GMs matching the filter
+            //      - GMs are also filtered to only show ones containing each selected user
+            //  2. Other non-deactivated users sorted by username
+            return [
+                ...recents,
+                ...usersWithoutDMs,
+            ];
+        },
+    );
 }
 
 function makeMapStateToProps() {
