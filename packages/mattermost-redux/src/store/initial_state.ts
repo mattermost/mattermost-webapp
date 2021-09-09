59c460cdb7747333e1c02d0c78d887ca1edf9eff
koox00 <cooxlee@gmail.com>
2021-09-09 14:28:19 +0300

    Fixes types

--- packages/mattermost-redux/src/store/initial_state.ts
+++ packages/mattermost-redux/src/store/initial_state.ts
@@ -76,6 +76,7 @@ const state: GlobalState = {
         },
         threads: {
             threadsInTeam: {},
+            unreadThreadsInTeam: {},
             threads: {},
             counts: {},
         },
