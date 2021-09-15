9fb7812b520ea8765fdb69e37744cc4973ab4265
Dean Whillier <dean@whillier.com>
2021-09-15 12:12:43 -0400

    update reducers/views/browser tests

--- reducers/views/browser.test.js
+++ reducers/views/browser.test.js
@@ -7,12 +7,20 @@ import {ActionTypes} from 'utils/constants';
 describe('Reducers.Browser', () => {
     const initialState = {
         focused: true,
+        windowSize: {
+            width: 1920,
+            height: 1080,
+        },
     };
 
     test('Initial state', () => {
         const nextState = browserReducer(
             {
                 focused: true,
+                windowSize: {
+                    width: 1920,
+                    height: 1080,
+                },
             },
             {},
         );
@@ -24,6 +32,10 @@ describe('Reducers.Browser', () => {
         const nextState = browserReducer(
             {
                 focused: true,
+                windowSize: {
+                    width: 1920,
+                    height: 1080,
+                },
             },
             {
                 type: ActionTypes.BROWSER_CHANGE_FOCUS,
@@ -41,6 +53,10 @@ describe('Reducers.Browser', () => {
         const nextState = browserReducer(
             {
                 focused: false,
+                windowSize: {
+                    width: 1920,
+                    height: 1080,
+                },
             },
             {
                 type: ActionTypes.BROWSER_CHANGE_FOCUS,
@@ -51,6 +67,33 @@ describe('Reducers.Browser', () => {
         expect(nextState).toEqual({
             ...initialState,
             focused: true,
+        });
+    });
+
+    test(`should reflect window resize update on ${ActionTypes.BROWSER_WINDOW_RESIZED}`, () => {
+        const nextState = browserReducer(
+            {
+                focused: true,
+                windowSize: {
+                    width: 1920,
+                    height: 1080,
+                },
+            },
+            {
+                type: ActionTypes.BROWSER_WINDOW_RESIZED,
+                windowSize: {
+                    width: 1280,
+                    height: 720,
+                },
+            },
+        );
+
+        expect(nextState).toEqual({
+            ...initialState,
+            windowSize: {
+                width: 1280,
+                height: 720,
+            },
         });
     });
 });
