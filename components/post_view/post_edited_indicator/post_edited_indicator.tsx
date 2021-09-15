4cd803ed8b43c4909cc68187662a186170dbc4cd
Michel Engelen <jsnerdic@gmail.com>
2021-09-15 15:12:57 +0200

    added overlay to the edited indicator
    - wrapped the edited indicator in `OverlayTrigger` component
    - did some date calculations to find the correct time and wording
    - used react-intl functions to get locale-correct times and dates
    - added a new function to datetime.ts `isWithinLastWeek`
    - added 2 new language properties: `datetime.today` and `datetime.yesterday`
    
    unrelated work:
    - added return types to functions in datetime.ts
    - re-structured some of the return values

--- components/post_view/post_edited_indicator/post_edited_indicator.tsx
+++ components/post_view/post_edited_indicator/post_edited_indicator.tsx
@@ -2,8 +2,12 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import {FormattedMessage} from 'react-intl';
+import {Tooltip} from 'react-bootstrap';
+import {useIntl} from 'react-intl';
 import Icon from '@mattermost/compass-components/foundations/icon/Icon';
+
+import {isSameDay, isWithinLastWeek, isYesterday} from '../../../utils/datetime';
+import OverlayTrigger from '../../overlay_trigger';
 
 interface Props {
     postId?: string | null;
@@ -11,22 +15,73 @@ interface Props {
 }
 
 const PostEditedIndicator = ({postId = null, editedAt = 0}: Props): JSX.Element | null => {
-    return !postId || editedAt === 0 ? null : (
-        <span
-            id={`postEdited_${postId}`}
-            className='post-edited__indicator'
-            data-post-id={postId}
-            data-edited-at={editedAt}
+    if (!postId || editedAt === 0) {
+        return null;
+    }
+
+    const {formatMessage, formatDate, formatTime} = useIntl();
+    const editedDate = new Date(editedAt);
+
+    let date;
+    switch (true) {
+    case isSameDay(editedDate):
+        date = formatMessage({id: 'datetime.today', defaultMessage: 'today '});
+        break;
+    case isYesterday(editedDate):
+        date = formatMessage({id: 'datetime.yesterday', defaultMessage: 'yesterday '});
+        break;
+    case isWithinLastWeek(editedDate):
+        date = formatDate(editedDate, {weekday: 'long'});
+        break;
+    case !isWithinLastWeek(editedDate):
+    default:
+        date = formatDate(editedDate, {month: 'long', day: 'numeric'});
+    }
+
+    const time = formatTime(editedDate, {hour: '2-digit', minute: '2-digit'});
+
+    const editedText = formatMessage({
+        id: 'post_message_view.edited',
+        defaultMessage: 'Edited',
+    });
+
+    const formattedTime = formatMessage({
+        id: 'timestamp.datetime',
+        defaultMessage: '{relativeOrDate} at {time}',
+    },
+    {
+        relativeOrDate: date,
+        time,
+    });
+
+    const tooltip = (
+        <Tooltip
+            id={`edited-post-tooltip_${postId}`}
+            className='hidden-xs'
         >
-            <Icon
-                glyph={'pencil-outline'}
-                size={10}
-            />
-            <FormattedMessage
-                id='post_message_view.edited'
-                defaultMessage='Edited'
-            />
-        </span>
+            {`${editedText} ${formattedTime}`}
+        </Tooltip>
+    );
+
+    return !postId || editedAt === 0 ? null : (
+        <OverlayTrigger
+            delayShow={250}
+            placement='top'
+            overlay={tooltip}
+        >
+            <span
+                id={`postEdited_${postId}`}
+                className='post-edited__indicator'
+                data-post-id={postId}
+                data-edited-at={editedAt}
+            >
+                <Icon
+                    glyph={'pencil-outline'}
+                    size={10}
+                />
+                {editedText}
+            </span>
+        </OverlayTrigger>
     );
 };
 
