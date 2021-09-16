6c30f1a97a142e3d8f4079fe5a91d4e8645639d0
Jason Frerich <jason.frerich@mattermost.com>
2021-09-16 13:43:13 -0500

    use formattedmarkdownmessage to add line breaks

--- components/actions_menu/actions_menu_tutorial_tip.tsx
+++ components/actions_menu/actions_menu_tutorial_tip.tsx
@@ -4,6 +4,8 @@
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
 
+import FormattedMarkdownMessage from 'components/formatted_markdown_message.jsx';
+
 import TutorialTip from 'components/tutorial/tutorial_tip';
 
 export default class ActionsTutorialTip extends React.PureComponent {
@@ -18,9 +20,9 @@ export default class ActionsTutorialTip extends React.PureComponent {
                     />
                 </h4>
                 <p>
-                    <FormattedMessage
+                    <FormattedMarkdownMessage
                         id='post_info.actions.tutorialTip'
-                        defaultMessage='Message actions that are provided through apps, integrations or plugins have moved to this menu item.'
+                        defaultMessage='Message actions that are provided\nthrough apps, integrations or plugins\nhave moved to this menu item.'
                     />
                 </p>
             </div>,
