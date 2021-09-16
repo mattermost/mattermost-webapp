5fdd1b3ac9502a9acd993a8e4ab0447a0f578bd8
Jason Frerich <jason.frerich@mattermost.com>
2021-09-16 16:37:39 -0500

    initital test for actionsMenu   - this is stripped down from dotMenu tests

---
+++ components/actions_menu/actions_menu.test.tsx
@@ -0,0 +1,65 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+
+import {PostType} from 'mattermost-redux/types/posts';
+
+import {shallowWithIntl} from 'tests/helpers/intl-test-helper';
+
+import {TestHelper} from 'utils/test_helper';
+
+import ActionsMenu, {PLUGGABLE_COMPONENT} from './actions_menu';
+
+jest.mock('utils/utils', () => {
+    const original = jest.requireActual('utils/utils');
+    return {
+        ...original,
+        isMobile: jest.fn(() => false),
+    };
+});
+
+describe('components/actions_menu/ActionsMenu', () => {
+    const baseProps = {
+        post: TestHelper.getPostMock({id: 'post_id_1', is_pinned: false, type: '' as PostType}),
+        handleDropdownOpened: jest.fn(),
+        components: {},
+        actions: {
+            openModal: jest.fn(),
+            doAppCall: jest.fn(),
+            postEphemeralCallResponseForPost: jest.fn(),
+        },
+        appBindings: [],
+        pluginMenuItems: [],
+        appsEnabled: false,
+        isSysAdmin: true,
+    };
+
+    test('should have divider when plugin menu item exists', () => {
+        const wrapper = shallowWithIntl(
+            <ActionsMenu {...baseProps}/>,
+        );
+        expect(wrapper.find('#divider_post_post_id_1_plugins').exists()).toBe(false);
+
+        wrapper.setProps({
+            pluginMenuItems: [
+                {id: 'test_plugin_menu_item_1', text: 'woof'},
+            ],
+        });
+        expect(wrapper.find('#divider_post_post_id_1_plugins').exists()).toBe(true);
+    });
+
+    test('should have divider when pluggable menu item exists', () => {
+        const wrapper = shallowWithIntl(
+            <ActionsMenu {...baseProps}/>,
+        );
+        expect(wrapper.find('#divider_post_post_id_1_plugins').exists()).toBe(false);
+
+        wrapper.setProps({
+            components: {
+                [PLUGGABLE_COMPONENT]: [{}],
+            },
+        });
+        expect(wrapper.find('#divider_post_post_id_1_plugins').exists()).toBe(true);
+    });
+});
---
+++ components/actions_menu/actions_menu_empty.test.tsx
@@ -0,0 +1,65 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import {shallow} from 'enzyme';
+import React from 'react';
+
+import DotMenu from 'components/dot_menu/dot_menu';
+import {TestHelper} from 'utils/test_helper';
+
+jest.mock('utils/utils', () => {
+    return {
+        isMobile: jest.fn(() => false),
+        localizeMessage: jest.fn().mockReturnValue(''),
+    };
+});
+
+jest.mock('utils/post_utils', () => {
+    const original = jest.requireActual('utils/post_utils');
+    return {
+        ...original,
+        isSystemMessage: jest.fn(() => true),
+    };
+});
+
+describe('components/dot_menu/DotMenu returning empty ("")', () => {
+    test('should match snapshot, return empty ("") on Center', () => {
+        const baseProps = {
+            post: TestHelper.getPostMock({id: 'post_id_1'}),
+            isLicensed: false,
+            postEditTimeLimit: '-1',
+            enableEmojiPicker: true,
+            components: {},
+            channelIsArchived: false,
+            currentTeamUrl: '',
+            actions: {
+                flagPost: jest.fn(),
+                unflagPost: jest.fn(),
+                setEditingPost: jest.fn(),
+                pinPost: jest.fn(),
+                unpinPost: jest.fn(),
+                openModal: jest.fn(),
+                markPostAsUnread: jest.fn(),
+                doAppCall: jest.fn(),
+                postEphemeralCallResponseForPost: jest.fn(),
+                setThreadFollow: jest.fn(),
+            },
+            canEdit: false,
+            canDelete: false,
+            appBindings: [],
+            pluginMenuItems: [],
+            appsEnabled: false,
+            isReadOnly: false,
+            isCollapsedThreadsEnabled: false,
+            currentTeamId: '',
+            threadId: 'post_id_1',
+            userId: 'user_id_1',
+        };
+
+        const wrapper = shallow(
+            <DotMenu {...baseProps}/>,
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+});
