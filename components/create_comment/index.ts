27cc69fc998e8f1a6a39e2687be55c84e39f4baa
Jesse Hallam <jesse.hallam@gmail.com>
2021-09-20 11:35:07 -0300

    drop unused readOnlyChannel from create_comment

--- components/create_comment/index.ts
+++ components/create_comment/index.ts
@@ -79,7 +79,6 @@ function makeMapStateToProps() {
             codeBlockOnCtrlEnter: getBool(state, Preferences.CATEGORY_ADVANCED_SETTINGS, 'code_block_ctrl_enter', true),
             ctrlSend: getBool(state, Preferences.CATEGORY_ADVANCED_SETTINGS, 'send_on_ctrl_enter'),
             createPostErrorId: err.server_error_id,
-            readOnlyChannel: false,
             enableConfirmNotificationsToChannel,
             enableEmojiPicker,
             enableGifPicker,
