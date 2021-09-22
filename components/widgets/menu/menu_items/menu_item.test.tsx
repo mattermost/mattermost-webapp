1d8afcf9855a5a86eacd95577dca79333be669e2
Michel Engelen <jsnerdic@gmail.com>
2021-09-22 14:54:44 +0200

    updated snapshots

--- components/widgets/menu/menu_items/menu_item.test.tsx
+++ components/widgets/menu/menu_items/menu_item.test.tsx
@@ -18,58 +18,54 @@ describe('components/MenuItem', () => {
 
     test('should match snapshot not shown', () => {
         const props = {...defaultProps, show: false};
-        const wrapper = shallow(
-            <TestComponent {...props}/>,
-        );
+        const wrapper = shallow(<TestComponent {...props}/>);
 
         expect(wrapper).toMatchInlineSnapshot('""');
     });
 
     test('should match snapshot shown with icon', () => {
         const props = {...defaultProps, icon: 'test-icon'};
-        const wrapper = shallow(
-            <TestComponent {...props}/>,
-        );
+        const wrapper = shallow(<TestComponent {...props}/>);
 
         expect(wrapper).toMatchInlineSnapshot(`
-<li
-  className="MenuItem MenuItem--with-icon"
-  id="test-id"
-  role="menuitem"
->
-  <Component
-    otherProp="extra-prop"
-    text={
-      <React.Fragment>
-        <span
-          className="icon"
-        >
-          test-icon
-        </span>
-        test-text
-      </React.Fragment>
-    }
-  />
-</li>
-`);
+            <li
+              className="MenuItem MenuItem--with-icon"
+              id="test-id"
+              role="menuitem"
+            >
+              <Component
+                ariaLabel="test-text"
+                otherProp="extra-prop"
+                text={
+                  <React.Fragment>
+                    <span
+                      className="icon"
+                    >
+                      test-icon
+                    </span>
+                    test-text
+                  </React.Fragment>
+                }
+              />
+            </li>
+        `);
     });
 
     test('should match snapshot shown without icon', () => {
-        const wrapper = shallow(
-            <TestComponent {...defaultProps}/>,
-        );
+        const wrapper = shallow(<TestComponent {...defaultProps}/>);
 
         expect(wrapper).toMatchInlineSnapshot(`
-<li
-  className="MenuItem"
-  id="test-id"
-  role="menuitem"
->
-  <Component
-    otherProp="extra-prop"
-    text="test-text"
-  />
-</li>
-`);
+            <li
+              className="MenuItem"
+              id="test-id"
+              role="menuitem"
+            >
+              <Component
+                ariaLabel="test-text"
+                otherProp="extra-prop"
+                text="test-text"
+              />
+            </li>
+        `);
     });
 });
