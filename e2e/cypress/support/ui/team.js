9ded226da94ca2b349b3fa0812887d2df19d4a19
Furqan Malik <Furqan.Malik@mattermost.com>
2021-09-22 17:30:53 -0400

    fixed cypress test cases

--- e2e/cypress/support/ui/team.js
+++ e2e/cypress/support/ui/team.js
@@ -3,7 +3,7 @@
 
 Cypress.Commands.add('uiInviteMemberToCurrentTeam', (username) => {
     // # Open member invite screen
-    cy.get('#sidebarHeaderDropdownButton').should('be.visible').click();
+    cy.get('.icon-chevron-down').should('be.visible').click();
     cy.get('#invitePeople > button').should('contain.text', 'Invite People').click();
 
     // # Open members section if licensed for guest accounts
