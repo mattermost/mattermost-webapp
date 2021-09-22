c5ada7f5c3075188141550af03d140a6aa671ed3
Jason Frerich <jason.frerich@mattermost.com>
2021-09-22 09:44:29 -0500

    update test snapshots

--- components/widgets/menu/menu_items/menu_item_action.test.tsx
+++ components/widgets/menu/menu_items/menu_item_action.test.tsx
@@ -1,58 +1,71 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
-import {shallow} from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import {MenuItemActionImpl} from './menu_item_action';
+import { MenuItemActionImpl } from "./menu_item_action";
 
-describe('components/MenuItemAction', () => {
-    test('should match snapshot', () => {
+describe("components/MenuItemAction", () => {
+    test("should match snapshot", () => {
         const wrapper = shallow(
-            <MenuItemActionImpl
-                onClick={jest.fn()}
-                text='Whatever'
-            />,
+            <MenuItemActionImpl onClick={jest.fn()} text="Whatever" />
         );
 
         expect(wrapper).toMatchInlineSnapshot(`
-      <button
-        className="style--none"
-        onClick={[MockFunction]}
-      >
-        <span
-          className="MenuItem__primary-text"
-        >
-          Whatever
-        </span>
-      </button>
-    `);
+            <button
+              className="style--none"
+              onClick={[MockFunction]}
+            >
+              <span>
+                <span
+                  className="MenuItem__left-decorator"
+                />
+                <span
+                  className="MenuItem__primary-text"
+                >
+                  Whatever
+                </span>
+                <span
+                  className="MenuItem__right-decorator MenuItem__text-color-opaque"
+                />
+              </span>
+            </button>
+        `);
     });
-    test('should match snapshot with extra text', () => {
+    test("should match snapshot with extra text", () => {
         const wrapper = shallow(
             <MenuItemActionImpl
                 onClick={jest.fn()}
-                text='Whatever'
-                extraText='Extra Text'
-            />,
+                text="Whatever"
+                extraText="Extra Text"
+            />
         );
 
         expect(wrapper).toMatchInlineSnapshot(`
-      <button
-        className="style--none MenuItem__with-help"
-        onClick={[MockFunction]}
-      >
-        <span
-          className="MenuItem__primary-text"
-        >
-          Whatever
-        </span>
-        <span
-          className="MenuItem__help-text"
-        >
-          Extra Text
-        </span>
-      </button>
-    `);
+            <button
+              className="style--none MenuItem__with-help"
+              onClick={[MockFunction]}
+            >
+              <span>
+                <span
+                  className="MenuItem__left-decorator"
+                />
+                <span
+                  className="MenuItem__primary-text"
+                >
+                  Whatever
+                </span>
+                <span
+                  className="MenuItem__right-decorator MenuItem__text-color-opaque"
+                />
+              </span>
+              <span
+                className="MenuItem__help-text"
+              >
+                Extra Text
+              </span>
+            </button>
+        `);
     });
 });
