7b053d3c10d72d49869b36f748913251bbc84a10
Claudio Costa <cstcld91@gmail.com>
2021-10-05 15:27:22 +0200

    [MM-31389] Show confirm modal for `@here` mentions (#8852)
    * Show confirm modal for at here mentions
    
    * Refactor confirmation modal into separate component
    
    * Implement specialMentionsInText util to improve logic
    
    * Update system console labels
    
    * Fix lint

--- utils/constants.jsx
+++ utils/constants.jsx
@@ -326,6 +326,7 @@ export const ModalIdentifiers = {
     NO_INTERNET_CONNECTION: 'no_internet_connection',
     JOIN_CHANNEL_PROMPT: 'join_channel_prompt',
     COLLAPSED_REPLY_THREADS_MODAL: 'collapsed_reply_threads_modal',
+    NOTIFY_CONFIRM_MODAL: 'notify_confirm_modal',
 };
 
 export const UserStatuses = {
@@ -1017,6 +1018,9 @@ export const Constants = {
 
     SPECIAL_MENTIONS: ['all', 'channel', 'here'],
     SPECIAL_MENTIONS_REGEX: /(?:\B|\b_+)@(channel|all|here)(?!(\.|-|_)*[^\W_])/gi,
+    ALL_MENTION_REGEX: /(?:\B|\b_+)@(all)(?!(\.|-|_)*[^\W_])/gi,
+    CHANNEL_MENTION_REGEX: /(?:\B|\b_+)@(channel)(?!(\.|-|_)*[^\W_])/gi,
+    HERE_MENTION_REGEX: /(?:\B|\b_+)@(here)(?!(\.|-|_)*[^\W_])/gi,
     NOTIFY_ALL_MEMBERS: 5,
     ALL_MEMBERS_MENTIONS_REGEX: /(?:\B|\b_+)@(channel|all)(?!(\.|-|_)*[^\W_])/gi,
     DEFAULT_CHARACTER_LIMIT: 4000,
