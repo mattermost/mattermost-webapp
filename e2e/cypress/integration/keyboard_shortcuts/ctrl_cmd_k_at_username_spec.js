24a7e60879dfd1e6eb79317d2554eec91d237d1d
Gandhar Tandale <gandhar.tandale14@gmail.com>
2021-10-13 10:39:53 +0530

    Incorporated review comments

--- e2e/cypress/integration/keyboard_shortcuts/ctrl_cmd_k_at_username_spec.js
+++ e2e/cypress/integration/keyboard_shortcuts/ctrl_cmd_k_at_username_spec.js
@@ -9,44 +9,33 @@
 
 // Group: @keyboard_shortcuts
 
-
 describe('Keyboard Shortcuts', () => {
     let testTeam;
-    let testChannel;
     let testUser;
     let otherUser;
 
     before(() => {
-        cy.apiInitSetup().then(({team, channel, user}) => {
+        cy.apiInitSetup().then(({team, user, offTopicUrl}) => {
             testTeam = team;
-            testChannel = channel;
             testUser = user;
-
             cy.apiCreateUser({prefix: 'other'}).then(({user: user1}) => {
                 otherUser = user1;
-
-                cy.apiAddUserToTeam(testTeam.id, otherUser.id).then(() => {
-                    cy.apiAddUserToChannel(testChannel.id, otherUser.id);
-                });
+                cy.apiAddUserToTeam(testTeam.id, otherUser.id);
+                cy.apiLogin(testUser);
+                cy.visit(offTopicUrl);
             });
         });
     });
 
-    beforeEach(() => {
-        cy.apiLogin(testUser);
-    });
     it('MM-T1246 CTRL/CMD+K - @ at beginning of username', () => {
-        
-        cy.visit(`/${testTeam.name}/channels/off-topic`);
-
         // # Type CTRL/CMD+K
         cy.get('#post_textbox').cmdOrCtrlShortcut('k');
 
         // # Enter @ followed by the first 2 characters of username of the other user
-        cy.get('#quickSwitchInput').type('@'+otherUser.username.slice(0,3))
+        cy.get('#quickSwitchInput').type('@' + otherUser.username.slice(0, 3));
 
         // # Click on the username in the suggestion list
-        cy.findByTestId(`${otherUser.username}`).click()
+        cy.findByTestId(otherUser.username).click();
 
         // * the direct message channel for the user opens
         cy.url().should('include', `/${testTeam.name}/messages/@${otherUser.username}`);
