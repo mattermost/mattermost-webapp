6047165715c4153ffdc9bde3d4f16d441db2a6f4
Cosmin Leferman <cosmin.leferman@accesa.eu>
2021-10-14 21:59:50 +0300

    Migrate permission_system_scheme_settings component to typescript
    -  Changed extensions from jsx to tsx.
    -  Removed propTypes from the component, typed Props and State.
    -  Typed Actions in redux wrapper.
    -  Added `defaultRole` variable to .test file to avoid sending a
    sending a `Partial<Role>` prop;
    -  Removed ` (this.canSave && !this.canSave())` from `SaveButton`
    disabled condition because it didn't exist inside the component;
    
    Signed-off-by: Cosmin Leferman <cosmin.leferman@accesa.eu>

--- components/admin_console/permission_schemes_settings/permission_system_scheme_settings/permission_system_scheme_settings.tsx
+++ components/admin_console/permission_schemes_settings/permission_system_scheme_settings/permission_system_scheme_settings.tsx
@@ -2,42 +2,57 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import {FormattedMessage} from 'react-intl';
 
 import GeneralConstants from 'mattermost-redux/constants/general';
-
-import ConfirmModal from 'components/confirm_modal';
+import {ClientConfig, ClientLicense} from 'mattermost-redux/types/config';
+import {Role} from 'mattermost-redux/types/roles';
+import {ActionResult} from 'mattermost-redux/types/actions';
 
 import {PermissionsScope, DefaultRolePermissions} from 'utils/constants';
 import {localizeMessage} from 'utils/utils.jsx';
 import {t} from 'utils/i18n';
 
+import ConfirmModal from 'components/confirm_modal';
 import SaveButton from 'components/save_button';
 import LoadingScreen from 'components/loading_screen';
 import FormError from 'components/form_error';
 import BlockableLink from 'components/admin_console/blockable_link';
 import AdminPanelTogglable from 'components/widgets/admin_console/admin_panel_togglable';
-
 import FormattedMarkdownMessage from 'components/formatted_markdown_message';
 
 import PermissionsTree, {EXCLUDED_PERMISSIONS} from '../permissions_tree';
 import GuestPermissionsTree, {GUEST_INCLUDED_PERMISSIONS} from '../guest_permissions_tree';
 
-export default class PermissionSystemSchemeSettings extends React.PureComponent {
-    static propTypes = {
-        config: PropTypes.object.isRequired,
-        roles: PropTypes.object.isRequired,
-        license: PropTypes.object.isRequired,
-        isDisabled: PropTypes.bool,
-        actions: PropTypes.shape({
-            loadRolesIfNeeded: PropTypes.func.isRequired,
-            editRole: PropTypes.func.isRequired,
-            setNavigationBlocked: PropTypes.func.isRequired,
-        }).isRequired,
+type Props = {
+    config: Partial<ClientConfig>;
+    roles: Record<string, Role>;
+    license: ClientLicense;
+    isDisabled?: boolean;
+    actions: {
+        loadRolesIfNeeded: (roles: Iterable<string>) => void;
+        editRole: (role: Partial<Role>) => Promise<ActionResult>;
+        setNavigationBlocked: (blocked: boolean) => void;
     };
+    location: Location;
+}
 
-    constructor(props) {
+type State = {
+    showResetDefaultModal: boolean;
+    loaded: boolean;
+    saving: boolean;
+    saveNeeded: boolean;
+    serverError: null;
+    roles: Record<string, Partial<Role>>;
+    selectedPermission: string | null;
+    openRoles: Record<string, boolean>;
+    urlParams: URLSearchParams;
+}
+
+export default class PermissionSystemSchemeSettings extends React.PureComponent<Props, State> {
+    private rolesNeeded: string[];
+
+    constructor(props: Props) {
         super(props);
         this.state = {
             showResetDefaultModal: false,
@@ -53,6 +68,7 @@ export default class PermissionSystemSchemeSettings extends React.PureComponent 
                 team_admin: true,
                 channel_admin: true,
             },
+            selectedPermission: null,
             urlParams: new URLSearchParams(props.location.search),
         };
         this.rolesNeeded = [
@@ -76,12 +92,12 @@ export default class PermissionSystemSchemeSettings extends React.PureComponent 
 
         if (this.state.urlParams.get('rowIdFromQuery')) {
             setTimeout(() => {
-                this.selectRow(this.state.urlParams.get('rowIdFromQuery'));
+                this.selectRow(this.state.urlParams.get('rowIdFromQuery')!);
             }, 1000);
         }
     }
 
-    UNSAFE_componentWillReceiveProps(nextProps) { // eslint-disable-line camelcase
+    UNSAFE_componentWillReceiveProps(nextProps: Props) {
         if (!this.state.loaded && this.rolesNeeded.every((roleName) => nextProps.roles[roleName])) {
             this.loadRolesIntoState(nextProps);
         }
@@ -105,7 +121,7 @@ export default class PermissionSystemSchemeSettings extends React.PureComponent 
         return false;
     }
 
-    selectRow = (permission) => {
+    selectRow = (permission: string) => {
         this.setState({selectedPermission: permission});
 
         // Wait until next render
@@ -117,109 +133,99 @@ export default class PermissionSystemSchemeSettings extends React.PureComponent 
         }, 3000);
     }
 
-    loadRolesIntoState(props) {
-        /* eslint-disable camelcase */
-        const {
-            system_admin,
-            team_admin,
-            channel_admin,
-            system_user,
-            team_user,
-            channel_user,
-            system_guest,
-            team_guest,
-            channel_guest,
-        } = props.roles;
-        /* eslint-enable camelcase */
-
+    loadRolesIntoState(props: Props) {
         this.setState({
             selectedPermission: null,
             loaded: true,
             roles: {
-                system_admin,
-                team_admin,
-                channel_admin,
+                system_admin: props.roles.system_admin,
+                team_admin: props.roles.team_admin,
+                channel_admin: props.roles.channel_admin,
                 all_users: {
                     name: 'all_users',
-                    displayName: 'All members',
-                    permissions: system_user.permissions.concat(team_user.permissions).concat(channel_user.permissions),
+                    display_name: 'All members',
+                    permissions: props.roles.system_user.permissions?.
+                        concat(props.roles.team_user.permissions).
+                        concat(props.roles.channel_user.permissions),
                 },
                 guests: {
                     name: 'guests',
-                    displayName: 'Guests',
-                    permissions: system_guest.permissions.concat(team_guest.permissions).concat(channel_guest.permissions),
+                    display_name: 'Guests',
+                    permissions: props.roles.system_guest.permissions?.
+                        concat(props.roles.team_guest.permissions).
+                        concat(props.roles.channel_guest.permissions),
                 },
             },
         });
     }
 
-    deriveRolesFromAllUsers = (role) => {
+    deriveRolesFromAllUsers = (role: Partial<Role>): Record<string, Partial<Role>> => {
         return {
             system_user: {
                 ...this.props.roles.system_user,
-                permissions: role.permissions.filter((p) => PermissionsScope[p] === 'system_scope'),
+                permissions: role.permissions?.filter((p) => PermissionsScope[p] === 'system_scope'),
             },
             team_user: {
                 ...this.props.roles.team_user,
-                permissions: role.permissions.filter((p) => PermissionsScope[p] === 'team_scope'),
+                permissions: role.permissions?.filter((p) => PermissionsScope[p] === 'team_scope'),
             },
             channel_user: {
                 ...this.props.roles.channel_user,
-                permissions: role.permissions.filter((p) => PermissionsScope[p] === 'channel_scope'),
+                permissions: role.permissions?.filter((p) => PermissionsScope[p] === 'channel_scope'),
             },
         };
     }
 
-    deriveRolesFromGuests = (role) => {
+    deriveRolesFromGuests = (role: Partial<Role>): Record<string, Partial<Role>> => {
         return {
             system_guest: {
                 ...this.props.roles.system_guest,
-                permissions: role.permissions.filter((p) => PermissionsScope[p] === 'system_scope'),
+                permissions: role.permissions?.filter((p) => PermissionsScope[p] === 'system_scope'),
             },
             team_guest: {
                 ...this.props.roles.team_guest,
-                permissions: role.permissions.filter((p) => PermissionsScope[p] === 'team_scope'),
+                permissions: role.permissions?.filter((p) => PermissionsScope[p] === 'team_scope'),
             },
             channel_guest: {
                 ...this.props.roles.channel_guest,
-                permissions: role.permissions.filter((p) => PermissionsScope[p] === 'channel_scope'),
+                permissions: role.permissions?.filter((p) => PermissionsScope[p] === 'channel_scope'),
             },
         };
     }
 
-    restoreExcludedPermissions = (roles) => {
+    restoreExcludedPermissions = (roles: Record<string, Partial<Role>>) => {
         for (const permission of this.props.roles.system_user.permissions) {
             if (EXCLUDED_PERMISSIONS.includes(permission)) {
-                roles.system_user.permissions.push(permission);
+                roles.system_user.permissions?.push(permission);
             }
         }
         for (const permission of this.props.roles.team_user.permissions) {
             if (EXCLUDED_PERMISSIONS.includes(permission)) {
-                roles.team_user.permissions.push(permission);
+                roles.team_user.permissions?.push(permission);
             }
         }
         for (const permission of this.props.roles.channel_user.permissions) {
             if (EXCLUDED_PERMISSIONS.includes(permission)) {
-                roles.channel_user.permissions.push(permission);
+                roles.channel_user.permissions?.push(permission);
             }
         }
         return roles;
     }
 
-    restoreGuestPermissions = (roles) => {
+    restoreGuestPermissions = (roles: Record<string, Partial<Role>>) => {
         for (const permission of this.props.roles.system_guest.permissions) {
             if (!GUEST_INCLUDED_PERMISSIONS.includes(permission)) {
-                roles.system_guest.permissions.push(permission);
+                roles.system_guest.permissions?.push(permission);
             }
         }
         for (const permission of this.props.roles.team_guest.permissions) {
             if (!GUEST_INCLUDED_PERMISSIONS.includes(permission)) {
-                roles.team_guest.permissions.push(permission);
+                roles.team_guest.permissions?.push(permission);
             }
         }
         for (const permission of this.props.roles.channel_guest.permissions) {
             if (!GUEST_INCLUDED_PERMISSIONS.includes(permission)) {
-                roles.channel_guest.permissions.push(permission);
+                roles.channel_guest.permissions?.push(permission);
             }
         }
         return roles;
@@ -260,16 +266,16 @@ export default class PermissionSystemSchemeSettings extends React.PureComponent 
         this.props.actions.setNavigationBlocked(saveNeeded);
     }
 
-    toggleRole = (roleId) => {
+    toggleRole = (roleId: string) => {
         const newOpenRoles = {...this.state.openRoles};
         newOpenRoles[roleId] = !newOpenRoles[roleId];
         this.setState({openRoles: newOpenRoles});
     }
 
-    togglePermission = (roleId, permissions) => {
+    togglePermission = (roleId: string, permissions: Iterable<string>) => {
         const roles = {...this.state.roles};
         const role = {...roles[roleId]};
-        const newPermissions = [...role.permissions];
+        const newPermissions = [...role.permissions!];
         for (const permission of permissions) {
             if (newPermissions.indexOf(permission) === -1) {
                 newPermissions.push(permission);
@@ -434,7 +440,7 @@ export default class PermissionSystemSchemeSettings extends React.PureComponent 
                 <div className='admin-console-save'>
                     <SaveButton
                         saving={this.state.saving}
-                        disabled={this.props.isDisabled || !this.state.saveNeeded || (this.canSave && !this.canSave())}
+                        disabled={this.props.isDisabled || !this.state.saveNeeded}
                         onClick={this.handleSubmit}
                         savingMessage={localizeMessage('admin.saving', 'Saving Config...')}
                     />
