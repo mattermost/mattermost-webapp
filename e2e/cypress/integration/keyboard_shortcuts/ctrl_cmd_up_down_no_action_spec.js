3a55f98b60d0715759b914ef631b33db73ed9e9a
ZeeshanAmjad0495 <zeeshan04956@gmail.com>
2021-10-18 19:25:02 +0500

    Assertions added and formatting corrected

--- e2e/cypress/integration/keyboard_shortcuts/ctrl_cmd_up_down_no_action_spec.js
+++ e2e/cypress/integration/keyboard_shortcuts/ctrl_cmd_up_down_no_action_spec.js
@@ -9,7 +9,7 @@
 
 // Group: @keyboard_shortcuts
 
-describe("Keyboard Shortcuts", () => {
+describe('Keyboard Shortcuts', () => {
     let testTeam;
     let testChannel;
     let testUser;
@@ -20,7 +20,7 @@ describe("Keyboard Shortcuts", () => {
             testChannel = channel;
             testUser = user;
 
-            cy.apiCreateUser({ prefix: "other" }).then(({ user: user1 }) => {
+            cy.apiCreateUser({ prefix: 'other' }).then(({ user: user1 }) => {
                 otherUser = user1;
 
                 cy.apiAddUserToTeam(testTeam.id, otherUser.id).then(() => {
@@ -30,20 +30,38 @@ describe("Keyboard Shortcuts", () => {
         });
     });
 
-    it("MM-T1255 CTRL/CMD+UP or DOWN no action on draft post", () => {
-        const message = "Test message from User 1";
+    it('MM-T1255 CTRL/CMD+UP or DOWN no action on draft post', () => {
+        const message = 'Test message from User 1';
         cy.apiLogin(testUser);
 
         // # Visit the channel using the channel name
         cy.visit(`/${testTeam.name}/channels/${testChannel.name}`);
 
         // # Post message in the channel from User 1
-        cy.get("#post_textbox").type(message);
+        cy.get('#post_textbox').type(message);
 
         // # Press CMD/CTRL+DOWN arrow and click to check cursor position
-        cy.get("#post_textbox").cmdOrCtrlShortcut("{downarrow}");
+        cy.get('#post_textbox').cmdOrCtrlShortcut('{downarrow}');
+
+        // * The post should have the same length as the typed text and
+        // * it should have the same text, and the text should not change
+        cy.get('#post_textbox').should('have.length', length(message))
+            .and('have.text', message)
+            .and(($div) => {
+                const text = $div.text;
+                expect(text).not.to.change();
+        });
 
         // # Press CMD/CTRL+UP arrow and click to check cursor position
-        cy.get("#post_textbox").cmdOrCtrlShortcut("{uparrow}");
+        cy.get('#post_textbox').cmdOrCtrlShortcut('{uparrow}');
+
+        // * The post should have the same length as the typed text and
+        // * it should have the same text, and the text should not change
+        cy.get('#post_textbox').should('have.length', length(message))
+            .and('have.text', message)
+            .and(($div) => {
+                const text = $div.text;
+                expect(text).not.to.change();
+        });
     });
 });
