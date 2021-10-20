4d8bf9819497b972a3d01b6b772730dbeb542f39
Furqan Malik <Furqan.Malik@mattermost.com>
2021-10-20 09:50:00 -0400

    fixed lint

--- e2e/cypress/integration/keyboard_shortcuts/keyboard_shortcuts_1_spec.js
+++ e2e/cypress/integration/keyboard_shortcuts/keyboard_shortcuts_1_spec.js
@@ -425,7 +425,7 @@ describe('Keyboard Shortcuts', () => {
         // # Type CTRL/CMD+SHIFT+A to open 'Account Settings' modal
         cy.get('#post_textbox').cmdOrCtrlShortcut('{shift}A');
         cy.uiOpenAccountSettingsModal();
-        
+
         // # Type CTRL/CMD+SHIFT+A to close 'Account Settings' modal
         cy.get('body').cmdOrCtrlShortcut('{shift}A');
         cy.get('#accountSettingsHeader').should('not.exist');
