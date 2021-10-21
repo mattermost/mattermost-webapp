71de016fcac9fab5a6fd654080df097ff840af85
Jesper Westervig Nielsen <jesper@app-it.dk>
2021-10-21 08:20:32 +0200

    Added cypress test

--- e2e/cypress/integration/keyboard_shortcuts/up_arrow_edit_message_spec.js
+++ e2e/cypress/integration/keyboard_shortcuts/up_arrow_edit_message_spec.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+
 // ***************************************************************
 // - [#] indicates a test step (e.g. # Go to a page)
 // - [*] indicates an assertion (e.g. * Check the title)
@@ -8,6 +9,8 @@
 // ***************************************************************
 
 // Group: @keyboard_shortcuts
+
+import * as TIMEOUTS from '../../fixtures/timeouts';
 
 describe('Keyboard Shortcuts', () => {
     let testTeam;
@@ -31,14 +34,16 @@ describe('Keyboard Shortcuts', () => {
         });
     });
 
-    it('MM-T1236 Arrow up key - Edit modal open up for own message of a user', () => {
-        const message1 = 'Test message from User 1';
-        const message2 = 'Test message from User 2';
-
+    beforeEach(() => {
         cy.apiLogin(testUser);
 
         // # Visit the channel using the channel name
         cy.visit(`/${testTeam.name}/channels/${testChannel.name}`);
+    });
+
+    it('MM-T1236 Arrow up key - Edit modal open up for own message of a user', () => {
+        const message1 = 'Test message from User 1';
+        const message2 = 'Test message from User 2';
 
         // # Post message in the channel from User 1
         cy.postMessage(message1);
@@ -64,5 +69,59 @@ describe('Keyboard Shortcuts', () => {
 
         // * Verify that the Edit textbox contains previously sent message by user 1
         cy.get('#edit_textbox').should('have.text', message1);
+    });
+
+    it('MM-T1271 UP - Removing all text in edit deletes post', () => {
+        // # Post message in center from otheruser
+        const message = 'Message to be deleted from other user';
+        cy.postMessageAs({sender: otherUser, message, channelId: testChannel.id});
+        cy.uiWaitUntilMessagePostedIncludes(message);
+
+        // * Verify that testuser sees post
+        cy.getLastPostId().then((postID) => {
+            cy.get(`#postMessageText_${postID}`).should('contain', message);
+        });
+
+        // # Other usesr deletes post
+        cy.getLastPostId().then((postID) => {
+            cy.externalRequest({
+                user: otherUser,
+                method: 'DELETE',
+                path: `posts/${postID}`,
+            });
+
+            // * Assert the testuser sees that message is deleted
+            cy.get(`#post_${postID}`).should('contain', '(message deleted)');
+        });
+
+        // # Login with other user
+        cy.apiLogout();
+        cy.apiLogin(otherUser);
+        cy.visit(`/${testTeam.name}/channels/${testChannel.name}`);
+
+        cy.postMessage(message);
+        cy.uiWaitUntilMessagePostedIncludes(message);
+
+        cy.getLastPostId().then((postID) => {
+            cy.get('#post_textbox').type('{uparrow}');
+
+            // * Validate that edit box contains just posted message
+            cy.get('#edit_textbox').should('have.text', message);
+
+            // # Mark all text, delete and confirm by pressing enter
+            cy.wait(TIMEOUTS.HALF_SEC);
+            cy.get('#edit_textbox').clear();
+            cy.wait(TIMEOUTS.HALF_SEC);
+            cy.get('#edit_textbox').type('{enter}');
+
+            // * Verify confirm modal is shown
+            cy.waitUntil(() => cy.get('#deletePostModal').should('be.visible'));
+
+            // # Press enter on confirm dialog
+            cy.get('#deletePostModalButton').click();
+
+            // * Verify post is deleted
+            cy.get(`#postMessageText_${postID}`).should('not.exist');
+        });
     });
 });
