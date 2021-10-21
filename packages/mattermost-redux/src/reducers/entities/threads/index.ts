3294ae3228cfe6d6acc9f39fcc77e706f01124ed
Kyriakos Z <3829551+koox00@users.noreply.github.com>
2021-10-21 14:17:32 +0300

    MM-39192: fixes mention badge updating (#9124)
    * MM-39192: fixes mention badge updating
    
    We are asking for all team unreads including threads. Those counts
    however exclude direct channels (DMs/GMs).
    We were saving those counts in entities.threads.counts, however when we
    are fetching user threads we also return counts, this time including the
    direct channels counts.
    This resulted in double updating the mention badges on teams sidebar.
    
    This commit attempts to fix that by separating the direct channels
    counts to a separate reducer (countsIncludingDirect).
    
    * Fixes types
    
    * Fixes document title and mobile current team counts
    
    * Fixes some counting and fetching issues
    
    * Add previously mistakenly removed test case
    
    * Renames function appropriately
    
    * Fixes return state when not updated
    
    (cherry picked from commit 6dda2aeee0c69c6c4ffc2f5309c958c1a4558114)

--- packages/mattermost-redux/src/reducers/entities/threads/index.ts
+++ packages/mattermost-redux/src/reducers/entities/threads/index.ts
@@ -1,15 +1,17 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {ChannelTypes, PostTypes, TeamTypes, ThreadTypes, UserTypes} from 'mattermost-redux/action_types';
+import {ChannelTypes, PostTypes, ThreadTypes, UserTypes} from 'mattermost-redux/action_types';
 import {GenericAction} from 'mattermost-redux/types/actions';
-import {Team, TeamUnread} from 'mattermost-redux/types/teams';
 import {Post} from 'mattermost-redux/types/posts';
 import {ThreadsState, UserThread} from 'mattermost-redux/types/threads';
 import {UserProfile} from 'mattermost-redux/types/users';
 import {IDMappedObjects} from 'mattermost-redux/types/utilities';
 
-import {threadsInTeamReducer, unreadThreadsInTeamReducer, ExtraData} from './threadsInTeam/threadsInTeam';
+import {threadsInTeamReducer, unreadThreadsInTeamReducer} from './threadsInTeam';
+import {countsReducer, countsIncludingDirectReducer} from './counts';
+
+import {ExtraData} from './types';
 
 export const threadsReducer = (state: ThreadsState['threads'] = {}, action: GenericAction, extra: ExtraData) => {
     switch (action.type) {
@@ -131,117 +133,6 @@ export const threadsReducer = (state: ThreadsState['threads'] = {}, action: Gene
     return state;
 };
 
-export const countsReducer = (state: ThreadsState['counts'] = {}, action: GenericAction, extra: ExtraData) => {
-    switch (action.type) {
-    case ThreadTypes.ALL_TEAM_THREADS_READ: {
-        const counts = state[action.data.team_id] ?? {};
-        return {
-            ...state,
-            [action.data.team_id]: {
-                ...counts,
-                total_unread_mentions: 0,
-                total_unread_threads: 0,
-            },
-        };
-    }
-    case TeamTypes.RECEIVED_MY_TEAM_UNREADS: {
-        const members = action.data;
-        return {
-            ...state,
-            ...members.reduce((result: ThreadsState['counts'], member: TeamUnread) => {
-                result[member.team_id] = {
-                    ...state[member.team_id],
-                    total_unread_threads: member.thread_count || 0,
-                    total_unread_mentions: member.thread_mention_count || 0,
-                };
-
-                return result;
-            }, {}),
-        };
-    }
-    case ThreadTypes.READ_CHANGED_THREAD: {
-        const {
-            teamId,
-            prevUnreadMentions = 0,
-            newUnreadMentions = 0,
-            prevUnreadReplies = 0,
-            newUnreadReplies = 0,
-        } = action.data;
-        const counts = state[teamId] ? {
-            ...state[teamId],
-        } : {
-            total_unread_threads: 0,
-            total: 0,
-            total_unread_mentions: 0,
-        };
-        const unreadMentionDiff = newUnreadMentions - prevUnreadMentions;
-
-        counts.total_unread_mentions += unreadMentionDiff;
-
-        if (newUnreadReplies > 0 && prevUnreadReplies === 0) {
-            counts.total_unread_threads += 1;
-        } else if (prevUnreadReplies > 0 && newUnreadReplies === 0) {
-            counts.total_unread_threads -= 1;
-        }
-
-        return {
-            ...state,
-            [action.data.teamId]: counts,
-        };
-    }
-    case ThreadTypes.RECEIVED_THREADS: {
-        return {
-            ...state,
-            [action.data.team_id]: {
-                total: action.data.total,
-                total_unread_threads: action.data.total_unread_threads,
-                total_unread_mentions: action.data.total_unread_mentions,
-            },
-        };
-    }
-    case TeamTypes.LEAVE_TEAM: {
-        const team: Team = action.data;
-
-        if (!state[team.id]) {
-            return state;
-        }
-
-        const nextState = {...state};
-        Reflect.deleteProperty(nextState, team.id);
-
-        return nextState;
-    }
-
-    case UserTypes.LOGOUT_SUCCESS:
-        return {};
-    case ChannelTypes.LEAVE_CHANNEL: {
-        if (!extra.threadsToDelete || extra.threadsToDelete.length === 0) {
-            return state;
-        }
-
-        const teamId = action.data.team_id;
-
-        const {unreadMentions, unreadThreads} = extra.threadsToDelete.reduce((curr, item: UserThread) => {
-            curr.unreadMentions += item.unread_mentions;
-            curr.unreadThreads = item.unread_replies > 0 ? curr.unreadThreads + 1 : curr.unreadThreads;
-            return curr;
-        }, {unreadMentions: 0, unreadThreads: 0});
-
-        const {total, total_unread_mentions: totalUnreadMentions, total_unread_threads: totalUnreadThreads} = state[teamId];
-
-        return {
-            ...state,
-            [teamId]: {
-                total: Math.max(total - extra.threadsToDelete.length, 0),
-                total_unread_mentions: Math.max(totalUnreadMentions - unreadMentions, 0),
-                total_unread_threads: Math.max(totalUnreadThreads - unreadThreads, 0),
-            },
-        };
-    }
-    }
-    return state;
-};
-
 function getThreadsOfChannel(threads: ThreadsState['threads'], channelId: string) {
     const channelThreads: UserThread[] = [];
     for (const rootId of Object.keys(threads)) {
@@ -262,6 +153,7 @@ const initialState = {
     threadsInTeam: {},
     unreadThreadsInTeam: {},
     counts: {},
+    countsIncludingDirect: {},
 };
 
 // custom combineReducers function
@@ -276,6 +168,10 @@ function reducer(state: ThreadsState = initialState, action: GenericAction): Thr
         }
     }
 
+    if (action.type === ThreadTypes.FOLLOW_CHANGED_THREAD) {
+        extra.threads = state.threads;
+    }
+
     const nextState = {
 
         // Object mapping thread ids to thread objects
@@ -287,15 +183,19 @@ function reducer(state: ThreadsState = initialState, action: GenericAction): Thr
         // Object mapping teams ids to unread thread ids
         unreadThreadsInTeam: unreadThreadsInTeamReducer(state.unreadThreadsInTeam, action, extra),
 
-        // Object mapping teams ids to unread counts
+        // Object mapping teams ids to unread counts without DM/GM
         counts: countsReducer(state.counts, action, extra),
+
+        // Object mapping teams ids to unread counts including direct channels
+        countsIncludingDirect: countsIncludingDirectReducer(state.countsIncludingDirect, action, extra),
     };
 
     if (
         state.threads === nextState.threads &&
         state.threadsInTeam === nextState.threadsInTeam &&
         state.unreadThreadsInTeam === nextState.unreadThreadsInTeam &&
-        state.counts === nextState.counts
+        state.counts === nextState.counts &&
+        state.countsIncludingDirect === nextState.countsIncludingDirect
     ) {
         // None of the children have changed so don't even let the parent object change
         return state;
