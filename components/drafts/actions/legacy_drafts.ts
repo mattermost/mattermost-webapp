419e35c1ad145a06962982c19dae253b508b3876
koox00 <cooxlee@gmail.com>
2021-10-22 16:56:06 +0300

    Improve legacy drafts sync

--- components/drafts/actions/legacy_drafts.ts
+++ components/drafts/actions/legacy_drafts.ts
@@ -11,9 +11,6 @@ import {makeGetDraftsByPrefix, DraftSelector} from 'selectors/drafts';
 import {GlobalState} from 'types/store';
 import {StoragePrefixes, StorageTypes} from 'utils/constants';
 
-const getChannelDrafts: DraftSelector = makeGetDraftsByPrefix(StoragePrefixes.DRAFT);
-const getThreadDrafts: DraftSelector = makeGetDraftsByPrefix(StoragePrefixes.COMMENT_DRAFT);
-
 // Drafts pre global drafts are missing the channelId in the value
 // thus making it hard to determine in which team they belong.
 //
@@ -22,22 +19,38 @@ const getThreadDrafts: DraftSelector = makeGetDraftsByPrefix(StoragePrefixes.COM
 // it's not guaranteed that they exist at mount.
 // This should be an one-off.
 export function syncLegacyDrafts() {
-    return (dispatch: DispatchFunc, getState: GetStateFunc) => {
+    const getChannelDrafts: DraftSelector = makeGetDraftsByPrefix(StoragePrefixes.DRAFT);
+    const getThreadDrafts: DraftSelector = makeGetDraftsByPrefix(StoragePrefixes.COMMENT_DRAFT);
+
+    return async (dispatch: DispatchFunc, getState: GetStateFunc) => {
         const state = getState() as GlobalState;
 
+        let posts = getAllPosts(state);
         const channelDrafts = getChannelDrafts(state);
         const threadDrafts = getThreadDrafts(state);
-        const posts = getAllPosts(state);
-
         const actions: GenericAction[] = [];
-
         const legacyChannelDrafts = channelDrafts?.filter((draft) => !draft?.value?.channelId);
         const legacyThreadDrafts = threadDrafts?.filter((draft) => !draft?.value?.channelId);
 
+        // root post pre-fetching
+        await legacyChannelDrafts.reduce(async (prevPromise, draft): Promise<any> => {
+            await prevPromise;
+
+            // do nothing if we already have the root post
+            if (posts[draft.id]) {
+                return Promise.resolve();
+            }
+
+            return dispatch(getPost(draft.id));
+        }, Promise.resolve());
+
+        // new posts state
+        posts = getAllPosts(getState());
+
         legacyChannelDrafts.forEach((draft) => {
             const updatedValue = {...draft.value, channelId: draft.id};
-            localStorage.setItem(String(draft.key), JSON.stringify(updatedValue));
 
+            localStorage.setItem(String(draft.key), JSON.stringify(updatedValue));
             actions.push({
                 type: StorageTypes.SET_GLOBAL_ITEM,
                 data: {
@@ -51,8 +64,8 @@ export function syncLegacyDrafts() {
         legacyThreadDrafts.forEach(async (draft) => {
             if (posts[draft.id]) {
                 const updatedValue = {...draft.value, channelId: posts[draft.id].channel_id};
-                localStorage.setItem(String(draft.key), JSON.stringify(updatedValue));
 
+                localStorage.setItem(String(draft.key), JSON.stringify(updatedValue));
                 actions.push({
                     type: StorageTypes.SET_GLOBAL_ITEM,
                     data: {
@@ -61,22 +74,6 @@ export function syncLegacyDrafts() {
                         value: updatedValue,
                     },
                 });
-            } else {
-                const {data, error} = await dispatch(getPost(draft.id));
-
-                if (!error && data) {
-                    const updatedValue = {...draft.value, channelId: data.channel_id};
-                    localStorage.setItem(String(draft.key), JSON.stringify(updatedValue));
-
-                    actions.push({
-                        type: StorageTypes.SET_GLOBAL_ITEM,
-                        data: {
-                            name: draft.key,
-                            timestamp: new Date(),
-                            value: updatedValue,
-                        },
-                    });
-                }
             }
         });
 
