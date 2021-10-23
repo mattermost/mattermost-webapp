bde79e5549b9634dc640f30d76c9236b999a8df1
ZeeshanAmjad0495 <62217382+ZeeshanAmjad0495@users.noreply.github.com>
2021-10-23 11:31:43 +0500

    Update ctrl_cmd_up_down_in_rhs_spec.js

--- e2e/cypress/ctrl_cmd_up_down_in_rhs_spec.js
+++ e2e/cypress/ctrl_cmd_up_down_in_rhs_spec.js
@@ -17,10 +17,10 @@ describe('Keyboard Shortcuts', () => {
     });
 
     it('MM - T1257 CTRL / CMD + UP or DOWN in RHS', () => {
-        // # Reload page
         const firstMessage = 'Hello World!';
         const messages = ['This', 'is', 'an', 'e2e test', '/shrug'];
 
+        // # Reload page
         cy.reload();
 
         cy.postMessage(firstMessage);
@@ -33,49 +33,45 @@ describe('Keyboard Shortcuts', () => {
             // * Confirm that reply text box has focus
             cy.get('#reply_textbox').should('be.focused');
 
-            // * Confirm the RHS is shown
-            cy.get('#rhsCloseButton').should('exist');
-
-            // # Post messages in rhs sidebar
-            cy.get('.sidebar-right-container').should('be.visible').within(() => {
-                for (let idx = 0; idx < 5; idx++) {
-                    cy.findByRole('form', {name: 'reply input region'}).type(messages[idx]);
-                    cy.findByTestId('reply_textbox').type('{enter}');
-
-                    // # Clear textbox
-                    cy.findByTestId('reply_textbox').clear();
+            // * Verify RHS is opened
+            cy.uiGetRHS().within(() => {
+                for (let idx = 0; idx < messages.length; idx++) {
+                    // # Post each message as a reply
+                    cy.get('#reply_textbox').
+                        type(messages[idx]).
+                        type('{enter}').
+                        clear();
                 }
+
+                // * Confirm that reply textbox has focus
+
+                cy.get('#reply_textbox').should('be.focused');
+
+                // # Press CTRL/CMD + uparrow repeatedly
+                const previousMessageIndex = messages.length - 1;
+                for (let idx = 0; idx < messages.length + 3; idx++) {
+                    cy.findByTestId('reply_textbox').cmdOrCtrlShortcut('{uparrow}');
+
+                    // * Check if the message is equal to the last message
+                    cy.findByTestId('reply_textbox').should('have.text', messages[previousMessageIndex]);
+                    if (idx === messages.length + 2) {
+                        // * Check if the last message is equal to the first message
+                        cy.findByTestId('reply_textbox').should('have.text', firstMessage);
+                    }
+                }
+
+                // * Press CTRL/CMD + downarrow check if the current text is equal to the second message
+                cy.findByTestId('reply_textbox').cmdOrCtrlShortcut('{downarrow}').should('have.text', messages[0]);
+
+                // # Close the rhs bar
+                cy.get('#rhsCloseButton').click();
             });
 
-            // * Check if text box is focused
-            cy.findByTestId('reply_textbox').should('be.focused');
+            // * Press CTRL/CMD + uparrow in central textbox check if the text is equal to the last message
+            cy.findByTestId('post_textbox').cmdOrCtrlShortcut('{uparrow}').should('have.text', messages[messages.length - 1]);
 
-            // # Clear textbox
-            cy.focused().clear();
-
-            // # Press CTRL/CMD + uparrow repeatedly
-            for (let idx = 0; idx < 8; idx++) {
-                cy.findByTestId('reply_textbox').cmdOrCtrlShortcut('{upArrow}');
-                if (idx === 7) {
-                    // * Check if the last message is equal to the first message
-                    cy.findByTestId('reply_textbox').should('have.text', firstMessage);
-                }
-            }
-
-            // # Press CTRL/CMD + downarrow
-            // * Check if the current text is equal to the second message
-            cy.findByTestId('reply_textbox').cmdOrCtrlShortcut('{downArrow}').should('have.text', messages[0]);
-
-            // # Close the rhs bar
-            cy.get('#rhsCloseButton').click();
-
-            // # Press CTRL/CMD + uparrow in central textbox
-            // * Check if the text is equal to the last message
-            cy.findByTestId('post_textbox').cmdOrCtrlShortcut('{upArrow}').should('have.text', messages[4]);
-
-            // # Press CTRL/CMD + downarrow in central textbox
-            // * Check if the text is equal to the last message / there is no change in text
-            cy.findByTestId('post_textbox').cmdOrCtrlShortcut('{downArrow}').should('have.text', messages[4]);
+            // * Press CTRL/CMD + downarrow in central textbox check if the text is equal to the last message / there is no change in text
+            cy.findByTestId('post_textbox').cmdOrCtrlShortcut('{downarrow}').should('have.text', messages[messages.length - 1]);
         });
     });
 });
