3595f16e0dd259f04694ca827c332b66dc1fdedc
Harrison Healey <harrisonmhealey@gmail.com>
2021-10-28 18:18:36 -0400

    MM-39580 Add onExited prop that temporarily duplicates onHide (#9280)
    * MM-39580 Add onExited prop that duplicates onHide
    
    * Remove 'as any' interfering with a type check

--- components/modal_controller/modal_controller.tsx
+++ components/modal_controller/modal_controller.tsx
@@ -50,6 +50,12 @@ export default class ModalController extends React.PureComponent<Props> {
                 const modal = modalState[modalId];
                 if (modal.open) {
                     const modalComponent = React.createElement(modal.dialogType, Object.assign({}, modal.dialogProps, {
+                        onExited: () => {
+                            props.actions.closeModal(modalId);
+
+                            // Call any onExited prop provided by whoever opened the modal, if one was provided
+                            modal.dialogProps?.onExited?.();
+                        },
                         onHide: props.actions.closeModal.bind(this, modalId),
                         key: `${modalId}_modal`,
                     }));
