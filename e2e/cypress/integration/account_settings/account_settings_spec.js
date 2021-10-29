107300e22c328bbdda39e90103d9cd4812d57268
shzmr <shzmrdev@gmail.com>
2021-10-29 08:47:15 +0000

    [GH-18811] Added E2E test account Settings link in own popover

--- e2e/cypress/integration/account_settings/account_settings_spec.js
+++ e2e/cypress/integration/account_settings/account_settings_spec.js
@@ -14,26 +14,39 @@ describe('Account Settings', () => {
         // # Login as new user and visit off-topic
         cy.apiInitSetup({loginAfter: true}).then(({offTopicUrl}) => {
             cy.visit(offTopicUrl);
-
-            // # Go to Account Settings
-            cy.uiOpenAccountSettingsModal('Security');
-
-            // * Check that the Security tab is loaded
-            cy.get('#securityButton').should('be.visible');
-
-            // # Click the Security tab
-            cy.get('#securityButton').click();
         });
     });
 
+    it('MM-T2049 Account Settings link in own popover', () => {
+        // # Click avatar to open profile popover
+        cy.get('div.status-wrapper').should('be.visible').click();
+
+        // # Click account settings link
+        cy.get('#accountSettings').should('be.visible').click();
+
+        // # Check if account settings modal is open
+        cy.get('#accountSettingsModal').should('be.visible');
+
+        cy.uiClose();
+    });
+
     it('MM-T2081 Password: Error on blank', () => {
+        // # Go to Account Settings
+        cy.uiOpenAccountSettingsModal('Security');
+
+        // * Check that the Security tab is loaded
+        cy.get('#securityButton').should('be.visible');
+
+        // # Click the Security tab
+        cy.get('#securityButton').click();
+
         // # Click "Edit" to the right of "Password"
         cy.get('#passwordEdit').should('be.visible').click();
 
         // # Save the settings
         cy.uiSave();
 
-        // * Check that there are no errors
+        // * Check that there is an error
         cy.get('#clientError').should('be.visible').should('contain', 'Please enter your current password.');
         cy.get('#serverError').should('not.exist');
     });
