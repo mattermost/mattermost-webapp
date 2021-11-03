18271dde16f6d1d85277805f86c00f4027805fe0
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2021-11-03 11:00:38 -0400

    Revert "use original channel header icons instead of marketplace icons"
    This reverts commit 1cab72c00bd3f7d3cb3a56d54564f63a362ce659.

--- components/app_bar/app_bar.test.tsx
+++ components/app_bar/app_bar.test.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 import {Channel} from 'mattermost-redux/types/channels';
+import {MarketplacePlugin, MarketplaceApp} from 'mattermost-redux/types/marketplace';
 import {Theme} from 'mattermost-redux/types/themes';
 
 import {PluginComponent} from 'types/store/plugins';
@@ -12,13 +13,28 @@ import {PluginComponent} from 'types/store/plugins';
 import AppBar, {Props} from './app_bar';
 
 describe('components/app_bar/app_bar', () => {
-    const channelHeaderIcon = <img id='the_icon'/>;
+    const marketplaceListing: Array<MarketplacePlugin | MarketplaceApp> = [
+        {
+            icon_data: 'playbooks_icon_data',
+            manifest: {
+                id: 'playbooks',
+                version: '1.0.0',
+            },
+        } as MarketplacePlugin,
+        {
+            manifest: {
+                app_id: 'com.mattermost.zendesk',
+            },
+            installed: true,
+            icon_url: 'zendesk_icon_data',
+        } as MarketplaceApp,
+    ];
 
     const channelHeaderComponents: PluginComponent[] = [
         {
             id: 'the_component_id',
             pluginId: 'playbooks',
-            icon: channelHeaderIcon,
+            icon: 'fallback_component' as any,
             action: jest.fn(),
         },
     ];
@@ -27,32 +43,92 @@ describe('components/app_bar/app_bar', () => {
         activePluginId: 'playbooks',
         appBarBindings: [],
         channelHeaderComponents,
+        marketplaceListing,
         channel: {} as Channel,
+        actions: {
+            fetchListing: jest.fn().mockResolvedValue({data: []}),
+        },
         theme: {} as Theme,
-        show: true,
     };
 
     test('should match snapshot on mount', async () => {
         const props = {
             ...baseProps,
+            actions: {
+                fetchListing: jest.fn().mockResolvedValue({data: []}),
+            },
         };
 
         const wrapper = shallow<AppBar>(
             <AppBar {...props}/>,
         );
 
+        expect(props.actions.fetchListing).toHaveBeenCalled();
+        expect(wrapper.state().loadedMarketplaceListing).toBe(false);
+        expect(wrapper).toMatchSnapshot();
+
+        await props.actions.fetchListing();
+        expect(wrapper.state().loadedMarketplaceListing).toBe(true);
         expect(wrapper).toMatchSnapshot();
     });
 
-    test('should show provided icon for each channel header component', async () => {
+    test('should show icon from plugin marketplace', async () => {
         const props = {
             ...baseProps,
+            actions: {
+                fetchListing: jest.fn().mockResolvedValue({data: []}),
+            },
         };
 
         const wrapper = shallow<AppBar>(
             <AppBar {...props}/>,
         );
 
-        expect(wrapper.find(channelHeaderIcon).exists()).toBe(false);
+        await props.actions.fetchListing();
+
+        expect(wrapper.find('img[src="playbooks_icon_data"]').exists()).toBe(true);
+    });
+
+    test('should show fallback icon when marketplace entry is missing', async () => {
+        const props = {
+            ...baseProps,
+            actions: {
+                fetchListing: jest.fn().mockResolvedValue({data: []}),
+            },
+            marketplaceListing: [],
+        };
+
+        const wrapper = shallow<AppBar>(
+            <AppBar {...props}/>,
+        );
+
+        await props.actions.fetchListing();
+
+        expect(wrapper.find('img').exists()).toBe(false);
+        expect(wrapper.text()).toContain('fallback_component');
+    });
+
+    test('should show fallback icon when marketplace icon is missing', async () => {
+        const props = {
+            ...baseProps,
+            actions: {
+                fetchListing: jest.fn().mockResolvedValue({data: []}),
+            },
+            marketplaceListing: [
+                {
+                    ...marketplaceListing[0],
+                    icon_data: '',
+                },
+            ],
+        };
+
+        const wrapper = shallow<AppBar>(
+            <AppBar {...props}/>,
+        );
+
+        await props.actions.fetchListing();
+
+        expect(wrapper.find('img').exists()).toBe(false);
+        expect(wrapper.text()).toContain('fallback_component');
     });
 });
