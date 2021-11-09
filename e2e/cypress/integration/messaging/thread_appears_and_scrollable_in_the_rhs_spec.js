8b93ac1f1827b7bb2d87cf66938095f7a2adaa1a
Aleksey Poflankov <87149297+poflankov@users.noreply.github.com>
2021-11-09 21:50:10 +0300

    Major refactor

--- e2e/cypress/integration/messaging/thread_appears_and_scrollable_in_the_rhs_spec.js
+++ e2e/cypress/integration/messaging/thread_appears_and_scrollable_in_the_rhs_spec.js
@@ -9,79 +9,74 @@
 //
 // Group: @messaging
 
-import {postListOfMessages} from '../scroll/helpers';
-
 describe('Thread Scrolling Inside RHS ', () => {
-    let channelId;
-    let channelLink;
-    let mainUser;
-    let otherUser;
-
-    beforeEach(() => {
-        cy.apiAdminLogin();
-        cy.apiInitSetup().then(({user, team, channel}) => {
-            mainUser = user;
-            channelId = channel.id;
-            channelLink = `/${team.name}/channels/${channel.name}`;
-            cy.apiCreateUser().then(({user: user2}) => {
-                otherUser = user2;
-                cy.apiAddUserToTeam(team.id, otherUser.id).then(() => {
-                    cy.apiAddUserToChannel(channelId, otherUser.id);
-                });
-            });
-        });
+    beforeEach(function() {
+        cy.
+            apiCreateUser().its('user').as('otherUser').
+            apiInitSetup().
+            then(({user, team, channel, channelUrl}) => cy.
+                apiAddUserToTeam(team.id, this.otherUser.id).
+                apiAddUserToChannel(channel.id, this.otherUser.id).
+                wrap(user).as('mainUser').
+                wrap(channel).as('channel').
+                wrap(channelUrl).as('channelUrl'),
+            );
     });
 
     it('MM-T3293 The entire thread appears in the RHS (scrollable)', function() {
         const NUMBER_OF_REPLIES = 100;
         const NUMBER_OF_MAIN_THREAD_MESSAGES = 40;
-        const FIRST_MESSAGE = 'The 1 message';
-        const LAST_REPLY = 'Last Reply';
-
-        cy.apiLogin(mainUser);
 
         cy.
 
             // # Create a thread with several replies to make it scrollable in RHS
-            postMessageAs({sender: mainUser, message: FIRST_MESSAGE, channelId}).its('id').as('firstPostId').
-            then((rootId) => {
-                for (var i = 0; i < NUMBER_OF_REPLIES; i++) {
-                    cy.postMessageAs({sender: mainUser, message: `Reply ${i}`, channelId, rootId});
-                }
-            }).
+            postMessageAs({sender: this.mainUser, message: 'First message', channelId: this.channel.id}).
+            as('firstPost').
+
+            then((firstPost) => cy.postListOfMessages({
+                numberOfMessages: NUMBER_OF_REPLIES,
+                sender: this.mainUser,
+                channelId: this.channel.id,
+                rootId: firstPost.id,
+            })).
+            then((messages) => messages.map((m) => m.data.message)).
+            as('replies').
 
             // # Create enough posts from another user (not related to the thread on the same channel) to not load on a first load
-            then(() => postListOfMessages({sender: otherUser, channelId, numberOfMessages: NUMBER_OF_MAIN_THREAD_MESSAGES})).
+            then(() => cy.postListOfMessages({
+                numberOfMessages: NUMBER_OF_MAIN_THREAD_MESSAGES,
+                sender: this.otherUser,
+                channelId: this.channel.id,
+            })).
 
             // # Reply on original thread
-            then(() => cy.postMessageAs({sender: mainUser, message: LAST_REPLY, channelId, rootId: this.firstPostId})).
-            then(() => {
-                // # Load the channel
-                cy.visit(channelLink);
+            then(() => cy.postMessageAs({
+                sender: this.mainUser,
+                message: 'Last Reply',
+                channelId: this.channel.id,
+                rootId: this.firstPost.id,
+            })).
+            then((lastReply) => {
+            // # Load the channel
+                cy.visit(this.channelUrl);
 
                 // # Hit on reply to open thread on RHS
-                cy.clickPostCommentIcon();
+                cy.clickPostCommentIcon(lastReply.id);
 
                 // * Verify that entire thread appears in the RHS (scrollable)
                 cy.uiGetRHS().within(() => {
-                    cy.findByText(LAST_REPLY).should('exist');
-                    checkRepliesUpFrom(NUMBER_OF_REPLIES);
-                    cy.findByText(FIRST_MESSAGE).should('exist');
+                    cy.findByText(lastReply.data.message).should('exist');
+
+                    // We iterate the message list from the end
+                    // checking messages one by one
+                    // and scrolling up on every step to load more previous messages
+                    this.replies.reduceRight(
+                        (chain, reply) => chain.then(() => cy.findByText(reply).scrollIntoView()),
+                        cy,
+                    );
+
+                    cy.findByText(this.firstPost.data.message).should('exist');
                 });
             });
     });
 });
-
-function checkRepliesUpFrom(fromReplyNumber) {
-    const currentReplyNumber = fromReplyNumber - 1;
-
-    if (currentReplyNumber < 0) {
-        return undefined;
-    }
-
-    return cy.
-        findByText(`Reply ${currentReplyNumber}`).
-        should('exist').
-        scrollIntoView().
-        then(() => checkRepliesUpFrom(currentReplyNumber));
-}
