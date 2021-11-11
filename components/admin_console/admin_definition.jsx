b7cb246e4956f1576f124b3c9b43d666a8d786c4
Harrison Healey <harrisonmhealey@gmail.com>
2021-11-11 16:13:57 -0500

    MM-39292 Remove unused component DataRetentionSettingsOld

--- components/admin_console/admin_definition.jsx
+++ components/admin_console/admin_definition.jsx
@@ -48,7 +48,7 @@ import ChannelSettings from './team_channel_settings/channel';
 import ChannelDetails from './team_channel_settings/channel/details';
 import PasswordSettings from './password_settings.jsx';
 import PushNotificationsSettings from './push_settings.jsx';
-import DataRetentionSettings from './data_retention_settings/index.ts';
+import DataRetentionSettings from './data_retention_settings';
 import GlobalDataRetentionForm from './data_retention_settings/global_policy_form';
 import CustomDataRetentionForm from './data_retention_settings/custom_policy_form';
 import MessageExportSettings from './message_export_settings.jsx';
