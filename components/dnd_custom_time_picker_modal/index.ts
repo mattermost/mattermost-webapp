c4bdfaf418a314284fb3935f6d2fd2f291ed1907
Jesús Espino <jespinog@gmail.com>
2021-11-12 22:43:50 +0100

    Some other improvements around space usage reduction

--- components/dnd_custom_time_picker_modal/index.ts
+++ components/dnd_custom_time_picker_modal/index.ts
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import React from 'react';
 import {connect} from 'react-redux';
 
 import {bindActionCreators, Dispatch} from 'redux';
@@ -10,8 +11,9 @@ import {setStatus} from 'mattermost-redux/actions/users';
 import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
 
 import {GlobalState} from 'types/store';
+import {makeAsyncComponent} from 'components/async_load';
 
-import DndCustomTimePicker from './dnd_custom_time_picker_modal';
+const DndCustomTimePicker = makeAsyncComponent(React.lazy(() => import('./dnd_custom_time_picker_modal')));
 
 function mapStateToProps(state: GlobalState) {
     const userId = getCurrentUserId(state);
