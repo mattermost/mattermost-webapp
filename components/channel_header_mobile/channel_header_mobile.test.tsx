35381201145d103454e46fedb20670db3fd6d704
Harrison Healey <harrisonmhealey@gmail.com>
2021-11-24 11:08:06 -0500

    MM-40114 Migrate ChannelHeadedMobile to TS and switch to getIsMobileView

--- components/channel_header_mobile/channel_header_mobile.test.tsx
+++ components/channel_header_mobile/channel_header_mobile.test.tsx
@@ -1,8 +1,10 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
 import {shallow} from 'enzyme';
+import React from 'react';
+
+import {TestHelper} from 'utils/test_helper';
 
 import ChannelHeaderMobile from './channel_header_mobile';
 
@@ -13,27 +15,28 @@ describe('components/ChannelHeaderMobile/ChannelHeaderMobile', () => {
     });
 
     const baseProps = {
-        user: {
+        user: TestHelper.getUserMock({
             id: 'user_id',
-        },
-        channel: {type: 'O', id: 'channel_id', display_name: 'display_name', team_id: 'team_id'},
-        member: {id: 'member_id'},
+        }),
+        channel: TestHelper.getChannelMock({
+            type: 'O',
+            id: 'channel_id',
+            display_name: 'display_name',
+            team_id: 'team_id',
+        }),
+        member: TestHelper.getChannelMembershipMock({
+            channel_id: 'channel_id',
+            user_id: 'user_id',
+        }),
         teamDisplayName: 'team_display_name',
         isPinnedPosts: true,
         actions: {
             closeLhs: jest.fn(),
             closeRhs: jest.fn(),
             closeRhsMenu: jest.fn(),
-            leaveChannel: jest.fn(),
-            markFavorite: jest.fn(),
-            showPinnedPosts: jest.fn(),
-            toggleLhs: jest.fn(),
-            toggleRhsMenu: jest.fn(),
-            unmarkFavorite: jest.fn(),
-            updateChannelNotifyProps: jest.fn(),
-            updateRhsState: jest.fn(),
         },
         isLicensed: true,
+        isMobileView: false,
         isFavoriteChannel: false,
     };
 
@@ -48,7 +51,13 @@ describe('components/ChannelHeaderMobile/ChannelHeaderMobile', () => {
     test('should match snapshot, for default channel', () => {
         const props = {
             ...baseProps,
-            channel: {type: 'O', id: '123', name: 'town-square', display_name: 'Town Square', team_id: 'team_id'},
+            channel: TestHelper.getChannelMock({
+                type: 'O',
+                id: '123',
+                name: 'town-square',
+                display_name: 'Town Square',
+                team_id: 'team_id',
+            }),
         };
         const wrapper = shallow(
             <ChannelHeaderMobile {...props}/>,
@@ -57,32 +66,16 @@ describe('components/ChannelHeaderMobile/ChannelHeaderMobile', () => {
         expect(wrapper).toMatchSnapshot();
     });
 
-    test('should match snapshot, if not licensed', () => {
-        const wrapper = shallow(
-            <ChannelHeaderMobile
-                {...baseProps}
-                isLicensed={false}
-            />,
-        );
-
-        expect(wrapper).toMatchSnapshot();
-    });
-
-    test('should match snapshot, if enabled WebRTC and DM channel', () => {
+    test('should match snapshot, if DM channel', () => {
         const props = {
             ...baseProps,
-            channel: {type: 'D', id: 'channel_id', name: 'user_id_1__user_id_2', display_name: 'display_name', team_id: 'team_id'},
-            enableWebrtc: true,
-        };
-        const wrapper = shallow(<ChannelHeaderMobile {...props}/>);
-
-        expect(wrapper).toMatchSnapshot();
-    });
-
-    test('should match snapshot, if WebRTC is not enabled', () => {
-        const props = {
-            ...baseProps,
-            enableWebrtc: false,
+            channel: TestHelper.getChannelMock({
+                type: 'D',
+                id: 'channel_id',
+                name: 'user_id_1__user_id_2',
+                display_name: 'display_name',
+                team_id: 'team_id',
+            }),
         };
         const wrapper = shallow(<ChannelHeaderMobile {...props}/>);
 
@@ -92,7 +85,12 @@ describe('components/ChannelHeaderMobile/ChannelHeaderMobile', () => {
     test('should match snapshot, for private channel', () => {
         const props = {
             ...baseProps,
-            channel: {type: 'P', id: 'channel_id', display_name: 'display_name', team_id: 'team_id'},
+            channel: TestHelper.getChannelMock({
+                type: 'P',
+                id: 'channel_id',
+                display_name: 'display_name',
+                team_id: 'team_id',
+            }),
         };
         const wrapper = shallow(<ChannelHeaderMobile {...props}/>);
 
