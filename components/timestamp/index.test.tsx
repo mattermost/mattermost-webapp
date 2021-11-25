535277353b7b8d59031aa62e74cde9c45b41fddf
Julien Tant <julien@craftyx.fr>
2021-11-25 08:02:34 -0700

    simplify testStates

--- components/timestamp/index.test.tsx
+++ components/timestamp/index.test.tsx
@@ -2,6 +2,9 @@
 // See LICENSE.txt for license information.
 
 import {GlobalState} from 'types/store';
+import {UserTimezone} from 'mattermost-redux/types/users';
+
+import {PreferenceType} from 'mattermost-redux/types/preferences';
 
 import {mapStateToProps} from './index';
 
@@ -35,84 +38,48 @@ describe('mapStateToProps', () => {
     });
 
     test('hourCycle should be h23 when military time is true and the prop was not set', () => {
-        const testState = {
-            ...initialState,
-            entities: {
-                ...initialState.entities,
-                preferences: {
-                    myPreferences: {
-                        'display_settings--use_military_time': 'true',
-                    },
-                },
-            },
-        } as unknown as GlobalState;
+        const testState = {...initialState};
+        testState.entities.preferences.myPreferences['display_settings--use_military_time'] = {
+            category: 'display_settings',
+            name: 'use_military_time',
+            user_id: currentUserId,
+            value: 'true',
+        } as PreferenceType;
 
         const props = mapStateToProps(testState, {});
         expect(props.hourCycle).toBe('h23');
     });
 
     test('hourCycle should have the value of prop.hourCycle when given', () => {
-        const testState = {
-            ...initialState,
-            entities: {
-                ...initialState.entities,
-                preferences: {
-                    myPreferences: {
-                        'display_settings--use_military_time': 'true',
-                    },
-                },
-            },
-        } as unknown as GlobalState;
+        const testState = {...initialState};
+        testState.entities.preferences.myPreferences['display_settings--use_military_time'] = {
+            category: 'display_settings',
+            name: 'use_military_time',
+            user_id: currentUserId,
+            value: 'true',
+        } as PreferenceType;
 
         const props = mapStateToProps(testState, {hourCycle: 'h24'});
         expect(props.hourCycle).toBe('h24');
     });
 
     test('timeZone should be the user TZ when the prop was not set', () => {
-        const testState = {
-            ...initialState,
-            entities: {
-                ...initialState.entities,
-                users: {
-                    ...initialState.entities.users,
-                    profiles: {
-                        ...initialState.entities.users.profiles,
-                        [currentUserId]: {
-                            ...initialState.entities.users.profiles[currentUserId],
-                            timezone: {
-                                useAutomaticTimezone: false,
-                                manualTimezone: 'Europe/Paris',
-                            },
-                        },
-                    },
-                },
-            },
-        } as unknown as GlobalState;
+        const testState = {...initialState};
+        testState.entities.users.profiles[currentUserId].timezone = {
+            useAutomaticTimezone: false,
+            manualTimezone: 'Europe/Paris',
+        } as UserTimezone;
 
         const props = mapStateToProps(testState, {});
         expect(props.timeZone).toBe('Europe/Paris');
     });
 
     test('timeZone should be the value of prop.timeZone when given', () => {
-        const testState = {
-            ...initialState,
-            entities: {
-                ...initialState.entities,
-                users: {
-                    ...initialState.entities.users,
-                    profiles: {
-                        ...initialState.entities.users.profiles,
-                        [currentUserId]: {
-                            ...initialState.entities.users.profiles[currentUserId],
-                            timezone: {
-                                useAutomaticTimezone: false,
-                                manualTimezone: 'Europe/Paris',
-                            },
-                        },
-                    },
-                },
-            },
-        } as unknown as GlobalState;
+        const testState = {...initialState};
+        testState.entities.users.profiles[currentUserId].timezone = {
+            useAutomaticTimezone: false,
+            manualTimezone: 'Europe/Paris',
+        } as UserTimezone;
 
         const props = mapStateToProps(testState, {timeZone: 'America/Phoenix'});
         expect(props.timeZone).toBe('America/Phoenix');
