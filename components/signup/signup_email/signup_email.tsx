e35e69283cee26bb8363e65478b39f3dd0747768
Carrie Warner (Mattermost) <74422101+cwarnermm@users.noreply.github.com>
2021-11-26 12:13:39 -0500

    Updated Terms of Service to Terms of Use (#9412)
    * Updated Terms of Service to Terms of Use
    
    * update remaing UI texts and fix E2E tests
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    Co-authored-by: Saturnino Abril <saturnino.abril@gmail.com>

--- components/signup/signup_email/signup_email.tsx
+++ components/signup/signup_email/signup_email.tsx
@@ -530,7 +530,7 @@ export default class SignupEmail extends React.PureComponent<Props, State> {
                 <p id='signup_agreement'>
                     <FormattedMarkdownMessage
                         id='create_team.agreement'
-                        defaultMessage='By proceeding to create your account and use {siteName}, you agree to our [Terms of Service]({TermsOfServiceLink}) and [Privacy Policy]({PrivacyPolicyLink}). If you do not agree, you cannot use {siteName}.'
+                        defaultMessage='By proceeding to create your account and use {siteName}, you agree to our [Terms of Use]({TermsOfServiceLink}) and [Privacy Policy]({PrivacyPolicyLink}). If you do not agree, you cannot use {siteName}.'
                         values={{
                             siteName,
                             TermsOfServiceLink: `!${termsOfServiceLink}`,
