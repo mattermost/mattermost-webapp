ca2314eb5f1bfb6a645603a0356021de3775172f
Collin <collineng@gmail.com>
2021-12-01 18:05:37 -0800

    MM-22846 init share message modal

---
+++ components/share_message_modal/share_message_modal.test.tsx
@@ -0,0 +1,223 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {shallow} from 'enzyme';
+import {Modal} from 'react-bootstrap';
+
+import {UserProfile} from 'mattermost-redux/types/users';
+import {Channel} from 'mattermost-redux/types/channels';
+import {RelationOneToOne} from 'mattermost-redux/types/utilities';
+
+import {Value} from 'components/multiselect/multiselect';
+
+import ChannelInviteModal from 'components/channel_invite_modal/channel_invite_modal';
+
+type UserProfileValue = Value & UserProfile;
+
+describe('components/channel_invite_modal', () => {
+    const users = [{
+        id: 'user-1',
+        label: 'user-1',
+        value: 'user-1',
+        delete_at: 0,
+    } as UserProfileValue, {
+        id: 'user-2',
+        label: 'user-2',
+        value: 'user-2',
+        delete_at: 0,
+    } as UserProfileValue];
+
+    const userStatuses = {
+        'user-1': 'online',
+        'user-2': 'offline',
+    } as RelationOneToOne<UserProfile, string>;
+
+    const channel = {
+        create_at: 1508265709607,
+        creator_id: 'zaktnt8bpbgu8mb6ez9k64r7sa',
+        delete_at: 0,
+        display_name: 'testing',
+        header: 'test',
+        id: 'owsyt8n43jfxjpzh9np93mx1wa',
+        last_post_at: 1508265709635,
+        name: 'testing',
+        purpose: 'test',
+        team_id: 'eatxocwc3bg9ffo9xyybnj4omr',
+        type: 'O',
+        update_at: 1508265709607,
+    } as Channel;
+
+    const baseProps = {
+        // channel,
+        // profilesNotInCurrentChannel: [],
+        // profilesNotInCurrentTeam: [],
+        // userStatuses: {},
+        // actions: {
+        //     addUsersToChannel: jest.fn().mockImplementation(() => {
+        //         const error = {
+        //             message: 'Failed',
+        //         };
+
+        //         return Promise.resolve({error});
+        //     }),
+        //     getProfilesNotInChannel: jest.fn().mockImplementation(() => Promise.resolve()),
+        //     getTeamStats: jest.fn(),
+        //     getUserStatuses: jest.fn().mockImplementation(() => Promise.resolve()),
+        //     loadStatusesForProfilesList: jest.fn(),
+        //     searchProfiles: jest.fn(),
+        // },
+        // onHide: jest.fn(),
+    };
+
+    // test('should match snapshot for channel_invite_modal with profiles', () => {
+    //     const wrapper = shallow(
+    //         <ChannelInviteModal
+    //             {...baseProps}
+    //             profilesNotInCurrentChannel={users}
+    //             profilesNotInCurrentTeam={[]}
+    //         />,
+    //     );
+    //     expect(wrapper).toMatchSnapshot();
+    // });
+
+    // test('should match snapshot with exclude and include users', () => {
+    //     const wrapper = shallow(
+    //         <ChannelInviteModal
+    //             {...baseProps}
+    //             profilesNotInCurrentChannel={users}
+    //             profilesNotInCurrentTeam={[]}
+    //             includeUsers={
+    //                 {
+    //                     'user-3': {
+    //                         id: 'user-3',
+    //                         label: 'user-3',
+    //                         value: 'user-3',
+    //                         delete_at: 0,
+    //                     } as UserProfileValue,
+    //                 }
+    //             }
+    //             excludeUsers={
+    //                 {
+    //                     'user-1': {
+    //                         id: 'user-1',
+    //                         label: 'user-1',
+    //                         value: 'user-1',
+    //                         delete_at: 0,
+    //                     } as UserProfileValue,
+    //                 }
+    //             }
+    //         />,
+    //     );
+    //     expect(wrapper).toMatchSnapshot();
+    // });
+
+    // test('should match snapshot for channel_invite_modal with userStatuses', () => {
+    //     const wrapper = shallow(
+    //         <ChannelInviteModal
+    //             {...baseProps}
+    //             profilesNotInCurrentChannel={users}
+    //             userStatuses={userStatuses}
+    //         />,
+    //     );
+    //     const instance = wrapper.instance() as ChannelInviteModal;
+    //     expect(instance.renderOption(users[0], true, jest.fn(), jest.fn())).toMatchSnapshot();
+    // });
+
+    // test('should match state when onHide is called', () => {
+    //     const wrapper = shallow<ChannelInviteModal>(
+    //         <ChannelInviteModal {...baseProps}/>,
+    //     );
+
+    //     wrapper.setState({show: true});
+    //     wrapper.instance().onHide();
+    //     expect(wrapper.state('show')).toEqual(false);
+    // });
+
+    // test('should have called props.onHide when Modal.onExited is called', () => {
+    //     const props = {...baseProps};
+    //     const wrapper = shallow(
+    //         <ChannelInviteModal {...props}/>,
+    //     );
+
+    //     wrapper.find(Modal).props().onExited!(document.createElement('div'));
+    //     expect(props.onHide).toHaveBeenCalledTimes(1);
+    // });
+
+    // test('should fail to add users on handleSubmit', (done) => {
+    //     const wrapper = shallow<ChannelInviteModal>(
+    //         <ChannelInviteModal
+    //             {...baseProps}
+    //         />,
+    //     );
+
+    //     wrapper.setState({values: users, show: true});
+    //     wrapper.instance().handleSubmit();
+    //     expect(wrapper.state('saving')).toEqual(true);
+    //     expect(wrapper.instance().props.actions.addUsersToChannel).toHaveBeenCalledTimes(1);
+    //     process.nextTick(() => {
+    //         expect(wrapper.state('inviteError')).toEqual('Failed');
+    //         expect(wrapper.state('saving')).toEqual(false);
+    //         done();
+    //     });
+    // });
+
+    // test('should add users on handleSubmit', (done) => {
+    //     const props = {
+    //         ...baseProps,
+    //         actions: {
+    //             ...baseProps.actions,
+    //             addUsersToChannel: jest.fn().mockImplementation(() => {
+    //                 const data = true;
+    //                 return Promise.resolve({data});
+    //             }),
+    //         },
+    //     };
+
+    //     const wrapper = shallow<ChannelInviteModal>(
+    //         <ChannelInviteModal
+    //             {...props}
+    //         />,
+    //     );
+
+    //     wrapper.setState({values: users, show: true});
+    //     wrapper.instance().handleSubmit();
+    //     expect(wrapper.state('saving')).toEqual(true);
+    //     expect(wrapper.instance().props.actions.addUsersToChannel).toHaveBeenCalledTimes(1);
+    //     process.nextTick(() => {
+    //         expect(wrapper.state('inviteError')).toBeUndefined();
+    //         expect(wrapper.state('saving')).toEqual(false);
+    //         expect(wrapper.state('show')).toEqual(false);
+    //         done();
+    //     });
+    // });
+
+    // test('should call onAddCallback on handleSubmit with skipCommit', () => {
+    //     const onAddCallback = jest.fn();
+    //     const props = {
+    //         ...baseProps,
+    //         skipCommit: true,
+    //         onAddCallback,
+    //     };
+
+    //     const wrapper = shallow<ChannelInviteModal>(
+    //         <ChannelInviteModal
+    //             {...props}
+    //         />,
+    //     );
+
+    //     wrapper.setState({values: users, show: true});
+    //     wrapper.instance().handleSubmit();
+    //     expect(onAddCallback).toHaveBeenCalled();
+    //     expect(wrapper.instance().props.actions.addUsersToChannel).toHaveBeenCalledTimes(0);
+    // });
+
+    // test('should trim the search term', () => {
+    //     const wrapper = shallow<ChannelInviteModal>(
+    //         <ChannelInviteModal {...baseProps}/>,
+    //     );
+
+    //     wrapper.instance().search(' something ');
+    //     expect(wrapper.state('term')).toEqual('something');
+    // });
+});
---
+++ components/share_message_modal/share_message_modal.tsx
@@ -0,0 +1,350 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {Modal} from 'react-bootstrap';
+import {FormattedMessage} from 'react-intl';
+
+import {Client4} from 'mattermost-redux/client';
+import {Dictionary, RelationOneToOne} from 'mattermost-redux/types/utilities';
+import {ActionFunc} from 'mattermost-redux/types/actions';
+import {Channel} from 'mattermost-redux/types/channels';
+import {UserProfile} from 'mattermost-redux/types/users';
+
+import {filterProfilesStartingWithTerm} from 'mattermost-redux/utils/user_utils';
+
+import {displayEntireNameForUser, localizeMessage, isGuest} from 'utils/utils.jsx';
+import ProfilePicture from 'components/profile_picture';
+import MultiSelect, {Value} from 'components/multiselect/multiselect';
+import AddIcon from 'components/widgets/icons/fa_add_icon';
+import GuestBadge from 'components/widgets/badges/guest_badge';
+import BotBadge from 'components/widgets/badges/bot_badge';
+
+import Constants from 'utils/constants';
+
+const USERS_PER_PAGE = 50;
+const MAX_SELECTABLE_VALUES = 20;
+
+type UserProfileValue = Value & UserProfile;
+
+export type Props = {
+    profilesNotInCurrentChannel: UserProfileValue[];
+    profilesNotInCurrentTeam: UserProfileValue[];
+    userStatuses: RelationOneToOne<UserProfile, string>;
+    onHide: () => void;
+    channel: Channel;
+
+    // skipCommit = true used with onAddCallback will result in users not being committed immediately
+    skipCommit?: boolean;
+
+    // onAddCallback takes an array of UserProfiles and should set usersToAdd in state of parent component
+    onAddCallback?: (userProfiles?: UserProfileValue[]) => void;
+
+    // Dictionaries of userid mapped users to exclude or include from this list
+    excludeUsers?: Dictionary<UserProfileValue>;
+    includeUsers?: Dictionary<UserProfileValue>;
+
+    actions: {
+        addUsersToChannel: any;
+        getProfilesNotInChannel: any;
+        getTeamStats: (teamId: string) => ActionFunc;
+        loadStatusesForProfilesList: (users: UserProfile[]) => Promise<{data: boolean}>;
+        searchProfiles: (term: string, options: any) => ActionFunc;
+    };
+}
+
+type State = {
+    values: UserProfileValue[];
+    term: string;
+    show: boolean;
+    saving: boolean;
+    loadingUsers: boolean;
+    inviteError?: string;
+}
+
+export default class ShareMessageModal extends React.PureComponent<Props, State> {
+    private searchTimeoutId = 0;
+    private selectedItemRef = React.createRef<HTMLDivElement>();
+
+    public static defaultProps = {
+        includeUsers: {},
+        excludeUsers: {},
+        skipCommit: false,
+    };
+
+    constructor(props: Props) {
+        super(props);
+
+        this.state = {
+            values: [],
+            term: '',
+            show: true,
+            saving: false,
+            loadingUsers: true,
+        } as State;
+    }
+
+    private addValue = (value: UserProfileValue): void => {
+        const values: UserProfileValue[] = Object.assign([], this.state.values);
+        if (values.indexOf(value) === -1) {
+            values.push(value);
+        }
+
+        this.setState({values});
+    };
+
+    public componentDidMount(): void {
+        this.props.actions.getProfilesNotInChannel(this.props.channel.team_id, this.props.channel.id, this.props.channel.group_constrained, 0).then(() => {
+            this.setUsersLoadingState(false);
+        });
+        this.props.actions.getTeamStats(this.props.channel.team_id);
+        this.props.actions.loadStatusesForProfilesList(this.props.profilesNotInCurrentChannel);
+    }
+
+    public onHide = (): void => {
+        this.setState({show: false});
+        this.props.actions.loadStatusesForProfilesList(this.props.profilesNotInCurrentChannel);
+    };
+
+    public handleInviteError = (err: any): void => {
+        if (err) {
+            this.setState({
+                saving: false,
+                inviteError: err.message,
+            });
+        }
+    };
+
+    private handleDelete = (values: UserProfileValue[]): void => {
+        this.setState({values});
+    };
+
+    private setUsersLoadingState = (loadingState: boolean): void => {
+        this.setState({
+            loadingUsers: loadingState,
+        });
+    };
+
+    private handlePageChange = (page: number, prevPage: number): void => {
+        if (page > prevPage) {
+            this.setUsersLoadingState(true);
+            this.props.actions.getProfilesNotInChannel(
+                this.props.channel.team_id,
+                this.props.channel.id,
+                this.props.channel.group_constrained,
+                page + 1, USERS_PER_PAGE).then(() => this.setUsersLoadingState(false));
+        }
+    };
+
+    public handleSubmit = (): void => {
+        const {actions, channel} = this.props;
+
+        const userIds = this.state.values.map((v) => v.id);
+        if (userIds.length === 0) {
+            return;
+        }
+
+        if (this.props.skipCommit && this.props.onAddCallback) {
+            this.props.onAddCallback(this.state.values);
+            this.setState({
+                saving: false,
+                inviteError: undefined,
+            });
+            this.onHide();
+            return;
+        }
+
+        this.setState({saving: true});
+
+        actions.addUsersToChannel(channel.id, userIds).then((result: any) => {
+            if (result.error) {
+                this.handleInviteError(result.error);
+            } else {
+                this.setState({
+                    saving: false,
+                    inviteError: undefined,
+                });
+                this.onHide();
+            }
+        });
+    };
+
+    public search = (searchTerm: string): void => {
+        const term = searchTerm.trim();
+        clearTimeout(this.searchTimeoutId);
+        this.setState({
+            term,
+        });
+
+        if (term) {
+            this.setUsersLoadingState(true);
+            this.searchTimeoutId = window.setTimeout(
+                async () => {
+                    const options = {
+                        team_id: this.props.channel.team_id,
+                        not_in_channel_id: this.props.channel.id,
+                        group_constrained: this.props.channel.group_constrained,
+                    };
+                    await this.props.actions.searchProfiles(term, options);
+                    this.setUsersLoadingState(false);
+                },
+                Constants.SEARCH_TIMEOUT_MILLISECONDS,
+            );
+        } else {
+            return;
+        }
+
+        this.searchTimeoutId = window.setTimeout(
+            async () => {
+                if (!term) {
+                    return;
+                }
+
+                const options = {
+                    team_id: this.props.channel.team_id,
+                    not_in_channel_id: this.props.channel.id,
+                    group_constrained: this.props.channel.group_constrained,
+                };
+                await this.props.actions.searchProfiles(term, options);
+                this.setUsersLoadingState(false);
+            },
+            Constants.SEARCH_TIMEOUT_MILLISECONDS,
+        );
+    };
+
+    private renderAriaLabel = (option: UserProfileValue): string => {
+        if (!option) {
+            return '';
+        }
+        return option.username;
+    }
+
+    renderOption = (option: UserProfileValue, isSelected: boolean, onAdd: (user: UserProfileValue) => void, onMouseMove: (user: UserProfileValue) => void) => {
+        let rowSelected = '';
+        if (isSelected) {
+            rowSelected = 'more-modal__row--selected';
+        }
+
+        return (
+            <div
+                key={option.id}
+                ref={isSelected ? this.selectedItemRef : option.id}
+                className={'more-modal__row clickable ' + rowSelected}
+                onClick={() => onAdd(option)}
+                onMouseMove={() => onMouseMove(option)}
+            >
+                <ProfilePicture
+                    src={Client4.getProfilePictureUrl(option.id, option.last_picture_update)}
+                    status={this.props.userStatuses[option.id]}
+                    size='md'
+                    username={option.username}
+                />
+                <div className='more-modal__details'>
+                    <div className='more-modal__name'>
+                        {displayEntireNameForUser(option)}
+                        <BotBadge
+                            show={Boolean(option.is_bot)}
+                            className='badge-popoverlist'
+                        />
+                        <GuestBadge
+                            show={isGuest(option)}
+                            className='popoverlist'
+                        />
+                    </div>
+                </div>
+                <div className='more-modal__actions'>
+                    <div className='more-modal__actions--round'>
+                        <AddIcon/>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
+    public render = (): JSX.Element => {
+        let inviteError = null;
+        if (this.state.inviteError) {
+            inviteError = (<label className='has-error control-label'>{this.state.inviteError}</label>);
+        }
+
+        const header = (
+            <h1>
+                <FormattedMessage
+                    id='channel_invite.addNewMembers'
+                    defaultMessage='Add people to {channel}'
+                    values={{
+                        channel: this.props.channel.display_name,
+                    }}
+                />
+            </h1>
+        );
+
+        const buttonSubmitText = localizeMessage('multiselect.add', 'Add');
+        const buttonSubmitLoadingText = localizeMessage('multiselect.adding', 'Adding...');
+
+        let users = filterProfilesStartingWithTerm(this.props.profilesNotInCurrentChannel, this.state.term).filter((user) => {
+            return user.delete_at === 0 &&
+                !this.props.profilesNotInCurrentTeam.includes(user as UserProfileValue) &&
+                (this.props.excludeUsers !== undefined && !this.props.excludeUsers[user.id]);
+        }).map((user) => user as UserProfileValue);
+
+        if (this.props.includeUsers) {
+            const includeUsers = Object.values(this.props.includeUsers);
+            users = [...users, ...includeUsers];
+        }
+
+        const content = (
+            <MultiSelect
+                key='addUsersToChannelKey'
+                options={users}
+                optionRenderer={this.renderOption}
+                selectedItemRef={this.selectedItemRef}
+                values={this.state.values}
+                ariaLabelRenderer={this.renderAriaLabel}
+                saveButtonPosition={'bottom'}
+                perPage={USERS_PER_PAGE}
+                handlePageChange={this.handlePageChange}
+                handleInput={this.search}
+                handleDelete={this.handleDelete}
+                handleAdd={this.addValue}
+                handleSubmit={this.handleSubmit}
+                maxValues={MAX_SELECTABLE_VALUES}
+                buttonSubmitText={buttonSubmitText}
+                buttonSubmitLoadingText={buttonSubmitLoadingText}
+                saving={this.state.saving}
+                loading={this.state.loadingUsers}
+                placeholderText={localizeMessage('multiselect.placeholder', 'Search for people')}
+                valueWithImage={true}
+            />
+        );
+
+        return (
+            <Modal
+                id='addUsersToChannelModal'
+                dialogClassName='a11y__modal channel-invite'
+                show={this.state.show}
+                onHide={this.onHide}
+                onExited={this.props.onHide}
+                role='dialog'
+                aria-labelledby='channelInviteModalLabel'
+            >
+                <Modal.Header
+                    id='channelInviteModalLabel'
+                    closeButton={true}
+                />
+                <Modal.Body
+                    role='application'
+                    className='overflow--visible'
+                >
+                    <div className='channel-invite__header'>
+                        {header}
+                    </div>
+                    {inviteError}
+                    <div className='channel-invite__content'>
+                        {content}
+                    </div>
+                </Modal.Body>
+            </Modal>
+        );
+    }
+}
