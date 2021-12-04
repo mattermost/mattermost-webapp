8a7156e9b4f8c2f77b5bb6d1884c357f81a288c2
Aleksey Poflankov <87149297+poflankov@users.noreply.github.com>
2021-12-04 18:30:21 +0300

    MM-T2075 Profile Picture: Cancel out of adding/changing profile picture

--- e2e/cypress/integration/account_settings/profile/profile_picture_change_spec.js
+++ e2e/cypress/integration/account_settings/profile/profile_picture_change_spec.js
@@ -4,22 +4,18 @@
 import * as TIMEOUTS from '../../../fixtures/timeouts';
 
 describe('Account Settings', () => {
-    before(() => {
+    beforeEach(() => {
         cy.apiAdminLogin().apiInitSetup({loginAfter: true}).its('user').as('user');
     });
 
     it('MM-T2079 Can remove profile pic then choose different pic without saving in between', function() {
         // # Add a profile picture (aka "old profile picture")
-        cy.apiUploadFile('image', 'mattermost-icon.png', {
-            url: `/api/v4/users/${this.user.id}/image`,
-            method: 'POST',
-            successStatus: 200,
-        });
+        setInitialPicture(this.user);
 
         cy.visit('/');
 
         // # Save the id of the old picture
-        cy.uiGetProfileHeader().find('.Avatar').invoke('attr', 'src').then(getPictureId).as('idOld');
+        getProfilePictureId().as('idOld');
 
         // # Open Profile > Profile Settings > Profile Picture > Edit
         cy.uiOpenProfileModal().findByRole('button', {name: /picture edit/i}).click();
@@ -33,17 +29,57 @@ describe('Account Settings', () => {
         cy.findByRole('button', {name: /close/i}).click();
 
         // * Verify that new profile image exists and isn't equal to the old one
-        cy.uiGetProfileHeader().
-            find('.Avatar').
-            invoke('attr', 'src').
-            then(getPictureId).
-            then((idNew) => expect(idNew).to.exist.and.not.to.be.equal(this.idOld));
+        getProfilePictureId().then((idNew) => expect(idNew).to.exist.and.not.to.be.equal(this.idOld));
+    });
+
+    it('MM-T2075_1 Profile Picture: Cancel out of adding profile picture', () => {
+        verifyProfilePictureDoesNotUpdateAfterCancel();
+    });
+
+    it('MM-T2075_2 Profile Picture: Cancel out of changing profile picture ', function() {
+        // # Add a profile picture (aka "old profile picture")
+        setInitialPicture(this.user);
+
+        verifyProfilePictureDoesNotUpdateAfterCancel();
     });
 });
 
-function getPictureId(urlString) {
-    const url = new URL(urlString);
-    const params = new URLSearchParams(url.search);
-    return params.get('_');
+function getProfilePictureId() {
+    return cy.uiGetProfileHeader().find('.Avatar').invoke('attr', 'src').then((urlString) => {
+        const url = new URL(urlString);
+        const params = new URLSearchParams(url.search);
+        return params.get('_');
+    });
 }
 
+function setInitialPicture(user) {
+    cy.apiUploadFile('image', 'mattermost-icon.png', {
+        url: `/api/v4/users/${user.id}/image`,
+        method: 'POST',
+        successStatus: 200,
+    });
+}
+
+function verifyProfilePictureDoesNotUpdateAfterCancel() {
+    let idOld;
+
+    cy.visit('/');
+
+    // # Save the id of the old picture
+    getProfilePictureId().then((id) => {
+        idOld = id;
+    });
+
+    // # Open Profile > Profile Settings > Profile Picture > Edit
+    cy.uiOpenProfileModal().findByRole('button', {name: /picture edit/i}).click();
+
+    // # Select a new profile picture
+    cy.findByTestId('uploadPicture').attachFile('png-image-file.png');
+
+    cy.uiCancel().wait(TIMEOUTS.HALF_SEC);
+
+    cy.uiClose();
+
+    // * Verify that 'new' profile image exists and equal to the old one
+    getProfilePictureId().then((idNew) => expect(idNew).to.equal(idOld));
+}
