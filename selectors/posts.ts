ef0f74c0a3527ca31861e884d4400724b8a3fa3d
M-ZubairAhmed <m-zubairahmed@protonmail.com>
2021-12-06 08:28:23 +0530

    typ fix

--- selectors/posts.ts
+++ selectors/posts.ts
@@ -1,9 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {createSelector} from 'reselect';
-
-import {getPost} from 'mattermost-redux/selectors/entities/posts';
 import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
 
 import {getGlobalItem} from 'selectors/storage';
