f4b5985f602a07d5b905f58452493fd483e503ae
koox00 <3829551+koox00@users.noreply.github.com>
2021-12-08 14:16:42 +0200

    Adds case on leave_channel_spec when CRT is ON

--- e2e/cypress/integration/channel/leave_channel_spec.js
+++ e2e/cypress/integration/channel/leave_channel_spec.js
@@ -15,11 +15,13 @@ import * as TIMEOUTS from '../../fixtures/timeouts';
 describe('Leave channel', () => {
     let testTeam;
     let testUser;
+    let testChannel;
 
     before(() => {
         cy.apiUpdateConfig({
-            TeamSettings: {
-                ExperimentalViewArchivedChannels: true,
+            ServiceSettings: {
+                ThreadAutoFollow: true,
+                CollapsedThreads: 'default_off',
             },
         });
 
@@ -30,9 +32,13 @@ describe('Leave channel', () => {
 
             cy.apiLogin(testUser);
             cy.apiCreateChannel(testTeam.id, 'channel', 'channel').then(({channel}) => {
-                cy.visit(`/${testTeam.name}/channels/${channel.name}`);
+                testChannel = channel;
             });
         });
+    });
+
+    beforeEach(() => {
+        cy.visit(`/${testTeam.name}/channels/${testChannel.name}`);
     });
 
     it('Leave a channel while RHS is open', () => {
@@ -42,7 +48,46 @@ describe('Leave channel', () => {
         // # Post a message in the channel
         cy.postMessage('Test leave channel while RHS open');
         cy.getLastPostId().then((id) => {
+            // # Open RHS
             cy.clickPostCommentIcon(id);
+
+            // # Post a reply message
+            cy.postMessageAs({sender: testUser, message: 'another reply!', channelId: testChannel.id, rootId: id});
+
+            // * RHS should be visible
+            cy.get('#rhsContainer').should('be.visible');
+
+            // * RHS text box should be visible
+            cy.get('#reply_textbox').should('be.visible');
+
+            // # Archive the channel
+            cy.uiLeaveChannel();
+            cy.wait(TIMEOUTS.TWO_SEC); // eslint-disable-line cypress/no-unnecessary-waiting
+
+            // * RHS should not be visible
+            cy.get('#rhsContainer').should('not.exist');
+
+            // * Assert that user is redirected to townsquare
+            cy.url().should('include', '/channels/town-square');
+            cy.get('#channelHeaderTitle').should('be.visible').and('contain', 'Town Square');
+        });
+    });
+
+    it('Leave a channel while RHS is open and CRT on', () => {
+        // * Post text box should be visible
+        cy.get('#post_textbox').should('be.visible');
+
+        // # Set CRT to on
+        cy.uiChangeCRTDisplaySetting('ON');
+
+        // # Post a message in the channel
+        cy.postMessage('Test leave channel while RHS open');
+        cy.getLastPostId().then((id) => {
+            // # Open RHS
+            cy.clickPostCommentIcon(id);
+
+            // # Post a reply message
+            cy.postMessageAs({sender: testUser, message: 'another reply!', channelId: testChannel.id, rootId: id});
 
             // * RHS should be visible
             cy.get('#rhsContainer').should('be.visible');
