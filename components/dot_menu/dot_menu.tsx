286fb6dad8f7bb848d817ddbee30af915a388469
Harrison Healey <harrisonmhealey@gmail.com>
2021-12-15 14:00:17 -0500

    MM-40114 Migrate post-related components to use getIsMobileView (#9508)
    * MM-40114 Switch DotMenu to getIsMobileView
    
    * MM-40114 Switch PostList to getIsMobileView
    
    * MM-41004 Remove unneeded isMobile prop from FloatingTimestamp
    
    * MM-40114 Switch PostTime to use getIsMobileView
    
    * MM-40114 Switch ProfilePopover to use getIsMobileView

--- components/dot_menu/dot_menu.tsx
+++ components/dot_menu/dot_menu.tsx
@@ -50,6 +50,7 @@ type Props = {
     teamUrl?: string; // TechDebt: Made non-mandatory while converting to typescript
     appBindings: AppBinding[] | null;
     appsEnabled: boolean;
+    isMobileView: boolean;
 
     /**
      * Components for overriding provided by plugins
@@ -399,8 +400,8 @@ export class DotMenuClass extends React.PureComponent<Props, State> {
     }
 
     render() {
+        const isMobile = this.props.isMobileView;
         const isSystemMessage = PostUtils.isSystemMessage(this.props.post);
-        const isMobile = Utils.isMobile();
 
         const pluginItems = this.props.pluginMenuItems?.
             filter((item) => {
