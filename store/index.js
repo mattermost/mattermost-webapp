86a71cd88dbb99af67f1b8056a48e978cb33c845
Harrison Healey <harrisonmhealey@gmail.com>
2021-12-16 10:51:47 -0500

    Remove redux-persist references from mattermost-redux

--- store/index.js
+++ store/index.js
@@ -14,7 +14,6 @@ import configureServiceStore from 'mattermost-redux/store';
 import {storageRehydrate, rehydrateDrafts} from 'actions/storage';
 import {clearUserCookie} from 'actions/views/cookie';
 import appReducer from 'reducers';
-import {ActionTypes} from 'utils/constants';
 import {getBasePath} from 'selectors/general';
 
 function getAppReducer() {
@@ -44,140 +43,137 @@ const whitelist = {
 window.Observable = Observable;
 
 export default function configureStore(initialState) {
-    const offlineOptions = {
-        persist: (store, options) => {
-            const localforage = extendPrototype(localForage);
-            const storage = localforage;
-            const KEY_PREFIX = 'reduxPersist:';
+    const store = configureServiceStore(initialState, appReducer, getAppReducer);
 
-            localforage.ready().then(() => {
-                const persistor = persistStore(store, {storage, keyPrefix: KEY_PREFIX, ...options}, () => {
-                    store.dispatch({
-                        type: General.STORE_REHYDRATION_COMPLETE,
-                        complete: true,
+    const localforage = extendPrototype(localForage);
+
+    const keyPrefix = 'reduxPersist:';
+    const persistOptions = {
+        autoRehydrate: {
+            log: false,
+        },
+        keyPrefix,
+        storage: localforage,
+        whitelist,
+        debounce: 30,
+        _stateIterator: (collection, callback) => {
+            return Object.keys(collection).forEach((key) => {
+                if (key === 'storage') {
+                    Object.keys(collection.storage.storage).forEach((storageKey) => {
+                        callback(collection.storage.storage[storageKey], 'storage:' + storageKey);
                     });
-                });
-
-                localforage.configObservables({
-                    crossTabNotification: true,
-                });
-
-                const observable = localforage.newObservable({
-                    crossTabNotification: true,
-                    changeDetection: true,
-                });
-
-                const restoredState = {};
-                localforage.iterate((value, key) => {
-                    if (key && key.indexOf(KEY_PREFIX + 'storage:') === 0) {
-                        const keyspace = key.substr((KEY_PREFIX + 'storage:').length);
-                        restoredState[keyspace] = value;
-                    }
-                }).then(() => {
-                    storageRehydrate(restoredState, persistor)(store.dispatch, store.getState);
-                    store.dispatch(rehydrateDrafts());
-                });
-
-                observable.subscribe({
-                    next: (args) => {
-                        if (args.key && args.key.indexOf(KEY_PREFIX + 'storage:') === 0 && args.oldValue === null) {
-                            const keyspace = args.key.substr((KEY_PREFIX + 'storage:').length);
-
-                            var statePartial = {};
-                            statePartial[keyspace] = args.newValue;
-
-                            storageRehydrate(statePartial, persistor)(store.dispatch, store.getState);
-                        }
-                    },
-                });
-
-                let purging = false;
-
-                // check to see if the logout request was successful
-                store.subscribe(() => {
-                    const state = store.getState();
-                    const basePath = getBasePath(state);
-
-                    if (state.requests.users.logout.status === RequestStatus.SUCCESS && !purging) {
-                        purging = true;
-
-                        persistor.purge().then(() => {
-                            clearUserCookie();
-
-                            // Preserve any query string parameters on logout, including parameters
-                            // used by the application such as extra and redirect_to.
-                            window.location.href = `${basePath}${window.location.search}`;
-
-                            setTimeout(() => {
-                                purging = false;
-                            }, 500);
-                        });
-                    }
-                });
-            }).catch((error) => {
-                store.dispatch({
-                    type: ActionTypes.STORE_REHYDRATION_FAILED,
-                    error,
-                });
+                } else {
+                    callback(collection[key], key);
+                }
             });
         },
-        persistOptions: {
-            autoRehydrate: {
-                log: false,
-            },
-            whitelist,
-            debounce: 30,
-            _stateIterator: (collection, callback) => {
-                return Object.keys(collection).forEach((key) => {
-                    if (key === 'storage') {
-                        Object.keys(collection.storage.storage).forEach((storageKey) => {
-                            callback(collection.storage.storage[storageKey], 'storage:' + storageKey);
-                        });
-                    } else {
-                        callback(collection[key], key);
-                    }
-                });
-            },
-            _stateGetter: (state, key) => {
+        _stateGetter: (state, key) => {
+            if (key.indexOf('storage:') === 0) {
+                return state.storage?.storage?.[key.substring(8)];
+            }
+            return state[key];
+        },
+        _stateSetter: (state, key, value) => {
+            // eslint-disable-next-line no-process-env
+            if (process.env.NODE_ENV === 'production') {
                 if (key.indexOf('storage:') === 0) {
-                    return state.storage?.storage?.[key.substr(8)];
-                }
-                return state[key];
-            },
-            _stateSetter: (state, key, value) => {
-                // eslint-disable-next-line no-process-env
-                if (process.env.NODE_ENV === 'production') {
-                    if (key.indexOf('storage:') === 0) {
-                        state.storage = state.storage || {storage: {}};
-                        state.storage.storage[key.substr(8)] = value;
-                    } else {
-                        state[key] = value;
-                    }
-
-                    return state;
+                    state.storage = state.storage || {storage: {}};
+                    state.storage.storage[key.substr(8)] = value;
+                } else {
+                    state[key] = value;
                 }
 
-                // In non-production environments, the store is immutable.
-                if (key.indexOf('storage:') === 0) {
-                    return {
-                        ...state,
-                        storage: {
-                            ...state.storage,
-                            storage: {
-                                ...state.storage?.storage,
-                                [key.substr(8)]: value,
-                            },
-                        },
-                    };
-                }
+                return state;
+            }
 
+            // In non-production environments, the store is immutable.
+            if (key.indexOf('storage:') === 0) {
                 return {
                     ...state,
-                    key: value,
+                    storage: {
+                        ...state.storage,
+                        storage: {
+                            ...state.storage?.storage,
+                            [key.substr(8)]: value,
+                        },
+                    },
                 };
-            },
+            }
+
+            return {
+                ...state,
+                key: value,
+            };
         },
     };
 
-    return configureServiceStore(initialState, appReducer, offlineOptions, getAppReducer);
+    localforage.ready().then(() => {
+        const persistor = persistStore(store, persistOptions, () => {
+            store.dispatch({
+                type: General.STORE_REHYDRATION_COMPLETE,
+                complete: true,
+            });
+        });
+
+        localforage.configObservables({
+            crossTabNotification: true,
+        });
+
+        const observable = localforage.newObservable({
+            crossTabNotification: true,
+            changeDetection: true,
+        });
+
+        // Initial rehydration from storage
+        const restoredState = {};
+        localforage.iterate((value, key) => {
+            if (key && key.indexOf(keyPrefix + 'storage:') === 0) {
+                const keyspace = key.substring((keyPrefix + 'storage:').length);
+                restoredState[keyspace] = value;
+            }
+        }).then(() => {
+            storageRehydrate(restoredState, persistor)(store.dispatch, store.getState);
+            store.dispatch(rehydrateDrafts());
+        });
+
+        // Rehydrating when another tab changes
+        observable.subscribe({
+            next: (args) => {
+                if (args.key && args.key.indexOf(keyPrefix + 'storage:') === 0 && args.oldValue === null) {
+                    const keyspace = args.key.substring((keyPrefix + 'storage:').length);
+
+                    var statePartial = {};
+                    statePartial[keyspace] = args.newValue;
+
+                    storageRehydrate(statePartial, persistor)(store.dispatch, store.getState);
+                }
+            },
+        });
+
+        let purging = false;
+
+        // Clean up after a logout
+        store.subscribe(() => {
+            const state = store.getState();
+            const basePath = getBasePath(state);
+
+            if (state.requests.users.logout.status === RequestStatus.SUCCESS && !purging) {
+                purging = true;
+
+                persistor.purge().then(() => {
+                    clearUserCookie();
+
+                    // Preserve any query string parameters on logout, including parameters
+                    // used by the application such as extra and redirect_to.
+                    window.location.href = `${basePath}${window.location.search}`;
+
+                    setTimeout(() => {
+                        purging = false;
+                    }, 500);
+                });
+            }
+        });
+    });
+
+    return store;
 }
