642b48864e80d870fe8031f33426f6cce6983889
Mattermost Build <build@mattermost.com>
2021-12-22 00:01:59 +0200

    Team sidebar in products (#9262) (#9557)
    Automatic Merge

--- components/root/root.jsx
+++ components/root/root.jsx
@@ -7,6 +7,8 @@ import React from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom';
 import throttle from 'lodash/throttle';
 
+import classNames from 'classnames';
+
 import {rudderAnalytics, RudderTelemetryHandler} from 'mattermost-redux/client/rudder';
 import {Client4} from 'mattermost-redux/client';
 import {setUrl} from 'mattermost-redux/actions/general';
@@ -60,6 +62,8 @@ import {getSiteURL} from 'utils/url';
 import {enableDevModeFeatures, isDevMode} from 'utils/utils';
 
 import A11yController from 'utils/a11y_controller';
+
+import TeamSidebar from 'components/team_sidebar';
 
 import {applyLuxonDefaults} from './effects';
 
@@ -388,6 +392,7 @@ export default class Root extends React.PureComponent {
                     <CompassThemeProvider theme={this.props.theme}>
                         <ModalController/>
                         <GlobalHeader/>
+                        <TeamSidebar/>
                         <Switch>
                             {this.props.products?.map((product) => (
                                 <Route
@@ -395,12 +400,14 @@ export default class Root extends React.PureComponent {
                                     path={product.baseURL}
                                     render={(props) => (
                                         <LoggedIn {...props}>
-                                            <Pluggable
-                                                pluggableName={'Product'}
-                                                subComponentName={'mainComponent'}
-                                                pluggableId={product.id}
-                                                webSocketClient={webSocketClient}
-                                            />
+                                            <div className={classNames(['product-wrapper', {wide: !product.showTeamSidebar}])}>
+                                                <Pluggable
+                                                    pluggableName={'Product'}
+                                                    subComponentName={'mainComponent'}
+                                                    pluggableId={product.id}
+                                                    webSocketClient={webSocketClient}
+                                                />
+                                            </div>
                                         </LoggedIn>
                                     )}
                                 />
