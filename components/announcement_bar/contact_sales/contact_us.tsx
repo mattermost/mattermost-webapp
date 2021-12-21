444c2449eec31fc1121578c6abc31fd854721461
Allan Guwatudde <guwats10@gmail.com>
2021-12-21 17:44:24 +0300

    [MM-40407] - Do not show the Renew Now if the license id does not exist in the portal (#9544)
    * [MM-40407] - Do not show the Renew Now if the license id does not exist in the portal
    
    * fix translation strings
    
    * add unit test
    
    * improvements

--- components/announcement_bar/contact_sales/contact_us.tsx
+++ components/announcement_bar/contact_sales/contact_us.tsx
@@ -18,7 +18,7 @@ const ContactUsButton: React.FC<Props> = (props: Props) => {
     const handleContactUsLinkClick = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
         trackEvent('admin', props.eventID || 'in_trial_contact_sales');
-        window.open('https://mattermost.com/contact-us/', '_blank');
+        window.open('https://mattermost.com/contact-sales/', '_blank');
     };
 
     return (
