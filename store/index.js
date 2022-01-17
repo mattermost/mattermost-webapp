9d004add0bee4b013897e455b3442f18175049e6
Harrison Healey <harrisonmhealey@gmail.com>
2022-01-17 17:17:03 -0500

    Upgrade to latest version of redux-persist and break state.storage persistance

--- store/index.js
+++ store/index.js
@@ -1,17 +1,15 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-/* eslint max-nested-callbacks: ["error", 3] */
-
 import Observable from 'zen-observable';
 import localForage from 'localforage';
 import {extendPrototype} from 'localforage-observable';
-import {persistStore} from 'redux-persist';
+
+import {persistStore, REHYDRATE} from 'redux-persist';
 
 import {General, RequestStatus} from 'mattermost-redux/constants';
 import configureServiceStore from 'mattermost-redux/store';
 
-import {storageRehydrate} from 'actions/storage';
 import {clearUserCookie} from 'actions/views/cookie';
 import appReducers from 'reducers';
 import {getBasePath} from 'selectors/general';
@@ -20,79 +18,19 @@ function getAppReducers() {
     return require('../reducers'); // eslint-disable-line global-require
 }
 
-// This is a hack to get the whitelist to work with our storage keys
-// We will implement it properly when we eventually upgrade redux-persist
-const whitelist = {
-    keys: [], // add normal whitelist keys here
-    prefixes: ['storage'], // add any whitelist prefixes here
-    indexOf: function indexOf(key) {
-        if (this.keys.indexOf(key) !== -1) {
-            return 0;
-        }
-
-        for (let i = 0; i < this.prefixes.length; i++) {
-            if (key.startsWith(this.prefixes[i])) {
-                return 0;
-            }
-        }
-
-        return -1;
-    },
-};
-
 window.Observable = Observable;
 
 export default function configureStore(preloadedState) {
-    const store = configureServiceStore({preloadedState, appReducers, getAppReducers});
+    const store = configureServiceStore({
+        appReducers,
+        getAppReducers,
+        preloadedState,
+    });
 
     const localforage = extendPrototype(localForage);
 
-    const keyPrefix = 'reduxPersist:';
-    const persistOptions = {
-        keyPrefix,
-        storage: localforage,
-        whitelist,
-        debounce: 30,
-        _stateIterator: (collection, callback) => {
-            return Object.keys(collection).forEach((key) => {
-                if (key === 'storage') {
-                    Object.keys(collection.storage.storage).forEach((storageKey) => {
-                        callback(collection.storage.storage[storageKey], 'storage:' + storageKey);
-                    });
-                } else {
-                    callback(collection[key], key);
-                }
-            });
-        },
-        _stateGetter: (state, key) => {
-            if (key.indexOf('storage:') === 0) {
-                return state.storage?.storage?.[key.substring(8)];
-            }
-            return state[key];
-        },
-        _stateSetter: (state, key, value) => {
-            if (key.indexOf('storage:') === 0) {
-                return {
-                    ...state,
-                    storage: {
-                        ...state.storage,
-                        storage: {
-                            ...state.storage?.storage,
-                            [key.substr(8)]: value,
-                        },
-                    },
-                };
-            }
-
-            return {
-                ...state,
-                key: value,
-            };
-        },
-    };
-
     localforage.ready().then(() => {
-        const persistor = persistStore(store, persistOptions, () => {
+        const persistor = persistStore(store, {}, () => {
             store.dispatch({
                 type: General.STORE_REHYDRATION_COMPLETE,
                 complete: true,
@@ -111,14 +49,37 @@ export default function configureStore(preloadedState) {
         // Rehydrating when another tab changes
         observable.subscribe({
             next: (args) => {
-                if (args.key && args.key.indexOf(keyPrefix + 'storage:') === 0 && args.oldValue === null) {
-                    const keyspace = args.key.substring((keyPrefix + 'storage:').length);
-
-                    var statePartial = {};
-                    statePartial[keyspace] = args.newValue;
-
-                    storageRehydrate(statePartial, persistor)(store.dispatch, store.getState);
+                if (!args.crossTabNotification) {
+                    // Ignore changes made by this tab
+                    return;
                 }
+
+                const keyPrefix = 'persist:';
+
+                if (!args.key.startsWith(keyPrefix)) {
+                    // Ignore changes that weren't made by redux-persist
+                    return;
+                }
+
+                const key = args.key.substring(keyPrefix.length);
+                const newValue = JSON.parse(args.newValue);
+
+                const payload = {};
+
+                for (const reducerKey of Object.keys(newValue)) {
+                    if (reducerKey === '_persist') {
+                        // Don't overwrite information used by redux-persist itself
+                        continue;
+                    }
+
+                    payload[reducerKey] = JSON.parse(newValue[reducerKey]);
+                }
+
+                store.dispatch({
+                    type: REHYDRATE,
+                    key,
+                    payload,
+                });
             },
         });
 
