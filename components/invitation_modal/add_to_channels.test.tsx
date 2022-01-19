3b3ac6d1a4b22ca7825262a6991e18a1b59861f5
Julien Tant <julien@craftyx.fr>
2022-01-19 15:33:18 -0700

    ensure the "Invite to channel" popin works when the user is not in a channel

--- components/invitation_modal/add_to_channels.test.tsx
+++ components/invitation_modal/add_to_channels.test.tsx
@@ -2,12 +2,14 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
+import {IntlProvider} from 'react-intl';
+import {render, screen} from '@testing-library/react';
 
 import {mountWithIntl} from 'tests/helpers/intl-test-helper';
-
 import deepFreeze from 'mattermost-redux/utils/deep_freeze';
-
+import {Channel} from 'mattermost-redux/types/channels';
 import CloseCircleIcon from 'components/widgets/icons/close_circle_icon';
+import en from 'i18n/en.json';
 
 import AddToChannels, {Props} from './add_to_channels';
 
@@ -28,15 +30,45 @@ const defaultProps: Props = deepFreeze({
     currentChannel: {
         display_name: '',
     },
-    townSquareDisplayName: '',
+    townSquareDisplayName: 'Town Square',
 });
 
 let props = defaultProps;
+
+const wrapIntl = (component: JSX.Element) => (
+    <IntlProvider
+        locale={'en'}
+        messages={en}
+    >
+        {component}
+    </IntlProvider>
+);
 
 describe('AddToChannels', () => {
     beforeEach(() => {
         props = defaultProps;
     });
+
+    describe('placeholder selection', () => {
+        it('should use townSqureDisplayName when not in a channel', () => {
+            props = {...props, currentChannel: undefined};
+            render(wrapIntl(<AddToChannels {...props}/>));
+            expect(screen.getByText(props.townSquareDisplayName, {exact: false})).toBeInTheDocument();
+        });
+
+        it('should use townSqureDisplayName when not in a public or private channel', () => {
+            props = {...props, currentChannel: {type: 'D', display_name: ''} as Channel};
+            render(wrapIntl(<AddToChannels {...props}/>));
+            expect(screen.getByText(props.townSquareDisplayName, {exact: false})).toBeInTheDocument();
+        });
+
+        it('should use the currentChannel display_name when in a channel', () => {
+            props = {...props, currentChannel: {type: 'O', display_name: 'My Awesome Channel'} as Channel};
+            render(wrapIntl(<AddToChannels {...props}/>));
+            expect(screen.getByText('My Awesome Channel', {exact: false})).toBeInTheDocument();
+        });
+    });
+
     describe('custom message', () => {
         it('UI to toggle custom message opens it when closed', () => {
             const wrapper = mountWithIntl(<AddToChannels {...props}/>);
