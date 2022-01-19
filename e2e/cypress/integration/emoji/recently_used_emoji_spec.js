0042397c83fef4ccfa6896dd0cb69b11822e9ce1
Md_ZubairAhmed <m-zubairahmed@protonmail.com>
2022-01-19 16:59:33 +0530

    test MM-T4438

--- e2e/cypress/integration/emoji/recently_used_emoji_spec.js
+++ e2e/cypress/integration/emoji/recently_used_emoji_spec.js
@@ -174,4 +174,49 @@ describe('Recent Emoji', () => {
         // * Verify most recent one is the system emoji in emoji picker and not the custom emoji
         cy.findAllByTestId('emojiItem').eq(0).find('img').should('have.attr', 'aria-label', 'lemon emoji');
     });
+
+    it('MM-T4438 Changing the skin of emojis should apply the same skin to emojis in recent section', () => {
+        // # Post a sample message
+        cy.postMessage(MESSAGES.TINY);
+
+        // # Post reaction to post
+        cy.clickPostReactionIcon();
+
+        cy.get('#emojiPicker').should('be.visible').within(() => {
+            // # Open skin picker
+            cy.findByAltText('emoji skin tone picker').should('exist').parent().click().wait(TIMEOUTS.ONE_SEC);
+
+            // # Select default yellow skin
+            cy.findByTestId('skin-pick-default').should('exist').parent().click();
+
+            // # Search for a system emoji with skin
+            cy.findByPlaceholderText('Search emojis').should('exist').type('thumbsup').wait(TIMEOUTS.HALF_SEC);
+
+            // # Select the emoji to add to post with default skin
+            cy.findByTestId('+1,thumbsup').parent().click();
+        });
+
+        // # Open emoji picker again to check if thumbsup emoji is present in recent section
+        cy.uiOpenEmojiPicker().wait(TIMEOUTS.TWO_SEC);
+
+        cy.get('#emojiPicker').should('be.visible').within(() => {
+            // * Verify recently used category is present in emoji picker
+            cy.findByText('Recently Used').should('exist').and('be.visible');
+
+            // * Verify most recent one is the thumbsup emoji with default skin
+            cy.findAllByTestId('emojiItem').eq(0).find('img').should('have.attr', 'aria-label', '+1 emoji');
+
+            // # Open skin picker again to change the skin
+            cy.findByAltText('emoji skin tone picker').should('exist').parent().click().wait(TIMEOUTS.ONE_SEC);
+
+            // # Select a dark skin tone
+            cy.findByTestId('skin-pick-1F3FF').should('exist').parent().click();
+
+            // * Verify most recent one is the same thumbsup emoji but now with a dark skin tone
+            cy.findAllByTestId('emojiItem').eq(0).find('img').should('have.attr', 'aria-label', '+1 dark skin tone emoji');
+        });
+
+        // # Close emoji picker
+        cy.get('body').type('{esc}', {force: true});
+    });
 });
