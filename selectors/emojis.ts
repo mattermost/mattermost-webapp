11a0d25a447218f632b2040d7c42e05001cdf361
Joram Wilander <jwawilander@gmail.com>
2022-01-20 16:35:07 -0500

    wip

--- selectors/emojis.ts
+++ selectors/emojis.ts
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import deepEqual from 'fast-deep-equal';
+
 import {createSelector} from 'reselect';
 
 import {getCustomEmojisByName} from 'mattermost-redux/selectors/entities/emojis';
@@ -31,6 +33,7 @@ export const getRecentEmojis = createSelector(
     (state: GlobalState) => state.storage,
     getCurrentUserId,
     (storage, currentUserId) => {
+        console.log('getRecentEmojis');
         const recentEmojis: string[] = LocalStorageStore.getRecentEmojis(currentUserId) ||
             JSON.parse(getItemFromStorage(storage.storage, Constants.RECENT_EMOJI_KEY, null)); // Prior to release v5.9, recent emojis were saved as object in localforage.
 
@@ -51,10 +54,21 @@ export function isCustomEmojiEnabled(state: GlobalState) {
     return config && config.EnableCustomEmoji === 'true';
 }
 
+let prevThreeMostRecentEmojis: string[];
 export const getOneClickReactionEmojis = (state: GlobalState) => {
-    const recentEmojis = getRecentEmojis(state).slice(-3);
+    const threeMostRecentEmojis = getRecentEmojis(state).slice(-3);
     const emojiMap = getEmojiMap(state);
 
-    const emojis = recentEmojis.map((recentEmoji) => emojiMap.get(recentEmoji)).filter(Boolean).reverse();
+    // Temporary deep equal check to prevent PostInfo components from re-rendering in many unnecessary scenarios.
+    // The proper fix is to turn this function into a proper selector but that's more complicated than it seems
+    // because getRecentEmojis never returns a new array and the array it returns originally is modified mutably
+    // when a new recent emoji is added, meaning that the selector would never recalculate. Fixing that is another
+    // rabbit hole that would make more sense to do with the other storage refactor.
+    const emojis = threeMostRecentEmojis.map((recentEmoji) => emojiMap.get(recentEmoji)).filter(Boolean).reverse();
+    if (deepEqual(emojis, prevThreeMostRecentEmojis)) {
+        return prevThreeMostRecentEmojis;
+    }
+
+    prevThreeMostRecentEmojis = emojis;
     return emojis;
 };
