6ebca02860790226a1e93600ec326e27b5da2631
Pablo Velez Vidal <pablo.velez@mattermost.com>
2022-01-20 18:12:30 +0100

    finish adding unit tests

--- components/common/accordion/accordion.test.tsx
+++ components/common/accordion/accordion.test.tsx
@@ -6,7 +6,7 @@ import {shallow} from 'enzyme';
 
 import Accordion from 'components/common/accordion/accordion';
 
-// import {mountWithIntl} from 'tests/helpers/intl-test-helper';
+import {mountWithIntl} from 'tests/helpers/intl-test-helper';
 
 describe('/components/common/Accordion', () => {
     const texts = ['First List Item', 'Second List Item', 'Third List Item'];
@@ -20,7 +20,7 @@ describe('/components/common/Accordion', () => {
                 items: [
                     (
                         <p
-                            className='accordion-item-content'
+                            className='accordion-item-content-el'
                             key={1}
                         >
                             {texts[0]}
@@ -28,7 +28,7 @@ describe('/components/common/Accordion', () => {
                     ),
                     (
                         <p
-                            className='accordion-item-content'
+                            className='accordion-item-content-el'
                             key={2}
                         >
                             {texts[1]}
@@ -42,7 +42,7 @@ describe('/components/common/Accordion', () => {
                 items: [
                     (
                         <p
-                            className='accordion-item-content'
+                            className='accordion-item-content-el'
                             key={1}
                         >
                             {texts[2]}
@@ -63,5 +63,78 @@ describe('/components/common/Accordion', () => {
         const accordionItems = wrapper.find('AccordionItem');
 
         expect(accordionItems.length).toBe(2);
+    });
+
+    test('test accordion opens first accordion item when clicked', () => {
+        const wrapper = mountWithIntl(<Accordion {...baseProps}/>);
+        const firstAccordionItem = wrapper.find('ul AccordionItem:first-child');
+        const header = firstAccordionItem.find('div.accordion-item-header');
+        header.simulate('click');
+
+        const firstChildItem = firstAccordionItem.find('div.accordion-item-container p.accordion-item-content-el:first-child');
+        const slide1Text = firstChildItem.text();
+        expect(slide1Text).toEqual('First List Item');
+    });
+
+    test('test accordion opens ONLY one accordion item at a time if NO openMultiple prop is set or set to FALSE', () => {
+        const wrapper = mountWithIntl(<Accordion {...baseProps}/>);
+        const firstAccordionItem = wrapper.find('ul AccordionItem:first-child');
+        const secondAccordionItem = wrapper.find('ul AccordionItem:last-child');
+
+        const header1 = firstAccordionItem.find('div.accordion-item-header');
+        const header2 = secondAccordionItem.find('div.accordion-item-header');
+
+        header1.simulate('click');
+
+        // refind the element after making changes so those gets reflected
+        const firstAccordionItemAfterEvent = wrapper.find('ul AccordionItem:first-child');
+        const secondAccordionItemAfterEvent = wrapper.find('ul AccordionItem:last-child');
+
+        // clicking first list element should only apply the active class to the first one and not to the last
+        expect(firstAccordionItemAfterEvent.find('li.accordion-item').hasClass('active')).toEqual(true);
+        expect(secondAccordionItemAfterEvent.find('li.accordion-item').hasClass('active')).toEqual(false);
+
+        header2.simulate('click');
+
+        // refind the element after making changes so those gets reflected
+        const firstAccordionItemAfterEvent1 = wrapper.find('ul AccordionItem:first-child');
+        const secondAccordionItemAfterEvent1 = wrapper.find('ul AccordionItem:last-child');
+
+        // clicking last list element should only apply the active class to the last one and not to the first
+        expect(firstAccordionItemAfterEvent1.find('li.accordion-item').hasClass('active')).toEqual(false);
+        expect(secondAccordionItemAfterEvent1.find('li.accordion-item').hasClass('active')).toEqual(true);
+    });
+
+    test('test accordion opens MORE THAN one accordion item at a time if openMultiple prop IS set to TRUE', () => {
+        const wrapper = mountWithIntl(
+            <Accordion
+                {...baseProps}
+                openMultiple={true}
+            />);
+        const firstAccordionItem = wrapper.find('ul AccordionItem:first-child');
+        const secondAccordionItem = wrapper.find('ul AccordionItem:last-child');
+
+        const header1 = firstAccordionItem.find('div.accordion-item-header');
+        const header2 = secondAccordionItem.find('div.accordion-item-header');
+
+        header1.simulate('click');
+
+        // refind the element after making changes so those gets reflected
+        const firstAccordionItemAfterEvent = wrapper.find('ul AccordionItem:first-child');
+        const secondAccordionItemAfterEvent = wrapper.find('ul AccordionItem:last-child');
+
+        // clicking first list element should only apply the active class to the first one and not to the last
+        expect(firstAccordionItemAfterEvent.find('li.accordion-item').hasClass('active')).toEqual(true);
+        expect(secondAccordionItemAfterEvent.find('li.accordion-item').hasClass('active')).toEqual(false);
+
+        header2.simulate('click');
+
+        // refind the element after making changes so those gets reflected
+        const firstAccordionItemAfterEvent1 = wrapper.find('ul AccordionItem:first-child');
+        const secondAccordionItemAfterEvent1 = wrapper.find('ul AccordionItem:last-child');
+
+        // clicking last list element should apply active class to both
+        expect(firstAccordionItemAfterEvent1.find('li.accordion-item').hasClass('active')).toEqual(true);
+        expect(secondAccordionItemAfterEvent1.find('li.accordion-item').hasClass('active')).toEqual(true);
     });
 });
