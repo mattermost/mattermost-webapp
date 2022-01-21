ed6bafb34b9db84678932ea1a8f6ac9c8641acfe
Suneet Srivastava <suneetbond91@gmail.com>
2022-01-22 04:39:33 +0530

    feat:40442 Added shortcut to expand the sidebar (#9523)
    * fix:40442 Added shortcut to expand the sidebar
    
    * fix: Changed shortcut to cmd+shift+period
    
    * chore: Ran i18n extract
    
    * feat: Added snapshot for the new shortcut
    
    * feat: Added to shrink RHS when it's opened and shortcut is pressed
    
    * feat: Added keyboard shortcut to tooltip  to expand/hide sidebar
    
    * feat: Added integration test
    
    * fix: Changes to the right sidebar text, tooltip placed to bottom
    
    * feat: Added tooltip to search
    
    * fix: Lint errors
    
    * test: Added a new test case for expanded/collapse rhs
    
    * chore: Added comments to test
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- components/rhs_header_post/rhs_header_post.tsx
+++ components/rhs_header_post/rhs_header_post.tsx
@@ -10,6 +10,9 @@ import LocalizedIcon from 'components/localized_icon';
 import OverlayTrigger from 'components/overlay_trigger';
 import Tooltip from 'components/tooltip';
 import FollowButton from 'components/threading/common/follow_button';
+import KeyboardShortcutSequence, {
+    KEYBOARD_SHORTCUTS,
+} from 'components/keyboard_shortcuts/keyboard_shortcuts_sequence';
 
 import {browserHistory} from 'utils/browser_history';
 import Constants, {RHSStates} from 'utils/constants';
@@ -125,7 +128,12 @@ export default class RhsHeaderPost extends React.PureComponent<RhsHeaderPostProp
             <Tooltip id='expandSidebarTooltip'>
                 <FormattedMessage
                     id='rhs_header.expandSidebarTooltip'
-                    defaultMessage='Expand Sidebar'
+                    defaultMessage='Expand the right sidebar'
+                />
+                <KeyboardShortcutSequence
+                    shortcut={KEYBOARD_SHORTCUTS.navExpandSidebar}
+                    hideDescription={true}
+                    isInsideTooltip={true}
                 />
             </Tooltip>
         );
@@ -134,7 +142,12 @@ export default class RhsHeaderPost extends React.PureComponent<RhsHeaderPostProp
             <Tooltip id='shrinkSidebarTooltip'>
                 <FormattedMessage
                     id='rhs_header.collapseSidebarTooltip'
-                    defaultMessage='Collapse Sidebar'
+                    defaultMessage='Collapse the right sidebar'
+                />
+                <KeyboardShortcutSequence
+                    shortcut={KEYBOARD_SHORTCUTS.navExpandSidebar}
+                    hideDescription={true}
+                    isInsideTooltip={true}
                 />
             </Tooltip>
         );
@@ -190,7 +203,7 @@ export default class RhsHeaderPost extends React.PureComponent<RhsHeaderPostProp
 
                     <OverlayTrigger
                         delayShow={Constants.OVERLAY_TIME_DELAY}
-                        placement='top'
+                        placement='bottom'
                         overlay={this.props.isExpanded ? shrinkSidebarTooltip : expandSidebarTooltip}
                     >
                         <button
