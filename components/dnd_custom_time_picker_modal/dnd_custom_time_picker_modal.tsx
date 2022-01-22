b8610c107fcfa0ab8710c7f745dec11ba2f1de6d
Mei Blatchford <mhblatchford@yahoo.com>
2022-01-21 20:15:37 -0500

    adding formatdate function back in

--- components/dnd_custom_time_picker_modal/dnd_custom_time_picker_modal.tsx
+++ components/dnd_custom_time_picker_modal/dnd_custom_time_picker_modal.tsx
@@ -52,6 +52,13 @@ export default class DndCustomTimePicker extends React.PureComponent<Props, Stat
             ...this.makeTimeMenuList(selectedDate),
         };
     }
+    
+    formatDate = (date: Date): string => {
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        const year = date.getFullYear().toString();
+        return [year, month, day].join('-');
+    }
 
     getText = () => {
         const modalHeaderText = (
