cda1778a3974f5411468ed0c43b10adada812a05
AGMETEOR <guwats10@gmail.com>
2022-02-01 18:23:20 +0300

    fix lint

--- components/global_header/left_controls/product_menu/product_menu.test.tsx
+++ components/global_header/left_controls/product_menu/product_menu.test.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 import React from 'react';
-import {shallow, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 import * as reactRedux from 'react-redux';
 import configureStore from 'redux-mock-store';
 
