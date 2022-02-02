0e7dbf6505823a743754bdedb6ff938b956e0003
neallred <neallred@protonmail.com>
2022-02-02 12:05:23 -0600

    clean up test output

--- components/admin_console/billing/billing_summary/billing_summary.tsx
+++ components/admin_console/billing/billing_summary/billing_summary.tsx
@@ -11,7 +11,7 @@ import BlockableLink from 'components/admin_console/blockable_link';
 import OverlayTrigger from 'components/overlay_trigger';
 import Tooltip from 'components/tooltip';
 import UpgradeSvg from 'components/common/svg_images_components/upgrade.svg';
-import EmptyBillingHistorySvg from 'components/common/svg_images_components/empty_billing_history.svg';
+import EmptyBillingHistorySvg from 'components/common/svg_images_components/empty_billing_history_svg';
 
 import {trackEvent} from 'actions/telemetry_actions';
 
