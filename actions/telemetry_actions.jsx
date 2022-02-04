c7e325c15c3c52d7bb19d665d187d4d9b6941b4b
Joram Wilander <jwawilander@gmail.com>
2022-02-04 09:48:02 -0500

    Mark channel/team switch end when posts are mounted

--- actions/telemetry_actions.jsx
+++ actions/telemetry_actions.jsx
@@ -14,8 +14,12 @@ const SUPPORTS_MEASURE_METHODS = isSupported([
     performance.clearMeasures,
 ]);
 
-export function trackEvent(category, event, props) {
+export function trackEvent(category, event, props = {}) {
     Client4.trackEvent(category, event, props);
+    if (isDevMode() && category === 'performance') {
+        // eslint-disable-next-line no-console
+        console.log(`${event}: ${props.duration}ms, fresh: ${props.fresh}`);
+    }
 }
 
 export function pageVisited(category, name) {
