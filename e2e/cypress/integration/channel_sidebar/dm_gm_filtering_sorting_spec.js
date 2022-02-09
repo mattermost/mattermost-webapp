940230676c49ff8ee7cf3cdea04d8d1f495d9df3
Md_ZubairAhmed <m-zubairahmed@protonmail.com>
2022-02-09 20:16:30 +0530

    fix 2nd e2e responsible for this pr

--- e2e/cypress/integration/channel_sidebar/dm_gm_filtering_sorting_spec.js
+++ e2e/cypress/integration/channel_sidebar/dm_gm_filtering_sorting_spec.js
@@ -10,6 +10,7 @@
 // Stage: @prod
 // Group: @dm_category
 
+import * as MESSAGES from '../../fixtures/messages';
 import {getAdminAccount} from '../../support/env';
 
 describe('DM/GM filtering and sorting', () => {
@@ -43,12 +44,12 @@ describe('DM/GM filtering and sorting', () => {
                     // # Post a message as the new user
                     cy.postMessageAs({
                         sender: user,
-                        message: `Hey ${receivingUser.username}`,
+                        message: MESSAGES.TINY,
                         channelId: channel.id,
                     });
 
                     // * Verify that the DM count is now correct
-                    cy.get('.SidebarChannelGroup:contains(DIRECT MESSAGES) a[id^="sidebarItem"]').should('have.length', Math.min(i + 1, 20));
+                    cy.get('.SidebarChannelGroup:contains(DIRECT MESSAGES) a[id^="sidebarItem"]').should('have.length', Math.min(i + 1, 2));
 
                     // # Click on the new DM channel to mark it read
                     cy.get(`#sidebarItem_${channel.name}`).should('be.visible').click();
