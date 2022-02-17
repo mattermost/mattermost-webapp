3478d1dc2c4a339ca83abc5a9bd35b162f60f41c
neallred <neallred@protonmail.com>
2022-02-17 15:01:56 -0600

    lint

--- components/preparing_workspace/preparing_workspace.tsx
+++ components/preparing_workspace/preparing_workspace.tsx
@@ -332,7 +332,7 @@ export default function PreparingWorkspace(props: Props) {
                 category: OnboardingTaskCategory,
                 name: OnboardingTaskList.ONBOARDING_TASK_LIST_OPEN,
                 value: 'true',
-            }
+            },
         ]));
 
         const sendFormEnd = Date.now();
