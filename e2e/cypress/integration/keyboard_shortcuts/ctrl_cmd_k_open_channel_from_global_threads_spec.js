c64af5061fa1a5c4c841758c436efdbbc7f9df86
Ashish Dhama <ashishdhama3@gmail.com>
2022-02-23 15:46:05 +0530

    add more threads and test cases

--- e2e/cypress/integration/keyboard_shortcuts/ctrl_cmd_k_open_channel_from_global_threads_spec.js
+++ e2e/cypress/integration/keyboard_shortcuts/ctrl_cmd_k_open_channel_from_global_threads_spec.js
@@ -46,42 +46,39 @@ describe('Keyboard Shortcuts', () => {
     });
 
     it('MM-T1243 CTRL/CMD+K - Create thread, Open global threads and then from find channels switch channel using arrow keys and Enter', () => {
-        // # Post message as other user
+        // # Post first message as other user
         cy.postMessageAs({
             sender: otherUser,
-            message: 'Root post,',
+            message: 'First post',
             channelId: testChannel.id,
         }).then(({id: rootId}) => {
-            // * Thread footer should not be visible
-            cy.get(`#post_${rootId}`).find('.ThreadFooter').should('not.exist');
-
             // # Click on post to open RHS
             cy.get(`#post_${rootId}`).click();
-
-            // * Button on header should say Follow as current user is not following
-            cy.get('#rhsContainer').find('.FollowButton').should('have.text', 'Follow');
 
             // # Post a reply as current user
             cy.postMessageReplyInRHS('Reply!');
 
-            // # Get last root post
-            cy.get(`#post_${rootId}`).
+            // # Close RHS
+            cy.uiCloseRHS();
+        });
 
-                // * thread footer should exist now
-                get('.ThreadFooter').should('exist').
-                within(() => {
-                // * the button on the footer should say Following
-                    cy.get('.FollowButton').should('have.text', 'Following');
-                });
+        // # Post second message as other user
+        cy.postMessageAs({
+            sender: otherUser,
+            message: 'Second post',
+            channelId: testChannel.id,
+        }).then(({id: rootId}) => {
+            // # Click on post to open RHS
+            cy.get(`#post_${rootId}`).click();
 
-            // * the button on the RHS header should now say Following
-            cy.get('#rhsContainer').find('.FollowButton').should('have.text', 'Following');
+            // # Post a reply as current user
+            cy.postMessageReplyInRHS('Reply!');
 
             // # Visit global threads
             cy.uiClickSidebarItem('threads');
 
             // * There should be a thread there
-            cy.get('article.ThreadItem').should('have.have.lengthOf', 1);
+            cy.get('article.ThreadItem').should('have.have.lengthOf', 2);
         });
 
         // # Press CTRL/CMD+K
@@ -92,12 +89,18 @@ describe('Keyboard Shortcuts', () => {
         cy.wait(TIMEOUTS.HALF_SEC);
         cy.get('body').type('{downarrow}');
 
+        // * should not select thread and switch thread in background
+        cy.url().should('equal', `${Cypress.config('baseUrl')}/${testTeam.name}/threads`);
+
         // * Confirm the offtopic channel is selected in the suggestion list
         cy.get('#suggestionList').findByTestId('off-topic').should('be.visible').and('have.class', 'suggestion--selected');
 
         // # Press up arrow
         cy.wait(TIMEOUTS.HALF_SEC);
         cy.get('body').type('{uparrow}');
+
+        // * should not select thread and switch thread in background
+        cy.url().should('equal', `${Cypress.config('baseUrl')}/${testTeam.name}/threads`);
 
         // * Confirm the townsquare channel is selected in the suggestion list
         cy.get('#suggestionList').findByTestId('town-square').should('be.visible').and('have.class', 'suggestion--selected');
@@ -106,14 +109,17 @@ describe('Keyboard Shortcuts', () => {
         cy.wait(TIMEOUTS.HALF_SEC);
         cy.get('body').type('{downarrow}');
 
+        // * should not select thread and switch thread in background
+        cy.url().should('equal', `${Cypress.config('baseUrl')}/${testTeam.name}/threads`);
+
         // * Confirm the offtopic channel is selected in the suggestion list
         cy.get('#suggestionList').findByTestId('off-topic').should('be.visible').and('have.class', 'suggestion--selected');
 
         // # Press ENTER
         cy.get('body').type('{enter}');
 
-        // * Confirm that channel is open, and post text box has focus
+        // * Confirm that channel is open
         cy.contains('#channelHeaderTitle', 'Off-Topic');
-        cy.get('#post_textbox').should('be.focused');
+        cy.url().should('include', '/channels/off-topic');
     });
 });
