999b6b28a9644a7365ad6a3821e7a0c205c3a21f
Claudio Costa <cstcld91@gmail.com>
2022-02-24 18:13:31 +0100

    Update plugins/registry.js
    Co-authored-by: Justine Geffen <justinegeffen@users.noreply.github.com>

--- plugins/registry.js
+++ plugins/registry.js
@@ -178,7 +178,7 @@ export default class PluginRegistry {
     // plugin, a dropdown menu is created to contain all the call plugin buttons.
     // Accepts the following:
     // - button - a React element to use as the main button to be displayed in case of a single registration.
-    // - dropdownButton - a React element to use as the dropdown button to be displayed in case of multiple registrations.
+    // - dropdownButton -A React element to use as the dropdown button to be displayed in case of multiple registrations.
     // - action - A function called when the button is clicked, passed the channel and channel member as arguments.
     // Returns an unique identifier
     // Minimum required version: 6.5
