9f1a344fa1764cf47336ef7f257c979efced1671
Carrie Warner (Mattermost) <74422101+cwarnermm@users.noreply.github.com>
2022-02-24 16:43:08 -0500

    Update e2e/utils/webhook_utils.js

--- e2e/utils/webhook_utils.js
+++ e2e/utils/webhook_utils.js
@@ -243,7 +243,7 @@ function getBooleanDialog(triggerId, webhookBaseUrl) {
             callback_id: 'somecallbackid',
             title: 'Title for Dialog Test with boolean element',
             icon_url:
-                'http://mattermost.com/wp-content/uploads/2022/02/icon_WS.png',
+                'https://mattermost.com/wp-content/uploads/2022/02/icon_WS.png',
             submit_label: 'Submit Test',
             notify_on_cancel: true,
             state: 'somestate',
