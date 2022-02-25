f9d371d53bea0a34f4baae3433028a2d0fddcfd9
Harrison Healey <harrisonmhealey@gmail.com>
2022-02-25 12:37:23 -0500

    Remove unused string ref

--- components/user_settings/security/user_settings_security.tsx
+++ components/user_settings/security/user_settings_security.tsx
@@ -982,7 +982,6 @@ export default class SecurityTab extends React.PureComponent<Props, State> {
                     </button>
                     <h4
                         className='modal-title'
-                        ref='title'
                     >
                         <div className='modal-back'>
                             <LocalizedIcon
