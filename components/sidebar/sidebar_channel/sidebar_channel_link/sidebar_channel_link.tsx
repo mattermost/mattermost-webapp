edae4563eef0c882bb96b091e907110bb4ac2e27
Nathaniel Allred <neallred@protonmail.com>
2022-03-10 08:09:09 -0600

    Update components/sidebar/sidebar_channel/sidebar_channel_link/sidebar_channel_link.tsx
    Co-authored-by: Ashish Dhama <ashishdhama3@gmail.com>

--- components/sidebar/sidebar_channel/sidebar_channel_link/sidebar_channel_link.tsx
+++ components/sidebar/sidebar_channel/sidebar_channel_link/sidebar_channel_link.tsx
@@ -177,7 +177,7 @@ export default class SidebarChannelLink extends React.PureComponent<Props, State
         // firstChannelName is based on channel.name,
         // but we want to display `display_name` to the user, so we check against `.name` for channel equality but pass in the .display_name value
         if (firstChannelName === channel.name || (!firstChannelName && showChannelsTutorialStep && channel.name === Constants.DEFAULT_CHANNEL)) {
-            channelsTutorialTip = (<ChannelsAndDirectMessagesTour firstChannelName={channel.display_name}/>);
+            channelsTutorialTip = firstChannelName ? (<ChannelsAndDirectMessagesTour firstChannelName={channel.display_name}/>) : (<ChannelsAndDirectMessagesTour>);
         }
 
         let labelElement: JSX.Element = (
