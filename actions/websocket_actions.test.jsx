5d7a597d842f39289fa5e52aedd4cf27d66eff02
Harrison Healey <harrisonmhealey@gmail.com>
2022-03-23 17:21:49 -0400

    Fix tests relying on bad memoization

--- actions/websocket_actions.test.jsx
+++ actions/websocket_actions.test.jsx
@@ -25,6 +25,8 @@ import configureStore from 'tests/test_store';
 import {browserHistory} from 'utils/browser_history';
 import Constants, {SocketEvents, UserStatuses, ActionTypes} from 'utils/constants';
 
+import mergeObjects from 'packages/mattermost-redux/test/merge_objects';
+
 import {
     handleChannelUpdatedEvent,
     handleEvent,
@@ -78,7 +80,7 @@ jest.mock('plugins', () => ({
 
 jest.mock('utils/browser_history');
 
-const mockState = {
+let mockState = {
     entities: {
         users: {
             currentUserId: 'currentUserId',
@@ -291,9 +293,38 @@ describe('handleUserRemovedEvent', () => {
             },
         };
 
-        mockState.entities.roles.roles = {system_guest: {permissions: []}};
+        mockState = mergeObjects(
+            mockState,
+            {
+                entities: {
+                    roles: {
+                        roles: {
+                            system_guest: {
+                                permissions: [],
+                            },
+                        },
+                    },
+                },
+            },
+        );
+
         handleUserRemovedEvent(msg);
-        mockState.entities.roles.roles = {system_guest: {permissions: ['view_members']}};
+
+        mockState = mergeObjects(
+            mockState,
+            {
+                entities: {
+                    roles: {
+                        roles: {
+                            system_guest: {
+                                permissions: ['view_members'],
+                            },
+                        },
+                    },
+                },
+            },
+        );
+
         expect(store.dispatch).toHaveBeenCalledWith(expectedAction);
     });
 
