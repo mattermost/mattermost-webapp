0f3a9b5d5a0838f36f0073495a40884c59544ac6
Julian Mondragon <bugshoo+github@gmail.com>
2022-03-29 09:41:08 -0500

    MM-41850_Webapp: Update to New Channel Modal e2e

--- e2e/cypress/tests/integration/enterprise/system_console/about/edition_and_license_spec.js
+++ e2e/cypress/tests/integration/enterprise/system_console/about/edition_and_license_spec.js
@@ -109,10 +109,10 @@ function verifyCreatePublicChannel(teamName, testUsers) {
 
         cy.findByRole('dialog', {name: 'Create a new channel'}).within(() => {
             // * Verify if creating a public channel is disabled or not
-            cy.get('#O.public-private-selector-button').should(canCreate ? 'not.have.class' : 'have.class', 'disabled');
+            cy.get('#O.public-private-selector-button').should(isSysadmin || canCreate ? 'not.have.class' : 'have.class', 'disabled');
 
-            // * Verify if creating a private channel is disabled or not
-            cy.get('#P.public-private-selector-button').should(isSysadmin ? 'not.have.class' : 'have.class', 'disabled');
+            // * Verify if creating a private channel is not disabled
+            cy.get('#P.public-private-selector-button').should('not.have.class', 'disabled');
         });
     }
 }
