c21552a36b525d48a1551fdbc1c4d242cd8ab8d2
Harrison Healey <harrisonmhealey@gmail.com>
2022-03-30 10:57:20 -0400

    MM-42542 Remove test files from packages/types

--- packages/types/src/another_file.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
-import {MyEnum, MyType} from './a_file';
-
-export type AnotherType = MyType & {
-    enum: MyEnum;
-}
