56512a3663dcd14097810bcec696ba46732ef2f7
Md_ZubairAhmed <m-zubairahmed@protonmail.com>
2022-04-01 20:25:57 +0530

    Update users.ts

--- packages/mattermost-redux/src/actions/users.ts
+++ packages/mattermost-redux/src/actions/users.ts
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {AnyAction} from 'redux';
 import {batchActions} from 'redux-batched-actions';
 
 import {Client4} from 'mattermost-redux/client';
