e4196e7c2a3a164c2a40f48fe9fd84763cbc911f
sridhar02 <kattasridhar02@gmail.com>
2022-04-01 08:58:29 +0530

    ✨ convert emoji picker overly and emoji picker tabs to typescript

---
+++ components/emoji_picker/emoji_picker_overlay.tsx
@@ -0,0 +1,111 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {Overlay} from 'react-bootstrap';
+import memoize from 'memoize-one';
+
+import {popOverOverlayPosition} from 'utils/position_utils';
+import {Constants} from 'utils/constants';
+
+import EmojiPickerTabs from './emoji_picker_tabs';
+
+enum PositionTypes {
+    LEFT ='left',
+    RIGHT = 'right',
+}
+
+interface Props {
+    show: boolean;
+    container: () => void;
+    target: () => void;
+    onEmojiClick: () => void;
+    onGifClick: () => void;
+    onHide: () => void;
+    topOffset: number;
+    rightOffset: number;
+    leftOffset: number;
+    spaceRequiredAbove: number;
+    spaceRequiredBelow: number;
+    enableGifPicker: boolean;
+    defaultHorizontalPosition: PositionTypes;
+}
+
+// An emoji picker in the center channel is contained within the post list, so it needs space
+// above for the channel header and below for the post textbox
+const CENTER_SPACE_REQUIRED_ABOVE = 476;
+const CENTER_SPACE_REQUIRED_BELOW = 497;
+
+// An emoji picker in the RHS isn't constrained by the RHS, so it just needs space to fit
+// the emoji picker itself
+// const RHS_SPACE_REQUIRED_ABOVE = 420;
+// const RHS_SPACE_REQUIRED_BELOW = 420;
+
+const EmojiPickerOverlay = ({show, container, target, onEmojiClick, onGifClick, onHide, topOffset, rightOffset,
+    leftOffset,
+    spaceRequiredAbove = CENTER_SPACE_REQUIRED_ABOVE, spaceRequiredBelow = CENTER_SPACE_REQUIRED_BELOW, enableGifPicker = false, defaultHorizontalPosition}: Props) => {
+    // Reasonable defaults calculated from from the center channel
+    // static defaultProps = {
+    //     spaceRequiredAbove: EmojiPickerOverlay.CENTER_SPACE_REQUIRED_ABOVE,
+    //     spaceRequiredBelow: EmojiPickerOverlay.CENTER_SPACE_REQUIRED_BELOW,
+    //     enableGifPicker: false,
+    // };
+
+    const emojiPickerPosition = memoize((emojiTrigger, show) => {
+        let calculatedRightOffset = Constants.DEFAULT_EMOJI_PICKER_RIGHT_OFFSET;
+
+        if (!show) {
+            return calculatedRightOffset;
+        }
+
+        if (emojiTrigger) {
+            calculatedRightOffset = window.innerWidth - emojiTrigger.getBoundingClientRect().left - Constants.DEFAULT_EMOJI_PICKER_LEFT_OFFSET;
+
+            if (calculatedRightOffset < Constants.DEFAULT_EMOJI_PICKER_RIGHT_OFFSET) {
+                calculatedRightOffset = Constants.DEFAULT_EMOJI_PICKER_RIGHT_OFFSET;
+            }
+        }
+
+        return calculatedRightOffset;
+    });
+
+    const getPlacement = memoize((target, spaceRequiredAbove, spaceRequiredBelow, defaultHorizontalPosition, show) => {
+        if (!show) {
+            return 'top';
+        }
+
+        if (target) {
+            const targetBounds = target.getBoundingClientRect();
+            return popOverOverlayPosition(targetBounds, window.innerHeight, spaceRequiredAbove, spaceRequiredBelow, defaultHorizontalPosition);
+        }
+
+        return 'top';
+    });
+
+    const calculatedRightOffset = typeof rightOffset === 'undefined' ? emojiPickerPosition(target(), show) : rightOffset;
+    const placement = getPlacement(target(), spaceRequiredAbove, spaceRequiredBelow, defaultHorizontalPosition, show);
+
+    return (
+        <Overlay
+            show={show}
+            placement={placement}
+            rootClose={true}
+            container={container}
+            onHide={onHide}
+            target={target}
+            animation={false}
+        >
+            <EmojiPickerTabs
+                enableGifPicker={enableGifPicker}
+                onEmojiClose={onHide}
+                onEmojiClick={onEmojiClick}
+                onGifClick={onGifClick}
+                rightOffset={calculatedRightOffset}
+                topOffset={topOffset}
+                leftOffset={leftOffset}
+            />
+        </Overlay>
+    );
+};
+
+export default EmojiPickerOverlay;
---
+++ components/emoji_picker/emoji_picker_tabs.tsx
@@ -0,0 +1,149 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React, {useState} from 'react';
+import {Tab, Tabs} from 'react-bootstrap';
+
+import EmojiIcon from 'components/widgets/icons/emoji_icon';
+import GfycatIcon from 'components/widgets/icons/gfycat_icon';
+import {makeAsyncComponent} from 'components/async_load';
+import EmojiPicker from 'components/emoji_picker';
+import EmojiPickerHeader from 'components/emoji_picker/components/emoji_picker_header';
+
+const GifPicker = makeAsyncComponent('GifPicker', React.lazy(() => import('components/gif_picker/gif_picker.jsx')));
+
+enum PlacementType {
+    TOP ='top',
+    BOTTOM='bottom',
+    LEFT ='left',
+    RIGHT ='right',
+}
+
+interface Props {
+    style?: any;
+    rightOffset: number;
+    topOffset: number;
+    leftOffset: number;
+    placement?: PlacementType;
+    customEmojis?: any;
+    onEmojiClose: () => void;
+    onEmojiClick: () => void;
+    onGifClick: () => void;
+    enableGifPicker: boolean;
+}
+
+const EmojiPickerTabs = ({rightOffset = 0,
+    topOffset = 0,
+    leftOffset = 0, onEmojiClick, onEmojiClose, onGifClick, placement, style, enableGifPicker}: Props) => {
+    const [filter, setFilter] = useState('');
+    const [emojiTabVisible, setEmojiTabVisible] = useState(true);
+
+    const handleEnterEmojiTab = () => {
+        setEmojiTabVisible(true);
+    };
+
+    const handleExitEmojiTab = () => {
+        setEmojiTabVisible(false);
+    };
+
+    const handleEmojiPickerClose = () => {
+        onEmojiClose();
+    };
+
+    const handleFilterChange = (filter: any) => {
+        setFilter(filter);
+    };
+
+    let pickerStyle;
+    if (style && !(style.left === 0 && style.top === 0)) {
+        if (placement === 'top' || placement === 'bottom') {
+            // Only take the top/bottom position passed by React Bootstrap since we want to be right-aligned
+            pickerStyle = {
+                top: style.top,
+                bottom: style.bottom,
+                right: rightOffset,
+            };
+        } else {
+            pickerStyle = {...style};
+        }
+
+        pickerStyle.top = pickerStyle.top ? pickerStyle.top + topOffset : topOffset;
+
+        if (pickerStyle.left) {
+            pickerStyle.left += leftOffset;
+        }
+    }
+
+    let pickerClass = 'emoji-picker';
+    if (placement === 'bottom') {
+        pickerClass += ' bottom';
+    }
+
+    if (enableGifPicker && typeof onGifClick != 'undefined') {
+        return (
+            <Tabs
+                defaultActiveKey={1}
+                id='emoji-picker-tabs'
+                style={pickerStyle}
+                className={pickerClass}
+                justified={true}
+            >
+                <EmojiPickerHeader handleEmojiPickerClose={handleEmojiPickerClose}/>
+                <Tab
+                    eventKey={1}
+                    onEnter={handleEnterEmojiTab}
+                    onExit={handleExitEmojiTab}
+                    title={
+                        <div className={'custom-emoji-tab__icon__text'}>
+                            <EmojiIcon
+                                className='custom-emoji-tab__icon'
+                            />
+                            <div>
+                                {'Emojis'}
+                            </div>
+                        </div>
+                    }
+                    tabClassName={'custom-emoji-tab'}
+                >
+                    <EmojiPicker
+                        filter={filter}
+                        visible={emojiTabVisible}
+                        onEmojiClick={onEmojiClick}
+                        handleFilterChange={handleFilterChange}
+                    />
+                </Tab>
+                <Tab
+                    eventKey={2}
+                    title={<GfycatIcon/>}
+                    mountOnEnter={true}
+                    unmountOnExit={true}
+                    tabClassName={'custom-emoji-tab'}
+                >
+                    <GifPicker
+                        onGifClick={onGifClick}
+                        defaultSearchText={filter}
+                        handleSearchTextChange={handleFilterChange}
+                    />
+                </Tab>
+            </Tabs>
+        );
+    }
+
+    return (
+        <div
+            id='emojiPicker'
+            style={pickerStyle}
+            className={`a11y__popup ${pickerClass} emoji-picker--single`}
+        >
+            <EmojiPickerHeader handleEmojiPickerClose={handleEmojiPickerClose}/>
+            <EmojiPicker
+                filter={filter}
+                visible={emojiTabVisible}
+                onEmojiClick={onEmojiClick}
+                handleFilterChange={handleFilterChange}
+            />
+        </div>
+    );
+};
+
+export default EmojiPickerTabs;
