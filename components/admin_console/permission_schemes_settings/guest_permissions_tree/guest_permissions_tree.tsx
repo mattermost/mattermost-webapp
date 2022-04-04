0dbd8c837e2c73216d7123c8c6ec72057db94801
Sinan Chaush <sinansonmez@outlook.com>
2022-04-04 21:16:59 +0200

    useCallback and useMemo are used passed functions variables

--- components/admin_console/permission_schemes_settings/guest_permissions_tree/guest_permissions_tree.tsx
+++ components/admin_console/permission_schemes_settings/guest_permissions_tree/guest_permissions_tree.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, {useCallback, useMemo} from 'react';
 import {FormattedMessage} from 'react-intl';
 
 import Permissions from 'mattermost-redux/constants/permissions';
@@ -26,45 +26,53 @@ type Props = {
 const GuestPermissionsTree = ({license, onToggle, readOnly, scope, selectRow, parentRole, selected, role = {permissions: []}}: Props) => {
     const [editTimeLimitModalIsVisible, setEditTimeLimitModalIsVisible] = React.useState(false);
 
-    const openPostTimeLimitModal = () => setEditTimeLimitModalIsVisible(true);
-    const closePostTimeLimitModal = () => setEditTimeLimitModalIsVisible(false);
-    const toggleGroup = (ids: string[]) => {
+    const openPostTimeLimitModal = useCallback(() => {
+        setEditTimeLimitModalIsVisible(true);
+    }, []);
+    const closePostTimeLimitModal = useCallback(() => {
+        setEditTimeLimitModalIsVisible(false);
+    }, []);
+    const toggleGroup = useCallback((ids: string[]) => {
         if (readOnly) {
             return;
         }
         onToggle(role.name!, ids);
-    };
+    }, [onToggle, readOnly, role.name]);
 
-    const ADDITIONAL_VALUES = {
-        guest_edit_post: {
-            editTimeLimitButton: (
-                <EditPostTimeLimitButton
-                    onClick={openPostTimeLimitModal}
-                    isDisabled={readOnly}
-                />
-            ),
-        },
-    };
+    const ADDITIONAL_VALUES = useMemo(() => {
+        return {
+            guest_edit_post: {
+                editTimeLimitButton: (
+                    <EditPostTimeLimitButton
+                        onClick={openPostTimeLimitModal}
+                        isDisabled={readOnly}
+                    />
+                ),
+            },
+        };
+    }, [openPostTimeLimitModal, readOnly]);
 
-    let permissions = [
-        Permissions.CREATE_PRIVATE_CHANNEL,
-        Permissions.EDIT_POST,
-        Permissions.DELETE_POST,
-        {
-            id: 'guest_reactions',
-            combined: true,
-            permissions: [
-                Permissions.ADD_REACTION,
-                Permissions.REMOVE_REACTION,
-            ],
-        },
-        Permissions.USE_CHANNEL_MENTIONS,
-    ];
+    let permissions = useMemo(() => {
+        return [
+            Permissions.CREATE_PRIVATE_CHANNEL,
+            Permissions.EDIT_POST,
+            Permissions.DELETE_POST,
+            Permissions.CREATE_POST,
+            {
+                id: 'guest_reactions',
+                combined: true,
+                permissions: [
+                    Permissions.ADD_REACTION,
+                    Permissions.REMOVE_REACTION,
+                ],
+            },
+            Permissions.USE_CHANNEL_MENTIONS,
+        ];
+    }, []);
 
     if (license && license.IsLicensed === 'true' && license.LDAPGroups === 'true') {
         permissions.push(Permissions.USE_GROUP_MENTIONS);
     }
-    permissions.push(Permissions.CREATE_POST);
     permissions = permissions.map((permission) => {
         if (typeof (permission) === 'string') {
             return {
