94063dc430a7a2d4d0c40a75d6b849fe496eded3
Benjamin Cooke <benjamincooke@Benjamins-MBP.ht.home>
2022-04-06 14:17:16 -0400

    top reactions loader

--- components/activity_and_insights/insights/top_reactions/top_reactions.tsx
+++ components/activity_and_insights/insights/top_reactions/top_reactions.tsx
@@ -2,13 +2,46 @@
 // See LICENSE.txt for license information.
 import React, {memo} from 'react';
 
+import BarChartLoader from '../skeleton_loader/bar_chart_loader/bar_chart_loader';
+import CircleLoader from '../skeleton_loader/circle_loader/circle_loader';
 import widgetHoc from '../widget_hoc/widget_hoc';
 
 import './../../activity_and_insights.scss';
 
 const TopReactions = () => {
     return (
-        <div className='top-reaction-container'/>
+        <div className='top-reaction-container'>
+            <div className='bar-chart-entry'>
+                <BarChartLoader />
+                <CircleLoader
+                    size={20}
+                />
+            </div>
+            <div className='bar-chart-entry'>
+                <BarChartLoader />
+                <CircleLoader
+                    size={20}
+                />
+            </div>
+            <div className='bar-chart-entry'>
+                <BarChartLoader />
+                <CircleLoader
+                    size={20}
+                />
+            </div>
+            <div className='bar-chart-entry'>
+                <BarChartLoader />
+                <CircleLoader
+                    size={20}
+                />
+            </div>
+            <div className='bar-chart-entry'>
+                <BarChartLoader />
+                <CircleLoader
+                    size={20}
+                />
+            </div>
+        </div>
     );
 };
 
