a27d9975a0f03b411f953056447de970c44226c0
Mario Vitale <mvitale1989@hotmail.com>
2022-04-08 01:47:49 +0200

    MM-T639 Add assertions

--- e2e/cypress/tests/integration/integrations/incoming_webhook/incoming_webhook_creates_dm_spec.js
+++ e2e/cypress/tests/integration/integrations/incoming_webhook/incoming_webhook_creates_dm_spec.js
@@ -21,16 +21,24 @@ describe('Incoming webhook', () => {
     let generatedChannel;
     let generatedUser;
     let generatedUserPassword;
+    let teamTownSquareUrl;
+    let sysAdminUser;
+    let sortedUserIds;
 
     before(() => {
+      cy.apiGetMe().then((me) => {
+          sysAdminUser = me.user;
+      });
+
       // # Enable username override
       enableUsernameAndIconOverride(true, false);
 
       // # Create a new user, their team, and a channel to tie the webhook to
-      cy.apiInitSetup({userPrefix: "mm-t639-"}).then(({team, channel, user}) => {
+      cy.apiInitSetup({userPrefix: "mm-t639-"}).then(({team, channel, user, channelUrl, offTopicUrl, townSquareUrl}) => {
           generatedTeam = team;
           generatedChannel = channel;
           generatedUser = user;
+          teamTownSquareUrl = townSquareUrl;
           incomingWebhookConfiguration = {
               channel_id: channel.id,
               channel_locked: false,
@@ -39,16 +47,35 @@ describe('Incoming webhook', () => {
           cy.apiCreateWebhook(incomingWebhookConfiguration).then((hook) => {
               incomingWebhook = hook;
           });
+      }).then(() => {
+        // # Send webhook notification
+        const webhookPayload = {channel: `@${generatedUser.username}`, text: incomingWebhookText};
+        cy.postIncomingWebhook({url: incomingWebhook.url, data: webhookPayload});
+      }).then(() => {
+        // # Open any page to get to the sidebar
+        cy.visit(`/${generatedTeam.name}/channels/${generatedChannel.name}`);
       });
     });
 
     it('MM-T639 🚀 incoming Webhook creates DM', () => {
-        // # Send webhook notification
-        const webhookPayload = {channel: `@${generatedUser.username}`, text: incomingWebhookText};
-        cy.postIncomingWebhook({url: incomingWebhook.url, data: webhookPayload});
+        // # Verify that the channel was created correctly with an unread message, and open it
+        cy.get(`a[href='/${generatedTeam.name}/messages/@${generatedUser.username}']`)
+          .scrollIntoView()
+          .should('be.visible')
+          .should('have.class', 'unread-title')
+          .click();
 
-        // # Verify that the message was posted correctly
-        cy.visit(`/${generatedTeam.name}/channels/${generatedChannel.name}`);
-        // TODO assertions
+        // # Verify that the post matches our expectation
+        cy.getLastPost()
+          .should('be.visible')
+          .get('.col__name > button.user-popover')
+          .contains(incomingWebhookConfiguration.display_name);
+        cy.getLastPost()
+          .should('be.visible')
+          .get('.col__name .BotBadge > span')
+          .contains("BOT");
+        cy.getLastPost()
+          .get('.post-message__text')
+          .contains(incomingWebhookText);
     });
 });
