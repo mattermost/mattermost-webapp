8bc9ce79578d9bc4c1f7c07509b23b4eeb106a77
Mario Vitale <mvitale1989@hotmail.com>
2022-04-08 20:19:26 +0200

    MM-T639 Fix lint

--- e2e/cypress/tests/integration/integrations/incoming_webhook/incoming_webhook_creates_dm_spec.js
+++ e2e/cypress/tests/integration/integrations/incoming_webhook/incoming_webhook_creates_dm_spec.js
@@ -11,71 +11,61 @@
 // Group: @incoming_webhook
 
 import {enableUsernameAndIconOverride} from './helpers';
-import {generateRandomUser} from '../../../support/api/user';
 
 describe('Incoming webhook', () => {
-    const incomingWebhookText = "This is a message to a newly created direct message channel";
+    const incomingWebhookText = 'This is a message to a newly created direct message channel';
     let incomingWebhookConfiguration;
     let incomingWebhook;
     let generatedTeam;
     let generatedChannel;
     let generatedUser;
-    let generatedUserPassword;
-    let teamTownSquareUrl;
-    let sysAdminUser;
-    let sortedUserIds;
 
     before(() => {
-      cy.apiGetMe().then((me) => {
-          sysAdminUser = me.user;
-      });
+        // # Enable username override
+        enableUsernameAndIconOverride(true, false);
 
-      // # Enable username override
-      enableUsernameAndIconOverride(true, false);
-
-      // # Create a new user, their team, and a channel to tie the webhook to
-      cy.apiInitSetup({userPrefix: "mm-t639-"}).then(({team, channel, user, channelUrl, offTopicUrl, townSquareUrl}) => {
-          generatedTeam = team;
-          generatedChannel = channel;
-          generatedUser = user;
-          teamTownSquareUrl = townSquareUrl;
-          incomingWebhookConfiguration = {
-              channel_id: channel.id,
-              channel_locked: false,
-              display_name: 'webhook',
-          };
-          cy.apiCreateWebhook(incomingWebhookConfiguration).then((hook) => {
-              incomingWebhook = hook;
-          });
-      }).then(() => {
-        // # Send webhook notification
-        const webhookPayload = {channel: `@${generatedUser.username}`, text: incomingWebhookText};
-        cy.postIncomingWebhook({url: incomingWebhook.url, data: webhookPayload});
-      }).then(() => {
-        // # Open any page to get to the sidebar
-        cy.visit(`/${generatedTeam.name}/channels/${generatedChannel.name}`);
-      });
+        // # Create a new user, their team, and a channel to tie the webhook to
+        cy.apiInitSetup({userPrefix: 'mm-t639-'}).then(({team, channel, user}) => {
+            generatedTeam = team;
+            generatedChannel = channel;
+            generatedUser = user;
+            incomingWebhookConfiguration = {
+                channel_id: channel.id,
+                channel_locked: false,
+                display_name: 'webhook',
+            };
+            cy.apiCreateWebhook(incomingWebhookConfiguration).then((hook) => {
+                incomingWebhook = hook;
+            });
+        }).then(() => {
+            // # Send webhook notification
+            const webhookPayload = {channel: `@${generatedUser.username}`, text: incomingWebhookText};
+            cy.postIncomingWebhook({url: incomingWebhook.url, data: webhookPayload});
+        }).then(() => {
+            // # Open any page to get to the sidebar
+            cy.visit(`/${generatedTeam.name}/channels/${generatedChannel.name}`);
+        });
     });
 
     it('MM-T639 🚀 incoming Webhook creates DM', () => {
         // # Verify that the channel was created correctly with an unread message, and open it
-        cy.get(`a[href='/${generatedTeam.name}/messages/@${generatedUser.username}']`)
-          .scrollIntoView()
-          .should('be.visible')
-          .should('have.class', 'unread-title')
-          .click();
+        cy.get(`a[href='/${generatedTeam.name}/messages/@${generatedUser.username}']`).
+            scrollIntoView().
+            should('be.visible').
+            should('have.class', 'unread-title').
+            click();
 
         // # Verify that the post matches our expectation
-        cy.getLastPost()
-          .should('be.visible')
-          .get('.col__name > button.user-popover')
-          .contains(incomingWebhookConfiguration.display_name);
-        cy.getLastPost()
-          .should('be.visible')
-          .get('.col__name .BotBadge > span')
-          .contains("BOT");
-        cy.getLastPost()
-          .get('.post-message__text')
-          .contains(incomingWebhookText);
+        cy.getLastPost().
+            should('be.visible').
+            get('.col__name > button.user-popover').
+            contains(incomingWebhookConfiguration.display_name);
+        cy.getLastPost().
+            should('be.visible').
+            get('.col__name .BotBadge > span').
+            contains('BOT');
+        cy.getLastPost().
+            get('.post-message__text').
+            contains(incomingWebhookText);
     });
 });
