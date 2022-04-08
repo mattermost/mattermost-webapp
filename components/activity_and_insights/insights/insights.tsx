da86d2a7a3731a6d9ad79c9e25a203370415a4a0
Ben Cooke <benkcooke@gmail.com>
2022-04-08 12:26:22 -0400

    [MM-42738] Filter and time picker for insights (#10086)
    * adding landing page
    
    * fixing some stuff for sidebar menu item
    
    * fixing lint
    
    * updating tests
    
    * Adding filter for header
    
    * translations
    
    * fixing types and translations
    
    * fixing lint and types
    
    * adding time dropdown
    
    * fixing lint and translations
    
    * fixing lint
    
    * fixing page title and hightlighting issue
    
    * name change
    
    * adding translation
    
    * fixing menu item errors
    
    * adding constants for time frames
    
    * fixing default
    
    * fixing styles
    
    * fixing lint
    
    * adding font-family
    
    Co-authored-by: Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>
    Co-authored-by: Benjamin Cooke <benjamincooke@Benjamins-MBP.ht.home>

--- components/activity_and_insights/insights/insights.tsx
+++ components/activity_and_insights/insights/insights.tsx
@@ -1,16 +1,37 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React, {memo, useEffect} from 'react';
+import React, {memo, useState, useCallback, useEffect} from 'react';
 import {useDispatch} from 'react-redux';
 
 import {selectChannel} from 'mattermost-redux/actions/channels';
 
-import TopChannels from './top_channels/top_channels';
+import {InsightsTimeFrames} from 'utils/constants';
+import {localizeMessage} from 'utils/utils';
+
+import InsightsHeader from './insights_header/insights_header';
 
 import './../activity_and_insights.scss';
 
 const Insights = () => {
     const dispatch = useDispatch();
+
+    const [filterType, setFilterType] = useState('my');
+    const [timeFrame, setTimeFrame] = useState({
+        value: InsightsTimeFrames.INSIGHTS_7_DAYS,
+        label: localizeMessage('insights.timeFrame.mediumRange', 'Last 7 days'),
+    });
+
+    const setFilterTypeTeam = useCallback(() => {
+        setFilterType('team');
+    }, []);
+
+    const setFilterTypeMy = useCallback(() => {
+        setFilterType('my');
+    }, []);
+
+    const setTimeFrameValue = useCallback((value) => {
+        setTimeFrame(value);
+    }, []);
 
     useEffect(() => {
         dispatch(selectChannel(''));
@@ -18,7 +39,13 @@ const Insights = () => {
 
     return (
         <>
-            <TopChannels/>
+            <InsightsHeader
+                filterType={filterType}
+                setFilterTypeTeam={setFilterTypeTeam}
+                setFilterTypeMy={setFilterTypeMy}
+                timeFrame={timeFrame}
+                setTimeFrame={setTimeFrameValue}
+            />
         </>
     );
 };
