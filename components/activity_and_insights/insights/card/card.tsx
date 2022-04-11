18d74a55b3e23a244aa397289be03acfb5f59a6f
Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
2022-04-11 16:25:35 -0400

    fixing styles

--- components/activity_and_insights/insights/card/card.tsx
+++ components/activity_and_insights/insights/card/card.tsx
@@ -39,12 +39,12 @@ const InsightsCard = (props: Props) => {
                         {props.subTitle}
                     </div>
                 </div>
-                <span className='icon'>
+                <button className='icon'>
                     <Icon
                         size={16}
                         glyph={'chevron-right'}
                     />
-                </span>
+                </button>
             </CardHeader>
             <div
                 className='Card__body expanded'
