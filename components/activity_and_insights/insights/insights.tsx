9c51df4cde8f105bc60fb46e95e83041bfa8a6d8
Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
2022-04-12 13:11:11 -0400

    fixing default time value

--- components/activity_and_insights/insights/insights.tsx
+++ components/activity_and_insights/insights/insights.tsx
@@ -26,7 +26,7 @@ const Insights = () => {
     const [filterType, setFilterType] = useState(InsightsScopes.MY);
     const [timeFrame, setTimeFrame] = useState({
         value: InsightsTimeFrames.INSIGHTS_7_DAYS,
-        label: localizeMessage('insights.timeFrame.mediumRange', 'Today'),
+        label: localizeMessage('insights.timeFrame.mediumRange', 'Last 7 days'),
     });
 
     const setFilterTypeTeam = useCallback(() => {
