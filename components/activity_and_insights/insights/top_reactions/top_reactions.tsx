68bfa30369f0b5e4bf2a5a1af6b31fff7ad02912
Benjamin Cooke <benjamincooke@Benjamins-MBP.ht.home>
2022-04-13 17:13:18 -0400

    adding styles for chart

--- components/activity_and_insights/insights/top_reactions/top_reactions.tsx
+++ components/activity_and_insights/insights/top_reactions/top_reactions.tsx
@@ -1,62 +1,104 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React, {memo, useEffect} from 'react';
+import React, {memo, useEffect, useState, useCallback} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 
 import {getTopReactionsForTeam} from 'mattermost-redux/actions/teams';
 import {getCurrentTeamId, getTopReactionsForCurrentTeam} from 'mattermost-redux/selectors/entities/teams';
 import {GlobalState} from 'mattermost-redux/types/store';
 
+import RenderEmoji from 'components/emoji/render_emoji';
 import BarChartLoader from '../skeleton_loader/bar_chart_loader/bar_chart_loader';
 import CircleLoader from '../skeleton_loader/circle_loader/circle_loader';
-import widgetHoc, { WidgetHocProps } from '../widget_hoc/widget_hoc';
+import widgetHoc, {WidgetHocProps} from '../widget_hoc/widget_hoc';
 
 import './../../activity_and_insights.scss';
-import {TimeFrames} from 'mattermost-redux/types/insights';
 
 const TopReactions = (props: WidgetHocProps) => {
     const dispatch = useDispatch();
+
+    const [loading, setLoading] = useState(true);
+
     const topTeamReactions = useSelector((state: GlobalState) => getTopReactionsForCurrentTeam(state, props.timeFrame));
     const currentTeamId = useSelector(getCurrentTeamId);
 
-    console.log(topTeamReactions);
+    const getTopTeamReactions = useCallback(async () => {
+        setLoading(true);
+        await dispatch(getTopReactionsForTeam(currentTeamId, 0, 10, props.timeFrame));
+        setLoading(false);
+    }, [props.timeFrame, currentTeamId])
 
     useEffect(() => {
-        dispatch(getTopReactionsForTeam(currentTeamId, 0, 10, props.timeFrame));
-    }, [props.timeFrame, currentTeamId]);
+        getTopTeamReactions();
+    }, [getTopTeamReactions]);
 
     return (
         <div className='top-reaction-container'>
-            <div className='bar-chart-entry'>
-                <BarChartLoader/>
-                <CircleLoader
-                    size={20}
-                />
-            </div>
-            <div className='bar-chart-entry'>
-                <BarChartLoader/>
-                <CircleLoader
-                    size={20}
-                />
-            </div>
-            <div className='bar-chart-entry'>
-                <BarChartLoader/>
-                <CircleLoader
-                    size={20}
-                />
-            </div>
-            <div className='bar-chart-entry'>
-                <BarChartLoader/>
-                <CircleLoader
-                    size={20}
-                />
-            </div>
-            <div className='bar-chart-entry'>
-                <BarChartLoader/>
-                <CircleLoader
-                    size={20}
-                />
-            </div>
+            {loading && 
+                <>
+                    <div className='bar-chart-entry'>
+                        <BarChartLoader/>
+                        <CircleLoader
+                            size={20}
+                        />
+                    </div>
+                    <div className='bar-chart-entry'>
+                        <BarChartLoader/>
+                        <CircleLoader
+                            size={20}
+                        />
+                    </div>
+                    <div className='bar-chart-entry'>
+                        <BarChartLoader/>
+                        <CircleLoader
+                            size={20}
+                        />
+                    </div>
+                    <div className='bar-chart-entry'>
+                        <BarChartLoader/>
+                        <CircleLoader
+                            size={20}
+                        />
+                    </div>
+                    <div className='bar-chart-entry'>
+                        <BarChartLoader/>
+                        <CircleLoader
+                            size={20}
+                        />
+                    </div>
+                </>
+            }
+            {
+                topTeamReactions &&
+                topTeamReactions.map((reaction) => {
+                    const highestCount = topTeamReactions[0].count;
+                    const maxHeight = 156;
+
+                    let barHeight = reaction.count/highestCount * maxHeight;
+
+                    if (highestCount === reaction.count) {
+                        barHeight = maxHeight;
+                    }
+
+                    return (
+                        <div className='bar-chart-entry'>
+                            <span className='reaction-count'>{reaction.count}</span>
+                            <div 
+                                className='bar-chart-data'
+                                style={{
+                                    height: `${barHeight}px`,
+                                }}
+                            />
+                            <RenderEmoji
+                                emojiName={reaction.emoji_name}
+                                size={20}
+                            />
+                        </div>
+                    );
+                })
+                
+            }
+            
         </div>
     );
 };
