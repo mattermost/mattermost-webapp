a9721854f518509c015030a80b7e90b80d31fcaf
Ashish Dhama <ashishdhama3@gmail.com>
2022-04-18 14:49:51 +0530

    fix name typo

--- components/command_palette/index.ts
+++ components/command_palette/index.ts
@@ -1,6 +1,4 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export function CommandPallet() {
-    return null;
-}
+export {default} from './command_palatte';
