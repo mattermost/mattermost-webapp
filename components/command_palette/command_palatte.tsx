52f6a25f7b315b72403196d9ed015c0cdcf7c439
Ashish Dhama <ashishdhama3@gmail.com>
2022-04-18 14:49:51 +0530

    fix name typo

--- components/command_palette/command_palatte.tsx
+++ components/command_palette/command_palatte.tsx
@@ -1,4 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export {CommandPallet} from './command_pallet';
+function CommandPalette() {
+    return null;
+}
+
+export default CommandPalette;
