9e0b12d68fd1a1e409e31c262a54039685738840
neallred <neallred@protonmail.com>
2022-04-19 10:42:01 -0500

    update import references

--- packages/mattermost-redux/src/selectors/entities/cloud.ts
+++ packages/mattermost-redux/src/selectors/entities/cloud.ts
@@ -1,8 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {Limits} from 'mattermost-redux/types/cloud';
-import {GlobalState} from 'mattermost-redux/types/store';
+import {Limits} from '@mattermost/types/cloud';
+import {GlobalState} from '@mattermost/types/store';
 
 export function getCloudLimits(state: GlobalState): Limits {
     return state.entities.cloud.limits;
