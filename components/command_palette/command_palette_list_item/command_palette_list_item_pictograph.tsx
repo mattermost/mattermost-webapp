b4f244d4bd450e1f0e7c3f28d711ef245a811886
Ashish Dhama <ashishdhama3@gmail.com>
2022-04-21 18:40:15 +0530

    Command palette : Add command palette list, item and channel to command palette transformer (#10219)
    * add command palette list, item and channel to command palette transformer
    
    * add feature flag for command palette keyboard shortcut

---
+++ components/command_palette/command_palette_list_item/command_palette_list_item_pictograph.tsx
@@ -0,0 +1,48 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+import React from 'react';
+import classNames from 'classnames';
+
+import RenderEmoji from 'components/emoji/render_emoji';
+import ProfilePicture from 'components/profile_picture';
+
+export enum CmdPalettePictographType {
+    IMAGE= 'image',
+    ICON= 'icon',
+    EMOJI= 'emoji',
+    TEXT= 'text',
+}
+
+export enum CmdPalettePictographIcon{
+    ARCHIVE = 'icon-archive-outline',
+    PENCIl = 'icon-pencil-outline',
+    GLOBE = 'icon-globe',
+    LOCK = 'icon-lock-outline',
+}
+
+export interface CmdPalettePictograph{
+    type: CmdPalettePictographType;
+    pictographItem: CmdPalettePictographIcon | string;
+    userStatus?: string;
+}
+
+export const CommandPaletteListItemPictograph = ({type, pictographItem, userStatus}: CmdPalettePictograph) => {
+    if (type === CmdPalettePictographType.ICON) {
+        return <i className={classNames('cmd-pl-list-item__icon', pictographItem)}/>;
+    } else if (type === CmdPalettePictographType.TEXT) {
+        return <div className='status status--group'>{pictographItem}</div>;
+    } else if (type === CmdPalettePictographType.IMAGE) {
+        return (
+            <ProfilePicture
+                src={pictographItem}
+                status={userStatus}
+                size='sm'
+            />);
+    } else if (type === CmdPalettePictographType.EMOJI) {
+        return (
+            <RenderEmoji
+                emojiName={pictographItem}
+            />);
+    }
+    return null;
+};
