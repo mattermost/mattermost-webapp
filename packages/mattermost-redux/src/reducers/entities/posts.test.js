b30702319045ed720f954aa33f520327ffc1b350
koox00 <3829551+koox00@users.noreply.github.com>
2022-04-29 15:22:57 +0300

    Fixes bug when receiving a new reply you lose previous ones
    If the postsInThread state is empty, when you received a new post it
    would be added in, but then fetching new replies would fetch replies
    after that one loosing all pre-existing ones.

--- packages/mattermost-redux/src/reducers/entities/posts.test.js
+++ packages/mattermost-redux/src/reducers/entities/posts.test.js
@@ -2831,6 +2831,29 @@ describe('mergePostOrder', () => {
 });
 
 describe('postsInThread', () => {
+    it('should store a comment without other comments loaded for the thread when RECEIVED_POST', () => {
+        const state = deepFreeze({});
+        const nextState = reducers.postsInThread(state, {
+            type: PostTypes.RECEIVED_POST,
+            data: {id: 'comment1', root_id: 'root1'},
+        });
+
+        expect(nextState).not.toBe(state);
+        expect(nextState).toEqual({
+            root1: ['comment1'],
+        });
+    });
+
+    it('should NOT store a comment without other comments loaded for the thread when RECEIVED_NEW_POST', () => {
+        const state = deepFreeze({});
+        const nextState = reducers.postsInThread(state, {
+            type: PostTypes.RECEIVED_NEW_POST,
+            data: {id: 'comment1', root_id: 'root1'},
+        });
+
+        expect(nextState).toBe(state);
+    });
+
     for (const actionType of [
         PostTypes.RECEIVED_POST,
         PostTypes.RECEIVED_NEW_POST,
@@ -2881,20 +2904,6 @@ describe('postsInThread', () => {
                 expect(nextState).not.toBe(state);
                 expect(nextState).toEqual({
                     root1: ['comment1', 'comment2', 'comment3'],
-                });
-            });
-
-            it('should store a comment without other comments loaded for the thread', () => {
-                const state = deepFreeze({});
-
-                const nextState = reducers.postsInThread(state, {
-                    type: actionType,
-                    data: {id: 'comment1', root_id: 'root1'},
-                });
-
-                expect(nextState).not.toBe(state);
-                expect(nextState).toEqual({
-                    root1: ['comment1'],
                 });
             });
 
--- packages/mattermost-redux/src/reducers/entities/posts.ts
+++ packages/mattermost-redux/src/reducers/entities/posts.ts
@@ -899,7 +899,48 @@ export function mergePostOrder(left: string[], right: string[], posts: Record<st
 
 export function postsInThread(state: RelationOneToMany<Post, Post> = {}, action: GenericAction, prevPosts: Record<string, Post>) {
     switch (action.type) {
-    case PostTypes.RECEIVED_NEW_POST:
+    case PostTypes.RECEIVED_NEW_POST: {
+        const post = action.data;
+
+        if (!post.root_id) {
+            // Only store comments, not the root post
+            return state;
+        }
+
+        const postsForThread = state[post.root_id];
+        if (!postsForThread) {
+            // Don't save newly created replies until the thread has been loaded
+            return state;
+        }
+
+        const nextPostsForThread = [...postsForThread];
+
+        let changed = false;
+
+        if (!postsForThread.includes(post.id)) {
+            nextPostsForThread.push(post.id);
+            changed = true;
+        }
+
+        // If this is a new non-pending post, remove any pending post that exists for it
+        if (post.pending_post_id && post.id !== post.pending_post_id) {
+            const index = nextPostsForThread.indexOf(post.pending_post_id);
+
+            if (index !== -1) {
+                nextPostsForThread.splice(index, 1);
+                changed = true;
+            }
+        }
+
+        if (!changed) {
+            return state;
+        }
+
+        return {
+            ...state,
+            [post.root_id]: nextPostsForThread,
+        };
+    }
     case PostTypes.RECEIVED_POST: {
         const post = action.data;
 
