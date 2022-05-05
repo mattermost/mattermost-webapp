d7eac7ef360f6c2d9aba7091367458916a4c3721
Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
2022-05-05 14:38:03 -0400

    fixing bug where reactions were not clearing

--- packages/mattermost-redux/src/reducers/entities/insights.ts
+++ packages/mattermost-redux/src/reducers/entities/insights.ts
@@ -6,12 +6,8 @@ import {InsightTypes} from 'mattermost-redux/action_types';
 import {GenericAction} from 'mattermost-redux/types/actions';
 import {TopReaction, TimeFrame} from 'mattermost-redux/types/insights';
 
-const sortReactionsIntoState = (existingState: Record<TimeFrame, Record<string, TopReaction>>, data: TopReaction[], timeFrame: TimeFrame) => {
-    if (!existingState || !existingState[timeFrame]) {
-        existingState[timeFrame] = {};
-    }
-
-    const newItems = {...existingState[timeFrame] || {}};
+const sortReactionsIntoState = (data: TopReaction[], timeFrame: TimeFrame) => {
+    const newItems: Record<string, TopReaction> = {};
 
     for (let i = 0; i < data.length; i++) {
         const emojiObj = data[i];
@@ -24,11 +20,10 @@ const sortReactionsIntoState = (existingState: Record<TimeFrame, Record<string, 
 function topReactions(state: Record<string, Record<TimeFrame, Record<string, TopReaction>>> = {}, action: GenericAction) {
     switch (action.type) {
     case InsightTypes.RECEIVED_TOP_REACTIONS: {
-        const reactions = {...(state[action.id] || {})};
         const results = action.data.data.items || [];
         const timeFrame = action.data.timeFrame as TimeFrame;
 
-        const newItems = sortReactionsIntoState(reactions, results, timeFrame);
+        const newItems = sortReactionsIntoState(results, timeFrame);
 
         return {
             ...state,
@@ -46,11 +41,10 @@ function topReactions(state: Record<string, Record<TimeFrame, Record<string, Top
 function myTopReactions(state: Record<string, Record<TimeFrame, Record<string, TopReaction>>> = {}, action: GenericAction) {
     switch (action.type) {
     case InsightTypes.RECEIVED_MY_TOP_REACTIONS: {
-        const reactions = {...(state[action.id] || {})};
         const results = action.data.data.items || [];
         const timeFrame = action.data.timeFrame as TimeFrame;
 
-        const newItems = sortReactionsIntoState(reactions, results, timeFrame);
+        const newItems = sortReactionsIntoState(results, timeFrame);
 
         return {
             ...state,
