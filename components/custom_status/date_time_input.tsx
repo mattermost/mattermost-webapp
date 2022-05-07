5e27d2326b3267634ae52401692b5f343067b29e
Rohan Kakar <40962760+RKRohk@users.noreply.github.com>
2022-05-07 16:40:12 +0530

    Fix lint issues

--- components/custom_status/date_time_input.tsx
+++ components/custom_status/date_time_input.tsx
@@ -143,7 +143,7 @@ const DateTimeInputContainer: React.FC<Props> = (props: Props) => {
                     <i className='icon-calendar-outline'/>
                 </span>
                 <DayPickerInput
-                    value={time.toDate().toLocaleDateString("fr-CA")}
+                    value={time.toDate().toLocaleDateString('fr-CA')}
                     onDayChange={handleDayChange}
                     inputProps={{
                         readOnly: true,
