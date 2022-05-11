f6a6626bb5f151e9e0a02e7bf97aba324bdfdfb1
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2022-05-11 13:58:20 -0400

    add test for integrations on subscriptions page

--- components/admin_console/billing/billing_subscriptions/limits.test.tsx
+++ components/admin_console/billing/billing_subscriptions/limits.test.tsx
@@ -9,6 +9,11 @@ import * as redux from 'react-redux';
 
 import {renderWithIntl} from 'tests/react_testing_utils';
 
+import {InstalledIntegration} from '@mattermost/types/integrations';
+import {GlobalState} from '@mattermost/types/store';
+
+import {getEnabledIntegrations} from 'mattermost-redux/selectors/entities/integrations';
+
 import * as cloudActions from 'actions/cloud';
 
 import {FileSizes} from 'utils/file_utils';
@@ -34,16 +39,43 @@ const limits = {
     },
 };
 
+const installedIntegrations: InstalledIntegration[] = [
+    {
+        id: 'github',
+        enabled: true,
+        name: 'GitHub',
+        type: 'plugin',
+        version: '2.1.1',
+    },
+    {
+        id: 'jira',
+        enabled: false,
+        name: 'Jira',
+        type: 'plugin',
+        version: '3.2.0',
+    },
+];
+
+const integrationsState = {
+    entities: {
+        integrations: {
+            installedIntegrations,
+        },
+    },
+} as GlobalState;
+
 function mockUseSelector(spy: jest.SpyInstance, hasLimits: boolean) {
     // initial render
     spy.mockImplementationOnce(() => hasLimits);
     spy.mockImplementationOnce(() => (hasLimits ? limits : {}));
     spy.mockImplementationOnce(() => hasLimits);
+    spy.mockImplementationOnce(() => []);
 
     // after effect
     spy.mockImplementationOnce(() => hasLimits);
     spy.mockImplementationOnce(() => (hasLimits ? limits : {}));
     spy.mockImplementationOnce(() => hasLimits);
+    spy.mockImplementationOnce(() => getEnabledIntegrations(integrationsState));
 }
 
 describe('Limits', () => {
@@ -65,6 +97,16 @@ describe('Limits', () => {
         renderWithIntl(<Limits/>);
         screen.getByText('File Storage');
         screen.getByText(/of 10GB/);
+    });
+
+    test('enabled integration count is shown', () => {
+        jest.spyOn(redux, 'useDispatch').mockImplementation(jest.fn(() => jest.fn()));
+        const spy = jest.spyOn(redux, 'useSelector');
+        mockUseSelector(spy, true);
+
+        renderWithIntl(<Limits/>);
+        screen.getByText('Enabled Integrations');
+        screen.getByText('1 of 10 integrations (10%)');
     });
 
     test('requests limits when cloud free feature is enabled', () => {
