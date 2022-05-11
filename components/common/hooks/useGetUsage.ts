84575f263dd1cf02f5bc8e466eeedd57fb89b46e
neallred <neallred@protonmail.com>
2022-05-11 13:49:56 -0500

    revert admin specific usage

--- components/common/hooks/useGetUsage.ts
+++ components/common/hooks/useGetUsage.ts
@@ -4,9 +4,6 @@
 import {useMemo} from 'react';
 import {useSelector} from 'react-redux';
 
-import {GlobalState} from 'types/store';
-import {AnalyticsRow} from '@mattermost/types/admin';
-import Constants from 'utils/constants';
 import {FileSizes} from 'utils/file_utils';
 
 export interface CloudUsage {
@@ -25,16 +22,6 @@ export interface CloudUsage {
     };
 }
 
-function getStatValue(stat: number | AnalyticsRow[]): number | undefined {
-    if (typeof stat === 'number') {
-        return stat;
-    }
-    if (!stat || stat.length === 0) {
-        return undefined;
-    }
-    return stat[0].value;
-}
-
 // TODO: Replace this with actual usages stored in redux,
 // that ideally are updated with a websocket event in near real time.
 export default function useGetUsage(): CloudUsage {
@@ -42,16 +29,15 @@ export default function useGetUsage(): CloudUsage {
     const boardsCards = useSelector(() => 400);
     const boardsViews = useSelector(() => 2);
     const integrationsEnabled = useSelector(() => 3);
-    const adminStats = useSelector((state: GlobalState) => state.entities.admin.analytics);
+    const messageHistory = useSelector(() => 6000);
 
-    const messageHistory = getStatValue(adminStats![Constants.StatTypes.TOTAL_POSTS]);
     const usage = useMemo(() => (
         {
             files: {
                 totalStorage,
             },
             messages: {
-                history: messageHistory || 0,
+                history: messageHistory,
             },
             boards: {
                 cards: boardsCards,
