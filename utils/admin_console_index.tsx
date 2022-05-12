5dfc46ea03dd458c144cf8d276e7fc0d47f7a78d
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-05-12 21:43:59 +0000

    eslint disable comment is deleted

--- utils/admin_console_index.tsx
+++ utils/admin_console_index.tsx
@@ -1,6 +1,5 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 import FlexSearch from 'flexsearch/dist/flexsearch.es5';
 import {IntlShape} from 'react-intl';
 
