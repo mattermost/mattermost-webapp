8b81ee17f2cf5a0941c0f00d5351914645f1ad09
Harrison Healey <harrisonmhealey@gmail.com>
2022-05-16 13:40:44 -0400

    MM-43187 Move most internal stuff out of types package (#10292)
    (cherry picked from commit a93fda4aa87de490e890225ae09f854d9043bcb1)

--- packages/types/src/schemes.ts
+++ packages/types/src/schemes.ts
@@ -1,5 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
+
 export type SchemeScope = 'team' | 'channel';
 export type Scheme = {
     id: string;
