8c0d39ac9379ad4d048a88767fc2b54e3763c263
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2022-05-16 12:05:26 +0200

    Render channel header components in the App Bar
    When the App Bar is shown, the buttons in the channel header are no
    longer rendered there, so they need to be rendered in the App Bar
    instead. This commit does that, and takes care of an explicit order in
    the icons, so that Playbooks and Boards icons are the first ones, then
    the explicitly registered App Bar icons, and finally the channel header
    icons.

--- components/app_bar/app_bar.tsx
+++ components/app_bar/app_bar.tsx
@@ -6,7 +6,7 @@ import {useSelector} from 'react-redux';
 
 import {getAppBarAppBindings} from 'mattermost-redux/selectors/entities/apps';
 
-import {getAppBarPluginComponents, shouldShowAppBar} from 'selectors/plugins';
+import {getAppBarPluginComponents, getChannelHeaderPluginComponents, shouldShowAppBar} from 'selectors/plugins';
 
 import AppBarPluginComponent from './app_bar_plugin_component';
 import AppBarBinding from './app_bar_binding';
@@ -14,6 +14,7 @@ import AppBarBinding from './app_bar_binding';
 import './app_bar.scss';
 
 export default function AppBar() {
+    const channelHeaderComponents = useSelector(getChannelHeaderPluginComponents);
     const appBarPluginComponents = useSelector(getAppBarPluginComponents);
     const appBarBindings = useSelector(getAppBarAppBindings);
 
@@ -23,9 +24,40 @@ export default function AppBar() {
         return null;
     }
 
+    let playbooksComponent;
+    let boardsComponent;
+
+    // Extract the Playbooks and Boards components so they can be reordered at the beginning of the list
+    const filteredComponents = appBarPluginComponents.filter((component) => {
+        switch (component.pluginId) {
+        case 'playbooks':
+            playbooksComponent = component;
+            return false;
+
+        case 'focalboard':
+            boardsComponent = component;
+            return false;
+
+        default:
+            return true;
+        }
+    });
+
+    const pluginComponents = filteredComponents.concat(channelHeaderComponents);
+
     return (
         <div className={'app-bar'}>
-            {appBarPluginComponents.map((component) => (
+            {playbooksComponent &&
+            <AppBarPluginComponent
+                component={playbooksComponent}
+            />
+            }
+            {boardsComponent &&
+            <AppBarPluginComponent
+                component={boardsComponent}
+            />
+            }
+            {pluginComponents.map((component) => (
                 <AppBarPluginComponent
                     key={component.id}
                     component={component}
