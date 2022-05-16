a93fda4aa87de490e890225ae09f854d9043bcb1
Harrison Healey <harrisonmhealey@gmail.com>
2022-05-16 13:40:44 -0400

    MM-43187 Move most internal stuff out of types package (#10292)

--- components/admin_console/team_channel_settings/channel/details/types.ts
+++ components/admin_console/team_channel_settings/channel/details/types.ts
@@ -1,8 +1,4 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export type Typing = {
-    [x: string]: {
-        [x: string]: number;
-    };
-};
+export type ChannelModerationRoles = 'members' | 'guests';
