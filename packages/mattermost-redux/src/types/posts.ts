8d5ff32d5f8009b7721e210c8ec0a8b3f13bfa5d
Mattermost Build <build@mattermost.com>
2022-05-17 18:52:54 +0200

    MM-43187 Move most internal stuff out of types package (#10292) (#10319)
    (cherry picked from commit a93fda4aa87de490e890225ae09f854d9043bcb1)
    
    Co-authored-by: Harrison Healey <harrisonmhealey@gmail.com>

--- packages/mattermost-redux/src/types/posts.ts
+++ packages/mattermost-redux/src/types/posts.ts
@@ -1,4 +1,22 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {Post} from '@mattermost/types/posts';
+
 export * from '@mattermost/types/posts';
+
+export type UserActivityPost = Post & {
+    system_post_ids: string[];
+    user_activity_posts: Post[];
+}
+
+export type PostWithFormatData = Post & {
+    isFirstReply: boolean;
+    isLastReply: boolean;
+    previousPostIsComment: boolean;
+    commentedOnPost?: Post;
+    consecutivePostByUser: boolean;
+    replyCount: number;
+    isCommentMention: boolean;
+    highlight: boolean;
+};
