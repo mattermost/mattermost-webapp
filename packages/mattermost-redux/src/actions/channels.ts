37d7b7f9f928a639c8fb411d894a24a5fdc4bb2f
Ashish Dhama <ashishdhama3@gmail.com>
2022-05-23 15:06:10 +0530

    add loop for all team channel members api

--- packages/mattermost-redux/src/actions/channels.ts
+++ packages/mattermost-redux/src/actions/channels.ts
@@ -525,7 +525,24 @@ export function fetchAllMyTeamsChannelsAndChannelMembers(): ActionFunc {
         const state = getState();
         const {currentUserId} = state.entities.users;
         let channels;
-        let channelsMembers;
+        let channelsMembers: ChannelMembership[] = [];
+        let allMembers = true;
+        let page = 0;
+        do {
+            try {
+                await Client4.getAllChannelsMembers(currentUserId, page, 100).then((data) => {
+                    channelsMembers = [...channelsMembers, ...data];
+                    page++;
+                    if (data.length < 100) {
+                        allMembers = false;
+                    }
+                });
+            } catch (error) {
+                forceLogoutIfNecessary(error, dispatch, getState);
+                dispatch(logError(error));
+                return {error};
+            }
+        } while (allMembers);
         try {
             channels = await Client4.getAllTeamsChannels();
             channelsMembers = await Client4.getAllChannelsMembers(currentUserId);
