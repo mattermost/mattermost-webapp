445bb3fd4163e6a4c10cfb2383d3aca6eadb83ba
Harrison Healey <harrisonmhealey@gmail.com>
2022-05-24 17:08:30 -0400

    MM-43188 Remove mattermost-redux/types/saml

--- packages/mattermost-redux/src/reducers/entities/admin.ts
+++ packages/mattermost-redux/src/reducers/entities/admin.ts
@@ -14,7 +14,7 @@ import {Compliance} from '@mattermost/types/compliance';
 import {AdminConfig, EnvironmentConfig} from '@mattermost/types/config';
 import {MixedUnlinkedGroupRedux} from '@mattermost/types/groups';
 import {PluginRedux, PluginStatusRedux} from '@mattermost/types/plugins';
-import {SamlCertificateStatus, SamlMetadataResponse} from 'mattermost-redux/types/saml';
+import {SamlCertificateStatus, SamlMetadataResponse} from '@mattermost/types/saml';
 import {Team} from 'mattermost-redux/types/teams';
 import {UserAccessToken, UserProfile} from 'mattermost-redux/types/users';
 import {RelationOneToOne, IDMappedObjects} from 'mattermost-redux/types/utilities';
