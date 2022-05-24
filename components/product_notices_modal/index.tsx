927c75bf110857c3aeac9b945217f963540adcf5
Harrison Healey <harrisonmhealey@gmail.com>
2022-05-24 17:05:23 -0400

    MM-43188 Remove mattermost-redux/types/product_notices

--- components/product_notices_modal/index.tsx
+++ components/product_notices_modal/index.tsx
@@ -5,7 +5,7 @@ import {connect} from 'react-redux';
 import {bindActionCreators, Dispatch, ActionCreatorsMapObject} from 'redux';
 
 import {ActionFunc} from 'mattermost-redux/types/actions';
-import {ProductNotices} from 'mattermost-redux/types/product_notices';
+import {ProductNotices} from '@mattermost/types/product_notices';
 import {WebsocketStatus} from 'mattermost-redux/types/websocket';
 import {getInProductNotices, updateNoticesAsViewed} from 'mattermost-redux/actions/teams';
 import {getCurrentTeamId} from 'mattermost-redux/selectors/entities/teams';
