ccfce1bd1b0601ef496fe1b26e266c0a535aadc4
Nick Misasi <nick.misasi@mattermost.com>
2022-05-24 23:14:40 -0400

    Working example, little weird with refreshes

--- components/admin_console/team_channel_settings/team/details/team_profile.jsx
+++ components/admin_console/team_channel_settings/team/details/team_profile.jsx
@@ -5,7 +5,15 @@ import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {FormattedMessage} from 'react-intl';
 import classNames from 'classnames';
+import {useDispatch} from 'react-redux';
 
+import Tooltip from 'components/tooltip';
+
+import {ModalIdentifiers} from 'utils/constants';
+import {openModal} from 'actions/views/modals';
+import PricingModal from 'components/pricing_modal';
+
+import OverlayTrigger from 'components/overlay_trigger';
 import useGetUsageDeltas from 'components/common/hooks/useGetUsageDeltas';
 import {t} from 'utils/i18n';
 
@@ -13,32 +21,36 @@ import AdminPanel from 'components/widgets/admin_console/admin_panel';
 import FormattedMarkdownMessage from 'components/formatted_markdown_message.jsx';
 import ArchiveIcon from 'components/widgets/icons/archive_icon';
 import UnarchiveIcon from 'components/widgets/icons/unarchive_icon';
-import OverlayTrigger from 'components/overlay_trigger';
-import Tooltip from 'components/tooltip';
 
 import * as Utils from 'utils/utils';
 
 import TeamIcon from 'components/widgets/team_icon/team_icon';
 
+import './team_profile.scss';
+
 export function TeamProfile({team, isArchived, isDisabled, onToggleArchive}) {
     const teamIconUrl = Utils.imageURLForTeam(team);
     const usageDeltas = useGetUsageDeltas();
-    const [unarchivedDisabled, setUnarchivedDisabled] = useState(false);
+    const dispatch = useDispatch();
+    const [restoreDisabled, setRestoreDisabled] = useState(false);
 
     useEffect(() => {
-        setUnarchivedDisabled(usageDeltas.teams.teamsLoaded && usageDeltas.teams.active >= 0);
-        console.log(unarchivedDisabled);
-    }, [usageDeltas]);
+        setRestoreDisabled(usageDeltas.teams.teamsLoaded && usageDeltas.teams.active >= 0 && isArchived);
+    }, [usageDeltas, isArchived]);
 
     let archiveBtnID;
     let archiveBtnDefault;
-    if (isArchived) {
+    if (restoreDisabled) {
+        archiveBtnID = t('admin.billing.subscription.upgrade');
+        archiveBtnDefault = 'Upgrade';
+    } else if (isArchived) {
         archiveBtnID = t('admin.team_settings.team_details.unarchiveTeam');
         archiveBtnDefault = 'Unarchive Team';
     } else {
         archiveBtnID = t('admin.team_settings.team_details.archiveTeam');
         archiveBtnDefault = 'Archive Team';
     }
+
     return (
         <AdminPanel
             id='team_profile'
@@ -81,20 +93,63 @@ export function TeamProfile({team, isArchived, isDisabled, onToggleArchive}) {
                     </div>
                     <div className='AdminChannelDetails_archiveContainer'>
                         <OverlayTrigger
-                            delayShow={400}
+                            delay={400}
                             placement='right'
-                            disabled={unarchivedDisabled === false}
+                            disabled={!restoreDisabled}
                             overlay={
                                 <Tooltip id='sharedTooltip'>
-                                    <div>
-                                        {'You\'ve reached the team limit for your current plan. Consider upgrading or deactivating other teams'}
-                                    </div>
+                                    <FormattedMessage
+                                        id={'workspace_limits.teams_limit_reached.tool_tip'}
+                                        defaultMessage={'You\'ve reached the team limit for your current plan. Consider upgrading or deactivating other teams'}
+                                    />
                                 </Tooltip>
                             }
                         >
-                            <button
+                            {/* See" https://github.com/react-bootstrap/react-bootstrap/issues/1588 - you can't use overlaytrigger with a disabled button.
+                                workaround: https://github.com/react-bootstrap/react-bootstrap/issues/2428#issuecomment-407800236
+
+                            */}
+                            {restoreDisabled ? <div
+                                onClick={() => {
+                                    dispatch(openModal({
+                                        modalId: ModalIdentifiers.PRICING_MODAL,
+                                        dialogType: PricingModal,
+                                    }));
+                                }}
+                                style={{display: 'inline-block', cursor: 'pointer'}}
+                            >
+                                <button
+                                    type='button'
+                                    disabled={restoreDisabled}
+                                    style={{pointerEvents: 'none'}}
+                                    className={
+                                        classNames(
+                                            'btn',
+                                            'btn-secondary',
+                                            'ArchiveButton',
+                                            {ArchiveButton___archived: isArchived},
+                                            {ArchiveButton___unarchived: !isArchived},
+                                            {disabled: isDisabled},
+                                            'cloud-limits-disabled',
+                                        )
+                                    }
+                                    onClick={onToggleArchive}
+                                >
+                                    {isArchived ? (
+                                        <UnarchiveIcon
+                                            className='channel-icon channel-icon__unarchive'
+                                        />
+                                    ) : (
+                                        <ArchiveIcon className='channel-icon channel-icon__archive'/>
+                                    )}
+                                    <FormattedMessage
+                                        id={archiveBtnID}
+                                        defaultMessage={archiveBtnDefault}
+                                    />
+                                </button>
+                            </div> : <button
                                 type='button'
-                                disabled={unarchivedDisabled}
+                                disabled={restoreDisabled}
                                 className={
                                     classNames(
                                         'btn',
@@ -103,6 +158,7 @@ export function TeamProfile({team, isArchived, isDisabled, onToggleArchive}) {
                                         {ArchiveButton___archived: isArchived},
                                         {ArchiveButton___unarchived: !isArchived},
                                         {disabled: isDisabled},
+                                        'cloud-limits-disabled',
                                     )
                                 }
                                 onClick={onToggleArchive}
@@ -119,6 +175,7 @@ export function TeamProfile({team, isArchived, isDisabled, onToggleArchive}) {
                                     defaultMessage={archiveBtnDefault}
                                 />
                             </button>
+                            }
                         </OverlayTrigger>
                     </div>
                 </div>
