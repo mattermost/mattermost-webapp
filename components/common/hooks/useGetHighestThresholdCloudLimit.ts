302b3ca772941a53d05f873822966dc8414a7f45
neallred <neallred@protonmail.com>
2022-05-25 16:50:05 -0500

    test hook

--- components/common/hooks/useGetHighestThresholdCloudLimit.ts
+++ components/common/hooks/useGetHighestThresholdCloudLimit.ts
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {useMemo} from 'react';
+
 import {CloudUsage, Limits} from '@mattermost/types/cloud';
 import {limitThresholds} from 'utils/limits';
 
@@ -17,7 +19,7 @@ export interface LimitSummary {
 
 function refineToDefined(...args: MaybeLimitSummary[]): LimitSummary[] {
     return args.reduce((acc: LimitSummary[], maybeLimitType: MaybeLimitSummary) => {
-        if (maybeLimitType.limit) {
+        if (maybeLimitType.limit !== undefined) {
             acc.push(maybeLimitType as LimitSummary);
         }
         return acc;
@@ -32,50 +34,51 @@ export const LimitTypes = {
 } as const;
 
 export default function useGetHighestThresholdCloudLimit(usage: CloudUsage, limits: Limits): LimitSummary | false {
-    if (Object.keys(limits).length === 0) {
-        return false;
-    }
-    const maybeMessageHistoryLimit = limits.messages?.history;
-    const messageHistoryUsage = usage.messages.history;
+    return useMemo(() => {
+        if (Object.keys(limits).length === 0) {
+            return false;
+        }
+        const maybeMessageHistoryLimit = limits.messages?.history;
+        const messageHistoryUsage = usage.messages.history;
 
-    const maybeBoardsCardsLimit = limits.boards?.cards;
-    const boardsCardsUsage = usage.boards.cards;
+        const maybeBoardsCardsLimit = limits.boards?.cards;
+        const boardsCardsUsage = usage.boards.cards;
 
-    const maybeFileStorageLimit = limits.files?.total_storage;
-    const fileStorageUsage = usage.files.totalStorage;
+        const maybeFileStorageLimit = limits.files?.total_storage;
+        const fileStorageUsage = usage.files.totalStorage;
 
-    const maybeEnabledIntegrationsLimit = limits.integrations?.enabled;
-    const enabledIntegrationsUsage = usage.integrations.enabled;
+        const maybeEnabledIntegrationsLimit = limits.integrations?.enabled;
+        const enabledIntegrationsUsage = usage.integrations.enabled;
 
-    // Order matters for this array. The designs specify:
-    // > Show the plan limit that is the highest.
-    // > Otherwise if there is a tie,
-    // > default to showing Message History first,
-    // > File storage second,
-    // > and App limit third.
-    const highestLimit = refineToDefined(
-        {
-            id: 'messageHistory',
-            limit: maybeMessageHistoryLimit,
-            usage: messageHistoryUsage,
-        },
-        {
-            id: 'fileStorage',
-            limit: maybeFileStorageLimit,
-            usage: fileStorageUsage,
-        },
-        {
-            id: 'enabledIntegrations',
-            limit: maybeEnabledIntegrationsLimit,
-            usage: enabledIntegrationsUsage,
-        },
-        {
-            id: 'boardsCards',
-            limit: maybeBoardsCardsLimit,
-            usage: boardsCardsUsage,
-        },
-    ).
-        reduce((acc: LimitSummary | false, curr: LimitSummary) => {
+        // Order matters for this array. The designs specify:
+        // > Show the plan limit that is the highest.
+        // > Otherwise if there is a tie,
+        // > default to showing Message History first,
+        // > File storage second,
+        // > and App limit third.
+        const highestLimit = refineToDefined(
+            {
+                id: LimitTypes.messageHistory,
+                limit: maybeMessageHistoryLimit,
+                usage: messageHistoryUsage,
+            },
+            {
+                id: LimitTypes.fileStorage,
+                limit: maybeFileStorageLimit,
+                usage: fileStorageUsage,
+            },
+            {
+                id: LimitTypes.enabledIntegrations,
+                limit: maybeEnabledIntegrationsLimit,
+                usage: enabledIntegrationsUsage,
+            },
+            {
+                id: LimitTypes.boardsCards,
+                limit: maybeBoardsCardsLimit,
+                usage: boardsCardsUsage,
+            },
+        ).
+            reduce((acc: LimitSummary | false, curr: LimitSummary) => {
             if (!acc) {
                 if (curr.limit && curr.limit > 0) {
                     return curr;
@@ -87,8 +90,9 @@ export default function useGetHighestThresholdCloudLimit(usage: CloudUsage, limi
             }
             return acc;
         }, false);
-    if (!highestLimit || (highestLimit.usage / highestLimit.limit) < (limitThresholds.warn / 100)) {
-        return false;
-    }
-    return highestLimit;
+        if (!highestLimit || (highestLimit.usage / highestLimit.limit) < (limitThresholds.warn / 100)) {
+            return false;
+        }
+        return highestLimit;
+    }, [usage, limits]);
 }
