89bd654e1a3af5dd90a08ef7340257ca37f3a826
Ashish Dhama <ashishdhama3@gmail.com>
2022-06-01 01:20:07 +0530

    Advanced Text Editor UX changes (#10384)
    * Move Fromatting toggle button and other uX changes
    
    * adde seperator after more icon and various UI changes
    
    * fix editor border issue on hover when focused and more button showing up if formatting bar is disabled
    
    * fix preview padding
    
    * update snapshot
    
    * fix lint
    
    * if channel read only remove send button, disable hover state and other UI changes
    
    * fix snapshot and lint
    
    * fix issue with attachment button if Advanced text editor is diasbled
    
    * fix padding issue after file preview in editor
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- components/advanced_create_post/advanced_create_post.tsx
+++ components/advanced_create_post/advanced_create_post.tsx
@@ -245,7 +245,6 @@ class AdvancedCreatePost extends React.PureComponent<Props, State> {
     private topDiv: React.RefObject<HTMLFormElement>;
     private textboxRef: React.RefObject<TextboxClass>;
     private fileUploadRef: React.RefObject<FileUploadClass>;
-    private createPostControlsRef: React.RefObject<HTMLSpanElement>;
 
     static getDerivedStateFromProps(props: Props, state: State): Partial<State> {
         let updatedState: Partial<State> = {
@@ -282,7 +281,6 @@ class AdvancedCreatePost extends React.PureComponent<Props, State> {
         this.topDiv = React.createRef<HTMLFormElement>();
         this.textboxRef = React.createRef<TextboxClass>();
         this.fileUploadRef = React.createRef<FileUploadClass>();
-        this.createPostControlsRef = React.createRef<HTMLSpanElement>();
     }
 
     componentDidMount() {
@@ -1000,10 +998,6 @@ class AdvancedCreatePost extends React.PureComponent<Props, State> {
         return null;
     }
 
-    getCreatePostControls = () => {
-        return this.createPostControlsRef.current;
-    }
-
     fillMessageFromHistory() {
         const lastMessage = this.props.messageInHistoryItem;
         if (lastMessage) {
@@ -1323,7 +1317,6 @@ class AdvancedCreatePost extends React.PureComponent<Props, State> {
                     shouldShowPreview={this.props.shouldShowPreview}
                     maxPostSize={this.props.maxPostSize}
                     canPost={this.props.canPost}
-                    createPostControlsRef={this.createPostControlsRef}
                     applyMarkdown={this.applyMarkdown}
                     useChannelMentions={this.props.useChannelMentions}
                     badConnection={this.props.badConnection}
@@ -1343,7 +1336,6 @@ class AdvancedCreatePost extends React.PureComponent<Props, State> {
                     handleEmojiClick={this.handleEmojiClick}
                     handleEmojiClose={this.handleEmojiClose}
                     hideEmojiPicker={this.hideEmojiPicker}
-                    getCreatePostControls={this.getCreatePostControls}
                     toggleAdvanceTextEditor={this.toggleAdvanceTextEditor}
                     handleUploadProgress={this.handleUploadProgress}
                     handleUploadError={this.handleUploadError}
