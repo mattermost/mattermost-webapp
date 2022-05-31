1e5353de35e371e03f48d66838f149937fb4db50
Nick Misasi <nick.misasi@mattermost.com>
2022-05-31 15:03:39 -0400

    Remove comment

--- components/announcement_bar/cloud_trial_ended_announcement_bar/cloud_trial_ended_announcement_bar.test.tsx
+++ components/announcement_bar/cloud_trial_ended_announcement_bar/cloud_trial_ended_announcement_bar.test.tsx
@@ -12,7 +12,6 @@ import {getPreferenceKey} from 'mattermost-redux/utils/preference_utils';
 
 import CloudTrialEndAnnouncementBar from './index';
 
-//
 describe('components/global/CloudTrialEndAnnouncementBar', () => {
     const useDispatchMock = jest.spyOn(reactRedux, 'useDispatch');
 
