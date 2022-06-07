22da4c78a32c4c5b28af729d5c7c03393015c480
Mattermost Build <build@mattermost.com>
2022-06-07 19:36:12 +0300

    MM44687: Add divider between the core products and the rest of icons (#10471) (#10507)
    * Add a divider between the products and the plugins
    
    * npm test -- -u app_bar
    
    * Update components/app_bar/app_bar.tsx
    
    Co-authored-by: Michael Kochell <6913320+mickmister@users.noreply.github.com>
    
    Co-authored-by: Michael Kochell <6913320+mickmister@users.noreply.github.com>
    (cherry picked from commit 2a9dbc6a5fea36498f265fc9c0b7f84226c1debf)
    
    Co-authored-by: Alejandro García Montoro <alejandro.garciamontoro@gmail.com>

--- components/app_bar/app_bar.tsx
+++ components/app_bar/app_bar.tsx
@@ -7,6 +7,7 @@ import {useSelector} from 'react-redux';
 import {getAppBarAppBindings} from 'mattermost-redux/selectors/entities/apps';
 
 import {getAppBarPluginComponents, getChannelHeaderPluginComponents, shouldShowAppBar} from 'selectors/plugins';
+import {PluginComponent} from 'types/store/plugins';
 
 import AppBarPluginComponent from './app_bar_plugin_component';
 import AppBarBinding from './app_bar_binding';
@@ -24,26 +25,32 @@ export default function AppBar() {
         return null;
     }
 
-    // Order the plugin components so that Playbooks is the first one,
-    // Boards is the second one, and the others remain in the same place.
-    const orderedAppBarPluginComponents = [...appBarPluginComponents];
-    orderedAppBarPluginComponents.sort((a, b) => {
-        switch (a.pluginId) {
-        case 'playbooks':
-            return -1;
+    const coreProductsIds = ['playbooks', 'focalboard'];
 
-        case 'focalboard':
-            return b.pluginId === 'playbooks' ? 1 : -1;
+    // The type guard in the filter (which removes all undefined elements) is needed for
+    // Typescript to correctly type coreProducts.
+    const coreProducts = coreProductsIds.
+        map((id) => appBarPluginComponents.find((element) => element.pluginId === id)).
+        filter((element): element is PluginComponent => Boolean(element));
 
-        default:
-            return 1;
-        }
-    });
+    const appBarPluginComponentsWithoutCoreProducts = appBarPluginComponents.filter((element) => !coreProductsIds.includes(element.pluginId));
+    const pluginComponents = appBarPluginComponentsWithoutCoreProducts.concat(channelHeaderComponents);
 
-    const pluginComponents = orderedAppBarPluginComponents.concat(channelHeaderComponents);
+    const isCoreProductsSectionNonEmpty = coreProducts.length > 0;
+    const isRegularIconsSectionNonEmpty = pluginComponents.length > 0 || appBarBindings.length > 0;
+    const isDividerVisible = isCoreProductsSectionNonEmpty && isRegularIconsSectionNonEmpty;
 
     return (
         <div className={'app-bar'}>
+            {coreProducts.map((product) => (
+                <AppBarPluginComponent
+                    key={product.id}
+                    component={product}
+                />
+            ))}
+            {isDividerVisible && (
+                <hr className={'app-bar__divider'}/>
+            )}
             {pluginComponents.map((component) => (
                 <AppBarPluginComponent
                     key={component.id}
