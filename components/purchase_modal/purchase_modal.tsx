449e8e72f2500f2349385c2d897a3a7d0d0fddea
Allan Guwatudde <guwats10@gmail.com>
2022-06-08 20:46:31 +0300

    [MM-44648] - Card input not visible in darker modes (#10496)
    * [MM-44648] - Card input not visible in darker modes
    
    * fix lint
    
    * change starter calls text
    
    * change label button color

--- components/purchase_modal/purchase_modal.tsx
+++ components/purchase_modal/purchase_modal.tsx
@@ -37,6 +37,7 @@ import StarMarkSvg from 'components/widgets/icons/star_mark_icon';
 import PricingModal from 'components/pricing_modal';
 import PlanLabel from 'components/common/plan_label';
 import {ModalData} from 'types/actions';
+import {Theme} from 'mattermost-redux/types/themes';
 
 import {getNextBillingDate} from 'utils/utils';
 
@@ -83,6 +84,7 @@ type Props = {
     isFreeTrial: boolean;
     productId: string | undefined;
     intl: IntlShape;
+    theme: Theme;
     actions: {
         openModal: <P>(modalData: ModalData<P>) => void;
         closeModal: () => void;
@@ -450,6 +452,7 @@ class PurchaseModal extends React.PureComponent<Props, State> {
                         onInputChange={this.onPaymentInput}
                         onCardInputChange={this.handleCardInputChange}
                         initialBillingDetails={initialBillingDetails}
+                        theme={this.props.theme}
                     // eslint-disable-next-line react/jsx-closing-bracket-location
                     />
                     ) : (<div className='PaymentDetails'>
@@ -496,7 +499,7 @@ class PurchaseModal extends React.PureComponent<Props, State> {
                             <PlanLabel
                                 text={formatMessage({id: 'pricing_modal.planLabel.mostPopular', defaultMessage: 'MOST POPULAR'})}
                                 bgColor='var(--title-color-indigo-500)'
-                                color='var(--center-channel-bg)'
+                                color='var(--button-color)'
                                 firstSvg={<StarMarkSvg/>}
                                 secondSvg={<StarMarkSvg/>}
                             />) : undefined}
