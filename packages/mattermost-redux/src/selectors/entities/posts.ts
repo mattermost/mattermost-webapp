4640e9879e209217c79c9ef56f47090bff291320
kyeongsoosoo <shakramon@naver.com>
2022-06-09 22:41:28 +0900

    fix typos

--- packages/mattermost-redux/src/selectors/entities/posts.ts
+++ packages/mattermost-redux/src/selectors/entities/posts.ts
@@ -1,7 +1,5 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
 
 /* eslint-disable max-lines */
 
