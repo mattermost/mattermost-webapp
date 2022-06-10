584459aa8b32e36ef00e32e961db860f4a67f8ba
Julien Tant <julien@craftyx.fr>
2022-06-10 14:32:18 -0700

    fix style

--- components/channel_members_rhs/member_list.tsx
+++ components/channel_members_rhs/member_list.tsx
@@ -2,7 +2,6 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import styled from 'styled-components';
 import AutoSizer from 'react-virtualized-auto-sizer';
 import {FixedSizeList} from 'react-window';
 import InfiniteLoader from 'react-window-infinite-loader';
