c9d2a88c626a732c0ded90411039110c9503b2b1
AGMETEOR <guwats10@gmail.com>
2022-06-21 14:27:58 +0300

    add snooze ability

--- components/advanced_create_post/file_limit_sticky_banner.tsx
+++ components/advanced_create_post/file_limit_sticky_banner.tsx
@@ -1,16 +1,21 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import {useIntl} from 'react-intl';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
 import OverlayTrigger from 'components/overlay_trigger';
 import Tooltip from 'components/tooltip';
 
-import {isCurrentUserSystemAdmin} from 'mattermost-redux/selectors/entities/users';
+import {getCurrentUser, isCurrentUserSystemAdmin} from 'mattermost-redux/selectors/entities/users';
+import {get as selectPreference} from 'mattermost-redux/selectors/entities/preferences';
 import {isCurrentLicenseCloud, getSubscriptionProduct as selectSubscriptionProduct} from 'mattermost-redux/selectors/entities/cloud';
-import Constants, {CloudProducts} from 'utils/constants';
+import {savePreferences} from 'mattermost-redux/actions/preferences';
+import Constants, {CloudProducts, Preferences} from 'utils/constants';
+import {GlobalState} from 'types/store';
+
+const snoozeCoolOffDays = 10;
 
 const StyledDiv = styled.div`
 width: 100%;
@@ -41,16 +46,54 @@ color: #FFBC1F;
 `;
 
 function FileLimitStickyBanner() {
+    const [show, setShow] = useState(true);
     const {formatMessage} = useIntl();
+    const dispatch = useDispatch();
 
+    const user = useSelector(getCurrentUser);
     const isAdmin = useSelector(isCurrentUserSystemAdmin);
     const isCloud = useSelector(isCurrentLicenseCloud);
     const product = useSelector(selectSubscriptionProduct);
     const isStarter = product?.sku === CloudProducts.STARTER;
 
+    const snoozePreferenceVal = useSelector((state: GlobalState) => selectPreference(state, Preferences.CLOUD_USER_EPHEMERAL_INFO, 'file_limit_banner_snooze'));
+
+    let shouldShowAgain = true;
+    if (snoozePreferenceVal !== '') {
+        const snoozeInfo = JSON.parse(snoozePreferenceVal);
+        const snoozeCoolOffDaysMillis = snoozeCoolOffDays * 24 * 60 * 60 * 1000;
+        const timeDiff = Date.now() - snoozeInfo.LastSnoozeTimestamp;
+        shouldShowAgain = timeDiff >= snoozeCoolOffDaysMillis;
+    }
+
+    if (!show) {
+        return null;
+    }
+
+    if (!shouldShowAgain) {
+        return null;
+    }
+
     if (!isCloud || !isStarter) {
         return null;
     }
+
+    const snoozeBanner = () => {
+        const fileLimitBannerSnoozeInfo = {
+            LastSnoozeTimestamp: Date.now(),
+        };
+
+        dispatch(savePreferences(user.id, [
+            {
+                category: Preferences.CLOUD_USER_EPHEMERAL_INFO,
+                name: 'file_limit_banner_snooze',
+                user_id: user.id,
+                value: JSON.stringify(fileLimitBannerSnoozeInfo),
+            },
+        ]));
+
+        setShow(false);
+    };
 
     const AdminMessageLink = (
         <a>{formatMessage({id: 'create_post.file_limit_sticky_banner.admin_link_message', defaultMessage: 'upgrade to a paid plan.'})}</a>
@@ -106,7 +149,7 @@ function FileLimitStickyBanner() {
                     placement='bottom'
                     overlay={tooltip}
                 >
-                    <CloseIcon>
+                    <CloseIcon onClick={snoozeBanner}>
                         <i className='icon icon-close'/>
                     </CloseIcon>
                 </OverlayTrigger>
