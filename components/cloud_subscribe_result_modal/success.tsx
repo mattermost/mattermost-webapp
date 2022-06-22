0afb5c6f6897fa649e3b17f67173e5883a68fc8b
Nick Misasi <nick.misasi@mattermost.com>
2022-06-22 14:35:09 -0400

    [MM-44315] Downgrade - Team selection flow (#10452)
    * stashing
    
    * Lots done, need master code
    
    * Why does it keep removing my i18n
    
    * Stashing
    
    * Add props.onHide()
    
    * Finished, clean up time
    
    * Fix conflicts
    
    * Adding files, starting tests
    
    * Adding test file
    
    * Adding tests
    
    * Fix tests, fix types, fix linter
    
    * Fix scss style
    
    * Fixes
    
    * Bunch of PR feedback
    
    * Fixes
    
    * Fix tests
    
    * Remove unused prop
    
    * Add await
    
    * Fix tests
    
    * Add button to open pricing modal on billing_subscriptions page
    
    * Moving files around, various fixes, final touches, etc
    
    * Fix i18n, linter
    
    * Fix tests
    
    * Adjust e2e tests
    
    * add extra check
    
    * PR feedback
    
    * Fixes
    
    * Fix spacing

---
+++ components/cloud_subscribe_result_modal/success.tsx
@@ -0,0 +1,89 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {FormattedMessage} from 'react-intl';
+
+import {useDispatch, useSelector} from 'react-redux';
+
+import {getSubscriptionProduct} from 'mattermost-redux/selectors/entities/cloud';
+
+import PaymentSuccessStandardSvg from 'components/common/svg_images_components/payment_success_standard_svg';
+
+import IconMessage from 'components/purchase_modal/icon_message';
+
+import FullScreenModal from 'components/widgets/modals/full_screen_modal';
+
+import {closeModal} from 'actions/views/modals';
+import {ModalIdentifiers} from 'utils/constants';
+import {isModalOpen} from 'selectors/views/modals';
+import {GlobalState} from 'types/store';
+
+import './style.scss';
+
+type Props = {
+    onHide?: () => void;
+};
+
+function SuccessModal(props: Props) {
+    const dispatch = useDispatch();
+    const subscriptionProduct = useSelector(getSubscriptionProduct);
+
+    const isSuccessModalOpen = useSelector((state: GlobalState) =>
+        isModalOpen(state, ModalIdentifiers.SUCCESS_MODAL),
+    );
+
+    const onHide = () => {
+        dispatch(closeModal(ModalIdentifiers.SUCCESS_MODAL));
+        if (typeof props.onHide === 'function') {
+            props.onHide();
+        }
+    };
+
+    return (
+        <FullScreenModal
+            show={isSuccessModalOpen}
+            onClose={onHide}
+        >
+            <IconMessage
+                formattedTitle={
+                    <FormattedMessage
+                        defaultMessage={
+                            'You are now subscribed to {selectedProductName}'
+                        }
+                        id={'admin.billing.subscription.proratedPayment.title'}
+                        values={{
+                            selectedProductName: subscriptionProduct?.name,
+                        }}
+                    />
+                }
+                formattedSubtitle={
+                    <FormattedMessage
+                        id={'success_modal.subtitle'}
+                        defaultMessage={
+                            'Your final bill will be prorated. Your workspace now has {plan} limits.'
+                        }
+                        values={{plan: subscriptionProduct?.name}}
+                    />
+                }
+                error={false}
+                icon={
+                    <PaymentSuccessStandardSvg
+                        width={444}
+                        height={313}
+                    />
+                }
+                formattedButtonText={
+                    <FormattedMessage
+                        defaultMessage={'Return to workspace'}
+                        id={'success_modal.return_to_workspace'}
+                    />
+                }
+                buttonHandler={onHide}
+                className={'success'}
+            />
+        </FullScreenModal>
+    );
+}
+
+export default SuccessModal;
