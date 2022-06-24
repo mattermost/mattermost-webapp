5baada94138a845c9222e260114d40eed8a857c2
Nick Misasi <nick.misasi@mattermost.com>
2022-06-24 13:07:16 -0400

    Remove negation on condition for disabling downgrade button

--- components/pricing_modal/content.tsx
+++ components/pricing_modal/content.tsx
@@ -299,7 +299,7 @@ function Content(props: ContentProps) {
                                 }
                             },
                             text: formatMessage({id: 'pricing_modal.btn.downgrade', defaultMessage: 'Downgrade'}),
-                            disabled: !isStarter,
+                            disabled: isStarter,
                             customClass: ButtonCustomiserClasses.secondary,
                         }}
                         planLabel={
