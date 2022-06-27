a269db1affa985838acbbc48df899645fa3f8ba0
Ashish Dhama <ashishdhama3@gmail.com>
2022-06-27 11:29:40 +0530

    improve if form select

--- components/admin_console/systembus/node-modal/if-form.tsx
+++ components/admin_console/systembus/node-modal/if-form.tsx
@@ -1,21 +1,38 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {ChangeEvent, useState} from 'react';
+import React, {useState} from 'react';
 import {useIntl} from 'react-intl';
 
+import ReactSelect, {ValueType} from 'react-select';
+
 import Input from 'components/widgets/inputs/input/input';
-import system from '../../../../reducers/views/system';
-import {SearchUserTeamFilter, UserFilters} from '../../../../utils/constants';
-import * as Utils from '../../../../utils/utils';
-import systembus from '../systembus';
+
 type Props = {
     handleChange: (data: Record<string, any>) => void;
 }
+
+type SelectedOption = {
+    value: string;
+    label: string;
+}
+
+const selectOptions = [
+    {value: 'eq', label: 'eq'},
+    {value: 'gt', label: 'gt'},
+    {value: 'lt', label: 'lt'},
+    {value: 'gte', label: 'gte'},
+    {value: 'lte', label: 'lte'},
+    {value: 'contains', label: 'contains'},
+    {value: 'prefix', label: 'prefix'},
+    {value: 'suffix', label: 'suffix'},
+];
+
 const WebhookForm = ({handleChange}: Props): JSX.Element => {
     const intl = useIntl();
     const {formatMessage} = intl;
-    const [data, setData] = useState<{ifValue: string;ifComparison: string}>({ifValue: '', ifComparison: ''});
+    const [openMenu, setOpenMenu] = useState(false);
+    const [data, setData] = useState<{ifValue: string;ifComparison: SelectedOption}>({ifValue: '', ifComparison: selectOptions[0]});
 
     const handleIfValueOnChange = ({target: {value}}: React.ChangeEvent<HTMLInputElement>) => {
         setData({
@@ -25,12 +42,45 @@ const WebhookForm = ({handleChange}: Props): JSX.Element => {
         handleChange(data);
     };
 
-    const handleIfComparisonOnChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        setData({
-            ...data,
-            ifComparison: e.target.value,
-        });
-        handleChange(data);
+    const handleIfComparisonOnChange = (selectedOption: ValueType<SelectedOption>) => {
+        if (selectedOption && 'value' in selectedOption) {
+            setData({
+                ...data,
+                ifComparison: selectedOption,
+            });
+            handleChange({ifComparison: data.ifComparison.value, ifValue: data.ifValue});
+        }
+    };
+
+    // const handleIfComparisonOnChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    //     setData({
+    //         ...data,
+    //         ifComparison: e.target.value,
+    //     });
+    //     handleChange(data);
+    // };
+
+    const handleMenuClose = () => {
+        const modalBody = document.querySelector('.modal-body');
+        if (modalBody) {
+            modalBody.classList.remove('no-scroll');
+        }
+        setOpenMenu(false);
+    };
+
+    const handleMenuOpen = () => {
+        const modalBody = document.querySelector('.modal-body');
+        if (modalBody) {
+            modalBody.classList.add('no-scroll');
+        }
+        setOpenMenu(true);
+    };
+
+    const reactStyles = {
+        menuPortal: (provided: React.CSSProperties) => ({
+            ...provided,
+            zIndex: 9999,
+        }),
     };
 
     return (
@@ -48,21 +98,21 @@ const WebhookForm = ({handleChange}: Props): JSX.Element => {
                 maxLength={64}
                 autoFocus={true}
             />
-            <select
+            <ReactSelect
+                className='react-select'
+                classNamePrefix='react-select'
                 id='selectComparison'
-                className='node-modal__select'
-                value={data.ifComparison}
+                options={selectOptions}
+                clearable={false}
+                menuIsOpen={openMenu}
+                styles={reactStyles}
+                menuPortalTarget={document.body}
                 onChange={handleIfComparisonOnChange}
-            >
-                <option value='eq'>{'eq'}</option>
-                <option value='gt'>{'gt'}</option>
-                <option value='lt'>{'lt'}</option>
-                <option value='gte'>{'gte'}</option>
-                <option value='lte'>{'lte'}</option>
-                <option value='contains'>{'contains'}</option>
-                <option value='prefix'>{'prefix'}</option>
-                <option value='suffix'>{''}</option>
-            </select>
+                value={data.ifComparison}
+                isSearchable={false}
+                onMenuClose={handleMenuClose}
+                onMenuOpen={handleMenuOpen}
+            />
         </div>
     );
 };
