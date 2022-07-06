47be38ed0c9e9a592d24f2990c1c1955ddf5e10e
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-07-06 19:15:46 +0000

    groups type is updated

--- components/admin_console/permission_schemes_settings/permissions_tree/permissions_tree.test.tsx
+++ components/admin_console/permission_schemes_settings/permissions_tree/permissions_tree.test.tsx
@@ -129,12 +129,12 @@ describe('components/admin_console/permission_schemes_settings/permission_tree',
             />,
         );
 
-        const groups = wrapper.find(PermissionGroup).first().prop('permissions');
-        expect((groups[0] as Group | Permission).id).toStrictEqual('teams');
-        expect((groups[6] as Group | Permission).id).toStrictEqual('posts');
-        expect((groups[7] as Group | Permission).id).toStrictEqual('integrations');
-        expect((groups[8] as Group | Permission).id).toStrictEqual('manage_shared_channels');
-        expect((groups[9] as Group | Permission).id).toStrictEqual('custom_groups');
+        const groups = wrapper.find(PermissionGroup).first().prop('permissions') as Array<Group | Permission>;
+        expect(groups[0].id).toStrictEqual('teams');
+        expect(groups[6].id).toStrictEqual('posts');
+        expect(groups[7].id).toStrictEqual('integrations');
+        expect(groups[8].id).toStrictEqual('manage_shared_channels');
+        expect(groups[9].id).toStrictEqual('custom_groups');
     });
 
     describe('should show playbook permissions', () => {
@@ -155,8 +155,8 @@ describe('components/admin_console/permission_schemes_settings/permission_tree',
                 );
 
                 const groups: Omit<Permissions, string> = wrapper.find(PermissionGroup).first().prop('permissions');
-                expect((groups[3] as Group | Permission).id).toStrictEqual('playbook_public');
-                expect((groups[4] as Group | Permission).id).toStrictEqual('runs');
+                expect(groups[3].id).toStrictEqual('playbook_public');
+                expect(groups[4].id).toStrictEqual('runs');
             }));
         });
 
@@ -177,9 +177,9 @@ describe('components/admin_console/permission_schemes_settings/permission_tree',
                 );
 
                 const groups = wrapper.find(PermissionGroup).first().prop('permissions');
-                expect((groups[3] as Group | Permission).id).toStrictEqual('playbook_public');
-                expect((groups[4] as Group | Permission).id).toStrictEqual('playbook_private');
-                expect((groups[5] as Group | Permission).id).toStrictEqual('runs');
+                expect(groups[3].id).toStrictEqual('playbook_public');
+                expect(groups[4].id).toStrictEqual('playbook_private');
+                expect(groups[5].id).toStrictEqual('runs');
             }));
         });
     });
