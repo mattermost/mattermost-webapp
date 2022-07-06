1809141dfe354e3ad0ed33a62a48a02cff04f37b
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-07-06 19:17:13 +0000

    change remaining groups

--- components/admin_console/permission_schemes_settings/permissions_tree/permissions_tree.test.tsx
+++ components/admin_console/permission_schemes_settings/permissions_tree/permissions_tree.test.tsx
@@ -154,7 +154,7 @@ describe('components/admin_console/permission_schemes_settings/permission_tree',
                     />,
                 );
 
-                const groups: Omit<Permissions, string> = wrapper.find(PermissionGroup).first().prop('permissions');
+                const groups = wrapper.find(PermissionGroup).first().prop('permissions') as Array<Group | Permission>;;
                 expect(groups[3].id).toStrictEqual('playbook_public');
                 expect(groups[4].id).toStrictEqual('runs');
             }));
@@ -176,7 +176,7 @@ describe('components/admin_console/permission_schemes_settings/permission_tree',
                     />,
                 );
 
-                const groups = wrapper.find(PermissionGroup).first().prop('permissions');
+                const groups = wrapper.find(PermissionGroup).first().prop('permissions') as Array<Group | Permission>;;
                 expect(groups[3].id).toStrictEqual('playbook_public');
                 expect(groups[4].id).toStrictEqual('playbook_private');
                 expect(groups[5].id).toStrictEqual('runs');
