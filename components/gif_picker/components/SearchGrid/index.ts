1c08d9c1d319da7f34939e9f28f4972adfd6d986
apahadi73 <apahadi73@gmail.com>
2022-07-14 21:49:12 -0500

    feat: migrate index.js to index.ts for SearchGrid

--- components/gif_picker/components/SearchGrid/index.ts
+++ components/gif_picker/components/SearchGrid/index.ts
@@ -4,10 +4,11 @@
 import {connect} from 'react-redux';
 
 import {saveSearchScrollPosition} from 'mattermost-redux/actions/gifs';
+import {GlobalState} from 'types/store';
 
 import SearchGrid from './SearchGrid';
 
-function mapStateToProps(state) {
+function mapStateToProps(state: GlobalState) {
     return {
         ...state.entities.gifs.cache,
         ...state.entities.gifs.search,
