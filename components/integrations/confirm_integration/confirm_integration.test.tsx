6ca2bb71e8ac96d2b31bfd1f700b8caf2ba5ad4c
Sinan Sonmez (Chaush) <37421564+sinansonmez@users.noreply.github.com>
2022-07-18 15:19:49 +0200

    MM-20451: Migrate 'components/integrations/confirm_integration' module and associated tests to TypeScript (#10742)
    * component is migrated
    
    * index is migrated
    
    * test is migrated
    
    * fix merge conflict
    
    * improve type in test
    
    * fix referrer error
    
    * improve type of incomingHooks in test
    
    * delete old jsx file

--- components/integrations/confirm_integration/confirm_integration.test.tsx
+++ components/integrations/confirm_integration/confirm_integration.test.tsx
@@ -6,36 +6,41 @@ import {shallow} from 'enzyme';
 import {Router} from 'react-router-dom';
 
 import {browserHistory} from 'utils/browser_history';
+import {TestHelper} from 'utils/test_helper';
 import {renderWithIntl} from 'tests/react_testing_utils';
 
-import ConfirmIntegration from 'components/integrations/confirm_integration/confirm_integration.jsx';
+import ConfirmIntegration from 'components/integrations/confirm_integration/confirm_integration';
+
+import {IncomingWebhook, OAuthApp, OutgoingWebhook} from '@mattermost/types/integrations';
+import {Bot} from '@mattermost/types/bots';
+import {IDMappedObjects} from '@mattermost/types/utilities';
 
 describe('components/integrations/ConfirmIntegration', () => {
     const id = 'r5tpgt4iepf45jt768jz84djic';
     const token = 'jb6oyqh95irpbx8fo9zmndkp1r';
-    const getSearchString = (type, identifier = id) => `?type=${type}&id=${identifier}`;
+    const getSearchString = (type: string, identifier = id) => `?type=${type}&id=${identifier}`;
 
     const location = {
         search: '',
     };
-    const team = {
+    const team = TestHelper.getTeamMock({
         name: 'team_test',
-    };
+    });
     const oauthApp = {
         id,
         client_secret: '<==secret==>',
         callback_urls: ['https://someCallback', 'https://anotherCallback'],
     };
     const userId = 'b5tpgt4iepf45jt768jz84djhd';
-    const bot = {
+    const bot = TestHelper.getBotMock({
         user_id: userId,
         display_name: 'bot',
-    };
-    const commands = {[id]: {id, token}};
-    const oauthApps = {[id]: oauthApp};
-    const incomingHooks = {[id]: {id}};
-    const outgoingHooks = {[id]: {id, token}};
-    const bots = {[userId]: bot};
+    });
+    const commands = {[id]: TestHelper.getCommandMock({id, token})};
+    const oauthApps = {[id]: oauthApp} as unknown as IDMappedObjects<OAuthApp>;
+    const incomingHooks: IDMappedObjects<IncomingWebhook> = {[id]: TestHelper.getIncomingWebhookMock({id})};
+    const outgoingHooks = {[id]: {id, token}} as unknown as IDMappedObjects<OutgoingWebhook>;
+    const bots: Record<string, Bot> = {[userId]: bot};
 
     const props = {
         team,
