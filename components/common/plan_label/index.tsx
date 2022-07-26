4ccd915485fe3b4f805ba5d7c3cda4ac648a62c4
AGMETEOR <guwats10@gmail.com>
2022-07-26 16:04:31 +0300

    cloud pricing modal update

--- components/common/plan_label/index.tsx
+++ components/common/plan_label/index.tsx
@@ -1,7 +1,15 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
+
 import React from 'react';
+import {useSelector} from 'react-redux';
 import styled from 'styled-components';
+import {useIntl} from 'react-intl';
+
+import {TrialPeriodDays} from 'utils/constants';
+import {getRemainingDaysFromFutureTimestamp} from 'utils/utils';
+
+import {getCloudSubscription as selectCloudSubscription} from 'mattermost-redux/selectors/entities/cloud';
 
 type PlanLabelProps = {
     text: string;
@@ -22,6 +30,17 @@ color: ${(props) => props.color};
 `;
 
 function PlanLabel(props: PlanLabelProps) {
+    const {formatMessage} = useIntl();
+    const subscription = useSelector(selectCloudSubscription);
+    let text = props.text;
+
+    if (subscription?.is_free_trial === 'true') {
+        const daysLeftOnTrial = Math.min(
+            getRemainingDaysFromFutureTimestamp(subscription.trial_end_at),
+            TrialPeriodDays.TRIAL_30_DAYS,
+        );
+        text = formatMessage({id: 'pricing_modal.plan_label_trialDays', defaultMessage: '{days} DAYS LEFT ON TRIAL'}, {days: daysLeftOnTrial});
+    }
     return (
         <StyledPlanLabel
             className='planLabel'
@@ -29,7 +48,7 @@ function PlanLabel(props: PlanLabelProps) {
             color={props.color}
         >
             {props.firstSvg}
-            {props.text}
+            {text}
             {props.secondSvg}
         </StyledPlanLabel>
     );
