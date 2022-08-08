36e9b54d3279b0381e2c98dc875c8edb8e430c87
Jesse Hallam <jesse.hallam@gmail.com>
2022-08-08 12:20:37 -0300

    add memoization test

--- selectors/plugins.test.js
+++ selectors/plugins.test.js
@@ -105,5 +105,79 @@ describe('selectors/plugins', () => {
             const components = getChannelHeaderMenuPluginComponents(state);
             expect(components).toEqual(expectedComponents);
         });
+
+        test('memoization', () => {
+            let shouldRenderResult = false;
+
+            let state = {
+                entities: {
+                    general: {
+                        config: {},
+                    },
+                    preferences: {
+                        myPreferences: {},
+                    },
+                },
+                plugins: {
+                    components: {
+                        ChannelHeader: [{
+                            shouldRender: () => shouldRenderResult,
+                        }],
+                    },
+                },
+            };
+
+            const firstResult = getChannelHeaderMenuPluginComponents(state);
+
+            expect(firstResult).toEqual([]);
+
+            // No changes to state
+            const secondResult = getChannelHeaderMenuPluginComponents(state);
+
+            expect(secondResult).toBe(firstResult);
+
+            // Something unrelated changed in state
+            state = {...state};
+
+            const thirdResult = getChannelHeaderMenuPluginComponents(state);
+
+            expect(thirdResult).toBe(firstResult);
+
+            // shouldRender changed because something else in state changed
+            state = {...state};
+            shouldRenderResult = true;
+
+            const fourthResult = getChannelHeaderMenuPluginComponents(state);
+
+            expect(fourthResult).not.toBe(firstResult);
+            expect(fourthResult).toEqual([
+                state.plugins.components.ChannelHeader[0],
+            ]);
+
+            // A new plugin was added
+            state = {
+                ...state,
+                plugins: {
+                    ...state.plugins,
+                    components: {
+                        ...state.plugins.components,
+                        ChannelHeader: [
+                            ...state.plugins.components.ChannelHeader,
+                            {
+                                id: 'anotherPlugin',
+                            },
+                        ],
+                    },
+                },
+            };
+
+            const fifthResult = getChannelHeaderMenuPluginComponents(state);
+
+            expect(fifthResult).not.toBe(fourthResult);
+            expect(fifthResult).toEqual([
+                state.plugins.components.ChannelHeader[0],
+                state.plugins.components.ChannelHeader[1],
+            ]);
+        });
     });
 });
