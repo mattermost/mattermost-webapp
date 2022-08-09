2aac0e8e8a4271f82df4ec09cd32dba583a162da
neallred <neallred@protonmail.com>
2022-08-09 14:33:10 -0500

    DRY code

--- components/onboarding_tour/onboarding_tour_manager.tsx
+++ components/onboarding_tour/onboarding_tour_manager.tsx
@@ -1,16 +1,21 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState, useMemo} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 
 import {getInt} from 'mattermost-redux/selectors/entities/preferences';
-import {GlobalState} from '@mattermost/types/store';
+import {Action} from 'mattermost-redux/types/actions';
 import {savePreferences, savePreferences as storeSavePreferences} from 'mattermost-redux/actions/preferences';
 import {getCurrentUserId} from 'mattermost-redux/selectors/entities/common';
 import {setAddChannelDropdown} from 'actions/views/add_channel_dropdown';
 import {open as openLhs} from 'actions/views/lhs.js';
+import {setProductMenuSwitcherOpen} from 'actions/views/product_menu';
 import {trackEvent as trackEventAction} from 'actions/telemetry_actions';
+
+import {GlobalState} from '@mattermost/types/store';
+import {getPlugins} from 'selectors/plugins';
+
 import {
     generateTelemetryTag,
     OnboardingTaskCategory,
@@ -22,6 +27,8 @@ import {switchToChannels} from 'actions/views/onboarding_tasks';
 import {
     AutoTourStatus,
     ChannelsTour,
+    OtherToolsTour,
+    ExploreOtherToolsTourSteps,
     FINISHED,
     OnboardingTourSteps,
     SKIPPED,
@@ -44,96 +51,143 @@ export interface OnBoardingTourTipManager {
     handleJump: (e: React.MouseEvent, jumpStep: number) => void;
 }
 
-const useHandleNavigationAndExtraActions = () => {
+function finishTourAndOpenOnboardingTaskList(currentUserId: string, onboardingTaskName: string, dispatch: (action: Action) => void) {
+    let preferences = [
+        {
+            user_id: currentUserId,
+            category: OnboardingTaskCategory,
+            name: onboardingTaskName,
+            value: FINISHED.toString(),
+        },
+    ];
+    preferences = [...preferences,
+        {
+            user_id: currentUserId,
+            category: OnboardingTaskCategory,
+            name: OnboardingTaskList.ONBOARDING_TASK_LIST_OPEN,
+            value: 'true',
+        },
+    ];
+    dispatch(savePreferences(currentUserId, preferences));
+}
+
+function makeOnboardingNextStepActions(currentUserId: string, dispatch: (action: Action) => void) {
+    return {
+        [OnboardingTourSteps.CHANNELS_AND_DIRECT_MESSAGES]: () => {
+            dispatch(openLhs());
+        },
+        [OnboardingTourSteps.CREATE_AND_JOIN_CHANNELS]: () => {
+            dispatch(setAddChannelDropdown(true));
+        },
+        [OnboardingTourSteps.INVITE_PEOPLE]: () => {
+            dispatch(setAddChannelDropdown(true));
+        },
+        [OnboardingTourSteps.SEND_MESSAGE]: () => {
+            dispatch(switchToChannels());
+        },
+        [OnboardingTourSteps.CUSTOMIZE_EXPERIENCE]: () => {
+        },
+        [OnboardingTourSteps.FINISHED]: () => {
+            finishTourAndOpenOnboardingTaskList(currentUserId, OnboardingTasksName.EXPLORE_OTHER_TOOLS, dispatch);
+        },
+    };
+}
+
+function makeOnboardingLastStepActions(dispatch: (action: Action) => void) {
+    return {
+        [OnboardingTourSteps.CHANNELS_AND_DIRECT_MESSAGES]: () => {
+        },
+        [OnboardingTourSteps.CREATE_AND_JOIN_CHANNELS]: () => {
+            dispatch(setAddChannelDropdown(false));
+        },
+        [OnboardingTourSteps.INVITE_PEOPLE]: () => {
+            dispatch(setAddChannelDropdown(false));
+        },
+        [OnboardingTourSteps.SEND_MESSAGE]: () => {
+        },
+        [OnboardingTourSteps.CUSTOMIZE_EXPERIENCE]: () => {
+        },
+    };
+}
+
+function makeOtherToolsNextStepActions(currentUserId: string, dispatch: (action: Action) => void) {
+    return {
+        [ExploreOtherToolsTourSteps.FINISHED]: () => {
+            finishTourAndOpenOnboardingTaskList(currentUserId, OnboardingTasksName.EXPLORE_OTHER_TOOLS, dispatch);
+        },
+    };
+}
+
+interface MakeActionsDeps {
+    tourName: string;
+    dispatch: (action: Action) => void;
+    currentUserId: string;
+}
+
+type TourActions = Record<number, () => void>
+
+function useMakeActions({tourName, dispatch, currentUserId}: MakeActionsDeps): {nextStepActions: TourActions; lastStepActions: TourActions} {
+    let actions = {
+        nextStepActions: {},
+        lastStepActions: {},
+    };
+    if (tourName === TutorialTourName.EXPLORE_OTHER_TOOLS) {
+        actions = {
+            lastStepActions: {},
+            nextStepActions: makeOtherToolsNextStepActions(currentUserId, dispatch),
+        };
+    } else if (tourName === TutorialTourName.ONBOARDING_TUTORIAL_STEP) {
+        actions = {
+            nextStepActions: makeOnboardingNextStepActions(currentUserId, dispatch),
+            lastStepActions: makeOnboardingLastStepActions(dispatch),
+        };
+    } else {
+        throw new Error(`Unsupported usage of useHandleNavigationAndExtraActions. Please add ${tourName} as a handled type of tour.`);
+    }
+    return useMemo(() => actions, [tourName, dispatch, currentUserId]);
+}
+
+const useHandleNavigationAndExtraActions = (tourName: string) => {
     const dispatch = useDispatch();
     const currentUserId = useSelector(getCurrentUserId);
 
-    const nextStepActions = useCallback((step: number) => {
-        switch (step) {
-        case OnboardingTourSteps.CHANNELS_AND_DIRECT_MESSAGES : {
-            dispatch(openLhs());
-            break;
-        }
-        case OnboardingTourSteps.CREATE_AND_JOIN_CHANNELS : {
-            dispatch(setAddChannelDropdown(true));
-            break;
-        }
-        case OnboardingTourSteps.INVITE_PEOPLE : {
-            dispatch(setAddChannelDropdown(true));
-            break;
-        }
-        case OnboardingTourSteps.SEND_MESSAGE : {
-            dispatch(switchToChannels());
-            break;
-        }
-        case OnboardingTourSteps.CUSTOMIZE_EXPERIENCE : {
-            break;
-        }
-        case OnboardingTourSteps.FINISHED: {
-            let preferences = [
-                {
-                    user_id: currentUserId,
-                    category: OnboardingTaskCategory,
-                    name: OnboardingTasksName.CHANNELS_TOUR,
-                    value: FINISHED.toString(),
-                },
-            ];
-            preferences = [...preferences,
-                {
-                    user_id: currentUserId,
-                    category: OnboardingTaskCategory,
-                    name: OnboardingTaskList.ONBOARDING_TASK_LIST_OPEN,
-                    value: 'true',
-                },
-            ];
-            dispatch(savePreferences(currentUserId, preferences));
-            break;
-        }
-        default:
-        }
-    }, [currentUserId]);
-
-    const lastStepActions = useCallback((lastStep: number) => {
-        switch (lastStep) {
-        case OnboardingTourSteps.CHANNELS_AND_DIRECT_MESSAGES : {
-            break;
-        }
-        case OnboardingTourSteps.CREATE_AND_JOIN_CHANNELS : {
-            dispatch(setAddChannelDropdown(false));
-            break;
-        }
-        case OnboardingTourSteps.INVITE_PEOPLE : {
-            dispatch(setAddChannelDropdown(false));
-            break;
-        }
-        case OnboardingTourSteps.SEND_MESSAGE : {
-            break;
-        }
-        case OnboardingTourSteps.CUSTOMIZE_EXPERIENCE : {
-            break;
-        }
-        default:
-        }
-    }, [currentUserId]);
+    const {nextStepActions, lastStepActions} = useMakeActions({dispatch, currentUserId, tourName});
 
     return useCallback((step: number, lastStep: number) => {
-        lastStepActions(lastStep);
-        nextStepActions(step);
+        lastStepActions[lastStep]?.();
+        nextStepActions[step]?.();
     }, [nextStepActions, lastStepActions]);
 };
 
-const useOnBoardingTourTipManager = (): OnBoardingTourTipManager => {
+const trackingCategoriesByTourName = {
+    [TutorialTourName.ONBOARDING_TUTORIAL_STEP]: ChannelsTour,
+    [TutorialTourName.EXPLORE_OTHER_TOOLS]: OtherToolsTour,
+};
+
+function useGetOtherToolsLastStep(): number {
+    const pluginsList = useSelector(getPlugins);
+    const focalboard = pluginsList?.focalboard;
+    const playbooks = pluginsList?.playbooks;
+
+    // if both plugins are enabled, then the last step is one, otherwise if just one is enabled it must be 0
+    return useMemo(() => ((focalboard && playbooks) ? 1 : 0), [focalboard, playbooks]);
+}
+
+const useOnBoardingTourTipManager = (tourCategory: typeof TutorialTourName['ONBOARDING_TUTORIAL_STEP'] | typeof TutorialTourName['EXPLORE_OTHER_TOOLS']): OnBoardingTourTipManager => {
     const [show, setShow] = useState(false);
     const tourSteps = OnboardingTourSteps;
 
     // Function to save the tutorial step in redux store start here which needs to be modified
     const dispatch = useDispatch();
     const currentUserId = useSelector(getCurrentUserId);
-    const tourCategory = TutorialTourName.ONBOARDING_TUTORIAL_STEP;
     const currentStep = useSelector((state: GlobalState) => getInt(state, tourCategory, currentUserId, 0));
     const autoTourStatus = useSelector((state: GlobalState) => getInt(state, tourCategory, TTNameMapToATStatusKey[tourCategory], 0));
     const isAutoTourEnabled = autoTourStatus === AutoTourStatus.ENABLED;
-    const handleActions = useHandleNavigationAndExtraActions();
+    const handleActions = useHandleNavigationAndExtraActions(tourCategory);
+
+    // if both plugins are enabled, then the last step is one, otherwise if just one is enabled it must be 0
+    const otherToolsLastStep = useGetOtherToolsLastStep();
+    const lastStep = tourCategory === TutorialTourName.ONBOARDING_TUTORIAL_STEP ? getLastStep(tourSteps) : otherToolsLastStep;
 
     const handleSaveDataAndTrackEvent = useCallback(
         (stepValue: number, eventSource: ActionType, autoTour = true, trackEvent = true) => {
@@ -154,7 +208,7 @@ const useOnBoardingTourTipManager = (): OnBoardingTourTipManager => {
             dispatch(storeSavePreferences(currentUserId, preferences));
             if (trackEvent) {
                 const eventSuffix = `${stepValue}--${eventSource}`;
-                const telemetryTag = generateTelemetryTag(ChannelsTour, tourCategory, eventSuffix);
+                const telemetryTag = generateTelemetryTag(trackingCategoriesByTourName[tourCategory], tourCategory, eventSuffix);
                 trackEventAction(tourCategory, telemetryTag);
             }
         },
@@ -205,6 +259,9 @@ const useOnBoardingTourTipManager = (): OnBoardingTourTipManager => {
         handleEventPropagationAndDefault(e);
         handleHide();
         handleSaveDataAndTrackEvent(currentStep, 'dismiss', false);
+        if (tourCategory === TutorialTourName.EXPLORE_OTHER_TOOLS) {
+            dispatch(setProductMenuSwitcherOpen(false));
+        }
     }, [handleSaveDataAndTrackEvent, handleHide]);
 
     const handlePrevious = useCallback((e: React.MouseEvent): void => {
@@ -216,7 +273,7 @@ const useOnBoardingTourTipManager = (): OnBoardingTourTipManager => {
         if (e) {
             handleEventPropagationAndDefault(e);
         }
-        if (getLastStep(tourSteps) === currentStep) {
+        if (lastStep === currentStep) {
             handleSavePreferences(FINISHED);
         } else {
             handleSavePreferences(true);
