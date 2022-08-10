297370dac40e45a356ec4f288c5798d5259fe5e3
Kent Nguyen <kentn@kents-air.gateway.sonic.net>
2022-08-10 14:33:28 -0700

    testing branch

--- components/toast_wrapper/index.tsx
+++ components/toast_wrapper/index.tsx
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+git // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
 import {connect} from 'react-redux';
