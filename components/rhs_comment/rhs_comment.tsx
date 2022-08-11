8a819a932c7075121e56fb665f4da7a1ad1efb38
Sinan Sonmez (Chaush) <37421564+sinansonmez@users.noreply.github.com>
2022-08-11 15:02:59 +0200

    delete unnecessary eslint disabling

--- components/rhs_comment/rhs_comment.tsx
+++ components/rhs_comment/rhs_comment.tsx
@@ -1,6 +1,5 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
-/* eslint-disable react/no-string-refs */
 
 import React, {MouseEvent, ReactNode, RefObject} from 'react';
 import {FormattedMessage} from 'react-intl';
