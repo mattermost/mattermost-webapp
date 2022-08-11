7f21c81f26485c9eb9baf4a35b47017f60b07f45
Ashish Dhama <ashishdhama3@gmail.com>
2022-08-11 16:17:10 +0530

    fix condition checking if the hostname is test.mattermost.com

--- components/root/root.jsx
+++ components/root/root.jsx
@@ -265,8 +265,8 @@ export default class Root extends React.PureComponent {
         } else {
             landing = desktopAppDownloadLink;
         }
-
-        if (landing && !BrowserStore.hasSeenLandingPage() && !toResetPasswordScreen && !this.props.location.pathname.includes('/landing') && !this.props.location.href?.includes('.test.mattermost.com') && !UserAgent.isDesktopApp()) {
+        const isTestServer = window.location.hostname.search(/^.*\.test\.mattermost\.com$/g) !== 0;
+        if (landing && !BrowserStore.hasSeenLandingPage() && !toResetPasswordScreen && !this.props.location.pathname.includes('/landing') && isTestServer && !UserAgent.isDesktopApp()) {
             this.props.history.push('/landing#' + this.props.location.pathname + this.props.location.search);
             BrowserStore.setLandingPageSeen(true);
         }
