4c7de77c72512d577ff82f15934e091e0cb6a749
AGMETEOR <guwats10@gmail.com>
2022-08-12 21:39:28 +0300

    fix test case name

--- e2e/cypress/tests/integration/enterprise/system_console/about/starter_edition_spec.js
+++ e2e/cypress/tests/integration/enterprise/system_console/about/starter_edition_spec.js
@@ -17,7 +17,7 @@ describe('System console', () => {
         cy.apiDeleteLicense();
     });
 
-    it('MM-41397 - License page shows View plans button', () => {
+    it('License page shows View plans button', () => {
         cy.visit('/admin_console/about/license');
 
         // *Validate View plans button exits
