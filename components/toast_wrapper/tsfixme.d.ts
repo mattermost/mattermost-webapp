d9cead0b4b1b1ec85bdb3c43018ad66712f8343b
Diego Vega <92145039+Dvega49@users.noreply.github.com>
2022-08-12 13:49:31 -0700

    delete tsfixme.d.ts

--- components/toast_wrapper/tsfixme.d.ts
+++
@@ -1,4 +0,0 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-type $TSFixMe = any;
-type $TSFixMeFunction = (...args: any[]) => any;
