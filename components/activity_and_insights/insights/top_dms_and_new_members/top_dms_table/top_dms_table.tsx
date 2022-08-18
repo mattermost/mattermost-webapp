112aa0608a13cd8ed0bf063ce891eeeda238b2d5
Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
2022-08-18 11:09:58 -0400

    telemetry

--- components/activity_and_insights/insights/top_dms_and_new_members/top_dms_table/top_dms_table.tsx
+++ components/activity_and_insights/insights/top_dms_and_new_members/top_dms_table/top_dms_table.tsx
@@ -5,6 +5,8 @@ import {FormattedMessage} from 'react-intl';
 import {useDispatch, useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
 
+import {trackEvent} from 'actions/telemetry_actions';
+
 import classNames from 'classnames';
 
 import {getMyTopDMs} from 'mattermost-redux/actions/insights';
@@ -56,6 +58,7 @@ const TopDMsTable = (props: Props) => {
     }, [getMyTopTeamDMs]);
 
     const closeModal = useCallback(() => {
+        trackEvent('insights', 'open_dm_from_top_dms_modal');
         props.closeModal();
     }, [props.closeModal]);
 
