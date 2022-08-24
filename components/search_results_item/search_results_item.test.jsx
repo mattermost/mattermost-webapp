59e684d984ff30dfe666d7c8ebef6ae2da57a825
koox00 <3829551+koox00@users.noreply.github.com>
2022-08-24 16:54:41 +0300

    Fixes tests

--- components/search_results_item/search_results_item.test.jsx
+++ components/search_results_item/search_results_item.test.jsx
@@ -71,6 +71,7 @@ describe('components/SearchResultsItem', () => {
         enablePostUsernameOverride: false,
         isBot: false,
         isMobileView: false,
+        isPostPriorityEnabled: true,
         actions: {
             closeRightHandSide: mockFunc,
             selectPost: mockFunc,
@@ -337,4 +338,24 @@ describe('components/SearchResultsItem', () => {
 
         expect(wrapper).toMatchSnapshot();
     });
+
+    test('should match snapshot for post with priority', () => {
+        const props = {
+            ...defaultProps,
+            isPostPriorityEnabled: true,
+            post: {
+                ...post,
+                props: {
+                    ...post.props,
+                    priority: 'important',
+                },
+            },
+        };
+
+        const wrapper = shallow(
+            <SearchResultsItem {...props}/>,
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
 });
