e37c85f43ac278e1d0d4bfec1f558830f7d85d00
Michel Engelen <jsnerdic@gmail.com>
2022-08-24 13:31:02 +0200

    fixed stage setting

--- e2e/cypress/tests/integration/message_forwarding/forward_message_from_private_channel_spec.js
+++ e2e/cypress/tests/integration/message_forwarding/forward_message_from_private_channel_spec.js
@@ -7,7 +7,8 @@
 // - Use element ID when selecting an element. Create one if none.
 // ***************************************************************
 
-// Group: @prod @enterprise @messaging
+// Stage: @prod
+// Group: @enterprise @messaging
 
 import * as TIMEOUTS from '../../fixtures/timeouts';
 
