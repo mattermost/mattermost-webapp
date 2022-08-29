c53ad1cb46df79d5193e2be0a17b6d46f4d453d0
Saturnino Abril <saturnino.abril@gmail.com>
2022-08-29 18:47:58 +0800

    revert api and token keys

--- e2e/playwright/support/visual/percy.ts
+++ e2e/playwright/support/visual/percy.ts
@@ -8,8 +8,8 @@ import testConfig from '@test.config';
 
 export default async function snapshotWithPercy(page: Page, isMobile: boolean, browserName: string, name: string) {
     if (!isMobile && browserName === 'chromium' && testConfig.percyEnabled) {
-        if (!process.env.PW_PERCY_TOKEN) {
-            console.error('Error: Token is missing! Please set using: "export PW_PERCY_TOKEN=<change_me>"');
+        if (!process.env.PERCY_TOKEN) {
+            console.error('Error: Token is missing! Please set using: "export PERCY_TOKEN=<change_me>"');
         }
 
         await percySnapshot(page, name);
