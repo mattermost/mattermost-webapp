830e33e7d2557071e07257e8323ef43a3260013e
neallred <neallred@protonmail.com>
2022-09-01 15:20:42 -0500

    better mergeObjects

--- packages/mattermost-redux/test/merge_objects.ts
+++ packages/mattermost-redux/test/merge_objects.ts
@@ -1,13 +1,11 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-function isObject(obj: any): boolean {
+function isObject(obj: any): obj is Record<string, any> {
     return Boolean(obj && typeof obj === 'object' && !Array.isArray(obj));
 }
 
-// Returns the result of merging two objects. If a field is specified in both a and b, the value from b takes precedence
-// unless both values are objects in which case mergeObjects will be called recursively.
-export default function mergeObjects(a: any, b: any, path = '.') {
+export default function mergeObjects(a: Record<string, any>, b: Record<string, any>, path = '.') {
     if (a === null || a === undefined) {
         return b;
     } else if (b === null || b === undefined) {
