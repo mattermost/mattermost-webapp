2edfe20b876d9656de0eba78f6cccd2bf84bdfd7
Nevyana Angelova <nevyangelova@Nevyanas-MacBook-Pro.local>
2022-09-01 18:31:33 +0300

    Fix lint

--- components/at_mention/at_mention.tsx
+++ components/at_mention/at_mention.tsx
@@ -57,7 +57,9 @@ export default class AtMention extends React.PureComponent<Props, State> {
     }
 
     handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-        e.stopPropagation && e.stopPropagation();
+        if (e.stopPropagation) {
+            e.stopPropagation();
+        }
         const targetBounds = this.overlayRef.current?.getBoundingClientRect();
 
         if (targetBounds) {
