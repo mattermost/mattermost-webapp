61574963bd04df45b9c13a332fc0058141d3400e
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-09-11 12:21:41 +0000

    fix test

--- components/info_toast/info_toast.test.tsx
+++ components/info_toast/info_toast.test.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, {ComponentProps} from 'react';
 import {shallow} from 'enzyme';
 
 import {CheckIcon} from '@mattermost/compass-icons/components';
@@ -9,11 +9,22 @@ import {CheckIcon} from '@mattermost/compass-icons/components';
 import InfoToast from './info_toast';
 
 describe('components/InfoToast', () => {
+    const baseProps: ComponentProps<typeof InfoToast> = {
+        content: {
+            icon: <CheckIcon/>,
+            message: 'test',
+            undo: jest.fn(),
+        },
+        actions: {
+            closeModal: jest.fn(),
+        },
+        className: 'className',
+    };
+
     test('should match snapshot', () => {
         const wrapper = shallow(
             <InfoToast
-                actions={{closeModal: jest.fn()}}
-                content={{message: 'A message'}}
+                {...baseProps}
             />,
         );
 
@@ -21,87 +32,24 @@ describe('components/InfoToast', () => {
     });
 
     test('should close the toast on undo', () => {
-        const closeModal = jest.fn();
         const wrapper = shallow(
             <InfoToast
-                actions={{closeModal}}
-                content={{message: 'A message', undo: jest.fn()}}
+                {...baseProps}
             />,
         );
 
         wrapper.find('button').simulate('click');
-
-        expect(closeModal).toHaveBeenCalled();
-    });
-
-    test('should close the toast on timeout', () => {
-        jest.useFakeTimers();
-
-        const closeModal = jest.fn();
-        shallow(
-            <InfoToast
-                actions={{closeModal}}
-                content={{message: 'A message'}}
-            />,
-        );
-
-        // setTimeout(() => {
-        //     const activeSlide = wrapper.find('div.active-anim');
-        //     const slide1Text = activeSlide.find('p.slide').text();
-        //     expect(slide1Text).toEqual('Second Slide');
-        //     done();
-        // }, 1000);
-
-        jest.runAllTimers();
-
-        expect(closeModal).toHaveBeenCalled();
+        expect(baseProps.actions.closeModal).toHaveBeenCalled();
     });
 
     test('should close the toast on close button click', () => {
-        const closeModal = jest.fn();
         const wrapper = shallow(
             <InfoToast
-                actions={{closeModal}}
-                content={{message: 'A message'}}
+                {...baseProps}
             />,
         );
 
         wrapper.find('.info-toast__icon_button').simulate('click');
-
-        expect(closeModal).toHaveBeenCalled();
-    });
-
-    test('should match snapshot with icon', () => {
-        const wrapper = shallow(
-            <InfoToast
-                actions={{closeModal: jest.fn()}}
-                content={{message: 'A message', icon: <CheckIcon/>}}
-            />,
-        );
-
-        expect(wrapper).toMatchSnapshot();
-    });
-
-    test('should match snapshot with undo', () => {
-        const wrapper = shallow(
-            <InfoToast
-                actions={{closeModal: jest.fn()}}
-                content={{message: 'A message', undo: jest.fn()}}
-            />,
-        );
-
-        expect(wrapper).toMatchSnapshot();
-    });
-
-    test('should match snapshot with className', () => {
-        const wrapper = shallow(
-            <InfoToast
-                actions={{closeModal: jest.fn()}}
-                content={{message: 'A message'}}
-                className='className'
-            />,
-        );
-
-        expect(wrapper).toMatchSnapshot();
+        expect(baseProps.actions.closeModal).toHaveBeenCalled();
     });
 });
