0aee248973fc03ae2f7beeb0936632e0cd3579f6
Ashish Dhama <ashishdhama3@gmail.com>
2022-09-13 21:06:52 +0530

    fix import issue

--- packages/components/src/pulsating_dot/index.tsx
+++ packages/components/src/pulsating_dot/index.tsx
@@ -1,7 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 import React from 'react';
-import {Coords} from '@mattermost/types/lib/punchout';
+
+import {Coords} from '../common/hooks/useMeasurePunchouts';
 
 import './pulsating_dot.scss';
 
