36978e8f55aac490bffb6f269046d89e0dabbacb
Harrison Healey <harrisonmhealey@gmail.com>
2022-09-13 13:20:03 -0400

    MM-46004/MM-46274 Add Module Federation and Boards as a POC Product (#11001)
    * Implement POC version of module federation
    
    * Rework how ModuleFederationPlugin is initialized
    
    * Add some comments
    
    * MM-46274 Add Boards as a POC Product
    
    * Fix broken tests
    
    * Fix ESLint and remove unused variable
    
    * Remove unneeded eslint-disable
    
    * Force error when a singleton shared module is overriden

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@
 // See LICENSE.txt for license information.
 
 const childProcess = require('child_process');
+const http = require('http');
 const path = require('path');
 
 const url = require('url');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
+const {ModuleFederationPlugin} = require('webpack').container;
 const nodeExternals = require('webpack-node-externals');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -14,6 +16,10 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const LiveReloadPlugin = require('webpack-livereload-plugin');
 
 // const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+
+const packageJson = require('./package.json');
+
+/* eslint-disable no-console */
 
 const NPM_TARGET = process.env.npm_lifecycle_event; //eslint-disable-line no-process-env
 
@@ -27,9 +33,6 @@ const DEV = targetIsRun || targetIsStats || targetIsDevServer;
 const STANDARD_EXCLUDE = [
     path.join(__dirname, 'node_modules'),
 ];
-
-// react-hot-loader and development source maps require eval
-const CSP_UNSAFE_EVAL_IF_DEV = DEV ? ' \'unsafe-eval\'' : '';
 
 var MYSTATS = {
 
@@ -266,12 +269,6 @@ var config = {
             filename: 'root.html',
             inject: 'head',
             template: 'root.html',
-            meta: {
-                csp: {
-                    'http-equiv': 'Content-Security-Policy',
-                    content: 'script-src \'self\' cdn.rudderlabs.com/ js.stripe.com/v3 ' + CSP_UNSAFE_EVAL_IF_DEV,
-                },
-            },
         }),
         new CopyWebpackPlugin({
             patterns: [
@@ -382,6 +379,113 @@ var config = {
     ],
 };
 
+async function initializeModuleFederation() {
+    function makeSingletonSharedModules(packageNames) {
+        const sharedObject = {};
+
+        for (const packageName of packageNames) {
+            const version = packageJson.dependencies[packageName];
+
+            sharedObject[packageName] = {
+                requiredVersion: version,
+                singleton: true,
+                strictVersion: true,
+                version,
+            };
+        }
+
+        return sharedObject;
+    }
+
+    function isWebpackDevServerAvailable(baseUrl) {
+        return new Promise((resolve) => {
+            if (!DEV) {
+                resolve(false);
+                return;
+            }
+
+            const req = http.request(`${baseUrl}/remote_entry.js`, (response) => {
+                return resolve(response.statusCode === 200);
+            });
+
+            req.on('error', () => {
+                resolve(false);
+            });
+
+            req.end();
+        });
+    }
+
+    async function getRemoteModules() {
+        const products = [
+            {name: 'focalboard', baseUrl: 'http://localhost:9006'},
+        ];
+
+        const productsFound = await Promise.all(products.map((product) => isWebpackDevServerAvailable(product.baseUrl)));
+
+        const remotes = {};
+        const aliases = {};
+
+        for (let i = 0; i < products.length; i++) {
+            const product = products[i];
+            const found = productsFound[i];
+
+            if (found) {
+                console.log(`Product ${product.name} found, adding as remote module`);
+
+                remotes[product.name] = `${product.name}@${product.baseUrl}/remote_entry.js`;
+            } else {
+                console.log(`Product ${product.name} not found`);
+
+                // Add false aliases to prevent Webpack from trying to resolve the missing modules
+                aliases[product.name] = false;
+                aliases[`${product.name}/manifest`] = false;
+            }
+        }
+
+        return {remotes, aliases};
+    }
+
+    const {remotes, aliases} = await getRemoteModules();
+
+    config.plugins.push(new ModuleFederationPlugin({
+        name: 'mattermost-webapp',
+        remotes,
+        shared: [
+
+            // Shared modules will be made available to other containers (ie products and plugins using module federation).
+            // To allow for better sharing, containers shouldn't require exact versions of packages like the web app does.
+
+            // Other containers will use these shared modules if their required versions match. If they don't match, the
+            // version packaged with the container will be used.
+            '@mattermost/client',
+            '@mattermost/components',
+            '@mattermost/types',
+            'luxon',
+            'prop-types',
+
+            // Other containers will be forced to use the exact versions of shared modules that the web app provides.
+            makeSingletonSharedModules([
+                'react',
+                'react-bootstrap',
+                'react-dom',
+                'react-intl',
+                'react-redux',
+                'react-router-dom',
+            ]),
+        ],
+    }));
+
+    config.resolve.alias = {
+        ...config.resolve.alias,
+        ...aliases,
+    };
+
+    config.plugins.push(new webpack.DefinePlugin({
+        REMOTE_MODULES: JSON.stringify(remotes),
+    }));
+}
+
 if (!targetIsStats) {
     config.stats = MYSTATS;
 }
@@ -484,4 +588,14 @@ if (process.env.PRODUCTION_PERF_DEBUG) { //eslint-disable-line no-process-env
     };
 }
 
-module.exports = config;
+if (NPM_TARGET === 'check') {
+    // ESLint can't handle setting an async config, so just skip the async part
+    module.exports = config;
+} else {
+    module.exports = async () => {
+        // Do this asynchronously so we can determine whether which remote modules are available
+        await initializeModuleFederation();
+
+        return config;
+    };
+}
