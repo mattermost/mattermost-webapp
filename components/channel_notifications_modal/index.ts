0cabfcfb6ac5a018410f9f2352c475c442ae89e0
Sinan Sonmez (Chaush) <sinansonmez@outlook.com>
2022-09-18 07:38:17 +0000

    migrate index

--- components/channel_notifications_modal/index.ts
+++ components/channel_notifications_modal/index.ts
@@ -1,24 +1,30 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
+import {bindActionCreators, Dispatch} from 'redux';
+import {connect, ConnectedProps} from 'react-redux';
 
 import {updateChannelNotifyProps} from 'mattermost-redux/actions/channels';
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
 import {getMyCurrentChannelMembership} from 'mattermost-redux/selectors/entities/channels';
 
+import {GlobalState} from 'types/store/index';
+
 import ChannelNotificationsModal from './channel_notifications_modal.jsx';
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state: GlobalState) => ({
     channelMember: getMyCurrentChannelMembership(state),
     sendPushNotifications: getConfig(state).SendPushNotifications === 'true',
 });
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = (dispatch: Dispatch) => ({
     actions: bindActionCreators({
         updateChannelNotifyProps,
     }, dispatch),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ChannelNotificationsModal);
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+export type PropsFromRedux = ConnectedProps<typeof connector>
+
+export default connector(ChannelNotificationsModal);
