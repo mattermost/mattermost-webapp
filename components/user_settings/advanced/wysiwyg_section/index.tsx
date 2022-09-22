71cfd584d21c050e22be16e5ccdb119d5db2bc6e
Jason Frerich <jason.frerich@mattermost.com>
2022-09-22 17:11:04 -0500

    [contributor / wysiwyg] Add Feature Flag (#11185)

--- components/user_settings/advanced/wysiwyg_section/index.tsx
+++ components/user_settings/advanced/wysiwyg_section/index.tsx
@@ -9,23 +9,15 @@ import {GlobalState} from 'types/store/index.js';
 import {GenericAction} from 'mattermost-redux/types/actions.js';
 
 import {savePreferences} from 'mattermost-redux/actions/preferences';
-import {Preferences} from 'mattermost-redux/constants';
-import {get as getPreference} from 'mattermost-redux/selectors/entities/preferences';
+import {getWysiwygPreference} from 'mattermost-redux/selectors/entities/preferences';
 import {getCurrentUserId} from 'mattermost-redux/selectors/entities/users';
 
-import WysiwygSection from './wysiwyg_editor';
+import WysiwygSection from './wysiwyg_section';
 
 function mapStateToProps(state: GlobalState) {
-    const wysiwyg = getPreference(
-        state,
-        Preferences.CATEGORY_ADVANCED_SETTINGS,
-        Preferences.ADVANCED_WYSIWYG,
-        'true',
-    );
-
     return {
         currentUserId: getCurrentUserId(state),
-        wysiwyg,
+        wysiwygEnabled: getWysiwygPreference(state),
     };
 }
 
