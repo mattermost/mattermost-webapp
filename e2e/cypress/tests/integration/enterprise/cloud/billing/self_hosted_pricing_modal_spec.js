ec962168fc72b30af99b58db862562f0b3c19188
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2022-09-28 18:54:35 +0530

    fix cloud test cases

--- e2e/cypress/tests/integration/enterprise/cloud/billing/self_hosted_pricing_modal_spec.js
+++ e2e/cypress/tests/integration/enterprise/cloud/billing/self_hosted_pricing_modal_spec.js
@@ -149,7 +149,6 @@ describe('Self hosted Pricing modal', () => {
         cy.visit('admin_console/about/license');
         cy.get('div.Badge').should('exist').should('contain', 'Trial');
         cy.findByTitle('Back Icon').should('be.visible').click();
-
         cy.visit(urlL);
 
         // * Open pricing modal
