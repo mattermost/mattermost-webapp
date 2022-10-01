8f92d73bb0569c09f056dd2cd2b4275265abbed4
sk409 <deepstrap353@gmail.com>
2022-10-01 23:45:55 +0900

    Reset unnecessary changes

--- e2e/cypress/tests/integration/emoji/helpers.js
+++ e2e/cypress/tests/integration/emoji/helpers.js
@@ -16,18 +16,16 @@ export function verifyLastPostedEmoji(emojiName, emojiImageFile) {
     cy.getLastPost().find('p').find('span > span').then((imageSpan) => {
         cy.expect(imageSpan.attr('title')).to.equal(emojiName);
 
-        if (emojiImageFile) {
-            // # Filter out the url from the css background property
-            // url("https://imageurl") => https://imageurl
-            const url = imageSpan.css('background-image').split('"')[1];
+        // # Filter out the url from the css background property
+        // url("https://imageurl") => https://imageurl
+        const url = imageSpan.css('background-image').split('"')[1];
 
-            // * Verify that the emoji image is the correct one
-            cy.fixture(emojiImageFile).then((overrideImage) => {
-                cy.request({url, encoding: 'base64'}).then((response) => {
-                    expect(response.status).to.equal(200);
-                    expect(response.body).to.eq(overrideImage);
-                });
+        // * Verify that the emoji image is the correct one
+        cy.fixture(emojiImageFile).then((overrideImage) => {
+            cy.request({url, encoding: 'base64'}).then((response) => {
+                expect(response.status).to.equal(200);
+                expect(response.body).to.eq(overrideImage);
             });
-        }
+        });
     });
 }
