a2a0333a042f25cc38416f81208ab399aa4453b7
Sinan Sonmez (Chaush) <37421564+sinansonmez@users.noreply.github.com>
2022-10-05 21:58:21 +0200

    MM-46286: Change default # of DMs shown in sidebar from 20 to 40 (#11142)
    * change default number
    
    * fix style
    
    * fix test
    
    * fix more tests
    
    * change default value
    
    * revert test changes
    
    * fix style
    
    * fix e2e
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -1873,6 +1873,7 @@ export const Constants = {
     TRIAL_MODAL_AUTO_SHOWN: 'trial_modal_auto_shown',
     DEFAULT_SITE_URL: 'http://localhost:8065',
     CHANNEL_HEADER_BUTTON_DISABLE_TIMEOUT: 1000,
+    DEFAULT_DM_NUMBER: 40,
 };
 
 export const ValidationErrors = {
