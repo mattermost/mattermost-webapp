b8052c8a937a5961c875e35c846702d38f3b43c4
Caleb Roseland <caleb@calebroseland.com>
2022-10-05 13:51:55 -0500

    border radius and color

--- components/people/profile/profile.tsx
+++ components/people/profile/profile.tsx
@@ -143,8 +143,8 @@ const EditProfileBtn = () => {
 
 const MainContent = styled.div`
     padding: 28px 32px;
-    border: 1px solid rgba(var(--sys-center-channel-color-rgb), 0.08);
-    background: var(--sys-center-channel-bg);
-    border-radius: 4px;
+    border: 1px solid rgba(var(--center-channel-color-rgb), 0.08);
+    background: var(--center-channel-bg);
+    border-radius: 8px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.08);
 `;
