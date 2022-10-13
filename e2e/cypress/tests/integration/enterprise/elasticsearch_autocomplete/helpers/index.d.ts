220daf6c5a0db2cbd4e4e7c095c27ba357b226a4
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2022-10-13 20:04:30 -0300

    chore: add d.ts for tests/integration/enterprise/elasticsearch_autocomplete/helpers/index.d.ts

---
+++ e2e/cypress/tests/integration/enterprise/elasticsearch_autocomplete/helpers/index.d.ts
@@ -0,0 +1,4 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+export function createPrivateChannel(teamId: string, userToAdd?: string): Cypress.Chainable<Cypress.Channel>;
