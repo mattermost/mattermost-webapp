e2979d5030a96e2d62dbc369e666cc094851548f
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2022-10-13 20:00:22 -0300

    chore: migrate support/ui/search.js to TS

--- e2e/cypress/tests/support/ui/search.ts
+++ e2e/cypress/tests/support/ui/search.ts
@@ -1,16 +1,20 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-Cypress.Commands.add('uiSearchPosts', (searchTerm) => {
+import {ChainableT} from 'tests/types';
+
+const uiSearchPosts = (searchTerm: string): ChainableT<any> => {
     // # Enter the search terms and hit enter to start the search
     cy.get('#searchBox').clear().type(searchTerm).type('{enter}');
 
     // * Wait for the RHS to open and the search results to appear
     cy.contains('.sidebar--right__header', 'Search Results').should('be.visible');
     cy.get('#searchContainer .LoadingSpinner').should('not.exist');
-});
+    return cy
+};
+Cypress.Commands.add('uiSearchPosts', uiSearchPosts);
 
-Cypress.Commands.add('uiJumpToSearchResult', (postId) => {
+const uiJumpToSearchResult = (postId: string): ChainableT<any> => {
     // # Find the post in the search results and click Jump
     cy.get(`#searchResult_${postId}`).contains('a', 'Jump').click();
 
@@ -19,4 +23,16 @@ Cypress.Commands.add('uiJumpToSearchResult', (postId) => {
 
     // * Verify that the permalinked post is highlighted in the center channel
     cy.get(`#post_${postId}.post--highlight`).should('be.visible');
-});
+    return cy
+};
+Cypress.Commands.add('uiJumpToSearchResult', uiJumpToSearchResult);
+
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace Cypress {
+        interface Chainable {
+            uiSearchPosts: typeof uiSearchPosts;
+            uiJumpToSearchResult: typeof uiJumpToSearchResult;
+        }
+    }
+}
