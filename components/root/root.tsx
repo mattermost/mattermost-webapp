cc230c8f8e3f24e1d76079e945288988e4f402b4
Julien Tant <785518+JulienTant@users.noreply.github.com>
2022-10-13 14:23:18 -0700

    [MM-46694] A/B Test: welcome post (#11041)
    Co-authored-by: Pablo Andrés Vélez Vidal <pablovv2012@gmail.com>
    (cherry picked from commit 7ea7c5d1a410c6ef558c92cd7579f8e962300e2c)

--- components/root/root.tsx
+++ components/root/root.tsx
@@ -76,6 +76,8 @@ import {UserProfile} from '@mattermost/types/users';
 
 import {ActionResult} from 'mattermost-redux/types/actions';
 
+import WelcomePostRenderer from 'components/welcome_post_renderer';
+
 import {applyLuxonDefaults} from './effects';
 
 import RootProvider from './root_provider';
@@ -375,6 +377,7 @@ export default class Root extends React.PureComponent<Props, State> {
 
         // See figma design on issue https://mattermost.atlassian.net/browse/MM-43649
         this.props.actions.registerCustomPostRenderer('custom_up_notification', OpenPricingModalPost, 'upgrade_post_message_renderer');
+        this.props.actions.registerCustomPostRenderer('system_welcome_post', WelcomePostRenderer, 'welcome_post_renderer');
 
         if (this.desktopMediaQuery.addEventListener) {
             this.desktopMediaQuery.addEventListener('change', this.handleMediaQueryChangeEvent);
