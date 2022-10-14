cbf6ba04b35e9793801f0e31cae0fea41ed80cc6
David Taing <adavidtaing@gmail.com>
2022-10-14 21:22:46 +1100

    refactor: convert helpers to TS

--- e2e/cypress/tests/integration/enterprise/guest_accounts/helpers.ts
+++ e2e/cypress/tests/integration/enterprise/guest_accounts/helpers.ts
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {Team} from '@mattermost/types/lib/teams';
+
 export function changeGuestFeatureSettings(featureFlag = true, emailInvitation = true, whitelistedDomains = '') {
     // # Update Guest Accounts, Email Invitations, and Whitelisted Domains
     cy.apiUpdateConfig({
@@ -14,7 +16,7 @@ export function changeGuestFeatureSettings(featureFlag = true, emailInvitation =
     });
 }
 
-export function invitePeople(typeText, resultsCount, verifyText, channelName = 'Town Square', clickInvite = true) {
+export function invitePeople(typeText: string, resultsCount: number, verifyText: string, channelName = 'Town Square', clickInvite = true) {
     // # Open team menu and click 'Invite People'
     cy.uiOpenTeamMenu('Invite People');
 
@@ -42,7 +44,7 @@ export function invitePeople(typeText, resultsCount, verifyText, channelName = '
     }
 }
 
-export function verifyInvitationError(user, team, errorText, verifyGuestBadge = false) {
+export function verifyInvitationError(user: string, team: Team, errorText: string, verifyGuestBadge = false) {
     // * Verify the content and error message in the Invitation Modal
     cy.findByTestId('invitationModal').within(() => {
         cy.get('h1').should('have.text', `Guests invited to ${team.display_name}`);
@@ -64,7 +66,7 @@ export function verifyInvitationError(user, team, errorText, verifyGuestBadge = 
     cy.get('.InvitationModal').should('not.exist');
 }
 
-export function verifyInvitationSuccess(user, team, successText, verifyGuestBadge = false) {
+export function verifyInvitationSuccess(user: string, team: Team, successText: string, verifyGuestBadge = false) {
     // * Verify the content and success message in the Invitation Modal
     cy.findByTestId('invitationModal').within(() => {
         cy.get('h1').should('have.text', `Guests invited to ${team.display_name}`);
