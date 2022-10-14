cf0da079a4b291aea1c7673a417ac0d0f2195372
sk409 <deepstrap353@gmail.com>
2022-10-15 02:11:01 +0900

    Use map to get language

--- utils/syntax_highlighting.tsx
+++ utils/syntax_highlighting.tsx
@@ -1,7 +1,6 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {LanguageFn} from 'highlight.js';
 import hlJS from 'highlight.js/lib/core';
 
 import * as TextFormatting from 'utils/text_formatting';
@@ -82,201 +81,79 @@ function getLanguageFromNameOrAlias(name: string) {
 }
 
 async function registerLanguage(languageName: string) {
-    let language: LanguageFn | undefined;
-    switch (languageName) {
-    case '1c':
-        language = (await import('highlight.js/lib/languages/1c')).default;
-        break;
-    case 'actionscript':
-        language = (await import('highlight.js/lib/languages/actionscript')).default;
-        break;
-    case 'applescript':
-        language = (await import('highlight.js/lib/languages/applescript')).default;
-        break;
-    case 'bash':
-        language = (await import('highlight.js/lib/languages/bash')).default;
-        break;
-    case 'clojure':
-        language = (await import('highlight.js/lib/languages/clojure')).default;
-        break;
-    case 'coffeescript':
-        language = (await import('highlight.js/lib/languages/coffeescript')).default;
-        break;
-    case 'cpp':
-        language = (await import('highlight.js/lib/languages/cpp')).default;
-        break;
-    case 'csharp':
-        language = (await import('highlight.js/lib/languages/csharp')).default;
-        break;
-    case 'css':
-        language = (await import('highlight.js/lib/languages/css')).default;
-        break;
-    case 'd':
-        language = (await import('highlight.js/lib/languages/d')).default;
-        break;
-    case 'dart':
-        language = (await import('highlight.js/lib/languages/dart')).default;
-        break;
-    case 'delphi':
-        language = (await import('highlight.js/lib/languages/delphi')).default;
-        break;
-    case 'diff':
-        language = (await import('highlight.js/lib/languages/diff')).default;
-        break;
-    case 'django':
-        language = (await import('highlight.js/lib/languages/django')).default;
-        break;
-    case 'dockerfile':
-        language = (await import('highlight.js/lib/languages/dockerfile')).default;
-        break;
-    case 'elixir':
-        language = (await import('highlight.js/lib/languages/elixir')).default;
-        break;
-    case 'erlang':
-        language = (await import('highlight.js/lib/languages/erlang')).default;
-        break;
-    case 'fortran':
-        language = (await import('highlight.js/lib/languages/fortran')).default;
-        break;
-    case 'fsharp':
-        language = (await import('highlight.js/lib/languages/fsharp')).default;
-        break;
-    case 'gcode':
-        language = (await import('highlight.js/lib/languages/gcode')).default;
-        break;
-    case 'go':
-        language = (await import('highlight.js/lib/languages/go')).default;
-        break;
-    case 'groovy':
-        language = (await import('highlight.js/lib/languages/groovy')).default;
-        break;
-    case 'handlebars':
-        language = (await import('highlight.js/lib/languages/handlebars')).default;
-        break;
-    case 'haskell':
-        language = (await import('highlight.js/lib/languages/haskell')).default;
-        break;
-    case 'haxe':
-        language = (await import('highlight.js/lib/languages/haxe')).default;
-        break;
-    case 'java':
-        language = (await import('highlight.js/lib/languages/java')).default;
-        break;
-    case 'javascript':
-        language = (await import('highlight.js/lib/languages/javascript')).default;
-        break;
-    case 'json':
-        language = (await import('highlight.js/lib/languages/json')).default;
-        break;
-    case 'julia':
-        language = (await import('highlight.js/lib/languages/julia')).default;
-        break;
-    case 'kotlin':
-        language = (await import('highlight.js/lib/languages/kotlin')).default;
-        break;
-    case 'latex':
-        language = (await import('highlight.js/lib/languages/latex')).default;
-        break;
-    case 'less':
-        language = (await import('highlight.js/lib/languages/less')).default;
-        break;
-    case 'lisp':
-        language = (await import('highlight.js/lib/languages/lisp')).default;
-        break;
-    case 'lua':
-        language = (await import('highlight.js/lib/languages/lua')).default;
-        break;
-    case 'makefile':
-        language = (await import('highlight.js/lib/languages/makefile')).default;
-        break;
-    case 'markdown':
-        language = (await import('highlight.js/lib/languages/markdown')).default;
-        break;
-    case 'matlab':
-        language = (await import('highlight.js/lib/languages/matlab')).default;
-        break;
-    case 'objectivec':
-        language = (await import('highlight.js/lib/languages/objectivec')).default;
-        break;
-    case 'ocaml':
-        language = (await import('highlight.js/lib/languages/ocaml')).default;
-        break;
-    case 'perl':
-        language = (await import('highlight.js/lib/languages/perl')).default;
-        break;
-    case 'pgsql':
-        language = (await import('highlight.js/lib/languages/pgsql')).default;
-        break;
-    case 'php':
-        language = (await import('highlight.js/lib/languages/php')).default;
-        break;
-    case 'plaintext':
-        language = (await import('highlight.js/lib/languages/plaintext')).default;
-        break;
-    case 'powershell':
-        language = (await import('highlight.js/lib/languages/powershell')).default;
-        break;
-    case 'puppet':
-        language = (await import('highlight.js/lib/languages/puppet')).default;
-        break;
-    case 'python':
-        language = (await import('highlight.js/lib/languages/python')).default;
-        break;
-    case 'r':
-        language = (await import('highlight.js/lib/languages/r')).default;
-        break;
-    case 'ruby':
-        language = (await import('highlight.js/lib/languages/ruby')).default;
-        break;
-    case 'rust':
-        language = (await import('highlight.js/lib/languages/rust')).default;
-        break;
-    case 'scala':
-        language = (await import('highlight.js/lib/languages/scala')).default;
-        break;
-    case 'scheme':
-        language = (await import('highlight.js/lib/languages/scheme')).default;
-        break;
-    case 'scss':
-        language = (await import('highlight.js/lib/languages/scss')).default;
-        break;
-    case 'smalltalk':
-        language = (await import('highlight.js/lib/languages/smalltalk')).default;
-        break;
-    case 'sql':
-        language = (await import('highlight.js/lib/languages/sql')).default;
-        break;
-    case 'stylus':
-        language = (await import('highlight.js/lib/languages/stylus')).default;
-        break;
-    case 'swift':
-        language = (await import('highlight.js/lib/languages/swift')).default;
-        break;
-    case 'typescript':
-        language = (await import('highlight.js/lib/languages/typescript')).default;
-        break;
-    case 'vbnet':
-        language = (await import('highlight.js/lib/languages/vbnet')).default;
-        break;
-    case 'vbscript':
-        language = (await import('highlight.js/lib/languages/vbscript')).default;
-        break;
-    case 'verilog':
-        language = (await import('highlight.js/lib/languages/verilog')).default;
-        break;
-    case 'vhdl':
-        language = (await import('highlight.js/lib/languages/vhdl')).default;
-        break;
-    case 'xml':
-        language = (await import('highlight.js/lib/languages/xml')).default;
-        break;
-    case 'yaml':
-        language = (await import('highlight.js/lib/languages/yaml')).default;
-        break;
-    }
+    const languageImports: {
+        [key: string]: any;
+    } = {
+        '1c': () => import('highlight.js/lib/languages/1c'),
+        actionscript: () => import('highlight.js/lib/languages/actionscript'),
+        applescript: () => import('highlight.js/lib/languages/applescript'),
+        bash: () => import('highlight.js/lib/languages/bash'),
+        clojure: () => import('highlight.js/lib/languages/clojure'),
+        coffeescript: () => import('highlight.js/lib/languages/coffeescript'),
+        cpp: () => import('highlight.js/lib/languages/cpp'),
+        csharp: () => import('highlight.js/lib/languages/csharp'),
+        css: () => import('highlight.js/lib/languages/css'),
+        d: () => import('highlight.js/lib/languages/d'),
+        dart: () => import('highlight.js/lib/languages/dart'),
+        delphi: () => import('highlight.js/lib/languages/delphi'),
+        diff: () => import('highlight.js/lib/languages/diff'),
+        django: () => import('highlight.js/lib/languages/django'),
+        dockerfile: () => import('highlight.js/lib/languages/dockerfile'),
+        elixir: () => import('highlight.js/lib/languages/elixir'),
+        erlang: () => import('highlight.js/lib/languages/erlang'),
+        fortran: () => import('highlight.js/lib/languages/fortran'),
+        fsharp: () => import('highlight.js/lib/languages/fsharp'),
+        gcode: () => import('highlight.js/lib/languages/gcode'),
+        go: () => import('highlight.js/lib/languages/go'),
+        groovy: () => import('highlight.js/lib/languages/groovy'),
+        handlebars: () => import('highlight.js/lib/languages/handlebars'),
+        haskell: () => import('highlight.js/lib/languages/haskell'),
+        haxe: () => import('highlight.js/lib/languages/haxe'),
+        java: () => import('highlight.js/lib/languages/java'),
+        javascript: () => import('highlight.js/lib/languages/javascript'),
+        json: () => import('highlight.js/lib/languages/json'),
+        julia: () => import('highlight.js/lib/languages/julia'),
+        kotlin: () => import('highlight.js/lib/languages/kotlin'),
+        latex: () => import('highlight.js/lib/languages/latex'),
+        less: () => import('highlight.js/lib/languages/less'),
+        lisp: () => import('highlight.js/lib/languages/lisp'),
+        lua: () => import('highlight.js/lib/languages/lua'),
+        makefile: () => import('highlight.js/lib/languages/makefile'),
+        markdown: () => import('highlight.js/lib/languages/markdown'),
+        matlab: () => import('highlight.js/lib/languages/matlab'),
+        objectivec: () => import('highlight.js/lib/languages/objectivec'),
+        ocaml: () => import('highlight.js/lib/languages/ocaml'),
+        perl: () => import('highlight.js/lib/languages/perl'),
+        pgsql: () => import('highlight.js/lib/languages/pgsql'),
+        php: () => import('highlight.js/lib/languages/php'),
+        plaintext: () => import('highlight.js/lib/languages/plaintext'),
+        powershell: () => import('highlight.js/lib/languages/powershell'),
+        puppet: () => import('highlight.js/lib/languages/puppet'),
+        python: () => import('highlight.js/lib/languages/python'),
+        r: () => import('highlight.js/lib/languages/r'),
+        ruby: () => import('highlight.js/lib/languages/ruby'),
+        rust: () => import('highlight.js/lib/languages/rust'),
+        scala: () => import('highlight.js/lib/languages/scala'),
+        scheme: () => import('highlight.js/lib/languages/scheme'),
+        scss: () => import('highlight.js/lib/languages/scss'),
+        smalltalk: () => import('highlight.js/lib/languages/smalltalk'),
+        sql: () => import('highlight.js/lib/languages/sql'),
+        stylus: () => import('highlight.js/lib/languages/stylus'),
+        swift: () => import('highlight.js/lib/languages/swift'),
+        typescript: () => import('highlight.js/lib/languages/typescript'),
+        vbnet: () => import('highlight.js/lib/languages/vbnet'),
+        vbscript: () => import('highlight.js/lib/languages/vbscript'),
+        verilog: () => import('highlight.js/lib/languages/verilog'),
+        vhdl: () => import('highlight.js/lib/languages/vhdl'),
+        xml: () => import('highlight.js/lib/languages/xml'),
+        yaml: () => import('highlight.js/lib/languages/yaml'),
+    };
 
-    if (!language) {
+    if (!languageImports[languageName]) {
         return;
     }
+
+    const language = (await languageImports[languageName]()).default;
+
     hlJS.registerLanguage(languageName, language);
 }
