998dfb27d2fff26df680e486e0e15744bd1c3e89
Adithya Krishna <adikrish@redhat.com>
2022-10-17 19:25:47 +0530

    Updated Variable Types
    Signed-off-by: Adithya Krishna <adikrish@redhat.com>

--- e2e/cypress/tests/integration/enterprise/system_console/about/edition_and_license_spec.ts
+++ e2e/cypress/tests/integration/enterprise/system_console/about/edition_and_license_spec.ts
@@ -10,8 +10,6 @@
 // Stage: @prod
 // Group: @enterprise @not_cloud @system_console @license_removal
 
-import {UserNotifyProps, UserTimezone} from '@mattermost/types/lib/users';
-
 import * as TIMEOUTS from '../../../../fixtures/timeouts';
 import {getAdminAccount} from '../../../../support/env';
 
@@ -19,10 +17,10 @@ import {promoteToChannelOrTeamAdmin} from '../channel_moderation/helpers.js';
 
 describe('System console', () => {
     const sysadmin = getAdminAccount();
-    let teamAdmin: { id: string; create_at?: number; update_at?: number; delete_at?: number; username?: string; password?: string; auth_data?: string; auth_service?: string; email?: string; email_verified?: boolean; nickname?: string; first_name?: string; last_name?: string; position?: string; roles?: string; allow_marketing?: boolean; props?: Record<string, string>; notify_props?: UserNotifyProps; last_password_update?: number; last_picture_update?: number; failed_attempts?: number; locale?: string; timezone?: UserTimezone; mfa_active?: boolean; mfa_secret?: string; last_activity_at?: number; is_bot?: boolean; bot_description?: string; bot_last_icon_update?: number; terms_of_service_id?: string; terms_of_service_create_at?: number; remote_id?: string; status?: string };
-    let regularUser: { id: string; create_at?: number; update_at?: number; delete_at?: number; username?: string; password?: string; auth_data?: string; auth_service?: string; email?: string; email_verified?: boolean; nickname?: string; first_name?: string; last_name?: string; position?: string; roles?: string; allow_marketing?: boolean; props?: Record<string, string>; notify_props?: UserNotifyProps; last_password_update?: number; last_picture_update?: number; failed_attempts?: number; locale?: string; timezone?: UserTimezone; mfa_active?: boolean; mfa_secret?: string; last_activity_at?: number; is_bot?: boolean; bot_description?: string; bot_last_icon_update?: number; terms_of_service_id?: string; terms_of_service_create_at?: number; remote_id?: string; status?: string };
-    let teamName: string;
-    let privateChannelName: string;
+    let teamAdmin: Cypress.Team;
+    let regularUser: Cypress.UserProfile;
+    let teamName: Cypress.Team['name'];
+    let privateChannelName: Cypress.Channel['name'];
 
     before(() => {
         cy.shouldNotRunOnCloudEdition();
