a1182b858861342cb1c3e370d55efa456dfeae64
Saturnino Abril <saturnino.abril@gmail.com>
2022-10-18 15:26:52 +0800

    experiment mobile snapshots with percy

--- e2e/playwright/support/visual/percy.ts
+++ e2e/playwright/support/visual/percy.ts
@@ -6,7 +6,7 @@ import percySnapshot from '@percy/playwright';
 import testConfig, {TestArgs} from '@test.config';
 
 export default async function snapshotWithPercy(name: string, testArgs: TestArgs) {
-    if (!testArgs.isMobile && testArgs.browserName === 'chromium' && testConfig.percyEnabled) {
+    if (testArgs.browserName === 'chromium' && testConfig.percyEnabled) {
         if (!process.env.PERCY_TOKEN) {
             console.error('Error: Token is missing! Please set using: "export PERCY_TOKEN=<change_me>"');
         }
