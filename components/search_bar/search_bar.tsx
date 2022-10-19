f74df461b3328002817ce08df4c3c8d1dbefe00f
Nimret <nimretbedi@gmail.com>
2022-10-19 11:43:40 +0530

    Changes made as requested

--- components/search_bar/search_bar.tsx
+++ components/search_bar/search_bar.tsx
@@ -9,9 +9,7 @@ import Constants from 'utils/constants';
 import * as Utils from 'utils/utils';
 
 import SuggestionDate from 'components/suggestion/suggestion_date';
-
 import SearchSuggestionList from 'components/suggestion/search_suggestion_list.jsx';
-
 import SuggestionBox from 'components/suggestion/suggestion_box';
 import SuggestionBoxComponent from 'components/suggestion/suggestion_box/suggestion_box';
 import LoadingSpinner from 'components/widgets/loading/loading_spinner';
