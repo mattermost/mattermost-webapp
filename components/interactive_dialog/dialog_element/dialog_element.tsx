d2072c888105bff0d3335a2e0916cda12afbb784
jatinmark <73272963+jatinmark@users.noreply.github.com>
2022-10-20 19:37:33 +0530

    Apply suggestions from code review
    Co-authored-by: Michael Kochell <6913320+mickmister@users.noreply.github.com>

--- components/interactive_dialog/dialog_element/dialog_element.tsx
+++ components/interactive_dialog/dialog_element/dialog_element.tsx
@@ -177,7 +177,7 @@ export default class DialogElement extends React.PureComponent<Props, State> {
                     placeholder={placeholder}
                     helpText={helpTextContent}
                     onChange={onChange}
-                    resizable={true}
+                    resizable={subtype === 'textarea'}
                 />
             );
         } else if (type === 'select') {
