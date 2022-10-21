7c5c43e180a31cbdb3bb399753de4835264bfc93
koox00 <3829551+koox00@users.noreply.github.com>
2022-10-21 14:40:11 +0300

    Fixes graphql queries

--- actions/channel_queries.ts
+++ actions/channel_queries.ts
@@ -83,6 +83,7 @@ const channelMembersFragment = `
           msg_count_root: msgCountRoot
           mention_count: mentionCount
           mention_count_root: mentionCountRoot
+          urgent_mention_count: urgentMentionCount,
           notify_props: notifyProps
           last_update_at: lastUpdateAt
           scheme_admin: schemeAdmin
@@ -212,6 +213,7 @@ export function transformToReceivedChannelMembersReducerPayload(
         msg_count_root: channelMember?.msg_count_root ?? 0,
         mention_count: channelMember?.mention_count ?? 0,
         mention_count_root: channelMember?.mention_count_root ?? 0,
+        urgent_mention_count: channelMember?.urgent_mention_count ?? 0,
         notify_props:
             channelMember && channelMember.notify_props ? {...channelMember.notify_props} : {},
         last_update_at: channelMember?.last_update_at ?? 0,
