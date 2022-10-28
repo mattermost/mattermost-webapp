1e072b8d8bfcf8b7a9002efe9512be15662596ba
Said Atrahouch <said.atrahouch@gmail.com>
2022-10-28 17:14:25 +0200

    [MM-47414] Code changes to keep consistency for Cloud and Self hosted new name (#11396)
    * [MM-47414] Change key translations for free and cloud starter for cloud free
    
    * [MM-47414] Code changes to keep consistency related to cloud
    
    * Revert makefile
    
    * fix i18n
    
    * missing add default message translations
    
    * missing empty translations
    
    * Revert sku changes
    
    * fix ci
    
    * revert admin definitions
    
    * self hosted changes
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- components/pricing_modal/downgrade_team_removal_modal/downgrade_team_removal_modal.test.tsx
+++ components/pricing_modal/downgrade_team_removal_modal/downgrade_team_removal_modal.test.tsx
@@ -268,7 +268,7 @@ describe('components/pricing_modal/downgrade_team_removal_modal', () => {
                 subscription: {
                     is_free_trial: 'false',
                     trial_end_at: 0,
-                    product_id: 'prod_starter',
+                    product_id: 'prod_free',
                 },
                 limits: {
                     limitsLoaded: true,
@@ -292,9 +292,9 @@ describe('components/pricing_modal/downgrade_team_removal_modal', () => {
                     },
                 },
                 products: {
-                    prod_starter: {
-                        id: 'prod_starter',
-                        name: 'Cloud Starter',
+                    prod_free: {
+                        id: 'prod_free',
+                        name: 'Cloud Free',
                         sku: CloudProducts.STARTER,
                         price_per_seat: 0,
                         product_family: 'cloud',
@@ -327,8 +327,8 @@ describe('components/pricing_modal/downgrade_team_removal_modal', () => {
         renderWithIntl(
             <redux.Provider store={store}>
                 <DowngradeTeamRemovalModal
-                    product_id={'prod_starter'}
-                    starterProduct={state.entities.cloud.products.prod_starter}
+                    product_id={'prod_free'}
+                    starterProduct={state.entities.cloud.products.prod_free}
                 />
             </redux.Provider>,
         );
@@ -341,8 +341,8 @@ describe('components/pricing_modal/downgrade_team_removal_modal', () => {
         renderWithIntl(
             <redux.Provider store={store}>
                 <DowngradeTeamRemovalModal
-                    product_id={'prod_starter'}
-                    starterProduct={state.entities.cloud.products.prod_starter}
+                    product_id={'prod_free'}
+                    starterProduct={state.entities.cloud.products.prod_free}
                 />
             </redux.Provider>,
         );
@@ -356,8 +356,8 @@ describe('components/pricing_modal/downgrade_team_removal_modal', () => {
         renderWithIntl(
             <redux.Provider store={store}>
                 <DowngradeTeamRemovalModal
-                    product_id={'prod_starter'}
-                    starterProduct={state.entities.cloud.products.prod_starter}
+                    product_id={'prod_free'}
+                    starterProduct={state.entities.cloud.products.prod_free}
                 />
             </redux.Provider>,
         );
