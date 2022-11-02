81eb980d53b4086e0c3aad31cceb3f8d94b90a9d
Babinder Rathi <102028591+babinderrathi@users.noreply.github.com>
2022-11-02 07:03:37 +0530

    MM-47056: Migrate "components/setting_item_max.jsx" to Typescript (#11329)
    * migrate setting item max to typescript
    
    * update snapshot
    
    * replace any with HTMLElement
    
    * apply recommended changes
    
    * conflicts resolved
    
    * fix lint issues
    
    Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

--- components/user_settings/general/user_settings_general.tsx
+++ components/user_settings/general/user_settings_general.tsx
@@ -5,19 +5,21 @@
 import React from 'react';
 import {defineMessages, FormattedDate, FormattedMessage, injectIntl, IntlShape} from 'react-intl';
 
+import SettingItemMax from 'components/setting_item_max';
+
 import {isEmail} from 'mattermost-redux/utils/helpers';
-import {UserProfile} from '@mattermost/types/users';
 
 import {trackEvent} from 'actions/telemetry_actions.jsx';
 import * as Utils from 'utils/utils';
 import {t} from 'utils/i18n';
 
 import LocalizedIcon from 'components/localized_icon';
-import SettingItemMax from 'components/setting_item_max.jsx';
 import SettingItemMin from 'components/setting_item_min';
 import SettingPicture from 'components/setting_picture.jsx';
 import LoadingWrapper from 'components/widgets/loading/loading_wrapper';
 import {AnnouncementBarMessages, AnnouncementBarTypes, AcceptedProfileImageTypes, Constants, ValidationErrors} from 'utils/constants';
+
+import {UserProfile} from '@mattermost/types/users';
 
 const holders = defineMessages({
     usernameReserved: {
