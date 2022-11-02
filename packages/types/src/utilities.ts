6735d2ec4c547c69ced8f044d86300ff8a4b870d
Julien Tant <julien@craftyx.fr>
2022-11-02 14:25:52 -0700

    remove unused type utility

--- packages/types/src/utilities.ts
+++ packages/types/src/utilities.ts
@@ -24,9 +24,3 @@ export type ValueOf<T> = T[keyof T];
  */
 export type RequireOnlyOne<T, Keys extends keyof T = keyof T> =
 Pick<T, Exclude<keyof T, Keys>> & {[K in Keys]-?: Required<Pick<T, K>> & Partial<Record<Exclude<Keys, K>, undefined>>}[Keys];
-
-/**
- * Based on https://stackoverflow.com/a/49725198
- */
-export type RequireAtLeastOne<T, Keys extends keyof T = keyof T> =
-    Pick<T, Exclude<keyof T, Keys>> & {[K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>}[Keys]
