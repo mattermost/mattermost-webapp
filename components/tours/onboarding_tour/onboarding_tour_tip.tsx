838b147ce66e0eba5f1292f26b6d35f390c1776c
Pablo Velez Vidal <pablovv2016@gmail.com>
2022-11-09 17:54:38 +0100

    fix issue loading guest tour

--- components/tours/onboarding_tour/onboarding_tour_tip.tsx
+++ components/tours/onboarding_tour/onboarding_tour_tip.tsx
@@ -2,21 +2,14 @@
 // See LICENSE.txt for license information.
 
 import React from 'react';
-import {useSelector} from 'react-redux';
-
-import {GlobalState} from '@mattermost/types/store';
-
-import {isCurrentUserGuestUser} from 'mattermost-redux/selectors/entities/users';
 
 import {ChannelsTourTip, ChannelsTourTipProps, TutorialTourName} from 'components/tours';
 
 const OnboardingTourTip = (props: Omit<ChannelsTourTipProps, 'tourCategory'>) => {
-    // restrict the 'learn more about messaging' tour when user is guest (townSquare, channel creation and user invite are restricted to guest)
-    const isGuestUser = useSelector((state: GlobalState) => isCurrentUserGuestUser(state));
     return (
         <ChannelsTourTip
             {...props}
-            tourCategory={isGuestUser ? TutorialTourName.ONBOARDING_TUTORIAL_STEP_FOR_GUESTS : TutorialTourName.ONBOARDING_TUTORIAL_STEP}
+            tourCategory={TutorialTourName.ONBOARDING_TUTORIAL_STEP}
         />
     );
 };
