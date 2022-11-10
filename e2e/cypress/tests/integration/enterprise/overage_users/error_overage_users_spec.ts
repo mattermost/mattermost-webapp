ef16bf71ec9a3965fad48e27dc22e7a491b40c2a
Said Atrahouch <said.atrahouch@gmail.com>
2022-11-10 12:36:52 +0100

    yasser comments

--- e2e/cypress/tests/integration/enterprise/overage_users/error_overage_users_spec.ts
+++ e2e/cypress/tests/integration/enterprise/overage_users/error_overage_users_spec.ts
@@ -7,6 +7,8 @@ const seatsPurchased = parseInt(Cypress.env('numberOfTrialUsers'), 10);
 const seatsMinimumFor10PercentageState = (Math.ceil(seatsPurchased * 0.1)) + seatsPurchased;
 const emailTest = 'test@mattermost.com';
 const text10PercentageState = `Your workspace user count has exceeded your paid license seat count by ${seatsMinimumFor10PercentageState - seatsPurchased} seats. Purchase additional seats to remain compliant.`;
+const seatsMinimumFor5PercentageState = (Math.ceil(seatsPurchased * 0.05)) + seatsPurchased;
+const text5PercentageState = `Your workspace user count has exceeded your paid license seat count by ${seatsMinimumFor5PercentageState - seatsPurchased} seats. Purchase additional seats to remain compliant.`;
 
 const withEmailLicense = (email = emailTest) => {
     cy.intercept('GET', '**/api/v4/license/client', {
@@ -24,6 +26,59 @@ const withEmailLicense = (email = emailTest) => {
         },
     });
 };
+
+describe('Global Info Banner overage users', () => {
+    let url: string;
+    let createdUser: UserProfile;
+
+    beforeEach(() => {
+        cy.intercept('GET', '**/api/v4/analytics/old', {
+            statusCode: 200,
+            body: [
+                {name: 'channel_open_count', value: 2},
+                {name: 'channel_private_count', value: 0},
+                {name: 'post_count', value: 3},
+                {name: 'unique_user_count', value: seatsMinimumFor5PercentageState},
+                {name: 'team_count', value: 1},
+                {name: 'total_websocket_connections', value: 0},
+                {name: 'total_master_db_connections', value: 12},
+                {name: 'total_read_db_connections', value: 0},
+                {name: 'daily_active_users', value: 1},
+                {name: 'monthly_active_users', value: 1},
+                {name: 'inactive_user_count', value: 0},
+            ],
+        });
+
+        cy.apiInitSetup().then(({user, offTopicUrl}) => {
+            url = offTopicUrl;
+            createdUser = user;
+            cy.apiAdminLogin();
+            cy.visit(url);
+        });
+    });
+
+    it('should show the banner with CTA because it\'s the admin who purchased the license', () => {
+        withEmailLicense(createdUser.email);
+        cy.apiLogout();
+        cy.apiLogin(createdUser);
+        cy.visit(url);
+
+        // * Check the cta banner exist
+        cy.findByText(text5PercentageState).should('exist');
+        cy.findByText('Contact Sales').should('exist');
+    });
+
+    it('should show the banner without CTA because it isn\'t the admin who purchased the license', () => {
+        withEmailLicense();
+        cy.apiLogout();
+        cy.apiLogin(createdUser);
+        cy.visit(url);
+
+        // * Check the cta banner exist
+        cy.findByText(text5PercentageState).should('exist');
+        cy.findByText('Contact Sales').should('not.exist');
+    });
+});
 
 describe('Global Error Banner overage users', () => {
     let url: string;
@@ -51,7 +106,6 @@ describe('Global Error Banner overage users', () => {
             url = offTopicUrl;
             createdUser = user;
             cy.apiAdminLogin();
-            cy.apiDeleteLicense();
             cy.visit(url);
         });
     });
