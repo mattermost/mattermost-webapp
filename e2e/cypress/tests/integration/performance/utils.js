07ae625b2f962a3260c022f71e30c78c0092d78c
Nevyana Angelova <nevyangelova@Nevyanas-MacBook-Pro.local>
2022-11-16 19:45:27 +0200

    Errors while rebasing

--- e2e/cypress/tests/integration/performance/utils.js
+++ e2e/cypress/tests/integration/performance/utils.js
@@ -1,26 +1,26 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export const measurePerformance = (startMark, upperLimit, endMark = undefined, runs = 5) => {
+export const measurePerformance = (name, upperLimit, callback, runs = 5) => {
     return cy.window().its('performance').then(async (performance) => {
         const durations = [];
 
-        for (let i = 0; i < runs; i++) {
-            const startName = `${startMark}_run_${i}`;
-            if (endMark) {
-                const endName = `${endMark}_run_${i}`;
-                performance.mark(endName);
-                performance.measure(startName, endName);
-            }
-            performance.mark(startName);
-            performance.measure(startName);
+        Cypress._.times(runs, (i) => {
+            const measureName = `${name}_run_${i}`;
+            performance.mark(measureName);
+            callback();
+            performance.measure(measureName);
 
-            const measure = performance.getEntriesByName(startName)[0];
+            const measure = performance.getEntriesByName(measureName)[0];
             durations.push(measure.duration);
-        }
+        });
 
         const avgDuration = Math.round(durations.reduce((a, b) => a + b) / runs);
-        cy.log(`[PERFORMANCE] ${startMark}: ${avgDuration}ms`, avgDuration <= upperLimit ? ' within upper limit' : ' outside upper limit', ` of ${upperLimit}ms`);
+        cy.log(`[PERFORMANCE] ${name}: ${avgDuration}ms`, avgDuration <= upperLimit ? ' within upper limit' : ' outside upper limit', ` of ${upperLimit}ms`);
         assert.isAtMost(avgDuration, upperLimit);
+
+        // Clean the marks
+        performance.clearMarks();
+        performance.clearMeasures();
     });
 };
