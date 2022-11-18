126ef4b5772de3f04817fb843475247bb9869653
babinder rathi <babinderrathi@gmail.com>
2022-11-18 14:21:03 +0530

    applying some changes

--- components/sidebar/channel_filter/channel_filter.tsx
+++ components/sidebar/channel_filter/channel_filter.tsx
@@ -66,7 +66,7 @@ export class ChannelFilter extends React.PureComponent<Props> {
             tooltipMessage = intl.formatMessage({id: 'sidebar_left.channel_filter.showAllChannels', defaultMessage: 'Show all channels'});
         }
 
-        const unreadsAriaLabel = intl.formatMessage({id: 'sidebar_left.channel_filter.filter_unread_aria', defaultMessage: 'unreads filter button'});
+        const unreadsAriaLabel = intl.formatMessage({id: 'sidebar_left.channel_filter.filterUnreadAria', defaultMessage: 'unreads filter button'});
 
         const tooltip = (
             <Tooltip
@@ -95,7 +95,7 @@ export class ChannelFilter extends React.PureComponent<Props> {
                             active: unreadFilterEnabled,
                         })}
                         onClick={this.toggleUnreadFilter}
-                        aria-label={unreadAriaLabel}
+                        aria-label={unreadsAriaLabel}
                     >
                         <i className='icon icon-filter-variant'/>
                     </a>
