568a3db6ba1a8b3f2fcb6d98568e047ad37bd004
abhinavVihan <abhinavchaudhary348@gmail.com>
2022-11-19 16:49:09 +0530

    leave and archive channel display as destructive e2e tests

--- e2e/cypress/tests/integration/channel/leave_and_archive_channel_destructive_spec.ts
+++ e2e/cypress/tests/integration/channel/leave_and_archive_channel_destructive_spec.ts
@@ -1,6 +1,8 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import {UserProfile} from 'mattermost-redux/types/users';
+
 // ***************************************************************
 // - [#] indicates a test step (e.g. # Go to a page)
 // - [*] indicates an assertion (e.g. * Check the title)
@@ -11,11 +13,10 @@
 // Group: @channel
 
 describe('Leave and Archive channel actions display as destructive', () => {
-    let testUser;
-    let offTopicUrl;
+    let testUser: UserProfile;
+    let offTopicUrl: string;
 
     before(() => {
-
         // # Login as test user and visit off-topic channel
         cy.apiInitSetup().then(({user, offTopicUrl: url}) => {
             testUser = user;
@@ -27,103 +28,78 @@ describe('Leave and Archive channel actions display as destructive', () => {
     });
 
     it('MM-T4943_1 Leave and Archive channel actions display as destructive in the channel dropdown menu', () => {
-        // # Post a message in the channel
-        cy.postMessage('These tests are written by Abi Vihan');
-
-        // Click on the channel name to open menu dropdown
+        // # click on channel drop-down menu
         cy.get('#channelHeaderTitle').should('be.visible').click();
 
-        // verify that the first option in the menu is 'View Info'
-        // cy.focused().tab().then(text => {
-        //     expect(text).to.contain('View Info');
-        // })
-        cy.findByText('View Info').should('be.visible')       
-        
-        
-        // press tab() for next option in the menu i.e. 'Move to...'
-        // 'Move to...' opens a sub-menu
-        // cy.focused().tab().then(text => {
-        //     expect(text).to.contain('Move to...');
-        // }).type('{enter}');
-        cy.findByText('Move to...').should('be.visible')
-        
-        
-        // verify all its options in the sub-menu
-        // verify first option in the sub-menu i.e.'Favorites'
-        // cy.focused().tab().then(text => {
-        //     expect(text).to.contain('Favorites');
-        // })
-        // cy.findByText('Favorites').should('be.visible')
-        
-        
-        // press tab() for next option in the sub-menu i.e.'Channels'
-        // cy.focused().tab().then(text => {
-        //     expect(text).to.contain('Channels');
-        // })
-        // cy.findByText('Channels').should('be.visible')
-        
-        
-        // pressing tab() here would take the focus to the border-divider in the sub-menu
-        // cy.focused().tab();
-        // .type('{tab}')  
-        // cy.findByText('New Category').should('be.visible')
+        // // * View Info menu option should be visible;
+        cy.findByText('View Info').should('be.visible');
 
-        // press tab() for next option in the sub-menu i.e.'New Category'
-        // cy.focused().tab().then(text => {
-        //     expect(text).to.contain('New Category');
-        // })
+        // // * Move to... menu option should be visible;
+        cy.findByText('Move to...').should('be.visible').children().trigger('mouseover');
 
-        // all 3 options in the sub-menu have been verified
-        // press tab() to take the focus back to the main menu
-        // verifiy next option in the menu i.e.'Notification Preferences'
-        // cy.focused().tab().then(text => {
-        //     expect(text).to.contain('Notification Preferences');
-        // })
-        cy.findByText('Notification Preferences').should('be.visible')
+        // // * Favorites Sub-menu option should be visible;
+        cy.findByText('Favorites').should('be.visible');
 
-        cy.findByText('Mute Channel').should('be.visible')
-        cy.findByText('Add Members').should('be.visible')
+        // // * New Category Sub-menu option should be visible;
+        cy.findByText('New Category').should('be.visible');
 
+        // // * Notification Preferences menu option should be visible;
+        cy.get('#channelNotificationPreferences').should('be.visible');
 
-        // cy.focused().tab().then(text => {
-        //     expect(text).to.contain('Mute Channel');
-        // })
+        // // * Mute Channel menu option should be visible;
+        cy.get('#channelToggleMuteChannel').should('be.visible');
 
-        // cy.focused().tab().then(text => {
-        //     expect(text).to.contain('Add Members');
-        // })
+        // // * Add Members menu option should be visible;
+        cy.get('#channelAddMembers').should('be.visible');
 
-        // cy.focused().tab().then(text => {
-        //     expect(text).to.contain('Manage Members');
-        // })
-        cy.findByText('Manage Members').should('be.visible')
+        // // * Manage Members menu option should be visible;
+        cy.get('#channelManageMembers').should('be.visible');
 
+        // // * Edit Channel Header menu option should be visible;
+        cy.get('#channelEditHeader').should('be.visible');
 
-        // cy.focused().tab().then(text => {
-        //     expect(text).to.contain('Edit Channel Header');
-        // })
-        cy.findByText('Edit Channel Header').should('be.visible')
+        // // * Edit Channel Purpose menu option should be visible;
+        cy.get('#channelEditPurpose').should('be.visible');
 
-        // cy.focused().tab().then(text => {
-        //     expect(text).to.contain('Edit Channel Purpose');
-        // })
+        // // * Rename Channel menu option should be visible;
+        cy.get('#channelRename').should('be.visible');
 
-        // cy.focused().tab().then(text => {
-        //     expect(text).to.contain('Rename Channel');
-        // })
-        cy.findByText('Rename Channel').should('be.visible')
+        // // * Archive Channel menu option should be visible and hav a background-color (destructive);
+        cy.get('#channelArchiveChannel').should('be.visible').children().focus().should('have.css', 'background-color', 'rgb(210, 75, 78)');
 
-        // cy.findByText('Archive Channel').should('be.visible').trigger('mouseover', {force: true})
-        cy.get('#channelArchiveChannel').should('be.visible').trigger('mouseover', {force: true});
+        // // * Leave Channel menu option should be visible and hav a background-color (destructive);
+        cy.get('#channelLeaveChannel').should('be.visible').children().focus().should('have.css', 'background-color', 'rgb(210, 75, 78)');
+    });
 
+    it('MM-T4943_2 Leave channel actions display as destructive in the Edit Channel Menu ', () => {
+        // # Open Edit Channel Menu and verify menu optins
+        cy.get('#sidebarItem_off-topic').find('.SidebarMenu_menuButton').click({force: true});
 
-        // // cy.focused().tab().then(text => {
-        // //     expect(text).to.contain('Archive Channel');
-        // // }).trigger('mouseover').should('have.css', 'background-color').and('eq', '#d24b4e')
+        // // * Favorite menu option should be visible;
+        cy.findByText('Favorite').should('be.visible');
 
-        // // cy.findByText('Archive Channel').invoke('show').trigger('mouseover')
-        // // cy.get('.MenuItem__dangerous').should('have.css', 'background-color').and('eq', '#d24b4e')
-        // // cy.get('[aria-label="Archive Channel dialog"]').trigger('mouseover')
-        // cy.get('#channelArchiveChannel').trigger('mouseover')
+        // // * Mute Channel menu option should be visible;
+        cy.findByText('Mute Channel').should('be.visible');
+
+        // // * Move to... menu option should be visible;
+        cy.findByText('Move to...').should('be.visible').trigger('mouseover');
+
+        // // * Favorites Sub-menu option should be visible;
+        cy.findByText('Favorites').should('be.visible');
+
+        // // * New Category Sub-menu option should be visible;
+        cy.findByText('New Category').should('be.visible');
+
+        // // * Copy Link menu option should be visible;
+        cy.findByText('Copy Link').should('be.visible');
+
+        // // * Add Members menu option should be visible;
+        cy.findByText('Add Members').should('be.visible');
+
+        // // * Leave Channel menu option should be visible and hav a color (destructive);
+        cy.findByText('Leave Channel').should('be.visible').should('have.css', 'color', 'rgb(210, 75, 78)');
+
+        // // * Leave Channel menu option should be visible and hav a background-color (destructive);
+        cy.findByText('Leave Channel').should('be.visible').parent().focus().should('have.css', 'background-color', 'rgb(210, 75, 78)');
     });
 });
