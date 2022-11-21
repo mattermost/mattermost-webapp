dde8f33ae61248dff7d9275e1c11421fc354182c
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2022-11-21 06:25:59 +0530

    chore: fix cloud tests

--- e2e/cypress/tests/integration/enterprise/cloud/billing/billing_history_free_trial_spec.js
+++ e2e/cypress/tests/integration/enterprise/cloud/billing/billing_history_free_trial_spec.js
@@ -10,12 +10,146 @@
 // Stage: @prod
 // Group: @cloud_only @cloud_trial
 
-const dayJs = require('dayjs');
+function simulateSubscription() {
+    cy.intercept('GET', '**/api/v4/cloud/subscription/invoices', {
+        statusCode: 200,
+        body: [
+            {
+                id: 'in_1Lz8b0I67GP2qpb43kcuMyFP',
+                number: '8D53267B-0006',
+                create_at: 1667263490000,
+                total: 65,
+                tax: 0,
+                status: 'open',
+                description: '',
+                period_start: 1664582400000,
+                period_end: 1667260800000,
+                subscription_id: 'prod_K0AxuWCDoDD9Qq',
+                line_items: [
+                    {
+                        price_id: 'price_1KLUYiI67GP2qpb48DXFukcJ',
+                        total: 65,
+                        quantity: 0.06451612903225806,
+                        price_per_unit: 1000,
+                        description: 'Cloud Professional',
+                        type: 'metered',
+                        metadata: {
+                            current_product_id: 'prod_LSBESgGXq9KlLj',
+                            'cws-date-converted-to-paid': '1660215462',
+                            'cws-dns': 'testsaid.test.mattermost.cloud',
+                            'cws-installation': 'xhp8uy46gtngfpa4ant5b8f1ho',
+                            'cws-installation-state': 'stable',
+                        },
+                    },
+                ],
+                current_product_name: 'Cloud Professional',
+            },
+            {
+                id: 'in_1LntnKI67GP2qpb4VObu3NgP',
+                number: '8D53267B-0005',
+                create_at: 1664584986000,
+                total: 733,
+                tax: 0,
+                status: 'failed',
+                description: '',
+                period_start: 1661990400000,
+                period_end: 1664582400000,
+                subscription_id: 'prod_K0AxuWCDoDD9Qq',
+                line_items: [
+                    {
+                        price_id: 'price_1KLUZ2I67GP2qpb45uTS89eb',
+                        total: 733,
+                        quantity: 0.7333333333333333,
+                        price_per_unit: 999,
+                        description: 'Cloud Professional',
+                        type: 'metered',
+                        metadata: {
+                            current_product_id: 'prod_LSBESgGXq9KlLj',
+                            'cws-date-converted-to-paid': '1660215462',
+                            'cws-dns': 'testsaid.test.mattermost.cloud',
+                            'cws-installation': 'xhp8uy46gtngfpa4ant5b8f1ho',
+                            'cws-installation-state': 'stable',
+                        },
+                    },
+                ],
+                current_product_name: 'Cloud Professional',
+            },
+            {
+                id: 'in_1LntnKI67GP2qpb4VObu3NgV',
+                number: '8D53267B-0005',
+                create_at: 1664584986000,
+                total: 733,
+                tax: 0,
+                status: 'paid',
+                description: '',
+                period_start: 1661990400000,
+                period_end: 1664582400000,
+                subscription_id: 'prod_K0AxuWCDoDD9Qq',
+                line_items: [
+                    {
+                        price_id: 'price_1KLUZ2I67GP2qpb45uTS89eb',
+                        total: 733,
+                        quantity: 0.7333333333333333,
+                        price_per_unit: 999,
+                        description: 'Cloud Professional',
+                        type: 'metered',
+                        metadata: {
+                            current_product_id: 'prod_LSBESgGXq9KlLj',
+                            'cws-date-converted-to-paid': '1660215462',
+                            'cws-dns': 'testsaid.test.mattermost.cloud',
+                            'cws-installation': 'xhp8uy46gtngfpa4ant5b8f1ho',
+                            'cws-installation-state': 'stable',
+                        },
+                    },
+                ],
+                current_product_name: 'Cloud Professional',
+            },
+        ],
+    });
+    cy.intercept('GET', '**/api/v4/cloud/subscription', {
+        statusCode: 200,
+        body: {
+            id: 'sub_test1',
+            is_free_trial: 'true',
+            customer_id: '5zqhakmibpgyix9juiwwkpfnmr',
+            product_id: 'prod_K0AxuWCDoDD9Qq',
+            seats: 25,
+            status: 'active',
+        },
+    });
+
+    cy.intercept('GET', '**/api/v4/cloud/products**', {
+        statusCode: 200,
+        body:
+            [
+                {
+                    id: 'prod_LSBESgGXq9KlLj',
+                    sku: 'cloud-starter',
+                    price_per_seat: 0,
+                    name: 'Cloud Free',
+                },
+                {
+                    id: 'prod_K0AxuWCDoDD9Qq',
+                    sku: 'cloud-professional',
+                    price_per_seat: 10,
+                    name: 'Cloud Professional',
+                },
+                {
+                    id: 'prod_Jh6tBLcgWWOOog',
+                    sku: 'cloud-enterprise',
+                    price_per_seat: 30,
+                    name: 'Cloud Enterprise',
+                },
+            ],
+    });
+}
 
 describe('System Console - Billing History', () => {
     let productSubscription;
 
     before(() => {
+        simulateSubscription();
+
         // * Check if server has license for Cloud
         cy.apiRequireLicenseForFeature('Cloud');
 
@@ -26,21 +160,23 @@ describe('System Console - Billing History', () => {
         cy.contains('.admin-console__header', 'Billing History').should('be.visible');
     });
 
-    beforeEach(() => {
-        cy.apiGetCloudProducts().then(({products}) => {
-            cy.apiGetCloudSubscription().then(({subscription}) => {
-                productSubscription = products.find((product) => product.id === subscription.product_id);
-            });
-        });
-    });
+    it.only('MM-T3491_1 Invoice is shown in a table', () => {
+        cy.get('tr.BillingHistory__table-row').as('tableRows');
 
-    it('MM-T3491_1 Invoice is shown in a table', () => {
-        const currentDate = new Date();
+        // * Check the first row where payment is pending
+        cy.get('@tableRows').eq(0).find('td').eq(0).should('have.text', '10/01/2022');
+        cy.get('@tableRows').eq(0).find('td.BillingHistory__table-total').should('have.text', '$0.65');
+        cy.get('@tableRows').eq(0).find('div.BillingHistory__paymentStatus').should('have.text', 'Pending');
 
-        // * Check for default record in grid
-        cy.contains('td:nth-of-type(1) span', dayJs(new Date(currentDate.getFullYear(), currentDate.getMonth(), 1)).format('MM/DD/YYYY')).should('exist');
-        cy.contains('td:nth-of-type(3) span', '$0.00').should('exist');
-        cy.contains('td:nth-of-type(4) span', 'Paid').should('exist');
+        // * Check the first row where payment has failed
+        cy.get('@tableRows').eq(1).find('td').eq(0).should('have.text', '09/01/2022');
+        cy.get('@tableRows').eq(1).find('td.BillingHistory__table-total').should('have.text', '$7.33');
+        cy.get('@tableRows').eq(1).find('div.BillingHistory__paymentStatus').should('have.text', 'Payment failed');
+
+        // * Check the first row where payment was successfull
+        cy.get('@tableRows').eq(2).find('td').eq(0).should('have.text', '09/01/2022');
+        cy.get('@tableRows').eq(2).find('td.BillingHistory__table-total').should('have.text', '$7.33');
+        cy.get('@tableRows').eq(2).find('div.BillingHistory__paymentStatus').should('have.text', 'Paid');
     });
 
     it('MM-T3491_2 Validate the contents of downloaded PDF invoice', () => {
