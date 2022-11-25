cea7e6f5f72db98761e1e641043a4b60cef4a8c3
EricssonLiu <ericssonliu10@gmail.com>
2022-11-25 03:36:55 -0500

    filter before sorting

--- packages/mattermost-redux/src/selectors/entities/threads.ts
+++ packages/mattermost-redux/src/selectors/entities/threads.ts
@@ -168,7 +168,7 @@ export const getUnreadThreadOrderInCurrentTeam: (
 );
 
 function sortByLastReply(ids: Array<UserThread['id']>, threads: ReturnType<typeof getThreads>) {
-    return ids.sort((a, b) => threads[b].last_reply_at - threads[a].last_reply_at).filter((id) => threads[id].last_reply_at !== 0);
+    return ids.filter((id) => threads[id].last_reply_at !== 0).sort((a, b) => threads[b].last_reply_at - threads[a].last_reply_at);
 }
 
 export const getThreadsInChannel: (
