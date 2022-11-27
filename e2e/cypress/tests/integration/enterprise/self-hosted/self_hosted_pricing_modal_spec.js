580374ddf86fbe52bad4d252b41e88114e629eb4
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2022-11-28 00:38:06 +0530

    test -37

--- e2e/cypress/tests/integration/enterprise/self-hosted/self_hosted_pricing_modal_spec.js
+++ e2e/cypress/tests/integration/enterprise/self-hosted/self_hosted_pricing_modal_spec.js
@@ -39,7 +39,7 @@ describe('Self hosted Pricing modal', () => {
             urlL = url;
             createdUser = user;
             cy.apiAdminLogin();
-            cy.apiDeleteLicense('Cloud');
+            cy.apiDeleteLicense();
             cy.visit(url);
         });
     });
