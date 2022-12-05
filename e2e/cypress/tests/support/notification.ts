86b6c0cf7fce049a87668c57a6ecaa4fe1cf7467
zuhair <zuhairhussain574@gmail.com>
2022-12-05 21:18:11 +0300

    Removed NotificationWindow interface as it is not needed

--- e2e/cypress/tests/support/notification.ts
+++ e2e/cypress/tests/support/notification.ts
@@ -1,13 +1,9 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-export interface NotificationWindow extends Window {
-    Notification(title: string, opts?: NotificationOptions): void;
-}
-
 // Stub the browser notification API with the given name and permission
 export function spyNotificationAs(name: string, permission: string) {
-    cy.window().then((win: NotificationWindow) => {
+    cy.window().then((win: any) => {
         function Notification(title: string, opts?: NotificationOptions) {
             this.title = title;
             this.opts = opts;
