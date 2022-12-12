5e21ddcd9eb0f2707cdb768bb79bb46c0d5717c6
Ben Cooke <benkcooke@gmail.com>
2022-12-12 14:44:39 -0500

    [MM-48987] Removing team insights if no enterprise build available (#11778)
    * removing team insights is no enterprise build available
    
    * fix hook
    
    * adding depenency to useEffect
    
    Co-authored-by: Benjamin Cooke <benjamincooke@Benjamins-MacBook-Pro.local>
    (cherry picked from commit f039ee039bbfa0529933aa119dc268a46b289a9f)

--- components/activity_and_insights/insights/hooks.ts
+++ components/activity_and_insights/insights/hooks.ts
@@ -12,7 +12,7 @@ import {CloudProducts} from 'utils/constants';
 /**
  * Returns some checks for free trial users or starter licenses
  */
-export function useLicenseChecks(): [boolean, boolean] {
+export function useLicenseChecks(): {isStarterFree: boolean; isFreeTrial: boolean; isEnterpriseReady: boolean} {
     const subscription = useSelector(getCloudSubscription);
     const license = useSelector(getLicense);
     const subscriptionProduct = useSelector(getSubscriptionProduct);
@@ -29,8 +29,9 @@ export function useLicenseChecks(): [boolean, boolean] {
     const isStarterFree = isCloudStarterFree || isSelfHostedStarter;
     const isFreeTrial = isCloudFreeTrial || isSelfHostedFreeTrial;
 
-    return [
+    return {
         isStarterFree,
         isFreeTrial,
-    ];
+        isEnterpriseReady,
+    };
 }
