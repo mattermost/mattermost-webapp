bc545e9782fcd5e04c0910d3ca5e20ab680b01c3
Md_ZubairAhmed <m-zubairahmed@protonmail.com>
2023-01-03 13:43:26 +0530

    update test

--- components/activity_and_insights/insights/top_reactions/top_reactions.test.tsx
+++ components/activity_and_insights/insights/top_reactions/top_reactions.test.tsx
@@ -104,6 +104,9 @@ describe('components/activity_and_insights/insights/top_reactions', () => {
     test('check if empty', async () => {
         const store = await mockStore({
             entities: {
+                general: {
+                    config: {},
+                },
                 teams: {
                     currentTeamId: 'team_id1',
                 },
