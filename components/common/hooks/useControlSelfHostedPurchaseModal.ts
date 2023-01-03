187a0e0f56666c1058d1079da780454df95f6533
Nathaniel Allred <neallred@protonmail.com>
2023-01-03 08:59:50 -0600

    Simplify form checking, add confirmed_intent state, show modal to admins when purchase is already in progress (#11879)
    * Handle confirmed intent case
    * Admins are presented with a purchase in progress modal when purchase is already in progress

--- components/common/hooks/useControlSelfHostedPurchaseModal.ts
+++ components/common/hooks/useControlSelfHostedPurchaseModal.ts
@@ -2,14 +2,18 @@
 // See LICENSE.txt for license information.
 
 import {useMemo} from 'react';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
 import {trackEvent} from 'actions/telemetry_actions';
-import {closeModal} from 'actions/views/modals';
+import {closeModal, openModal} from 'actions/views/modals';
 import {ModalIdentifiers, TELEMETRY_CATEGORIES} from 'utils/constants';
-import SelfHostedPurchaseModal from 'components/self_hosted_purchase_modal';
+import SelfHostedPurchaseModal, {STORAGE_KEY_PURCHASE_IN_PROGRESS} from 'components/self_hosted_purchase_modal';
+import PurchaseInProgressModal from 'components/purchase_in_progress_modal';
 import {Client4} from 'mattermost-redux/client';
+import {getCurrentUserEmail} from 'mattermost-redux/selectors/entities/common';
 import {HostedCustomerTypes} from 'mattermost-redux/action_types';
+
+import {makeGetItem} from 'selectors/storage';
 
 import {useControlModal, ControlModal} from './useControlModal';
 
@@ -21,6 +25,8 @@ interface HookOptions{
 
 export default function useControlSelfHostedPurchaseModal(options: HookOptions): ControlModal {
     const dispatch = useDispatch();
+    const userEmail = useSelector(getCurrentUserEmail);
+    const purchaseInProgress = useSelector(makeGetItem(STORAGE_KEY_PURCHASE_IN_PROGRESS, '')) === 'true';
     const controlModal = useControlModal({
         modalId: ModalIdentifiers.SELF_HOSTED_PURCHASE,
         dialogType: SelfHostedPurchaseModal,
@@ -28,10 +34,26 @@ export default function useControlSelfHostedPurchaseModal(options: HookOptions):
             productId: options.productId,
         },
     });
+
     return useMemo(() => {
         return {
             ...controlModal,
             open: async () => {
+                // check if user already has an open purchase modal in current browser.
+                if (purchaseInProgress) {
+                    // User within the same browser session
+                    // is already trying to purchase. Notify them of this
+                    // and request the exit that purchase flow before attempting again.
+                    dispatch(openModal({
+                        modalId: ModalIdentifiers.PURCHASE_IN_PROGRESS,
+                        dialogType: PurchaseInProgressModal,
+                        dialogProps: {
+                            purchaserEmail: userEmail,
+                        },
+                    }));
+                    return;
+                }
+
                 trackEvent(TELEMETRY_CATEGORIES.SELF_HOSTED_PURCHASING, 'click_open_purchase_modal', {
                     callerInfo: options.trackingLocation,
                 });
@@ -40,10 +62,26 @@ export default function useControlSelfHostedPurchaseModal(options: HookOptions):
                 }
                 try {
                     const result = await Client4.bootstrapSelfHostedSignup();
+
+                    if (result.email !== userEmail) {
+                        // JWT already exists and was created by another admin,
+                        // meaning another admin is already trying to purchase.
+                        // Notify user of this and do not allow them to try to purchase concurrently.
+                        dispatch(openModal({
+                            modalId: ModalIdentifiers.PURCHASE_IN_PROGRESS,
+                            dialogType: PurchaseInProgressModal,
+                            dialogProps: {
+                                purchaserEmail: result.email,
+                            },
+                        }));
+                        return;
+                    }
+
                     dispatch({
                         type: HostedCustomerTypes.RECEIVED_SELF_HOSTED_SIGNUP_PROGRESS,
                         data: result.progress,
                     });
+
                     dispatch(closeModal(ModalIdentifiers.PRICING_MODAL));
                     controlModal.open();
                 } catch (e) {
@@ -52,5 +90,5 @@ export default function useControlSelfHostedPurchaseModal(options: HookOptions):
                 }
             },
         };
-    }, [controlModal, options.productId, options.onClick, options.trackingLocation]);
+    }, [controlModal, options.productId, options.onClick, options.trackingLocation, purchaseInProgress]);
 }
