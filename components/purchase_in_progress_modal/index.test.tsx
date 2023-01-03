44a5ef61edae7a79fb4705c441739659d9bc9dcc
neallred <neallred@protonmail.com>
2023-01-03 16:19:39 -0600

    lint

--- components/purchase_in_progress_modal/index.test.tsx
+++ components/purchase_in_progress_modal/index.test.tsx
@@ -11,17 +11,18 @@ import {GlobalState} from 'types/store';
 import {TestHelper as TH} from 'utils/test_helper';
 import * as storageActions from 'actions/storage';
 
+import {STORAGE_KEY_PURCHASE_IN_PROGRESS} from 'components/self_hosted_purchase_modal/constants';
+
 import PurchaseInProgressModal from './';
 
-import {STORAGE_KEY_PURCHASE_IN_PROGRESS} from 'components/self_hosted_purchase_modal/constants';
 jest.mock('mattermost-redux/client', () => {
     const original = jest.requireActual('mattermost-redux/client');
     return {
         __esModule: true,
         ...original,
         Client4: {
-          ...original,
-          bootstrapSelfHostedSignup: jest.fn(),
+            ...original,
+            bootstrapSelfHostedSignup: jest.fn(),
         },
     };
 });
@@ -53,7 +54,7 @@ const initialState: DeepPartial<GlobalState> = {
 
 describe('PurchaseInProgressModal', () => {
     it('when purchaser and user emails are different, user is instructed to wait', () => {
-        const stateOverride: DeepPartial<GlobalState> = JSON.parse(JSON.stringify(initialState))
+        const stateOverride: DeepPartial<GlobalState> = JSON.parse(JSON.stringify(initialState));
         stateOverride.entities!.users!.currentUserId = 'otherUserId';
         renderWithIntlAndStore(<div id='root-portal'><PurchaseInProgressModal purchaserEmail={'admin@example.com'}/></div>, stateOverride);
 
@@ -69,4 +70,4 @@ describe('PurchaseInProgressModal', () => {
         screen.getByText('Reset purchase flow').click();
         expect(removeItemSpy).toHaveBeenCalledWith(STORAGE_KEY_PURCHASE_IN_PROGRESS);
     });
-})
+});
