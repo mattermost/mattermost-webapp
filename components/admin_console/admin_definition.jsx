a532ccd44eb14e526feb8743dea9d1fb1dd18ede
Mattermost Build <build@mattermost.com>
2023-01-05 18:19:11 +0200

    [MM-47489] Move Shared Channels (Experimental) to Professional (#11799) (#11894)
    Automatic Merge

--- components/admin_console/admin_definition.jsx
+++ components/admin_console/admin_definition.jsx
@@ -6885,7 +6885,11 @@ const AdminDefinition = {
                         help_text: t('admin.experimental.enableSharedChannels.desc'),
                         help_text_default: 'Toggles Shared Channels',
                         help_text_markdown: false,
-                        isHidden: it.not(it.licensedForFeature('SharedChannels')),
+                        isHidden: it.not(it.any(
+                            it.licensedForFeature('SharedChannels'),
+                            it.licensedForSku(LicenseSkus.Enterprise),
+                            it.licensedForSku(LicenseSkus.Professional),
+                        )),
                         isDisabled: it.not(it.userHasWritePermissionOnResource(RESOURCE_KEYS.EXPERIMENTAL.FEATURES)),
                     },
                     {
