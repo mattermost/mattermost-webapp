e17f75781c55642c847eee243bfee96d3116f5bf
Michel Engelen <jsnerdic@gmail.com>
2023-01-09 14:27:12 +0100

    make linter happy

--- components/suggestion/at_mention_provider/at_mention_suggestion.tsx
+++ components/suggestion/at_mention_provider/at_mention_suggestion.tsx
@@ -6,22 +6,21 @@ import {FormattedMessage, injectIntl} from 'react-intl';
 
 import classNames from 'classnames';
 
-import BotTag from 'components/widgets/tag/bot_tag';
-import GuestTag from 'components/widgets/tag/guest_tag';
-
 import {Constants} from 'utils/constants';
 import * as Utils from 'utils/utils';
 import {isGuest} from 'mattermost-redux/utils/user_utils';
 
 import SharedUserIndicator from 'components/shared_user_indicator';
 import Avatar from 'components/widgets/users/avatar';
-
+import Tag from 'components/widgets/tag/tag';
+import BotTag from 'components/widgets/tag/bot_tag';
+import GuestTag from 'components/widgets/tag/guest_tag';
 import CustomStatusEmoji from 'components/custom_status/custom_status_emoji';
 import StatusIcon from 'components/status_icon';
 
 import Suggestion from '../suggestion.jsx';
 
-import {UserProfile} from '../command_provider/app_command_parser/app_command_parser_dependencies.js';
+import {UserProfile} from '@mattermost/types/users';
 
 interface Item extends UserProfile {
     display_name: string;
@@ -158,15 +157,17 @@ class AtMentionSuggestion extends Suggestion {
         if (item.type === Constants.MENTION_GROUPS) {
             countBadge = (
                 <span className='suggestion-list__group-count'>
-                    <Badge>
-                        <FormattedMessage
-                            id='suggestion.group.members'
-                            defaultMessage='{member_count} {member_count, plural, one {member} other {members}}'
-                            values={{
-                                member_count: (item as Group).member_count,
-                            }}
-                        />
-                    </Badge>
+                    <Tag
+                        text={
+                            <FormattedMessage
+                                id='suggestion.group.members'
+                                defaultMessage='{member_count} {member_count, plural, one {member} other {members}}'
+                                values={{
+                                    member_count: (item as Group).member_count,
+                                }}
+                            />
+                        }
+                    />
                 </span>
             );
         }
