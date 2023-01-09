3452cddacc92b2a8ef9099fe657577701345561c
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2023-01-10 00:04:50 +0530

    fix multi-team-join and edit-incoming-webhook tests

--- e2e/cypress/tests/integration/integrations/incoming_webhook/edit_incoming_webhook_spec.js
+++ e2e/cypress/tests/integration/integrations/incoming_webhook/edit_incoming_webhook_spec.js
@@ -33,10 +33,13 @@ describe('Incoming webhook', () => {
     });
 
     it('MM-T641 Edit incoming webhook, webhook posts attachment', () => {
-        // # Go to test team/channel, open product menu and click "Integrations"
         cy.intercept('GET', '**api/v4/channels/**').as('channels');
+        cy.intercept('GET', '**/api/v4/**').as('networkCalls');
+        
+        // # Go to test team/channel, open product menu and click "Integrations"
         cy.visit(`${testTeam.name}/channels/town-square`);
         cy.wait('@channels');
+        cy.wait('@networkCalls');
         cy.uiOpenProductMenu('Integrations');
 
         // * Verify that it redirects to integrations URL. Then, click "Incoming Webhooks"
@@ -55,7 +58,7 @@ describe('Incoming webhook', () => {
 
         // # Redirect to test team/channel
         cy.visit(`${testTeam.name}/channels/town-square`);
-        cy.wait('@channels');
+
         // # Post an incoming webhook and verify that it is posted in the channel
         const payload = {
             channel: 'town-square',
@@ -63,7 +66,6 @@ describe('Incoming webhook', () => {
             attachments: [{fallback: 'fallback text', pretext: 'Optional text that appears above the attachment block', author_name: 'Authors Name', author_link: 'http://mattermost.org', author_icon: 'http://www.mattermost.org/wp-content/uploads/2016/04/icon.png', text: 'This is the text of the attachment. It should appear just above the image. \nIts very long, so it makes the text collapse behind a \\"Show More\\" button. If you click \\"Show More\\" the text should expand, and then if you click "Show Less" it should collapse again. The rest of the attachment should include one image of a graph and one thumbnail image of the Mattermost logo on the right hand side of the attachment. It should also include additional fields below the image that are formatted more like a table, in two columns. The left border of the attachment should be colored green. At the top of the attachment, there should be an author name followed by a bolded title. Both the author name and the title should be hyperlinks.', thumb_url: 'http://www.mattermost.org/wp-content/uploads/2016/04/icon.png', title: 'Testing Integration Attachments', title_link: 'https://www.google.com', color: '#00ff00', image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/ScientificGraphSpeedVsTime.svg/2000px-ScientificGraphSpeedVsTime.svg.png', fields: [{short: false, title: 'Area', value: 'Testing with a very long piece of text that will take up the whole width of the table. And then some more space even because it is really not a short field.'}, {short: true, title: 'Iteration', value: 'Testing'}, {short: true, title: 'State', value: 'New'}, {short: false, title: 'Reason', value: 'New defect reported'}]}],
         };
         cy.postIncomingWebhook({url: incomingWebhook.url, data: payload});
-        cy.wait(2000);
 
         cy.getLastPost().within(() => {
             cy.findByRole('link', {name: 'Testing Integration Attachments', hidden: true});
