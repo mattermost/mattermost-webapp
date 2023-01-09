183bd26b766bdeab172e57b44fbd0e2b8fcda9ac
Ashish Dhama <ashishdhama3@gmail.com>
2023-01-09 23:03:49 +0530

    [MM-47487] Remove cloud professional file storage limit (#11860)
    Automatic Merge
    
    (cherry picked from commit c5b692219d24f675165102bc514c606baf7aca1e)

--- components/admin_console/billing/plan_details/feature_list.tsx
+++ components/admin_console/billing/plan_details/feature_list.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import {useIntl} from 'react-intl';
 
-import {fallbackStarterLimits, fallbackProfessionalLimits, asGBString, hasSomeLimits} from 'utils/limits';
+import {fallbackStarterLimits, asGBString, hasSomeLimits} from 'utils/limits';
 import useGetLimits from 'components/common/hooks/useGetLimits';
 import {CloudProducts} from 'utils/constants';
 
@@ -93,10 +93,7 @@ const FeatureList = (props: FeatureListProps) => {
         intl.formatMessage(
             {
                 id: 'admin.billing.subscription.planDetails.features.fileStorage',
-                defaultMessage: '{limit} file storage',
-            },
-            {
-                limit: asGBString(fallbackProfessionalLimits.files.totalStorage, intl.formatNumber),
+                defaultMessage: 'Unlimited file storage',
             },
         ),
         intl.formatMessage({
