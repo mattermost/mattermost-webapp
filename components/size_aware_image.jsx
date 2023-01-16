mattermost/mattermost-webapp

commit c667766d27ddd6bb6376535ee84f0d27134feb06

[MM-45540] Fix channel header not displaying inline images (markdown) (#11110)
* Fix channel header is not displaying inline images (markdown)

* Fix inline image styling and tests

Co-authored-by: Mattermod <mattermod@users.noreply.github.com>

components/size_aware_image.jsx
@@ -391,7 +391,7 @@ export default class SizeAwareImage extends React.PureComponent {
                 {placeHolder}
                 <div
                     className='file-preview__button'
-                    style={{display: shouldShowImg ? 'initial' : 'none'}}
+                    style={{display: shouldShowImg ? 'inline' : 'none'}}
                 >
                     {this.renderImageWithContainerIfNeeded()}
                 </div>

sass/layout/_markdown.scss
@@ -85,6 +85,10 @@ h6.markdown__heading {
             top: 14px;
             right: 12px;
         }
+
+        .image-loaded-container {
+            display: inline;
+        }
     }
 
     .markdown-inline-img--loading {
@@ -169,6 +173,10 @@ h6.markdown__heading {
     .broken-image {
         max-height: 18px;
         border-radius: unset;
+    }
+
+    .image-loaded-container {
+        display: inline;
     }
 }
 

components/size_aware_image.test.jsx
@@ -57,7 +57,7 @@ describe('components/SizeAwareImage', () => {
         wrapper.setState({loaded: true, error: false});
 
         const style = wrapper.find('.file-preview__button').prop('style');
-        expect(style).toHaveProperty('display', 'initial');
+        expect(style).toHaveProperty('display', 'inline');
     });
 
     test('should render the actual image when first mounted without dimensions', () => {

e2e/cypress/tests/integration/markdown/markdown_image_spec.js
@@ -125,7 +125,7 @@ describe('Markdown', () => {
         cy.updateChannelHeader('![MM Logo](https://raw.githubusercontent.com/furqanmlk/furqanmlk.github.io/main/images/small-image.png)').wait(TIMEOUTS.TWO_SEC);
 
         // * Verify image in header
-        cy.get('#channelHeaderDescription').find('div.markdown__paragraph-inline').as('imageDiv');
+        cy.get('#channelHeaderDescription').find('span.markdown__paragraph-inline').as('imageDiv');
 
         cy.get('@imageDiv').find('img.markdown-inline-img').
             should('have.class', 'markdown-inline-img--hover').
@@ -148,7 +148,7 @@ describe('Markdown', () => {
         cy.updateChannelHeader('[![Build Status](https://raw.githubusercontent.com/furqanmlk/furqanmlk.github.io/main/images/small-image.png)](https://raw.githubusercontent.com/furqanmlk/furqanmlk.github.io/main/images/small-image.png)').wait(TIMEOUTS.TWO_SEC);
 
         // * Verify image in header
-        cy.get('#channelHeaderDescription').find('div.markdown__paragraph-inline').as('imageDiv');
+        cy.get('#channelHeaderDescription').find('span.markdown__paragraph-inline').as('imageDiv');
 
         cy.get('@imageDiv').find('img.markdown-inline-img').
             should('have.class', 'markdown-inline-img--hover').

utils/markdown/renderer.tsx
@@ -184,7 +184,7 @@ export default class Renderer extends marked.Renderer {
          ** use a div tag instead of a p tag to allow other divs to be nested,
          ** which avoids errors of incorrect DOM nesting (<div> inside <p>)
          */
-                result = `<div class="markdown__paragraph-inline">${text}</div>`;
+                result = `<span class="markdown__paragraph-inline">${text}</span>`;
             } else {
                 result = `<p class="markdown__paragraph-inline">${text}</p>`;
             }

commit 1b4b2d6a48f1aef7ebb52ad0ef1a43ff7b670ce6

Correct inline image container button positions
Bug introduced in c667766d27ddd6bb6376535ee84f0d27134feb06

components/size_aware_image.jsx
@@ -429,7 +429,7 @@ export default class SizeAwareImage extends React.PureComponent {
                 {fallback}
                 <div
                     className='file-preview__button'
-                    style={{display: shouldShowImg ? 'inline' : 'none'}}
+                    style={{display: shouldShowImg ? 'inline-block' : 'none'}}
                 >
                     {this.renderImageWithContainerIfNeeded()}
                 </div>

sass/components/_files.scss
@@ -200,17 +200,25 @@
             }
         }
 
+        $max-image-height: 350px;
+
         .image-loaded {
             bottom: 0;
             min-width: 40px;
             max-width: 100%;
             min-height: 40px;
-            max-height: 350px;
+            max-height: $max-image-height;
             border-radius: 4px;
             transition: all 0.1s linear;
 
             .file-preview__button {
                 display: block;
+
+                &,
+                .small-image-utility-buttons-wrapper {
+                    max-width: 100%;
+                    max-height: $max-image-height;
+                }
             }
 
             .image-permalink {
@@ -222,7 +230,7 @@
                 position: relative;
                 z-index: 2;
                 overflow: hidden;
-                max-height: 350px;
+                max-height: $max-image-height;
                 align-self: center;
                 border-radius: 4px;
                 cursor: pointer;

sass/layout/_markdown.scss
@@ -52,7 +52,6 @@ h6.markdown__heading {
 #rhsContainer {
     .markdown-inline-img {
         max-height: 500px;
-        margin: 5px 2px;
         border-radius: 4px;
         -moz-force-broken-image-icon: 1;
     }
@@ -73,6 +72,11 @@ h6.markdown__heading {
         }
     }
 
+    .markdown-inline-img,
+    .markdown-inline-img__container {
+        margin: 5px 2px;
+    }
+
     .markdown-inline-img__container {
         display: inline-block;
         width: calc(100% - 5px);
@@ -87,7 +91,11 @@ h6.markdown__heading {
         }
 
         .image-loaded-container {
-            display: inline;
+            display: inline-block;
+        }
+
+        .markdown-inline-img {
+            margin: 0;
         }
     }
 
@@ -176,7 +184,7 @@ h6.markdown__heading {
     }
 
     .image-loaded-container {
-        display: inline;
+        display: inline-block;
     }
 }
 

components/size_aware_image.test.jsx
@@ -75,7 +75,7 @@ describe('components/SizeAwareImage', () => {
         wrapper.setState({loaded: true, error: false});
 
         const style = wrapper.find('.file-preview__button').prop('style');
-        expect(style).toHaveProperty('display', 'inline');
+        expect(style).toHaveProperty('display', 'inline-block');
     });
 
     test('should render the actual image when first mounted without dimensions', () => {

e2e/cypress/tests/integration/files_and_attachments/image_link_preview_spec.js
@@ -166,7 +166,7 @@ describe('Image Link Preview', () => {
             {
                 filename: 'image-40x400.jpg',
                 originalSize: {width: 40, height: 400},
-                thumbnailSize: {width: 40, height: 400},
+                thumbnailSize: {width: 35, height: 350},
                 containerSize: {width: 46},
             },
             {

e2e/cypress/tests/integration/messaging/image_attachment_spec.js
@@ -56,7 +56,7 @@ describe('Image attachment', () => {
         verifyFileThumbnail({
             filename,
             actualImage: {height: 24, width: 340},
-            container: {height: 45, width: 342},
+            container: {height: 45, width: 339},
         });
     });
 
@@ -77,7 +77,7 @@ describe('Image attachment', () => {
         verifyFileThumbnail({
             filename,
             actualImage: {height: 350, width: 21},
-            container: {height: 352, width: 46},
+            container: {height: 348, width: 46},
         });
     });
 