94f0476ae9fbab5d72baa05c998697b26ec18512
Julian Mondragon <bugshoo+github@gmail.com>
2023-01-17 15:49:34 -0500

    temp

--- components/common/hooks/useGetUsageDeltas.ts
+++ components/common/hooks/useGetUsageDeltas.ts
@@ -46,7 +46,7 @@ export default function useGetUsageDeltas(): CloudUsage {
                 },
             }
         );
-    }, [usage, limits]);
+    }, [usage, limits, limitsLoaded]);
 
     return usageDelta;
 }
