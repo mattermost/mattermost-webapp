d1f31875283a1de4d8ff06ea8d0996ff1702e50c
neallred <neallred@protonmail.com>
2023-01-20 10:59:03 -0600

    allow distinguishing between failure to contact CWS and CWS contacted but the service is off

--- components/common/hooks/useCanSelfHostedSignup.ts
+++ components/common/hooks/useCanSelfHostedSignup.ts
@@ -1,18 +1,67 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {useState, useEffect} from 'react';
+import {useMemo, useState, useEffect} from 'react';
 
 import {Client4} from 'mattermost-redux/client';
 
 import useLoadStripe from './useLoadStripe';
 
-export default function useCanSelfHostedSignup() {
-    const [canReachPortal, setCanReachPortal] = useState(false);
-    const canReachStripe = (useLoadStripe().current);
+interface CWSSignupAvailability {
+    cwsContacted: boolean;
+    cwsServiceOn: boolean;
+}
+
+const cwsAvailable: CWSSignupAvailability = {
+    cwsContacted: true,
+    cwsServiceOn: true,
+};
+const cwsAvailableEmptyState: CWSSignupAvailability = {
+    cwsContacted: false,
+    cwsServiceOn: false,
+};
+
+type SignupAvailability = CWSSignupAvailability & {
+    stripeAvailable: boolean;
+    ok: boolean;
+}
+
+export default function useCanSelfHostedSignup(): SignupAvailability {
+    const [cwsAvailability, setCwsAvailability] = useState(cwsAvailableEmptyState);
+    const stripeAvailable = Boolean(useLoadStripe().current);
     useEffect(() => {
-        Client4.getAvailabilitySelfHostedSignup().then(() => setCanReachPortal(true));
+        Client4.getAvailabilitySelfHostedSignup().
+            then(() => {
+                setCwsAvailability(cwsAvailable);
+            }).
+            catch((err) => {
+                let errorValue = {...cwsAvailableEmptyState};
+                switch (err.status_code) {
+                case 503: {
+                    errorValue = {
+                        cwsServiceOn: false,
+                        cwsContacted: true,
+                    };
+                    break;
+                }
+
+                // defined as switch because in the future we will add a separate
+                // screening pending reason
+                default: {
+                    errorValue = {...cwsAvailableEmptyState};
+                }
+                }
+                setCwsAvailability(errorValue);
+            });
     }, []);
 
-    return canReachPortal && canReachStripe;
+    // not implemented
+
+    return useMemo(() => {
+        return {
+            ...cwsAvailability,
+            stripeAvailable,
+            ok: stripeAvailable && cwsAvailability.cwsContacted && cwsAvailability.cwsServiceOn,
+        };
+    }, [stripeAvailable, cwsAvailability]);
 }
