5d64965a7c3c4c4f5a45dcd526a85cae3e281247
Michel Engelen <jsnerdic@gmail.com>
2023-01-24 09:51:57 +0100

    adds wysiwyg test file

---
+++ components/wysiwyg/wysiwyg.test.tsx
@@ -0,0 +1,56 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import {NewPostDraft} from 'types/store/draft';
+
+import {PostType} from '../file_upload';
+
+import Wysiwyg, {WysiwygConfig} from './wysiwyg';
+
+const wysiwygConfig: WysiwygConfig = {
+    enterHandling: {
+        ctrlSend: true,
+        codeBlockOnCtrlEnter: true,
+    },
+    suggestions: {
+        mention: {
+            teamId: 'teamId',
+            channelId: 'channelId',
+            useSpecialMentions: false,
+            useGroupMentions: false,
+        },
+        channel: {teamId: 'teamId'},
+        command: {
+            teamId: 'teamId',
+            channelId: 'channelId',
+        },
+    },
+    fileUpload: {
+        rootId: '',
+        channelId: 'channelId',
+        postType: PostType.post,
+    },
+    enablePriority: false,
+};
+
+const props = {
+    onSubmit: jest.fn(),
+    onChange: jest.fn(),
+    onAttachmentChange: jest.fn(),
+    config: wysiwygConfig,
+    readOnly: false,
+    draft: {
+        message: '',
+    } as NewPostDraft,
+    placeholder: 'Write to test-channel',
+};
+
+describe('WYSIWYG editor', () => {
+    it('matches snapshot', () => {
+        const wrapper = shallow(<Wysiwyg {...props}/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
