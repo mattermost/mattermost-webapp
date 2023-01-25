dfa20079945f5a395b04659bc47c2b4072e28fc4
koox00 <3829551+koox00@users.noreply.github.com>
2023-01-25 13:36:59 +0200

    Changes error message for special mentions

--- components/post_priority/error_messages.tsx
+++ components/post_priority/error_messages.tsx
@@ -1,29 +1,34 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
-import {FormattedMessage} from 'react-intl';
-
-function getFirstSpecialMention(specialMentions: {[key: string]: boolean}): string {
-    switch (true) {
-    case specialMentions.all:
-        return 'all';
-    case specialMentions.here:
-        return 'here';
-    case specialMentions.channel:
-        return 'channel';
-    default:
-        return '';
-    }
-}
+import React, {useMemo} from 'react';
+import {FormattedMessage, FormattedList} from 'react-intl';
 
 export function HasSpecialMentions({specialMentions}: {specialMentions: {[key: string]: boolean}}) {
+    const mentions = useMemo(() => {
+        return Object.keys(specialMentions).
+            filter((key) => specialMentions[key]).
+            map((key) => `@${key}`);
+
+        /* eslint-disable react-hooks/exhaustive-deps */
+    }, [
+        specialMentions.all,
+        specialMentions.here,
+        specialMentions.channel,
+    ]);
+    /* eslint-enable react-hooks/exhaustive-deps */
+
     return (
         <FormattedMessage
             id={'post_priority.error.special_mentions'}
-            defaultMessage={'Cannot use @{mention} to mention recipients of persistent notifications'}
+            defaultMessage={'{mention} can’t be used with persistent notifications'}
             values={{
-                mention: getFirstSpecialMention(specialMentions),
+                mention: (
+                    <FormattedList
+                        value={mentions}
+                        type='disjunction'
+                    />
+                ),
             }}
         />
     );
