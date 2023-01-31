5242df0fc8a21920cb4439abc3455819e0e23e58
Conor Macpherson <conor.macpherson@mattermost.com>
2023-01-31 15:59:57 -0500

    Add missing prop.

--- components/self_hosted_purchase_modal/self_hosted_card.tsx
+++ components/self_hosted_purchase_modal/self_hosted_card.tsx
@@ -98,7 +98,12 @@ export default function SelfHostedCard(props: Props) {
                         onChange={props.updateSeats}
                     />
                 )}
-                afterButtonContent={<Consequences isCloud={false}/>}
+                afterButtonContent={
+                    <Consequences
+                        isCloud={false}
+                        buttonText={intl.formatMessage({id: 'self_hosted_signup.cta', defaultMessage: 'Upgrade'})}
+                    />
+                }
                 buttonDetails={{
                     action: props.submit,
                     disabled: !props.canSubmit,
