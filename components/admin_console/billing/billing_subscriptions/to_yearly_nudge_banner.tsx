379a87a4b563986d0c1d0210482475bca9ea6aa0
AGMETEOR <guwats10@gmail.com>
2023-02-01 14:40:38 +0300

    open purchase modal

--- components/admin_console/billing/billing_subscriptions/to_yearly_nudge_banner.tsx
+++ components/admin_console/billing/billing_subscriptions/to_yearly_nudge_banner.tsx
@@ -3,12 +3,10 @@
 
 import React from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {t} from 'utils/i18n';
-
 import {useIntl, FormattedMessage} from 'react-intl';
 
 import AlertBanner from 'components/alert_banner';
-import useOpenPricingModal from 'components/common/hooks/useOpenPricingModal';
+import useOpenCloudPurchaseModal from 'components/common/hooks/useOpenCloudPurchaseModal';
 import useOpenSalesLink from 'components/common/hooks/useOpenSalesLink';
 import AnnouncementBar from 'components/announcement_bar/default_announcement_bar';
 
@@ -19,6 +17,7 @@ import {savePreferences} from 'mattermost-redux/actions/preferences';
 import {get as getPreference} from 'mattermost-redux/selectors/entities/preferences';
 
 import {AnnouncementBarTypes, CloudBanners, CloudProducts, Preferences, RecurringIntervals} from 'utils/constants';
+import {t} from 'utils/i18n';
 
 import {GlobalState} from '@mattermost/types/store';
 
@@ -27,7 +26,7 @@ import './to_yearly_nudge_banner.scss';
 const ToYearlyNudgeBannerDismissable = () => {
     const dispatch = useDispatch();
 
-    const openPricingModal = useOpenPricingModal();
+    const openPurchaseModal = useOpenCloudPurchaseModal({});
 
     const nudgeDismissed = useSelector((state: GlobalState) => getPreference(state, Preferences.CLOUD_YEARLY_NUDGE_BANNER, CloudBanners.NUDGE_TO_YEARLY_BANNER_DISMISSED)) === 'true';
     const currentUser = useSelector(getCurrentUser);
@@ -67,7 +66,7 @@ const ToYearlyNudgeBannerDismissable = () => {
         <AnnouncementBar
             type={AnnouncementBarTypes.ANNOUNCEMENT}
             showCloseButton={true}
-            onButtonClick={() => openPricingModal({trackingLocation: 'to_yearly_nudge_annoucement_bar'})}
+            onButtonClick={() => openPurchaseModal({trackingLocation: 'to_yearly_nudge_annoucement_bar'})}
             modalButtonText={t('cloud_billing.nudge_to_yearly.learn_more')}
             modalButtonDefaultText='Learn more'
             message={<FormattedMessage {...message}/>}
@@ -81,7 +80,7 @@ const ToYearlyNudgeBanner = () => {
     const {formatMessage} = useIntl();
 
     const openSalesLink = useOpenSalesLink(SalesInquiryIssue.AboutPurchasing);
-    const openPricingModal = useOpenPricingModal();
+    const openPurchaseModal = useOpenCloudPurchaseModal({});
 
     const product = useSelector(selectSubscriptionProduct);
     const currentProductProfessional = product?.sku === CloudProducts.PROFESSIONAL;
@@ -108,7 +107,7 @@ const ToYearlyNudgeBanner = () => {
 
     const viewPlansAction = (
         <button
-            onClick={() => openPricingModal({trackingLocation: 'to_yearly_nudge_banner'})}
+            onClick={() => openPurchaseModal({trackingLocation: 'to_yearly_nudge_banner'})}
             className='btn ToYearlyNudgeBanner__primary'
         >
             {formatMessage({id: 'cloud_billing.nudge_to_yearly.learn_more', defaultMessage: 'Learn more'})}
