996641cc6392bc1db58bd06f8710564d6fae47f2
Nathaniel Allred <neallred@protonmail.com>
2023-02-02 13:34:38 -0600

    Mm 49772 (#12046)
    * allow distinguishing between failure to contact CWS and CWS contacted but the service is off
    
    (cherry picked from commit 66f759983e5e00f48013c059ba7bd0abc1d7c1a9)

--- components/common/hooks/useCanSelfHostedSignup.ts
+++ components/common/hooks/useCanSelfHostedSignup.ts
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {useState, useEffect} from 'react';
+import {useMemo, useState, useEffect} from 'react';
 import {useSelector} from 'react-redux';
 
 import {Client4} from 'mattermost-redux/client';
@@ -9,17 +9,64 @@ import {getConfig} from 'mattermost-redux/selectors/entities/general';
 
 import useLoadStripe from './useLoadStripe';
 
-export default function useCanSelfHostedSignup() {
-    const [canReachPortal, setCanReachPortal] = useState(false);
-    const canReachStripe = (useLoadStripe().current);
+interface CWSSignupAvailability {
+    cwsContacted: boolean;
+    cwsServiceOn: boolean;
+}
+
+const cwsAvailable: CWSSignupAvailability = {
+    cwsContacted: true,
+    cwsServiceOn: true,
+};
+const cwsAvailableEmptyState: CWSSignupAvailability = {
+    cwsContacted: false,
+    cwsServiceOn: false,
+};
+
+type SignupAvailability = CWSSignupAvailability & {
+    stripeAvailable: boolean;
+    ok: boolean;
+}
+
+export default function useCanSelfHostedSignup(): SignupAvailability {
+    const [cwsAvailability, setCwsAvailability] = useState(cwsAvailableEmptyState);
     const config = useSelector(getConfig);
     const isEnterpriseReady = config.BuildEnterpriseReady === 'true';
+    const stripeAvailable = Boolean(useLoadStripe().current);
     useEffect(() => {
         if (!isEnterpriseReady) {
             return;
         }
-        Client4.getAvailabilitySelfHostedSignup().then(() => setCanReachPortal(true));
-    }, [isEnterpriseReady]);
+        Client4.getAvailabilitySelfHostedSignup().
+            then(() => {
+                setCwsAvailability(cwsAvailable);
+            }).
+            catch((err) => {
+                let errorValue = {...cwsAvailableEmptyState};
+                switch (err.status_code) {
+                case 503: {
+                    errorValue = {
+                        cwsServiceOn: false,
+                        cwsContacted: true,
+                    };
+                    break;
+                }
 
-    return canReachPortal && canReachStripe;
+                // defined as switch because in the future we will add a separate
+                // screening pending reason
+                default: {
+                    errorValue = {...cwsAvailableEmptyState};
+                }
+                }
+                setCwsAvailability(errorValue);
+            });
+    }, []);
+
+    return useMemo(() => {
+        return {
+            ...cwsAvailability,
+            stripeAvailable,
+            ok: stripeAvailable && cwsAvailability.cwsContacted && cwsAvailability.cwsServiceOn,
+        };
+    }, [stripeAvailable, cwsAvailability]);
 }
