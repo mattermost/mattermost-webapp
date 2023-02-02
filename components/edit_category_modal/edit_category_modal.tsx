3f454acd736248ae9a0d4fdc5d51fa2fc9fbb921
Md_ZubairAhmed <m-zubairahmed@protonmail.com>
2023-02-02 20:38:32 +0530

    init

--- components/edit_category_modal/edit_category_modal.tsx
+++ components/edit_category_modal/edit_category_modal.tsx
@@ -4,14 +4,17 @@
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
 
+import {GenericModal} from '@mattermost/components';
+
 import {ChannelCategory} from '@mattermost/types/channel_categories';
 
 import {trackEvent} from 'actions/telemetry_actions';
+
 import QuickInput, {MaxLengthInput} from 'components/quick_input';
+
 import {localizeMessage} from 'utils/utils';
 
 import '../category_modal.scss';
-import GenericModal from 'components/generic_modal';
 
 const MAX_LENGTH = 22;
 
@@ -121,15 +124,17 @@ export default class EditCategoryModal extends React.PureComponent<Props, State>
 
         return (
             <GenericModal
-                ariaLabel={localizeMessage('rename_category_modal.renameCategory', 'Rename Category')}
-                onExited={this.props.onExited}
-                modalHeaderText={modalHeaderText}
-                handleConfirm={this.handleConfirm}
-                handleEnterKeyPress={this.handleConfirm}
-                handleCancel={this.handleCancel}
-                confirmButtonText={editButtonText}
-                isConfirmDisabled={this.isConfirmDisabled()}
                 id='editCategoryModal'
+                ariaLabel={localizeMessage('rename_category_modal.renameCategory', 'Rename Category')}
+                modalHeaderText={modalHeaderText}
+                confirmButtonText={editButtonText}
+                onExited={this.props.onExited}
+                handleEnterKeyPress={this.handleConfirm}
+                handleConfirm={this.handleConfirm}
+                handleCancel={this.handleCancel}
+                isConfirmDisabled={this.isConfirmDisabled()}
+                enforceFocus={false}
+                shouldFocusTrapHandleFocus={true}
             >
                 <QuickInput
                     inputComponent={MaxLengthInput}
