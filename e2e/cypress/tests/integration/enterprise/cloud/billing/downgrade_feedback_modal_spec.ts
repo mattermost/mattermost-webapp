375d0cc05c469c9df26b773642bf88c3751d9d5a
Conor Macpherson <conor.macpherson@mattermost.com>
2023-02-07 16:20:36 -0500

    add/fix e2e tests, lint.

--- e2e/cypress/tests/integration/enterprise/cloud/billing/downgrade_feedback_modal_spec.ts
+++ e2e/cypress/tests/integration/enterprise/cloud/billing/downgrade_feedback_modal_spec.ts
@@ -10,6 +10,11 @@
 // Stage: @prod
 // Group: @cloud_only @cloud_trial
 describe('Feedback modal', () => {
+    beforeEach(() => {
+        cy.apiLogout();
+        cy.apiAdminLogin();
+    });
+
     it('Should display feedback modal when downgrading to cloud free', () => {
         const subscription = {
             id: 'sub_test1',
@@ -17,58 +22,66 @@ describe('Feedback modal', () => {
             is_free_trial: 'false',
         };
         cy.simulateSubscription(subscription);
-        cy.apiLogout();
-        cy.apiAdminLogin();
+
+        // # Intercepts the teams request to avoid team selection modal.
+        cy.intercept('**/api/v4/usage/teams', {statusCode: 200, body: {active: 1, cloud_archived: 0}}).as('teams');
 
         // # Open the pricing modal
         cy.visit('/admin_console/billing/subscription?action=show_pricing_modal');
+
+        cy.wait('@teams');
 
         // * Pricing modal should be open
         cy.get('#pricingModal').should('exist');
 
         // * Free action (downgrade) should exist.
+        // # Click it.
         cy.get('#free_action').contains('Downgrade').should('exist').should('be.enabled').click();
 
         // * Downgrade feedback should exist.
         cy.findByText('Please share your reason for downgrading').should('exist');
 
-        // * The downgrade button should be disabled until feedback is provided.
-        cy.findByText('Downgrade').should('exist').should('be.disabled').click();
+        // * The submit (Downgrade) button should be disabled.
+        cy.get('.GenericModal__button.confirm').contains('Downgrade').should('exist').should('be.disabled');
 
         // # Click the free action (downgrade).
         cy.findByTestId('Exploring other solutions').click();
 
         // # Click the submit for the downgrade feedback.
-        cy.findByText('Downgrade').should('exist').should('be.enabled').click();
+        cy.get('.GenericModal__button.confirm').contains('Downgrade').should('exist').should('be.enabled').click();
 
         // * The downgrade modal should exist.
         cy.findByText('Downgrading your workspace').should('exist');
     });
 
-    it('Downgrade Feedback modal submit button should be disabled if not option is selected', () => {
+    it('Downgrade Feedback modal submit button should be disabled if no option is selected', () => {
         const subscription = {
             id: 'sub_test1',
             product_id: 'prod_2',
             is_free_trial: 'false',
         };
         cy.simulateSubscription(subscription);
-        cy.apiLogout();
-        cy.apiAdminLogin();
+
+        // # Intercepts the teams request to avoid team selection modal.
+        cy.intercept('**/api/v4/usage/teams', {statusCode: 200, body: {active: 1, cloud_archived: 0}}).as('teams');
 
         // # Open the pricing modal
         cy.visit('/admin_console/billing/subscription?action=show_pricing_modal');
+
+        cy.wait('@teams');
 
         // * Pricing modal should be open
         cy.get('#pricingModal').should('exist');
 
         // * Free action (downgrade) should exist.
+        // # Click it.
         cy.get('#free_action').contains('Downgrade').should('exist').should('be.enabled').click();
 
         // * Downgrade feedback should exist.
         cy.findByText('Please share your reason for downgrading').should('exist');
 
-        // * The submit button should be disabled.
-        cy.findByText('Downgrade').should('exist').should('be.disabled');
+        // * The submit (Downgrade) button should be disabled.
+        cy.get('.GenericModal__button.confirm').contains('Downgrade').should('exist').should('be.disabled');
     });
 
     it('Downgrade Feedback modal shows error state when "other" option is selected but not comments have been provided', () => {
@@ -78,33 +91,86 @@ describe('Feedback modal', () => {
             is_free_trial: 'false',
         };
         cy.simulateSubscription(subscription);
-        cy.apiLogout();
-        cy.apiAdminLogin();
+
+        // # Intercepts the teams request to avoid team selection modal.
+        cy.intercept('**/api/v4/usage/teams', {statusCode: 200, body: {active: 1, cloud_archived: 0}}).as('teams');
 
         // # Open the pricing modal
         cy.visit('/admin_console/billing/subscription?action=show_pricing_modal');
+
+        cy.wait('@teams');
 
         // * Pricing modal should be open
         cy.get('#pricingModal').should('exist');
 
         // * Free action (downgrade) should exist.
-        // # Click free action to downgrade.
+        // # Click it.
         cy.get('#free_action').contains('Downgrade').should('exist').should('be.enabled').click();
 
         // * Downgrade feedback should exist.
         cy.findByText('Please share your reason for downgrading').should('exist');
 
-        // * The submit button should be disabled.
-        cy.findByText('Downgrade').should('exist').should('be.disabled');
+        // * The submit (Downgrade) button should be disabled.
+        cy.get('.GenericModal__button.confirm').contains('Downgrade').should('exist').should('be.disabled');
 
         // # Click the other option, requiring extra comments.
         cy.get('input[value="Other"]').click();
 
-        cy.findByText('Downgrade').should('exist').should('be.disabled').click();
-
+        // # Fill in the comments.
         cy.findByTestId('FeedbackModal__TextInput').type('Do not need it anymore.');
 
-        // * The submit button should be enabled.
-        cy.findByText('Downgrade').should('exist').should('be.enabled').click();
+        // * The submit (Downgrade) button should be enabled.
+        // # Click it.
+        cy.get('.GenericModal__button.confirm').contains('Downgrade').should('exist').should('be.enabled').click();
+    });
+
+    it('Downgrade Feedback modal appears and downgrades after team selection modal is submitted', () => {
+        const subscription = {
+            id: 'sub_test1',
+            product_id: 'prod_2',
+            is_free_trial: 'false',
+        };
+        cy.simulateSubscription(subscription);
+
+        // # Intercepts the teams request to avoid team selection modal.
+        cy.intercept('**/api/v4/usage/teams', {statusCode: 200, body: {active: 2, cloud_archived: 0}}).as('teams');
+
+        // # Open the pricing modal
+        cy.visit('/admin_console/billing/subscription?action=show_pricing_modal');
+
+        cy.wait('@teams');
+
+        // * Pricing modal should be open
+        cy.get('#pricingModal').should('exist');
+
+        // * Free action (downgrade) should exist.
+        // # Click it.
+        cy.get('#free_action').contains('Downgrade').should('exist').should('be.enabled').click();
+
+        // * Team selection modal should exist.
+        cy.findByText('Confirm Plan Downgrade').should('exist');
+
+        // The test-id is the team id, can't find it in any network requests for interception.
+        // # Click the first team.
+        cy.get('input[name="deleteTeamRadioGroup"]').first().click();
+
+        // Can't seem to get the button via findByText, use css selector instead.
+        cy.get('.DowngradeTeamRemovalModal__buttons > .btn-primary').should('exist').should('be.enabled').click();
+
+        // * Downgrade feedback should exist.
+        cy.findByText('Please share your reason for downgrading').should('exist');
+
+        // * The submit (Downgrade) button should be disabled.
+        cy.get('.GenericModal__button.confirm').contains('Downgrade').should('exist').should('be.disabled');
+
+        // # Click the other option, requiring extra comments.
+        cy.get('input[value="Other"]').click();
+
+        // # Fill in the comments.
+        cy.findByTestId('FeedbackModal__TextInput').type('Do not need it anymore.');
+
+        // * The submit (Downgrade) button should be enabled.
+        // # Click it.
+        cy.get('.GenericModal__button.confirm').contains('Downgrade').should('exist').should('be.enabled').click();
     });
 });
