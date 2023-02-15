dad5ab9b7c4184b2ad7e3899023bd4a9e4bcec2a
AGMETEOR <guwats10@gmail.com>
2023-02-15 17:27:00 +0300

    [MM-50233] - Fix useGetLimits hook

--- components/common/hooks/useGetLimits.ts
+++ components/common/hooks/useGetLimits.ts
@@ -1,31 +1,31 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import {useState, useEffect, useMemo} from 'react';
+import {useEffect, useMemo} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 
-import {Limits} from '@mattermost/types/cloud';
-import {getCloudLimits, getCloudLimitsLoaded, isCurrentLicenseCloud} from 'mattermost-redux/selectors/entities/cloud';
+import {getCloudLimits, getCloudLimitsLoaded, getSubscriptionProduct, isCurrentLicenseCloud} from 'mattermost-redux/selectors/entities/cloud';
 import {getCloudLimits as getCloudLimitsAction} from 'actions/cloud';
 import {useIsLoggedIn} from 'components/global_header/hooks';
+
+import {Limits} from '@mattermost/types/cloud';
 
 export default function useGetLimits(): [Limits, boolean] {
     const isCloud = useSelector(isCurrentLicenseCloud);
     const isLoggedIn = useIsLoggedIn();
     const cloudLimits = useSelector(getCloudLimits);
+    const subscriptionProduct = useSelector(getSubscriptionProduct);
     const cloudLimitsReceived = useSelector(getCloudLimitsLoaded);
     const dispatch = useDispatch();
-    const [requestedLimits, setRequestedLimits] = useState(false);
 
     useEffect(() => {
-        if (isLoggedIn && isCloud && !requestedLimits && !cloudLimitsReceived) {
+        if (isLoggedIn && isCloud) {
             dispatch(getCloudLimitsAction());
-            setRequestedLimits(true);
         }
-    }, [isLoggedIn, isCloud, requestedLimits, cloudLimitsReceived]);
+    }, [isLoggedIn, isCloud, subscriptionProduct]);
 
     const result: [Limits, boolean] = useMemo(() => {
         return [cloudLimits, cloudLimitsReceived];
-    }, [cloudLimits, cloudLimitsReceived]);
+    }, [cloudLimits, subscriptionProduct]);
     return result;
 }
