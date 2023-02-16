0545067176685939079d4410f52ee66ec4c906c2
Scott Bishel <scott.bishel@mattermost.com>
2023-02-16 16:28:30 -0700

    lint fix

--- e2e/cypress/tests/integration/boards/create_board_spec.ts
+++ e2e/cypress/tests/integration/boards/create_board_spec.ts
@@ -1,6 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+/* eslint-disable cypress/no-unnecessary-waiting */
 describe('Create and delete board / card', () => {
     const timestamp = new Date().toLocaleString();
     const boardTitle = `Test Board (${timestamp})`;
