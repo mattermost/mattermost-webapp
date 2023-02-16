dcbaaff4b5da5873372a751d21fba24b71c1f08b
Furqan Malik <Furqan.Malik@mattermost.com>
2023-02-17 00:59:35 +0500

    fixed team_spec file

--- e2e/cypress/tests/integration/team_settings/teams_spec.js
+++ e2e/cypress/tests/integration/team_settings/teams_spec.js
@@ -76,7 +76,7 @@ describe('Teams Suite', () => {
             // # Open team menu and click "Invite People"
             cy.uiOpenTeamMenu();
             cy.uiGetLHSTeamMenu().findByText('Add people to the team');
-            cy.uiGetLHSTeamMenu().findByText('Invite People').click();
+            cy.uiGetLHSTeamMenu().findByText('Invite People').click().wait(TIMEOUTS.ONE_SEC);
 
             // * Check that the Invitation Modal opened up
             cy.findByTestId('invitationModal', {timeout: TIMEOUTS.HALF_SEC}).should('be.visible');
