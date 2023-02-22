b4efc721adb434efacd6d761aebdfb8719c1bb89
Harrison Healey <harrisonmhealey@gmail.com>
2023-02-22 15:49:56 -0500

    MM-50573 Clean up WebSocket listeners when connection is closed (#12227)
    * MM-50573 Clean up WebSocket listeners when connection is closed
    
    * MM-50573 Add additional logging to WebSocket code
    
    * Fix log message

--- actions/websocket_actions.test.jsx
+++ actions/websocket_actions.test.jsx
@@ -572,7 +572,7 @@ describe('handleNewPostEvents', () => {
 
 describe('reconnect', () => {
     test('should call syncPostsInChannel when socket reconnects', () => {
-        reconnect(false);
+        reconnect();
         expect(syncPostsInChannel).toHaveBeenCalledWith('otherChannel', '12345');
     });
 });
