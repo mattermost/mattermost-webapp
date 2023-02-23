7f15da8a8fcecb032e289167192e5e29a0bd622b
Conor Macpherson <conor.macpherson@mattermost.com>
2023-02-23 09:55:29 -0500

    Change request param naming for delete and downgrade feedback.

--- components/purchase_modal/process_payment_setup.tsx
+++ components/purchase_modal/process_payment_setup.tsx
@@ -46,7 +46,7 @@ type Props = RouteComponentProps & {
         isDevMode: boolean
     ) => Promise<boolean | null>;
     subscribeCloudSubscription:
-    | ((productId: string, shippingAddress: Address, seats?: number, feedback?: Feedback) => Promise<boolean | null> | ComplianceError)
+    | ((productId: string, shippingAddress: Address, seats?: number, downgradeFeedback?: Feedback) => Promise<boolean | null> | ComplianceError)
     | null;
     onBack: () => void;
     onClose: () => void;
