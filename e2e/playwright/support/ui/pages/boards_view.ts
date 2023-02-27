b2252eafc47c9c91915b7f7b8cf53e7d48414de1
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2023-02-27 20:01:04 +0530

    chore(e2e): add delete boards test case

--- e2e/playwright/support/ui/pages/boards_view.ts
+++ e2e/playwright/support/ui/pages/boards_view.ts
@@ -13,6 +13,9 @@ export default class BoardsViewPage {
     readonly topHead: Locator;
     readonly editableTitle: Locator;
     readonly shareButton: Locator;
+    readonly duplicateBoardButton: Locator;
+    readonly deleteBoardButton: Locator;
+    readonly deleteBoardConfirmationButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
@@ -21,6 +24,10 @@ export default class BoardsViewPage {
         this.topHead = page.locator('.top-head');
         this.editableTitle = this.topHead.getByPlaceholder('Untitled board');
         this.shareButton = page.getByRole('button', {name: '󰍁 Share'});
+        this.duplicateBoardButton = page.getByLabel('Duplicate board');
+        this.deleteBoardButton = page.getByLabel('Delete board');
+        this.deleteBoardConfirmationButton = page.locator('button.Button.filled.danger');
+        
     }
 
     async goto(teamId = '', boardId = '', viewId = '', cardId = '') {
@@ -52,6 +59,32 @@ export default class BoardsViewPage {
         await this.editableTitle.isVisible();
         expect(await this.editableTitle.getAttribute('value')).toBe('');
         await expect(this.page.getByTitle('(Untitled Board)')).toBeVisible();
+    }
+
+    async createDuplicateBoard(boardName: string) {
+        await this.page.hover(`div[title='${boardName}']`);
+        await this.page.locator(`//div[@title='${boardName}']/following-sibling::div`).click();
+        await this.duplicateBoardButton.click();
+        await this.page.waitForResponse(response => response.url().includes('duplicate?asTemplate=false') && response.status() === 200);
+        expect(await this.editableTitle.getAttribute('value')).toBe(`${boardName} copy`);
+    }
+
+    async shouldHaveBoardName(boardName: string) {
+        await this.editableTitle.isVisible();
+        expect(await this.editableTitle.getAttribute('value')).toBe(boardName);
+    }
+
+    async deleteBoard(boardName: string) {
+        await this.page.hover(`div[title='${boardName}']`);
+        await this.page.locator(`//div[@title='${boardName}']/following-sibling::div`).click();
+        await this.deleteBoardButton.click();
+        await this.deleteBoardConfirmationButton.click();
+        await this.page.waitForResponse(response => response.url().includes('plugins/boards/api/v2/boards') && response.status() === 200);
+        await this.waitforElementToNotExists(`div[title='${boardName}']`)
+    }
+
+    async waitforElementToNotExists(locator: string){
+        return await this.page.waitForFunction(locator => !!document.querySelector(locator), locator);
     }
 }
 
