e4b34ae574b87b43cd40e7274b6abddc5f3d72c2
Saturnino Abril <saturnino.abril@gmail.com>
2023-03-02 19:52:10 +0800

    E2E/Playwright: Initial setup for Boards (#12237)
    * add test for boards
    
    * add test extension for playwright
    
    * not related but fix sub_menu type
    
    * update snapshots to hide flaky profile image, remove components from pw and have snapshotEnabled default to false
    
    * update snapshots
    
    * change updating storage state
    
    * remove the boards banner

--- e2e/playwright/support/visual/index.ts
+++ e2e/playwright/support/visual/index.ts
@@ -1,14 +1,27 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
+import os from 'node:os';
+
+import chalk from 'chalk';
 import {expect, TestInfo} from '@playwright/test';
 
-import {illegalRe} from '@e2e-support/utils';
+import {illegalRe} from '@e2e-support/util';
 import testConfig, {TestArgs} from '@e2e-test.config';
 
 import snapshotWithPercy from './percy';
 
 export async function matchSnapshot(testInfo: TestInfo, testArgs: TestArgs) {
+    if (os.platform() !== 'linux') {
+        // eslint-disable-next-line no-console
+        console.log(
+            chalk.yellow(
+                `^ Warning: No visual test performed. Run in Linux or Playwright docker image to match snapshot.`
+            )
+        );
+        return;
+    }
+
     if (testConfig.snapshotEnabled) {
         // Visual test with built-in snapshot
         const filename = testInfo.title.replace(illegalRe, '').replace(/\s/g, '-').trim().toLowerCase();
