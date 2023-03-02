c011d356c6f130aa0a9e90cf8351decf6c0c23ed
yasserfaraazkhan <attitude3cena.yf@gmail.com>
2023-03-02 19:36:32 +0530

    chore(e2e): add delete boards test case

--- e2e/playwright/tests/functional/boards/board-creation-and-set-up/create_empty_board.spec.ts
+++ e2e/playwright/tests/functional/boards/board-creation-and-set-up/create_empty_board.spec.ts
@@ -44,3 +44,52 @@ test('MM-T4274 Create an Empty Board', async ({pw, pages}) => {
     expect(await boardsViewPage.editableTitle.getAttribute('value')).toBe(title);
     await boardsViewPage.sidebar.waitForTitle(title);
 });
+
+test('MM-T4290: Duplicating and deleting a board', async ({pw, pages}) => {
+    await pw.shouldHaveBoardsEnabled();
+
+    // Create and sign in a new user
+    const {user} = await pw.initSetup();
+
+    // Log in a user in new browser context
+    const {page} = await pw.testBrowser.login(user);
+
+    // Visit a default channel page
+    const channelsPage = new pages.ChannelsPage(page);
+    await channelsPage.goto();
+    await channelsPage.toBeVisible();
+
+    // Switch to Boards page
+    await channelsPage.globalHeader.switchProduct('Boards');
+
+    // Should have redirected to boards create page
+    const boardsCreatePage = new pages.BoardsCreatePage(page);
+    await boardsCreatePage.toBeVisible();
+
+    // Create empty board
+    await boardsCreatePage.createEmptyBoard();
+
+    // Should have redirected to boards view page
+    const boardsViewPage = new pages.BoardsViewPage(page);
+    await boardsViewPage.toBeVisible();
+    await boardsViewPage.shouldHaveUntitledBoard();
+
+    // Type new title and hit enter
+    const title = 'Testing';
+    await boardsViewPage.editableTitle.fill(title);
+    await boardsViewPage.editableTitle.press('Enter');
+
+    // Should update the title in heading and in sidebar
+    expect(await boardsViewPage.editableTitle.getAttribute('value')).toBe(title);
+    await boardsViewPage.sidebar.waitForTitle(title);
+
+    // Should create a duplicate board
+    await boardsViewPage.createDuplicateBoard(title);
+
+    // Should verify duplicate board is created
+    await boardsViewPage.shouldHaveBoardName(`${title} copy`);
+
+    // Should delete board and confirm deletion
+    await boardsViewPage.deleteBoard(`${title} copy`);
+
+});
